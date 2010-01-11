104ee29f4eac575914afae2b1facd7fb32b07c62
Fabien Potencier <fabien.potencier@gmail.com>
2010-01-11 15:57:07 +0100

    [DependencyInjection] added some extensions

---
+++ src/Symfony/Components/DependencyInjection/Loader/Extension/DoctrineExtension.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Symfony\Components\DependencyInjection\Loader\Extension;
+
+use Symfony\Components\DependencyInjection\Loader\LoaderExtension;
+use Symfony\Components\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Components\DependencyInjection\BuilderConfiguration;
+
+/*
+ * This file is part of the symfony framework.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * DoctrineExtension is an extension for the Doctrine DBAL and ORM library.
+ *
+ * @package    symfony
+ * @subpackage dependency_injection
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ */
+class DoctrineExtension extends LoaderExtension
+{
+  /**
+   * Loads the DBAL configuration.
+   *
+   * @param array $config A configuration array
+   *
+   * @return BuilderConfiguration A BuilderConfiguration instance
+   */
+  public function dbalLoad($config)
+  {
+    $configuration = new BuilderConfiguration();
+
+    $loader = new XmlFileLoader(__DIR__.'/xml/doctrine');
+    $configuration->merge($loader->load('dbal-1.0.xml'));
+
+    foreach (array('dbname', 'driverClass', 'host', 'username', 'password') as $key)
+    {
+      if (isset($config[$key]))
+      {
+        $configuration->setParameter('doctrine.dbal.'.$key, $config[$key]);
+      }
+    }
+
+    return $configuration;
+  }
+
+  /**
+   * Returns the namespace to be used for this extension (XML namespace).
+   *
+   * @return string The XML namespace
+   */
+  public function getNamespace()
+  {
+    return 'http://www.symfony-project.org/schema/doctrine';
+  }
+
+  /**
+   * Returns the recommanded alias to use in XML.
+   *
+   * This alias is also the mandatory prefix to use when using YAML.
+   *
+   * @return string The alias
+   */
+  public function getAlias()
+  {
+    return 'doctrine';
+  }
+}

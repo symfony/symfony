symfony/symfony

commit de37e5225b60a5265982d6062b6d68f277427ae0

[WebBundle] Fixing Console application for projects using subversion
Signed-off-by: Jordi Boggiano <j.boggiano@seld.be>

src/Symfony/Framework/WebBundle/Console/Application.php
@@ -92,7 +92,7 @@ class Application extends BaseApplication
         // look for commands
         foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($commandDir), \RecursiveIteratorIterator::LEAVES_ONLY) as $file)
         {
-          if ($file->isDir())
+          if ($file->isDir() || strpos($file, -4) !== '.php')
           {
             continue;
           }

commit 635135a4e04eaa5b88e2cc4120dd34ffb95cae7e

[WebBundle] Fixing regression introduced in de37e5225b60a5265982d6062b6d68f277427ae0
Signed-off-by: Jordi Boggiano <j.boggiano@seld.be>

src/Symfony/Framework/WebBundle/Console/Application.php
@@ -18,7 +18,7 @@ use Symfony\Foundation\Kernel;
  */
 
 /**
- * 
+ *
  *
  * @package    symfony
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
@@ -95,7 +95,7 @@ class Application extends BaseApplication
         // look for commands
         foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($commandDir), \RecursiveIteratorIterator::LEAVES_ONLY) as $file)
         {
-          if ($file->isDir() || strpos($file, -4) !== '.php')
+          if ($file->isDir() || substr($file, -4) !== '.php')
           {
             continue;
           }
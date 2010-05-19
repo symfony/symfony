e09d57ca8b7b834cabd06f71af9c829244526472
Fabien Potencier <fabien.potencier@gmail.com>
2010-05-19 09:45:49 +0200

    moved Doctrine migration commands to another bundle as migrations are not part of Doctrine core

---
+++ src/Symfony/Framework/DoctrineMigrationsBundle/Bundle.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Symfony\Framework\DoctrineMigrationsBundle;
+
+use Symfony\Foundation\Bundle\Bundle as BaseBundle;
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * Bundle.
+ *
+ * @package    Symfony
+ * @subpackage Framework_DoctrineBundle
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @author     Jonathan H. Wage <jonwage@gmail.com>
+ */
+class Bundle extends BaseBundle
+{
+}

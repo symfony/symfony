d3f9431757a3b8783810bd2861e304d30b59593e
Francois Zaninotto <francois@MacBook-Pro-de-Francois-Zaninotto.local>
2010-06-07 21:58:12 +0200

    [PropelBundle] Removed old model autoloader

--- src/Symfony/Framework/PropelBundle/Bundle.php
+++ src/Symfony/Framework/PropelBundle/Bundle.php
@@ -23,27 +23,6 @@ class Bundle extends BaseBundle
             set_include_path($container->getParameter('propel.phing_path').'/classes'.PATH_SEPARATOR.get_include_path());
         }
 
-        $kernel = $container->getKernelService();
-        if (!file_exists($autoload = $kernel->getCacheDir().'/propel_autoload.php')) {
-            $map = array();
-            foreach ($kernel->getBundles() as $bundle) {
-                if (!file_exists($file = $bundle->getPath().'/Resources/config/classmap.php')) {
-                    continue;
-                }
-
-                $local = include($file);
-                foreach ($local as $class => $path) {
-                    $map[$class] = $bundle->getPath().'/'.$path;
-                }
-            }
-
-            file_put_contents($autoload, '<?php return '.var_export($map, true).';');
-        }
-
-        $autoloader = \PropelAutoloader::getInstance();
-        $autoloader->addClassPaths(include($autoload));
-        $autoloader->register();
-
         $container->getPropelService();
     }
 }

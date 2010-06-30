ddc4e625a9a3cb594d9b78d59f0d5a48968e7d3c
Fabien Potencier <fabien.potencier@gmail.com>
2010-06-30 08:31:29 +0200

    added 2 scripts to ease installing and updating vendors

---
+++ update_vendors.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+CURRENT=`pwd`/vendor
+
+# Doctrine
+cd $CURRENT/doctrine && git pull
+
+# Doctrine migrations
+cd $CURRENT/doctrine-migrations && git pull
+
+# Doctrine MongoDB
+cd $CURRENT/doctrine-mongodb && git pull
+
+# Propel
+cd $CURRENT/propel && svn up
+
+# Phing
+cd $CURRENT/phing && svn up
+
+# Swiftmailer
+cd $CURRENT/swiftmailer && git pull
+
+# Twig
+cd $CURRENT/twig && git pull
+
+# Zend Framework
+cd $CURRENT/zend && git pull

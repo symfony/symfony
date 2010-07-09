3eec6b98fe2c83d7a0d2c7f51b55ab14a3ca77b9
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-07-09 08:05:21 -0700

    [Framework] Fixed test namespaces.

--- tests/Symfony/Tests/Framework/Debug/ErrorHandlerTest.php
+++ tests/Symfony/Tests/Framework/Debug/ErrorHandlerTest.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Tests\Foundation\Debug;
+namespace Symfony\Tests\Framework\Debug;
 
 use Symfony\Framework\Debug\ErrorHandler;
 
--- tests/Symfony/Tests/Framework/KernelTest.php
+++ tests/Symfony/Tests/Framework/KernelTest.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Tests\Foundation;
+namespace Symfony\Tests\Framework;
 
 use Symfony\Framework\Kernel;
 

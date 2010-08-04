05b47c4a078b53a64fa23a0779512b6cc55d7d01
Fabien Potencier <fabien.potencier@gmail.com>
2010-08-04 08:44:38 +0200

    [DoctrineMongoDBBundle] fixed use statement

--- src/Symfony/Bundle/DoctrineMongoDBBundle/DoctrineMongoDBBundle.php
+++ src/Symfony/Bundle/DoctrineMongoDBBundle/DoctrineMongoDBBundle.php
@@ -4,7 +4,7 @@ namespace Symfony\Bundle\DoctrineMongoDBBundle;
 
 use Symfony\Framework\Bundle\Bundle;
 use Symfony\Components\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Components\DependencyInjection\Loader\Loader;
+use Symfony\Components\DependencyInjection\ContainerBuilder;
 use Symfony\Bundle\DoctrineMongoDBBundle\DependencyInjection\MongoDBExtension;
 
 /**

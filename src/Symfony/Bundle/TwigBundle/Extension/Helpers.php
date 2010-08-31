8367df1265443a49f490f3ce90a356be81449e94
fivestar <ko.fivestar@gmail.com>
2010-08-31 11:48:47 +0900

    [TwigBundle] fixed grammar for render helper.

--- src/Symfony/Bundle/TwigBundle/Extension/Helpers.php
+++ src/Symfony/Bundle/TwigBundle/Extension/Helpers.php
@@ -47,7 +47,7 @@ class Helpers extends \Twig_Extension
             new HelperTokenParser('route', '<route> [with <arguments:array>]', 'router', 'generate'),
 
             // {% render 'BlogBundle:Post:list' with ['limit': 2], ['alt': 'BlogBundle:Post:error'] %}
-            new HelperTokenParser('render', '<template> [with <attributes:array>, [<options:array>]]', 'actions', 'render'),
+            new HelperTokenParser('render', '<template> [with <attributes:array>[, <options:array>]]', 'actions', 'render'),
 
             // {% flash 'notice' %}
             new HelperTokenParser('flash', '<name>', 'session', 'flash'),

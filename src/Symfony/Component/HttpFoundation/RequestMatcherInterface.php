4f337615e3b465f5f325a4ef50dac1def32fdda9
Fabien Potencier <fabien.potencier@gmail.com>
2010-09-01 13:56:06 +0200

    [HttpFoundation] added request matcher

---
+++ src/Symfony/Component/HttpFoundation/RequestMatcherInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Symfony\Component\HttpFoundation;
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * RequestMatcherInterface is an interface for strategies to match a Request.
+ *
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ */
+interface RequestMatcherInterface
+{
+    /**
+     * Decides whether the rule(s) implemented by the strategy matches the supplied request.
+     *
+     * @param  Request $request The request to check for a match
+     *
+     * @return Boolean true if the request matches, false otherwise
+     */
+    function matches(Request $request);
+}

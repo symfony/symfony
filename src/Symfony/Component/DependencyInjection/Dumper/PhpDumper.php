4c17ce8e5e6cc3657691d19f261e6cd162951cad
Fabien Potencier <fabien.potencier@gmail.com>
2010-09-03 11:38:21 +0200

    changed calls to has() to use the new proxy method

--- src/Symfony/Component/DependencyInjection/Dumper/PhpDumper.php
+++ src/Symfony/Component/DependencyInjection/Dumper/PhpDumper.php
@@ -412,7 +412,7 @@ EOF;
 
     public function dumpParameter($name)
     {
-        if ($this->container->isFrozen() && $this->container->getParameterBag()->has($name)) {
+        if ($this->container->isFrozen() && $this->container->has($name)) {
             return $this->dumpValue($this->container->getParameter($name), false);
         }
 

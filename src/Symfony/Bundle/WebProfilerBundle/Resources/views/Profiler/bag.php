2f8db9135a25a32fc77a9f8387f1f4b350f02cff
Fabien Potencier <fabien.potencier@gmail.com>
2010-09-06 15:25:33 +0200

    fixed webprofiler on Windows (closes #9045)

--- src/Symfony/Bundle/WebProfilerBundle/Resources/views/Profiler/bag.php
+++ src/Symfony/Bundle/WebProfilerBundle/Resources/views/Profiler/bag.php
@@ -6,7 +6,17 @@
     <?php foreach ($bag->keys() as $key): ?>
         <tr>
             <th><?php echo $key ?></th>
-            <td><?php echo $bag->get($key) ?></td>
+            <td>
+                <?php if (is_object($bag->get($key))): ?>
+                    <em>Object</em>
+                <?php elseif (is_resource($bag->get($key))): ?>
+                    <em>Resource</em>
+                <?php elseif (is_array($bag->get($key))): ?>
+                    <em>Array</em>
+                <?php else: ?>
+                    <?php echo $bag->get($key) ?>
+                <?php endif; ?>
+            </td>
         </tr>
     <?php endforeach; ?>
 </table>

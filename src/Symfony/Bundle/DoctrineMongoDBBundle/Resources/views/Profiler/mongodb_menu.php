e6bff045c994de097f2feca67d53753c8e08a552
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-09-13 14:10:51 -0700

    [DoctrineMongoDBBundle] added a quick profiler panel

--- src/Symfony/Bundle/DoctrineMongoDBBundle/Resources/views/Profiler/mongodb_menu.php
+++ src/Symfony/Bundle/DoctrineMongoDBBundle/Resources/views/Profiler/mongodb_menu.php
@@ -1,2 +1,3 @@
+<div class="count"><?php echo $data->getQueryCount() ?></div>
 <img style="margin: 0 5px 0 0; vertical-align: middle; width: 32px" alt="" src="<?php echo $view->get('assets')->getUrl('bundles/webprofiler/images/db.png') ?>" />
 Doctrine MongoDB
--- src/Symfony/Bundle/DoctrineMongoDBBundle/Resources/views/Profiler/mongodb_panel.php
+++ src/Symfony/Bundle/DoctrineMongoDBBundle/Resources/views/Profiler/mongodb_panel.php
@@ -1,6 +1,17 @@
 <h2>Queries</h2>
 
-<?php if (0 == $data->getQueryCount()): ?>
+<?php if (false === $queries = $data->getQueries()): ?>
+    <em>Query logging is disabled.</em>
+<?php elseif (0 == $data->getQueryCount()): ?>
     <em>No queries.</em>
 <?php else: ?>
+    <ul class="alt">
+        <?php foreach ($queries as $i => $query): ?>
+            <li class="<?php echo $i % 2 ? 'odd' : 'even' ?>">
+                <div>
+                    <code><?php echo $query ?></code>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 <?php endif; ?>

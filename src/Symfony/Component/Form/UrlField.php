733290c112a86b7147b0c477d28bde6f74e702ef
Bernhard Schussek <bernhard.schussek@symfony-project.com>
2010-10-19 15:20:55 +0200

    [Form] Implemented UrlField

---
+++ src/Symfony/Component/Form/UrlField.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Symfony\Component\Form;
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * Field for entering URLs
+ *
+ * @author Bernhard Schussek <bernhard.schussek@symfony-project.com>
+ */
+class UrlField extends InputField
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this->addOption('default_protocol', 'http');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function processData($data)
+    {
+        $protocol = $this->getOption('default_protocol');
+
+        if ($protocol && $data && !preg_match('~^\w+://~', $data)) {
+            $data = $protocol . '://' . $data;
+        }
+
+        return $data;
+    }
+}

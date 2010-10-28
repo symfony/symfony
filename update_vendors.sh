b9f33a610ee956647d3321cebac621abf97d1313
Fabien Potencier <fabien.potencier@gmail.com>
2010-10-28 14:52:34 +0200

    removed code related to Propel support

--- update_vendors.sh
+++ update_vendors.sh
@@ -20,12 +20,6 @@ cd $CURRENT/doctrine-migrations && git pull
 # Doctrine MongoDB
 cd $CURRENT/doctrine-mongodb && git pull
 
-# Propel
-cd $CURRENT/propel && svn up
-
-# Phing
-cd $CURRENT/phing && svn up
-
 # Swiftmailer
 cd $CURRENT/swiftmailer && git pull
 

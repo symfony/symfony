f21c58c56b2bfc811538a937663f14b969436a62
Fabien Potencier <fabien.potencier@gmail.com>
2010-11-15 11:13:01 +0100

    [FrameworkBundle] finished code migration after the removal of the ArrayAccess interface for the Container

--- src/Symfony/Bundle/FrameworkBundle/FrameworkBundle.php
+++ src/Symfony/Bundle/FrameworkBundle/FrameworkBundle.php
@@ -27,7 +27,7 @@ class FrameworkBundle extends Bundle
     public function boot()
     {
         if ($this->container->has('error_handler')) {
-            $this->container['error_handler'];
+            $this->container->get('error_handler');
         }
 
         if ($this->container->hasParameter('csrf_secret')) {

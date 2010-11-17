ac0081f8b9b7cdc3ed037374a0ce41115a5bf112
Jordi Boggiano <j.boggiano@seld.be>
2010-11-17 16:04:57 +0100

    Switching doctypes to HTML5, it's the future

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Exception/layout.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Exception/layout.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $view->getCharset() ?>"/>
         <title><?php echo $exception->getMessage() ?> (<?php echo $exception->getStatusCode() ?> <?php echo $exception->getStatusText() ?>)</title>

0c0853c6364557873e80ab412abcb81bc67d19ff
Fabien Potencier <fabien.potencier@gmail.com>
2010-12-10 09:30:44 +0100

    fixed unit tests

--- tests/Symfony/Tests/Component/HttpKernel/Exception/FlattenExceptionTest.php
+++ tests/Symfony/Tests/Component/HttpKernel/Exception/FlattenExceptionTest.php
@@ -21,7 +21,6 @@ class FlattenExceptionTest extends \PHPUnit_Framework_TestCase
     {
         $flattened = FlattenException::create($exception);
 
-        $this->assertEquals($statusCode, $flattened->getStatusCode(), 'A HttpKernel exception uses the error code as the status code.');
         $this->assertEquals($exception->getMessage(), $flattened->getMessage(), 'The message is copied from the original exception.');
         $this->assertEquals($exception->getCode(), $flattened->getCode(), 'The code is copied from the original exception.');
         $this->assertEquals(get_class($exception), $flattened->getClass(), 'The class is set to the class of the original exception');
@@ -31,15 +30,7 @@ class FlattenExceptionTest extends \PHPUnit_Framework_TestCase
     public function flattenDataProvider()
     {
         return array(
-            array(new TestHttpException('test', 404), 404),
             array(new \Exception('test', 123), 500),
         );
     }
-}
-
-use Symfony\Component\HttpKernel\Exception\HttpException;
-
-// stub Exception class that extends HttpException
-class TestHttpException extends HttpException
-{
 }

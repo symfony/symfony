b2476719f6710898932ffb07c5450b1da7d11708
Victor Berchet <victor.berchet@yahoo.com>
2010-12-21 10:57:57 +0100

    Container PHP dumper: optimize string dumping ('f'.'o'.'o' -> 'foo')

--- src/Symfony/Component/DependencyInjection/Dumper/PhpDumper.php
+++ src/Symfony/Component/DependencyInjection/Dumper/PhpDumper.php
@@ -436,7 +436,7 @@ EOF;
                 $code = str_replace('%%', '%', preg_replace_callback('/(?<!%)(%)([^%]+)\1/', $replaceParameters, var_export($value, true)));
 
                 // optimize string
-                $code = preg_replace(array("/^''\./", "/\.''$/", "/\.''\./"), array('', '', '.'), $code);
+                $code = preg_replace(array("/^''\./", "/\.''$/", "/'\.'/", "/\.''\./"), array('', '', '', '.'), $code);
 
                 return $code;
             }

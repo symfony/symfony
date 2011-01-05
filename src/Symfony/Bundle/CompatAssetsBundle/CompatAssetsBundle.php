598d458a3c5e45f67a2f6686433272abde29b1e3
Fabien Potencier <fabien.potencier@gmail.com>
2011-01-05 14:11:43 +0100

    added CompatAssetsBundle to reintroduce the deprecated css/js helpers/tags
    Just add this line in your configuration to enable it:
    
        <import resource="CompatAssetsBundle/Resources/config/assets.xml" />
    
    This bundle is just to ease the upgrade path. Please don't use it if you don't need to
    and upgrade your templates as this bundle will be removed before RC1.

---
+++ src/Symfony/Bundle/CompatAssetsBundle/CompatAssetsBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Symfony\Bundle\CompatAssetsBundle;
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * CompatAssetsBundle.
+ *
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ */
+class CompatAssetsBundle extends Bundle
+{
+}

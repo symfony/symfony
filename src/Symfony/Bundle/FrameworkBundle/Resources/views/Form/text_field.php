17acdd971cbbe16e7db8e3a748f7cf5da5dcb47a
Bernhard Schussek <bernhard.schussek@symfony-project.com>
2011-01-05 12:34:58 +0100

    [FrameworkBundle] Fixed maxlength attribute in TextField in PHP templates

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/text_field.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/text_field.php
@@ -3,6 +3,6 @@
     name="<?php echo $field->getName() ?>"
     value="<?php echo $field->getDisplayedData() ?>"
     <?php if ($field->isDisabled()): ?>disabled="disabled"<?php endif ?>
-    <?php // FIXME if (!isset($attr['maxlength']) && $field->getMaxLength() > 0) $attr['maxlength'] = $field->getMaxLength() ?>
+    <?php if (!isset($attr['maxlength']) && $field->getMaxLength() > 0) $attr['maxlength'] = $field->getMaxLength() ?>
     <?php echo $view['form']->attributes($attr) ?>
 />

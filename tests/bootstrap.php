f598182afc54972c677952d8b32a75cdd32a1a6c
umpirsky <umpirsky@gmail.com>
2011-01-07 15:09:46 +0100

    Set default timezone on bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,8 @@
  * file that was distributed with this source code.
  */
 
+date_default_timezone_set('Europe/Paris');
+ 
 if (file_exists($file = __DIR__.'/../autoload.php')) {
     require_once $file;
 } elseif (file_exists($file = __DIR__.'/../autoload.php.dist')) {

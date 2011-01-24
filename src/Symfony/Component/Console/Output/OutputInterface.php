0ef67112b252c0fa6a307c79a1c40f9d72bdc996
Andy Stanberry <andystanberry@gmail.com>
2011-01-24 16:33:24 -0500

    [Console] added writeln to the OutputInterface

--- src/Symfony/Component/Console/Output/OutputInterface.php
+++ src/Symfony/Component/Console/Output/OutputInterface.php
@@ -30,6 +30,14 @@ interface OutputInterface
     function write($messages, $newline = false, $type = 0);
 
     /**
+     * Writes a message to the output and adds a newline at the end.
+     *
+     * @param string|array $messages The message as an array of lines of a single string
+     * @param integer      $type     The type of output
+     */
+    function writeln($messages, $type = 0);
+
+    /**
      * Sets the verbosity of the output.
      *
      * @param integer $level The level of verbosity

d245fb8edc575173dc16aee162f60f2fc2bf5d5f
Eriksen Costa <eriksen.costa@infranology.com.br>
2011-01-29 21:56:54 -0200

    [Locale] partialy implemented \NumberFormatter's format() and formatCurrency() methods in PHP

--- src/Symfony/Component/Locale/SimpleNumberFormatter.php
+++ src/Symfony/Component/Locale/SimpleNumberFormatter.php
@@ -19,15 +19,75 @@ use Symfony\Component\Locale\NumberFormatterInterface;
  */
 class SimpleNumberFormatter implements NumberFormatterInterface
 {
-    private $formatter = null;
+    /**
+     * Default values for the en locale.
+     */
+    private $attributes = array(
+        self::FRACTION_DIGITS => 0,
+        self::GROUPING_USED   => 1,
+        self::ROUNDING_MODE   => self::ROUND_HALFEVEN
+    );
 
     /**
-     * @see http://source.icu-project.org/repos/icu/icu/trunk/source/data/curr/en.txt
+     * The supported styles to the constructor $styles argument.
+     */
+    private static $supportedStyles = array(
+        'CURRENCY' => self::CURRENCY,
+        'DECIMAL'  => self::DECIMAL
+    );
+
+    /**
+     * Supported attributes to the setAttribute() $attr argument.
+     */
+    private static $supportedAttributes = array(
+        'FRACTION_DIGITS' => self::FRACTION_DIGITS,
+        'GROUPING_USED'   => self::GROUPING_USED,
+        'ROUNDING_MODE'   => self::ROUNDING_MODE
+    );
+
+    /**
+     * The available rounding modes for setAttribute() usage with
+     * SimpleNumberFormatter::ROUNDING_MODE. SimpleNumberFormatter::ROUND_DOWN
+     * and SimpleNumberFormatter::ROUND_UP does not have a PHP only equivalent.
+     */
+    private static $roundingModes = array(
+        'ROUND_CEILING'  => self::ROUND_CEILING,
+        'ROUND_FLOOR'    => self::ROUND_FLOOR,
+        'ROUND_HALFEVEN' => self::ROUND_HALFEVEN,
+        'ROUND_HALFDOWN' => self::ROUND_HALFDOWN,
+        'ROUND_HALFUP'   => self::ROUND_HALFUP
+    );
+
+    /**
+     * The available values for setAttribute() usage with
+     * SimpleNumberFormatter::GROUPING_USED.
+     */
+    private static $groupingUsedValues = array(0, 1);
+
+    /**
+     * The mapping between \NumberFormatter rounding modes to the available
+     * modes in PHP's round() function.
+     *
+     * @see http://www.php.net/manual/en/function.round.php
+     */
+    private static $phpRoundingMap = array(
+        self::ROUND_HALFDOWN => \PHP_ROUND_HALF_DOWN,
+        self::ROUND_HALFEVEN => \PHP_ROUND_HALF_EVEN,
+        self::ROUND_HALFUP   => \PHP_ROUND_HALF_UP
+    );
+
+    /**
+     * The currencies symbols. Each array have the symbol definition in
+     * hexadecimal and the decimal digits.
+     *
+     * @see  http://source.icu-project.org/repos/icu/icu/trunk/source/data/curr/en.txt
+     * @todo Move this to Resources/data and use \ResourceBundle to load the data.
+     * @todo Search in the icu data where the currency subunits (usage of cents) are defined
      */
     private $currencies = array(
-        'ALL' => array('0x410x4c0x4c', '%.0f'),
-        'BRL' => array('0x520x24', '%.2f'),
-        'CRC' => array('0xe20x820xa1', '%.0f')
+        'ALL' => array('0x410x4c0x4c', 0),
+        'BRL' => array('0x520x24', 2),
+        'CRC' => array('0xe20x820xa1', 0)
     );
 
     /**
@@ -35,33 +95,58 @@ class SimpleNumberFormatter implements NumberFormatterInterface
      */
     public function __construct($locale = 'en', $style = null, $pattern = null)
     {
+        if ('en' != $locale) {
+            throw new \InvalidArgumentException('Unsupported $locale value. Only the \'en\' locale is supported. Install the intl extension for full localization capabilities.');
+        }
+
+        if (!in_array($style, self::$supportedStyles)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Unsupported $style value. The available styles are: %s. Install the intl extension for full localization capabilities.',
+                implode(', ', array_keys(self::$supportedStyles))
+            ));
+        }
+
+        if (!is_null($pattern)) {
+            throw new \InvalidArgumentException('The $pattern value must be null. Install the intl extension for full localization capabilities.');
+        }
     }
 
     /**
      * @{inheritDoc}
+     * @todo With the default rounding mode (ROUND_HALFEVEN), the currency value
+     *       seems to be correctly rounded. However, since ROUND_CEILING is
+     *       mapping to the ceil() function, the value is being returned. This
+     *       is wrong.
      */
     public function formatCurrency($value, $currency)
     {
-        $symbol = '';
-        $hexSymbol = $this->currencies[$currency][0];
-        $format    = $this->currencies[$currency][1];
+        $symbol = $this->getCurrencySymbol($currency);
+        $value  = $this->round($value, $this->currencies[$currency][1]);
 
-        $hex = explode('0x', $hexSymbol);
-        unset($hex[0]);
-
-        foreach ($hex as $h) {
-            $symbol .= chr(hexdec($h));
+        $negative = false;
+        if (0 > $value) {
+            $negative = true;
+            $value *= -1;
         }
 
-        return sprintf('%s'.$format, $symbol, $value);
+        $value = $this->formatNumber($value, $this->currencies[$currency][1]);
+
+        $ret = $symbol.$value;
+        return $negative ? '('.$ret.')' : $ret;
     }
 
     /**
      * @{inheritDoc}
      */
-    public function format($value, $type = null)
+    public function format($value, $type = self::TYPE_DEFAULT)
     {
+        if (0 > ($fractionDigits = $this->getAttribute(self::FRACTION_DIGITS))) {
+            $fractionDigits = 0;
+        }
 
+        // Rounding
+        $value = $this->round($value, $fractionDigits);
+        return $this->formatNumber($value, $fractionDigits);
     }
 
     /**
@@ -69,7 +154,9 @@ class SimpleNumberFormatter implements NumberFormatterInterface
      */
     public function getAttribute($attr)
     {
-
+        if (isset($this->attributes[$attr])) {
+            return $this->attributes[$attr];
+        }
     }
 
     /**
@@ -138,10 +225,30 @@ class SimpleNumberFormatter implements NumberFormatterInterface
 
     /**
      * @{inheritDoc}
+     * @todo Decide between throwing an exception if ROUDING_MODE or GROUPING_USED are invalid.
+     *       In \NumberFormatter, a true is returned and the format/parse() methods have undefined values
+     *       in these cases.
+     * @throws InvalidArgumentException  When the $attr is not supported
      */
     public function setAttribute($attr, $value)
     {
+        if (!in_array($attr, self::$supportedAttributes)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Unsupported $attr value. The available attributes are: %s. Install the intl extension for full localization capabilities.',
+                implode(', ', array_keys(self::$supportedAttributes))
+            ));
+        }
 
+        if (self::$supportedAttributes['ROUNDING_MODE'] == $attr && $this->isInvalidRoundingMode($value)) {
+            return false;
+        }
+
+        if (self::$supportedAttributes['GROUPING_USED'] == $attr && $this->isInvalidGroupingUsedValue($value)) {
+            return false;
+        }
+
+        $this->attributes[$attr] = $value;
+        return true;
     }
 
     /**
@@ -166,5 +273,92 @@ class SimpleNumberFormatter implements NumberFormatterInterface
     public function setTextAttribute($attr, $value)
     {
 
+    }
+
+    /**
+     * Returns the currency symbol.
+     *
+     * @param  string   $currency   The 3-letter ISO 4217 currency code indicating the currency to use
+     * @return string               The currency symbol
+     */
+    private function getCurrencySymbol($currency)
+    {
+        $symbol = '';
+        $hexSymbol = $this->currencies[$currency][0];
+        $hex = explode('0x', $hexSymbol);
+        unset($hex[0]);
+
+        foreach ($hex as $h) {
+            $symbol .= chr(hexdec($h));
+        }
+
+        return $symbol;
+    }
+
+    /**
+     * Rounds a value.
+     *
+     * @param  numeric   $value      The value to round
+     * @param  int       $precision  The number of decimal digits to round to
+     * @return numeric               The rounded value
+     */
+    private function round($value, $precision)
+    {
+        switch ($this->getAttribute(self::ROUNDING_MODE)):
+            case self::ROUND_CEILING:
+                $value = ceil($value);
+                break;
+            case self::ROUND_FLOOR:
+                $value = floor($value);
+                break;
+            default:
+                $roundingMode = self::$phpRoundingMap[$this->getAttribute(self::ROUNDING_MODE)];
+                $value = round($value, $precision, $roundingMode);
+                break;
+            endswitch;
+
+        return $value;
+    }
+
+    /**
+     * Formats a number.
+     *
+     * @param  numeric   $value      The numeric value to format
+     * @param  int       $precision  The number of decimal digits to use
+     * @return string                The formatted number
+     */
+    private function formatNumber($value, $precision)
+    {
+        return number_format($value, $precision, '.', $this->getAttribute(self::GROUPING_USED) ? ',' : '');
+    }
+
+    /**
+     * Check if the rounding mode is invalid.
+     *
+     * @param  int    $value  The rounding mode value to check
+     * @return bool           true if the rounding mode is invalid, false otherwise
+     */
+    private function isInvalidRoundingMode($value)
+    {
+        if (in_array($value, self::$roundingModes, true)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if the grouping value is invalid.
+     *
+     * @param  int    $value  The grouping value to check
+     * @return bool           true if the grouping value is invalid, false otherwise
+     */
+    private function isInvalidGroupingUsedValue($value)
+    {
+        if (in_array($value, self::$groupingUsedValues, true)) {
+            return false;
+        }
+
+        return true;
     }
 }
--- tests/Symfony/Tests/Component/Locale/SimpleNumberFormatterTest.php
+++ tests/Symfony/Tests/Component/Locale/SimpleNumberFormatterTest.php
@@ -16,26 +16,100 @@ use Symfony\Component\Locale\SimpleNumberFormatter;
 
 class SimpleNumberFormatterTest extends \PHPUnit_Framework_TestCase
 {
-    private $formatter = null;
-
-    public function setUp()
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testConstructorWithUnsupportedLocale()
     {
-        $this->formatter = new SimpleNumberFormatter();
+        $formatter = new SimpleNumberFormatter('pt_BR');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testConstructorWithUnsupportedStyle()
+    {
+        $formatter = new SimpleNumberFormatter('en', SimpleNumberFormatter::PATTERN_DECIMAL);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testConstructorWithPatternDifferentThanNull()
+    {
+        $formatter = new SimpleNumberFormatter('en', SimpleNumberFormatter::DECIMAL, '');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetAttributeWithUnsupportedAttribute()
+    {
+        $formatter = new SimpleNumberFormatter('en', SimpleNumberFormatter::DECIMAL);
+        $formatter->setAttribute(SimpleNumberFormatter::LENIENT_PARSE, null);
+    }
+
+    public function testSetAttributeInvalidRoundingMode()
+    {
+        $formatter = new SimpleNumberFormatter('en', SimpleNumberFormatter::DECIMAL);
+
+        $ret = $formatter->setAttribute(SimpleNumberFormatter::ROUNDING_MODE, null);
+        $roundingMode = $formatter->getAttribute(SimpleNumberFormatter::ROUNDING_MODE);
+
+        $this->assertFalse($ret);
+        $this->assertEquals(SimpleNumberFormatter::ROUND_HALFEVEN, $roundingMode);
+    }
+
+    public function testSetAttributeInvalidGroupingUsedValue()
+    {
+        $formatter = new SimpleNumberFormatter('en', SimpleNumberFormatter::DECIMAL);
+
+        $ret = $formatter->setAttribute(SimpleNumberFormatter::GROUPING_USED, null);
+        $groupingUsed = $formatter->getAttribute(SimpleNumberFormatter::GROUPING_USED);
+
+        $this->assertFalse($ret);
+        $this->assertEquals(SimpleNumberFormatter::GROUPING_USED, $groupingUsed);
+    }
+
+    public function testFormat()
+    {
+        $formatter = new SimpleNumberFormatter('en', SimpleNumberFormatter::DECIMAL);
+
+        // Rounds to the next highest integer
+        $formatter->setAttribute(SimpleNumberFormatter::ROUNDING_MODE, SimpleNumberFormatter::ROUND_CEILING);
+        $this->assertSame('10', $formatter->format(9.5));
+
+        // Use the defined fraction digits
+        $formatter->setAttribute(SimpleNumberFormatter::FRACTION_DIGITS, 2);
+        $this->assertSame('10.00', $formatter->format(9.5));
+        $formatter->setAttribute(SimpleNumberFormatter::FRACTION_DIGITS, -1);
+        $this->assertSame('10', $formatter->format(9.5));
+
+        // Set the grouping size
+        $formatter->setAttribute(SimpleNumberFormatter::ROUNDING_MODE, SimpleNumberFormatter::ROUND_HALFEVEN);
+        $formatter->setAttribute(SimpleNumberFormatter::FRACTION_DIGITS, 2);
+        $this->assertSame('9.56', $formatter->format(9.555));
+        $this->assertSame('1,000,000.12', $formatter->format(1000000.123));
     }
 
     /**
      * @dataProvider formatCurrencyProvider
+     * @see  SimpleNumberFormatter::formatCurrency()
+     * @todo Test with ROUND_CEILING and ROUND_FLOOR modes
      */
     public function testFormatCurrency($value, $currency, $expected)
     {
-        // just for testing purposes
-        $f = new \NumberFormatter('en', \NumberFormatter::CURRENCY);
+        $formatter = new SimpleNumberFormatter('en', SimpleNumberFormatter::DECIMAL);
+        $this->assertEquals($expected, $formatter->formatCurrency($value, $currency));
 
-        $this->assertEquals(
-            //$expected,
-            $f->formatCurrency($value, $currency),
-            $this->formatter->formatCurrency($value, $currency)
-        );
+        if (extension_loaded('intl')) {
+            $numberFormatter = new \NumberFormatter('en', \NumberFormatter::CURRENCY);
+
+            $this->assertEquals(
+                $numberFormatter->formatCurrency($value, $currency),
+                $formatter->formatCurrency($value, $currency)
+            );
+        }
     }
 
     public function formatCurrencyProvider()
@@ -43,7 +117,17 @@ class SimpleNumberFormatterTest extends \PHPUnit_Framework_TestCase
         return array(
             array(100, 'ALL', 'ALL100'),
             array(100, 'BRL', 'R$100.00'),
-            array(100, 'CRC', '₡100')
+            array(100, 'CRC', '₡100'),
+            array(-100, 'ALL', '(ALL100)'),
+            array(-100, 'BRL', '(R$100.00)'),
+            array(-100, 'CRC', '(₡100)'),
+            array(1000.12, 'ALL', 'ALL1,000'),
+            array(1000.12, 'BRL', 'R$1,000.12'),
+            array(1000.12, 'CRC', '₡1,000'),
+            // Test with other rounding modes
+            // array(1000.127, 'ALL', 'ALL1,000'),
+            // array(1000.127, 'BRL', 'R$1,000.12'),
+            // array(1000.127, 'CRC', '₡1,000'),
         );
     }
 }

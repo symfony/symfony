c24a50f839a6163042b1ce73f209bb9b1193ee13
Jeremy Mikola <jmikola@gmail.com>
2011-02-06 04:29:16 -0500

    [FrameworkBundle] Integrate Configuration\Builder class for config merging and normalization
    This fixes some BC problems introduced in f9138d313b83f951cf82a2f7f902a2d6dd14fbb3. Some top-level can now be simply enabled by providing true/null in PHP/YAML. Additionally, the Configuration\Builder allows options to be unset by providing "false" (helpful for overriding activation in a previous config file). All options supporting these behaviors can be found in the Configuration.php file (look for canBeUnset() and treatNull/TrueLike()).
    
    Major changes:
    
     * Removed "enabled" option for profiler config. Profiler is now enabled if its config is true, null or a map.
     * Restore original config structure for validation namespaces. In PHP/YAML, namespaces are defined under annotations as an alternative to false (disabled) and true/null (enabled). For XML, annotation remains a boolean attribute for validation and a one or more optional namespace tags may appear within <app:validation />. During config normalization, namespace tags under validation will be moved to annotations to conform to the PHP/YAML structure (this occurs transparently to the user).
     * Restore behavior for router/templating config sections being optional (as shown in changes to session/validation test fixtures). If either top-level section is unset in the configuration, neither feature will be enabled and the user will no longer receive exceptions due to missing a resource option (router) or engines (templating). Resource/engines will still be properly required if the respective feature is enabled.
     * Remove unused router type option from XML config XSD. Type is only relevant for import statements, so this option is likely useless.
    
    Additional small changes:
    
     * Added isset()'s, since config options may be unset
     * Wrap registerXxxConfiguration() calls in isset() checks
     * Load translation.xml in configLoad(), since it's always required
     * Default cache_warmer value (!kernel.debug) is determined via Configuration class
    
    Things to be fixed:
    
     * Configuration\Builder doesn't seem to respect isRequired() and requiresAtLeastOneElement() (or I haven't set it properly); this should replace the need for FrameworkExtension to throw exceptions for bad router/templating configs
     * The config nodes for session options don't have the "pdo." prefix, as dots are not allowed in node names. To preserve BC for now, the "pdo." prefix is still allowed (and mandated by XSD) in configuration files. In the future, we may just want to do away with the "pdo." prefix.
     * Translator has an "enabled" option. If there's no use case for setting "fallback" independently (when "enabled" is false), perhaps "enabled" should be removed entirely and translator should function like profiler currently does.
     * Profiler matcher merging might need to be adjusted so multiple configs simply overwrite matcher instead of merging its array keys.

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/session_pdo.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/session_pdo.php
@@ -1,17 +1,11 @@
 <?php
 
 $container->loadFromExtension('app', 'config', array(
-    'router' => array(
-        'resource' => '%kernel.root_dir%/config/routing.xml',
-    ),
     'session' => array(
         'storage_id'      => 'pdo',
         'pdo.db_table'    => 'table',
         'pdo.db_id_col'   => 'id',
         'pdo.db_data_col' => 'data',
         'pdo.db_time_col' => 'time',
-    ),
-    'templating' => array(
-        'engine' => 'php'
     ),
 ));
--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/validation_annotations.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/validation_annotations.php
@@ -1,17 +1,12 @@
 <?php
 
 $container->loadFromExtension('app', 'config', array(
-    'router' => array(
-        'resource' => '%kernel.root_dir%/config/routing.xml',
-    ),
     'validation' => array(
         'enabled'     => true,
-        'annotations' => true,
-        'namespaces' => array(
-            'app' => 'Application\\Validator\\Constraints\\',
+        'annotations' => array(
+            'namespaces' => array(
+                'app' => 'Application\\Validator\\Constraints\\',
+            ),
         ),
-    ),
-    'templating' => array(
-        'engine' => 'php'
     ),
 ));

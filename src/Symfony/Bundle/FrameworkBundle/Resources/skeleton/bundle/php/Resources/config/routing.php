13469a2d8269178d08b02db78d6cbf734b96cc5f
Fabrice Bernhard <fabriceb@theodo.fr>
2011-02-08 01:05:42 +0100

    [FrameworkBundle] added --format=xml,php,yml optional parameter to the init:bundle command
    * Added the --format parameter to the InitBundleCommand.php file
    * Moved all the non-format-dependent files from Resources/skeleton/bundle to Resources/skeleton/bundle/generic
    * Created Resources/skeleton/bundle/[php,yml,xml] subfolders containing the files config/routing.[xml,yml,php]

---
+++ src/Symfony/Bundle/FrameworkBundle/Resources/skeleton/bundle/php/Resources/config/routing.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+
+$collection = new RouteCollection();
+/*
+$collection->add('homepage', new Route('/', array(
+    '_controller' => 'NewBundle:Default:index',
+)));
+*/
+return $collection;

b588caf46791ebf371bb98b0183f5d89ee93a1d0
Fabien Potencier <fabien.potencier@gmail.com>
2011-02-11 00:13:02 +0100

    [DependencyInjection] made a small change to mimic a similar change in Routing

--- src/Symfony/Component/DependencyInjection/Loader/FileLoader.php
+++ src/Symfony/Component/DependencyInjection/Loader/FileLoader.php
@@ -51,7 +51,7 @@ abstract class FileLoader extends Loader
             $loader = $this->resolve($resource);
 
             if ($loader instanceof FileLoader && null !== $this->currentDir) {
-                $resource = $this->locator->getAbsolutePath($resource, $this->currentDir);
+                $resource = $this->locator->locate($resource, $this->currentDir);
             }
 
             $loader->load($resource);

3bfb59907415c7d79712cdaad1afd4142a543936
Victor Berchet <victor.berchet@yahoo.com>
2011-02-24 15:44:07 +0100

    [Config] Simplify ScalarNode by inheriting from VariableNode

--- src/Symfony/Component/Config/Definition/ScalarNode.php
+++ src/Symfony/Component/Config/Definition/ScalarNode.php
@@ -11,7 +11,7 @@
 
 namespace Symfony\Component\Config\Definition;
 
-use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Config\Definition\VariableNode;
 use Symfony\Component\Config\Definition\Exception\InvalidTypeException;
 
 /**
@@ -26,56 +26,8 @@ use Symfony\Component\Config\Definition\Exception\InvalidTypeException;
  *
  * @author Johannes M. Schmitt <schmittjoh@gmail.com>
  */
-class ScalarNode extends BaseNode implements PrototypeNodeInterface
+class ScalarNode extends VariableNode
 {
-    protected $defaultValueSet = false;
-    protected $defaultValue;
-    protected $allowEmptyValue = true;
-
-    /**
-     * {@inheritDoc}
-     */
-    public function setDefaultValue($value)
-    {
-        $this->defaultValueSet = true;
-        $this->defaultValue = $value;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function hasDefaultValue()
-    {
-        return $this->defaultValueSet;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getDefaultValue()
-    {
-        return $this->defaultValue;
-    }
-
-    /**
-     * Sets if this node is allowed to have an empty value.
-     *
-     * @param boolean $boolean True if this entity will accept empty values.
-     * @return void
-     */
-    public function setAllowEmptyValue($boolean)
-    {
-        $this->allowEmptyValue = (Boolean) $boolean;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
     /**
      * {@inheritDoc}
      */
@@ -88,37 +40,5 @@ class ScalarNode extends BaseNode implements PrototypeNodeInterface
                 json_encode($value)
             ));
         }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function finalizeValue($value)
-    {
-        if (!$this->allowEmptyValue && empty($value)) {
-            throw new InvalidConfigurationException(sprintf(
-                'The path "%s" cannot contain an empty value, but got %s.',
-                $this->getPath(),
-                json_encode($value)
-            ));
-        }
-
-        return $value;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function normalizeValue($value)
-    {
-        return $value;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function mergeValues($leftSide, $rightSide)
-    {
-        return $rightSide;
     }
 }

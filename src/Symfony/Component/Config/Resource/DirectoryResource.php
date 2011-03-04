a93aa5519967c2bd6aa76c1cb9e49b47863fc1a2
develop <develop@symfony.localdomain>
2011-03-04 14:17:12 +0100

    If you use an annotation resource pointing to a controller directory in your routing configuration, only existing files in this directory get tracked for changes in this directory. So if you add a new controller file in this directory you'd have to manually clear the cache since the new file gets ignored. This patch adds a DirectoryResource for this case which tracks all changes in the given directory (and files and directories contained within).

---
+++ src/Symfony/Component/Config/Resource/DirectoryResource.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Config\Resource;
+
+/**
+ * DirectoryResource represents a resources stored in a subdirectory tree.
+ *
+ * @author Fabien Potencier <fabien.potencier@symfony-project.com>
+ */
+class DirectoryResource implements ResourceInterface
+{
+    protected $resource;
+
+    /**
+     * Constructor.
+     *
+     * @param string $resource The file path to the resource
+     */
+    public function __construct($resource)
+    {
+        $this->resource = realpath($resource);
+    }
+
+    /**
+     * Returns a string representation of the Resource.
+     *
+     * @return string A string representation of the Resource
+     */
+    public function __toString()
+    {
+        return (string) $this->resource;
+    }
+
+    /**
+     * Returns the resource tied to this Resource.
+     *
+     * @return mixed The resource
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
+
+    /**
+     * Returns true if the resource has not been updated since the given timestamp.
+     *
+     * @param integer $timestamp The last time the resource was loaded
+     *
+     * @return Boolean true if the resource has not been updated, false otherwise
+     */
+    public function isFresh($timestamp)
+    {
+        if (!file_exists($this->resource)) {
+            return false;
+        }
+
+        $newestMTime = 0;
+        foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->resource), \RecursiveIteratorIterator::LEAVES_ONLY) as $file) {
+          $newestMTime = max(filemtime($file), $newestMTime);
+        }
+        return $newestMTime < $timestamp;
+    }
+}

edb620122a0eed36389bacd817a1ce624c412355
Fabien Potencier <fabien.potencier@gmail.com>
2011-03-11 11:07:11 +0100

    fixed some emails

--- src/Symfony/Component/Config/Definition/Builder/BuilderInterface.php
+++ src/Symfony/Component/Config/Definition/Builder/BuilderInterface.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Symfony package.
  *
- * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ * (c) Fabien Potencier <fabien@symfony.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,6 +17,6 @@ namespace Symfony\Component\Config\Definition\Builder;
  * @author Victor Berchet <victor@suumit.com>
  */
 interface BuilderInterface
-{    
+{
 }
  

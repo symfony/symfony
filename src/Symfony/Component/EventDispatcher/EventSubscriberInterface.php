699e046b4f0022fbd510a917efef9cf8b9587ed2
Bernhard Schussek <bernhard.schussek@symfony-project.com>
2011-03-13 19:03:18 +0100

    [EventDispatcher] Replaced EventDispatcher by Doctrine's implementation

--- src/Symfony/Component/EventDispatcher/EventSubscriberInterface.php
+++ src/Symfony/Component/EventDispatcher/EventSubscriberInterface.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- *  $Id$
+ *  $Id: EventListener.php 4653 2008-07-10 17:17:58Z romanb $
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -23,50 +23,25 @@
 namespace Symfony\Component\EventDispatcher;
 
 /**
- * Event is the base class for classes containing event data.
- *
- * This class contains no event data. It is used by events that do not pass
- * state information to an event handler when an event is raised.
- *
- * You can call the method stopPropagation() to abort the execution of
- * further listeners in your event listener.
+ * An EventSubscriber knows himself what events he is interested in.
+ * If an EventSubscriber is added to an EventManager, the manager invokes
+ * {@link getSubscribedEvents} and registers the subscriber as a listener for all
+ * returned events.
  *
  * @license http://www.opensource.org/licenses/lgpl-license.php LGPL
  * @link    www.doctrine-project.org
  * @since   2.0
- * @version $Revision: 3938 $
  * @author  Guilherme Blanco <guilhermeblanco@hotmail.com>
  * @author  Jonathan Wage <jonwage@gmail.com>
  * @author  Roman Borschel <roman@code-factory.org>
  * @author  Bernhard Schussek <bschussek@gmail.com>
  */
-class Event
+interface EventSubscriberInterface
 {
     /**
-     * @var Boolean Whether no further event listeners should be triggered
-     */
-    private $propagationStopped = false;
-
-    /**
-     * Returns whether further event listeners should be triggered.
+     * Returns an array of events this subscriber wants to listen to.
      *
-     * @see Event::stopPropagation
-     * @return Boolean Whether propagation was already stopped for this event.
+     * @return array
      */
-    public function isPropagationStopped()
-    {
-        return $this->propagationStopped;
-    }
-
-    /**
-     * Stops the propagation of the event to further event listeners.
-     *
-     * If multiple event listeners are connected to the same event, no
-     * further event listener will be triggered once any trigger calls
-     * stopPropagation().
-     */
-    public function stopPropagation()
-    {
-        $this->propagationStopped = true;
-    }
+    public static function getSubscribedEvents();
 }

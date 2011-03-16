7edb1a5f130fb8d0ed70ad263ee18e4ac701d572
Fabien Potencier <fabien.potencier@gmail.com>
2011-03-16 21:39:20 +0100

    fixed previous commit

--- src/Symfony/Bundle/AsseticBundle/Tests/TestKernel.php
+++ src/Symfony/Bundle/AsseticBundle/Tests/TestKernel.php
@@ -16,7 +16,7 @@ use Symfony\Component\HttpKernel\Kernel;
 
 class TestKernel extends Kernel
 {
-    public function registerRootDir()
+    public function getRootDir()
     {
         return __DIR__.'/Resources';
     }

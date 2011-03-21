b9af10384bc50adf59e972905cbee9f4da85f770
Sven Paulus <sven@karlsruhe.org>
2011-03-21 16:31:24 +0100

    Eleminate the need to manually clear the cache if a new controller file containing routing annotations is added - part II * add unit tests * introduce filename filter to DirectoryResource (to restrict change monitoring to a subset of files) * modify AnnotationDirectoryLoader.php to use filename filter set on Controller\.php$

--- src/Symfony/Component/Config/Resource/DirectoryResource.php
+++ src/Symfony/Component/Config/Resource/DirectoryResource.php
@@ -19,6 +19,7 @@ namespace Symfony\Component\Config\Resource;
 class DirectoryResource implements ResourceInterface
 {
     protected $resource;
+    protected $filterRegexList;
 
     /**
      * Constructor.
@@ -28,6 +29,26 @@ class DirectoryResource implements ResourceInterface
     public function __construct($resource)
     {
         $this->resource = $resource;
+    }
+
+    /**
+     * Set a list of filter regex (to restrict the list of monitored files)
+     *
+     * @param array $filterRegexList An array of regular expressions
+     */
+    public function setFilterRegexList(array $filterRegexList)
+    {
+        $this->filterRegexList = $filterRegexList;
+    }
+
+    /**
+     * Returns the list of filter regex
+     *
+     * @return array An array of regular expressions
+     */
+    public function getFilterRegexList()
+    {
+        return $this->filterRegexList;
     }
 
     /**
@@ -63,9 +84,26 @@ class DirectoryResource implements ResourceInterface
             return false;
         }
 
-        $newestMTime = 0;
-        foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->resource), \RecursiveIteratorIterator::LEAVES_ONLY) as $file) {
-            $newestMTime = max(filemtime($file), $newestMTime);
+        $newestMTime = filemtime($this->resource);
+        foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->resource), \RecursiveIteratorIterator::SELF_FIRST) as $file) {
+            // if regex filtering is enabled only check matching files
+            if (isset($this->filterRegexList) && $file->isFile()) {
+                $regexMatched = false;
+                foreach ($this->filterRegexList as $regex) {
+                    if (preg_match($regex, $file->__toString())) {
+                        $regexMatched = true;
+                    }
+                }
+                if (!$regexMatched) {
+                  continue;
+                }
+            }
+            // always monitor directories for changes, except the .. entries
+            // (otherwise deleted files wouldn't get detected)
+            if ($file->isDir() && '/..' === substr($file, -3)) {
+                continue;
+            }
+            $newestMTime = max($file->getMTime(), $newestMTime);
         }
 
         return $newestMTime < $timestamp;

e2492a4879585b5d5bd7ebc1298d5bf59ecc7126
Aurelijus <aurelijus@astdev.lt>
2011-03-24 16:21:55 +0200

    removed 0

--- src/Symfony/Bundle/FrameworkBundle/RequestListener.php
+++ src/Symfony/Bundle/FrameworkBundle/RequestListener.php
@@ -110,7 +110,7 @@ class RequestListener
             if (null !== $this->logger) {
                 $this->logger->err($message);
             }
-            throw new MethodNotAllowedHttpException($e->getAllowedMethods(), $message, $e, 0);
+            throw new MethodNotAllowedHttpException($e->getAllowedMethods(), $message, $e);
         }
     }
 }

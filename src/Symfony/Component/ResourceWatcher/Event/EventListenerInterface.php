9eec6a139760092ebc1447ba0cc3844015a08590
everzet <ever.zet@gmail.com>
2011-03-25 20:33:48 +0200

    [ResourceWatcher] resource event listener interface described

---
+++ src/Symfony/Component/ResourceWatcher/Event/EventListenerInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\ResourceWatcher\Event;
+
+/**
+ * Resource change listener interface.
+ *
+ * @author Konstantin Kudryashov <ever.zet@gmail.com>
+ */
+interface EventListenerInterface
+{
+    /**
+     * Returns listening resource.
+     *
+     * @return  ResourceInterface
+     */
+    function getResource();
+
+    /**
+     * Returns callback.
+     *
+     * @return  callable
+     */
+    function getCallback();
+
+    /**
+     * Checks whether listener can handle provided resource event.
+     *
+     * @param   Event   $event
+     */
+    function handles(Event $event);
+}

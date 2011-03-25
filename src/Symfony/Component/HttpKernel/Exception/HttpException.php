0e57943531775a6fe571958b60bea46261b8d7bd
Fabien Potencier <fabien.potencier@gmail.com>
2011-03-25 07:19:02 +0100

    [HttpKernel] fixed typo

--- src/Symfony/Component/HttpKernel/Exception/HttpException.php
+++ src/Symfony/Component/HttpKernel/Exception/HttpException.php
@@ -26,7 +26,7 @@ class HttpException extends \RuntimeException implements HttpExceptionInterface
         $this->statusCode = $statusCode;
         $this->headers = $headers;
 
-        parent::__construct($message, 0, $previous);
+        parent::__construct($message, $code, $previous);
     }
 
     public function getStatusCode()

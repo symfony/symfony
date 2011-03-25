27afac85241e8e62cae79329ab841db7d1ea35b8
everzet <ever.zet@gmail.com>
2011-03-25 20:35:05 +0200

    [ResourceWatcher] resource state checker interface described

---
+++ src/Symfony/Component/ResourceWatcher/StateChecker/StateCheckerInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\ResourceWatcher\StateChecker;
+
+/**
+ * Resource state checker interface.
+ *
+ * @author Konstantin Kudryashov <ever.zet@gmail.com>
+ */
+interface StateCheckerInterface
+{
+    /**
+     * Is tracked resource still exists.
+     *
+     * @return  Boolean
+     */
+    function isResourceExists();
+
+    /**
+     * Returns tracked resource.
+     *
+     * @return  ResourceInterface
+     */
+    function getResource();
+
+    /**
+     * Check tracked resource for changes.
+     *
+     * @return  array
+     */
+    function checkChanges();
+}

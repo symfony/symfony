9ff2ca7f1dc2785c4efe791cf4d195d91f24c36f
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-03-29 14:06:30 -0700

    [Validator] fixed apc cache

--- src/Symfony/Component/Validator/Mapping/Cache/ApcCache.php
+++ src/Symfony/Component/Validator/Mapping/Cache/ApcCache.php
@@ -6,30 +6,28 @@ use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 class ApcCache implements CacheInterface
 {
+    private $prefix;
+
+    public function __construct($prefix)
+    {
+        $this->prefix = $prefix;
+    }
+
     public function has($class)
     {
-        apc_delete($this->computeCacheKey($class));
-        apc_fetch($this->computeCacheKey($class), $exists);
+        $exists = false;
+        apc_fetch($this->prefix.$class, $exists);
 
         return $exists;
     }
 
     public function read($class)
     {
-        if (!$this->has($class)) {
-            // TODO exception
-        }
-
-        return apc_fetch($this->computeCacheKey($class));
+        return apc_fetch($this->prefix.$class);
     }
 
     public function write(ClassMetadata $metadata)
     {
-        apc_store($this->computeCacheKey($metadata->getClassName()), $metadata);
-    }
-
-    protected function computeCacheKey($class)
-    {
-        return 'Symfony\Components\Validator:'.$class;
+        apc_store($this->prefix.$metadata->getClassName(), $metadata);
     }
 }

bdc07ad13e96da5c082e4b25d3f8c65a2454fe58
Fabien Potencier <fabien.potencier@gmail.com>
2011-03-30 19:31:32 +0200

    [WebProfilerBundle] converted the configuration to the new system

---
+++ src/Symfony/Bundle/WebProfilerBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\WebProfilerBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+/**
+ * This class contains the configuration information for the bundle
+ *
+ * This information is solely responsible for how the different configuration
+ * sections are normalized, and merged.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class Configuration
+{
+    /**
+     * Generates the configuration tree.
+     *
+     * @return \Symfony\Component\Config\Definition\ArrayNode The config tree
+     */
+    public function getConfigTree()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('swiftmailer');
+
+        $rootNode
+            ->children()
+                ->booleanNode('toolbar')->defaultFalse()->end()
+                ->scalarNode('intercept_redirects')->defaultFalse()->end()
+            ->end()
+        ;
+
+        return $treeBuilder->buildTree();
+    }
+}

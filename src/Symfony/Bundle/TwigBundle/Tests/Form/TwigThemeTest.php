b219e92bf46a32db2c7c12fafd162744c3b6f194
Fabien Potencier <fabien.potencier@gmail.com>
2011-03-31 07:27:55 +0200

    [Form] fixed Twig theme tests

--- src/Symfony/Bundle/TwigBundle/Tests/Form/TwigThemeTest.php
+++ src/Symfony/Bundle/TwigBundle/Tests/Form/TwigThemeTest.php
@@ -12,22 +12,22 @@
 namespace Symfony\Bundle\FrameworkBundle\Tests\Form;
 
 use Symfony\Tests\Component\Form\Renderer\Theme\AbstractThemeTest;
-use Symfony\Bundle\TwigBundle\Form\TwigTheme;
-use Symfony\Bundle\TwigBundle\Extension\TransExtension;
+use Symfony\Component\Form\Renderer\Theme\TwigThemeFactory;
+use Symfony\Bridge\Twig\Extension\TranslationExtension;
 use Symfony\Component\Translation\MessageSelector;
 use Symfony\Component\Translation\Translator;
 
 class TwigThemeTest extends AbstractThemeTest
 {
-    protected function createTheme()
+    protected function createThemeFactory()
     {
         $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../../Resources/views/');
         $environment = new \Twig_Environment($loader, array(
           'cache' => false,
           'debug' => true
         ));
-        $environment->addExtension(new TransExtension(new Translator('en', new MessageSelector())));
+        $environment->addExtension(new TranslationExtension(new Translator('en', new MessageSelector())));
 
-        return new TwigTheme($environment, 'div_plain_layout.html.twig');
+        return new TwigThemeFactory($environment, 'div_plain_layout.html.twig');
     }
 }

b3ea620f53765e1aca3d752ae07a32ae289936f8
Antoine Hérault <antoine.herault@gmail.com>
2011-04-04 18:30:42 +0200

    [HttpKernel] Fix the default extension name

--- tests/Symfony/Tests/Component/HttpKernel/Bundle/BundleTest.php
+++ tests/Symfony/Tests/Component/HttpKernel/Bundle/BundleTest.php
@@ -16,21 +16,64 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Tests\Component\HttpKernel\Fixtures\ExtensionPresentBundle\ExtensionPresentBundle;
 use Symfony\Tests\Component\HttpKernel\Fixtures\ExtensionAbsentBundle\ExtensionAbsentBundle;
 use Symfony\Tests\Component\HttpKernel\Fixtures\ExtensionPresentBundle\Command\FooCommand;
+use Symfony\Tests\Component\HttpKernel\Fixtures\BundlesBundle\BundlesBundle;
 
 class BundleTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @dataProvider getTestBuildData
+     */
+    public function testBuild($bundleClass, $expectedExtensionClass)
+    {
+        $container = $this->getMock(
+            'Symfony\Component\DependencyInjection\ContainerBuilder',
+            array('registerExtension')
+        );
+
+        if (false === $expectedExtensionClass) {
+            $container->expects($this->never())
+                ->method('registerExtension');
+        } else {
+            $extension = new $expectedExtensionClass();
+            $container->expects($this->once())
+                ->method('registerExtension')
+                ->with($this->isInstanceOf($expectedExtensionClass));
+        }
+
+        $bundle = new $bundleClass();
+        $bundle->build($container);
+    }
+
+    public function getTestBuildData()
+    {
+        return array(
+            array(
+                'Symfony\Tests\Component\HttpKernel\Fixtures\ExtensionPresentBundle\ExtensionPresentBundle',
+                'Symfony\Tests\Component\HttpKernel\Fixtures\ExtensionPresentBundle\DependencyInjection\ExtensionPresentExtension'
+            ),
+            array(
+                'Symfony\Tests\Component\HttpKernel\Fixtures\ExtensionAbsentBundle\ExtensionAbsentBundle',
+                false
+            ),
+            array(
+                'Symfony\Tests\Component\HttpKernel\Fixtures\BundlesBundle\BundlesBundle',
+                'Symfony\Tests\Component\HttpKernel\Fixtures\BundlesBundle\DependencyInjection\BundlesExtension'
+            )
+        );
+    }
+
     public function testRegisterCommands()
     {
         $cmd = new FooCommand();
         $app = $this->getMock('Symfony\Component\Console\Application');
         $app->expects($this->once())->method('add')->with($this->equalTo($cmd));
-        
+
         $bundle = new ExtensionPresentBundle();
         $bundle->registerCommands($app);
-        
+
         $bundle2 = new ExtensionAbsentBundle();
-        
+
         $this->assertNull($bundle2->registerCommands($app));
-        
+
     }
 }

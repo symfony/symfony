23fc0371d7a79f941df5d54722ba1f86ad817f72
Johannes M. Schmitt <schmittjoh@gmail.com>
2011-04-08 12:07:31 +0200

    [Process][Assetic] refactored some code to allow for better executable defaults

---
+++ src/Symfony/Component/Process/Exception/RuntimeException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Symfony\Component\Process\Exception;
+
+/**
+ * RuntimeException for this component.
+ *
+ * @author Johannes M. Schmitt <schmittjoh@gmail.com>
+ */
+class RuntimeException extends \RuntimeException implements Exception
+{
+}
---
+++ src/Symfony/Component/Process/PhpExecutableFinder.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Process;
+
+use Symfony\Component\Process\Exception\ExecutableNotFoundException;
+
+/**
+ * An executable finder specifically designed for the PHP executable.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ * @author Johannes M. Schmitt <schmittjoh@gmail.com>
+ */
+class PhpExecutableFinder
+{
+    private $executableFinder;
+
+    public function __construct()
+    {
+        $this->executableFinder = new ExecutableFinder();
+    }
+
+    public function find()
+    {
+        if ($php = getenv('PHP_PATH')) {
+            if (!is_executable($php)) {
+                throw new \RuntimeException('The defined PHP_PATH environment variable is not a valid PHP executable.');
+            }
+
+            return $php;
+        }
+
+        $suffixes = DIRECTORY_SEPARATOR == '\\' ? (getenv('PATHEXT') ? explode(PATH_SEPARATOR, getenv('PATHEXT')) : array('.exe', '.bat', '.cmd', '.com')) : array('');
+        foreach ($suffixes as $suffix) {
+            if (is_executable($php = PHP_BINDIR.DIRECTORY_SEPARATOR.'php'.$suffix)) {
+                return $php;
+            }
+        }
+
+        if ($php = getenv('PHP_PEAR_PHP_BIN')) {
+            if (is_executable($php)) {
+                return $php;
+            }
+        }
+
+        return $this->executableFinder->find('php');
+    }
+}

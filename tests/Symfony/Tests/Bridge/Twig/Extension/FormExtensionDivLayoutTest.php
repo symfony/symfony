96f040cec1d6ab28892d3e8b552aac180a7ad932
Bernhard Schussek <bschussek@gmail.com>
2011-04-14 03:15:56 +0200

    [Form] Added extensive test coverage for div_layout.html.twig

---
+++ tests/Symfony/Tests/Bridge/Twig/Extension/FormExtensionDivLayoutTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Tests\Bridge\Twig\Extension;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Bridge\Twig\Extension\FormExtension;
+use Symfony\Bridge\Twig\Extension\TranslationExtension;
+use Symfony\Tests\Component\Form\AbstractDivLayoutTest;
+
+class FormExtensionDivLayoutTest extends AbstractDivLayoutTest
+{
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $loader = new \Twig_Loader_Filesystem(array(
+            __DIR__.'/../../../../../../src/Symfony/Bundle/TwigBundle/Resources/views',
+        ));
+
+        $this->extension = new FormExtension(array('div_layout.html.twig'));
+
+        $environment = new \Twig_Environment($loader);
+        $environment->addExtension($this->extension);
+        $environment->addExtension(new TranslationExtension(new StubTranslator()));
+
+        $this->extension->initRuntime($environment);
+    }
+
+    protected function renderEnctype(FormInterface $form)
+    {
+        return (string)$this->extension->renderEnctype($form->getContext());
+    }
+
+    protected function renderLabel(FormInterface $form, $label = null)
+    {
+        return (string)$this->extension->renderLabel($form->getContext(), $label);
+    }
+
+    protected function renderErrors(FormInterface $form)
+    {
+        return (string)$this->extension->renderErrors($form->getContext());
+    }
+
+    protected function renderWidget(FormInterface $form, array $vars = array())
+    {
+        return (string)$this->extension->renderWidget($form->getContext(), $vars);
+    }
+
+    protected function renderRow(FormInterface $form, array $vars = array())
+    {
+        return (string)$this->extension->renderRow($form->getContext(), $vars);
+    }
+
+    protected function renderRest(FormInterface $form, array $vars = array())
+    {
+        return (string)$this->extension->renderRest($form->getContext(), $vars);
+    }
+}
+
+class StubTranslator implements TranslatorInterface
+{
+    public function trans($id, array $parameters = array(), $domain = null, $locale = null)
+    {
+        return '[trans]'.$id.'[/trans]';
+    }
+
+    public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null)
+    {
+        return '[trans]'.$id.'[/trans]';
+    }
+
+    public function setLocale($locale)
+    {
+    }
+
+    public function getLocale()
+    {
+    }
+
+}

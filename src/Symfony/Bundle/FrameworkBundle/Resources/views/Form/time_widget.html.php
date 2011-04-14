65276b5c37e8a9c9fff76f35b73da6d7bdf47e4b
Bernhard Schussek <bschussek@gmail.com>
2011-04-14 15:29:55 +0200

    [Form] Added functional test for the 'size' attribute in the select tags of a time field

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/time_widget.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/time_widget.html.php
@@ -2,13 +2,13 @@
     <?php
         // There should be no spaces between the colons and the widgets, that's why
         // this block is written in a single PHP tag
-        echo $view['form']->widget($form['hour'], array('size' => 1));
+        echo $view['form']->widget($form['hour'], array('attr' => array('size' => 1)));
         echo ':';
-        echo $view['form']->widget($form['minute'], array('size' => 1));
+        echo $view['form']->widget($form['minute'], array('attr' => array('size' => 1)));
 
         if ($with_seconds) {
             echo ':';
-            echo $view['form']->widget($form['second'], array('size' => 1));
+            echo $view['form']->widget($form['second'], array('attr' => array('size' => 1)));
         }
     ?>
 </div>

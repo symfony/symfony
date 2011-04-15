fd5caa9546eda2ba1c12ae8fb533f921f4611f8e
Johannes Schmitt <schmittjoh@gmail.com>
2011-04-15 08:47:28 +0200

    [DependencyInjection] also check references of inlined services

--- src/Symfony/Component/DependencyInjection/Compiler/CheckExceptionOnInvalidReferenceBehaviorPass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/CheckExceptionOnInvalidReferenceBehaviorPass.php
@@ -2,6 +2,8 @@
 
 namespace Symfony\Component\DependencyInjection\Compiler;
 
+use Symfony\Component\DependencyInjection\Definition;
+
 use Symfony\Component\DependencyInjection\Exception\NonExistentServiceException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Reference;
@@ -23,10 +25,15 @@ class CheckExceptionOnInvalidReferenceBehaviorPass implements CompilerPassInterf
 
         foreach ($container->getDefinitions() as $id => $definition) {
             $this->sourceId = $id;
-            $this->processReferences($definition->getArguments());
-            $this->processReferences($definition->getMethodCalls());
-            $this->processReferences($definition->getProperties());
+            $this->processDefinition($definition);
         }
+    }
+
+    private function processDefinition(Definition $definition)
+    {
+        $this->processReferences($definition->getArguments());
+        $this->processReferences($definition->getMethodCalls());
+        $this->processReferences($definition->getProperties());
     }
 
     private function processReferences(array $arguments)
@@ -34,6 +41,8 @@ class CheckExceptionOnInvalidReferenceBehaviorPass implements CompilerPassInterf
         foreach ($arguments as $argument) {
             if (is_array($argument)) {
                 $this->processReferences($argument);
+            } else if ($argument instanceof Definition) {
+                $this->processDefinition($argument);
             } else if ($argument instanceof Reference && ContainerInterface::EXCEPTION_ON_INVALID_REFERENCE === $argument->getInvalidBehavior()) {
                 $destId = (string) $argument;
 

49ecb90221637e437213dbb16eb634d205dc9e8b
Johannes M. Schmitt <schmittjoh@gmail.com>
2011-04-15 10:14:13 +0200

    [DependencyInjection] enable debug related passes only in debug environment

--- src/Symfony/Bundle/FrameworkBundle/FrameworkBundle.php
+++ src/Symfony/Bundle/FrameworkBundle/FrameworkBundle.php
@@ -82,7 +82,10 @@ class FrameworkBundle extends Bundle
         $container->addCompilerPass(new AddClassesToAutoloadMapPass());
         $container->addCompilerPass(new TranslatorPass());
         $container->addCompilerPass(new AddCacheWarmerPass());
-        $container->addCompilerPass(new ContainerBuilderDebugDumpPass(), PassConfig::TYPE_BEFORE_REMOVING);
-        $container->addCompilerPass(new CompilerDebugDumpPass(), PassConfig::TYPE_AFTER_REMOVING);
+
+        if ($container->getParameter('kernel.debug')) {
+            $container->addCompilerPass(new ContainerBuilderDebugDumpPass(), PassConfig::TYPE_BEFORE_REMOVING);
+            $container->addCompilerPass(new CompilerDebugDumpPass(), PassConfig::TYPE_AFTER_REMOVING);
+        }
     }
 }

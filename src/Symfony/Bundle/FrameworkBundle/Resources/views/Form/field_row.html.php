617a2f3a0d446a8acbd65e0ccbda06801ec3d714
Ryan Weaver <ryan@thatsquality.com>
2011-04-16 20:31:23 -0500

    [FrameworkBundle] Adding support for label and attributes setting from form row() helper.
    This matches the change made for Twig - the labeland any variables are now properly passed from row() to widget()
    and label().

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/field_row.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/field_row.html.php
@@ -1,5 +1,5 @@
 <div>
-    <?php echo $view['form']->label($form) ?>
+    <?php echo $view['form']->label($form, $label) ?>
     <?php echo $view['form']->errors($form) ?>
-    <?php echo $view['form']->widget($form) ?>
+    <?php echo $view['form']->widget($form, $parameters) ?>
 </div>

d5e7e5713da21468063571923d24eda6ceb4b2e0
Victor Berchet <victor.berchet@yahoo.com>
2011-04-18 15:05:53 +0200

    [FrameworkBundle] Added a test for listener services not available in the current scope

--- src/Symfony/Bundle/FrameworkBundle/Tests/ContainerAwareEventDispatcherTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/ContainerAwareEventDispatcherTest.php
@@ -5,6 +5,7 @@ namespace Symfony\Bundle\FrameworkBundle\Tests;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Bundle\FrameworkBundle\ContainerAwareEventDispatcher;
 use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\DependencyInjection\Scope;
 
 class ContainerAwareEventDispatcherTest extends \PHPUnit_Framework_TestCase
 {
@@ -51,6 +52,26 @@ class ContainerAwareEventDispatcherTest extends \PHPUnit_Framework_TestCase
         $dispatcher->dispatch('onEvent', $event);
     }
 
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testTriggerAListenerServiceOutOfScope()
+    {
+        $service = $this->getMock('Symfony\Bundle\FrameworkBundle\Tests\Service');
+
+        $scope = new Scope('scope');
+
+        $container = new Container();
+        $container->addScope($scope);
+        $container->enterScope('scope');
+        $container->set('service.listener', $service, 'scope');
+
+        $dispatcher = new ContainerAwareEventDispatcher($container);
+        $dispatcher->addListenerService('onEvent', 'service.listener');
+
+        $container->leaveScope('scope');
+        $dispatcher->dispatch('onEvent');        
+    }
 }
 
 class Service

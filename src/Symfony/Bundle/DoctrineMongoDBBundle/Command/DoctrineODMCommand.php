52bca1b48d9a81dc1536ae6376fa4d9ffbda72ef
Ivan Rey <ivan.rey@inpltda.com>
2011-04-17 21:30:23 -0500

    Better Exception Message

--- src/Symfony/Bundle/DoctrineMongoDBBundle/Command/DoctrineODMCommand.php
+++ src/Symfony/Bundle/DoctrineMongoDBBundle/Command/DoctrineODMCommand.php
@@ -113,7 +113,7 @@ abstract class DoctrineODMCommand extends Command
         $destination = str_replace('/'.$path, "", $search, $c);
 
         if ($c != 1) {
-            throw new \RuntimeException("Something went terribly wrong.");
+            throw new \RuntimeException("Can't find base path for bundle. Path: $path , Destination: $destination");
         }
 
         return $destination;

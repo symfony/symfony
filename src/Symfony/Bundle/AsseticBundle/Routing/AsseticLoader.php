6a227f858a2a3687459dcda899409150eec24512
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-04-21 07:08:29 -0700

    [AsseticBundle] removed fake front controller from URL before creating route

--- src/Symfony/Bundle/AsseticBundle/Routing/AsseticLoader.php
+++ src/Symfony/Bundle/AsseticBundle/Routing/AsseticLoader.php
@@ -80,6 +80,17 @@ class AsseticLoader extends Loader
         return $routes;
     }
 
+    /**
+     * Loads a route to serve an supplied asset.
+     *
+     * The fake front controller that {@link UseControllerWorker} adds to the
+     * target URL will be removed before set as a route pattern.
+     *
+     * @param RouteCollection $routes The route collection
+     * @param AssetInterface  $asset  The asset
+     * @param string          $name   The name to use
+     * @param integer         $pos    The leaf index
+     */
     private function loadRouteForAsset(RouteCollection $routes, AssetInterface $asset, $name, $pos = null)
     {
         $defaults = array(
@@ -88,7 +99,8 @@ class AsseticLoader extends Loader
             'pos'         => $pos,
         );
 
-        $pattern = $asset->getTargetUrl();
+        // remove the fake front controller
+        $pattern = str_replace('_controller/', '', $asset->getTargetUrl());
 
         if ($format = pathinfo($pattern, PATHINFO_EXTENSION)) {
             $defaults['_format'] = $format;

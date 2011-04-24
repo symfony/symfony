4eb047594204bb516e4e61d37ce05a05737e7e1a
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2011-04-24 12:02:14 +0300

    fix bug in TemporaryStorage. make tempDir if it's not exist

--- src/Symfony/Component/HttpFoundation/File/TemporaryStorage.php
+++ src/Symfony/Component/HttpFoundation/File/TemporaryStorage.php
@@ -24,6 +24,11 @@ class TemporaryStorage
 
     public function __construct($secret, $directory)
     {
+        // make tempDir if it's not exist
+        if (!file_exists($directory)) {
+            mkdir($directory, 0777, true);
+        }
+    
         $this->directory = realpath($directory);
         $this->secret = $secret;
     }

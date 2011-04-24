9f2382d67b58d90b8612f85a81c976d16b5e4429
Fabien Potencier <fabien.potencier@gmail.com>
2011-04-24 13:06:02 +0200

    [Routing] fixed URL generation when a non-optional variable is empty

--- src/Symfony/Component/Routing/Generator/UrlGenerator.php
+++ src/Symfony/Component/Routing/Generator/UrlGenerator.php
@@ -108,7 +108,7 @@ class UrlGenerator implements UrlGeneratorInterface
                         throw new \InvalidArgumentException(sprintf('Parameter "%s" for route "%s" must match "%s" ("%s" given).', $token[3], $name, $requirements[$token[3]], $tparams[$token[3]]));
                     }
 
-                    if (isset($tparams[$token[3]])) {
+                    if ($tparams[$token[3]] || !$optional) {
                         // %2F is not valid in a URL, so we don't encode it (which is fine as the requirements explicitly allowed it)
                         $url = $token[1].str_replace('%2F', '/', urlencode($tparams[$token[3]])).$url;
                     }

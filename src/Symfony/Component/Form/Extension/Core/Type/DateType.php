85be4da964ab46ae1b7919516d931b69d42cb318
Fabien Potencier <fabien.potencier@gmail.com>
2011-04-24 13:59:46 +0200

    fixed email

--- src/Symfony/Component/Form/Extension/Core/Type/DateType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/DateType.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Symfony package.
  *
- * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ * (c) Fabien Potencier <fabien@symfony.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.

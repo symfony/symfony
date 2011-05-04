fe9ef5c68bcdf5fe07af18efe586aa6c86f466dc
Fabien Potencier <fabien.potencier@gmail.com>
2011-05-04 23:29:47 +0200

    [Routing] fixed deep nested route collections (closes #770)

--- tests/Symfony/Tests/Component/Routing/Fixtures/dumper/url_matcher1.php
+++ tests/Symfony/Tests/Component/Routing/Fixtures/dumper/url_matcher1.php
@@ -88,6 +88,24 @@ class ProjectUrlMatcher extends Symfony\Component\Routing\Matcher\UrlMatcher
             return array (  'def' => 'test',  '_route' => 'foofoo',);
         }
 
+        if (0 === strpos($pathinfo, '/a')) {
+            if (0 === strpos($pathinfo, '/a/b')) {
+                // foo
+                if (0 === strpos($pathinfo, '/a/b') && preg_match('#^/a/b/(?P<foo>[^/]+?)$#x', $pathinfo, $matches)) {
+                    $matches['_route'] = 'foo';
+                    return $matches;
+                }
+        
+                // bar
+                if (0 === strpos($pathinfo, '/a/b') && preg_match('#^/a/b/(?P<bar>[^/]+?)$#x', $pathinfo, $matches)) {
+                    $matches['_route'] = 'bar';
+                    return $matches;
+                }
+        
+            }
+    
+        }
+
         throw 0 < count($allow) ? new MethodNotAllowedException(array_unique($allow)) : new NotFoundException();
     }
 }
--- tests/Symfony/Tests/Component/Routing/Matcher/UrlMatcherTest.php
+++ tests/Symfony/Tests/Component/Routing/Matcher/UrlMatcherTest.php
@@ -111,6 +111,21 @@ class UrlMatcherTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(array('_route' => 'bar', 'bar' => 'bar'), $matcher->match('/'));
     }
 
+    public function testMatchWithPrefixes()
+    {
+        $collection1 = new RouteCollection();
+        $collection1->add('foo', new Route('/{foo}'));
+
+        $collection2 = new RouteCollection();
+        $collection2->addCollection($collection1, '/b');
+
+        $collection = new RouteCollection();
+        $collection->addCollection($collection2, '/a');
+
+        $matcher = new UrlMatcher($collection, new RequestContext(), array());
+        $this->assertEquals(array('_route' => 'foo', 'foo' => 'foo'), $matcher->match('/a/b/foo'));
+    }
+
     public function testMatchRegression()
     {
         $coll = new RouteCollection();

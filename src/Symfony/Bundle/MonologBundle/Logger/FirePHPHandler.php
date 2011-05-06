5afe7d20a6cc77fe36d5dadc3199de3e4d52daa3
Christophe Coevoet <stof@notk.org>
2011-05-06 18:01:38 +0200

    [MonologBundle] Fixed the handling of subrequests in FirePHPHandler

--- src/Symfony/Bundle/MonologBundle/Logger/FirePHPHandler.php
+++ src/Symfony/Bundle/MonologBundle/Logger/FirePHPHandler.php
@@ -14,6 +14,7 @@ namespace Symfony\Bundle\MonologBundle\Logger;
 use Monolog\Handler\FirePHPHandler as BaseFirePHPHandler;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
  * FirePHPHandler.
@@ -49,6 +50,10 @@ class FirePHPHandler extends BaseFirePHPHandler
      */
     public function onCoreResponse(FilterResponseEvent $event)
     {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
         $this->response = $event->getResponse();
         foreach ($this->headers as $header => $content) {
             $this->response->headers->set($header, $content);

2db53b5e280f2a0e584827586dd30582102a67ea
Victor Berchet <victor.berchet@yahoo.com>
2011-05-06 12:48:43 +0200

    [Form] Order method according to their visibility to respect the CS

--- src/Symfony/Component/Form/Extension/Validator/ValidatorTypeGuesser.php
+++ src/Symfony/Component/Form/Extension/Validator/ValidatorTypeGuesser.php
@@ -64,38 +64,6 @@ class ValidatorTypeGuesser implements FormTypeGuesserInterface
     }
 
     /**
-     * Iterates over the constraints of a property, executes a constraints on
-     * them and returns the best guess
-     *
-     * @param string $class       The class to read the constraints from
-     * @param string $property    The property for which to find constraints
-     * @param \Closure $guessForConstraint   The closure that returns a guess
-     *                            for a given constraint
-     * @return Guess  The guessed value with the highest confidence
-     */
-    protected function guess($class, $property, \Closure $guessForConstraint)
-    {
-        $guesses = array();
-        $classMetadata = $this->metadataFactory->getClassMetadata($class);
-
-        if ($classMetadata->hasMemberMetadatas($property)) {
-            $memberMetadatas = $classMetadata->getMemberMetadatas($property);
-
-            foreach ($memberMetadatas as $memberMetadata) {
-                $constraints = $memberMetadata->getConstraints();
-
-                foreach ($constraints as $constraint) {
-                    if ($guess = $guessForConstraint($constraint)) {
-                        $guesses[] = $guess;
-                    }
-                }
-            }
-        }
-
-        return Guess::getBestGuess($guesses);
-    }
-
-    /**
      * Guesses a field class name for a given constraint
      *
      * @param  Constraint $constraint  The constraint to guess for
@@ -296,5 +264,37 @@ class ValidatorTypeGuesser implements FormTypeGuesserInterface
                     Guess::HIGH_CONFIDENCE
                 );
         }
+    }
+
+    /**
+     * Iterates over the constraints of a property, executes a constraints on
+     * them and returns the best guess
+     *
+     * @param string $class       The class to read the constraints from
+     * @param string $property    The property for which to find constraints
+     * @param \Closure $guessForConstraint   The closure that returns a guess
+     *                            for a given constraint
+     * @return Guess  The guessed value with the highest confidence
+     */
+    protected function guess($class, $property, \Closure $guessForConstraint)
+    {
+        $guesses = array();
+        $classMetadata = $this->metadataFactory->getClassMetadata($class);
+
+        if ($classMetadata->hasMemberMetadatas($property)) {
+            $memberMetadatas = $classMetadata->getMemberMetadatas($property);
+
+            foreach ($memberMetadatas as $memberMetadata) {
+                $constraints = $memberMetadata->getConstraints();
+
+                foreach ($constraints as $constraint) {
+                    if ($guess = $guessForConstraint($constraint)) {
+                        $guesses[] = $guess;
+                    }
+                }
+            }
+        }
+
+        return Guess::getBestGuess($guesses);
     }
 }

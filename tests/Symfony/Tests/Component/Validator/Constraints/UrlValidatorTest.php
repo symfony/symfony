ed7888a664ba23536913ee060d567454010256dd
stloyd <stloyd@gmail.com>
2011-05-12 11:12:32 +0200

    Refactoring of UrlValidator to use native php filter extension. Adding few additional URLs to test validation. This version is almost twice fast as old one.

--- tests/Symfony/Tests/Component/Validator/Constraints/UrlValidatorTest.php
+++ tests/Symfony/Tests/Component/Validator/Constraints/UrlValidatorTest.php
@@ -52,13 +52,34 @@ class UrlValidatorTest extends \PHPUnit_Framework_TestCase
     {
         return array(
             array('http://www.google.com'),
+            array('http://www.google.com/?foo=bar'),
             array('https://google.com/'),
             array('https://google.com:80/'),
+            array('http://foo.bar.us/?foo=bar'),
+            array('http://foo.bar.us/?foobar[]=1'),
+            array('http://foo.bar.us/?foo[]&bar[]'),
+            array('http://bar2.foo.bar.us/'),
             array('http://www.symfony.com/'),
             array('http://127.0.0.1/'),
             array('http://127.0.0.1:80/'),
+
+            // IP6 addresses
             array('http://[::1]/'),
-            array('http://[::1]:80/'),
+            array('http://[::1]/?foo=bar'),
+            array('http://[::1]/?foobar[]'),
+            array('http://[::1]/?foo[]&bar[]'),
+            array('http://[::1]:80'),
+            array('http://[::1]:80/?foo=bar'),
+            array('http://[::]/'),
+            array('http://[fdfe:dcba:9876:ffff:fdc6:c46b:bb8f:7d4c]/'),
+            array('http://[fdc6:c46b:bb8f:7d4c:fdc6:c46b:bb8f:7d4c]/'),
+            array('http://[fdc6:c46b:bb8f:7d4c:0000:8a2e:0370:7334]/'),
+            array('http://[fe80:0000:0000:0000:0202:b3ff:fe1e:8329]/'),
+            array('http://[fe80:0:0:0:202:b3ff:fe1e:8329]/'),
+            array('http://[fe80::202:b3ff:fe1e:8329]/'),
+            array('http://[0:0:0:0:0:0:0:0]/'),
+            array('http://[2001:0db8:85a3:0000:0000:8a2e:0370:7334]/'),
+            array('http://[2001:0db8:85a3:0000:0000:8a2e:0.0.0.0]'),
         );
     }
 
@@ -74,8 +95,21 @@ class UrlValidatorTest extends \PHPUnit_Framework_TestCase
     {
         return array(
             array('google.com'),
+            array('foobar.example'),
             array('http:/google.com'),
             array('http://google.com::aa'),
+            array('http://google.com[]'),
+            array('ftp://google.com/'),
+
+            // IP6 addresses
+            array('http://::1/'),
+            array('http://[]/'),
+            array('http://[0:0:0:0:0:0:0:0/'),
+            array('http://[0:0:0:0:0:0:0:0/]'),
+            array('http://[z001:0db8:85a3:0000:0000:8a2e:0370:7334]'),
+            array('http://[fe80]'),
+            array('http://[fe80:8329]'),
+            array('http://[2001:0db8:85a3:0000:0000:8a2e:0370:0.0.0.0]'),
         );
     }
 

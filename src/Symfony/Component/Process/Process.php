cf0fee40aefcf5c832aba37b8102c942ad6932ca
Adrian Rudnik <adrian.rudnik@gmail.com>
2011-05-13 02:22:14 +0200

    Fixed wrong descriptor mode for stderr

--- src/Symfony/Component/Process/Process.php
+++ src/Symfony/Component/Process/Process.php
@@ -100,7 +100,7 @@ class Process
             }
         };
 
-        $descriptors = array(array('pipe', 'r'), array('pipe', 'w'), array('pipe', 'w'));
+        $descriptors = array(array('pipe', 'r'), array('pipe', 'w'), array('pipe', 'r'));
 
         $process = proc_open($this->commandline, $descriptors, $pipes, $this->cwd, $this->env, $this->options);
 

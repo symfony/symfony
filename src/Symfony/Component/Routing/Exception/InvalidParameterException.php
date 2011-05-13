07b7dc0c86f300e8a2db5ffa32889e4c894afafb
alexandresalome <alexandre.salome@gmail.com>
2011-05-13 17:46:31 +0200

    [Routing] Change the Exception namespacing + base class for every exception + update PHPDoc

--- src/Symfony/Component/Routing/Exception/InvalidParameterException.php
+++ src/Symfony/Component/Routing/Exception/InvalidParameterException.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Symfony\Component\Routing\Exception\Generator;
+namespace Symfony\Component\Routing\Exception;
 
 /**
  * Exception thrown when a parameter is not valid
  *
  * @author Alexandre Salomé <alexandre.salome@gmail.com>
  */
-class InvalidParameterException extends \Exception
+class InvalidParameterException extends RoutingException
 {
 }
---
+++ src/Symfony/Component/Routing/Exception/RoutingException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Symfony\Component\Routing\Exception;
+
+/**
+ * Base routing exception
+ *
+ * @author Alexandre Salomé <alexandre.salome@gmail.com>
+ */
+class RoutingException extends \Exception
+{
+}

319f01cc377ad3754dd2596af2d0b294fb710675
Victor Berchet <victor.berchet@yahoo.com>
2011-05-18 11:45:23 +0200

    [Validator] Update the file validator with File changes

--- src/Symfony/Component/Validator/Constraints/FileValidator.php
+++ src/Symfony/Component/Validator/Constraints/FileValidator.php
@@ -29,11 +29,7 @@ class FileValidator extends ConstraintValidator
             throw new UnexpectedTypeException($value, 'string');
         }
 
-        if ($value instanceof FileObject && null === $value->getPath()) {
-            return true;
-        }
-
-        $path = $value instanceof FileObject ? $value->getPath() : (string) $value;
+        $path = $value instanceof FileObject ? $value->getPathname() : (string) $value;
 
         if (!file_exists($path)) {
             $this->setMessage($constraint->notFoundMessage, array('{{ file }}' => $path));
@@ -82,9 +78,9 @@ class FileValidator extends ConstraintValidator
 
             if (!in_array($value->getMimeType(), (array) $constraint->mimeTypes)) {
                 $this->setMessage($constraint->mimeTypesMessage, array(
-                    '{{ type }}' => '"'.$value->getMimeType().'"',
-                    '{{ types }}' => '"'.implode('", "', (array) $constraint->mimeTypes).'"',
-                    '{{ file }}' => $path,
+                    '{{ type }}'    => '"'.$value->getMimeType().'"',
+                    '{{ types }}'   => '"'.implode('", "', (array) $constraint->mimeTypes).'"',
+                    '{{ file }}'    => $path,
                 ));
 
                 return false;

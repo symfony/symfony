ddc00fa0f6bf003bed54c7752fa5c8a4d7fd701d
Johannes Schmitt <schmittjoh@gmail.com>
2011-05-19 23:19:23 +0200

    removed obsolete compiler pass

--- src/Symfony/Bundle/DoctrineBundle/DoctrineBundle.php
+++ src/Symfony/Bundle/DoctrineBundle/DoctrineBundle.php
@@ -13,7 +13,6 @@ namespace Symfony\Bundle\DoctrineBundle;
 
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Bundle\DoctrineBundle\DependencyInjection\Compiler\RegisterEventListenersAndSubscribersPass;
-use Symfony\Bundle\DoctrineBundle\DependencyInjection\Compiler\AddValidatorNamespaceAliasPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -30,6 +29,5 @@ class DoctrineBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new RegisterEventListenersAndSubscribersPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION);
-        $container->addCompilerPass(new AddValidatorNamespaceAliasPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION);
     }
 }

2f4644d7889a4387f6a7fb62529cf8e6bcfa98f1
Oleg Stepura <oleg@stepura.com>
2011-05-23 15:00:45 +0300

    more like the original from stealth35

--- vendors.bat
+++ vendors.bat
@@ -1,79 +1,68 @@
 @echo off
-cd %~dp0
+setlocal
 
-rem initialization
-if "%1" == "--reinstall" rmdir /s /q vendor
+cd /d %~dp0
 
-mkdir vendor
+:: initialization
+if [%1] == [--reinstall] (
+    if exist vendor (
+        rd /s /q vendor
+    )
+)
+
+if not exist vendor (
+    mkdir vendor
+)
+
 cd vendor
 
-goto Install_Assetic
+:: Assetic
+call:install_git assetic git://github.com/kriswallsmith/assetic.git
 
+:: Doctrine ORM
+call:install_git doctrine git://github.com/doctrine/doctrine2.git 2.0.5
+
+:: Doctrine DBAL
+call:install_git doctrine-dbal git://github.com/doctrine/dbal.git 2.0.5
+
+:: Doctrine Common
+call:install_git doctrine-common git://github.com/doctrine/common.git origin/3.0.x
+
+:: Doctrine migrations
+call:install_git doctrine-migrations git://github.com/doctrine/migrations.git
+
+:: Monolog
+call:install_git monolog git://github.com/Seldaek/monolog.git
+
+:: Swiftmailer
+call:install_git swiftmailer git://github.com/swiftmailer/swiftmailer.git origin/4.1
+
+:: Twig
+call:install_git twig git://github.com/fabpot/Twig.git
+
+endlocal
+goto:eof
+
+::::
+:: @param destination directory (e.g. "doctrine")
+:: @param URL of the git remote (e.g. http://github.com/doctrine/doctrine2.git)
+:: @param revision to point the head (e.g. origin/HEAD)
+::
 :install_git
-    if "%VERSION%" == "" set VERSION=origin/HEAD
+    set INSTALL_DIR=%1
+    set SOURCE_URL=%2
+    set REV=%3
 
-    if NOT EXIST "%VENDOR_DIR%" git clone %VENDOR_URL% %VENDOR_DIR%
+    if [%REV%] == [] (
+        set REV="origin/HEAD"
+    )
 
-    cd %VENDOR_DIR%
+    if not exist %INSTALL_DIR% (
+        git clone %SOURCE_URL% %INSTALL_DIR%
+    )
+	
+    cd %INSTALL_DIR%
     git fetch origin
-    git reset --hard %VERSION%
+    git reset --hard %REV%
     cd ..
-    goto %NEXT_STEP%
-
-:Install_Assetic
-set VENDOR_DIR=assetic
-set VENDOR_URL=git://github.com/kriswallsmith/assetic.git
-set VERSION=
-set NEXT_STEP=Install_Doctrine_ORM
-goto install_git
-
-:Install_Doctrine_ORM
-set VENDOR_DIR=doctrine
-set VENDOR_URL=git://github.com/doctrine/doctrine2.git
-set VERSION=2.0.5
-set NEXT_STEP=Install_Doctrine_DBAL
-goto install_git
-
-:Install_Doctrine_DBAL
-set VENDOR_DIR=doctrine-dbal
-set VENDOR_URL=git://github.com/doctrine/dbal.git
-set VERSION=2.0.5
-set NEXT_STEP=Install_Doctrine_Common
-goto install_git
-
-:Install_Doctrine_Common
-set VENDOR_DIR=doctrine-common
-set VENDOR_URL=git://github.com/doctrine/common.git
-set VERSION=origin/3.0.x
-set NEXT_STEP=Install_Doctrine_Migrations
-goto install_git
-
-:Install_Doctrine_Migrations
-set VENDOR_DIR=doctrine-migrations
-set VENDOR_URL=git://github.com/doctrine/migrations.git
-set VERSION=
-set NEXT_STEP=Install_Monolog
-goto install_git
-
-:Install_Monolog
-set VENDOR_DIR=monolog
-set VENDOR_URL=git://github.com/Seldaek/monolog.git
-set VERSION=
-set NEXT_STEP=Install_Swiftmailer
-goto install_git
-
-:Install_Swiftmailer
-set VENDOR_DIR=swiftmailer
-set VENDOR_URL=git://github.com/swiftmailer/swiftmailer.git
-set VERSION=origin/4.1
-set NEXT_STEP=Install_Twig
-goto install_git
-
-:Install_Twig
-set VENDOR_DIR=twig
-set VENDOR_URL=git://github.com/fabpot/Twig.git
-set VERSION=
-set NEXT_STEP=end
-goto install_git
-
-:end
+goto:eof

5cda240c6f4ffefacc5f25699e4b7e88ef5a5154
Johannes Schmitt <schmittjoh@gmail.com>
2011-05-28 17:16:55 +0200

    refactored functional tests a bit

--- src/Symfony/Bundle/SecurityBundle/Tests/Functional/WebTestCase.php
+++ src/Symfony/Bundle/SecurityBundle/Tests/Functional/WebTestCase.php
@@ -40,6 +40,7 @@ class WebTestCase extends BaseWebTestCase
 
         return new $class(
             $options['test_case'],
+            isset($options['root_config']) ? $options['root_config'] : 'config.yml',
             isset($options['environment']) ? $options['environment'] : 'test',
             isset($options['debug']) ? $options['debug'] : true
         );

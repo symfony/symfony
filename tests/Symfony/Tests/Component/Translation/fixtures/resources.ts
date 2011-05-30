04435083b8cc7d2b0a28578d962c0a9b30d0ce15
Benjamin Eberlei <kontakt@beberlei.de>
2011-05-30 16:12:18 +0200

    [Translation] Add Qt Translation component with tests

---
+++ tests/Symfony/Tests/Component/Translation/fixtures/resources.ts
@@ -0,0 +1,10 @@
+<?xml version="1.0"?>
+<TS>
+    <context>
+        <name>resources</name>
+        <message>
+            <source>foo</source>
+            <translation>bar</translation>
+        </message>
+    </context>
+</TS>

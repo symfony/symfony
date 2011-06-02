597a646347fff5bf2090fbe97096dc47ac25c6fd
Brikou CARRE <brikou@gmail.com>
2011-06-02 13:20:51 +0200

    added mb_detect_encoding when formatting block (usefull when mb_internal_encoding is not properly set)

--- tests/Symfony/Tests/Component/Console/Helper/FormatterHelperTest.php
+++ tests/Symfony/Tests/Component/Console/Helper/FormatterHelperTest.php
@@ -19,16 +19,44 @@ class FormatterHelperTest extends \PHPUnit_Framework_TestCase
     {
         $formatter = new FormatterHelper();
 
-        $this->assertEquals('<info>[cli]</info> Some text to display', $formatter->formatSection('cli', 'Some text to display'), '::formatSection() formats a message in a section');
+        $this->assertEquals(
+            '<info>[cli]</info> Some text to display',
+            $formatter->formatSection('cli', 'Some text to display'),
+            '::formatSection() formats a message in a section'
+        );
     }
 
     public function testFormatBlock()
     {
         $formatter = new FormatterHelper();
 
-        $this->assertEquals('<error> Some text to display </error>', $formatter->formatBlock('Some text to display', 'error'), '::formatBlock() formats a message in a block');
-        $this->assertEquals("<error> Some text to display </error>\n<error> foo bar              </error>", $formatter->formatBlock(array('Some text to display', 'foo bar'), 'error'), '::formatBlock() formats a message in a block');
+        $this->assertEquals(
+            '<error> Some text to display </error>',
+            $formatter->formatBlock('Some text to display', 'error'),
+            '::formatBlock() formats a message in a block'
+        );
 
-        $this->assertEquals("<error>                        </error>\n<error>  Some text to display  </error>\n<error>                        </error>", $formatter->formatBlock('Some text to display', 'error', true), '::formatBlock() formats a message in a block');
+        $this->assertEquals(
+            '<error> Some text to display </error>' . "\n" .
+            '<error> foo bar              </error>',
+            $formatter->formatBlock(array('Some text to display', 'foo bar'), 'error'),
+            '::formatBlock() formats a message in a block'
+        );
+
+        $this->assertEquals(
+            '<error>                        </error>' . "\n" .
+            '<error>  Some text to display  </error>' . "\n" .
+            '<error>                        </error>',
+            $formatter->formatBlock('Some text to display', 'error', true),
+            '::formatBlock() formats a message in a block'
+        );
+
+        $this->assertEquals(
+            '<error>                       </error>' . "\n" .
+            '<error>  Du texte à afficher  </error>' . "\n" .
+            '<error>                       </error>',
+            $formatter->formatBlock('Du texte à afficher', 'error', true),
+            '::formatBlock() formats a message in a block'
+        );
     }
 }

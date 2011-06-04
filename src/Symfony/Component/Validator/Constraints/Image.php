4b268c8b2f00bf994a24b714b1350f0e9a195a9d
Benjamin Dulau <benjamin.dulau@gmail.com>
2011-06-04 19:22:42 +0200

    [ImageValidator] Added dedicated ImageValidator class with min width, max width, min height and max height validations

--- src/Symfony/Component/Validator/Constraints/Image.php
+++ src/Symfony/Component/Validator/Constraints/Image.php
@@ -19,13 +19,15 @@ namespace Symfony\Component\Validator\Constraints;
 class Image extends File
 {
     public $mimeTypes = 'image/*';
-    public $mimeTypesMessage = 'This file is not a valid image';
+    public $minWidth = null;
+    public $maxWidth = null;
+    public $maxHeight = null;
+    public $minHeight = null;
 
-    /**
-     * @inheritDoc
-     */
-    public function validatedBy()
-    {
-        return __NAMESPACE__.'\FileValidator';
-    }
+    public $mimeTypesMessage = 'This file is not a valid image';
+    public $notDetectedMessage = 'The size of image could not be detected';
+    public $maxWidthMessage = 'The image width is too big ({{ width }}px). Allowed maximum width is {{ maxWidth }}px';
+    public $minWidthMessage = 'The image width is too small ({{ width }}px). Minimum width expected is {{ minWidth }}px';
+    public $maxHeightMessage = 'The image height is too big ({{ height }}px). Allowed maximum width is {{ maxHeight }}px';
+    public $minHeightMessage = 'The image width is too small ({{ height }}px). Minimum height expected is {{ minHeight }}px';
 }

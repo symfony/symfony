8c0e5029a0cea711453cb6da3e6bfe8b3b5d4470
Fabien Potencier <fabien.potencier@gmail.com>
2011-06-05 08:12:53 +0200

    [DoctrineBundle] fixed extensions of generated files

--- src/Symfony/Bundle/DoctrineBundle/Command/Proxy/ConvertMappingDoctrineCommand.php
+++ src/Symfony/Bundle/DoctrineBundle/Command/Proxy/ConvertMappingDoctrineCommand.php
@@ -17,6 +17,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
 use Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand;
+use Doctrine\ORM\Tools\Export\Driver\XmlExporter;
+use Doctrine\ORM\Tools\Export\Driver\YamlExporter;
 
 /**
  * Convert Doctrine ORM metadata mapping information between the various supported
@@ -47,5 +49,18 @@ EOT
         DoctrineCommandHelper::setApplicationEntityManager($this->getApplication(), $input->getOption('em'));
 
         return parent::execute($input, $output);
+    }
+
+
+    protected function getExporter($toType, $destPath)
+    {
+        $exporter = parent::getExporter($toType, $destPath);
+        if ($exporter instanceof XmlExporter) {
+            $exporter->setExtension('.orm.xml');
+        } elseif ($exporter instanceof YamlExporter) {
+            $exporter->setExtension('.orm.yml');
+        }
+
+        return $exporter;
     }
 }

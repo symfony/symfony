b12b11c1314fd08ec09f84b01d28b6fe55144ee8
Victor Berchet <victor.berchet@yahoo.com>
2011-06-06 17:15:11 +0200

    [Form] Never render a view again
    If some of the nested views are rendered individually they should not be rendered again when calling form_rest.
    A typical would be when some nested file views are rendered, form_rest should not render them again.
    
    It is still possible to render a label once the widget has been rendered. This is for checkboxes and radios
    where the widget is typically rendered before the label.

--- tests/Symfony/Tests/Component/Form/AbstractDivLayoutTest.php
+++ tests/Symfony/Tests/Component/Form/AbstractDivLayoutTest.php
@@ -144,14 +144,11 @@ abstract class AbstractDivLayoutTest extends AbstractLayoutTest
         $html = $this->renderRest($view);
 
         $this->assertMatchesXpath($html,
-'/input
-    [@type="hidden"]
-    [@id="parent__token"]
+'/input[@type="hidden"][@id="parent__token"]
 /following-sibling::div
     [
         ./label[@for="parent_child1"]
-        /following-sibling::div
-            [@id="parent_child1"]
+        /following-sibling::div[@id="parent_child1"]
             [
                 ./div
                     [
@@ -160,23 +157,21 @@ abstract class AbstractDivLayoutTest extends AbstractLayoutTest
                     ]
             ]
     ]
+
 /following-sibling::div
     [
         ./label[@for="parent_child2"]
-        /following-sibling::div
-            [@id="parent_child2"]
+        /following-sibling::div[@id="parent_child2"]
             [
                 ./div
                     [
                         ./label[@for="parent_child2_field1"]
                         /following-sibling::input[@id="parent_child2_field1"]
                     ]
-                /following-sibling::div
-                    [
-                        ./label[@for="parent_child2_field2"]
-                    ]
             ]
     ]
+    [count(//label)=4]
+    [count(//input[@type="text"])=2]
 '
         );
     }

293ba3426afc10ebe6380f9689cf24c647c15b04
Fabien Potencier <fabien.potencier@gmail.com>
2011-06-10 19:41:49 +0200

    [DoctrineBridge] fixed guesser

--- src/Symfony/Bridge/Doctrine/Form/DoctrineOrmTypeGuesser.php
+++ src/Symfony/Bridge/Doctrine/Form/DoctrineOrmTypeGuesser.php
@@ -98,7 +98,7 @@ class DoctrineOrmTypeGuesser implements FormTypeGuesserInterface
     public function guessMaxLength($class, $property)
     {
         $ret = $this->getMetadata($class);
-        if ($ret && !$ret[0]->hasAssociation($property)) {
+        if ($ret && $ret[0]->hasField($property) && !$ret[0]->hasAssociation($property)) {
             $mapping = $ret[0]->getFieldMapping($property);
 
             if (isset($mapping['length'])) {

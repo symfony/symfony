8d58826085bc6181c2bfe30a14ffe33355ea6415
Fabien Potencier <fabien.potencier@gmail.com>
2011-06-11 07:33:55 +0200

    [Routing] fixed unit tests for previous commit

--- tests/Symfony/Tests/Component/Routing/Fixtures/dumper/url_matcher2.php
+++ tests/Symfony/Tests/Component/Routing/Fixtures/dumper/url_matcher2.php
@@ -114,13 +114,13 @@ class ProjectUrlMatcher extends Symfony\Tests\Component\Routing\Fixtures\Redirec
         if (0 === strpos($pathinfo, '/a')) {
             if (0 === strpos($pathinfo, '/a/b')) {
                 // foo
-                if (0 === strpos($pathinfo, '/a/b') && preg_match('#^/a/b/(?P<foo>[^/]+?)$#x', $pathinfo, $matches)) {
+                if (preg_match('#^/a/b/(?P<foo>[^/]+?)$#x', $pathinfo, $matches)) {
                     $matches['_route'] = 'foo';
                     return $matches;
                 }
         
                 // bar
-                if (0 === strpos($pathinfo, '/a/b') && preg_match('#^/a/b/(?P<bar>[^/]+?)$#x', $pathinfo, $matches)) {
+                if (preg_match('#^/a/b/(?P<bar>[^/]+?)$#x', $pathinfo, $matches)) {
                     $matches['_route'] = 'bar';
                     return $matches;
                 }

c031b72a977ae9f977273d3281f92f6d3a436fdf
Fabien Potencier <fabien.potencier@gmail.com>
2011-06-13 11:58:58 +0200

    [FrameworkBundle] fixed typo

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/collection_widget.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/collection_widget.html.php
@@ -1,4 +1,4 @@
-<?php $view->render('FrameworkBundle:Form:form_widget.html.php', array('form' => $form)) ?>
+<?php echo $view->render('FrameworkBundle:Form:form_widget.html.php', array('form' => $form)) ?>
 
 <?php if (isset($prototype)): ?>
 <script type="text/html" id="<?php echo $view->escape($id) ?>_prototype"><?php echo $view['form']->row($prototype) ?></script>

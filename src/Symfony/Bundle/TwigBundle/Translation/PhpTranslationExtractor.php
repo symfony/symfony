fb2171d364ea14b3cb3f7949499b37c30d56b498
xaav <xaav@xaav.tk>
2011-06-13 13:38:24 -0500

    Updated extractor.
    Change-Id: I1e5fa125576451b4357aaf9b71f3186eb28c0ffd

--- src/Symfony/Bundle/TwigBundle/Translation/PhpTranslationExtractor.php
+++ src/Symfony/Bundle/TwigBundle/Translation/PhpTranslationExtractor.php
@@ -6,6 +6,23 @@ use Symfony\Component\Translation\MessageCatalogue;
 
 class PhpTranslationExtractor
 {
+    const MESSAGE_TOKEN = 300;
+    const IGNORE_TOKEN = 400;
+
+    protected $sequences = array(
+        array(
+            '$view',
+            '[',
+            '\'translator\'',
+            ']',
+            '->',
+            'trans',
+            '(',
+            self::MESSAGE_TOKEN,
+			')',
+        ),
+    );
+
     public function extractMessages($directory, MessageCatalogue $catalog)
     {
         $finder = new Finder();
@@ -15,8 +32,50 @@ class PhpTranslationExtractor
         }
     }
 
+    protected function normalizeToken($token)
+    {
+        if(is_array($token)) {
+
+            return $token[1];
+        }
+        else {
+
+            return $token;
+        }
+    }
+
     protected function parseTokens($tokens, $catalog)
     {
+        foreach ($tokens as $key => $token) {
 
+            foreach ($this->sequences as $sequence) {
+
+                $message = '';
+
+                foreach ($sequence as $id => $item) {
+
+                    if($this->normalizeToken($tokens[$key + $id]) == $item) {
+                        continue;
+                    }
+                    elseif (self::MESSAGE_TOKEN == $item) {
+
+                        $message = $this->normalizeToken($tokens[$key + $id]);
+                    }
+                    elseif(self::IGNORE_TOKEN == $item) {
+
+                        continue;
+                    }
+                    else {
+                        break;
+                    }
+                }
+
+                if($message) {
+
+                    $catalog->set($message, $message);
+                    break;
+                }
+            }
+        }
     }
 }

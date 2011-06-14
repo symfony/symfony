ad5d2c13e11abba361267ed78df7182e5597354a
stloyd <stloyd@gmail.com>
2011-06-14 15:55:06 +0200

    Added to `TimeType` extension possibility to render form as `single_text` (similar to DateType option) (issue #1205) Adjusted `DateTimeType` to allow usage of this new feature

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/time_widget.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/time_widget.html.php
@@ -1,14 +1,25 @@
-<div <?php echo $view['form']->renderBlock('container_attributes') ?>>
-    <?php
-        // There should be no spaces between the colons and the widgets, that's why
-        // this block is written in a single PHP tag
-        echo $view['form']->widget($form['hour'], array('attr' => array('size' => 1)));
-        echo ':';
-        echo $view['form']->widget($form['minute'], array('attr' => array('size' => 1)));
-
-        if ($with_seconds) {
+<?php if ($widget == 'single_text'): ?>
+    <input type="text"
+        <?php echo $view['form']->renderBlock('attributes') ?>
+        name="<?php echo $view->escape($full_name) ?>"
+        value="<?php echo $view->escape($value) ?>"
+        <?php if ($read_only): ?>disabled="disabled"<?php endif ?>
+        <?php if ($required): ?>required="required"<?php endif ?>
+        <?php if ($max_length): ?>maxlength="<?php echo $max_length ?>"<?php endif ?>
+    />
+<?php else: ?>
+    <div<?php echo $view['form']->renderBlock('container_attributes') ?>>
+        <?php
+            // There should be no spaces between the colons and the widgets, that's why
+            // this block is written in a single PHP tag
+            echo $view['form']->widget($form['hour'], array('attr' => array('size' => 1)));
             echo ':';
-            echo $view['form']->widget($form['second'], array('attr' => array('size' => 1)));
-        }
-    ?>
-</div>
+            echo $view['form']->widget($form['minute'], array('attr' => array('size' => 1)));
+
+            if ($with_seconds) {
+                echo ':';
+                echo $view['form']->widget($form['second'], array('attr' => array('size' => 1)));
+            }
+        ?>
+    </div>
+<?php endif ?>

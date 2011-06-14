ecf9fed90969fdbbcf7d093b0ff5e9aec80cb5ee
Victor Berchet <victor.berchet@yahoo.com>
2011-06-14 10:47:04 +0200

    [HttpFoundation] Make File extends \SplFileInfo

--- tests/Symfony/Tests/Component/HttpFoundation/File/UploadedFileTest.php
+++ tests/Symfony/Tests/Component/HttpFoundation/File/UploadedFileTest.php
@@ -32,11 +32,10 @@ class UploadedFileTest extends \PHPUnit_Framework_TestCase
             UPLOAD_ERR_OK
         );
 
-        $this->assertAttributeEquals('application/octet-stream', 'mimeType', $file);
+        $this->assertEquals('application/octet-stream', $file->getClientMimeType());
+
         if (extension_loaded('fileinfo')) {
             $this->assertEquals('image/gif', $file->getMimeType());
-        } else {
-            $this->assertEquals('application/octet-stream', $file->getMimeType());
         }
     }
 
@@ -50,8 +49,7 @@ class UploadedFileTest extends \PHPUnit_Framework_TestCase
             UPLOAD_ERR_OK
         );
 
-        $this->assertAttributeEquals('application/octet-stream', 'mimeType', $file);
-        $this->assertEquals('application/octet-stream', $file->getMimeType());
+        $this->assertEquals('application/octet-stream', $file->getClientMimeType());
     }
 
     public function testErrorIsOkByDefault()
@@ -67,7 +65,7 @@ class UploadedFileTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(UPLOAD_ERR_OK, $file->getError());
     }
 
-    public function testGetOriginalName()
+    public function testGetClientOriginalName()
     {
         $file = new UploadedFile(
             __DIR__.'/Fixtures/test.gif',
@@ -77,10 +75,54 @@ class UploadedFileTest extends \PHPUnit_Framework_TestCase
             null
         );
 
-        $this->assertEquals('original.gif', $file->getOriginalName());
+        $this->assertEquals('original.gif', $file->getClientOriginalName());
     }
 
-    public function testGetOriginalNameSanitizeFilename()
+    /**
+     * @expectedException Symfony\Component\HttpFoundation\File\Exception\FileException
+     */
+    public function testMoveLocalFileIsNotAllowed()
+    {
+        $file = new UploadedFile(
+            __DIR__.'/Fixtures/test.gif',
+            'original.gif',
+            'image/gif',
+            filesize(__DIR__.'/Fixtures/test.gif'),
+            UPLOAD_ERR_OK
+        );
+
+        $movedFile = $file->move(__DIR__.'/Fixtures/directory');
+    }
+
+    public function testMoveLocalFileIsAllowedInTestMode()
+    {
+        $path = __DIR__.'/Fixtures/test.copy.gif';
+        $targetDir = __DIR__.'/Fixtures/directory';
+        $targetPath = $targetDir.'/test.copy.gif';
+        @unlink($path);
+        @unlink($targetPath);
+        copy(__DIR__.'/Fixtures/test.gif', $path);
+
+        $file = new UploadedFile(
+            $path,
+            'original.gif',
+            'image/gif',
+            filesize($path),
+            UPLOAD_ERR_OK,
+            true
+        );
+
+        $movedFile = $file->move(__DIR__.'/Fixtures/directory');
+
+        $this->assertTrue(file_exists($targetPath));
+        $this->assertFalse(file_exists($path));
+        $this->assertEquals(realpath($targetPath), $movedFile->getRealPath());
+
+        @unlink($targetPath);
+    }
+
+
+    public function testGetClientOriginalNameSanitizeFilename()
     {
         $file = new UploadedFile(
             __DIR__.'/Fixtures/test.gif',
@@ -90,6 +132,6 @@ class UploadedFileTest extends \PHPUnit_Framework_TestCase
             null
         );
 
-        $this->assertEquals('original.gif', $file->getOriginalName());
+        $this->assertEquals('original.gif', $file->getClientOriginalName());
     }
 }

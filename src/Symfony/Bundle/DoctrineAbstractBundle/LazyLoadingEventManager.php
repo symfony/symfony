1c0f6524e2ba5f5b9630ff52f4630f111baaae6a
Johannes M. Schmitt <schmittjoh@gmail.com>
2011-06-17 13:40:42 +0200

    removed method which doesn't necessarily have to be implemented in the sub-class

--- src/Symfony/Bundle/DoctrineAbstractBundle/LazyLoadingEventManager.php
+++ src/Symfony/Bundle/DoctrineAbstractBundle/LazyLoadingEventManager.php
@@ -120,15 +120,4 @@ class LazyLoadingEventManager extends EventManager
             }
         }
     }
-
-    /**
-     * Adds an EventSubscriber. The subscriber is asked for all the events he is
-     * interested in and added as a listener for these events.
-     *
-     * @param Doctrine\Common\EventSubscriber $subscriber The subscriber.
-     */
-    public function addEventSubscriber(EventSubscriber $subscriber)
-    {
-        $this->addEventListener($subscriber->getSubscribedEvents(), $subscriber);
-    }
 }

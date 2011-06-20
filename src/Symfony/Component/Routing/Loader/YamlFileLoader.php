36861b2413cf6567e5498dbd102e723d85dfd2af
Florin Patan <florinpatan@gmail.com>
2011-06-20 19:30:09 -0400

    Removed useless comparison

--- src/Symfony/Component/Routing/Loader/YamlFileLoader.php
+++ src/Symfony/Component/Routing/Loader/YamlFileLoader.php
@@ -112,10 +112,6 @@ class YamlFileLoader extends FileLoader
             throw new \InvalidArgumentException(sprintf('You must define a "pattern" for the "%s" route.', $name));
         }
 
-        if (isset($options['enabled']) && true !== $options['enabled']) {
-            return;
-        }
-
         $route = new Route($config['pattern'], $defaults, $requirements, $options);
 
         $collection->add($name, $route);

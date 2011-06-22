a4451b48cb2ce508b218e10642dc532d3d8e8e5a
Alexander <iam.asm89@gmail.com>
2011-06-22 21:42:59 +0200

    [DomCrawler] Added tests for links starting with ?foo

--- tests/Symfony/Tests/Component/DomCrawler/LinkTest.php
+++ tests/Symfony/Tests/Component/DomCrawler/LinkTest.php
@@ -88,6 +88,11 @@ class LinkTest extends \PHPUnit_Framework_TestCase
             array('?a=b', 'http://localhost/bar/', 'http://localhost/bar/?a=b'),
 
             array('http://login.foo.com/foo', 'http://localhost/bar/', 'http://login.foo.com/foo'),
+
+            array('?foo=2', 'http://localhost?foo=1', 'http://localhost?foo=2'),
+            array('?foo=2', 'http://localhost/?foo=1', 'http://localhost/?foo=2'),
+            array('?foo=2', 'http://localhost/bar?foo=1', 'http://localhost/bar?foo=2'),
+            array('?foo=2', 'http://localhost/bar/?foo=1', 'http://localhost/bar/?foo=2'),
         );
     }
 }

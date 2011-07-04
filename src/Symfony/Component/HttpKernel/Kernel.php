292d97d147e670f9645f357222d8d8fa25cf9493
Fabien Potencier <fabien.potencier@gmail.com>
2011-07-04 22:45:04 +0200

    bumped version to RC5-DEV

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -56,7 +56,7 @@ abstract class Kernel implements KernelInterface
     protected $startTime;
     protected $classes;
 
-    const VERSION = '2.0.0-RC4';
+    const VERSION = '2.0.0-RC5-DEV';
 
     /**
      * Constructor.

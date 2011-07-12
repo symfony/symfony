c6224722872d362ef847010f0188526ad2de180a
Fabien Potencier <fabien.potencier@gmail.com>
2011-07-12 08:16:22 +0200

    [Process] fixed CS

--- src/Symfony/Component/Process/Process.php
+++ src/Symfony/Component/Process/Process.php
@@ -174,13 +174,13 @@ class Process
             $this->status = proc_get_status($process);
         }
 
-        $exitCode = proc_close($process);
+        $exitcode = proc_close($process);
 
         if ($this->status['signaled']) {
             throw new \RuntimeException(sprintf('The process stopped because of a "%s" signal.', $this->status['stopsig']));
         }
-        
-        return $this->exitcode = ($this->status["running"] ? $exitCode : $this->status["exitcode"]);
+
+        return $this->exitcode = $this->status['running'] ? $exitcode : $this->status['exitcode'];
     }
 
     /**

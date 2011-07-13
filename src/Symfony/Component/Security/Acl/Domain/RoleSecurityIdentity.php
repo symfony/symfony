d80ee4113058f826d7e81d44ce3fa4109bf110cf
Fabien Potencier <fabien.potencier@gmail.com>
2011-07-13 12:21:56 +0200

    Revert "merged branch yktd26/master (PR #1673)"
    This reverts commit af70ac8d777873c49347ac828a817a400006cbea, reversing
    changes made to c881379fe7b849c4262b6b47af2834c94393a130.

--- src/Symfony/Component/Security/Acl/Domain/RoleSecurityIdentity.php
+++ src/Symfony/Component/Security/Acl/Domain/RoleSecurityIdentity.php
@@ -12,7 +12,7 @@
 namespace Symfony\Component\Security\Acl\Domain;
 
 use Symfony\Component\Security\Acl\Model\SecurityIdentityInterface;
-use Symfony\Component\Security\Core\Role\RoleInterface;
+use Symfony\Component\Security\Core\Role\Role;
 
 /**
  * A SecurityIdentity implementation for roles
@@ -31,7 +31,7 @@ final class RoleSecurityIdentity implements SecurityIdentityInterface
      */
     public function __construct($role)
     {
-        if ($role instanceof RoleInterface) {
+        if ($role instanceof Role) {
             $role = $role->getRole();
         }
 

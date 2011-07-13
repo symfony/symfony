26e96c4ab0075c5017e98a3cdf04693e0214d5ae
marc.weistroff <marc.weistroff@sensio.com>
2011-07-13 08:44:37 +0200

    [DoctrineBridge] Changed namespace of EntityUserProvider[B

--- src/Symfony/Bridge/Doctrine/Security/User/EntityUserProvider.php
+++ src/Symfony/Bridge/Doctrine/Security/User/EntityUserProvider.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Component\Security\Core\User;
+namespace Symfony\Bridge\Doctrine\Security\User;
 
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;

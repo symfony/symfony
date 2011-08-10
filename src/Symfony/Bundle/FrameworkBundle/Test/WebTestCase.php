9e93cea1700c7210ba610f0cdfa783cfd886f835
realmfoo <konstantin.leboev@gmail.com>
2011-08-10 11:16:31 +0400

    expanded namespaces within phpdoc (special for PhpStorm)

--- src/Symfony/Bundle/FrameworkBundle/Test/WebTestCase.php
+++ src/Symfony/Bundle/FrameworkBundle/Test/WebTestCase.php
@@ -32,7 +32,7 @@ abstract class WebTestCase extends \PHPUnit_Framework_TestCase
      * @param array   $options An array of options to pass to the createKernel class
      * @param array   $server  An array of server parameters
      *
-     * @return Client A Client instance
+     * @return \Symfony\Bundle\FrameworkBundle\Client A Client instance
      */
     static protected function createClient(array $options = array(), array $server = array())
     {

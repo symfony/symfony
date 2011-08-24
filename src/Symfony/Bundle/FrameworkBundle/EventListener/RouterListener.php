d7f078929cea2a6ed756aae75912db2e765cf28e
Fabien Potencier <fabien.potencier@gmail.com>
2011-08-24 15:10:47 +0200

    [FrameworkBundle] fixed duplicated RequestContext instances

--- src/Symfony/Bundle/FrameworkBundle/EventListener/RouterListener.php
+++ src/Symfony/Bundle/FrameworkBundle/EventListener/RouterListener.php
@@ -49,19 +49,16 @@ class RouterListener
         }
 
         $request = $event->getRequest();
+        $context = $this->router->getContext();
 
         // set the context even if the parsing does not need to be done
         // to have correct link generation
-        $context = new RequestContext(
-            $request->getBaseUrl(),
-            $request->getMethod(),
-            $request->getHost(),
-            $request->getScheme(),
-            $request->isSecure() ? $this->httpPort : $request->getPort(),
-            $request->isSecure() ? $request->getPort() : $this->httpsPort
-        );
-
-        $this->router->setContext($context);
+        $context->setBaseUrl($request->getBaseUrl());
+        $context->setMethod($request->getMethod());
+        $context->setHost($request->getHost());
+        $context->setScheme($request->getScheme());
+        $context->setHttpPort($request->isSecure() ? $this->httpPort : $request->getPort());
+        $context->setHttpsPort($request->isSecure() ? $request->getPort() : $this->httpsPort);
     }
 
     public function onKernelRequest(GetResponseEvent $event)

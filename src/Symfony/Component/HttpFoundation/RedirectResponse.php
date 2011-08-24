1a463d03c20f47eac57ac11e869742faaf82fe91
Joseph Bielawski <stloyd@gmail.com>
2011-08-24 05:31:42 +0300

    [RedirectResponse] Added missing `doctype` and `title` tag

--- src/Symfony/Component/HttpFoundation/RedirectResponse.php
+++ src/Symfony/Component/HttpFoundation/RedirectResponse.php
@@ -37,10 +37,13 @@ class RedirectResponse extends Response
         }
 
         parent::__construct(
-            sprintf('<html>
+            sprintf('<!DOCTYPE html>
+<html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="refresh" content="1;url=%1$s" />
+
+        <title>Redirecting to %1$s</title>
     </head>
     <body>
         Redirecting to <a href="%1$s">%1$s</a>.

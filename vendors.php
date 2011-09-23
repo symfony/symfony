709233c24dc7c86d441d1b122edac3fff2c3f553
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-09-23 09:52:33 +0200

    updated vendors to point to lsmith77's fork of doctrine-common until its merged

--- vendors.php
+++ vendors.php
@@ -28,7 +28,7 @@ if (!is_dir($vendorDir = dirname(__FILE__).'/vendor')) {
 $deps = array(
     array('doctrine', 'http://github.com/doctrine/doctrine2.git', 'origin/2.1.x'),
     array('doctrine-dbal', 'http://github.com/doctrine/dbal.git', 'origin/2.1.x'),
-    array('doctrine-common', 'http://github.com/doctrine/common.git', 'origin/2.1.x'),
+    array('doctrine-common', 'http://github.com/lsmith77/common.git', 'origin/ManagerRegistry'),
     array('monolog', 'http://github.com/Seldaek/monolog.git', '1.0.1'),
     array('swiftmailer', 'http://github.com/swiftmailer/swiftmailer.git', 'v4.1.2'),
     array('twig', 'http://github.com/fabpot/Twig.git', 'v1.2.0'),

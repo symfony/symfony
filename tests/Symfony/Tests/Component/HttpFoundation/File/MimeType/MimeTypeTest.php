66ba5e1b2c1f33272aa2da6cd6271f5c7ba3f3df
Miha Vrhovnik <miha.vrhovnik@cordia.si>
2011-10-08 15:21:30 +0300

    Skip test if running as superuser as it will always fail

--- tests/Symfony/Tests/Component/HttpFoundation/File/MimeType/MimeTypeTest.php
+++ tests/Symfony/Tests/Component/HttpFoundation/File/MimeType/MimeTypeTest.php
@@ -79,6 +79,10 @@ class MimeTypeTest extends \PHPUnit_Framework_TestCase
             $this->markTestSkipped('Can not verify chmod operations on Windows');
         }
 
+        if (in_array(get_current_user(), array('root'))) {
+            $this->markTestSkipped('This test will fail if run under superuser'
+        }
+
         $path = __DIR__.'/../Fixtures/to_delete';
         touch($path);
         chmod($path, 0333);

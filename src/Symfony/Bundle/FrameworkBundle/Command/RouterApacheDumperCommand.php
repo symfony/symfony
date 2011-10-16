63d2ce28fd43c6a2feccdbd94da6dcc7361444d2
Christophe Coevoet <stof@notk.org>
2011-10-16 13:56:51 +0200

    [FrameworkBundle] Fixed the ckeck for the router class
    The getRouteCollection method is now part of the RouterInterface so the
    command should accept any implementation of the interface instead of just
    the implementations extending the core one.

--- src/Symfony/Bundle/FrameworkBundle/Command/RouterApacheDumperCommand.php
+++ src/Symfony/Bundle/FrameworkBundle/Command/RouterApacheDumperCommand.php
@@ -15,9 +15,8 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Routing\Matcher\Dumper\ApacheMatcherDumper;
-use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * RouterApacheDumperCommand.
@@ -35,7 +34,7 @@ class RouterApacheDumperCommand extends ContainerAwareCommand
             return false;
         }
         $router = $this->getContainer()->get('router');
-        if (!$router instanceof Router) {
+        if (!$router instanceof RouterInterface) {
             return false;
         }
         return parent::isEnabled();

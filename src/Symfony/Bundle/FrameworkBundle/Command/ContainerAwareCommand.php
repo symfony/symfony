b5cc00b1c8264db0e108bcc6cc23756230304e72
Hugo Hamon <hugo.hamon@sensio.com>
2011-10-16 18:42:34 +0200

    [FrameworkBundle] removed "use" statements for unused classes.

--- src/Symfony/Bundle/FrameworkBundle/Command/ContainerAwareCommand.php
+++ src/Symfony/Bundle/FrameworkBundle/Command/ContainerAwareCommand.php
@@ -11,11 +11,6 @@
 
 namespace Symfony\Bundle\FrameworkBundle\Command;
 
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;

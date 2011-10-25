6cb7acfa7526b973390a7da1c78c3ce474a5eddb
Eric Clemmons <eric@smarterspam.com>
2011-10-25 09:12:57 -0700

    CS - camelCase & curly braces

--- tests/Symfony/Tests/Bridge/Doctrine/Fixtures/ItemGroupEntity.php
+++ tests/Symfony/Tests/Bridge/Doctrine/Fixtures/ItemGroupEntity.php
@@ -16,11 +16,12 @@ class ItemGroupEntity
     public $name;
 
     /** @Column(type="string", nullable=true) */
-    public $group_name;
+    public $groupName;
 
-    public function __construct($id, $name, $group_name) {
+    public function __construct($id, $name, $groupName)
+    {
         $this->id = $id;
         $this->name = $name;
-        $this->group_name = $group_name;
+        $this->groupName = $groupName;
     }
 }

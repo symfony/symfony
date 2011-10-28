2cbccbd9834883acce0aaede42d19cabf27d238c
Cédric Lahouste <cedric.lahouste@gmail.com>
2011-10-28 09:38:14 +0300

    Allow empty result; change default *choices* value to **null** instead of **array()**. - added *testEmptyChoicesAreManaged* test - `null` as default value for choices. - is_array() used to test if choices are user-defined. - `null` as default value in __construct too. - `null` as default value for choices in EntityType.

--- tests/Symfony/Tests/Bridge/Doctrine/Form/ChoiceList/EntityChoiceListTest.php
+++ tests/Symfony/Tests/Bridge/Doctrine/Form/ChoiceList/EntityChoiceListTest.php
@@ -88,6 +88,26 @@ class EntityChoiceListTest extends DoctrineOrmTestCase
         $this->assertSame(array(1 => 'Foo', 2 => 'Bar'), $choiceList->getChoices());
     }
 
+    public function testEmptyChoicesAreManaged()
+    {
+        $entity1 = new SingleIdentEntity(1, 'Foo');
+        $entity2 = new SingleIdentEntity(2, 'Bar');
+
+        // Persist for managed state
+        $this->em->persist($entity1);
+        $this->em->persist($entity2);
+
+        $choiceList = new EntityChoiceList(
+            $this->em,
+            self::SINGLE_IDENT_CLASS,
+            'name',
+            null,
+            array()
+        );
+
+        $this->assertSame(array(), $choiceList->getChoices());
+    }
+
     public function testNestedChoicesAreManaged()
     {
         $entity1 = new SingleIdentEntity(1, 'Foo');

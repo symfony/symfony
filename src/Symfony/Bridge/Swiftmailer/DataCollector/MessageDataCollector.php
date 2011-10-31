e490af8108bbe7187dde3b7dc507c27b08f5336e
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-10-31 16:15:10 +0100

    added MessageDataCollector forward compatibility

---
+++ src/Symfony/Bridge/Swiftmailer/DataCollector/MessageDataCollector.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bridge\Swiftmailer\DataCollector;
+
+use Symfony\Bundle\SwiftmailerBundle\DataCollector\MessageDataCollector as BaseMessageDataCollector;
+
+/**
+ * MessageDataCollector.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ * @author Clément JOBEILI <clement.jobeili@gmail.com>
+ */
+class MessageDataCollector extends BaseMessageDataCollector
+{
+}

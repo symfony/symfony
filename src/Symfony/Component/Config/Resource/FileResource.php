8dd13e8119d3e2f724f0ecf1c866de0b03a7e3c5
Christophe Coevoet <stof@notk.org>
2011-11-01 02:15:01 +0100

    [Config] Implemented Serializable on resources

--- src/Symfony/Component/Config/Resource/FileResource.php
+++ src/Symfony/Component/Config/Resource/FileResource.php
@@ -18,7 +18,7 @@ namespace Symfony\Component\Config\Resource;
  *
  * @author Fabien Potencier <fabien@symfony.com>
  */
-class FileResource implements ResourceInterface
+class FileResource implements ResourceInterface, \Serializable
 {
     private $resource;
 
@@ -66,5 +66,15 @@ class FileResource implements ResourceInterface
         }
 
         return filemtime($this->resource) < $timestamp;
+    }
+
+    public function serialize()
+    {
+        return serialize($this->resource);
+    }
+
+    public function unserialize($serialized)
+    {
+        $this->resource = unserialize($serialized);
     }
 }

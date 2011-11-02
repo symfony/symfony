0682e6152e237c284b6aa22e8300ad3f06d6c088
Fabien Potencier <fabien.potencier@gmail.com>
2011-11-02 06:58:56 +0100

    [Process] removed an incomplete test that cannot be fixed

--- tests/Symfony/Tests/Component/Process/PhpExecutableFinderTest.php
+++ tests/Symfony/Tests/Component/Process/PhpExecutableFinderTest.php
@@ -52,27 +52,4 @@ class PhpExecutableFinderTest extends \PHPUnit_Framework_TestCase
             $this->assertEquals($current, PHP_BINDIR.DIRECTORY_SEPARATOR.'php', '::find() returns the executable php with suffixes');
         }
     }
-
-    /**
-     * tests find() with env var PHP_BINDIR
-     */
-    public function testFindWithPHP_PEAR_PHP_BIN()
-    {
-        //TODO the code for suffixes in PHP_BINDIR always catches, so the rest cant be tested
-        //maybe remove the code or move the PHP_PEAR_PHP_BIN code above
-
-        $this->markTestIncomplete();
-
-        $f = new PhpExecutableFinder();
-
-        $current = $f->find();
-
-        //not executable PHP_PEAR_PHP_BIN
-        putenv('PHP_PEAR_PHP_BIN=/not/executable/php');
-        $this->assertFalse($f->find(), '::find() returns false for not executable php');
-
-        //executable PHP_PEAR_PHP_BIN
-        putenv('PHP_PEAR_PHP_BIN='.$current);
-        $this->assertEquals($f->find(), $current, '::find() returns the executable php');
-    }
 }

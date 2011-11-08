f8e27872249be66cd8f0524bb452bbf04bf533d5
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-11-08 21:04:30 +0100

    refactored Normalizer interfaces

---
+++ src/Symfony/Component/Serializer/Normalizer/DenormalizerInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Symfony\Component\Serializer\Normalizer;
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * Defines the interface of denormalizers.
+ *
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+interface DenormalizerInterface
+{
+    /**
+     * Denormalizes data back into an object of the given class
+     *
+     * @param mixed $data data to restore
+     * @param string $class the expected class to instantiate
+     * @param string $format format the given data was extracted from
+     * @return object
+     */
+    function denormalize($data, $class, $format = null);
+}

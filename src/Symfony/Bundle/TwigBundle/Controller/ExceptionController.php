84e4920c5c66400adbc63419144f459c936f053b
Igor Wiedler <igor@wiedler.ch>
2011-11-11 12:03:15 +0100

    [TwigBundle] Do not clean output buffering below initial level
    This resulted in issues with PHPUnit 3.6, which will buffer all output and clean them in the end. Since
    we cleaned their buffer, the subsequent clean would raise a warning. This is documented in issue 390 of
    the PHPUnit tracker.
    
    Closes #2531.

--- src/Symfony/Bundle/TwigBundle/Controller/ExceptionController.php
+++ src/Symfony/Bundle/TwigBundle/Controller/ExceptionController.php
@@ -41,8 +41,9 @@ class ExceptionController extends ContainerAware
         // some Windows configurations where ob_get_level()
         // never reaches 0
         $count = 100;
+        $startObLevel = $this->container->get('kernel')->getStartObLevel();
         $currentContent = '';
-        while (ob_get_level() && --$count) {
+        while (ob_get_level() > $startObLevel && --$count) {
             $currentContent .= ob_get_clean();
         }
 

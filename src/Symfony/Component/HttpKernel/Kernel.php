3ef8079b50e1e4fa21ad652685a9293d785c38b6
Fabien Potencier <fabien.potencier@gmail.com>
2011-11-17 06:58:18 +0100

    bumped Symfony version to 2.0.7-DEV

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -57,7 +57,7 @@ abstract class Kernel implements KernelInterface
     protected $startTime;
     protected $classes;
 
-    const VERSION = '2.0.6';
+    const VERSION = '2.0.7-DEV';
 
     /**
      * Constructor.

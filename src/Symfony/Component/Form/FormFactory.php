36cebf0924bad80712a964617c5ec26e8a9a8de1
Dariusz Górecki <darek.krk@gmail.com>
2011-11-18 14:23:22 +0100

    Fix infinite loop on circullar reference in form factory
    When `->getName()` returns the same as `getParent()` we're going to infinite loop.

--- src/Symfony/Component/Form/FormFactory.php
+++ src/Symfony/Component/Form/FormFactory.php
@@ -220,6 +220,10 @@ class FormFactory implements FormFactoryInterface
 
         while (null !== $type) {
             if ($type instanceof FormTypeInterface) {
+                if ($type->getName() == $type->getParent($options)) {
+                    throw new FormException(sprintf('The "%s" form type name ("%s") is not valid. It contains circullar reference to parent type name ("%s")', get_class($type), $type->getName(), $type->getParent($options)));
+                }
+
                 $this->addType($type);
             } else {
                 $type = $this->getType($type);

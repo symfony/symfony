5d570b5e4baaf51c5f272239140a2584d5703c76
Dustin Dobervich <ddobervich@gmail.com>
2011-12-12 16:39:17 -0600

    added cache clear hook

---
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/AddCacheClearerPass.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Symfony\Bundle\FrameworkBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Reference;
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * Registers the cache clearers.
+ *
+ * @author Dustin Dobervich <ddobervich@gmail.com>
+ */
+class AddCacheClearerPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('cache_clearer')) {
+            return;
+        }
+
+        $clearers = array();
+        foreach ($container->findTaggedServiceIds('kernel.cache_clearer') as $id => $attributes) {
+            $clearers[] = new Reference($id);
+        }
+
+        $container->getDefinition('cache_clearer')->replaceArgument(0, $clearers);
+    }
+}
+

f03692a507e1591c9e2ec3a6d91bde3aa7ccad56
Fabien Potencier <fabien.potencier@gmail.com>
2011-12-17 11:02:17 +0100

    fixed functional tests so that the cache/logs are specific to one version of Symfony (to avoid weird side effects)

--- src/Symfony/Bundle/FrameworkBundle/Tests/Functional/WebTestCase.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Functional/WebTestCase.php
@@ -13,6 +13,7 @@ namespace Symfony\Bundle\FrameworkBundle\Tests\Functional;
 
 use Symfony\Component\HttpKernel\Util\Filesystem;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
+use Symfony\Component\HttpKernel\Kernel;
 
 class WebTestCase extends BaseWebTestCase
 {
@@ -33,7 +34,7 @@ class WebTestCase extends BaseWebTestCase
 
     protected function deleteTmpDir($testCase)
     {
-        if (!file_exists($dir = sys_get_temp_dir().'/'.$testCase)) {
+        if (!file_exists($dir = sys_get_temp_dir().'/'.Kernel::VERSION.'/'.$testCase)) {
             return;
         }
 

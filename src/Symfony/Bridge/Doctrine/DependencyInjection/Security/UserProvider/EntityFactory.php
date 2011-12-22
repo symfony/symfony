9653be618a72885fea2c93d45afab3f57226bcc2
Christophe Coevoet <stof@notk.org>
2011-12-22 16:54:29 +0100

    Moved the EntityFactory to the bridge

--- src/Symfony/Bridge/Doctrine/DependencyInjection/Security/UserProvider/EntityFactory.php
+++ src/Symfony/Bridge/Doctrine/DependencyInjection/Security/UserProvider/EntityFactory.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Bundle\DoctrineBundle\DependencyInjection\Security\UserProvider;
+namespace Symfony\Bridge\Doctrine\DependencyInjection\Security\UserProvider;
 
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 
@@ -25,10 +25,19 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  */
 class EntityFactory implements UserProviderFactoryInterface
 {
+    private $key;
+    private $providerId;
+
+    public function __construct($key, $providerId)
+    {
+        $this->key = $key;
+        $this->providerId = $providerId;
+    }
+
     public function create(ContainerBuilder $container, $id, $config)
     {
         $container
-            ->setDefinition($id, new DefinitionDecorator('doctrine.orm.security.user.provider'))
+            ->setDefinition($id, new DefinitionDecorator($this->providerId))
             ->addArgument($config['class'])
             ->addArgument($config['property'])
             ->addArgument($config['manager_name'])
@@ -37,7 +46,7 @@ class EntityFactory implements UserProviderFactoryInterface
 
     public function getKey()
     {
-        return 'entity';
+        return $this->key;
     }
 
     public function addConfiguration(NodeDefinition $node)

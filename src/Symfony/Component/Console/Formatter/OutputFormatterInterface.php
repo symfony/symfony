03f23f9bcf7f09d4911aeff66f80ffae4c96c5aa
Javier Eguíluz <javier.eguiluz@gmail.com>
2011-12-28 10:29:22 +0100

    [Console][Formatter] Added missing PHPDoc @throws and fixed some minor typos and grammatical errors

--- src/Symfony/Component/Console/Formatter/OutputFormatterInterface.php
+++ src/Symfony/Component/Console/Formatter/OutputFormatterInterface.php
@@ -23,7 +23,7 @@ interface OutputFormatterInterface
     /**
      * Sets the decorated flag.
      *
-     * @param Boolean $decorated Whether to decorated the messages or not
+     * @param Boolean $decorated Whether to decorate the messages or not
      *
      * @api
      */

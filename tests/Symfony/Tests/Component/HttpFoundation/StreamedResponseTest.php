7ae93483f9d13931342cb4bc06b27a6598708026
Igor Wiedler <igor@wiedler.ch>
2012-01-02 20:21:31 +0100

    [streaming] Document and test that Transfer-Encoding is absent

--- tests/Symfony/Tests/Component/HttpFoundation/StreamedResponseTest.php
+++ tests/Symfony/Tests/Component/HttpFoundation/StreamedResponseTest.php
@@ -33,6 +33,7 @@ class StreamedResponseTest extends \PHPUnit_Framework_TestCase
         $response->prepare($request);
 
         $this->assertEquals('1.1', $response->getProtocolVersion());
+        $this->assertNotEquals('chunked', $response->headers->get('Transfer-Encoding'), 'Apache assumes responses with a Transfer-Encoding header set to chunked to already be encoded.');
         $this->assertEquals('no-cache, private', $response->headers->get('Cache-Control'));
     }
 

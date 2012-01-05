a9172623e67f7efc9feded12cd6c31f12347614a
Luis Cordova <cordoval@gmail.com>
2012-01-05 09:36:07 -0500

    added WebSpec an analog class WebTestCase but for PHPSpec spec framework for specking symfony2 projects

---
+++ src/Symfony/Bundle/FrameworkBundle/Spec/WebSpec.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\FrameworkBundle\Spec;
+
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * WebSpec is the base class for BDD specifications.
+ *
+ * @author Luis Cordova <cordoval@gmail.com>
+ */
+abstract class WebSpec extends \PHPSpec\Context
+{
+    static protected $class;
+    static protected $kernel;
+
+    /**
+     * Creates a Client.
+     *
+     * @param array   $options An array of options to pass to the createKernel class
+     * @param array   $server  An array of server parameters
+     *
+     * @return Client A Client instance
+     */
+    static protected function createClient(array $options = array(), array $server = array())
+    {
+        static::$kernel = static::createKernel($options);
+        static::$kernel->boot();
+
+        $client = static::$kernel->getContainer()->get('test.client');
+        $client->setServerParameters($server);
+
+        return $client;
+    }
+
+    /**
+     * Creates a Kernel.
+     *
+     * Available options:
+     *
+     *  * environment
+     *  * debug
+     *
+     * @param array $options An array of options
+     *
+     * @return HttpKernelInterface A HttpKernelInterface instance
+     */
+    static protected function createKernel(array $options = array())
+    {
+        if (null === static::$class) {
+            static::$class = static::getKernelClass();
+        }
+
+        return new static::$class(
+            isset($options['environment']) ? $options['environment'] : 'test',
+            isset($options['debug']) ? $options['debug'] : true
+        );
+    }
+
+    /**
+     * Attempts to guess the kernel location.
+     *
+     * When the Kernel is located, the file is required.
+     *
+     * @return string The Kernel class name
+     */
+    static protected function getKernelClass()
+    {
+        $dir = isset($_SERVER['KERNEL_DIR']) ? $_SERVER['KERNEL_DIR'] : static::getPhpUnitXmlDir();
+
+        $finder = new Finder();
+        $finder->name('*Kernel.php')->depth(0)->in($dir);
+        $results = iterator_to_array($finder);
+        if (!count($results)) {
+            throw new \RuntimeException('Either set KERNEL_DIR in your phpunit.xml according to http://symfony.com/doc/current/book/testing.html#your-first-functional-test or override the WebTestCase::createKernel() method.');
+        }
+
+        $file = current($results);
+        $class = $file->getBasename('.php');
+
+        require_once $file;
+
+        return $class;
+    }
+
+    /**
+     * Finds the directory where the phpunit.xml(.dist) is stored.
+     *
+     * If you run tests with the PHPUnit CLI tool, everything will work as expected.
+     * If not, override this method in your test classes.
+     *
+     * @return string The directory where phpunit.xml(.dist) is stored
+     */
+    static protected function getPhpUnitXmlDir()
+    {
+        if (!isset($_SERVER['argv']) || false === strpos($_SERVER['argv'][0], 'phpspec')) {
+            throw new \RuntimeException('You must override the WebTestCase::createKernel() method.');
+        }
+        // vendor/symfony/src/Symfony/Bundle/FrameworkBundle/Spec/
+        $dir = __DIR__.'/../../../../../../../app/';
+
+        // Can't continue
+        if ($dir === null) {
+            throw new \RuntimeException('Unable to guess the Kernel directory.');
+        }
+
+        if (!is_dir($dir)) {
+            $dir = dirname($dir);
+        }
+
+        return $dir;
+    }
+
+    /**
+     * Shuts the kernel down if it was used in the test.
+     */
+    public function after()
+    {
+        if (null !== static::$kernel) {
+            static::$kernel->shutdown();
+        }
+    }
+}

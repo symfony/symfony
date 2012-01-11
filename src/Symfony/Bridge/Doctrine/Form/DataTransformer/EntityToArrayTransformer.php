064cdd331bac3117cda85384c4add5e653b27c1b
Cedric Lahouste <cedric.lahouste@gmail.com>
2012-01-11 19:02:16 +0100

    Add widget property to allow text field for the entitytype without having to retrieve the whole table content.

---
+++ src/Symfony/Bridge/Doctrine/Form/DataTransformer/EntityToArrayTransformer.php
@@ -0,0 +1,215 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bridge\Doctrine\Form\DataTransformer;
+
+use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityChoiceOnRequestList;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Doctrine\Common\Collections\Collection;
+
+use Symfony\Component\Form\Util\PropertyPath;
+use Symfony\Component\Form\Exception\FormException;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\NoResultException;
+
+class EntityToArrayTransformer implements DataTransformerInterface
+{
+    /**
+     * @var Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * @var Doctrine\ORM\Mapping\ClassMetadata
+     */
+    private $class;
+    
+    /**
+     * @var \Doctrine\Common\Persistence\Mapping\ClassMetadata
+     */
+    private $classMetadata;    
+    
+    /**
+     * Contains the query builder that builds the query for fetching the
+     * entities
+     *
+     * This property should only be accessed through queryBuilder.
+     *
+     * @var Doctrine\ORM\QueryBuilder
+     */
+    private $queryBuilder;
+    
+    /**
+     * The fields of which the identifier of the underlying class consists
+     *
+     * This property should only be accessed through identifier.
+     *
+     * @var array
+     */
+    private $identifier = array();    
+    
+    private $propertyPath;
+    
+    private $property;
+    
+    /**
+     * Constructor.
+     *
+     * @param ObjectManager         $manager      An EntityManager instance
+     * @param string                $class        The class name
+     * @param string                $property     The property name
+     * @param QueryBuilder|\Closure $queryBuilder An optional query builder
+     */
+    public function __construct(ObjectManager $manager, $class, $property = null, $queryBuilder = null)
+    {
+        // If a query builder was passed, it must be a closure or QueryBuilder
+        // instance
+        if (!(null === $queryBuilder || $queryBuilder instanceof QueryBuilder || $queryBuilder instanceof \Closure)) {
+            throw new UnexpectedTypeException($queryBuilder, 'Doctrine\ORM\QueryBuilder or \Closure');
+        }
+
+        if ($queryBuilder instanceof \Closure) {
+            $queryBuilder = $queryBuilder($manager->getRepository($class));
+
+            if (!$queryBuilder instanceof QueryBuilder) {
+                throw new UnexpectedTypeException($queryBuilder, 'Doctrine\ORM\QueryBuilder');
+            }
+        }
+
+        $this->em = $manager;
+        $this->class = $class;
+        $this->property = $property;
+        $this->classMetadata = $this->em->getClassMetadata($class);
+        $this->queryBuilder = $queryBuilder;
+        $this->identifier = $this->em->getClassMetadata($class)->getIdentifierFieldNames();
+
+        // The property option defines, which property (path) is used for
+        // displaying entities as strings
+        if ($property) {
+            $this->propertyPath = new PropertyPath($property);
+        } elseif (!method_exists($this->classMetadata->getName(), '__toString')) {
+            // Otherwise expect a __toString() method in the entity
+            throw new FormException('Entities passed to the choice field must have a "__toString()" method defined (or you can also override the "property" option).');
+        }
+    }
+
+    /**
+     * Transforms entities into choice keys.
+     *
+     * @param Collection|object $entity A collection of entities, a single entity or NULL
+     *
+     * @return mixed An array of choice keys, a single key or NULL
+     */
+    public function transform($entity)
+    {
+        if (null === $entity || '' === $entity) {
+            return array();
+        }
+
+        if (!is_object($entity)) {
+            throw new UnexpectedTypeException($entity, 'object');
+        }
+
+        if ($entity instanceof Collection) {
+            throw new \InvalidArgumentException('Expected an object, but got a collection. Did you forget to pass "multiple=true" to an entity field?');
+        }
+        
+        $values = array('id' => current($this->getIdentifierValues($entity)));
+        
+        if ($this->property) {
+            $values[ $this->property ] = $this->propertyPath->getValue($entity);
+        }
+        
+        return $values;
+    }
+
+    /**
+     * Transforms choice keys into entities.
+     *
+     * @param  mixed $key   An array of keys, a single key or NULL
+     *
+     * @return Collection|object  A collection of entities, a single entity or NULL
+     */
+    public function reverseTransform($value)
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        if (!is_array($value)) {
+            throw new UnexpectedTypeException($value, 'array');
+        }
+        
+        if (implode('', $value) === '') {
+            return null;
+        }        
+        
+        if (isset($value['id']) && !ctype_digit($value['id']) && !is_int($value['id'])) {
+            throw new TransformationFailedException('Identifier is invalid');
+        }
+        
+        $key = $value['id'];
+
+        if(null === $key) {
+            return null;
+        }
+
+        if (count($this->identifier) > 1 && !is_numeric($key)) {
+            throw new UnexpectedTypeException($key, 'numeric');
+        }
+        
+        if ($qb = $this->queryBuilder) {
+            $alias = $qb->getRootAlias();
+            $where = $qb->expr()->eq($alias.'.'.current($this->identifier), $key);
+            try{
+                $entity = $qb->andWhere($where)->getQuery()->getSingleResult();
+            } catch (NoResultException $e) {
+                return null;
+            }
+        } else {
+            $entity = $this->em->find($this->class, $key);
+        }
+
+        if (!$entity) {
+            return null;
+        }
+
+        return $entity;
+    }
+    
+    /**
+     * Returns the values of the identifier fields of an entity.
+     *
+     * Doctrine must know about this entity, that is, the entity must already
+     * be persisted or added to the identity map before. Otherwise an
+     * exception is thrown.
+     *
+     * @param  object $entity The entity for which to get the identifier
+     *
+     * @return array          The identifier values
+     *
+     * @throws FormException  If the entity does not exist in Doctrine's identity map
+     */
+    public function getIdentifierValues($entity)
+    {
+        if (!$this->em->contains($entity)) {
+            throw new FormException('Entities passed to the choice field must be managed');
+        }
+
+        $this->em->initializeObject($entity);
+
+        return $this->classMetadata->getIdentifierValues($entity);
+    }
+    
+}
--- src/Symfony/Bridge/Doctrine/Form/Type/DoctrineType.php
+++ src/Symfony/Bridge/Doctrine/Form/Type/DoctrineType.php
@@ -19,7 +19,9 @@ use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityLoaderInterface;
 use Symfony\Bridge\Doctrine\Form\EventListener\MergeCollectionListener;
 use Symfony\Bridge\Doctrine\Form\DataTransformer\EntitiesToArrayTransformer;
 use Symfony\Bridge\Doctrine\Form\DataTransformer\EntityToIdTransformer;
+use Symfony\Bridge\Doctrine\Form\DataTransformer\EntityToArrayTransformer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Exception\FormException;
 
 abstract class DoctrineType extends AbstractType
 {
@@ -35,13 +37,39 @@ abstract class DoctrineType extends AbstractType
 
     public function buildForm(FormBuilder $builder, array $options)
     {
-        if ($options['multiple']) {
-            $builder
-                ->addEventSubscriber(new MergeCollectionListener())
-                ->prependClientTransformer(new EntitiesToArrayTransformer($options['choice_list']))
-            ;
+        if($options['widget'] === 'choice') {
+            if ($options['multiple']) {
+                $builder
+                    ->addEventSubscriber(new MergeCollectionListener())
+                    ->prependClientTransformer(new EntitiesToArrayTransformer($options['choice_list']))
+                ;
+            } else {
+                $builder->prependClientTransformer(new EntityToIdTransformer($options['choice_list']));
+            }
         } else {
-            $builder->prependClientTransformer(new EntityToIdTransformer($options['choice_list']));
+            if($options['multiple']) {
+                throw new FormException(sprintf('Using multiple entities is currently only supported with the widget "choice".'));
+            }
+            
+            $propertyOptions = $identifierOptions = array();
+            
+            foreach (array('required', 'translation_domain') as $passOpt) {
+                $propertyOptions[$passOpt] = $identifierOptions[$passOpt] = $options[$passOpt];
+            }
+            
+            if($options['property']) {
+                $builder->add($options['property'], 'text', $propertyOptions);
+            }
+            
+            $builder
+                ->add('id', $options['widget'], $identifierOptions)
+                ->prependClientTransformer(new EntityToArrayTransformer(
+                    $this->registry->getManager($options['em']), 
+                    $options['class'],
+                    $options['property'],
+                    $options['query_builder']
+                ))
+            ;            
         }
     }
 
@@ -55,11 +83,14 @@ abstract class DoctrineType extends AbstractType
             'loader'            => null,
             'choices'           => null,
             'group_by'          => null,
+            'widget'            => 'choice',
+            'preferred_choices' => array(),
+            'multiple'          => false,
         );
 
         $options = array_replace($defaultOptions, $options);
 
-        if (!isset($options['choice_list'])) {
+        if (!isset($options['choice_list']) && $options['widget'] === 'choice') {
             $manager = $this->registry->getManager($options['em']);
             if (isset($options['query_builder'])) {
                 $options['loader'] = $this->getLoader($manager, $options);
@@ -89,6 +120,6 @@ abstract class DoctrineType extends AbstractType
 
     public function getParent(array $options)
     {
-        return 'choice';
+        return $options['widget'] === 'choice' ? 'choice' : 'form';
     }
 }

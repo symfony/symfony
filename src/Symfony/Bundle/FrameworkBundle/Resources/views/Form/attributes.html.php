3768a4bd7a0c6bd2ccffd0b756ef894a0022763b
Helmer Aaviksoo <helmer.aaviksoo@gmail.com>
2012-01-26 17:54:42 +0200

    [Form] read_only and disabled attributes

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/attributes.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/attributes.html.php
@@ -1,6 +1,7 @@
 id="<?php echo $view->escape($id) ?>"
 name="<?php echo $view->escape($full_name) ?>"
-<?php if ($read_only): ?>disabled="disabled" <?php endif ?>
+<?php if ($read_only): ?>readonly="readonly" <?php endif ?>
+<?php if ($disabled): ?>disabled="disabled" <?php endif ?>
 <?php if ($required): ?>required="required" <?php endif ?>
 <?php if ($max_length): ?>maxlength="<?php echo $view->escape($max_length) ?>" <?php endif ?>
 <?php if ($pattern): ?>pattern="<?php echo $view->escape($pattern) ?>" <?php endif ?>
--- src/Symfony/Component/Form/Form.php
+++ src/Symfony/Component/Form/Form.php
@@ -166,7 +166,7 @@ class Form implements \IteratorAggregate, FormInterface
      * Whether this form may only be read, but not bound
      * @var Boolean
      */
-    private $readOnly = false;
+    private $disabled = false;
 
     /**
      * The dispatcher for distributing events of this form
@@ -190,7 +190,7 @@ class Form implements \IteratorAggregate, FormInterface
         array $types = array(), array $clientTransformers = array(),
         array $normTransformers = array(),
         DataMapperInterface $dataMapper = null, array $validators = array(),
-        $required = false, $readOnly = false, $errorBubbling = false,
+        $required = false, $disabled = false, $errorBubbling = false,
         $emptyData = null, array $attributes = array())
     {
         foreach ($clientTransformers as $transformer) {
@@ -219,7 +219,7 @@ class Form implements \IteratorAggregate, FormInterface
         $this->dataMapper = $dataMapper;
         $this->validators = $validators;
         $this->required = (Boolean) $required;
-        $this->readOnly = (Boolean) $readOnly;
+        $this->disabled = (Boolean) $disabled;
         $this->errorBubbling = (Boolean) $errorBubbling;
         $this->emptyData = $emptyData;
         $this->attributes = $attributes;
@@ -266,7 +266,6 @@ class Form implements \IteratorAggregate, FormInterface
     public function isRequired()
     {
         if (null === $this->parent || $this->parent->isRequired()) {
-
             return $this->required;
         }
 
@@ -274,21 +273,12 @@ class Form implements \IteratorAggregate, FormInterface
     }
 
     /**
-     * Returns whether this form is read only.
-     *
-     * The content of a read-only form is displayed, but not allowed to be
-     * modified. The validation of modified read-only forms should fail.
-     *
-     * Fields whose parents are read-only are considered read-only regardless of
-     * their own state.
-     *
-     * @return Boolean
+     * {@inheritDoc}
      */
-    public function isReadOnly()
+    public function isDisabled()
     {
-        if (null === $this->parent || !$this->parent->isReadOnly()) {
-
-            return $this->readOnly;
+        if (null === $this->parent || !$this->parent->isDisabled()) {
+            return $this->disabled;
         }
 
         return true;
@@ -457,7 +447,7 @@ class Form implements \IteratorAggregate, FormInterface
      */
     public function bind($clientData)
     {
-        if ($this->readOnly) {
+        if ($this->isDisabled()) {
             $this->bound = true;
 
             return $this;
@@ -674,7 +664,6 @@ class Form implements \IteratorAggregate, FormInterface
     {
         foreach ($this->children as $child) {
             if (!$child->isEmpty()) {
-
                 return false;
             }
         }
@@ -697,10 +686,9 @@ class Form implements \IteratorAggregate, FormInterface
             return false;
         }
 
-        if (!$this->readOnly) {
+        if (!$this->isDisabled()) {
             foreach ($this->children as $child) {
                 if (!$child->isValid()) {
-
                     return false;
                 }
             }
@@ -865,7 +853,6 @@ class Form implements \IteratorAggregate, FormInterface
     public function get($name)
     {
         if (isset($this->children[$name])) {
-
             return $this->children[$name];
         }
 

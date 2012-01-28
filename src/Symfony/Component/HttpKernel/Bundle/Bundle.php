f8043438a7834944304b3e8628a6a69421bf26c7
Adrián Chaves Fernández (Gallaecio) <adriyetichaves@gmail.com>
2012-01-28 17:20:27 +0100

    Usage of ’ instead of programming single quote.

--- src/Symfony/Component/HttpKernel/Bundle/Bundle.php
+++ src/Symfony/Component/HttpKernel/Bundle/Bundle.php
@@ -60,7 +60,7 @@ abstract class Bundle extends ContainerAware implements BundleInterface
     }
 
     /**
-     * Returns the bundle's container extension.
+     * Returns bundle’s container extension.
      *
      * @return ExtensionInterface|null The container extension
      *

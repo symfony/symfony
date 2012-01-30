9e7e2af08762a6ea356ae507ea917e5b56029346
Bernhard Schussek <bschussek@gmail.com>
2012-01-30 18:21:53 +0100

    [Form] Fixed PHPDoc: Used {@inheritdoc} where applicable

--- src/Symfony/Component/Form/Extension/Core/ChoiceList/LazyChoiceList.php
+++ src/Symfony/Component/Form/Extension/Core/ChoiceList/LazyChoiceList.php
@@ -32,10 +32,8 @@ abstract class LazyChoiceList implements ChoiceListInterface
     private $choiceList;
 
     /**
-    * Returns the list of choices
-     *
-    * @return array The choices with their indices as keys.
-    */
+     * {@inheritdoc}
+     */
     public function getChoices()
     {
         if (!$this->choiceList) {
@@ -46,10 +44,8 @@ abstract class LazyChoiceList implements ChoiceListInterface
     }
 
     /**
-    * Returns the values for the choices
-    *
-    * @return array The values with the corresponding choice indices as keys.
-    */
+     * {@inheritdoc}
+     */
     public function getValues()
     {
         if (!$this->choiceList) {
@@ -60,27 +56,8 @@ abstract class LazyChoiceList implements ChoiceListInterface
     }
 
     /**
-    * Returns the choice views of the preferred choices as nested array with
-    * the choice groups as top-level keys.
-    *
-    * Example:
-    *
-    * <source>
-    * array(
-    *     'Group 1' => array(
-    *         10 => ChoiceView object,
-    *         20 => ChoiceView object,
-    *     ),
-    *     'Group 2' => array(
-    *         30 => ChoiceView object,
-    *     ),
-    * )
-    * </source>
-    *
-    * @return array A nested array containing the views with the corresponding
-    *               choice indices as keys on the lowest levels and the choice
-    *               group names in the keys of the higher levels.
-    */
+     * {@inheritdoc}
+     */
     public function getPreferredViews()
     {
         if (!$this->choiceList) {
@@ -91,29 +68,8 @@ abstract class LazyChoiceList implements ChoiceListInterface
     }
 
     /**
-    * Returns the choice views of the choices that are not preferred as nested
-    * array with the choice groups as top-level keys.
-    *
-    * Example:
-    *
-    * <source>
-    * array(
-    *     'Group 1' => array(
-    *         10 => ChoiceView object,
-    *         20 => ChoiceView object,
-    *     ),
-    *     'Group 2' => array(
-    *         30 => ChoiceView object,
-    *     ),
-    * )
-    * </source>
-    *
-    * @return array A nested array containing the views with the corresponding
-    *               choice indices as keys on the lowest levels and the choice
-    *               group names in the keys of the higher levels.
-    *
-    * @see getPreferredValues
-    */
+     * {@inheritdoc}
+     */
     public function getRemainingViews()
     {
         if (!$this->choiceList) {
@@ -124,13 +80,8 @@ abstract class LazyChoiceList implements ChoiceListInterface
     }
 
     /**
-    * Returns the choices corresponding to the given values.
-    *
-    * @param array $values An array of choice values. Not existing values in
-    *                      this array are ignored.
-    *
-    * @return array An array of choices with ascending, 0-based numeric keys
-    */
+     * {@inheritdoc}
+     */
     public function getChoicesForValues(array $values)
     {
         if (!$this->choiceList) {
@@ -141,14 +92,8 @@ abstract class LazyChoiceList implements ChoiceListInterface
     }
 
     /**
-    * Returns the values corresponding to the given choices.
-    *
-    * @param array $choices An array of choices. Not existing choices in this
-    *                       array are ignored.
-    *
-    * @return array An array of choice values with ascending, 0-based numeric
-    *               keys
-    */
+     * {@inheritdoc}
+     */
     public function getValuesForChoices(array $choices)
     {
         if (!$this->choiceList) {
@@ -159,13 +104,8 @@ abstract class LazyChoiceList implements ChoiceListInterface
     }
 
     /**
-    * Returns the indices corresponding to the given choices.
-    *
-    * @param array $choices An array of choices. Not existing choices in this
-    *                       array are ignored.
-    *
-    * @return array An array of indices with ascending, 0-based numeric keys
-    */
+     * {@inheritdoc}
+     */
     public function getIndicesForChoices(array $choices)
     {
         if (!$this->choiceList) {
@@ -176,13 +116,8 @@ abstract class LazyChoiceList implements ChoiceListInterface
     }
 
     /**
-    * Returns the indices corresponding to the given values.
-    *
-    * @param array $values An array of choice values. Not existing values in
-    *                      this array are ignored.
-    *
-    * @return array An array of indices with ascending, 0-based numeric keys
-    */
+     * {@inheritdoc}
+     */
     public function getIndicesForValues(array $values)
     {
         if (!$this->choiceList) {

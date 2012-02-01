7824c3ea7e1184856cb353beae1639daa5aa57de
Bernhard Schussek <bschussek@gmail.com>
2012-02-01 18:52:43 +0100

    [Form] Implemented MergeCollectionListener which calls addXxx() and removeXxx() in your model if found
    The listener is used by the Collection type as well as the Choice and Entity type (with multiple
    selection). The effect is that you can have for example this model:
    
        class Article
        {
            public function addTag($tag) { ... }
            public function removeTag($tag) { ... }
            public function getTags($tag) { ... }
        }
    
    You can create a form for the article with a field "tags" of either type "collection" or "choice"
    (or "entity"). The field will correctly use the three methods of the model for displaying and
    editing tags.

--- src/Symfony/Component/Form/Extension/Core/Type/CollectionType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/CollectionType.php
@@ -16,6 +16,7 @@ use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\Extension\Core\EventListener\ResizeFormListener;
+use Symfony\Component\Form\Extension\Core\EventListener\MergeCollectionListener;
 
 class CollectionType extends AbstractType
 {
@@ -29,7 +30,7 @@ class CollectionType extends AbstractType
             $builder->setAttribute('prototype', $prototype->getForm());
         }
 
-        $listener = new ResizeFormListener(
+        $resizeListener = new ResizeFormListener(
             $builder->getFormFactory(),
             $options['type'],
             $options['options'],
@@ -37,8 +38,14 @@ class CollectionType extends AbstractType
             $options['allow_delete']
         );
 
+        $mergeListener = new MergeCollectionListener(
+            $options['allow_add'],
+            $options['allow_delete']
+        );
+
         $builder
-            ->addEventSubscriber($listener)
+            ->addEventSubscriber($resizeListener)
+            ->addEventSubscriber($mergeListener)
             ->setAttribute('allow_add', $options['allow_add'])
             ->setAttribute('allow_delete', $options['allow_delete'])
         ;

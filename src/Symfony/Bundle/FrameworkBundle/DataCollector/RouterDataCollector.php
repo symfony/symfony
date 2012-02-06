df53bfb94b6510bb0791221f5393606f1095753d
Victor Berchet <victor@suumit.com>
2012-02-06 13:04:17 +0100

    [FrameworkBundle] Add a RouterDataCollector

---
+++ src/Symfony/Bundle/FrameworkBundle/DataCollector/RouterDataCollector.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\FrameworkBundle\DataCollector;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Bundle\FrameworkBundle\Controller\ControllerResolver;
+use Symfony\Bundle\FrameworkBundle\Controller\RedirectController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+
+/**
+ * RouterDataCollector.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class RouterDataCollector extends DataCollector
+{
+    protected $controllers;
+
+    public function __construct()
+    {
+        $this->controllers = new \SplObjectStorage();
+
+        $this->data = array(
+            'redirect' => false,
+            'url'      => null,
+            'route'    => null,
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $redirect = false;
+        $info = array();
+
+        if ($response instanceof RedirectResponse) {
+            $this->data['redirect'] = true;
+            $this->data['url'] = $response->getTargetUrl();
+
+            if ($this->controllers->contains($request)) {
+                $controller = $this->controllers[$request];
+                if (is_array($controller)) {
+                    $controller = $controller[0];
+                }
+
+                if ($controller instanceof RedirectController) {
+                    $this->data['route'] = $request->attributes->get('_route', 'n/a');
+                }
+            }
+        }
+
+    }
+
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        $this->controllers[$event->getRequest()] = $event->getController();
+    }
+
+    /**
+     * @return Boolean Whether this request will result on a redirect
+     */
+    public function isRedirect()
+    {
+        return $this->data['redirect'];
+    }
+
+    /**
+     * @return string|null The target URL
+     */
+    public function getTargetUrl()
+    {
+        return $this->data['url'];
+    }
+
+    /**
+     * @return string|null The target route
+     */
+    public function getTargetRoute()
+    {
+        return $this->data['route'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'router';
+    }
+}

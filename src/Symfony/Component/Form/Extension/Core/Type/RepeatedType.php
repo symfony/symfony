3b0c70357d5ba1e1692bed390eea5e2c3b2bf890
Juti Noppornpitak <jnopporn@shiroyuki.com>
2012-02-07 09:52:10 -0500

    This is to add support to RepeatedType such that the users (developers) can provide options for each input field.
    This change is to mimic the change made for Symfony 2.1 but maintain the backward compatibility.

--- src/Symfony/Component/Form/Extension/Core/Type/RepeatedType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/RepeatedType.php
@@ -22,14 +22,73 @@ class RepeatedType extends AbstractType
      */
     public function buildForm(FormBuilder $builder, array $options)
     {
+        // Prepare for spliting options.
+        $splitOptions = array(
+            'first'  => array(),
+            'second' => array()
+        );
+        
+        // Split options. If the type of option is not specified, treat this as a global option.
+        foreach ($options['options'] as $optionKey => $optionValue) {
+            list($key, $type) = $this->extractSplitOption($optionKey);
+            
+            if ( ! $type) {
+                $splitOptions['first'][$key] = $optionValue;
+                $splitOptions['second'][$key] = $optionValue;
+                continue;
+            }
+            
+            $splitOptions[$type][$key] = $optionValue;
+        }
+        
         $builder
             ->appendClientTransformer(new ValueToDuplicatesTransformer(array(
                 $options['first_name'],
                 $options['second_name'],
             )))
-            ->add($options['first_name'], $options['type'], $options['options'])
-            ->add($options['second_name'], $options['type'], $options['options'])
+            ->add($options['first_name'], $options['type'], $splitOptions['options'])
+            ->add($options['second_name'], $options['type'], $splitOptions['options'])
         ;
+    }
+    
+    /**
+     * Check if this option key contains the given prefix.
+     *
+     * @param string $optionKey
+     * @param string $prefix
+     *
+     * @return boolean
+     */
+    protected function doesOptionKeyContain($optionKey, $prefix)
+    {
+        return strpos($optionKey, $prefix) === 0;
+    }
+    
+    /**
+     * Extract the option based on its prefix.
+     *
+     * @param string $optionKey
+     * @return array where the first index (0) is a sanitized option key and the second one is a type of option ('first' or 'second').
+     */
+    protected function extractSplitOption($optionKey)
+    {
+        $optionType = null;
+        
+        // Figure out the type of option
+        if ($this->doesOptionKeyContain($optionKey, 'first_') {
+            $optionType = 'first';
+        } else if ($this->doesOptionKeyContain($optionKey, 'second_') {
+            $optionType = 'second';
+        }
+        
+        if ($optionType) {
+            $optionKey = substr($optionKey, strlen($optionType) + 1);
+        }
+        
+        return array(
+            $optionKey,
+            $optionType
+        );
     }
 
     /**

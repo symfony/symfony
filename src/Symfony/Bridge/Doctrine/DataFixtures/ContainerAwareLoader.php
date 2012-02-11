c754f28fcb8e3b412eed256be13066256852f98e
Jeremy Mikola <jmikola@gmail.com>
2012-02-11 16:48:09 -0500

    [DoctrineBridge] Rename data fixtures loader class

--- src/Symfony/Bridge/Doctrine/DataFixtures/ContainerAwareLoader.php
+++ src/Symfony/Bridge/Doctrine/DataFixtures/ContainerAwareLoader.php
@@ -12,21 +12,29 @@
 namespace Symfony\Bridge\Doctrine\DataFixtures;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
-use Doctrine\Common\DataFixtures\Loader as BaseLoader;
+use Doctrine\Common\DataFixtures\Loader;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Doctrine data fixtures loader that injects the service container for
- * fixtures objects that implement ContainerAwareInterface.
+ * Doctrine data fixtures loader that injects the service container into
+ * fixture objects that implement ContainerAwareInterface.
  *
- * Note: This class cannot be used without the Doctrine data fixtures extension,
- * which is not listed as a required dependency for Symfony.
+ * Note: Use of this class requires the Doctrine data fixtures extension, which
+ * is a suggested dependency for Symfony.
  */
-class Loader extends BaseLoader
+class ContainerAwareLoader extends Loader
 {
+    /**
+     * @var ContainerInterface
+     */
     private $container;
 
+    /**
+     * Constructor.
+     *
+     * @param ContainerInterface $container A ContainerInterface instance
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;

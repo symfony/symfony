6e6f56b48b4d4ad1bdff398a5fcda9b22d9ba16b
Victor Berchet <victor@suumit.com>
2012-02-15 00:48:14 +0100

    [Config] Throw exceptions on invalid definition

--- src/Symfony/Component/Config/Definition/PrototypedArrayNode.php
+++ src/Symfony/Component/Config/Definition/PrototypedArrayNode.php
@@ -14,6 +14,7 @@ namespace Symfony\Component\Config\Definition;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\Definition\Exception\DuplicateKeyException;
 use Symfony\Component\Config\Definition\Exception\UnsetKeyException;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 /**
  * Represents a prototyped Array node in the config tree.
@@ -158,7 +159,7 @@ class PrototypedArrayNode extends ArrayNode
      */
     public function addChild(NodeInterface $node)
     {
-        throw new \RuntimeException('A prototyped array node can not have concrete children.');
+        throw new Exception('A prototyped array node can not have concrete children.');
     }
 
     /**

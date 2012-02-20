3c1688983598241a9cb432145996eb5d07de3977
Fabien Potencier <fabien.potencier@gmail.com>
2012-02-20 13:28:51 +0100

    Revert "fixed tests for the latest Twig"
    This reverts commit 3236fc5af3915be5d33ffd469fcf462b420880fd.

--- tests/Symfony/Tests/Bridge/Twig/Node/FormThemeTest.php
+++ tests/Symfony/Tests/Bridge/Twig/Node/FormThemeTest.php
@@ -62,6 +62,10 @@ class FormThemeTest extends TestCase
 
     protected function getVariableGetter($name)
     {
+        if (version_compare(phpversion(), '5.4.0RC1', '>=')) {
+            return sprintf('(isset($context["%s"]) ? $context["%s"] : null)', $name, $name);
+        }
+
         return sprintf('$this->getContext($context, "%s")', $name);
     }
 }

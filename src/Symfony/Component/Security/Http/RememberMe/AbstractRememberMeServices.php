ddeac9a9ea3e20ecaacb503f962be2d81c47cc8e
Fabien Potencier <fabien.potencier@gmail.com>
2012-03-02 21:45:57 +0100

    [Security] added support for the remember_me parameter in the query (closes #3460)

--- src/Symfony/Component/Security/Http/RememberMe/AbstractRememberMeServices.php
+++ src/Symfony/Component/Security/Http/RememberMe/AbstractRememberMeServices.php
@@ -283,7 +283,7 @@ abstract class AbstractRememberMeServices implements RememberMeServicesInterface
             return true;
         }
 
-        $parameter = $request->request->get($this->options['remember_me_parameter'], null, true);
+        $parameter = $request->get($this->options['remember_me_parameter'], null, true);
 
         if ($parameter === null && null !== $this->logger) {
             $this->logger->debug(sprintf('Did not send remember-me cookie (remember-me parameter "%s" was not sent).', $this->options['remember_me_parameter']));

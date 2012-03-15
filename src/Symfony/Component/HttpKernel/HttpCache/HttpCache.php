fbed9ff8debf67def7c5e496f85b50fca5edbc4f
Arnaud Buathier <arnaud@arnapou.net>
2012-03-15 20:27:40 +0100

    Update src/Symfony/Component/HttpKernel/HttpCache/HttpCache.php

--- src/Symfony/Component/HttpKernel/HttpCache/HttpCache.php
+++ src/Symfony/Component/HttpKernel/HttpCache/HttpCache.php
@@ -503,7 +503,8 @@ class HttpCache implements HttpKernelInterface
             // wait for the lock to be released
             $wait = 0;
             while (file_exists($lock) && $wait < 5000000) {
-                usleep($wait += 50000);
+                usleep(50000);
+                $wait += 50000;
             }
 
             if ($wait < 2000000) {

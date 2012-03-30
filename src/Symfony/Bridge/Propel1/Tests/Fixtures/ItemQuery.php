64c7183bf5ed24d12a33336b6e44518d7586b593
Toni Uebernickel <tuebernickel@gmail.com>
2012-03-30 10:53:43 +0200

    clean file docs in Propel1 fixtures

--- src/Symfony/Bridge/Propel1/Tests/Fixtures/ItemQuery.php
+++ src/Symfony/Bridge/Propel1/Tests/Fixtures/ItemQuery.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * this file is part of the symfony package.
+ * This file is part of the Symfony package.
  *
- * (c) fabien potencier <fabien@symfony.com>
+ * (c) Fabien Potencier <fabien@symfony.com>
  *
- * for the full copyright and license information, please view the license
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 

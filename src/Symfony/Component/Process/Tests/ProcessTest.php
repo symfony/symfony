c90ba7528e89a3b844decae7207f7ffee6212971
Jordi Boggiano <j.boggiano@seld.be>
2012-04-08 20:28:28 +0200

    [Process] Skip test that is still getting stuck on windows

--- src/Symfony/Component/Process/Tests/ProcessTest.php
+++ src/Symfony/Component/Process/Tests/ProcessTest.php
@@ -55,8 +55,8 @@ class ProcessTest extends \PHPUnit_Framework_TestCase
      */
     public function testProcessPipes($expected, $code)
     {
-        if (strpos(PHP_OS, "WIN") === 0 && version_compare(phpversion(), "5.3.9", "<")) {
-            $this->markTestSkipped('Test hangs on Windows & PHP due to https://bugs.php.net/bug.php?id=60120 fixed in http://svn.php.net/viewvc?view=revision&revision=318366');
+        if (strpos(PHP_OS, "WIN") === 0) {
+            $this->markTestSkipped('Test hangs on Windows & PHP due to https://bugs.php.net/bug.php?id=60120 and https://bugs.php.net/bug.php?id=51800');
         }
 
         $p = new Process(sprintf('php -r %s', escapeshellarg($code)));

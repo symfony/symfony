6f56dfc0d6b1bde8b2ab5e922c26743a03c7dbf1
William DURAND <william.durand1@gmail.com>
2012-04-11 18:56:33 +0200

    [Form] Fixed DateType default options

--- src/Symfony/Component/Form/Extension/Core/Type/DateType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/DateType.php
@@ -44,7 +44,7 @@ class DateType extends AbstractType
         // If $format is not in the allowed options, it's considered as the pattern of the formatter if it is a string
         if (!in_array($format, $allowedFormatOptionValues, true)) {
             if (is_string($format)) {
-                $defaultOptions = array_merge($this->getDefaultOptions(), $options);
+                $defaultOptions = $this->getDefaultOptions();
 
                 $format = $defaultOptions['format'];
                 $pattern = $options['format'];

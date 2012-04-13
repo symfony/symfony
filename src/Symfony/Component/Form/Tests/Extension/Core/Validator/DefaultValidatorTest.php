92874421c7d949bbcef5e7fb5287cb505adf3dad
l3l0 <leszek.prabucki@gmail.com>
2012-04-13 20:52:29 +0200

    Added tests for core DefaultValidator

---
+++ src/Symfony/Component/Form/Tests/Extension/Core/Validator/DefaultValidatorTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Form\Tests\Extension\Core\Validator;
+
+use Symfony\Component\Form\Extension\Core\Validator\DefaultValidator;
+
+class DefaultValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testThatAddErrorWhenNotSynchornized()
+    {
+        $form = $this->getFormMock();
+        $form->expects($this->once())
+            ->method('isSynchronized')
+            ->will($this->returnValue(false));
+        $form->expects($this->at(1))
+            ->method('getAttribute')
+            ->will($this->returnValue('message'));
+        $form->expects($this->at(2))
+            ->method('getAttribute')
+            ->will($this->returnValue(array()));
+        $form->expects($this->once())
+            ->method('addError');
+
+        $validator = new DefaultValidator();
+        $validator->validate($form);
+    }
+
+    public function testThatAddErrorWhenHaveExtraFields()
+    {
+        $form = $this->getFormMock();
+        $form->expects($this->once())
+            ->method('isSynchronized')
+            ->will($this->returnValue(true));
+        $form->expects($this->once())
+            ->method('getExtraData')
+            ->will($this->returnValue(array('extra_data_field' => 1)));
+        $form->expects($this->once())
+            ->method('addError');
+
+        $validator = new DefaultValidator();
+        $validator->validate($form);
+    }
+
+    public function testThatAddErrorWhenContentSizeIsTooLarge()
+    {
+        $form = $this->getFormMock();
+        $form->expects($this->once())
+            ->method('isSynchronized')
+            ->will($this->returnValue(true));
+        $form->expects($this->once())
+            ->method('isRoot')
+            ->will($this->returnValue(true));
+        $form->expects($this->once())
+            ->method('addError');
+
+        $_SERVER['CONTENT_LENGTH'] = 1073741825;
+
+        $validator = $this->getMockBuilder('Symfony\Component\Form\Extension\Core\Validator\DefaultValidator')
+            ->setMethods(array('getPostMaxSize'))
+            ->getMock();
+        $validator->expects($this->once())
+            ->method('getPostMaxSize')
+            ->will($this->returnValue('1G'));
+
+        $validator->validate($form);
+    }
+
+    private function getFormMock()
+    {
+        return $this->getMock('Symfony\Component\Form\Tests\FormInterface');
+    }
+}

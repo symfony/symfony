d3e85a0c090f4c0146203cbf1969675eb11c0c37
everzet <ever.zet@gmail.com>
2012-04-16 13:34:00 +0200

    [ResourceWatcher] refactored TrackerTest and added missing test cases

--- src/Symfony/Component/ResourceWatcher/Tests/Tracker/TrackerTest.php
+++ src/Symfony/Component/ResourceWatcher/Tests/Tracker/TrackerTest.php
@@ -11,7 +11,8 @@
 
 namespace Symfony\Component\ResourceWatcher\Tests\Tracker;
 
-use Symfony\Component\ResourceWatcher\Event\Event;
+use Symfony\Component\ResourceWatcher\Event\FilesystemEvent;
+use Symfony\Component\ResourceWatcher\Resource\TrackedResource;
 use Symfony\Component\ResourceWatcher\Tracker\TrackerInterface;
 use Symfony\Component\Config\Resource\DirectoryResource;
 use Symfony\Component\Config\Resource\FileResource;
@@ -28,6 +29,8 @@ abstract class TrackerTest extends \PHPUnit_Framework_TestCase
         }
 
         mkdir($this->tmpDir);
+
+        $this->tmpDir = realpath($this->tmpDir);
     }
 
     public function tearDown()
@@ -42,7 +45,7 @@ abstract class TrackerTest extends \PHPUnit_Framework_TestCase
     {
         $tracker = $this->getTracker();
 
-        $tracker->track(new FileResource(__DIR__.'/missingfile'));
+        $tracker->track(new TrackedResource('missing', new FileResource(__DIR__.'/missingfile')));
     }
 
     /**
@@ -52,30 +55,144 @@ abstract class TrackerTest extends \PHPUnit_Framework_TestCase
     {
         $tracker = $this->getTracker();
 
-        $tracker->track(new DirectoryResource(__DIR__.'/missingdir'));
+        $tracker->track(new TrackedResource('missing', new DirectoryResource(__DIR__.'/missingdir')));
     }
 
-    public function testTrackFileChanges()
+    public function testTrackSimpleFileChanges()
     {
         $tracker = $this->getTracker();
 
         touch($file = $this->tmpDir.'/foo');
 
-        $tracker->track($resource = new FileResource($file));
+        $tracker->track(new TrackedResource('foo', $resource = new FileResource($file)));
 
-        usleep($this->getMiminumInterval());
+        $this->sleep();
         touch($file);
 
         $events = $tracker->getEvents();
         $this->assertCount(1, $events);
-        $this->assertEquals(Event::MODIFIED, $events[0]->getType());
+        $this->assertHasResourceEvent($file, FilesystemEvent::IN_MODIFY, $events);
 
-        usleep($this->getMiminumInterval());
+        $this->sleep();
+
+        $events = $tracker->getEvents();
+        $this->assertCount(0, $events);
+
+        $this->sleep();
         unlink($file);
 
         $events = $tracker->getEvents();
         $this->assertCount(1, $events);
-        $this->assertEquals(Event::DELETED, $events[0]->getType());
+        $this->assertHasResourceEvent($file, FilesystemEvent::IN_DELETE, $events);
+
+        $this->sleep();
+
+        $events = $tracker->getEvents();
+        $this->assertCount(0, $events);
+    }
+
+    public function testTrackSimpleDirChanges()
+    {
+        $tracker = $this->getTracker();
+
+        mkdir($directory = $this->tmpDir.'/bar');
+
+        $tracker->track(new TrackedResource('bar', $resource = new DirectoryResource($directory)));
+
+        touch($file1 = $directory.'/new_file');
+        $this->sleep();
+
+        $events = $tracker->getEvents();
+        $this->assertCount(1, $events);
+        $this->assertHasResourceEvent($file1, FilesystemEvent::IN_CREATE, $events);
+
+        touch($file2 = $directory.'/new_file2');
+        touch($file3 = $directory.'/new_file3');
+        touch($file1);
+        $this->sleep();
+
+        $events = $tracker->getEvents();
+        $this->assertCount(3, $events);
+
+        $this->assertHasResourceEvent($file1, FilesystemEvent::IN_MODIFY, $events);
+        $this->assertHasResourceEvent($file2, FilesystemEvent::IN_CREATE, $events);
+        $this->assertHasResourceEvent($file3, FilesystemEvent::IN_CREATE, $events);
+
+        unlink($file1);
+        unlink($file3);
+        $this->sleep();
+
+        $events = $tracker->getEvents();
+        $this->assertCount(2, $events);
+
+        $this->assertHasResourceEvent($file1, FilesystemEvent::IN_DELETE, $events);
+        $this->assertHasResourceEvent($file3, FilesystemEvent::IN_DELETE, $events);
+    }
+
+    public function testTrackDeepDirChanges()
+    {
+        $tracker = $this->getTracker();
+
+        mkdir($directory1 = $this->tmpDir.'/bar2');
+
+        $tracker->track(
+            new TrackedResource('bar2', $resource = new DirectoryResource($directory1))
+        );
+
+        mkdir($directory2 = $directory1.'/subdir');
+        touch($file1 = $directory2.'/sub_file');
+        $this->sleep();
+
+        $events = $tracker->getEvents();
+        $this->assertCount(2, $events);
+
+        $this->assertHasResourceEvent($directory2, FilesystemEvent::IN_CREATE, $events);
+        $this->assertHasResourceEvent($file1, FilesystemEvent::IN_CREATE, $events);
+
+        $this->sleep();
+
+        $events = $tracker->getEvents();
+        $this->assertCount(0, $events);
+
+        touch($file2 = $directory1.'/dir1_file.txt');
+        touch($file3 = $directory2.'/dir2_file.txt');
+        touch($file1);
+        $this->sleep();
+
+        $events = $tracker->getEvents();
+        $this->assertCount(3, $events);
+
+        $this->assertHasResourceEvent($file1, FilesystemEvent::IN_MODIFY, $events);
+        $this->assertHasResourceEvent($file2, FilesystemEvent::IN_CREATE, $events);
+        $this->assertHasResourceEvent($file3, FilesystemEvent::IN_CREATE, $events);
+
+        $this->cleanDir($directory2);
+        touch($file2);
+        $this->sleep();
+
+        $events = $tracker->getEvents();
+        $this->assertCount(4, $events);
+
+        $this->assertHasResourceEvent($directory2, FilesystemEvent::IN_DELETE, $events);
+        $this->assertHasResourceEvent($file1, FilesystemEvent::IN_DELETE, $events);
+        $this->assertHasResourceEvent($file3, FilesystemEvent::IN_DELETE, $events);
+        $this->assertHasResourceEvent($file2, FilesystemEvent::IN_MODIFY, $events);
+    }
+
+    protected function assertHasResourceEvent($file, $type, array $events)
+    {
+        foreach ($events as $event) {
+            if ($file === (string) $event->getResource()) {
+                return $this->assertSame($type, $event->getType());
+            }
+        }
+
+        $this->fail(sprintf('Can not find "%s" change event', $file));
+    }
+
+    protected function sleep()
+    {
+        usleep($this->getMiminumInterval());
     }
 
     abstract protected function getMiminumInterval();
@@ -93,12 +210,15 @@ abstract class TrackerTest extends \PHPUnit_Framework_TestCase
 
         $flags = \FilesystemIterator::SKIP_DOTS;
         $iterator = new \RecursiveDirectoryIterator($dir, $flags);
-        $iterator = new \RecursiveIteratorIterator($iterator, \RecursiveIteratorIterator::SELF_FIRST);
+        $iterator = new \RecursiveIteratorIterator(
+            $iterator, \RecursiveIteratorIterator::CHILD_FIRST
+        );
 
-        foreach ($iterator as $file)
-        {
-            if (is_file($file)) {
-                unlink($file);
+        foreach ($iterator as $path) {
+            if ($path->isDir()) {
+                rmdir((string) $path);
+            } else {
+                unlink((string) $path);
             }
         }
 

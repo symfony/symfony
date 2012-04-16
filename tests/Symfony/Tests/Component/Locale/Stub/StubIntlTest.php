5799d2532468ac6d58bcf6ac3f9b2cc18556d6e6
stealth35 <jinmoku@hotmail.com>
2012-04-16 13:33:13 +0200

    Add BOGUS UErrorCode test

--- tests/Symfony/Tests/Component/Locale/Stub/StubIntlTest.php
+++ tests/Symfony/Tests/Component/Locale/Stub/StubIntlTest.php
@@ -22,9 +22,11 @@ class StubIntlTest extends LocaleTestCase
     public function codeProvider()
     {
         return array (
+            array(-129, '[BOGUS UErrorCode]'),
             array(0, 'U_ZERO_ERROR'),
             array(1, 'U_ILLEGAL_ARGUMENT_ERROR'),
             array(9, 'U_PARSE_ERROR'),
+            array(129, '[BOGUS UErrorCode]'),
         );
     }
 

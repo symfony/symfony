ef2d91b43d6f442b5a212f7c12f1d105933e02a8
Tobias Schultze <webmaster@tubo-world.de>
2012-04-16 10:25:33 +0200

    [Routing] add support for path-relative and scheme-relative URL generation

--- src/Symfony/Component/Routing/Generator/UrlGeneratorInterface.php
+++ src/Symfony/Component/Routing/Generator/UrlGeneratorInterface.php
@@ -18,20 +18,34 @@ use Symfony\Component\Routing\Exception\RouteNotFoundException;
  * UrlGeneratorInterface is the interface that all URL generator classes must implement.
  *
  * @author Fabien Potencier <fabien@symfony.com>
+ * @author Tobias Schultze <http://tobion.de>
  *
  * @api
  */
 interface UrlGeneratorInterface extends RequestContextAwareInterface
 {
     /**
+     * These constants define the different types of resource references that are declared
+     * in RFC 3986: http://tools.ietf.org/html/rfc3986
+     * We are using the term "URL" instead of "URI" as this is more common in web applications
+     * and we do not need to distinguish them as the difference is mostly semantical and
+     * less technical. Generating URIs, i.e. representation-independent resource identifiers,
+     * is still possible.
+     */
+    const ABSOLUTE_URL = 'url';
+    const ABSOLUTE_PATH = 'path';
+    const RELATIVE_PATH = 'relative';
+    const NETWORK_PATH = 'network';
+
+    /**
      * Generates a URL from the given parameters.
      *
      * If the generator is not able to generate the url, it must throw the RouteNotFoundException
      * as documented below.
      *
-     * @param string  $name       The name of the route
-     * @param mixed   $parameters An array of parameters
-     * @param Boolean $absolute   Whether to generate an absolute URL
+     * @param string $name          The name of the route
+     * @param mixed  $parameters    An array of parameters
+     * @param string $referenceType The type of reference to be generated (see defined constants)
      *
      * @return string The generated URL
      *
@@ -39,5 +53,5 @@ interface UrlGeneratorInterface extends RequestContextAwareInterface
      *
      * @api
      */
-    public function generate($name, $parameters = array(), $absolute = false);
+    public function generate($name, $parameters = array(), $referenceType = self::ABSOLUTE_PATH);
 }

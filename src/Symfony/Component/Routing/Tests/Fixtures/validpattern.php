153fcf2596725139a3204e9b10b0cd5d8b3c0fcd
Fabien Potencier <fabien.potencier@gmail.com>
2012-04-25 03:53:46 +0200

    [Routing] added some unit tests for the PHP loader

--- src/Symfony/Component/Routing/Tests/Fixtures/validpattern.php
+++ src/Symfony/Component/Routing/Tests/Fixtures/validpattern.php
@@ -6,8 +6,9 @@ $collection = new RouteCollection();
 $collection->add('blog_show', new Route(
     '/blog/{slug}',
     array('_controller' => 'MyBlogBundle:Blog:show'),
-    array(),
-    array('compiler_class' => 'RouteCompiler')
+    array('_method' => 'GET'),
+    array('compiler_class' => 'RouteCompiler'),
+    '{locale}.example.com'
 ));
 
 return $collection;

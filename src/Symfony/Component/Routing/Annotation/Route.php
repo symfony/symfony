d91e5a26a27cc6d3575075beb4f573177a715957
Fabien Potencier <fabien.potencier@gmail.com>
2012-04-25 03:14:38 +0200

    [Routing] fixed Route annotation for hostname (should be hostname_pattern instead of hostnamePattern)

--- src/Symfony/Component/Routing/Annotation/Route.php
+++ src/Symfony/Component/Routing/Annotation/Route.php
@@ -44,7 +44,7 @@ class Route
         }
 
         foreach ($data as $key => $value) {
-            $method = 'set'.$key;
+            $method = 'set'.str_replace('_', '', $key);
             if (!method_exists($this, $method)) {
                 throw new \BadMethodCallException(sprintf("Unknown property '%s' on annotation '%s'.", $key, get_class($this)));
             }

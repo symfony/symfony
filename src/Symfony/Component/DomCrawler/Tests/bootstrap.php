14f4248fe1132007be86b249609f1437950c3717
William DURAND <william.durand1@gmail.com>
2012-04-27 17:40:17 +0200

    [Components] Excluded vendor in phpunit.xml.dist files, fixed message in bootstrap.php files

--- src/Symfony/Component/DomCrawler/Tests/bootstrap.php
+++ src/Symfony/Component/DomCrawler/Tests/bootstrap.php
@@ -14,5 +14,5 @@ if (!$loader = include __DIR__.'/../vendor/autoload.php') {
     die('You must set up the project dependencies.'.PHP_EOL.
         'Run the following commands in '.dirname(__DIR__).':'.PHP_EOL.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
+        'php composer.phar install --dev'.PHP_EOL);
 }

b214041c8d03e5722286288f8a5bc054952eed79
William DURAND <william.durand1@gmail.com>
2012-04-27 15:21:31 +0200

    [Routing] Switched to composer to run the tests

--- src/Symfony/Component/Routing/Tests/bootstrap.php
+++ src/Symfony/Component/Routing/Tests/bootstrap.php
@@ -9,27 +9,10 @@
  * file that was distributed with this source code.
  */
 
-spl_autoload_register(function ($class) {
-    foreach (array(
-        'SYMFONY_CONFIG' => 'Config',
-        'SYMFONY_YAML'   => 'Yaml',
-    ) as $env => $name) {
-        if (isset($_SERVER[$env]) && 0 === strpos(ltrim($class, '/'), 'Symfony\Component\\'.$name)) {
-            if (file_exists($file = $_SERVER[$env].'/'.substr(str_replace('\\', '/', $class), strlen('Symfony\Component\\'.$name)).'.php')) {
-                require_once $file;
-            }
-        }
-    }
-
-    if (isset($_SERVER['DOCTRINE_COMMON']) && 0 === strpos(ltrim($class, '/'), 'Doctrine\Common')) {
-        if (file_exists($file = $_SERVER['DOCTRINE_COMMON'].'/lib/'.str_replace('\\', '/', $class).'.php')) {
-            require_once $file;
-        }
-    }
-
-    if (0 === strpos(ltrim($class, '/'), 'Symfony\Component\Routing')) {
-        if (file_exists($file = __DIR__.'/../'.substr(str_replace('\\', '/', $class), strlen('Symfony\Component\Routing')).'.php')) {
-            require_once $file;
-        }
-    }
-});
+if (!$loader = include __DIR__.'/../vendor/autoload.php') {
+    echo PHP_EOL.PHP_EOL;
+    die('You must set up the project dependencies.'.PHP_EOL.
+        'Run the following commands in '.dirname(__DIR__).':'.PHP_EOL.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}

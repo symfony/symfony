5ff4b69954b0242e91fef6f1d7b0567285e2b3aa
William DURAND <william.durand1@gmail.com>
2012-04-27 16:19:02 +0200

    [Validator] Switched to composer to run the tests

--- src/Symfony/Component/Validator/Tests/bootstrap.php
+++ src/Symfony/Component/Validator/Tests/bootstrap.php
@@ -9,36 +9,20 @@
  * file that was distributed with this source code.
  */
 
-spl_autoload_register($loader = function ($class) {
-    foreach (array(
-        'SYMFONY_YAML'            => 'Yaml',
-        'SYMFONY_LOCALE'          => 'Locale',
-        'SYMFONY_HTTP_FOUNDATION' => 'HttpFoundation',
-    ) as $env => $name) {
-        if (isset($_SERVER[$env]) && 0 === strpos(ltrim($class, '/'), 'Symfony\Component\\'.$name)) {
-            if (file_exists($file = $_SERVER[$env].'/'.substr(str_replace('\\', '/', $class), strlen('Symfony\Component\\'.$name)).'.php')) {
-                require_once $file;
-            }
-        }
-    }
+if (!$loader = include __DIR__.'/../vendor/autoload.php') {
+    echo PHP_EOL.PHP_EOL;
+    die('You must set up the project dependencies.'.PHP_EOL.
+        'Run the following commands in '.dirname(__DIR__).':'.PHP_EOL.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}
 
-    if (isset($_SERVER['DOCTRINE_COMMON']) && 0 === strpos(ltrim($class, '/'), 'Doctrine\Common')) {
-        if (file_exists($file = $_SERVER['DOCTRINE_COMMON'].'/lib/'.str_replace('\\', '/', $class).'.php')) {
-            require_once $file;
-        }
-    }
-
+Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(function($class) {
     if (0 === strpos(ltrim($class, '/'), 'Symfony\Component\Validator')) {
         if (file_exists($file = __DIR__.'/../'.substr(str_replace('\\', '/', $class), strlen('Symfony\Component\Validator')).'.php')) {
             require_once $file;
         }
     }
+
+    return class_exists($class, false);
 });
-
-if (isset($_SERVER['DOCTRINE_COMMON'])) {
-    Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(function($class) use ($loader) {
-        $loader($class);
-
-        return class_exists($class, false);
-    });
-}

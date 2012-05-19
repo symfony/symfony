3eb67fca7a2bdb6d180402a8140b3ad3a6978a32
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2012-05-19 10:42:27 +0200

    [2.1][Component][Finder] $this->current() fix

--- src/Symfony/Component/Finder/SplFileInfo.php
+++ src/Symfony/Component/Finder/SplFileInfo.php
@@ -54,4 +54,22 @@ class SplFileInfo extends \SplFileInfo
     {
         return $this->relativePathname;
     }
+
+    /**
+     * Returns the contents of the file
+     *
+     * @return string the contents of the file
+     */
+    public function getContents()
+    {
+        $level = error_reporting(0);
+        $content = file_get_contents($this->getRealpath());
+        error_reporting($level);
+        if (false === $content) {
+            $error = error_get_last();
+            throw new \RuntimeException($error['message']);
+        }
+
+        return $content;
+    }
 }

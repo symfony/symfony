452d89096c223860ad5cdadfe9117c72c1a5d377
Fabien Potencier <fabien.potencier@gmail.com>
2012-05-22 12:23:06 +0200

    added the possibility to translate the placeholder and title attributes in the PHP form templates (refs #4194)

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/widget_attributes.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/widget_attributes.html.php
@@ -5,5 +5,6 @@ name="<?php echo $view->escape($full_name) ?>"
 <?php if ($required): ?>required="required" <?php endif ?>
 <?php if ($max_length): ?>maxlength="<?php echo $view->escape($max_length) ?>" <?php endif ?>
 <?php if ($pattern): ?>pattern="<?php echo $view->escape($pattern) ?>" <?php endif ?>
-<?php foreach ($attr as $k => $v) { printf('%s="%s" ', $view->escape($k), $view->escape($v)); } ?>
-
+<?php foreach ($attr as $k => $v): ?>
+    <?php printf('%s="%s" ', $view->escape($k), $view->escape(in_array($v, array('placeholder', 'title')) ? $view['translator']->trans($v, array(), $translation_domain) : $v)) ?>
+<?php endforeach; ?>

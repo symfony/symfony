c53a66800ce80e21600237d390f1f70cd7ffd9cf
Bernhard Schussek <bschussek@gmail.com>
2012-06-01 07:58:58 +0200

    [Form] Removed invalid tests introduced again upon merge

--- src/Symfony/Component/Form/Tests/Extension/Core/Type/DateTimeTypeTest.php
+++ src/Symfony/Component/Form/Tests/Extension/Core/Type/DateTimeTypeTest.php
@@ -243,22 +243,4 @@ class DateTimeTypeTest extends LocalizedTestCase
         $view = $form->createView();
         $this->assertEquals('datetime', $view->getVar('type'));
     }
-
-    public function testSubmit_invalidDateTimeSingleText()
-    {
-        $form = $this->factory->create('datetime', null, array(
-            'data_timezone' => 'UTC',
-            'user_timezone' => 'UTC',
-            'input' => 'datetime',
-            'widget' => 'single_text',
-            'invalid_message' => 'Customized invalid message',
-        ));
-
-        $form->bind('2012-04-31 03:04:05');
-
-        $this->assertFalse($form->isValid());
-        $this->assertNull($form->getData());
-        $this->assertEquals('2012-04-31 03:04:05', $form->getClientData());
-        $this->assertEquals(array(new FormError('Customized invalid message', array())), $form->getErrors());
-    }
 }

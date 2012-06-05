24a0adaeb8d188202d6be8f94c597e46a578404d
jeanfrancois.simon <jeanfrancois.simon@sensio.com>
2012-06-05 10:37:51 +0200

    [http kernel] Added client response type test.

--- src/Symfony/Component/HttpKernel/Tests/ClientTest.php
+++ src/Symfony/Component/HttpKernel/Tests/ClientTest.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Component\HttpKernel\Tests;
 
+use Symfony\Component\BrowserKit\Response as DomResponse;
 use Symfony\Component\HttpKernel\Client;
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,6 +35,7 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 
         $client->request('GET', '/');
         $this->assertEquals('Request: /', $client->getResponse()->getContent(), '->doRequest() uses the request handler to make the request');
+        $this->assertTrue($client->getResponse() instanceof DomResponse, '->getResponse() returns a Symfony\Component\BrowserKit\Response instance');
 
         $client->request('GET', 'http://www.example.com/');
         $this->assertEquals('Request: /', $client->getResponse()->getContent(), '->doRequest() uses the request handler to make the request');

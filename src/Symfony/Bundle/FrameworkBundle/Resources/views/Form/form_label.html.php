b1827be0f415485de23af3f9eb5ffd05bc58cca6
Dinduks <samy@dindane.com>
2012-06-09 17:46:21 +0200

    Add escape_label option for FormType

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_label.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_label.html.php
@@ -1,3 +1,3 @@
 <?php if ($required) { $label_attr['class'] = trim((isset($label_attr['class']) ? $label_attr['class'] : '').' required'); } ?>
 <?php if (!$compound) { $label_attr['for'] = $id; } ?>
-<label <?php foreach ($label_attr as $k => $v) { printf('%s="%s" ', $view->escape($k), $view->escape($v)); } ?>><?php echo $view->escape($view['translator']->trans($label, array(), $translation_domain)) ?></label>
+<label <?php foreach ($label_attr as $k => $v) { printf('%s="%s" ', $view->escape($k), $view->escape($v)); } ?>><?php if ($escape_label) ?><?php echo $view->escape($view['translator']->trans($label, array(), $translation_domain)) ?><?php else ?><?php echo $view['translator']->trans($label, array(), $translation_domain) ?><?php endif ?></label>

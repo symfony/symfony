741927baf36b4ed314bdf044545e9363e26a1f0f
Fabien Potencier <fabien.potencier@gmail.com>
2012-06-30 09:42:40 +0200

    [FrameworkBundle] removed unneeded start call

--- src/Symfony/Bundle/FrameworkBundle/Tests/Templating/Helper/SessionHelperTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Templating/Helper/SessionHelperTest.php
@@ -25,7 +25,6 @@ class SessionHelperTest extends \PHPUnit_Framework_TestCase
         $this->request = new Request();
 
         $session = new Session(new MockArraySessionStorage());
-        $session->start();
         $session->set('foobar', 'bar');
         $session->getFlashBag()->set('notice', 'bar');
 

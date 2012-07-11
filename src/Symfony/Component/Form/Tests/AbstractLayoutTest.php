f37f444d234ce16f759e007412e2f67244a0efa3
Bernhard Schussek <bschussek@gmail.com>
2012-07-11 09:44:09 +0200

    [Form] Renamed the options "data_timezone" and "user_timezone"

--- src/Symfony/Component/Form/Tests/AbstractLayoutTest.php
+++ src/Symfony/Component/Form/Tests/AbstractLayoutTest.php
@@ -964,8 +964,8 @@ abstract class AbstractLayoutTest extends \PHPUnit_Framework_TestCase
         $form = $this->factory->createNamed('name', 'datetime', '2011-02-03 04:05:06', array(
             'input' => 'string',
             'widget' => 'single_text',
-            'data_timezone' => 'UTC',
-            'user_timezone' => 'UTC',
+            'model_timezone' => 'UTC',
+            'view_timezone' => 'UTC',
         ));
 
         $this->assertWidgetMatchesXpath($form->createView(), array(),
@@ -984,8 +984,8 @@ abstract class AbstractLayoutTest extends \PHPUnit_Framework_TestCase
             'date_widget' => 'choice',
             'time_widget' => 'choice',
             'widget' => 'single_text',
-            'data_timezone' => 'UTC',
-            'user_timezone' => 'UTC',
+            'model_timezone' => 'UTC',
+            'view_timezone' => 'UTC',
         ));
 
         $this->assertWidgetMatchesXpath($form->createView(), array(),

5dc3c39fd2d84b788eb4fff54a668f230ef38cce
Bernhard Schussek <bschussek@gmail.com>
2012-07-16 18:03:21 +0200

    [Form] Moved the access to templating helpers out of the choice loop for performance reasons (PHP +100ms)

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/choice_widget_options.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/choice_widget_options.html.php
@@ -1,9 +1,11 @@
+<?php $translatorHelper = $view['translator']; // outside of the loop for performance reasons! ?>
+<?php $formHelper = $view['form']; ?>
 <?php foreach ($choices as $index => $choice): ?>
     <?php if (is_array($choice)): ?>
-        <optgroup label="<?php echo $view->escape($view['translator']->trans($index, array(), $translation_domain)) ?>">
-            <?php echo $view['form']->block('choice_widget_options', array('choices' => $choice)) ?>
+        <optgroup label="<?php echo $view->escape($translatorHelper->trans($index, array(), $translation_domain)) ?>">
+            <?php echo $formHelper->block('choice_widget_options', array('choices' => $choice)) ?>
         </optgroup>
     <?php else: ?>
-        <option value="<?php echo $view->escape($choice->value) ?>"<?php if ($choice->isSelected($value)): ?> selected="selected"<?php endif?>><?php echo $view->escape($view['translator']->trans($choice->label, array(), $translation_domain)) ?></option>
+        <option value="<?php echo $view->escape($choice->value) ?>"<?php if ($choice->isSelected($value)): ?> selected="selected"<?php endif?>><?php echo $view->escape($translatorHelper->trans($choice->label, array(), $translation_domain)) ?></option>
     <?php endif ?>
 <?php endforeach ?>

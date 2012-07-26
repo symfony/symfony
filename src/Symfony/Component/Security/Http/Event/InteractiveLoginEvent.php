9030dc5dfc22845e27dd7247f5456065daea974e
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2012-07-26 16:07:46 +1000

    [Security] add docblocks to InteractiveLoginEvent
    Close #5053 (was easier to just create a new PR).

--- src/Symfony/Component/Security/Http/Event/InteractiveLoginEvent.php
+++ src/Symfony/Component/Security/Http/Event/InteractiveLoginEvent.php
@@ -17,21 +17,43 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class InteractiveLoginEvent extends Event
 {
+    /**
+     * @var Request
+     */
     private $request;
 
+    /**
+     * @var TokenInterface
+     */
     private $authenticationToken;
 
+    /**
+     * Constructor.
+     *
+     * @param Request        $request
+     * @param TokenInterface $authenticationToken
+     */
     public function __construct(Request $request, TokenInterface $authenticationToken)
     {
         $this->request = $request;
         $this->authenticationToken = $authenticationToken;
     }
 
+    /**
+     * Gets the request.
+     *
+     * @return Request
+     */
     public function getRequest()
     {
         return $this->request;
     }
 
+    /**
+     * Gets the authentication token.
+     *
+     * @return TokenInterface
+     */
     public function getAuthenticationToken()
     {
         return $this->authenticationToken;

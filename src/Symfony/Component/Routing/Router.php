37ac86f9b604072af4e7b025739d11648de30b63
Victor Berchet <victor@suumit.com>
2012-07-30 18:24:12 +0200

    [Routing] Add an interface for configuring strict_parameters

--- src/Symfony/Component/Routing/Router.php
+++ src/Symfony/Component/Routing/Router.php
@@ -14,6 +14,7 @@ namespace Symfony\Component\Routing;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\ConfigCache;
 use Symfony\Component\HttpKernel\Log\LoggerInterface;
+use Symfony\Component\Routing\Generator\ConfigurableRequirementsInterface;
 
 /**
  * The Router class is an example of the integration of all pieces of the
@@ -77,7 +78,7 @@ class Router implements RouterInterface
             'matcher_dumper_class'   => 'Symfony\\Component\\Routing\\Matcher\\Dumper\\PhpMatcherDumper',
             'matcher_cache_class'    => 'ProjectUrlMatcher',
             'resource_type'          => null,
-            'strict_parameters'      => true,
+            'strict_requirements'    => true,
         );
 
         // check option names and live merge, if errors are encountered Exception will be thrown
@@ -244,8 +245,8 @@ class Router implements RouterInterface
             $this->generator = new $class($this->context, $this->logger);
         }
 
-        if (false === $this->options['strict_parameters']) {
-            $this->generator->setStrictParameters(false);
+        if ($this->generator instanceof ConfigurableRequirementsInterface) {
+            $this->generator->setStrictRequirements($this->options['strict_requirements']);
         }
 
         return $this->generator;

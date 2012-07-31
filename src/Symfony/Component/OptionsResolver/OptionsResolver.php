a620d5f3bf1f87b792eb266a08e30df86c50704f
Evan Kaufman <ekaufman@collegedegrees.com>
2012-07-31 17:59:01 -0500

    OptionsResolver#validateOptionTypes should check if option value exists before checking its type; added corresponding test
    OptionsResolver#validateOptionsCompleteness would already have thrown exception if the option were required, so this should only affect something explicitly marked as optional

--- src/Symfony/Component/OptionsResolver/OptionsResolver.php
+++ src/Symfony/Component/OptionsResolver/OptionsResolver.php
@@ -312,6 +312,10 @@ class OptionsResolver implements OptionsResolverInterface
     private function validateOptionTypes(array $options)
     {
         foreach ($this->allowedTypes as $option => $allowedTypes) {
+            if (!isset($options[$option])) {
+                continue;
+            }
+
             $value = $options[$option];
             $allowedTypes = (array) $allowedTypes;
 

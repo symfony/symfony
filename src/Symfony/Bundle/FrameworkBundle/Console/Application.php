298b2a88eca090204fcc262c353817c1d7a6390d
smokeybear87 <alex@de-facto.com>
2012-08-03 11:50:44 +0200

    Fixes incorrect class used in src/Symfony/Bundle/FrameworkBundle/Console/Application.php
    Issue must be related to commit 7a5f6142405c599cf0eb54e80c98e54e41ab4fdf (merged 2.0), specifically this file src/Symfony/Bundle/FrameworkBundle/Console/Application.php, lines 86-88. 
    
    Presumably to do "instanceof Bundle" correct class has to be imported at the top of the file:
    
    instead of 
    use Symfony\Component\HttpKernel\Bundle;
    
    this should be
    use Symfony\Component\HttpKernel\Bundle\Bundle;
    Conflicts:
    
    	src/Symfony/Bundle/FrameworkBundle/Console/Application.php

--- src/Symfony/Bundle/FrameworkBundle/Console/Application.php
+++ src/Symfony/Bundle/FrameworkBundle/Console/Application.php
@@ -17,6 +17,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Application.

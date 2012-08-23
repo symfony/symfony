cc4f5945a83db37c15cd41119352378391a8a1d0
Michal Piotrowski <michal@eventhorizon.pl>
2012-08-23 23:07:32 +0200

    dump test
    fix things pointed out by stof
    
    fix things pointed out by pborreli
    
    fix things pointed out by fabpot

--- src/Symfony/Component/ClassLoader/Tests/ClassMapGeneratorTest.php
+++ src/Symfony/Component/ClassLoader/Tests/ClassMapGeneratorTest.php
@@ -15,6 +15,50 @@ use Symfony\Component\ClassLoader\ClassMapGenerator;
 
 class ClassMapGeneratorTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var string $workspace
+     */
+    private $workspace = null;
+
+    public function prepare_workspace()
+    {
+        $this->workspace = rtrim(sys_get_temp_dir(), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.time().rand(0, 1000);
+        mkdir($this->workspace, 0777, true);
+        $this->workspace = realpath($this->workspace);
+    }
+
+    /**
+     * @param string $file
+     */
+    private function clean($file)
+    {
+        if (is_dir($file) && !is_link($file)) {
+            $dir = new \FilesystemIterator($file);
+            foreach ($dir as $childFile) {
+                $this->clean($childFile);
+            }
+
+            rmdir($file);
+        } else {
+            unlink($file);
+        }
+    }
+
+    /**
+     * @dataProvider getTestCreateMapTests
+     */
+    public function testDump($directory, $expected)
+    {
+        $this->prepare_workspace();
+
+        $file = $this->workspace.'/file';
+
+        $generator = new ClassMapGenerator();
+        $generator->dump($directory, $file);
+        $this->assertFileExists($file);
+
+        $this->clean($this->workspace);
+    }
 
     /**
      * @dataProvider getTestCreateMapTests

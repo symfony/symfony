4868bec46c9d43f6d3ea6c467760b867bf47f54f
Tobias Schultze <webmaster@tubo-world.de>
2012-09-03 23:10:00 +0200

    [Routing] fix Compiler when previous char of placeholder is no real separator (closes #5423)

--- src/Symfony/Component/Routing/Tests/Matcher/UrlMatcherTest.php
+++ src/Symfony/Component/Routing/Tests/Matcher/UrlMatcherTest.php
@@ -252,6 +252,30 @@ class UrlMatcherTest extends \PHPUnit_Framework_TestCase
         $matcher->match('/wxy.html');
     }
 
+    public function testOptionalVariableWithNoRealSeparator()
+    {
+        $coll = new RouteCollection();
+        $coll->add('test', new Route('/get{what}', array('what' => 'All')));
+        $matcher = new UrlMatcher($coll, new RequestContext());
+
+        $this->assertEquals(array('what' => 'All', '_route' => 'test'), $matcher->match('/get'));
+        $this->assertEquals(array('what' => 'Sites', '_route' => 'test'), $matcher->match('/getSites'));
+
+        // Usually the character in front of an optional parameter can be left out, e.g. with pattern '/get/{what}' just '/get' would match.
+        // But here the 't' in 'get' is not a separating character, so it makes no sense to match without it.
+        $this->setExpectedException('Symfony\Component\Routing\Exception\ResourceNotFoundException');
+        $matcher->match('/ge');
+    }
+
+    public function testRequiredVariableWithNoRealSeparator()
+    {
+        $coll = new RouteCollection();
+        $coll->add('test', new Route('/get{what}Suffix'));
+        $matcher = new UrlMatcher($coll, new RequestContext());
+
+        $this->assertEquals(array('what' => 'Sites', '_route' => 'test'), $matcher->match('/getSitesSuffix'));
+    }
+
     /**
      * @expectedException Symfony\Component\Routing\Exception\ResourceNotFoundException
      */

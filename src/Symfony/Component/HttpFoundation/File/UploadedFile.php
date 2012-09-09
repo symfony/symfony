bdb9f2f5d8b5705d582951a13516826a9ff12326
Al Ganiev <helios.ag@gmail.com>
2012-09-09 23:24:25 +1100

    [HttpFoundation] Fix name sanitization after perfoming move

--- src/Symfony/Component/HttpFoundation/File/UploadedFile.php
+++ src/Symfony/Component/HttpFoundation/File/UploadedFile.php
@@ -94,9 +94,7 @@ class UploadedFile extends File
             throw new FileException(sprintf('Unable to create UploadedFile because "file_uploads" is disabled in your php.ini file (%s)', get_cfg_var('cfg_file_path')));
         }
 
-        $originalName = str_replace('\\', '/', $originalName);
-        $pos = strrpos($originalName, '/');
-        $this->originalName = false === $pos ? $originalName : substr($originalName, $pos + 1);
+        $this->originalName = $this->getName($originalName);
         $this->mimeType = $mimeType ?: 'application/octet-stream';
         $this->size = $size;
         $this->error = $error ?: UPLOAD_ERR_OK;
@@ -168,7 +166,7 @@ class UploadedFile extends File
     /**
      * Returns whether the file was uploaded successfully.
      *
-     * @return Boolean  True if no error occurred during uploading
+     * @return Boolean True if no error occurred during uploading
      *
      * @api
      */

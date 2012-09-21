2ceebdcaba4a5932a8cc5a5d044cef8152c1f267
Fabien Potencier <fabien.potencier@gmail.com>
2012-09-21 08:32:55 +0200

    fixed stringification of array objects in RequestDataCollector (closes #5295)

--- src/Symfony/Bridge/Doctrine/DataCollector/DoctrineDataCollector.php
+++ src/Symfony/Bridge/Doctrine/DataCollector/DoctrineDataCollector.php
@@ -95,38 +95,4 @@ class DoctrineDataCollector extends DataCollector
 
         return $queries;
     }
-
-    private function varToString($var)
-    {
-        if (is_object($var)) {
-            return sprintf('Object(%s)', get_class($var));
-        }
-
-        if (is_array($var)) {
-            $a = array();
-            foreach ($var as $k => $v) {
-                $a[] = sprintf('%s => %s', $k, $this->varToString($v));
-            }
-
-            return sprintf("Array(%s)", implode(', ', $a));
-        }
-
-        if (is_resource($var)) {
-            return sprintf('Resource(%s)', get_resource_type($var));
-        }
-
-        if (null === $var) {
-            return 'null';
-        }
-
-        if (false === $var) {
-            return 'false';
-        }
-
-        if (true === $var) {
-            return 'true';
-        }
-
-        return (string) $var;
-    }
 }

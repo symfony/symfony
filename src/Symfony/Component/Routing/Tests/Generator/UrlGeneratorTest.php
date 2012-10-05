90145d2b71c103a8699a3762c94fdabbad1566af
Tobias Schultze <webmaster@tubo-world.de>
2012-10-06 01:20:12 +0200

    [Routing] fix handling of two starting slashes in the pattern
    It would be confused with a network path e.g. '//domain/path' when generating a path so should be prevented.

--- src/Symfony/Component/Routing/Tests/Generator/UrlGeneratorTest.php
+++ src/Symfony/Component/Routing/Tests/Generator/UrlGeneratorTest.php
@@ -214,6 +214,14 @@ class UrlGeneratorTest extends \PHPUnit_Framework_TestCase
         $routes = $this->getRoutes('test', new Route('/', array(), array('_scheme' => 'http')));
         $this->assertEquals('http://localhost/app.php/', $this->getGenerator($routes, array('scheme' => 'https'))->generate('test'));
     }
+    
+    public function testPathWithTwoStartingSlashes()
+    {
+        $routes = $this->getRoutes('test', new Route('//path-and-not-domain'));
+
+        // this must not generate '//path-and-not-domain' because that would be a network path
+        $this->assertSame('/path-and-not-domain', $this->getGenerator($routes, array('BaseUrl' => ''))->generate('test'));
+    }
 
     public function testNoTrailingSlashForMultipleOptionalParameters()
     {

c7d2f0c4e22a155ed5fd990c52b2395eeda98ac3
Albert Casademont <albertcasademont@gmail.com>
2012-10-17 10:50:16 +0200

    Add the Request locale to the RequestDataCollector

--- src/Symfony/Component/HttpKernel/DataCollector/RequestDataCollector.php
+++ src/Symfony/Component/HttpKernel/DataCollector/RequestDataCollector.php
@@ -93,6 +93,7 @@ class RequestDataCollector extends DataCollector implements EventSubscriberInter
             'flashes'            => $flashes,
             'path_info'          => $request->getPathInfo(),
             'controller'         => 'n/a',
+            'locale'             => $request->getLocale(),
         );
 
         if (isset($this->controllers[$request])) {
@@ -187,6 +188,11 @@ class RequestDataCollector extends DataCollector implements EventSubscriberInter
     public function getFormat()
     {
         return $this->data['format'];
+    }
+
+    public function getLocale()
+    {
+        return $this->data['locale'];
     }
 
     /**

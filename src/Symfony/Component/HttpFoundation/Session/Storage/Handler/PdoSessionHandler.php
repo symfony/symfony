8937b11c343125656ac7fb226a9f8a3f33617f49
Fabien Potencier <fabien.potencier@gmail.com>
2012-10-21 18:11:21 +0200

    merged branch Seldaek/read-raw (PR #5799)
    This PR was merged into the master branch.
    
    Commits
    -------
    
    94f3ba5 [Console] Read input in raw mode to avoid processing escape sequences
    
    Discussion
    ----------
    
    [Console] Read input in raw mode to avoid processing escape sequences
    
    As outlined in composer/composer#1238:
    
    > Use read -r rather than the current read shell builtin to take a password from the user on the CLI, the -r is the raw option which means backslashes (\) aren't interpreted as escape characters, so \ is a literal \, otherwise they are nommed or double slashes become single ones etc.

--- src/Symfony/Component/HttpFoundation/Session/Storage/Handler/PdoSessionHandler.php
+++ src/Symfony/Component/HttpFoundation/Session/Storage/Handler/PdoSessionHandler.php
@@ -22,12 +22,12 @@ class PdoSessionHandler implements \SessionHandlerInterface
     /**
      * @var \PDO PDO instance.
      */
-    private $pdo;
+    protected $pdo;
 
     /**
      * @var array Database options.
      */
-    private $dbOptions;
+    protected $dbOptions;
 
     /**
      * Constructor.
@@ -168,7 +168,9 @@ class PdoSessionHandler implements \SessionHandlerInterface
         $encoded = base64_encode($data);
 
         try {
-            if ('mysql' === $this->pdo->getAttribute(\PDO::ATTR_DRIVER_NAME)) {
+            $driver = $this->pdo->getAttribute(\PDO::ATTR_DRIVER_NAME);
+
+            if ('mysql' === $driver) {
                 // MySQL would report $stmt->rowCount() = 0 on UPDATE when the data is left unchanged
                 // it could result in calling createNewSession() whereas the session already exists in
                 // the DB which would fail as the id is unique
@@ -180,7 +182,19 @@ class PdoSessionHandler implements \SessionHandlerInterface
                 $stmt->bindParam(':data', $encoded, \PDO::PARAM_STR);
                 $stmt->bindValue(':time', time(), \PDO::PARAM_INT);
                 $stmt->execute();
-            } else {
+            }
+            else if('oci' === $driver) {
+               
+                $sql = "MERGE INTO $dbTable USING DUAL ON($dbIdCol = :id) ". 
+                       "WHEN NOT MATCHED THEN INSERT ($dbIdCol, $dbDataCol, $dbTimeCol) VALUES (:id, :data, sysdate) " .
+                       "WHEN MATCHED THEN UPDATE SET $dbDataCol = :data WHERE $dbIdCol = :id"
+                
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->bindParam(':id', $id, \PDO::PARAM_STR);
+                $stmt->bindParam(':data', $encoded, \PDO::PARAM_STR);
+                $stmt->execute();
+            }
+            else {
                 $stmt = $this->pdo->prepare("UPDATE $dbTable SET $dbDataCol = :data, $dbTimeCol = :time WHERE $dbIdCol = :id");
                 $stmt->bindParam(':id', $id, \PDO::PARAM_STR);
                 $stmt->bindParam(':data', $encoded, \PDO::PARAM_STR);
@@ -208,7 +222,7 @@ class PdoSessionHandler implements \SessionHandlerInterface
      *
      * @return boolean True.
      */
-    private function createNewSession($id, $data = '')
+    protected function createNewSession($id, $data = '')
     {
         // get table/column
         $dbTable   = $this->dbOptions['db_table'];

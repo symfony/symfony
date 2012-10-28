4b99ca85a6be09edb16ae8ff999719e840918aa3
Fabien Potencier <fabien.potencier@gmail.com>
2012-10-28 07:37:44 +0100

    fixed tests when OpenSsl is not enabled in PHP, renamed a missnamed test, added missing license doc blocks

--- src/Symfony/Component/Security/Tests/Core/Util/StringUtilsTest.php
+++ src/Symfony/Component/Security/Tests/Core/Util/StringUtilsTest.php
@@ -1,10 +1,19 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Component\Security\Tests\Core\Util;
 
 use Symfony\Component\Security\Core\Util\StringUtils;
 
-class StringTest extends \PHPUnit_Framework_TestCase
+class StringUtilsTest extends \PHPUnit_Framework_TestCase
 {
     public function testEquals()
     {

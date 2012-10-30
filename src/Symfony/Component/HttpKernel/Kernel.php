c003d60bc365ec2e9adc7a83c1d408f0f9db8f85
Fabien Potencier <fabien.potencier@gmail.com>
2012-10-30 10:44:33 +0100

    bumped Symfony version to 2.1.4-DEV

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -58,12 +58,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $classes;
     protected $errorReportingLevel;
 
-    const VERSION         = '2.1.3';
+    const VERSION         = '2.1.4-DEV';
     const VERSION_ID      = '20100';
     const MAJOR_VERSION   = '2';
     const MINOR_VERSION   = '1';
-    const RELEASE_VERSION = '3';
-    const EXTRA_VERSION   = '';
+    const RELEASE_VERSION = '4';
+    const EXTRA_VERSION   = 'DEV';
 
     /**
      * Constructor.

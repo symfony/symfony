07bd5c630626cc4fe439fc68c2acbf8251ce1710
Roman Marintsenko <inoryy@gmail.com>
2012-10-31 12:04:56 +0200

    Make non-instantiable utils classes consistent with each other

--- src/Symfony/Component/Security/Core/Util/ClassUtils.php
+++ src/Symfony/Component/Security/Core/Util/ClassUtils.php
@@ -37,6 +37,11 @@ class ClassUtils
     const MARKER_LENGTH = 6;
 
     /**
+     * This class should not be instantiated
+     */
+    private function __construct() {}
+
+    /**
      * Gets the real class name of a class name that could be a proxy.
      *
      * @param string|object

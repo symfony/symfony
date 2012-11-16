768abaf0c79a9b92a39667ba83e44c63d7101363
Markus Lanthaler <mark_lanthaler@gmx.net>
2012-11-16 17:06:12 +0100

    Improve docBlock

--- src/Symfony/Component/Serializer/Serializer.php
+++ src/Symfony/Component/Serializer/Serializer.php
@@ -220,6 +220,7 @@ class Serializer implements SerializerInterface, NormalizerInterface, Denormaliz
      *
      * @param object $object object to normalize
      * @param string $format format name, present to give the option to normalizers to act differently based on formats
+     *
      * @return array|scalar
      */
     private function normalizeObject($object, $format = null)
@@ -250,6 +251,7 @@ class Serializer implements SerializerInterface, NormalizerInterface, Denormaliz
      * @param mixed  $data   data to restore
      * @param string $class  the expected class to instantiate
      * @param string $format format name, present to give the option to normalizers to act differently based on formats
+     *
      * @return object
      */
     private function denormalizeObject($data, $class, $format = null)

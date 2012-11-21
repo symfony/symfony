298ff6a6ca6ce6999dd34ca4f8fe2109e17fe5ef
Sebastian Krebs <krebs.seb@gmail.com>
2012-11-21 21:00:39 +0100

    [FrameworkBundle] Added caching to TemplateController

--- src/Symfony/Bundle/FrameworkBundle/Controller/TemplateController.php
+++ src/Symfony/Bundle/FrameworkBundle/Controller/TemplateController.php
@@ -22,14 +22,40 @@ use Symfony\Component\HttpFoundation\Response;
 class TemplateController extends ContainerAware
 {
     /**
+     * Default max age time in seconds for client (browser) caching
+     *
+     * @var int
+     */
+    const MAX_AGE = 86400;
+
+    /**
+     * Default max age time in seconds for shared (proxy) caching
+     *
+     * @var int
+     */
+    const SHARED_AGE = 86400;
+
+    /**
      * Renders a template.
      *
      * @param string $template The template name
+     * @param int|null $maxAge Max age for client caching
+     * @param int|null $sharedAge Max age for shared (proxy) caching
+     * @param bool|null $private Whether or not caching should apply for client caches only
      *
      * @return Response A Response instance
      */
-    public function templateAction($template)
+    public function templateAction($template, $maxAge = null, $sharedAge = null, $private = null)
     {
-        return $this->container->get('templating')->renderResponse($template);
+        /** @var $response \Symfony\Component\HttpFoundation\Response */
+        $response = $this->container->get('templating')->renderResponse($template);
+        $response->setMaxAge($maxAge ? : self::MAX_AGE);
+        $response->setSharedMaxAge($sharedAge ? : self::SHARED_AGE);
+        if ($private) {
+            $response->setPrivate();
+        } else {
+            $response->setPublic();
+        }
+        return $response;
     }
 }

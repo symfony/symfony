8af010aad640d08e9d3a23a476db46011ec1039a
Fabien Potencier <fabien.potencier@gmail.com>
2012-12-03 23:11:37 +0100

    [Routing] added a warning about UrlMatcher::getAttributes()

--- src/Symfony/Component/Routing/Matcher/UrlMatcher.php
+++ src/Symfony/Component/Routing/Matcher/UrlMatcher.php
@@ -162,17 +162,22 @@ class UrlMatcher implements UrlMatcherInterface
     }
 
     /**
-     * Returns an array of values to use as request attributes
+     * Returns an array of values to use as request attributes.
      *
-     * @param Route $route      The route we are matching against
-     * @param string $name      The name of the route
-     * @param array $attributes An array of attributes from the matcher
+     * As this method requires the Route object, it is not available
+     * in matchers that do not have access to the matched Route instance
+     * (like the PHP and Apache matcher dumpers).
+     *
+     * @param Route  $route      The route we are matching against
+     * @param string $name       The name of the route
+     * @param array  $attributes An array of attributes from the matcher
      *
      * @return array An array of parameters
      */
     protected function getAttributes(Route $route, $name, array $attributes)
     {
         $attributes['_route'] = $name;
+
         return $this->mergeDefaults($attributes, $route->getDefaults());
     }
 

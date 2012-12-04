245ee8a022b972a7b8684663351119dcf6dcc423
woodspire <woodspire@gmail.com>
2012-12-04 13:58:20 -0500

    Removed the PropelColumnTypes.php copy
    Using the Propel class instead, like in Column class

--- src/Symfony/Bridge/Propel1/Tests/Fixtures/ColumnMap.php
+++ src/Symfony/Bridge/Propel1/Tests/Fixtures/ColumnMap.php
@@ -56,6 +56,6 @@ class ColumnMap
    */
    public function getPdoType()
    {
-    return PropelColumnTypes::getPdoType($this->type);
+    return \PropelColumnTypes::getPdoType($this->type);
    }
 }

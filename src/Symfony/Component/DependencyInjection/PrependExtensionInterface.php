13b6da022588e5f3a4c8616d5573e04ba5880a78
Lukas Kahwe Smith <smith@pooteeweet.org>
2012-12-05 15:33:16 +0100

    moved PrependExtensionInterface to the DI component

--- src/Symfony/Component/DependencyInjection/PrependExtensionInterface.php
+++ src/Symfony/Component/DependencyInjection/PrependExtensionInterface.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Component\HttpKernel\DependencyInjection;
+namespace Symfony\Component\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 

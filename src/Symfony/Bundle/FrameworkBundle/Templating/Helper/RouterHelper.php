0ef13b6f4c52c59254fdea7e29f19b919a7b033f
Tobias Schultze <webmaster@tubo-world.de>
2012-12-13 19:02:15 +0100

    [Routing] made reference type fully BC and improved phpdoc considerably

--- src/Symfony/Bundle/FrameworkBundle/Templating/Helper/RouterHelper.php
+++ src/Symfony/Bundle/FrameworkBundle/Templating/Helper/RouterHelper.php
@@ -36,11 +36,13 @@ class RouterHelper extends Helper
     /**
      * Generates a URL from the given parameters.
      *
-     * @param string $name          The name of the route
-     * @param mixed  $parameters    An array of parameters
-     * @param string $referenceType The type of reference (see UrlGeneratorInterface)
+     * @param string         $name          The name of the route
+     * @param mixed          $parameters    An array of parameters
+     * @param Boolean|string $referenceType The type of reference (one of the constants in UrlGeneratorInterface)
      *
      * @return string The generated URL
+     *
+     * @see UrlGeneratorInterface
      */
     public function generate($name, $parameters = array(), $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
     {

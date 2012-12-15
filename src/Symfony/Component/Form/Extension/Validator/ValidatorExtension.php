5dbb9e3e7d7fba3c06b0164b15d79b1c1d2c82fd
Drak <drak@zikula.org>
2012-12-15 18:32:47 +0000

    Refactored calls to deprecated method calls.

--- src/Symfony/Component/Form/Extension/Validator/ValidatorExtension.php
+++ src/Symfony/Component/Form/Extension/Validator/ValidatorExtension.php
@@ -36,7 +36,7 @@ class ValidatorExtension extends AbstractExtension
         // code must be kept synchronized with validation.xml
 
         /** @var \Symfony\Component\Validator\Mapping\ClassMetadata $metadata */
-        $metadata = $this->validator->getMetadataFactory()->getClassMetadata('Symfony\Component\Form\Form');
+        $metadata = $this->validator->getMetadataFactory()->getMetadataFor('Symfony\Component\Form\Form');
         $metadata->addConstraint(new Form());
         $metadata->addPropertyConstraint('children', new Valid());
     }
--- src/Symfony/Component/Validator/Mapping/ClassMetadataFactory.php
+++ src/Symfony/Component/Validator/Mapping/ClassMetadataFactory.php
@@ -70,7 +70,7 @@ class ClassMetadataFactory implements ClassMetadataFactoryInterface, MetadataFac
 
         // Include constraints from the parent class
         if ($parent = $metadata->getReflectionClass()->getParentClass()) {
-            $metadata->mergeConstraints($this->getClassMetadata($parent->name));
+            $metadata->mergeConstraints($this->getMetadataFor($parent->name));
         }
 
         // Include constraints from all implemented interfaces
@@ -78,7 +78,7 @@ class ClassMetadataFactory implements ClassMetadataFactoryInterface, MetadataFac
             if ('Symfony\Component\Validator\GroupSequenceProviderInterface' === $interface->name) {
                 continue;
             }
-            $metadata->mergeConstraints($this->getClassMetadata($interface->name));
+            $metadata->mergeConstraints($this->getMetadataFor($interface->name));
         }
 
         if (null !== $this->loader) {

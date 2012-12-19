ad6a6c86512e98aa66770e447072acfd25deab99
Manuele Menozzi <mmenozzi@webgriffe.com>
2012-12-19 11:26:36 +0100

    [Finder] Re-enable BsdFindAdapter for Darwin shells and fix it
    BSD find command needs "-E" switch to evaluate POSIX regex.
    Added the ability to Command to insert bits at a given index.
    
    On some systems (Mac OS X for example) php's function
    sys_get_temp_dir() returns a directory that is a symlink.
    This causes tests failures because expected paths are different
    from path returned by the adapter. So, has been added a realpath.
    
    The building of sorting command has been totally moved on the adapter so
    the sorting command for BsdFindAdapter has been fixed.
    
    The building of content filtering command has been moved on the adapter.
    In BsdFindAdapter version, -r switch has been replaced with initial grep
    that srips out blank lines, this way is compatible with BSD shell.

--- src/Symfony/Component/Finder/Adapter/AbstractFindAdapter.php
+++ src/Symfony/Component/Finder/Adapter/AbstractFindAdapter.php
@@ -286,58 +286,25 @@ abstract class AbstractFindAdapter extends AbstractAdapter
 
     /**
      * @param Command $command
-     * @param array   $contains
-     * @param Boolean $not
-     */
-    private function buildContentFiltering(Command $command, array $contains, $not = false)
-    {
-        foreach ($contains as $contain) {
-            $expr = Expression::create($contain);
-
-            // todo: avoid forking process for each $pattern by using multiple -e options
-            $command
-                ->add('| xargs -r grep -I')
-                ->add($expr->isCaseSensitive() ? null : '-i')
-                ->add($not ? '-L' : '-l')
-                ->add('-Ee')->arg($expr->renderPattern());
-        }
-    }
-
-    /**
-     * @param Command $command
      * @param string  $sort
      *
      * @throws \InvalidArgumentException
      */
     private function buildSorting(Command $command, $sort)
     {
-        switch ($sort) {
-            case SortableIterator::SORT_BY_NAME:
-                $command->ins('sort')->add('| sort');
-
-                return;
-            case SortableIterator::SORT_BY_TYPE:
-                $format = '%y';
-                break;
-            case SortableIterator::SORT_BY_ACCESSED_TIME:
-                $format = '%A@';
-                break;
-            case SortableIterator::SORT_BY_CHANGED_TIME:
-                $format = '%C@';
-                break;
-            case SortableIterator::SORT_BY_MODIFIED_TIME:
-                $format = '%T@';
-                break;
-            default:
-                throw new \InvalidArgumentException('Unknown sort options: '.$sort.'.');
-        }
-
-        $this->buildFormatSorting($command, $format);
+        $this->buildFormatSorting($command, $sort);
     }
 
     /**
      * @param Command $command
-     * @param string  $format
+     * @param string  $sort
      */
-    abstract protected function buildFormatSorting(Command $command, $format);
+    abstract protected function buildFormatSorting(Command $command, $sort);
+
+    /**
+     * @param Command $command
+     * @param array   $contains
+     * @param Boolean $not
+     */
+    abstract protected function buildContentFiltering(Command $command, array $contains, $not = false);
 }

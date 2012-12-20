b53155f2083263552c231c2030bfe5e93ed05e4b
Joseph Bielawski <stloyd@gmail.com>
2012-12-20 22:10:24 +0100

    [Form] Fix failing `MonthChoiceList` in PHP 5.5

--- src/Symfony/Component/Form/Extension/Core/ChoiceList/MonthChoiceList.php
+++ src/Symfony/Component/Form/Extension/Core/ChoiceList/MonthChoiceList.php
@@ -39,7 +39,11 @@ class MonthChoiceList extends PaddedChoiceList
         $pattern = $this->formatter->getPattern();
         $timezone = $this->formatter->getTimezoneId();
 
-        $this->formatter->setTimezoneId('UTC');
+        if (version_compare(phpversion(), '5.5.0alpha1', '<')) {
+            $this->formatter->setTimezoneId('UTC');
+        } else {
+            $this->formatter->setTimezone('UTC');
+        }
 
         if (preg_match('/M+/', $pattern, $matches)) {
             $this->formatter->setPattern($matches[0]);
@@ -53,6 +57,10 @@ class MonthChoiceList extends PaddedChoiceList
             $this->formatter->setPattern($pattern);
         }
 
-        $this->formatter->setTimezoneId($timezone);
+        if (version_compare(phpversion(), '5.5.0alpha1', '<')) {
+            $this->formatter->setTimezoneId($timezone);
+        } else {
+            $this->formatter->setTimezone($timezone);
+        }
     }
 }

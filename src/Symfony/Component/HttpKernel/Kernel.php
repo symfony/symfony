2e9b67b5b93eb076d1636d3b2e9782a3599a60e8
Fabien Potencier <fabien.potencier@gmail.com>
2012-12-20 15:10:17 +0100

    bumped Symfony version to 2.0.21-DEV

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -56,12 +56,12 @@ abstract class Kernel implements KernelInterface
     protected $startTime;
     protected $classes;
 
-    const VERSION         = '2.0.20';
-    const VERSION_ID      = '20020';
+    const VERSION         = '2.0.21';
+    const VERSION_ID      = '20021';
     const MAJOR_VERSION   = '2';
     const MINOR_VERSION   = '0';
-    const RELEASE_VERSION = '20';
-    const EXTRA_VERSION   = '';
+    const RELEASE_VERSION = '21';
+    const EXTRA_VERSION   = 'DEV';
 
     /**
      * Constructor.

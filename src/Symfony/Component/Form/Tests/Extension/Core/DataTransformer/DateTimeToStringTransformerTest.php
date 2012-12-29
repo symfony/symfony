81967f6a70641ff4d036675a4f106d78b8ad398e
Jakub Zalas <jakub@zalas.pl>
2012-12-29 20:41:42 +0100

    [Form] Fixed failing tests for DateTimeToStringTransformer.
    Tests were only failing at the end of the month. PHP uses current day if it is not passed. Since February was used in the test cases, date was being moved to the next month (February has less days than other months).

--- src/Symfony/Component/Form/Tests/Extension/Core/DataTransformer/DateTimeToStringTransformerTest.php
+++ src/Symfony/Component/Form/Tests/Extension/Core/DataTransformer/DateTimeToStringTransformerTest.php
@@ -23,7 +23,7 @@ class DateTimeToStringTransformerTest extends DateTimeTestCase
             array('Y-m-d H:i', '2010-02-03 16:05', '2010-02-03 16:05:00 UTC'),
             array('Y-m-d H', '2010-02-03 16', '2010-02-03 16:00:00 UTC'),
             array('Y-m-d', '2010-02-03', '2010-02-03 00:00:00 UTC'),
-            array('Y-m', '2010-02', '2010-02-01 00:00:00 UTC'),
+            array('Y-m', '2010-12', '2010-12-01 00:00:00 UTC'),
             array('Y', '2010', '2010-01-01 00:00:00 UTC'),
             array('d-m-Y', '03-02-2010', '2010-02-03 00:00:00 UTC'),
             array('H:i:s', '16:05:06', '1970-01-01 16:05:06 UTC'),

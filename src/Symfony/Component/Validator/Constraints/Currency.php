5609aae8568199edee653dde61d82472d9090905
Miha Vrhovnik <miha.vrhovnik@pagein.si>
2013-01-05 10:58:46 +0100

    Added currency form type and validator

---
+++ src/Symfony/Component/Validator/Constraints/Currency.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ *
+ * @author Miha Vrhovnik <miha.vrhovnik@pagein.si>
+ *
+ * @api
+ */
+class Currency extends Constraint
+{
+    public $message = 'This value is not a valid currency.';
+}

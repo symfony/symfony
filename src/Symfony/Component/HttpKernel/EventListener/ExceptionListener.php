4500d30fea7c3fe25658cb24821808d4ce3a8eea
Igor Wiedler <igor@wiedler.ch>
2013-01-08 01:21:54 +0100

    [HttpKernel] Clarify misleading comment in ExceptionListener

--- src/Symfony/Component/HttpKernel/EventListener/ExceptionListener.php
+++ src/Symfony/Component/HttpKernel/EventListener/ExceptionListener.php
@@ -89,7 +89,7 @@ class ExceptionListener implements EventSubscriberInterface
             // set handling to false otherwise it wont be able to handle further more
             $handling = false;
 
-            // re-throw the exception as this is a catch-all
+            // re-throw the exception from within HttpKernel as this is a catch-all
             return;
         }
 

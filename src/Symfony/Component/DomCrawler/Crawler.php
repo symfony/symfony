41ce58a16f0090d382886e9ba8f19b11a90c365a
Diego Saint Esteben <diego@saintesteben.me>
2013-01-15 16:21:53 -0300

    Testing

--- src/Symfony/Component/DomCrawler/Crawler.php
+++ src/Symfony/Component/DomCrawler/Crawler.php
@@ -7,6 +7,8 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * Minor change to test
  */
 
 namespace Symfony\Component\DomCrawler;

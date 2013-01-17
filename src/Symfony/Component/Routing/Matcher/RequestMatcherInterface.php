45474e7cb45ab23e41af8313d8f6a45b5c2caa0a
Tobias Schultze <webmaster@tubo-world.de>
2013-01-17 21:12:13 +0100

    [Routing] remove redeclaration of interface method
    Unfortunately this is not allowed in PHP5.3 which we support, so we cannot override the phpdoc for the method either

--- src/Symfony/Component/Routing/Matcher/RequestMatcherInterface.php
+++ src/Symfony/Component/Routing/Matcher/RequestMatcherInterface.php
@@ -11,30 +11,15 @@
 
 namespace Symfony\Component\Routing\Matcher;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Exception\MethodNotAllowedException;
-
 /**
- * RequestMatcherInterface is the interface that all request matcher classes must implement.
+ * A matcher implementing RequestMatcherInterface indicates that it also accepts
+ * a {@link \Symfony\Component\HttpFoundation\Request} object as parameter of the
+ * match method. In that way it is a marker interface that widens the accepted
+ * range of the extended UrlMatcherInterface::match method.
  *
  * @author Fabien Potencier <fabien@symfony.com>
  */
 interface RequestMatcherInterface extends UrlMatcherInterface
 {
-    /**
-     * Tries to match a Request or a URL path with a set of routes.
-     *
-     * In contrast to the UrlMatcherInterface, a request matcher also accepts a Request object.
-     *
-     * @see UrlMatcherInterface
-     *
-     * @param Request|string $request A Request or a string with the path component of a URL (raw format, i.e. not urldecoded).
-     *
-     * @return array An array of parameters
-     *
-     * @throws ResourceNotFoundException If no matching resource could be found
-     * @throws MethodNotAllowedException If a matching resource was found but the request method is not allowed
-     */
-    public function match($request);
+
 }

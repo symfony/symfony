1ad7ff1b46bca450308f889f32fb850157910e50
Fran Moreno <franmomu@gmail.com>
2013-01-21 10:11:57 +0100

    Fix authentication typo

--- src/Symfony/Component/Security/Http/SecurityEvents.php
+++ src/Symfony/Component/Security/Http/SecurityEvents.php
@@ -15,7 +15,7 @@ final class SecurityEvents
 {
     /**
      * The INTERACTIVE_LOGIN event occurs after a user is logged in
-     * interactively for autentication based on http, cookies or X509.
+     * interactively for authentication based on http, cookies or X509.
      *
      * The event listener method receives a
      * Symfony\Component\Security\Http\Event\InteractiveLoginEvent instance.

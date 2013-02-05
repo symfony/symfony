7b6a6c90c79a2f7e82f9fbbe283c1f2e3da21326
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-05 09:33:19 +0100

    Use REQUEST_TIME_FLOAT if available.
    This will give a more correct initialization time when using the
    DataCollectors without a KernelInterface implementation such as Silex.

--- src/Symfony/Component/HttpKernel/DataCollector/TimeDataCollector.php
+++ src/Symfony/Component/HttpKernel/DataCollector/TimeDataCollector.php
@@ -35,8 +35,14 @@ class TimeDataCollector extends DataCollector
      */
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
+        $requestTime = null !== $this->kernel ? $this->kernel->getStartTime() : null;
+
+        if (null === $requestTime) {
+            $requestTime = $request->server->get('REQUEST_TIME_FLOAT', $request->server->get('REQUEST_TIME'));
+        }
+
         $this->data = array(
-            'start_time' => (null !== $this->kernel ? $this->kernel->getStartTime() : $_SERVER['REQUEST_TIME']) * 1000,
+            'start_time' => $requestTime * 1000,
             'events'     => array(),
         );
     }

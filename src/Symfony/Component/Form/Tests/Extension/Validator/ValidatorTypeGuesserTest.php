3bbd12fdd5d9137d1de4eb034a30ce2ec753ef1a
Bernhard Schussek <bschussek@gmail.com>
2013-02-27 12:10:38 +0100

    [Form] Fixed ValidatorTypeGuesser to guess unmapped properties not to be required

---
+++ src/Symfony/Component/Form/Tests/Extension/Validator/ValidatorTypeGuesserTest.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Form\Tests\Extension\Validator;
+
+use Symfony\Component\Form\Extension\Validator\ValidatorTypeGuesser;
+use Symfony\Component\Form\Guess\Guess;
+use Symfony\Component\Form\Guess\ValueGuess;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\True;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+
+/**
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class ValidatorTypeGuesserTest extends \PHPUnit_Framework_TestCase
+{
+    const TEST_CLASS = 'Symfony\Component\Form\Tests\Extension\Validator\ValidatorTypeGuesserTest_TestClass';
+
+    const TEST_PROPERTY = 'property';
+
+    /**
+     * @var ValidatorTypeGuesser
+     */
+    private $guesser;
+
+    /**
+     * @var ClassMetadata
+     */
+    private $metadata;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $metadataFactory;
+
+    protected function setUp()
+    {
+        $this->metadata = new ClassMetadata(self::TEST_CLASS);
+        $this->metadataFactory = $this->getMock('Symfony\Component\Validator\Mapping\ClassMetadataFactoryInterface');
+        $this->metadataFactory->expects($this->any())
+            ->method('getClassMetadata')
+            ->with(self::TEST_CLASS)
+            ->will($this->returnValue($this->metadata));
+        $this->guesser = new ValidatorTypeGuesser($this->metadataFactory);
+    }
+
+    public function guessRequiredProvider()
+    {
+        return array(
+            array(new NotNull(), new ValueGuess(true, Guess::HIGH_CONFIDENCE)),
+            array(new NotBlank(), new ValueGuess(true, Guess::HIGH_CONFIDENCE)),
+            array(new True(), new ValueGuess(true, Guess::HIGH_CONFIDENCE)),
+            array(new Length(10), new ValueGuess(false, Guess::LOW_CONFIDENCE)),
+            array(new Range(array('min' => 1, 'max' => 20)), new ValueGuess(false, Guess::LOW_CONFIDENCE)),
+        );
+    }
+
+    /**
+     * @dataProvider guessRequiredProvider
+     */
+    public function testGuessRequired($constraint, $guess)
+    {
+        // add distracting constraint
+        $this->metadata->addPropertyConstraint(self::TEST_PROPERTY, new Email());
+
+        // add constraint under test
+        $this->metadata->addPropertyConstraint(self::TEST_PROPERTY, $constraint);
+
+        $this->assertEquals($guess, $this->guesser->guessRequired(self::TEST_CLASS, self::TEST_PROPERTY));
+    }
+
+    public function testGuessRequiredReturnsFalseForUnmappedProperties()
+    {
+        $this->assertEquals(new ValueGuess(false, Guess::LOW_CONFIDENCE), $this->guesser->guessRequired(self::TEST_CLASS, self::TEST_PROPERTY));
+    }
+
+}
+
+class ValidatorTypeGuesserTest_TestClass
+{
+    private $property;
+}

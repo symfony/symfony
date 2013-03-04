2922f85707a93479abd9085c816237a12593f81d
Jean-François Simon <contact@jfsimon.fr>
2013-03-04 18:17:05 +0100

    [TwigBundle] changed conditional extensions registration strategy

---
+++ src/Symfony/Bundle/TwigBundle/DependencyInjection/Compiler/ExtensionsPass.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Symfony\Bundle\TwigBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * @author Jean-François Simon <jeanfrancois.simon@sensiolabs.com>
+ */
+class ExtensionsPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->has('form.extension')) {
+            $container->getDefinition('twig.extension.form')->addTag('twig.extension');
+        }
+
+        if (false === $container->has('translator')) {
+            $container->getDefinition('twig.extension.trans')->addTag('twig.extension');
+        }
+    }
+}

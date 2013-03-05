f6d0e9254b31ae83ad1d1130fcdb9556ec159699
sualko <github@spam.herberth.eu>
2013-03-05 15:28:25 +0100

    Update RouteCompiler.php
    Fixes INF problem on solaris.

--- src/Symfony/Component/Routing/RouteCompiler.php
+++ src/Symfony/Component/Routing/RouteCompiler.php
@@ -68,7 +68,7 @@ class RouteCompiler implements RouteCompilerInterface
         }
 
         // find the first optional token
-        $firstOptional = INF;
+        $firstOptional = PHP_INT_MAX;
         for ($i = count($tokens) - 1; $i >= 0; $i--) {
             $token = $tokens[$i];
             if ('variable' === $token[0] && $route->hasDefault($token[3])) {

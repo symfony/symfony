cdcce194438983c270cdf9d4e9931c729e741032
Jean-François Simon <contact@jfsimon.fr>
2013-03-05 09:24:29 +0100

    [TwigBundle] renamed compiler pass

--- src/Symfony/Bundle/TwigBundle/TwigBundle.php
+++ src/Symfony/Bundle/TwigBundle/TwigBundle.php
@@ -16,7 +16,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\TwigEnvironmentPass;
 use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\TwigLoaderPass;
 use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\ExceptionListenerPass;
-use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\ExtensionsPass;
+use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\ExtensionPass;
 
 /**
  * Bundle.
@@ -29,7 +29,7 @@ class TwigBundle extends Bundle
     {
         parent::build($container);
 
-        $container->addCompilerPass(new ExtensionsPass());
+        $container->addCompilerPass(new ExtensionPass());
         $container->addCompilerPass(new TwigEnvironmentPass());
         $container->addCompilerPass(new TwigLoaderPass());
         $container->addCompilerPass(new ExceptionListenerPass());

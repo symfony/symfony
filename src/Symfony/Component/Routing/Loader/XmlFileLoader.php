41ad9d8eb22b0908b72167c2eaa44d3a4d10f0f5
Tobias Schultze <webmaster@tubo-world.de>
2013-03-05 15:01:34 +0100

    [Routing] make xml loader more tolerant
    schemes and methods may also be delimited by whitespace, comma or pipe.
    this eases migration as now schemes="GET|POST" also works

--- src/Symfony/Component/Routing/Loader/XmlFileLoader.php
+++ src/Symfony/Component/Routing/Loader/XmlFileLoader.php
@@ -126,8 +126,8 @@ class XmlFileLoader extends FileLoader
             $node->removeAttribute('pattern');
         }
 
-        $schemes = array_filter(explode(' ', $node->getAttribute('schemes')));
-        $methods = array_filter(explode(' ', $node->getAttribute('methods')));
+        $schemes = preg_split('/[\s,\|]++/', $node->getAttribute('schemes'), -1, PREG_SPLIT_NO_EMPTY);
+        $methods = preg_split('/[\s,\|]++/', $node->getAttribute('methods'), -1, PREG_SPLIT_NO_EMPTY);
 
         list($defaults, $requirements, $options) = $this->parseConfigs($node, $path);
 
@@ -154,8 +154,8 @@ class XmlFileLoader extends FileLoader
         $type = $node->getAttribute('type');
         $prefix = $node->getAttribute('prefix');
         $host = $node->hasAttribute('host') ? $node->getAttribute('host') : null;
-        $schemes = $node->hasAttribute('schemes') ? array_filter(explode(' ', $node->getAttribute('schemes'))) : null;
-        $methods = $node->hasAttribute('methods') ? array_filter(explode(' ', $node->getAttribute('methods'))) : null;
+        $schemes = $node->hasAttribute('schemes') ? preg_split('/[\s,\|]++/', $node->getAttribute('schemes'), -1, PREG_SPLIT_NO_EMPTY) : null;
+        $methods = $node->hasAttribute('methods') ? preg_split('/[\s,\|]++/', $node->getAttribute('methods'), -1, PREG_SPLIT_NO_EMPTY) : null;
 
         list($defaults, $requirements, $options) = $this->parseConfigs($node, $path);
 

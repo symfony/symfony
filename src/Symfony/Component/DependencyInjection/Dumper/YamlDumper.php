778c4a8e01e7c6d929b6689e534b2a897e4ee08b
Thomas Ploch <tp@responsive-code.de>
2013-03-08 16:38:09 +0100

    Added '@@' escaping strategy for YamlFileLoader and YamlDumper
    Added the possibility to to use '@@' as an escaping strategy for
    parameters that should be treated as strings but start with '@'
    (i.e. safe mailer passwords).

--- src/Symfony/Component/DependencyInjection/Dumper/YamlDumper.php
+++ src/Symfony/Component/DependencyInjection/Dumper/YamlDumper.php
@@ -260,7 +260,7 @@ class YamlDumper extends Dumper
         foreach ($parameters as $key => $value) {
             if (is_array($value)) {
                 $value = $this->prepareParameters($value, $escape);
-            } elseif ($value instanceof Reference) {
+            } elseif ($value instanceof Reference || is_string($value) && 0 === strpos($value, '@')) {
                 $value = '@'.$value;
             }
 

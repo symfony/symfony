c5e999aad49d7ea9c5cf317ed522006ff14c1894
Jean-François Simon <contact@jfsimon.fr>
2013-03-12 17:19:53 +0100

    [TwigBridge] fixed non probant tests & added new one

--- src/Symfony/Bridge/Twig/Tests/NodeVisitor/TranslationDefaultDomainNodeVisitorTest.php
+++ src/Symfony/Bridge/Twig/Tests/NodeVisitor/TranslationDefaultDomainNodeVisitorTest.php
@@ -15,11 +15,10 @@ class TranslationDefaultDomainNodeVisitorTest extends TestCase
     public function testDefaultDomainAssignment(\Twig_Node $node)
     {
         $env = new \Twig_Environment(new \Twig_Loader_String(), array('cache' => false, 'autoescape' => false, 'optimizations' => 0));
-
         $visitor = new TranslationDefaultDomainNodeVisitor();
 
         // visit trans_default_domain tag
-        $defaultDomain = TwigNodeProvider::getTransDefaultDomainTag('domain');
+        $defaultDomain = TwigNodeProvider::getTransDefaultDomainTag(self::$domain);
         $visitor->enterNode($defaultDomain, $env);
         $visitor->leaveNode($defaultDomain, $env);
 
@@ -38,12 +37,38 @@ class TranslationDefaultDomainNodeVisitorTest extends TestCase
         $this->assertEquals(array(array(self::$message, self::$domain)), $visitor->getMessages());
     }
 
+    /** @dataProvider getDefaultDomainAssignmentTestData */
+    public function testNewModuleWithoutDefaultDomainTag(\Twig_Node $node)
+    {
+        $env = new \Twig_Environment(new \Twig_Loader_String(), array('cache' => false, 'autoescape' => false, 'optimizations' => 0));
+        $visitor = new TranslationDefaultDomainNodeVisitor();
+
+        // visit trans_default_domain tag
+        $newModule = TwigNodeProvider::getModule('test');
+        $visitor->enterNode($newModule, $env);
+        $visitor->leaveNode($newModule, $env);
+
+        // visit tested node
+        $enteredNode = $visitor->enterNode($node, $env);
+        $leavedNode = $visitor->leaveNode($node, $env);
+        $this->assertSame($node, $enteredNode);
+        $this->assertSame($node, $leavedNode);
+
+        // extracting tested node messages
+        $visitor = new TranslationNodeVisitor();
+        $visitor->enable();
+        $visitor->enterNode($node, $env);
+        $visitor->leaveNode($node, $env);
+
+        $this->assertEquals(array(array(self::$message, null)), $visitor->getMessages());
+    }
+
     public function getDefaultDomainAssignmentTestData()
     {
         return array(
-            array(TwigNodeProvider::getTransFilter(self::$message, self::$domain)),
-            array(TwigNodeProvider::getTransChoiceFilter(self::$message, self::$domain)),
-            array(TwigNodeProvider::getTransTag(self::$message, self::$domain)),
+            array(TwigNodeProvider::getTransFilter(self::$message)),
+            array(TwigNodeProvider::getTransChoiceFilter(self::$message)),
+            array(TwigNodeProvider::getTransTag(self::$message)),
         );
     }
 }

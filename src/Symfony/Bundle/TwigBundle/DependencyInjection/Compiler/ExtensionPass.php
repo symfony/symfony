77c15d491e44fdd8eb73ccac21ea6a25b21a111e
Jean-François Simon <contact@jfsimon.fr>
2013-03-13 09:15:46 +0100

    [TwigBundle] added missing code

--- src/Symfony/Bundle/TwigBundle/DependencyInjection/Compiler/ExtensionPass.php
+++ src/Symfony/Bundle/TwigBundle/DependencyInjection/Compiler/ExtensionPass.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Bundle\TwigBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
@@ -14,6 +23,8 @@ class ExtensionPass implements CompilerPassInterface
     {
         if ($container->has('form.extension')) {
             $container->getDefinition('twig.extension.form')->addTag('twig.extension');
+            $reflClass = new \ReflectionClass('Symfony\Bridge\Twig\Extension\FormExtension');
+            $container->getDefinition('twig.loader')->addMethodCall('addPath', array(dirname(dirname($reflClass->getFileName())).'/Resources/views/Form'));
         }
 
         if (false === $container->has('translator')) {

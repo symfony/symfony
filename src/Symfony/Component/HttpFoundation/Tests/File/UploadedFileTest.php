5bb44f52a0aaa36191db05bd11da6758ca625c21
Bilal Amarni <bilal.amarni@gmail.com>
2013-03-23 10:56:11 +0100

    [HttpFoundation] UploadedFile - moved a security check
    Squashed commit of the following:
    
    commit b03b32ecc985c4a4f9dc7df2d3336a4cd75aae30
    Merge: fb7004b fc70e13
    Author: Bilal Amarni <bilal.amarni@gmail.com>
    Date:   Wed Feb 27 11:33:37 2013 +0100
    
        [HttpFoundation] UploadedFile - moved a security check
    
    commit fc70e138c1d3858775c9efe51268cae6d7ec3f69
    Author: Bilal Amarni <bilal.amarni@gmail.com>
    Date:   Thu Jan 24 11:07:29 2013 +0100
    
        explicitly passed UPLOAD_ERR_OK constant in a test
    
    commit dda03a2faab9539ca3a93736dd2bc0ec27feb4e7
    Author: Bilal Amarni <bilal.amarni@gmail.com>
    Date:   Fri Jan 18 17:24:06 2013 +0100
    
        [HttpFoundation] UploadedFile - moved a security check from move() to isValid()

--- src/Symfony/Component/HttpFoundation/Tests/File/UploadedFileTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/File/UploadedFileTest.php
@@ -197,7 +197,8 @@ class UploadedFileTest extends \PHPUnit_Framework_TestCase
             'original.gif',
             null,
             filesize(__DIR__.'/Fixtures/test.gif'),
-            UPLOAD_ERR_OK
+            UPLOAD_ERR_OK,
+                true
         );
 
         $this->assertTrue($file->isValid());
@@ -228,5 +229,18 @@ class UploadedFileTest extends \PHPUnit_Framework_TestCase
             array(UPLOAD_ERR_NO_TMP_DIR),
             array(UPLOAD_ERR_EXTENSION),
         );
+    }
+
+    public function testIsInvalidIfNotHttpUpload()
+    {
+        $file = new UploadedFile(
+            __DIR__.'/Fixtures/test.gif',
+            'original.gif',
+            null,
+            filesize(__DIR__.'/Fixtures/test.gif'),
+            UPLOAD_ERR_OK
+        );
+
+        $this->assertFalse($file->isValid());
     }
 }

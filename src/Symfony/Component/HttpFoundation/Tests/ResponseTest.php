6c26518c359883710eb465e5daf84e28c551ac93
Gordon Franke <info@nevalon.de>
2013-04-09 10:58:35 +0200

    add 204 No Content to isRedirect

--- src/Symfony/Component/HttpFoundation/Tests/ResponseTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/ResponseTest.php
@@ -624,6 +624,16 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
         $response = new Response('', 301, array('Location' => '/good-uri'));
         $this->assertFalse($response->isRedirect('/bad-uri'));
         $this->assertTrue($response->isRedirect('/good-uri'));
+
+        $response = new Response('', 201, array('Location' => '/good-uri'));
+        $this->assertFalse($response->isRedirection());
+        $this->assertFalse($response->isRedirect('/bad-uri'));
+        $this->assertTrue($response->isRedirect('/good-uri'));
+
+        $response = new Response('', 204, array('Location' => '/good-uri'));
+        $this->assertFalse($response->isRedirection());
+        $this->assertFalse($response->isRedirect('/bad-uri'));
+        $this->assertTrue($response->isRedirect('/good-uri'));
     }
 
     public function testIsNotFound()

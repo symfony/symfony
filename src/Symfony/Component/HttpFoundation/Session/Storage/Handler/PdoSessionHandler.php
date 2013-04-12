112e552b941b40d5ba518b0e7d3d23c26de16779
Brooks Boyd <boydb@midnightdesign.ws>
2013-04-12 08:27:40 -0500

    Update error message to be more helpful

--- src/Symfony/Component/HttpFoundation/Session/Storage/Handler/PdoSessionHandler.php
+++ src/Symfony/Component/HttpFoundation/Session/Storage/Handler/PdoSessionHandler.php
@@ -49,7 +49,7 @@ class PdoSessionHandler implements \SessionHandlerInterface
             throw new \InvalidArgumentException('You must provide the "db_table" option for a PdoSessionStorage.');
         }
         if ($pdo->getAttribute(\PDO::ATTR_ERRMODE) != \PDO::ERRMODE_EXCEPTION) {
-            throw new \InvalidArgumentException("Session Handler PDO must be set to throw Exceptions on error.");
+            throw new \InvalidArgumentException(sprintf('"%s" requires PDO error mode attribute be set to throw Exceptions (i.e. $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION))', __CLASS__));
         }
         $this->pdo = $pdo;
         $this->dbOptions = array_merge(array(

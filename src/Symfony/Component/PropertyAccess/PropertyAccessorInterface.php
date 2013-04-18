2a666cb7c3ecbaa6226e5a9da0476b10b6e997db
Bernhard Schussek <bschussek@gmail.com>
2013-04-18 16:39:54 +0200

    [PropertyAccess] Changed PropertyAccessor to continue searching when a non-public method/property are found

--- src/Symfony/Component/PropertyAccess/PropertyAccessorInterface.php
+++ src/Symfony/Component/PropertyAccess/PropertyAccessorInterface.php
@@ -43,11 +43,9 @@ interface PropertyAccessorInterface
      * @param string|PropertyPathInterface $propertyPath  The property path to modify
      * @param mixed                        $value         The value to set at the end of the property path
      *
-     * @throws Exception\NoSuchPropertyException       If a property does not exist
-     * @throws Exception\PropertyAccessDeniedException If a property cannot be accessed due to
-     *                                                 access restrictions (private or protected)
-     * @throws Exception\UnexpectedTypeException       If a value within the path is neither object
-     *                                                 nor array
+     * @throws Exception\NoSuchPropertyException If a property does not exist or is not public.
+     * @throws Exception\UnexpectedTypeException If a value within the path is neither object
+     *                                           nor array
      */
     public function setValue(&$objectOrArray, $propertyPath, $value);
 
@@ -77,8 +75,7 @@ interface PropertyAccessorInterface
      *
      * @return mixed The value at the end of the property path
      *
-     * @throws Exception\NoSuchPropertyException       If the property/getter does not exist
-     * @throws Exception\PropertyAccessDeniedException If the property/getter exists but is not public
+     * @throws Exception\NoSuchPropertyException If a property does not exist or is not public.
      */
     public function getValue($objectOrArray, $propertyPath);
 }

ed861bac63fe1121cc09e6ed73dfeab4e75568d5
Matthias Pigulla <mp@webfactory.de>
2013-04-21 12:25:52 +0200

    Remove unused methods.
    The only client of these methods was removed in 669898bf57b75a9d1f06a161045d4dcaca13f10d.

--- src/Symfony/Component/Config/Util/CacheFileUtils.php
+++ src/Symfony/Component/Config/Util/CacheFileUtils.php
@@ -20,42 +20,6 @@ namespace Symfony\Component\Config\Util;
  */
 class CacheFileUtils
 {
-
-    /**
-     * Make sure the given directory can be used as a cache directory, creating it if necessary.
-     * @param  string $dir       The directory path
-     * @param  string $name      An optional name to be used in the exception message in case of failure
-     * @throws \RuntimeException if the directory cannot be created or written to.
-     */
-    public static function createCacheDir($dir, $name = null)
-    {
-        if ($name) {
-            $name = " ($name)";
-        }
-
-        if (!is_dir($dir)) {
-            if (false === @mkdir($dir, 0777, true)) {
-                throw new \RuntimeException(sprintf('Unable to create the %s directory%s\n', $dir, $name));
-            }
-        } elseif (!is_writable($dir)) {
-            throw new \RuntimeException(sprintf('Unable to write in the %s directory%s\n', $dir, $name));
-        }
-    }
-
-    /**
-     * Tries to create a writeable directory that can contain a given file.
-     *
-     * @param  string $filename  The file the containing directory has to be created for
-     * @return string            The directory now available for caching
-     * @throws \RuntimeException If the directory cannot be created or written to
-     */
-    public static function createDirectoryForFile($filename)
-    {
-        $dir = dirname($filename);
-        self::createCacheDir($dir);
-        return $dir;
-    }
-
     /**
      * Dumps content into a file, trying to make it atomically. The directory for the file must exist.
      * @param  string $filename  The file to be written to.
@@ -64,14 +28,22 @@ class CacheFileUtils
      */
     public static function dumpInFile($filename, $content)
     {
-        $dir = self::createDirectoryForFile($filename);
+        $dir = dirname($filename);
+
+        if (!is_dir($dir)) {
+            if (false === @mkdir($dir, 0777, true)) {
+                throw new \RuntimeException(sprintf('Unable to create the %s directory\n', $dir));
+            }
+        } elseif (!is_writable($dir)) {
+            throw new \RuntimeException(sprintf('Unable to write in the %s directory\n', $dir));
+        }
 
         $tmpFile = tempnam($dir, basename($filename));
+
         if (false !== @file_put_contents($tmpFile, $content) && @rename($tmpFile, $filename)) {
             @chmod($filename, 0666 & ~umask());
         } else {
             throw new \RuntimeException(sprintf('Failed to write cache file "%s".', $filename));
         }
-
     }
 }

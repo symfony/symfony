eaaaac7432d394faf8237021691699517f138e72
Fabien Potencier <fabien.potencier@gmail.com>
2013-04-22 12:31:13 +0200

    [HttpFoundation] fixed CS for previous merge

--- src/Symfony/Component/HttpFoundation/File/UploadedFile.php
+++ src/Symfony/Component/HttpFoundation/File/UploadedFile.php
@@ -248,33 +248,26 @@ class UploadedFile extends File
     }
 
     /**
-     * Returns an informative upload error message
+     * Returns an informative upload error message.
      *
-     * @param int $code The error code returned by upload attempt
+     * @param int $code The error code returned by an upload attempt
      *
-     * @return string The error message regarding specified error code
+     * @return string The error message regarding the specified error code
      */
     private function getErrorMessage($errorCode)
     {
         static $errors = array(
-            UPLOAD_ERR_INI_SIZE   => 'The file "%s" exceeds your upload_max_filesize ini directive (limit is %d kb)',
-            UPLOAD_ERR_FORM_SIZE  => 'The file "%s" exceeds the upload limit defined in your form',
-            UPLOAD_ERR_PARTIAL    => 'The file "%s" was only partially uploaded',
-            UPLOAD_ERR_NO_FILE    => 'No file was uploaded',
-            UPLOAD_ERR_CANT_WRITE => 'The file "%s" could not be written on disk',
-            UPLOAD_ERR_NO_TMP_DIR => 'File could not be uploaded: missing temporary directory',
-            UPLOAD_ERR_EXTENSION  => 'File upload was stopped by a php extension',
+            UPLOAD_ERR_INI_SIZE   => 'The file "%s" exceeds your upload_max_filesize ini directive (limit is %d kb).',
+            UPLOAD_ERR_FORM_SIZE  => 'The file "%s" exceeds the upload limit defined in your form.',
+            UPLOAD_ERR_PARTIAL    => 'The file "%s" was only partially uploaded.',
+            UPLOAD_ERR_NO_FILE    => 'No file was uploaded.',
+            UPLOAD_ERR_CANT_WRITE => 'The file "%s" could not be written on disk.',
+            UPLOAD_ERR_NO_TMP_DIR => 'File could not be uploaded: missing temporary directory.',
+            UPLOAD_ERR_EXTENSION  => 'File upload was stopped by a php extension.',
         );
 
-        $maxFilesize = 0;
-        if ($errorCode === UPLOAD_ERR_INI_SIZE) {
-            $maxFilesize = self::getMaxFilesize() / 1024;
-        }
-
-        $message = isset($errors[$errorCode]) ? $errors[$errorCode] : null;
-        if (!isset($message)) {
-            $message = 'The file "%s" was not uploaded due to an unknown error';
-        }
+        $maxFilesize = $errorCode === UPLOAD_ERR_INI_SIZE ? self::getMaxFilesize() / 1024 : 0;
+        $message = isset($errors[$errorCode]) ? $errors[$errorCode] : 'The file "%s" was not uploaded due to an unknown error.';
 
        return sprintf($message, $this->getClientOriginalName(), $maxFilesize);
     }

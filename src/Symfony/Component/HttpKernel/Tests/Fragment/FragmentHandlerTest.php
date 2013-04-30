01ac8c91d03f19d06f5f5a15f38eb86a968d1202
Benjamin Eberlei <kontakt@beberlei.de>
2013-04-30 13:28:15 +0200

    [GH-7707] Adjusted fragmenting to not use synchronized services.

--- src/Symfony/Component/HttpKernel/Tests/Fragment/FragmentHandlerTest.php
+++ src/Symfony/Component/HttpKernel/Tests/Fragment/FragmentHandlerTest.php
@@ -17,12 +17,27 @@ use Symfony\Component\HttpFoundation\Response;
 
 class FragmentHandlerTest extends \PHPUnit_Framework_TestCase
 {
+    private $context;
+
+    public function setUp()
+    {
+        $this->context = $this->getMockBuilder('Symfony\\Component\\HttpKernel\\RequestContext')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+        $this->context
+            ->expects($this->any())
+            ->method('getCurrentRequest')
+            ->will($this->returnValue(Request::create('/')))
+        ;
+    }
+
     /**
      * @expectedException \InvalidArgumentException
      */
     public function testRenderWhenRendererDoesNotExist()
     {
-        $handler = new FragmentHandler();
+        $handler = new FragmentHandler($this->context);
         $handler->render('/', 'foo');
     }
 
@@ -72,9 +87,8 @@ class FragmentHandlerTest extends \PHPUnit_Framework_TestCase
             call_user_func_array(array($e, 'with'), $arguments);
         }
 
-        $handler = new FragmentHandler();
+        $handler = new FragmentHandler($this->context);
         $handler->addRenderer($renderer);
-        $handler->setRequest(Request::create('/'));
 
         return $handler;
     }

82ff36055ca7ce4fad66f2eda8f64e35cba567f3
Tobias Schultze <webmaster@tubo-world.de>
2013-05-04 02:14:05 +0200

    [Form] removed deprecated exceptions

--- src/Symfony/Bridge/Doctrine/Form/ChoiceList/EntityChoiceList.php
+++ src/Symfony/Bridge/Doctrine/Form/ChoiceList/EntityChoiceList.php
@@ -11,7 +11,7 @@
 
 namespace Symfony\Bridge\Doctrine\Form\ChoiceList;
 
-use Symfony\Component\Form\Exception\Exception;
+use Symfony\Component\Form\Exception\RuntimeException;
 use Symfony\Component\Form\Exception\StringCastException;
 use Symfony\Component\Form\Extension\Core\ChoiceList\ObjectChoiceList;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -406,12 +406,12 @@ class EntityChoiceList extends ObjectChoiceList
      *
      * @return array          The identifier values
      *
-     * @throws Exception If the entity does not exist in Doctrine's identity map
+     * @throws RuntimeException If the entity does not exist in Doctrine's identity map
      */
     private function getIdentifierValues($entity)
     {
         if (!$this->em->contains($entity)) {
-            throw new Exception(
+            throw new RuntimeException(
                 'Entities passed to the choice field must be managed. Maybe ' .
                 'persist them in the entity manager?'
             );

1dff89121dc715b27329f1eaf92623bc6119c120
Franz Liedke <franz@develophp.org>
2013-05-30 02:35:45 +0300

    Update unit tests.

--- src/Symfony/Component/HttpFoundation/Tests/JsonResponseTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/JsonResponseTest.php
@@ -57,13 +57,13 @@ class JsonResponseTest extends \PHPUnit_Framework_TestCase
     public function testConstructorAddsContentTypeHeader()
     {
         $response = new JsonResponse();
-        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+        $this->assertSame('application/json; charset=utf-8', $response->headers->get('Content-Type'));
     }
 
     public function testConstructorWithCustomHeaders()
     {
         $response = new JsonResponse(array(), 200, array('ETag' => 'foo'));
-        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+        $this->assertSame('application/json; charset=utf-8', $response->headers->get('Content-Type'));
         $this->assertSame('foo', $response->headers->get('ETag'));
     }
 
@@ -133,13 +133,13 @@ class JsonResponseTest extends \PHPUnit_Framework_TestCase
     public function testStaticCreateAddsContentTypeHeader()
     {
         $response = JsonResponse::create();
-        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+        $this->assertSame('application/json; charset=utf-8', $response->headers->get('Content-Type'));
     }
 
     public function testStaticCreateWithCustomHeaders()
     {
         $response = JsonResponse::create(array(), 200, array('ETag' => 'foo'));
-        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+        $this->assertSame('application/json; charset=utf-8', $response->headers->get('Content-Type'));
         $this->assertSame('foo', $response->headers->get('ETag'));
     }
 

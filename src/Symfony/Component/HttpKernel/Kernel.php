bc1bf3202dae3649387d4716b1816f580210fa9a
Fabien Potencier <fabien.potencier@gmail.com>
2013-06-02 18:01:55 +0200

    bumped Symfony version to 2.2.3-DEV

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -62,12 +62,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $classes;
     protected $errorReportingLevel;
 
-    const VERSION         = '2.2.2';
-    const VERSION_ID      = '20202';
+    const VERSION         = '2.2.3-DEV';
+    const VERSION_ID      = '20203';
     const MAJOR_VERSION   = '2';
     const MINOR_VERSION   = '2';
-    const RELEASE_VERSION = '2';
-    const EXTRA_VERSION   = '';
+    const RELEASE_VERSION = '3';
+    const EXTRA_VERSION   = 'DEV';
 
     /**
      * Constructor.

3acbb826943defe65ff4e168cbbfbea81ba1a7b7
Nick Matantsev <uberfob@gmail.com>
2013-06-12 18:20:42 +0000

    Explicitly disallowing serialization of objects as controller attributes in non-inline fragment renderers

--- src/Symfony/Component/HttpKernel/Fragment/RoutableFragmentRenderer.php
+++ src/Symfony/Component/HttpKernel/Fragment/RoutableFragmentRenderer.php
@@ -39,21 +39,33 @@ abstract class RoutableFragmentRenderer implements FragmentRendererInterface
     /**
      * Generates a fragment URI for a given controller.
      *
-     * @param ControllerReference  $reference A ControllerReference instance
-     * @param Request              $request    A Request instance
+     * @param ControllerReference  $reference         A ControllerReference instance
+     * @param Request              $request           A Request instance
+     * @param bool                 $includeAttributes whether to include reference attributes into the URI
      *
      * @return string A fragment URI
      */
-    protected function generateFragmentUri(ControllerReference $reference, Request $request)
+    protected function generateFragmentUri(ControllerReference $reference, Request $request, $includeAttributes = true)
     {
+        // work with copies of query and attributes data
+        $renderedAttributes = array('_controller' => $reference->controller);
+
         if (!isset($reference->attributes['_format'])) {
-            $reference->attributes['_format'] = $request->getRequestFormat();
+            $renderedAttributes['_format'] = $request->getRequestFormat();
         }
 
-        $reference->attributes['_controller'] = $reference->controller;
+        if ($includeAttributes) {
+            $renderedAttributes = array_merge($renderedAttributes, $reference->attributes);
+        }
 
-        $reference->query['_path'] = http_build_query($reference->attributes, '', '&');
+        $renderedQuery = array_merge($reference->query, array('_path' => http_build_query($renderedAttributes, '', '&')));
 
-        return $request->getUriForPath($this->fragmentPath.'?'.http_build_query($reference->query, '', '&'));
+        // make sure that logic entities do not end up haphazardly serialized
+        parse_str($renderedQuery['_path'], $serializedAttributes);
+        if ($serializedAttributes != $renderedAttributes) {
+            throw new \LogicException('controller attributes with objects are not supported');
+        }
+
+        return $request->getUriForPath($this->fragmentPath.'?'.http_build_query($renderedQuery, '', '&'));
     }
 }

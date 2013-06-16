24c1a51d9dbddf5e96383ccc6930719cb493f10a
Grégoire Pineau <lyrixx@lyrixx.info>
2013-06-16 03:54:21 +0200

    [CssSelector] Added test to highlight a regression

--- src/Symfony/Component/CssSelector/Tests/CssSelectorTest.php
+++ src/Symfony/Component/CssSelector/Tests/CssSelectorTest.php
@@ -19,6 +19,7 @@ class CssSelectorTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals('descendant-or-self::*', CssSelector::toXPath(''));
         $this->assertEquals('descendant-or-self::h1', CssSelector::toXPath('h1'));
+        $this->assertEquals('descendant-or-self::Project', CssSelector::toXPath('Project'));
         $this->assertEquals("descendant-or-self::h1[@id = 'foo']", CssSelector::toXPath('h1#foo'));
         $this->assertEquals("descendant-or-self::h1[@class and contains(concat(' ', normalize-space(@class), ' '), ' foo ')]", CssSelector::toXPath('h1.foo'));
         $this->assertEquals('descendant-or-self::foo:h1', CssSelector::toXPath('foo|h1'));

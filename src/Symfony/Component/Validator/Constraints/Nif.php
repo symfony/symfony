d02c280a313fb4b04f3e9bf0a9acdae94aa27d14
markitosgv <marcosmgv@gmail.com>
2013-06-21 12:19:46 +0200

    Update Nif.php
    update to symfony standars

--- src/Symfony/Component/Validator/Constraints/Nif.php
+++ src/Symfony/Component/Validator/Constraints/Nif.php
@@ -1,15 +1,22 @@
 <?php
 
-namespace Symfony\Component\Validator\Constraints;
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+namespace Symfony\Component\Validator\Constraints;
 use Symfony\Component\Validator\Constraint;
 
 /**
-* @Annotation
-*/
-class Nif extends Constraint
-{
-
-public $message = 'This DNI/NIF doesn´t seem to be valid.';
-
+ * @Annotation
+ *
+ * @api
+ */
+class Nif extends Constraint{
+    public $message = 'This DNI/NIF doesn´t seem to be valid.';
 }

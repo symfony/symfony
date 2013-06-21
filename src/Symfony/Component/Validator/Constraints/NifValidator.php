3e87742a126c52509fb52709c0e274382c1a4f88
markitosgv <marcosmgv@gmail.com>
2013-06-21 12:26:20 +0200

    Update NifValidator.php

--- src/Symfony/Component/Validator/Constraints/NifValidator.php
+++ src/Symfony/Component/Validator/Constraints/NifValidator.php
@@ -28,19 +28,19 @@ class NifValidator extends ConstraintValidator {
 	 * @param Constraint $constraint
 	 */
 	public function validate($value, Constraint $constraint) {
-        if (null === $value || '' === $value) {
-            return;
-        }
-        $letters = 'TRWAGMYFPDXBNJZSQVHLCKE';
-        $letter = strtoupper(substr($value,8,1));
-        $nif = strotoupper(substr($value,0,8));
-        $nif = str_replace('X','0',$nif);
-        $nif = str_replace('Y','1',$nif);
-        $nif = str_replace('Z','2',$nif);
-        $nif -= intval($nif/23) * 23;
-	if ($letters{$nif} != $letter) {
-		$this->context->addViolation($constraint->message);
-	}
+	        if (null === $value || '' === $value) {
+	            return;
+	        }
+	        $letters = 'TRWAGMYFPDXBNJZSQVHLCKE';
+	        $letter = strtoupper(substr($value,8,1));
+	        $nif = strotoupper(substr($value,0,8));
+	        $nif = str_replace('X','0',$nif);
+	        $nif = str_replace('Y','1',$nif);
+	        $nif = str_replace('Z','2',$nif);
+	        $nif -= intval($nif/23) * 23;
+		if ($letters{$nif} != $letter) {
+			$this->context->addViolation($constraint->message);
+		}
 	
 	}
 

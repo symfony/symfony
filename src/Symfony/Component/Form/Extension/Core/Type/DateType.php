fce20a1416fa01b1f76cf37ea47ea1d3458d5eeb
Remi <remibaar@gmail.com>
2013-08-02 13:49:21 +0200

    Correct DateTimZone object for IntlDateFormatter::setTimeZone() using PHP5.5
    A timezone object should be passed when using the new setTimeZone() function. DateTimeZone::UTC will return the TimeZoneId, so this is not suitable and will throw errors. A DateTimeZone object should be created and be passed.

--- src/Symfony/Component/Form/Extension/Core/Type/DateType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/DateType.php
@@ -248,7 +248,7 @@ class DateType extends AbstractType
         $timezone = $formatter->getTimezoneId();
 
         if (version_compare(\PHP_VERSION, '5.5.0-dev', '>=')) {
-            $formatter->setTimeZone(\DateTimeZone::UTC);
+            $formatter->setTimeZone(new \DateTimeZone('UTC'));
         } else {
             $formatter->setTimeZoneId(\DateTimeZone::UTC);
         }

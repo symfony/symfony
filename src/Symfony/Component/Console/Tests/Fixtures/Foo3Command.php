8c246ac5359c06ca7b291f0444b561e7516eba08
Grégoire Pineau <lyrixx@lyrixx.info>
2013-08-25 15:33:45 +0200

    [Console] Escape exception message during the rendering of an exception

--- src/Symfony/Component/Console/Tests/Fixtures/Foo3Command.php
+++ src/Symfony/Component/Console/Tests/Fixtures/Foo3Command.php
@@ -17,7 +17,7 @@ class Foo3Command extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         try {
-            throw new \Exception("First exception");
+            throw new \Exception("First exception <p>this is html</p>");
         } catch (\Exception $e) {
             throw new \Exception("Second exception", 0, $e);
         }

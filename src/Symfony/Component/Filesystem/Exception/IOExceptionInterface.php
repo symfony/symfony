1d0fbdb4b5549d283e5d11d02257b3202c4388be
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-09-01 14:05:37 +0200

    Update IOExceptionInterface.php

--- src/Symfony/Component/Filesystem/Exception/IOExceptionInterface.php
+++ src/Symfony/Component/Filesystem/Exception/IOExceptionInterface.php
@@ -1,2 +1,27 @@
 <?php
-/** To be written **/
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Filesystem\Exception;
+
+/**
+ * IOException interface for all exceptions thrown by the component.
+ *
+ * @author Christian Gärtner <christiangaertner.film@googlemail.com>
+ *
+ * @api
+ */
+interface IOExceptionInterface
+{
+    /**
+     * Returns the associated path for the exception
+     */
+    public function getPath();
+}

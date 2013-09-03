e369d14a2ce53d9cda80d7d05d47a26ca260bcaa
Fabien Potencier <fabien.potencier@gmail.com>
2013-09-03 22:06:29 +0200

    added a Twig extension to create Expression instances

---
+++ src/Symfony/Bridge/Twig/Extension/ExpressionExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bridge\Twig\Extension;
+
+use Symfony\Component\ExpressionLanguage\Expression;
+
+/**
+ * ExpressionExtension gives a way to create Expressions from a template.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class ExpressionExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('expression', array($this, 'createExpression')),
+        );
+    }
+
+    private function createExpression($expression)
+    {
+        return new Expression($expression);
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'expression';
+    }
+}

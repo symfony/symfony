f6c12bd6de732d0c1b15a084e3b8e180519277e2
Tobias Schultze <webmaster@tubo-world.de>
2013-09-04 14:01:22 +0200

    [Templating] fix logic regarding template references and many phpdocs

--- src/Symfony/Bundle/TwigBundle/Tests/Loader/FilesystemLoaderTest.php
+++ src/Symfony/Bundle/TwigBundle/Tests/Loader/FilesystemLoaderTest.php
@@ -11,11 +11,9 @@
 
 namespace Symfony\Bundle\TwigBundle\Tests\Loader;
 
-use Symfony\Bundle\TwigBundle\Tests\TestCase;
-use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
-use Symfony\Component\Config\FileLocatorInterface;
 use Symfony\Bundle\FrameworkBundle\Templating\TemplateReference;
-use Symfony\Component\Templating\TemplateNameParserInterface;
+use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
+use Symfony\Bundle\TwigBundle\Tests\TestCase;
 
 class FilesystemLoaderTest extends TestCase
 {

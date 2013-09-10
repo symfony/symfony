67ba1314580ad63cbbf923b59157f226210d62ce
Bernhard Schussek <bschussek@gmail.com>
2013-09-10 12:42:12 +0200

    [DoctrineBridge] Improved test coverage of EntityChoiceList

---
+++ src/Symfony/Bridge/Doctrine/Tests/Form/ChoiceList/UnloadedEntityChoiceListSingleStringIdTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bridge\Doctrine\Tests\Form\ChoiceList;
+
+/**
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class UnloadedEntityChoiceListSingleStringIdTest extends AbstractEntityChoiceListSingleStringIdTest
+{
+    public function testGetIndicesForValuesIgnoresNonExistingValues()
+    {
+        $this->markTestSkipped('Non-existing values are not detected for unloaded choice lists.');
+    }
+}

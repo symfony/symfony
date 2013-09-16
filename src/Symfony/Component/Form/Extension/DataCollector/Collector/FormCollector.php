a994a5d4104652c641cc1346af36445656383c7a
Robert Schönthal <caziel@gmx.net>
2013-09-16 09:26:48 +0200

    [Form] Merged subsriber/collector, also collect valid forms

--- src/Symfony/Component/Form/Extension/DataCollector/Collector/FormCollector.php
+++ src/Symfony/Component/Form/Extension/DataCollector/Collector/FormCollector.php
@@ -11,18 +11,29 @@
 
 namespace Symfony\Component\Form\Extension\DataCollector\Collector;
 
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector as BaseCollector;
 
 /**
- * DataCollector for Form Validation Failures.
+ * DataCollector for Form Validation.
  *
  * @author Robert Schönthal <robert.schoenthal@gmail.com>
  */
-class FormCollector extends BaseCollector
+class FormCollector extends BaseCollector implements EventSubscriberInterface
 {
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(FormEvents::POST_SUBMIT => array('collectForm', -255));
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -32,15 +43,47 @@ class FormCollector extends BaseCollector
     }
 
     /**
+     * Collects Form-Validation-Data and adds them to the Collector.
+     *
+     * @param FormEvent $event The event object
+     */
+    public function collectForm(FormEvent $event)
+    {
+        $form = $event->getForm();
+
+        if ($form->isRoot()) {
+            $this->data[$form->getName()] = array();
+            $this->addForm($form);
+        }
+    }
+
+    /**
+     * Adds an Form-Element to the Collector.
+     *
+     * @param FormInterface $form
+     */
+    private function addForm(FormInterface $form)
+    {
+        if ($form->getErrors()) {
+            $this->addError($form);
+        }
+
+        // recursively add all child-errors
+        foreach ($form->all() as $field) {
+            $this->addForm($field);
+        }
+    }
+
+    /**
      * Adds a Form-Error to the Collector.
      *
      * @param FormInterface $form
      */
-    public function addError(FormInterface $form)
+    private function addError(FormInterface $form)
     {
         $storeData = array(
             'root'   => $form->getRoot()->getName(),
-            'name'   => (string)$form->getPropertyPath(),
+            'name'   => (string) $form->getPropertyPath(),
             'type'   => $form->getConfig()->getType()->getName(),
             'errors' => $form->getErrors(),
             'value'  => $this->varToString($form->getViewData())
@@ -68,12 +111,20 @@ class FormCollector extends BaseCollector
     }
 
     /**
-     * Returns the number of invalid Forms.
+     * Returns the number of Forms with Errors.
      *
      * @return integer
      */
-    public function getDataCount()
+    public function getErrorCount()
     {
-        return count($this->data);
+        $errorCount = 0;
+
+        foreach ($this->data as $form) {
+            if (count($form)) {
+                $errorCount++;
+            }
+        }
+
+        return $errorCount;
     }
 }
--- src/Symfony/Component/Form/Tests/Extension/DataCollector/Collector/FormCollectorTest.php
+++ src/Symfony/Component/Form/Tests/Extension/DataCollector/Collector/FormCollectorTest.php
@@ -11,15 +11,23 @@
 namespace Symfony\Component\Form\Tests\Extension\DataCollector\Collector;
 
 use Symfony\Component\Form\Extension\DataCollector\Collector\FormCollector;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
-/**
- * @covers Symfony\Component\Form\Extension\DataCollector\Collector\FormCollector
- */
 class FormCollectorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAddError()
+    public function testSubscribedEvents()
+    {
+        $events = FormCollector::getSubscribedEvents();
+
+        $this->assertInternalType('array', $events);
+        $this->assertEquals(array(FormEvents::POST_SUBMIT => array('collectForm', -255)), $events);
+    }
+
+    public function testCollect()
     {
         $form = $this->getMock('Symfony\Component\Form\Test\FormInterface');
+        $subForm = $this->getMock('Symfony\Component\Form\Test\FormInterface');
 
         $type = $this->getMock('Symfony\Component\Form\FormTypeInterface');
         $type->expects($this->atLeastOnce())->method('getName')->will($this->returnValue('fizz'));
@@ -27,19 +35,23 @@ class FormCollectorTest extends \PHPUnit_Framework_TestCase
         $config = $this->getMock('Symfony\Component\Form\FormConfigInterface');
         $config->expects($this->atLeastOnce())->method('getType')->will($this->returnValue($type));
 
-        $form->expects($this->atLeastOnce())->method('getRoot')->will($this->returnSelf());
-        $form->expects($this->atLeastOnce())->method('getPropertyPath')->will($this->returnValue('bar'));
+        $form->expects($this->atLeastOnce())->method('all')->will($this->returnValue(array($subForm)));
+        $form->expects($this->atLeastOnce())->method('isRoot')->will($this->returnValue(true));
         $form->expects($this->atLeastOnce())->method('getName')->will($this->returnValue('foo'));
-        $form->expects($this->atLeastOnce())->method('getViewData')->will($this->returnValue('bazz'));
-        $form->expects($this->atLeastOnce())->method('getErrors')->will($this->returnValue(array('foo')));
-        $form->expects($this->atLeastOnce())->method('getConfig')->will($this->returnValue($config));
 
+        $subForm->expects($this->atLeastOnce())->method('all')->will($this->returnValue(array()));
+        $subForm->expects($this->atLeastOnce())->method('getErrors')->will($this->returnValue(array('foo')));
+        $subForm->expects($this->atLeastOnce())->method('getRoot')->will($this->returnValue($form));
+        $subForm->expects($this->atLeastOnce())->method('getConfig')->will($this->returnValue($config));
+        $subForm->expects($this->atLeastOnce())->method('getPropertyPath')->will($this->returnValue('bar'));
+        $subForm->expects($this->atLeastOnce())->method('getViewData')->will($this->returnValue('bazz'));
 
+        $event = new FormEvent($form, array());
         $c = new FormCollector();
-        $c->addError($form);
+        $c->collectForm($event);
 
         $this->assertInternalType('array', $c->getData());
-        $this->assertEquals(1, $c->getDataCount());
+        $this->assertEquals(1, $c->getErrorCount());
         $this->assertEquals(array('foo' => array('bar' => array('value' => 'bazz', 'root' => 'foo', 'type' => 'fizz', 'name' => 'bar', 'errors' => array('foo')))), $c->getData());
     }
 }

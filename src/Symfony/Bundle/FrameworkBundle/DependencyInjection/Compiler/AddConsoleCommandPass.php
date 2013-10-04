91951bc1958e4c61488ffd43d3d75eb1a74d0830
Grégoire Pineau <lyrixx@lyrixx.info>
2013-10-04 13:43:28 +0200

    Added tests for AddConsoleCommandPass

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/AddConsoleCommandPass.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/AddConsoleCommandPass.php
@@ -26,10 +26,20 @@ class AddConsoleCommandPass implements CompilerPassInterface
         $commandServices = $container->findTaggedServiceIds('console.command');
 
         foreach ($commandServices as $id => $tags) {
-            $class = $container->getParameterBag()->resolveValue($container->getDefinition($id)->getClass());
+            $definition = $container->getDefinition($id);
+
+            if (!$definition->isPublic()) {
+                throw new \InvalidArgumentException(sprintf('The service "%s" tagged "console.command" must be public.', $id));
+            }
+
+            if ($definition->isAbstract()) {
+                throw new \InvalidArgumentException(sprintf('The service "%s" tagged "console.command" must not be abstract.', $id));
+            }
+
+            $class = $container->getParameterBag()->resolveValue($definition->getClass());
             $r = new \ReflectionClass($class);
             if (!$r->isSubclassOf('Symfony\\Component\\Console\\Command\\Command')) {
-                throw new \InvalidArgumentException(sprintf('The service "%s" tagged "console.command" must be a subclass of "Symfony\\Component\\Console\\Command\\Command"', $id));
+                throw new \InvalidArgumentException(sprintf('The service "%s" tagged "console.command" must be a subclass of "Symfony\\Component\\Console\\Command\\Command".', $id));
             }
             $container->setAlias('console.command.'.strtolower(str_replace('\\', '_', $class)), $id);
         }

7a637c4249bf398c65dfae4ae1ef14547a7b9046
Ione Souza Junior <junior@ionixjunior.com.br>
2013-10-05 09:40:08 -0300

    Fixed copyright.

--- src/Symfony/Component/Form/Tests/Extension/Core/Type/PercentTypeTest.php
+++ src/Symfony/Component/Form/Tests/Extension/Core/Type/PercentTypeTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Symfony package.
  *
- * (c) Ione Souza Junior <junior@ionixjunior.com.br>
+ * (c) Fabien Potencier <fabien@symfony.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.

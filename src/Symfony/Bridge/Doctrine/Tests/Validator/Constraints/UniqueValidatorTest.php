af98688f94e89d6ea6d85a2152a7915005a583bb
Thomas Tourlourat <thomas@tourlourat.com>
2013-11-19 15:35:34 +0100

    No Entity Manager defined exception

--- src/Symfony/Bridge/Doctrine/Tests/Validator/Constraints/UniqueValidatorTest.php
+++ src/Symfony/Bridge/Doctrine/Tests/Validator/Constraints/UniqueValidatorTest.php
@@ -367,4 +367,50 @@ class UniqueValidatorTest extends \PHPUnit_Framework_TestCase
         );
         $violationsList = $validator->validate($associated);
     }
+
+    public function testDedicatedEntityManagerNullObject()
+    {
+        $uniqueFields = array('name');
+        $entityManagerName = 'foo'; 
+
+        $registry = $this->getMock('Doctrine\Common\Persistence\ManagerRegistry');
+
+        $constraint = new UniqueEntity(array(
+            'fields' => $uniqueFields,
+            'em' => $entityManagerName,
+        ));
+
+        $uniqueValidator = new UniqueEntityValidator($registry);
+
+        $entity = new SingleIntIdEntity(1, null);
+
+        $this->setExpectedException(
+            'Symfony\Component\Validator\Exception\ConstraintDefinitionException',
+            'Object manager "foo" does not exist.'
+        );
+
+        $uniqueValidator->validate($entity, $constraint);
+    }
+
+    public function testEntityManagerNullObject()
+    {
+        $uniqueFields = array('name');
+
+        $registry = $this->getMock('Doctrine\Common\Persistence\ManagerRegistry');
+
+        $constraint = new UniqueEntity(array(
+            'fields' => $uniqueFields,
+        ));
+
+        $uniqueValidator = new UniqueEntityValidator($registry);
+
+        $entity = new SingleIntIdEntity(1, null);
+
+        $this->setExpectedException(
+            'Symfony\Component\Validator\Exception\ConstraintDefinitionException',
+            'Unable to find the object manager associated with an entity of class "Symfony\Bridge\Doctrine\Tests\Fixtures\SingleIntIdEntity"'
+        );
+
+        $uniqueValidator->validate($entity, $constraint);
+    }
 }

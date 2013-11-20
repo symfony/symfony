8d32c9cd0b4f5154ff578dbe9e46a2df6d78bedc
Douglas Greenshields <dgreenshields@gmail.com>
2013-11-20 18:57:15 +0000

    [Validator] fix docblock typos

--- src/Symfony/Component/Validator/ConstraintValidatorFactory.php
+++ src/Symfony/Component/Validator/ConstraintValidatorFactory.php
@@ -18,7 +18,7 @@ use Symfony\Component\Validator\Constraint;
  * Default implementation of the ConstraintValidatorFactoryInterface.
  *
  * This enforces the convention that the validatedBy() method on any
- * Constrain will return the class name of the ConstraintValidator that
+ * Constraint will return the class name of the ConstraintValidator that
  * should validate the Constraint.
  */
 class ConstraintValidatorFactory implements ConstraintValidatorFactoryInterface

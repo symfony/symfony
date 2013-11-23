e50b0f43da99c0435154b71eb230a252e807982f
Rodrigo Díez Villamuera <rodrigo.diez@netropy.es>
2013-11-23 13:18:35 +0100

    Break statements removed. Return is used

--- src/Symfony/Component/Serializer/Encoder/JsonEncoder.php
+++ src/Symfony/Component/Serializer/Encoder/JsonEncoder.php
@@ -106,22 +106,16 @@ class JsonEncoder implements EncoderInterface, DecoderInterface
         switch (json_last_error()) {
             case JSON_ERROR_DEPTH:
                 return 'Maximum stack depth exceeded';
-                break;
             case JSON_ERROR_STATE_MISMATCH:
                 return 'Underflow or the modes mismatch';
-                break;
             case JSON_ERROR_CTRL_CHAR:
                 return 'Unexpected control character found';
-                break;
             case JSON_ERROR_SYNTAX:
                 return 'Syntax error, malformed JSON';
-                break;
             case JSON_ERROR_UTF8:
                 return 'Malformed UTF-8 characters, possibly incorrectly encoded';
-                break;
             default:
                 return 'Unknown error';
-                break;
         }
     }
 }

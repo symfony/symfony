a20b9a410317bb70d97854943197081867490da5
Fabien Potencier <fabien.potencier@gmail.com>
2013-12-03 18:55:28 +0100

    bumped Symfony version to 2.4.1

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -60,12 +60,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION         = '2.4.0';
-    const VERSION_ID      = '20400';
+    const VERSION         = '2.4.1-DEV';
+    const VERSION_ID      = '20401';
     const MAJOR_VERSION   = '2';
     const MINOR_VERSION   = '4';
-    const RELEASE_VERSION = '0';
-    const EXTRA_VERSION   = '';
+    const RELEASE_VERSION = '1';
+    const EXTRA_VERSION   = 'DEV';
 
     /**
      * Constructor.

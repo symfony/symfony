3ece6300aed9fab6be0cda8d02305077cbc744ea
artem kolesnikov <tyomo4ka@gmail.com>
2013-12-16 23:03:11 +0200

    Fixed issue in BaseDateTimeTransformer when invalid timezone cause Transformation filed exception (closes #9403).

---
+++ src/Symfony/Component/Form/Tests/Extension/Core/DataTransformer/BaseDateTimeTransformerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Form\Tests\Extension\Core\DataTransformer;
+
+class BaseDateTimeTransformerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \Symfony\Component\Form\Exception\InvalidArgumentException
+     * @expectedExceptionMessage this_timezone_does_not_exist
+     */
+    public function testConstructFailsIfInputTimezoneIsInvalid()
+    {
+        $this->getMock(
+            'Symfony\Component\Form\Extension\Core\DataTransformer\BaseDateTimeTransformer',
+            array(),
+            array('this_timezone_does_not_exist')
+        );
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Form\Exception\InvalidArgumentException
+     * @expectedExceptionMessage that_timezone_does_not_exist
+     */
+    public function testConstructFailsIfOutputTimezoneIsInvalid()
+    {
+        $this->getMock(
+            'Symfony\Component\Form\Extension\Core\DataTransformer\BaseDateTimeTransformer',
+            array(),
+            array(null, 'that_timezone_does_not_exist')
+        );
+    }
+}

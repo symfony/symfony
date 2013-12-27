e3e28fc44e2e327f949df1ef681ffb15d2cd4f33
Gildas Quéméner <gildas.quemener@gmail.com>
2013-12-27 14:23:44 +0100

    [FramworkBundle] Fixed CS

--- src/Symfony/Bundle/FrameworkBundle/Tests/EventListener/AddRequestFormatsListenerTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/EventListener/AddRequestFormatsListenerTest.php
@@ -45,9 +45,7 @@ class AddRequestFormatsListenerTest extends \PHPUnit_Framework_TestCase
     public function testRegisteredEvent()
     {
         $this->assertEquals(
-            array(
-                KernelEvents::REQUEST => 'onKernelRequest',
-            ),
+            array(KernelEvents::REQUEST => 'onKernelRequest'),
             AddRequestFormatsListener::getSubscribedEvents()
         );
     }

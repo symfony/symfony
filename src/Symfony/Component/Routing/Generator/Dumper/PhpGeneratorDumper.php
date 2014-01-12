2fbc833e698f83e0cb42253574153899a3172796
cmfcmf <cmfcmf@users.noreply.github.com>
2014-01-12 11:22:19 +0100

    Coding standards
    static MUST be declared after the visibility (PSR-2)

--- src/Symfony/Component/Routing/Generator/Dumper/PhpGeneratorDumper.php
+++ src/Symfony/Component/Routing/Generator/Dumper/PhpGeneratorDumper.php
@@ -57,7 +57,7 @@ use Psr\Log\LoggerInterface;
  */
 class {$options['class']} extends {$options['base_class']}
 {
-    static private \$declaredRoutes = {$this->generateDeclaredRoutes()};
+    private static \$declaredRoutes = {$this->generateDeclaredRoutes()};
 
     /**
      * Constructor.

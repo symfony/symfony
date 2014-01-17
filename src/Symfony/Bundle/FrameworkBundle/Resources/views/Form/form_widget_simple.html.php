82f98c4fb388f1ec919d2d074d38105c613c4d0a
Dariusz Czech <dczech@gmail.com>
2014-01-17 17:40:59 +0100

    [Form] fixed allow render 0 and 0.0 numeric input values

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_widget_simple.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_widget_simple.html.php
@@ -1,5 +1,5 @@
 <input
     type="<?php echo isset($type) ? $view->escape($type) : 'text' ?>"
-    <?php if (!empty($value)): ?>value="<?php echo $view->escape($value) ?>"<?php endif ?>
+    <?php if (!empty($value) || is_numeric($value)): ?>value="<?php echo $view->escape($value) ?>"<?php endif ?>
     <?php echo $view['form']->block($form, 'widget_attributes') ?>
 />

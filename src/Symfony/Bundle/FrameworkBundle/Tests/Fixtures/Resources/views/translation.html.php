a8a40fcb9edcd514206526cecf951cae95453a97
Michael Tibben <michael.tibben@99designs.com>
2014-02-04 19:26:21 +1100

    [FrameworkBundle] PhpExtractor bugfix and improvements

--- src/Symfony/Bundle/FrameworkBundle/Tests/Fixtures/Resources/views/translation.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Fixtures/Resources/views/translation.html.php
@@ -1,3 +1,33 @@
 This template is used for translation message extraction tests
 <?php echo $view['translator']->trans('single-quoted key') ?>
 <?php echo $view['translator']->trans("double-quoted key") ?>
+<?php echo $view['translator']->trans(<<<EOF
+heredoc key
+EOF
+) ?>
+<?php echo $view['translator']->trans(<<<'EOF'
+nowdoc key
+EOF
+) ?>
+<?php echo $view['translator']->trans(
+    "double-quoted key with whitespace and escaped \$\n\" sequences"
+) ?>
+<?php echo $view['translator']->trans(
+    'single-quoted key with whitespace and nonescaped \$\n\' sequences'
+) ?>
+<?php echo $view['translator']->trans( <<<EOF
+heredoc key with whitespace and escaped \$\n sequences
+EOF
+) ?>
+<?php echo $view['translator']->trans( <<<'EOF'
+nowdoc key with whitespace and nonescaped \$\n sequences
+EOF
+) ?>
+
+<?php echo $view['translator']->trans('single-quoted key with "quote mark at the end"') ?>
+
+<?php echo $view['translator']->transChoice(
+    '{0} There is no apples|{1} There is one apple|]1,Inf[ There are %count% apples',
+    10,
+    array('%count%' => 10)
+) ?>

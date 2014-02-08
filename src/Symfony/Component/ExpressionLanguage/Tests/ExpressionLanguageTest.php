2ef9efb16b0c3cdf460d9db8f6822e5a443f53a7
inalgnu <inal.djafar@sensiolabs.com>
2014-02-08 17:10:48 +0100

    [ExpressionLanguage][Tests] add tests for register functions

--- src/Symfony/Component/ExpressionLanguage/Tests/ExpressionLanguageTest.php
+++ src/Symfony/Component/ExpressionLanguage/Tests/ExpressionLanguageTest.php
@@ -44,4 +44,30 @@ class ExpressionLanguageTest extends \PHPUnit_Framework_TestCase
         $parsedExpression = $expressionLanguage->parse('1 + 1', array());
         $this->assertSame($savedParsedExpression, $parsedExpression);
     }
+
+    public function testRegisterFunctions()
+    {
+        $expressionLanguage = new ExpressionLanguage();
+        $functions = $this->getFunctions($expressionLanguage);
+
+        $this->assertArrayHasKey('constant', $functions);
+        $this->assertArrayHasKey('compiler', $functions['constant']);
+        $this->assertArrayHasKey('evaluator', $functions['constant']);
+
+        $this->assertTrue(is_callable($functions['constant']['compiler']));
+        $this->assertTrue(is_callable($functions['constant']['evaluator']));
+
+        $this->assertEquals($functions['constant']['compiler']('expression_language_foo'), 'constant(expression_language_foo)');
+        define('expression_language_foo', 'foo');
+        $this->assertEquals($functions['constant']['evaluator']('expression_language_foo'), 'foo');
+    }
+
+    private function getFunctions($obj)
+    {
+        $class = new \ReflectionClass('Symfony\Component\ExpressionLanguage\ExpressionLanguage');
+        $property = $class->getProperty('functions');
+        $property->setAccessible(true);
+
+        return $property->getValue($obj);
+    }
 }

34f4ef529711ffcf2e597a379c27a3cadf629915
Grégoire Pineau <lyrixx@lyrixx.info>
2014-02-13 15:09:55 +0100

    [FrameworkBundle][Console] Load command from DIC after command from bundles.
    It's better to load a command from DIC after other one. Because it
    allow easy override of default (symfony/assetic/doctrine) command.
    The end user could do:
    
        # config.yml
        services:
            assetic.command.dump:
                class: SensioLabs\Shim\Assetic\DumpCommand
                tags:
                    - { name: console.command }

--- src/Symfony/Bundle/FrameworkBundle/Console/Application.php
+++ src/Symfony/Bundle/FrameworkBundle/Console/Application.php
@@ -100,15 +100,15 @@ class Application extends BaseApplication
     {
         $container = $this->kernel->getContainer();
 
-        if ($container->hasParameter('console.command.ids')) {
-            foreach ($container->getParameter('console.command.ids') as $id) {
-                $this->add($container->get($id));
-            }
-        }
-
         foreach ($this->kernel->getBundles() as $bundle) {
             if ($bundle instanceof Bundle) {
                 $bundle->registerCommands($this);
+            }
+        }
+
+        if ($container->hasParameter('console.command.ids')) {
+            foreach ($container->getParameter('console.command.ids') as $id) {
+                $this->add($container->get($id));
             }
         }
     }

e3ea1599b460e2f6b958b22ae17380bf1fddd44a
Eric Hough <eric@tubepress.org>
2014-02-13 13:36:51 -0500

    [DependencyInjection] remove missing-service suggestions
    This is a rollback of 729db0fde: a change which introduced a significant performance penalty for little gain. During normal use of ContainerBuilder, levenshtein() is called many times each time a service is requested for the first time. In a container with just 20 services, for example, this will result in 400 calls to levenshtein(). That's a significant performance hit which only serves to provide some convenience for the comparatively rare situation when a developer asks for a truly non-existent service.

--- src/Symfony/Component/DependencyInjection/Exception/ServiceNotFoundException.php
+++ src/Symfony/Component/DependencyInjection/Exception/ServiceNotFoundException.php
@@ -21,21 +21,12 @@ class ServiceNotFoundException extends InvalidArgumentException
     private $id;
     private $sourceId;
 
-    public function __construct($id, $sourceId = null, \Exception $previous = null, array $alternatives = array())
+    public function __construct($id, $sourceId = null, \Exception $previous = null)
     {
         if (null === $sourceId) {
             $msg = sprintf('You have requested a non-existent service "%s".', $id);
         } else {
             $msg = sprintf('The service "%s" has a dependency on a non-existent service "%s".', $sourceId, $id);
-        }
-
-        if ($alternatives) {
-            if (1 == count($alternatives)) {
-                $msg .= ' Did you mean this: "';
-            } else {
-                $msg .= ' Did you mean one of these: "';
-            }
-            $msg .= implode('", "', $alternatives).'"?';
         }
 
         parent::__construct($msg, 0, $previous);

4ee7a84be226b1e951724fc50d0adaee16668a0a
Diego Agulló <aeoris@gmail.com>
2014-02-15 19:45:04 +0100

    CS fixes.

--- src/Symfony/Component/Validator/Constraints/CollectionValidator.php
+++ src/Symfony/Component/Validator/Constraints/CollectionValidator.php
@@ -14,7 +14,6 @@ namespace Symfony\Component\Validator\Constraints;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
-use Symfony\Component\Validator\Constraints\Optional;
 
 /**
  * @author Bernhard Schussek <bschussek@gmail.com>

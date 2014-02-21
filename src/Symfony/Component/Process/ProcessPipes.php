1d3702df54a374d323634f278d3e167c8be6979d
Andrey Ryaguzov <dev.aeryaguzov@gmail.com>
2014-02-21 11:48:08 +0400

    [Process] fix docs

--- src/Symfony/Component/Process/ProcessPipes.php
+++ src/Symfony/Component/Process/ProcessPipes.php
@@ -218,6 +218,8 @@ class ProcessPipes
     /**
      * Reads data in file handles.
      *
+     * @param Boolean $close Whether to close file handles or not.
+     *
      * @return array An array of read data indexed by their fd.
      */
     private function readFileHandles($close = false)
@@ -253,6 +255,7 @@ class ProcessPipes
      * Reads data in file pipes streams.
      *
      * @param Boolean $blocking Whether to use blocking calls or not.
+     * @param Boolean $close    Whether to close file handles or not.
      *
      * @return array An array of read data indexed by their fd.
      */

9986f03ce25143bb358f3367ecb6a56b7d6d6743
Bernhard Schussek <bschussek@gmail.com>
2014-02-21 16:10:43 +0100

    [Validator] Added inline documentation for the PropertyPath utility class

--- src/Symfony/Component/Validator/Util/PropertyPath.php
+++ src/Symfony/Component/Validator/Util/PropertyPath.php
@@ -12,11 +12,29 @@
 namespace Symfony\Component\Validator\Util;
 
 /**
- * @since  %%NextVersion%%
+ * Contains utility methods for dealing with property paths.
+ *
+ * For more extensive functionality, use Symfony's PropertyAccess component.
+ *
+ * @since  2.5
  * @author Bernhard Schussek <bschussek@gmail.com>
  */
 class PropertyPath
 {
+    /**
+     * Appends a path to a given property path.
+     *
+     * If the base path is empty, the appended path will be returned unchanged.
+     * If the base path is not empty, and the appended path starts with a
+     * squared opening bracket ("["), the concatenation of the two paths is
+     * returned. Otherwise, the concatenation of the two paths is returned,
+     * separated by a dot (".").
+     *
+     * @param string $basePath The base path
+     * @param string $subPath  The path to append
+     *
+     * @return string The concatenation of the two property paths
+     */
     public static function append($basePath, $subPath)
     {
         if ('' !== (string) $subPath) {
@@ -30,6 +48,9 @@ class PropertyPath
         return $basePath;
     }
 
+    /**
+     * Not instantiable.
+     */
     private function __construct()
     {
     }

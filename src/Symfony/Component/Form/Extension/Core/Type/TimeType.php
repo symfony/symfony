a379298db9c7323780e45b7e80e84f94f0c41c59
Tugdual Saunier <tugdual.saunier@gmail.com>
2014-03-04 11:12:50 +0000

    [Form] Automatically add step attribute to HTML5 time widgets to display seconds if needed

--- src/Symfony/Component/Form/Extension/Core/Type/TimeType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/TimeType.php
@@ -140,6 +140,14 @@ class TimeType extends AbstractType
 
         if ('single_text' === $options['widget']) {
             $view->vars['type'] = 'time';
+
+            // we need to force the browser to display the seconds by
+            // adding the HTML attribute step if not already defined.
+            // Otherwise the browser will not display and so not send the seconds
+            // therefore the value will always be considered as invalid.
+            if ($options['with_seconds'] && !isset($view->vars['attr']['step'])) {
+                $view->vars['attr']['step'] = 1;
+            }
         }
     }
 

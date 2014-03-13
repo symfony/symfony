7fc0c5f1fd684c48e3a4f71dc86513c5c3931e19
ureimers <ureimers@bimp.de>
2014-03-13 12:42:02 +0100

    Update DefaultAuthenticationSuccessHandler.php

--- src/Symfony/Component/Security/Http/Authentication/DefaultAuthenticationSuccessHandler.php
+++ src/Symfony/Component/Security/Http/Authentication/DefaultAuthenticationSuccessHandler.php
@@ -18,9 +18,6 @@ use Symfony\Component\Security\Http\HttpUtils;
 /**
  * Class with the default authentication success handling logic.
  *
- * Can be optionally be extended from by the developer to alter the behaviour
- * while keeping the default behaviour.
- *
  * @author Fabien Potencier <fabien@symfony.com>
  * @author Johannes M. Schmitt <schmittjoh@gmail.com>
  * @author Alexander <iam.asm89@gmail.com>

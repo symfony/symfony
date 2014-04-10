3e660ae3225e7b5764f40d7e6360720407bd4cf5
Mario Young <mayeco@users.noreply.github.com>
2014-04-09 20:17:01 -0400

    revert change
    revert change

--- src/Symfony/Component/Validator/Constraints/AbstractComparisonValidator.php
+++ src/Symfony/Component/Validator/Constraints/AbstractComparisonValidator.php
@@ -37,7 +37,7 @@ abstract class AbstractComparisonValidator extends ConstraintValidator
 
         if (!$this->compareValues($value, $constraint->value)) {
             $this->context->addViolation($constraint->message, array(
-                '{{ value }}' => $this->valueToString($value),
+                '{{ value }}' => $this->valueToString($constraint->value),
                 '{{ compared_value }}' => $this->valueToString($constraint->value),
                 '{{ compared_value_type }}' => $this->valueToType($constraint->value)
             ));

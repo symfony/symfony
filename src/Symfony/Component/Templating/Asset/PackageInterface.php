e0433297b28283686fd5b4475c4649cffb93e98b
ibasaw <ibasaw@gmail.com>
2014-04-18 13:56:04 +0200

    Update PackageInterface.php

--- src/Symfony/Component/Templating/Asset/PackageInterface.php
+++ src/Symfony/Component/Templating/Asset/PackageInterface.php
@@ -29,7 +29,7 @@ interface PackageInterface
      * Returns an absolute or root-relative public path.
      *
      * @param string              $path    A path
-     * @prama string|Boolean|null $version A specific version for the path
+     * @param string|Boolean|null $version A specific version for the path
      *
      * @return string The public path
      */

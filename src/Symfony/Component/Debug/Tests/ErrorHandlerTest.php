988a5dc937c1c45681d6c201f81e31b944d08873
Nicolas Grekas <nicolas.grekas@gmail.com>
2014-04-28 20:50:14 +0200

    [Debug] fix ErrorHandlerTest when context is not an array

--- src/Symfony/Component/Debug/Tests/ErrorHandlerTest.php
+++ src/Symfony/Component/Debug/Tests/ErrorHandlerTest.php
@@ -165,18 +165,18 @@ PHP
     public function testHandle()
     {
         $handler = ErrorHandler::register(0);
-        $this->assertFalse($handler->handle(0, 'foo', 'foo.php', 12, 'foo'));
+        $this->assertFalse($handler->handle(0, 'foo', 'foo.php', 12, array()));
 
         restore_error_handler();
 
         $handler = ErrorHandler::register(3);
-        $this->assertFalse($handler->handle(4, 'foo', 'foo.php', 12, 'foo'));
+        $this->assertFalse($handler->handle(4, 'foo', 'foo.php', 12, array()));
 
         restore_error_handler();
 
         $handler = ErrorHandler::register(3);
         try {
-            $handler->handle(111, 'foo', 'foo.php', 12, 'foo');
+            $handler->handle(111, 'foo', 'foo.php', 12, array());
         } catch (\ErrorException $e) {
             $this->assertSame('111: foo in foo.php line 12', $e->getMessage());
             $this->assertSame(111, $e->getSeverity());
@@ -187,12 +187,12 @@ PHP
         restore_error_handler();
 
         $handler = ErrorHandler::register(E_USER_DEPRECATED);
-        $this->assertTrue($handler->handle(E_USER_DEPRECATED, 'foo', 'foo.php', 12, 'foo'));
+        $this->assertTrue($handler->handle(E_USER_DEPRECATED, 'foo', 'foo.php', 12, array()));
 
         restore_error_handler();
 
         $handler = ErrorHandler::register(E_DEPRECATED);
-        $this->assertTrue($handler->handle(E_DEPRECATED, 'foo', 'foo.php', 12, 'foo'));
+        $this->assertTrue($handler->handle(E_DEPRECATED, 'foo', 'foo.php', 12, array()));
 
         restore_error_handler();
 
@@ -215,7 +215,7 @@ PHP
 
         $handler = ErrorHandler::register(E_USER_DEPRECATED);
         $handler->setLogger($logger);
-        $handler->handle(E_USER_DEPRECATED, 'foo', 'foo.php', 12, 'foo');
+        $handler->handle(E_USER_DEPRECATED, 'foo', 'foo.php', 12, array());
 
         restore_error_handler();
     }

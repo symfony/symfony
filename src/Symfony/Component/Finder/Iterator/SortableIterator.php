02cd235469777c809c7d5b66e09403de6958b7b4
prophet777 <johann_27@hotmail.fr>
2014-05-03 17:12:55 +0200

    Improve readability

--- src/Symfony/Component/Finder/Iterator/SortableIterator.php
+++ src/Symfony/Component/Finder/Iterator/SortableIterator.php
@@ -31,7 +31,7 @@ class SortableIterator implements \IteratorAggregate
      * Constructor.
      *
      * @param \Traversable $iterator The Iterator to filter
-     * @param int|callback $sort     The sort type (SORT_BY_NAME, SORT_BY_TYPE, or a PHP callback)
+     * @param int|callable $sort     The sort type (SORT_BY_NAME, SORT_BY_TYPE, or a PHP callback)
      *
      * @throws \InvalidArgumentException
      */
@@ -41,15 +41,7 @@ class SortableIterator implements \IteratorAggregate
 
         if (self::SORT_BY_NAME === $sort) {
             $this->sort = function ($a, $b) {
-
-                //strcmp returns < 0 if str1 is less than str2; > 0 if str1 is greater than str2, and 0 if they are equal.
-                $result = strcmp($a->getRealpath(), $b->getRealpath());
-
-                if ($result == 0) {
-                    return 0;
-                }
-
-                return $result < 0 ? -1 : 1;
+                return strcmp($a->getRealpath(), $b->getRealpath());
             };
         } elseif (self::SORT_BY_TYPE === $sort) {
             $this->sort = function ($a, $b) {
@@ -59,43 +51,24 @@ class SortableIterator implements \IteratorAggregate
                     return 1;
                 }
 
-                //strcmp returns < 0 if str1 is less than str2; > 0 if str1 is greater than str2, and 0 if they are equal.
-                $result = strcmp($a->getRealpath(), $b->getRealpath()) < 0 ? -1 : 1;
-
-                if ($result == 0) {
-                    return 0;
-                }
-
-                return $result < 0 ? -1 : 1;
+                return strcmp($a->getRealpath(), $b->getRealpath());
             };
         } elseif (self::SORT_BY_ACCESSED_TIME === $sort) {
             $this->sort = function ($a, $b) {
-                if ($a->getATime() === $b->getATime()) {
-                    return 0;
-                };
-
-                return ($a->getATime() > $b->getATime())  < 0 ? -1 : 1;
+                return ($a->getATime() - $b->getATime());
             };
         } elseif (self::SORT_BY_CHANGED_TIME === $sort) {
             $this->sort = function ($a, $b) {
-                if (($a->getCTime() === $b->getCTime())) {
-                    return 0;
-                }
-
-                return ($a->getCTime() > $b->getCTime()) < 0 ? -1 : 1;
+                return ($a->getCTime() - $b->getCTime());
             };
         } elseif (self::SORT_BY_MODIFIED_TIME === $sort) {
             $this->sort = function ($a, $b) {
-                if ($a->getMTime() === $b->getMTime()) {
-                    return 0;
-                }
-
-                return ($a->getMTime() > $b->getMTime())  < 0 ? -1 : 1;
+                return ($a->getMTime() - $b->getMTime());
             };
         } elseif (is_callable($sort)) {
             $this->sort = $sort;
         } else {
-            throw new \InvalidArgumentException('The SortableIterator takes a PHP callback or a valid built-in sort algorithm as an argument.');
+            throw new \InvalidArgumentException('The SortableIterator takes a PHP callable or a valid built-in sort algorithm as an argument.');
         }
     }
 

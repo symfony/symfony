e683c590a035d408be77b760ce08d6dadadf6904
prophet777 <johann_27@hotmail.fr>
2014-05-03 16:09:46 +0200

    Fix wrong implementation on callback comparator

--- src/Symfony/Component/Finder/Iterator/SortableIterator.php
+++ src/Symfony/Component/Finder/Iterator/SortableIterator.php
@@ -41,7 +41,9 @@ class SortableIterator implements \IteratorAggregate
 
         if (self::SORT_BY_NAME === $sort) {
             $this->sort = function ($a, $b) {
-                return strcmp($a->getRealpath(), $b->getRealpath());
+
+                //strcmp returns < 0 if str1 is less than str2; > 0 if str1 is greater than str2, and 0 if they are equal.
+                return strcmp($a->getRealpath(), $b->getRealpath()) < 0 ? -1 : 1;
             };
         } elseif (self::SORT_BY_TYPE === $sort) {
             $this->sort = function ($a, $b) {
@@ -51,19 +53,30 @@ class SortableIterator implements \IteratorAggregate
                     return 1;
                 }
 
-                return strcmp($a->getRealpath(), $b->getRealpath());
+                //strcmp returns < 0 if str1 is less than str2; > 0 if str1 is greater than str2, and 0 if they are equal.
+                return strcmp($a->getRealpath(), $b->getRealpath()) < 0 ? -1 : 1;
             };
         } elseif (self::SORT_BY_ACCESSED_TIME === $sort) {
             $this->sort = function ($a, $b) {
-                return ($a->getATime() > $b->getATime());
+                if($a->getATime() === $b->getATime()){
+                    return 0;
+                };
+                return ($a->getATime() > $b->getATime())  < 0 ? -1 : 1;
             };
         } elseif (self::SORT_BY_CHANGED_TIME === $sort) {
             $this->sort = function ($a, $b) {
-                return ($a->getCTime() > $b->getCTime());
+                if(($a->getCTime() === $b->getCTime())){
+                    return 0;
+                }
+                return ($a->getCTime() > $b->getCTime()) < 0 ? -1 : 1;
             };
         } elseif (self::SORT_BY_MODIFIED_TIME === $sort) {
             $this->sort = function ($a, $b) {
-                return ($a->getMTime() > $b->getMTime());
+                if($a->getMTime() === $b->getMTime()){
+                    return 0;
+                }
+
+                return ($a->getMTime() > $b->getMTime())  < 0 ? -1 : 1;
             };
         } elseif (is_callable($sort)) {
             $this->sort = $sort;

08bc4d920359a161409ca4d02d169cd8a66a7b9a
Fabien Potencier <fabien.potencier@gmail.com>
2014-05-22 18:17:54 +0200

    bumped Symfony version to 2.3.15

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -60,12 +60,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION         = '2.3.14';
-    const VERSION_ID      = '20314';
+    const VERSION         = '2.3.15-DEV';
+    const VERSION_ID      = '20315';
     const MAJOR_VERSION   = '2';
     const MINOR_VERSION   = '3';
-    const RELEASE_VERSION = '14';
-    const EXTRA_VERSION   = '';
+    const RELEASE_VERSION = '15';
+    const EXTRA_VERSION   = 'DEV';
 
     /**
      * Constructor.

c67db7e239f13477bdf0a23a65ae203fdd03b5e9
Chris Smith <chris@cs278.org>
2014-06-20 08:38:11 +0100

    Add tests for other aspects of the ExecutableFinder

--- src/Symfony/Component/Process/Tests/ExecutableFinderTest.php
+++ src/Symfony/Component/Process/Tests/ExecutableFinderTest.php
@@ -18,6 +18,76 @@ use Symfony\Component\Process\ExecutableFinder;
  */
 class ExecutableFinderTest extends \PHPUnit_Framework_TestCase
 {
+    private $path;
+
+    public function tearDown()
+    {
+        if ($this->path) {
+            // Restore path if it was changed.
+            putenv('PATH='.$this->path);
+        }
+    }
+
+    private function setPath($path)
+    {
+        $this->path = getenv('PATH');
+        putenv('PATH='.$path);
+    }
+
+    public function testFind()
+    {
+        if (!defined('PHP_BINARY')) {
+            $this->markTestSkipped('Requires the PHP_BINARY constant');
+        }
+
+        if (ini_get('open_basedir')) {
+            $this->markTestSkipped('Cannot test when open_basedir is set');
+        }
+
+        $this->setPath(dirname(PHP_BINARY));
+
+        $finder = new ExecutableFinder;
+        $result = $finder->find(basename(PHP_BINARY));
+
+        $this->assertEquals($result, PHP_BINARY);
+    }
+
+    public function testFindWithDefault()
+    {
+        if (ini_get('open_basedir')) {
+            $this->markTestSkipped('Cannot test when open_basedir is set');
+        }
+
+        $expected = 'defaultValue';
+
+        $this->setPath('');
+
+        $finder = new ExecutableFinder;
+        $result = $finder->find('foo', $expected);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testFindWithExtraDirs()
+    {
+        if (!defined('PHP_BINARY')) {
+            $this->markTestSkipped('Requires the PHP_BINARY constant');
+        }
+
+        if (ini_get('open_basedir')) {
+            $this->markTestSkipped('Cannot test when open_basedir is set');
+        }
+
+        $this->setPath('');
+
+        $extraDirs = array(dirname(PHP_BINARY));
+
+        $finder = new ExecutableFinder;
+        $result = $finder->find(basename(PHP_BINARY), null, $extraDirs);
+
+        $this->assertEquals(PHP_BINARY, $result);
+    }
+
     public function testFindWithOpenBaseDir()
     {
         if (!defined('PHP_BINARY')) {

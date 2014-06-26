994f81fd8696a95104b5c1e3817a82173b029b4c
Christophe Coevoet <stof@notk.org>
2014-06-27 01:18:27 +0200

    Refactored the CssSelector to remove the circular object graph
    This allows the translator and its extensions to be garbage collected
    based on the refcount rather than requiring the garbage collector run,
    making it much more likely to happen at the end of the
    CssSelector::toXPath call.

--- src/Symfony/Component/CssSelector/XPath/Extension/ExtensionInterface.php
+++ src/Symfony/Component/CssSelector/XPath/Extension/ExtensionInterface.php
@@ -24,6 +24,8 @@ interface ExtensionInterface
     /**
      * Returns node translators.
      *
+     * These callables will receive the node as first argument and the translator as second argument.
+     *
      * @return callable[]
      */
     public function getNodeTranslators();

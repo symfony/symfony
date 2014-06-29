cc058c9e2f698a6a4fce72c16767f6f97d8a3ff8
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2014-06-29 09:53:14 +0000

    [Translation:update][Xliff Dumper] added target-language.

--- src/Symfony/Component/Translation/Tests/Dumper/XliffFileDumperTest.php
+++ src/Symfony/Component/Translation/Tests/Dumper/XliffFileDumperTest.php
@@ -25,7 +25,7 @@ class XliffFileDumperTest extends \PHPUnit_Framework_TestCase
 
         $tempDir = sys_get_temp_dir();
         $dumper = new XliffFileDumper();
-        $dumper->dump($catalogue, array('path' => $tempDir));
+        $dumper->dump($catalogue, array('path' => $tempDir, 'default_locale' => 'fr'));
 
         $this->assertEquals(file_get_contents(__DIR__.'/../fixtures/resources-clean.xlf'), file_get_contents($tempDir.'/messages.en.xlf'));
 

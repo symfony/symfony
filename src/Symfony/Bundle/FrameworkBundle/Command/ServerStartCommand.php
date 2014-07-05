b601454448eeb770e5314a1f6250e48d82f917d2
Christian Flothmann <christian.flothmann@xabbuh.de>
2014-07-05 10:07:52 +0200

    new helper commands for PHP's built-in server

---
+++ src/Symfony/Bundle/FrameworkBundle/Command/ServerStartCommand.php
@@ -0,0 +1,155 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\FrameworkBundle\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+/**
+ * Runs PHP's built-in web server in a background process.
+ *
+ * @author Christian Flothmann <christian.flothmann@xabbuh.de>
+ */
+class ServerStartCommand extends ServerCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setDefinition(array(
+                new InputArgument('address', InputArgument::OPTIONAL, 'Address:port', '127.0.0.1:8000'),
+                new InputOption('docroot', 'd', InputOption::VALUE_REQUIRED, 'Document root', 'web/'),
+                new InputOption('router', 'r', InputOption::VALUE_REQUIRED, 'Path to custom router script'),
+            ))
+            ->setName('server:start')
+            ->setDescription('Starts PHP built-in web server in the background')
+            ->setHelp(<<<EOF
+The <info>%command.name%</info> runs PHP's built-in web server:
+
+  <info>%command.full_name%</info>
+
+To change the default bind address and the default port use the <info>address</info> argument:
+
+  <info>%command.full_name% 127.0.0.1:8080</info>
+
+To change the default document root directory use the <info>--docroot</info> option:
+
+  <info>%command.full_name% --docroot=htdocs/</info>
+
+If you have a custom document root directory layout, you can specify your own
+router script using the <info>--router</info> option:
+
+  <info>%command.full_name% --router=app/config/router.php</info>
+
+Specifying a router script is required when the used environment is not "dev" or
+"prod".
+
+See also: http://www.php.net/manual/en/features.commandline.webserver.php
+
+EOF
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $env = $this->getContainer()->getParameter('kernel.environment');
+
+        if ('prod' === $env) {
+            $output->writeln('<error>Running PHP built-in server in production environment is NOT recommended!</error>');
+        }
+
+        $pid = pcntl_fork();
+
+        if ($pid < 0) {
+            $output->writeln('<error>Unable to start the server process</error>');
+
+            return 1;
+        }
+
+        if ($pid > 0) {
+            $output->writeln('<info>Server started successfully</info>');
+
+            return;
+        }
+
+        if (posix_setsid() < 0) {
+            $output->writeln('<error>Unable to set the child process as session leader</error>');
+
+            return 1;
+        }
+
+        $process = $this->createServerProcess(
+            $input->getArgument('address'),
+            $input->getOption('docroot'),
+            $input->getOption('router'),
+            $env,
+            null
+        );
+        $process->disableOutput();
+        $process->start();
+        $lockFile = $this->getLockFile($input->getArgument('address'));
+        touch($lockFile);
+
+        if (!$process->isRunning()) {
+            $output->writeln('<error>Unable to start the server process</error>');
+            unlink($lockFile);
+
+            return 1;
+        }
+
+        // stop the web server when the lock file is removed
+        while ($process->isRunning()) {
+            if (!file_exists($lockFile)) {
+                $process->stop();
+            }
+
+            sleep(1);
+        }
+    }
+
+    /**
+     * Creates a process to start PHP's built-in web server.
+     *
+     * @param string $address      IP address and port to listen to
+     * @param string $documentRoot The application's document root
+     * @param string $router       The router filename
+     * @param string $env          The application environment
+     * @param int    $timeout      Process timeout
+     *
+     * @return Process The process
+     */
+    private function createServerProcess($address, $documentRoot, $router, $env, $timeout = null)
+    {
+        $router = $router ?: $this
+            ->getContainer()
+            ->get('kernel')
+            ->locateResource(sprintf('@FrameworkBundle/Resources/config/router_%s.php', $env))
+        ;
+        $script = implode(' ', array_map(array('Symfony\Component\Process\ProcessUtils', 'escapeArgument'), array(
+            PHP_BINARY,
+            '-S',
+            $address,
+            $router,
+        )));
+
+        return new Process('exec '.$script, $documentRoot, null, null, $timeout);
+    }
+}

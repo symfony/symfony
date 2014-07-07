c02f56f7948f2699c8cc8b118c7faeb13470e357
Fabien Potencier <fabien.potencier@gmail.com>
2014-07-07 14:59:36 +0200

    updated VERSION for 2.3.17

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -59,12 +59,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION         = '2.3.17-DEV';
+    const VERSION         = '2.3.17';
     const VERSION_ID      = '20317';
     const MAJOR_VERSION   = '2';
     const MINOR_VERSION   = '3';
     const RELEASE_VERSION = '17';
-    const EXTRA_VERSION   = 'DEV';
+    const EXTRA_VERSION   = '';
 
     /**
      * Constructor.

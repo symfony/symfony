ebfe0c160614a714379bcbc130137be7a25cef63
Barry vd. Heuvel <barryvdh@gmail.com>
2014-07-16 14:16:44 +0200

    Update SpecificityTest.php

--- src/Symfony/Component/CssSelector/Tests/Node/SpecificityTest.php
+++ src/Symfony/Component/CssSelector/Tests/Node/SpecificityTest.php
@@ -24,7 +24,7 @@ class SpecificityTest extends \PHPUnit_Framework_TestCase
     /** @dataProvider getValueTestData */
     public function testPlusValue(Specificity $specificity, $value)
     {
-        $this->assertEquals($value + 123, $specificity->plus(new Specificity(1, 2, 3))->getValue());
+        $this->assertEquals($value + 10203, $specificity->plus(new Specificity(1, 2, 3))->getValue());
     }
 
     public function getValueTestData()
@@ -32,9 +32,9 @@ class SpecificityTest extends \PHPUnit_Framework_TestCase
         return array(
             array(new Specificity(0, 0, 0), 0),
             array(new Specificity(0, 0, 2), 2),
-            array(new Specificity(0, 3, 0), 30),
-            array(new Specificity(4, 0, 0), 400),
-            array(new Specificity(4, 3, 2), 432),
+            array(new Specificity(0, 3, 0), 300),
+            array(new Specificity(4, 0, 0), 40000),
+            array(new Specificity(4, 3, 2), 40302),
         );
     }
 }

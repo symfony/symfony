05f5860c566ecf0e07de45bf9fd3f113b48bbb0d
DQNEO <dqneoo@gmail.com>
2014-09-01 03:06:56 +0900

    [HttpFoundation] Improve string values in test codes

--- src/Symfony/Component/HttpFoundation/Tests/RequestTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/RequestTest.php
@@ -200,23 +200,23 @@ class RequestTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('test.com:90', $request->getHttpHost());
         $this->assertFalse($request->isSecure());
 
-        $request = Request::create('http://test:test@test.com');
+        $request = Request::create('http://username:password@test.com');
         $this->assertEquals('http://test.com/', $request->getUri());
         $this->assertEquals('/', $request->getPathInfo());
         $this->assertEquals('', $request->getQueryString());
         $this->assertEquals(80, $request->getPort());
         $this->assertEquals('test.com', $request->getHttpHost());
-        $this->assertEquals('test', $request->getUser());
-        $this->assertEquals('test', $request->getPassword());
+        $this->assertEquals('username', $request->getUser());
+        $this->assertEquals('password', $request->getPassword());
         $this->assertFalse($request->isSecure());
 
-        $request = Request::create('http://testnopass@test.com');
+        $request = Request::create('http://username@test.com');
         $this->assertEquals('http://test.com/', $request->getUri());
         $this->assertEquals('/', $request->getPathInfo());
         $this->assertEquals('', $request->getQueryString());
         $this->assertEquals(80, $request->getPort());
         $this->assertEquals('test.com', $request->getHttpHost());
-        $this->assertEquals('testnopass', $request->getUser());
+        $this->assertEquals('username', $request->getUser());
         $this->assertSame('',$request->getPassword());
         $this->assertFalse($request->isSecure());
 

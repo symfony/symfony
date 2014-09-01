6892f356b28ecd6fefbe17cd8f01d2df93d6bcfe
Asmir Mustafic <goetas@gmail.com>
2014-09-01 10:16:24 +0200

    Added some methods to improve the handling of auto_mapping feature

--- src/Symfony/Bridge/Doctrine/DependencyInjection/AbstractDoctrineExtension.php
+++ src/Symfony/Bridge/Doctrine/DependencyInjection/AbstractDoctrineExtension.php
@@ -397,6 +397,54 @@ abstract class AbstractDoctrineExtension extends Extension
     }
 
     /**
+     * Search for a manager that is declared as 'auto_mapping' = true
+     * @param array $managerConfigs
+     * @throws \LogicException
+     * @return null|string The name of the manager. If no one manager is found, returns null.
+     */
+    private function validateAutoMapping(array $managerConfigs)
+    {
+        $autoMappedManager = null;
+        foreach ($managerConfigs as $name => $manager) {
+            if (!$manager['auto_mapping']) {
+                continue;
+            }
+
+            if (null !== $autoMappedManager) {
+                throw new \LogicException(sprintf('You cannot enable "auto_mapping" on more than one manager at the same time (found in "%s" and %s").', $autoMappedManager, $name));
+            }
+
+            $autoMappedManager = $name;
+        }
+
+        return $autoMappedManager;
+    }
+
+    /**
+     * Returns a modified version of $managerConfigs.
+     * The manager called $autoMappedManager will map all bundles that are not mepped by other managers.
+     * @param array $managerConfigs
+     * @param array $bundles
+     * @return array The modified version of $managerConfigs.
+     */
+    protected function fixManagersAutoMappings(array $managerConfigs, array $bundles)
+    {
+        if ($autoMappedManager = $this->validateAutoMapping($managerConfigs)) {
+            foreach (array_keys($bundles) as $bundle) {
+                foreach ($managerConfigs as $manager) {
+                    if (isset($manager['mappings'][$bundle])) {
+                        continue 2;
+                    }
+                }
+                $managerConfigs[$autoMappedManager]['mappings'][$bundle]['mapping'] = true;
+            }
+            $managerConfigs[$autoMappedManager]['auto_mapping'] = false;
+        }
+
+        return $managerConfigs;
+    }
+
+    /**
      * Prefixes the relative dependency injection container path with the object manager prefix.
      *
      * @example $name is 'entity_manager' then the result would be 'doctrine.orm.entity_manager'

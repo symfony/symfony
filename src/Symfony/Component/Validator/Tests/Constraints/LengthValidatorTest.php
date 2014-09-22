24c7aef6ddfa03d1733e9019b4cd7bb7582fcbb5
Fabien Potencier <fabien.potencier@gmail.com>
2014-09-22 11:21:49 +0200

    added missing use statement

--- src/Symfony/Component/Validator/Tests/Constraints/LengthValidatorTest.php
+++ src/Symfony/Component/Validator/Tests/Constraints/LengthValidatorTest.php
@@ -13,6 +13,7 @@ namespace Symfony\Component\Validator\Tests\Constraints;
 
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\LengthValidator;
+use Symfony\Component\Validator\Validation;
 
 class LengthValidatorTest extends AbstractConstraintValidatorTest
 {

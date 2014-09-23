4195a91d46998a42f5c203c18c95d79f850d74b4
Fabien Potencier <fabien.potencier@gmail.com>
2014-09-23 17:44:28 +0200

    [Routing] added support for custom expression language functions

--- src/Symfony/Component/Routing/Matcher/Dumper/PhpMatcherDumper.php
+++ src/Symfony/Component/Routing/Matcher/Dumper/PhpMatcherDumper.php
@@ -14,6 +14,7 @@ namespace Symfony\Component\Routing\Matcher\Dumper;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\ExpressionLanguage\ExpressionFunctionProviderInterface;
 
 /**
  * PhpMatcherDumper creates a PHP class able to match URLs for a given set of routes.
@@ -25,6 +26,11 @@ use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 class PhpMatcherDumper extends MatcherDumper
 {
     private $expressionLanguage;
+
+    /**
+     * @var ExpressionFunctionProviderInterface[]
+     */
+    private $expressionLanguageProviders = array();
 
     /**
      * Dumps a set of routes to a PHP class.
@@ -76,6 +82,11 @@ class {$options['class']} extends {$options['base_class']}
 }
 
 EOF;
+    }
+
+    public function addExpressionLanguageProvider(ExpressionFunctionProviderInterface $provider)
+    {
+        $this->expressionLanguageProviders[] = $provider;
     }
 
     /**
@@ -393,7 +404,7 @@ EOF;
             if (!class_exists('Symfony\Component\ExpressionLanguage\ExpressionLanguage')) {
                 throw new \RuntimeException('Unable to use expressions as the Symfony ExpressionLanguage component is not installed.');
             }
-            $this->expressionLanguage = new ExpressionLanguage();
+            $this->expressionLanguage = new ExpressionLanguage(null, $this->expressionLanguageProviders);
         }
 
         return $this->expressionLanguage;

e20283f8dc2bc46d40bb76e234bfdbeefb8615f5
Kevin Bond <kevinbond@gmail.com>
2014-09-25 10:24:36 -0400

    added OutputDecorator

--- src/Symfony/Component/Console/Helper/FormatterHelper.php
+++ src/Symfony/Component/Console/Helper/FormatterHelper.php
@@ -13,12 +13,7 @@ namespace Symfony\Component\Console\Helper;
 
 use Symfony\Component\Console\Helper\Formatter\BlockFormatter;
 use Symfony\Component\Console\Helper\Formatter\FormatterInterface;
-use Symfony\Component\Console\Helper\Formatter\ListElementFormatter;
 use Symfony\Component\Console\Helper\Formatter\SectionFormatter;
-use Symfony\Component\Console\Helper\Formatter\SectionTitleFormatter;
-use Symfony\Component\Console\Helper\Formatter\StyledBlockFormatter;
-use Symfony\Component\Console\Helper\Formatter\TextFormatter;
-use Symfony\Component\Console\Helper\Formatter\TitleFormatter;
 
 /**
  * The Formatter class provides helpers to format messages.
@@ -64,114 +59,6 @@ class FormatterHelper extends Helper
     public function formatBlock($messages, $style, $large = false, $padLength = 0)
     {
         return $this->format(new BlockFormatter($messages, $style, $large, $padLength));
-    }
-
-    /**
-     * Formats a command title
-     *
-     * @param string $message
-     *
-     * @return array
-     */
-    public function formatTitle($message)
-    {
-        return $this->format(new TitleFormatter($message));
-    }
-
-    /**
-     * Formats a section title
-     *
-     * @param string $message
-     *
-     * @return array
-     */
-    public function formatSectionTitle($message)
-    {
-        return $this->format(new SectionTitleFormatter($message));
-    }
-
-    /**
-     * Formats a list element
-     *
-     * @param string|array $messages
-     *
-     * @return array
-     */
-    public function formatListElement($messages)
-    {
-        return $this->format(new ListElementFormatter($messages));
-    }
-
-    /**
-     * Formats informational or debug text
-     *
-     * @param string|array $messages
-     *
-     * @return string
-     */
-    public function formatText($messages)
-    {
-        return $this->format(new TextFormatter($messages));
-    }
-
-    /**
-     * Formats a success result bar
-     *
-     * @param string|array $messages
-     *
-     * @return array
-     */
-    public function formatSuccessResultBar($messages)
-    {
-        return $this->format(new StyledBlockFormatter($messages, 'OK', 'fg=white;bg=green'));
-    }
-
-    /**
-     * Formats an error result bar
-     *
-     * @param string|array $messages
-     *
-     * @return array
-     */
-    public function formatErrorResultBar($messages)
-    {
-        return $this->format(new StyledBlockFormatter($messages, 'ERROR', 'fg=white;bg=red'));
-    }
-
-    /**
-     * Formats an warning result bar
-     *
-     * @param string|array $messages
-     *
-     * @return array
-     */
-    public function formatWarningResultBar($messages)
-    {
-        return $this->format(new StyledBlockFormatter($messages, 'WARNING', 'fg=black;bg=yellow'));
-    }
-
-    /**
-     * Formats a note admonition
-     *
-     * @param string|array $messages
-     *
-     * @return array
-     */
-    public function formatNoteBlock($messages)
-    {
-        return $this->format(new StyledBlockFormatter($messages, 'NOTE', 'fg=white', '! '));
-    }
-
-    /**
-     * Formats a caution admonition
-     *
-     * @param string|array $messages
-     *
-     * @return array
-     */
-    public function formatCautionBlock($messages)
-    {
-        return $this->format(new StyledBlockFormatter($messages, 'CAUTION', 'fg=white;bg=red', '! '));
     }
 
     /**

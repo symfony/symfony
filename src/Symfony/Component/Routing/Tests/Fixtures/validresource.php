66c82bad032a2d0730a9b517fe316aa51e771dcb
Strate <strate@yandex.ru>
2014-10-05 11:18:07 +0400

    Fabbot cs fix

--- src/Symfony/Component/Routing/Tests/Fixtures/validresource.php
+++ src/Symfony/Component/Routing/Tests/Fixtures/validresource.php
@@ -4,13 +4,13 @@
 /** @var \Symfony\Component\Routing\RouteCollection $collection */
 $collection = $loader->import('validpattern.php');
 $collection->addDefaults(array(
-    'foo' => 123
+    'foo' => 123,
 ));
 $collection->addRequirements(array(
-    'foo' => '\d+'
+    'foo' => '\d+',
 ));
 $collection->addOptions(array(
-    'foo' => 'bar'
+    'foo' => 'bar',
 ));
 $collection->setCondition('context.getMethod() == "POST"');
 $collection->addPrefix('/prefix');

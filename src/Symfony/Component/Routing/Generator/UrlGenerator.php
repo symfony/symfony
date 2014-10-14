cd2c2e332efd78eb08ef0de300eee9e363c59e11
Daniel Wehner <daniel.wehner@erdfisch.de>
2014-10-14 08:34:28 +0200

    decodes some special chars in a URL query

--- src/Symfony/Component/Routing/Generator/UrlGenerator.php
+++ src/Symfony/Component/Routing/Generator/UrlGenerator.php
@@ -279,7 +279,9 @@ class UrlGenerator implements UrlGeneratorInterface, ConfigurableRequirementsInt
         // add a query string if needed
         $extra = array_diff_key($parameters, $variables, $defaults);
         if ($extra && $query = http_build_query($extra, '', '&')) {
-            $url .= '?'.$query;
+            // "/" and "?" can be left decoded for better user experience, see
+            // http://tools.ietf.org/html/rfc3986#section-3.4
+            $url .= '?'.strtr($query, array('%2F' => '/'));
         }
 
         return $url;

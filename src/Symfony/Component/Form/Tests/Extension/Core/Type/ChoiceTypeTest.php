1e75013def4486fb870df22497066623dab17f84
Stepan Anchugov <kixxx1@gmail.com>
2014-11-06 22:45:08 +0500

    [Form] Disallow passing non-array values to 'preferred_choices' option of ChoiceType

--- src/Symfony/Component/Form/Tests/Extension/Core/Type/ChoiceTypeTest.php
+++ src/Symfony/Component/Form/Tests/Extension/Core/Type/ChoiceTypeTest.php
@@ -1671,4 +1671,14 @@ class ChoiceTypeTest extends \Symfony\Component\Form\Test\TypeTestCase
         // Trigger data initialization
         $form->getViewData();
     }
+
+    /**
+     * @expectedException \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     */
+    public function testSetInvalidPreferredChoicesOption()
+    {
+        $this->factory->create('choice', null, array(
+            'preferred_choices' => 'bad value',
+        ));
+    }
 }

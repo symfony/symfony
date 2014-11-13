6898552fbcb80cff483921767eb23c80a421d047
Christian Neff <christian.neff@gmail.com>
2014-11-13 21:24:11 +0100

    Fixed test case and added one more test

--- src/Symfony/Component/Console/Tests/Formatter/OutputFormatterDecoratorTest.php
+++ src/Symfony/Component/Console/Tests/Formatter/OutputFormatterDecoratorTest.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Component\Console\Tests\Formatter;
 
+use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Formatter\OutputFormatterDecorator;
 
@@ -28,5 +29,16 @@ class OutputFormatterDecoratorTest extends \PHPUnit_Framework_TestCase
 
         $style = new OutputFormatterStyle(null, 'white');
         $this->assertEquals("\033[47mfoo\033[49m", $decorator->decorate('foo', $style));
+    }
+
+    public function testFormatterWithDecorator()
+    {
+        $decorator = new OutputFormatterDecorator();
+        $formatter = new OutputFormatter($decorator);
+        
+        $style = new OutputFormatterStyle('green', 'black', array('bold', 'underscore'));
+        $formatter->setStyle('bar', $style);
+
+        $this->assertEquals("\033[32;40;1;4mfoo\033[39;49;22;24m", $formatter->format('<bar>foo</bar>', $style));
     }
 }

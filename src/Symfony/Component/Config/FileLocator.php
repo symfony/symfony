411510f7bfaa3cad1cfa0e926a1a46dfd1feb025
Geert De Deckere <geertdd@gmail.com>
2014-11-17 16:47:20 +0100

    [Config] Fixed FileLocator->isAbsolutePath()

--- src/Symfony/Component/Config/FileLocator.php
+++ src/Symfony/Component/Config/FileLocator.php
@@ -80,11 +80,11 @@ class FileLocator implements FileLocatorInterface
      */
     private function isAbsolutePath($file)
     {
-        if ($file[0] === '/' || $file[0] === '\\'
-            || (strlen($file) > 3 && ctype_alpha($file[0])
-                && $file[1] === ':'
-                && ($file[2] === '\\' || $file[2] === '/')
-            )
+        if (!is_string($file)) {
+            return false;
+        }
+
+        if (preg_match('~^(?:[a-z]:)?+[/\\\\]~i', $file)
             || null !== parse_url($file, PHP_URL_SCHEME)
         ) {
             return true;

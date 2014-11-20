205072497763394b5eae7f0226f780ac161bdd69
Daniel Wehner <daniel@tag1consulting.com>
2014-11-20 15:06:06 +0100

    adressed feedback from fabpot

--- src/Symfony/Component/DependencyInjection/Compiler/AutoAliasServicePass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/AutoAliasServicePass.php
@@ -26,30 +26,29 @@ class AutoAliasServicePass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        foreach ($container->findTaggedServiceIds('auto_alias') as $service_id => $tags) {
+        foreach ($container->findTaggedServiceIds('auto_alias') as $serviceId => $tags) {
             // We don't want to deal with an existing alias.
-            if ($container->hasAlias($service_id)) {
+            if ($container->hasAlias($serviceId)) {
                 continue;
             }
             foreach ($tags as $tag) {
                 if (!isset($tag['parameter_name'])) {
-                    throw new ParameterNotFoundException(sprintf('Missing tag information "parameter_name" on auto_alias service %s', $service_id));
+                    throw new ParameterNotFoundException(sprintf('Missing tag information "parameter_name" on auto_alias service "%s".', $serviceId));
                 }
 
-                $parameter_name = $tag['parameter_name'];
-                if (!$container->hasParameter($parameter_name)) {
-                    throw new ParameterNotFoundException(sprintf('Missing parameter %s', $parameter_name));
+                $parameterName = $tag['parameter_name'];
+                if (!$container->hasParameter($parameterName)) {
+                    throw new ParameterNotFoundException(sprintf('Missing parameter "%s"', $parameterName));
                 }
 
                 if (!isset($tag['format'])) {
-                    throw new InvalidArgumentException(sprintf('Missing tag information "format" on auto_alias service %s', $service_id));
+                    throw new InvalidArgumentException(sprintf('Missing tag information "format" on auto_alias service "%s".', $serviceId));
                 }
 
-                $parameter_value = $container->getParameter($parameter_name);
-                $format = $tag['format'];
-                $alias_id = str_replace('%s', $parameter_value, $format);
-                if ($container->hasDefinition($alias_id) || $container->hasAlias($alias_id)) {
-                    $container->setAlias($service_id, new Alias($alias_id));
+                $parameterValue = $container->getParameter($parameterName);
+                $aliasId = sprintf($tag['format'], $parameterValue);
+                if ($container->hasDefinition($aliasId) || $container->hasAlias($aliasId)) {
+                    $container->setAlias($serviceId, new Alias($aliasId));
                 }
             }
         }

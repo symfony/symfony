8aac2299544de51ec02b17be92506a5c763f8523
nyro <cedric@nyrodev.com>
2014-11-27 12:03:46 +0100

    Naturally sort paramaters to follow amazon AWS ordering rule

--- src/Symfony/Component/HttpKernel/UriSigner.php
+++ src/Symfony/Component/HttpKernel/UriSigner.php
@@ -91,7 +91,7 @@ class UriSigner
 
     private function buildUrl(array $url, array $params = array())
     {
-        ksort($params);
+        ksort($params, SORT_NATURAL);
         $url['query'] = http_build_query($params);
 
         $scheme   = isset($url['scheme']) ? $url['scheme'].'://' : '';

3267fd3cca3940fe0c8e1c3fa33aa6215f5e54fa
Lenard Palko <lpalko@pitechplus.com>
2014-11-29 15:18:57 +0100

    Added extra check on validation message for adding ". Issue #12101.

--- src/Symfony/Component/Validator/ConstraintValidator.php
+++ src/Symfony/Component/Validator/ConstraintValidator.php
@@ -168,7 +168,7 @@ abstract class ConstraintValidator implements ConstraintValidatorInterface
             return 'array';
         }
 
-        if (is_string($value)) {
+        if (is_string($value) && trim($value, '"') === $value) {
             return '"'.$value.'"';
         }
 

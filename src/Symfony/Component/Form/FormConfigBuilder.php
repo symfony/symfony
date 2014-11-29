d3320a056b3bb102f3e11b86e694b79b912a79cf
neoshadybeat <neoshadybeat@gmail.com>
2014-11-29 11:33:01 +0100

    [Hackday] [2.7] Deprecate PRE_BIND, BIND and POST_BIND form events #12601

--- src/Symfony/Component/Form/FormConfigBuilder.php
+++ src/Symfony/Component/Form/FormConfigBuilder.php
@@ -207,6 +207,8 @@ class FormConfigBuilder implements FormConfigBuilderInterface
      */
     public function addEventListener($eventName, $listener, $priority = 0)
     {
+        $this->checkIfIsDeprecatedEventName($eventName);
+
         if ($this->locked) {
             throw new BadMethodCallException('FormConfigBuilder methods cannot be accessed anymore once the builder is turned into a FormConfigInterface instance.');
         }
@@ -215,6 +217,29 @@ class FormConfigBuilder implements FormConfigBuilderInterface
 
         return $this;
     }
+
+
+    /**
+     * Check if it's deprecated event name an log it as a deprecated warning.
+     *
+     * @param string $eventName a form event name
+     */
+    private function checkIfIsDeprecatedEventName($eventName)
+    {
+        if ($eventName === FormEvents::PRE_BIND)
+        {
+            trigger_error('Event PRE_BIND is deprecated since Symfony 2.3. Use PRE_SUBMIT instead', E_USER_DEPRECATED);
+        }
+        elseif($eventName === FormEvents::BIND)
+        {
+            trigger_error('Event BIND is deprecated since Symfony 2.3. Use SUBMIT instead', E_USER_DEPRECATED);
+        }
+        elseif($eventName === FormEvents::POST_BIND)
+        {
+            trigger_error('Event POST_SUBMIT is deprecated since Symfony 2.3. Use POST_SUBMIT instead', E_USER_DEPRECATED);
+        }
+    }
+
 
     /**
      * {@inheritdoc}

8c7417617278b3142cd7af0c79d32a475e8e96b6
Henrik Bjornskov <henrik@bjrnskov.dk>
2014-11-29 10:15:31 +0100

    Add type aliases for allowed types in OptionsResolver
    Improves DX by allowing type names that does not correspond 1 to 1
    with php is_ functions.
    
    integer => int
    boolean => bool
    double => float

---
+++ src/Symfony/Component/OptionsResolver/Tests/Issue12586Test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Symfony\Component\OptionsResolver\Tests;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\Options;
+
+class Issue12586Test extends \PHPUnit_Framework_TestCase
+{
+    public function testBooleanIsAliasedAsBool()
+    {
+        $resolver = new OptionsResolver;
+        $resolver->setDefaults(array(
+            'force' => false,
+        ));
+
+        $resolver->setAllowedTypes(array(
+            'force' => 'boolean',
+        ));
+
+        $resolver->resolve(array(
+            'force' => true,
+        ));
+    }
+}

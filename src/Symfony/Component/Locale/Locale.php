5580a4f349a2552fcd02c8b4e36382757440e86d
Rodrigo Borrego Bernabé <rodrigobb@gmail.com>
2014-11-29 14:55:48 +0100

    [Locale] Adding a deprecation note about the Locale component

--- src/Symfony/Component/Locale/Locale.php
+++ src/Symfony/Component/Locale/Locale.php
@@ -11,6 +11,8 @@
 
 namespace Symfony\Component\Locale;
 
+trigger_error('\Symfony\Component\Locale\Locale is deprecated since version 2.7, to be removed in Symfony 3.0. Use the methods provided by \Symfony\Component\Intl\Intl instead.', E_USER_DEPRECATED);
+
 use Symfony\Component\Icu\IcuData;
 use Symfony\Component\Intl\Intl;
 
@@ -19,8 +21,8 @@ use Symfony\Component\Intl\Intl;
  *
  * @author Bernhard Schussek <bschussek@gmail.com>
  *
- * @deprecated Deprecated since version 2.3, to be removed in 3.0. Use
- *             {@link \Locale} and {@link \Symfony\Component\Intl\Intl} instead.
+ * @deprecated Deprecated since version 2.3, to be removed in 3.0.
+ *             Use {@link \Locale} and {@link \Symfony\Component\Intl\Intl} instead.
  */
 class Locale extends \Locale
 {

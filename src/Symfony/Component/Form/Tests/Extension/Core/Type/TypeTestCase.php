7ff8715ad7211a7f0a1bf3fa8de4930c0d8d146b
Jerzy Zawadzki <jerzy.zawadzki@polcode.com>
2014-11-29 14:01:39 +0100

    added parent::setUp()

--- src/Symfony/Component/Form/Tests/Extension/Core/Type/TypeTestCase.php
+++ src/Symfony/Component/Form/Tests/Extension/Core/Type/TypeTestCase.php
@@ -21,5 +21,6 @@ abstract class TypeTestCase extends BaseTypeTestCase
     protected function setUp()
     {
         trigger_error('Abstract class "Symfony\Component\Form\Tests\TypeTestCase" is deprecated since version 2.3 and will be removed in 3.0. Use "Symfony\Component\Form\Test\TypeTestCase" instead.', E_USER_DEPRECATED);
+        parent::setUp();
     }
 }

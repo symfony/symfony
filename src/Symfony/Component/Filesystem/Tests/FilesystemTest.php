b8f8234fdd245873050a746f5029d488b4206339
Joel Wurtz <jwurtz@jolicode.com>
2014-11-29 11:51:28 +0100

    [Filesystem] Keep executable permission when a file is copied

--- src/Symfony/Component/Filesystem/Tests/FilesystemTest.php
+++ src/Symfony/Component/Filesystem/Tests/FilesystemTest.php
@@ -967,4 +967,30 @@ class FilesystemTest extends FilesystemTestCase
         $this->assertFileExists($filename);
         $this->assertSame('bar', file_get_contents($filename));
     }
+
+    public function testCopyShouldKeepExecutionPermission()
+    {
+        $sourceFilePath = $this->workspace.DIRECTORY_SEPARATOR.'copy_source_file';
+        $targetFilePath = $this->workspace.DIRECTORY_SEPARATOR.'copy_target_file';
+
+        file_put_contents($sourceFilePath, 'SOURCE FILE');
+        chmod($sourceFilePath, 0755);
+
+        $this->filesystem->copy($sourceFilePath, $targetFilePath);
+
+        $this->assertFilePermissions(755, $targetFilePath);
+    }
+
+    public function testCopyShouldNotKeepWritePermissionOtherThanCurrentUser()
+    {
+        $sourceFilePath = $this->workspace.DIRECTORY_SEPARATOR.'copy_source_file';
+        $targetFilePath = $this->workspace.DIRECTORY_SEPARATOR.'copy_target_file';
+
+        file_put_contents($sourceFilePath, 'SOURCE FILE');
+        chmod($sourceFilePath, 0777);
+
+        $this->filesystem->copy($sourceFilePath, $targetFilePath);
+
+        $this->assertFilePermissions(755, $targetFilePath);
+    }
 }

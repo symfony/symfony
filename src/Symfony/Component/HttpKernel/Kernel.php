233c79927d271e3e2843855698b3f93885fed273
Daniel Kolvik <daniel.kolvik@gmail.com>
2014-11-30 01:39:29 +0100

    Update Kernel.php

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -93,7 +93,7 @@ abstract class Kernel implements KernelInterface, TerminableInterface
      */
     public function init()
     {
-        trigger_error('This method is going to be removed in 3.0', E_USER_DEPRECATED);
+        trigger_error('Deprecated since version 2.3, to be removed in 3.0. Move your logic in the constructor instead.', E_USER_DEPRECATED);
     }
 
     public function __clone()

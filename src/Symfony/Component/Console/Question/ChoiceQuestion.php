fa03e523d7315a81385f0337b3169a21c06f30e8
Saro0h <mkhalil.sarah@gmail.com>
2014-12-05 01:10:24 +0100

    Fixed the issue 11116

--- src/Symfony/Component/Console/Question/ChoiceQuestion.php
+++ src/Symfony/Component/Console/Question/ChoiceQuestion.php
@@ -36,7 +36,7 @@ class ChoiceQuestion extends Question
 
         $this->choices = $choices;
         $this->setValidator($this->getDefaultValidator());
-        $this->setAutocompleterValues(array_keys($choices));
+        $this->setAutocompleterValues($choices);
     }
 
     /**
@@ -134,17 +134,69 @@ class ChoiceQuestion extends Question
 
             $multiselectChoices = array();
             foreach ($selectedChoices as $value) {
-                if (empty($choices[$value])) {
+                $this->checkMultipleResults($choices, $value);
+                $result = $this->getResult($choices, $value);
+
+                if (empty($result)) {
                     throw new \InvalidArgumentException(sprintf($errorMessage, $value));
                 }
-                array_push($multiselectChoices, $choices[$value]);
+                array_push($multiselectChoices, $result);
             }
 
             if ($multiselect) {
                 return $multiselectChoices;
             }
 
-            return $choices[$selected];
+            return current($multiselectChoices);
         };
+    }
+
+    /**
+     * Checks if there are multiple keys corresponding to the value supplied
+     * by the user.
+     *
+     * @param array  $possibleChoices Possible value(s) that the user can supply
+     * @param string $value           Value supplied by the user
+     *
+     * @return null
+     */
+    private function checkMultipleResults(array $possibleChoices, $value)
+    {
+        $results = array();
+        foreach ($possibleChoices as $key => $choice) {
+            if ($choice === $value) {
+                $results[] = $key;
+            }
+        }
+
+        if (count($results) > 1) {
+            throw new \InvalidArgumentException(sprintf('The provided answer is ambiguous. Value should be one of %s.', implode(' or ', $results)));
+        }
+    }
+
+    /**
+     * Get the result according to what have been provided by the user and the
+     * possible choices.
+     *
+     * @param array $possibleChoices Possible value(s) that the user can supply
+     * @param string $value          Value supplied by the user
+     *
+     * @retun string
+     */
+    private function getResult(array $possibleChoices, $value)
+    {
+        $result = array_search($value, $possibleChoices);
+
+        if (!$this->isAssoc($possibleChoices)) {
+            if (!empty($result)) {
+                $result = $possibleChoices[$result];
+            } elseif (isset($possibleChoices[$value])) {
+                $result = $possibleChoices[$value];
+            }
+        } elseif (empty($result) && array_key_exists($value, $possibleChoices)) {
+            $result = $value;
+        }
+
+        return $result;
     }
 }

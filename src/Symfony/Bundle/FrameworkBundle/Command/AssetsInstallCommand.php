fcbfcd1d87c7dba6011d732b407e543d0cb2b1c0
Gábor Egyed <1ed@mailbox.hu>
2014-12-21 09:47:24 +0100

    [FrameworkBundle] Refactored assets:install command, tweaked output

--- src/Symfony/Bundle/FrameworkBundle/Command/AssetsInstallCommand.php
+++ src/Symfony/Bundle/FrameworkBundle/Command/AssetsInstallCommand.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Bundle\FrameworkBundle\Command;
 
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
@@ -22,9 +23,15 @@ use Symfony\Component\Finder\Finder;
  * Command that places bundle web assets into a given directory.
  *
  * @author Fabien Potencier <fabien@symfony.com>
+ * @author Gábor Egyed <gabor.egyed@gmail.com>
  */
 class AssetsInstallCommand extends ContainerAwareCommand
 {
+    /**
+     * @var \Symfony\Component\Filesystem\Filesystem
+     */
+    private $filesystem;
+
     /**
      * {@inheritdoc}
      */
@@ -74,11 +81,11 @@ EOT
             throw new \InvalidArgumentException(sprintf('The target directory "%s" does not exist.', $input->getArgument('target')));
         }
 
-        $filesystem = $this->getContainer()->get('filesystem');
+        $this->filesystem = $this->getContainer()->get('filesystem');
 
         // Create the bundles directory otherwise symlink will fail.
         $bundlesDir = $targetArg.'/bundles/';
-        $filesystem->mkdir($bundlesDir, 0777);
+        $this->filesystem->mkdir($bundlesDir, 0777);
 
         // relative implies symlink
         $symlink = $input->getOption('symlink') || $input->getOption('relative');
@@ -89,50 +96,78 @@ EOT
             $output->writeln('Installing assets as <comment>hard copies</comment>.');
         }
 
+        $table = new Table($output);
+        $table->setHeaders(array('Source', 'Target', 'Method / Error'));
+
+        $ret = 0;
         foreach ($this->getContainer()->get('kernel')->getBundles() as $bundle) {
             if (is_dir($originDir = $bundle->getPath().'/Resources/public')) {
                 $targetDir = $bundlesDir.preg_replace('/bundle$/', '', strtolower($bundle->getName()));
 
-                $output->writeln(sprintf('Installing assets for <comment>%s</comment> into <comment>%s</comment>', $bundle->getNamespace(), $targetDir));
-
-                $filesystem->remove($targetDir);
+                $this->filesystem->remove($targetDir);
 
                 if ($symlink) {
-                    if ($input->getOption('relative')) {
-                        $relativeOriginDir = $filesystem->makePathRelative($originDir, realpath($bundlesDir));
-                    } else {
-                        $relativeOriginDir = $originDir;
-                    }
-
                     try {
-                        $filesystem->symlink($relativeOriginDir, $targetDir);
-                        if (!file_exists($targetDir)) {
-                            throw new IOException('Symbolic link is broken');
-                        }
-                        $output->writeln('The assets were installed using symbolic links.');
-                    } catch (IOException $e) {
-                        if (!$input->getOption('relative')) {
-                            $this->hardCopy($originDir, $targetDir);
-                            $output->writeln('It looks like your system doesn\'t support symbolic links, so the assets were installed by copying them.');
-                        }
+                        $relative = $this->symlink($originDir, $targetDir, $input->getOption('relative'));
+                        $table->addRow(array(
+                            $bundle->getNamespace(),
+                            $targetDir,
+                            sprintf('%s symbolic link', $relative ? 'relative' : 'absolute'),
+                        ));
 
-                        // try again without the relative option
-                        try {
-                            $filesystem->symlink($originDir, $targetDir);
-                            if (!file_exists($targetDir)) {
-                                throw new IOException('Symbolic link is broken');
-                            }
-                            $output->writeln('It looks like your system doesn\'t support relative symbolic links, so the assets were installed by using absolute symbolic links.');
-                        } catch (IOException $e) {
-                            $this->hardCopy($originDir, $targetDir);
-                            $output->writeln('It looks like your system doesn\'t support symbolic links, so the assets were installed by copying them.');
-                        }
+                        continue;
+                    } catch (IOException $e) {
+                        // fall back to hard copy
                     }
-                } else {
+                }
+
+                try {
                     $this->hardCopy($originDir, $targetDir);
+                    $table->addRow(array($bundle->getNamespace(), $targetDir, 'hard copy'));
+                } catch (IOException $e) {
+                    $table->addRow(array($bundle->getNamespace(), $targetDir, sprintf('<error>%s</error>', $e->getMessage())));
+                    $ret = 1;
                 }
             }
         }
+
+        $table->render();
+
+        return $ret;
+    }
+
+    /**
+     * Creates links with absolute as a fallback.
+     *
+     * @param string $origin
+     * @param string $target
+     * @param bool $relative
+     *
+     * @throws IOException If link can not be created.
+     *
+     * @return bool Created a relative link or not.
+     */
+    private function symlink($origin, $target, $relative = true)
+    {
+        try {
+            $this->filesystem->symlink(
+                $relative ? $this->filesystem->makePathRelative($origin, realpath(dirname($target))) : $origin,
+                $target
+            );
+
+            if (!file_exists($target)) {
+                throw new IOException(sprintf('Symbolic link "%s" is created but appears to be broken.', $target), 0, null, $target);
+            }
+        } catch (IOException $e) {
+            if ($relative) {
+                // try again with absolute
+                return $this->symlink($origin, $target, false);
+            }
+
+            throw $e;
+        }
+
+        return $relative;
     }
 
     /**
@@ -141,10 +176,8 @@ EOT
      */
     private function hardCopy($originDir, $targetDir)
     {
-        $filesystem = $this->getContainer()->get('filesystem');
-
-        $filesystem->mkdir($targetDir, 0777);
+        $this->filesystem->mkdir($targetDir, 0777);
         // We use a custom iterator to ignore VCS files
-        $filesystem->mirror($originDir, $targetDir, Finder::create()->ignoreDotFiles(false)->in($originDir));
+        $this->filesystem->mirror($originDir, $targetDir, Finder::create()->ignoreDotFiles(false)->in($originDir));
     }
 }

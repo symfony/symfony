43f2a53f1929403fd3356c2a1136d68adc00ae84
frne <frank.neff@ymc.ch>
2014-12-23 13:19:45 +0100

    Added isVerbose(), isVeryVerbose(), isQuiet() and isDebug() methods to OutputInterface

--- src/Symfony/Component/Console/Output/OutputInterface.php
+++ src/Symfony/Component/Console/Output/OutputInterface.php
@@ -76,6 +76,42 @@ interface OutputInterface
     public function getVerbosity();
 
     /**
+     * Returns whether verbosity is quiet (-q)
+     *
+     * @return bool true if verbosity is set to VERBOSITY_QUIET, false otherwise
+     *
+     * @api
+     */
+    public function isQuiet();
+
+    /**
+     * Returns whether verbosity is verbose (-v)
+     *
+     * @return bool true if verbosity is set to VERBOSITY_VERBOSE, false otherwise
+     *
+     * @api
+     */
+    public function isVerbose();
+
+    /**
+     * Returns whether verbosity is very verbose (-vv)
+     *
+     * @return bool true if verbosity is set to VERBOSITY_VERY_VERBOSE, false otherwise
+     *
+     * @api
+     */
+    public function isVeryVerbose();
+
+    /**
+     * Returns whether verbosity is debug (-vvv)
+     *
+     * @return bool true if verbosity is set to VERBOSITY_DEBUG, false otherwise
+     *
+     * @api
+     */
+    public function isDebug();
+
+    /**
      * Sets the decorated flag.
      *
      * @param bool $decorated Whether to decorate the messages

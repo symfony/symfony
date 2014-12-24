bf573f9c3545b91132940483832b6ea9e3da5b2d
Nicolas Grekas <nicolas.grekas@gmail.com>
2014-12-24 09:22:35 +0100

    [travis] remove sh wrapper to expose tty correctly

--- src/Symfony/Component/Console/Tests/Helper/QuestionHelperTest.php
+++ src/Symfony/Component/Console/Tests/Helper/QuestionHelperTest.php
@@ -19,6 +19,9 @@ use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
 
+/**
+ * @group tty
+ */
 class QuestionHelperTest extends \PHPUnit_Framework_TestCase
 {
     public function testAskChoice()
@@ -125,9 +128,6 @@ class QuestionHelperTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('FooBundle', $dialog->ask($this->createInputInterfaceMock(), $this->createOutputInterface(), $question));
     }
 
-    /**
-     * @group tty
-     */
     public function testAskHiddenResponse()
     {
         if (defined('PHP_WINDOWS_VERSION_BUILD')) {

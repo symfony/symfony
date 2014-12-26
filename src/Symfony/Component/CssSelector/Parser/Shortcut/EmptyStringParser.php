1997b12f51d728c2a5958506c22adc002159f34c
Jakub Zalas <jakub@zalas.pl>
2014-12-26 23:22:03 +0100

    Use the correct cssselect library name in docblocks.
    The original python library is called cssselect, not cssselector.

--- src/Symfony/Component/CssSelector/Parser/Shortcut/EmptyStringParser.php
+++ src/Symfony/Component/CssSelector/Parser/Shortcut/EmptyStringParser.php
@@ -22,7 +22,7 @@ use Symfony\Component\CssSelector\Parser\ParserInterface;
  * - The parser fails to parse an empty string.
  * - In the previous version, an empty string matches each tags.
  *
- * This component is a port of the Python cssselector library,
+ * This component is a port of the Python cssselect library,
  * which is copyright Ian Bicking, @see https://github.com/SimonSapin/cssselect.
  *
  * @author Jean-François Simon <jeanfrancois.simon@sensiolabs.com>

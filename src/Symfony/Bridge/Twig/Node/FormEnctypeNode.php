550ca9d98795d02cb4d81887b21613fffd2f15c7
Fabien Potencier <fabien.potencier@gmail.com>
2014-12-26 12:36:36 +0100

    Revert "minor #12673 [Form] [Hackday] [2.7] Add a deprecation note about the `form_enctype` helper (mrcmorales)"
    This reverts commit e8d98578f4bc5037b8b36968843a7bee1d44641d, reversing
    changes made to 0c7df5898f322102a82e54900519197e0e6bdb7a.

--- src/Symfony/Bridge/Twig/Node/FormEnctypeNode.php
+++ src/Symfony/Bridge/Twig/Node/FormEnctypeNode.php
@@ -23,6 +23,7 @@ class FormEnctypeNode extends SearchAndRenderBlockNode
     {
         parent::compile($compiler);
 
-        $compiler->write('trigger_error(\'The helper form_enctype(form) is deprecated since version 2.3 and will be removed in 3.0. Use form_start(form) instead.\', E_USER_DEPRECATED)');
+        // Uncomment this as soon as the deprecation note should be shown
+        // $compiler->write('trigger_error(\'The helper form_enctype(form) is deprecated since version 2.3 and will be removed in 3.0. Use form_start(form) instead.\', E_USER_DEPRECATED)');
     }
 }

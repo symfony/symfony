83ad408dce2247ec9de10f5145af86297c5f1a13
Fabien Potencier <fabien.potencier@gmail.com>
2014-12-28 20:02:59 +0100

    [TwigBundle] removed the Container dependency on ActionsExtension

--- src/Symfony/Bundle/TwigBundle/Extension/ActionsExtension.php
+++ src/Symfony/Bundle/TwigBundle/Extension/ActionsExtension.php
@@ -13,6 +13,7 @@ namespace Symfony\Bundle\TwigBundle\Extension;
 
 use Symfony\Bundle\TwigBundle\TokenParser\RenderTokenParser;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Fragment\FragmentHandler;
 
 /**
  * Twig extension for Symfony actions helper.
@@ -23,16 +24,26 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class ActionsExtension extends \Twig_Extension
 {
-    private $container;
+    private $handler;
 
     /**
-     * Constructor.
+     * @param FragmentHandler|ContainerInterface $handler
      *
-     * @param ContainerInterface $container The service container
+     * @deprecated Passing a ContainerInterface as a first argument is deprecated as of 2.7 and will be removed in 3.0.
      */
-    public function __construct(ContainerInterface $container)
+    public function __construct($handler = null)
     {
-        $this->container = $container;
+        if ($handler instanceof FragmentHandler) {
+            $this->handler = $handler;
+        } elseif ($handler instanceof ContainerInterface) {
+            trigger_error(sprintf('The ability to pass a ContainerInterface instance as a first argument to %s was deprecated in 2.7 and will be removed in 3.0. Please, pass a FragmentHandler instance instead.', __METHOD__), E_USER_DEPRECATED);
+
+            $this->handler = $handler->get('templating.helper.actions');
+        } elseif (null !== $assetsHelper) {
+            throw new \BadFunctionCallException(sprintf('%s takes a FragmentHandler or a ContainerInterface object as its first argument.', __METHOD__));
+        }
+
+        $this->handler = $handler;
     }
 
     /**
@@ -41,11 +52,18 @@ class ActionsExtension extends \Twig_Extension
      * @param string $uri     A URI
      * @param array  $options An array of options
      *
-     * @see Symfony\Bundle\FrameworkBundle\Controller\ControllerResolver::render()
+     * @see FragmentHandler::render()
      */
     public function renderUri($uri, array $options = array())
     {
-        return $this->container->get('templating.helper.actions')->render($uri, $options);
+        if (null === $this->handler) {
+            throw new \RuntimeException('To render a URI, the Symfony HttpKernel component is required.');
+        }
+
+        $strategy = isset($options['strategy']) ? $options['strategy'] : 'inline';
+        unset($options['strategy']);
+
+        return $this->handler->render($uri, $strategy, $options);
     }
 
     /**

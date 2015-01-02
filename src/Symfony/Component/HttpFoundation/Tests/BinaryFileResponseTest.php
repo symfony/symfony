39c2c4d424cd1eeb4ce53c5ecaaf1593439e4ce7
Joshua Thijssen <jthijssen@noxlogic.nl>
2015-01-02 11:04:57 +0100

    Don't add Accept-Range header on unsafe HTTP requests

--- src/Symfony/Component/HttpFoundation/Tests/BinaryFileResponseTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/BinaryFileResponseTest.php
@@ -208,6 +208,24 @@ class BinaryFileResponseTest extends ResponseTestCase
         $this->assertFileNotExists($path);
     }
 
+    /**
+     *
+     */
+    public function testAcceptRangeOnUnsafeMethods()
+    {
+        $request = Request::create('/', 'POST');
+        $response = BinaryFileResponse::create(__DIR__.'/File/Fixtures/test.gif');
+        $response->prepare($request);
+        $this->assertEquals('none', $response->headers->get('Accept-Ranges'));
+
+        // Don't override when already set
+        $request = Request::create('/', 'POST');
+        $response = BinaryFileResponse::create(__DIR__.'/File/Fixtures/test.gif');
+        $response->headers->set('Accept-Ranges', 'foo');
+        $response->prepare($request);
+        $this->assertEquals('foo', $response->headers->get('Accept-Ranges'));
+    }
+
     public function getSampleXAccelMappings()
     {
         return array(

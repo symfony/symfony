4fdb83203f4c71c103a7fe941bed704248c97f8d
Fabien Potencier <fabien.potencier@gmail.com>
2015-01-07 22:37:06 +0100

    fixed typo

--- src/Symfony/Bundle/FrameworkBundle/Console/Descriptor/JsonDescriptor.php
+++ src/Symfony/Bundle/FrameworkBundle/Console/Descriptor/JsonDescriptor.php
@@ -196,7 +196,7 @@ class JsonDescriptor extends Descriptor
             'synthetic' => $definition->isSynthetic(),
             'lazy' => $definition->isLazy(),
             'synchronized' => $definition->isSynchronized(),
-            'abstract' => $definition->isSynchronized(),
+            'abstract' => $definition->isAbstract(),
             'file' => $definition->getFile(),
         );
 

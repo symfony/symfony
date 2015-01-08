98047ae20937e0f4984fcab7a1a34b647e8ec24b
Fabien Potencier <fabien.potencier@gmail.com>
2015-01-08 12:38:08 +0100

    [EventDispatcher] fixed deprecation notices in the EventDispatcher Component

--- src/Symfony/Component/EventDispatcher/Tests/AbstractEventDispatcherTest.php
+++ src/Symfony/Component/EventDispatcher/Tests/AbstractEventDispatcherTest.php
@@ -118,8 +118,16 @@ abstract class AbstractEventDispatcherTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Symfony\Component\EventDispatcher\Event', $this->dispatcher->dispatch(self::preFoo));
         $event = new Event();
         $return = $this->dispatcher->dispatch(self::preFoo, $event);
-        $this->assertEquals('pre.foo', $event->getName());
         $this->assertSame($event, $return);
+    }
+
+    public function testLegacyDispatch()
+    {
+        $this->iniSet('error_reporting', -1 & ~E_USER_DEPRECATED);
+
+        $event = new Event();
+        $return = $this->dispatcher->dispatch(self::preFoo, $event);
+        $this->assertEquals('pre.foo', $event->getName());
     }
 
     public function testDispatchForClosure()
@@ -239,8 +247,10 @@ abstract class AbstractEventDispatcherTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($this->dispatcher->hasListeners(self::preFoo));
     }
 
-    public function testEventReceivesTheDispatcherInstance()
+    public function testLegacyEventReceivesTheDispatcherInstance()
     {
+        $this->iniSet('error_reporting', -1 & ~E_USER_DEPRECATED);
+
         $dispatcher = null;
         $this->dispatcher->addListener('test', function ($event) use (&$dispatcher) {
             $dispatcher = $event->getDispatcher();

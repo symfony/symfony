5d3a1fc4c50d0433a92b55a5879fbfc2cc790a60
Warnar Boekkooi <warnar@boekkooi.net>
2015-01-10 13:50:07 +0100

    [Console] Helper\Table->addRow optimization

--- src/Symfony/Component/Console/Helper/Table.php
+++ src/Symfony/Component/Console/Helper/Table.php
@@ -174,8 +174,9 @@ class Table
 
         $this->rows[] = array_values($row);
 
-        $keys = array_keys($this->rows);
-        $rowKey = array_pop($keys);
+        end($this->rows);
+        $rowKey = key($this->rows);
+        reset($this->rows);
 
         foreach ($row as $key => $cellValue) {
             if (!strstr($cellValue, "\n")) {

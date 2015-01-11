2d3b9b9dd4f2f93eac2262107688d4fdaaef1ea9
Alexpts <alexpts@yandex.ru>
2015-01-11 15:57:41 +0300

    fix codestyle

--- src/Symfony/Component/HttpFoundation/JsonResponse.php
+++ src/Symfony/Component/HttpFoundation/JsonResponse.php
@@ -48,14 +48,12 @@ class JsonResponse extends Response
         $this->setData($data);
     }
     
-    /**
-     *  Encode <, >, ', &, and " for RFC4627-compliant JSON, which may also be embedded into HTML.
-     */ 
     protected function setDefaultEncodingOptions()
     {
+        // Encode <, >, ', &, and " for RFC4627-compliant JSON, which may also be embedded into HTML.
         $this->encodingOptions = JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT;
     }
-
+    
     /**
      * {@inheritdoc}
      */

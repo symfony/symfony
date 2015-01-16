5fcfd1b9f6e6d16e8599b252802f800a596e40e3
Fabien Potencier <fabien.potencier@gmail.com>
2015-01-16 18:23:24 +0100

    [Validator] fixed some legacy tests

--- src/Symfony/Component/Validator/Tests/Mapping/MemberMetadataTest.php
+++ src/Symfony/Component/Validator/Tests/Mapping/MemberMetadataTest.php
@@ -35,8 +35,10 @@ class MemberMetadataTest extends \PHPUnit_Framework_TestCase
         $this->metadata = null;
     }
 
-    public function testAddValidSetsMemberToCascaded()
+    public function testLegacyAddValidSetsMemberToCascaded()
     {
+        $this->iniSet('error_reporting', -1 & ~E_USER_DEPRECATED);
+
         $result = $this->metadata->addConstraint(new Valid());
 
         $this->assertEquals(array(), $this->metadata->getConstraints());
@@ -44,8 +46,10 @@ class MemberMetadataTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($this->metadata->isCascaded());
     }
 
-    public function testAddOtherConstraintDoesNotSetMemberToCascaded()
+    public function testLegacyAddOtherConstraintDoesNotSetMemberToCascaded()
     {
+        $this->iniSet('error_reporting', -1 & ~E_USER_DEPRECATED);
+
         $result = $this->metadata->addConstraint($constraint = new ConstraintA());
 
         $this->assertEquals(array($constraint), $this->metadata->getConstraints());

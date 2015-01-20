0a3f50e90931b9a62cff601119da9a47e2fce1da
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2015-01-20 23:16:06 +0100

    change comments type

--- src/Symfony/Component/HttpFoundation/Session/Storage/Handler/NativeSessionHandler.php
+++ src/Symfony/Component/HttpFoundation/Session/Storage/Handler/NativeSessionHandler.php
@@ -11,11 +11,8 @@
 
 namespace Symfony\Component\HttpFoundation\Session\Storage\Handler;
 
-/*
- * Adds SessionHandler functionality if available.
- *
- * @see http://php.net/sessionhandler
- */
+// Adds SessionHandler functionality if available.
+// @see http://php.net/sessionhandler
 if (PHP_VERSION_ID >= 50400) {
     class NativeSessionHandler extends \SessionHandler
     {

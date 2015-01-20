4af6b9a531eec5ba40ca13f0be488c2c447bcf60
Fred Cox <fred@ekreative.com>
2015-01-20 17:56:13 +0200

    Add test that shows that the container cannot be dumped when using this option

--- src/Symfony/Bundle/TwigBundle/Tests/DependencyInjection/TwigExtensionTest.php
+++ src/Symfony/Bundle/TwigBundle/Tests/DependencyInjection/TwigExtensionTest.php
@@ -15,6 +15,7 @@ use Symfony\Bundle\TwigBundle\DependencyInjection\TwigExtension;
 use Symfony\Bundle\TwigBundle\Tests\TestCase;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -136,6 +137,9 @@ class TwigExtensionTest extends TestCase
 
         $options = $container->getParameter('twig.options');
         $this->assertEquals(array(new Reference('my_project.some_bundle.template_escaping_guesser'), 'guess'), $options['autoescape']);
+
+        $dumper = new PhpDumper($container);
+        $this->assertNotNull($dumper->dump());
     }
 
     /**

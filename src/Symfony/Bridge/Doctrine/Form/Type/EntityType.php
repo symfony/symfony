99e93b805874595b5725cafa06e84cab42700eaa
Dominik Zogg <dominik.zogg@gmail.com>
2015-02-01 22:03:48 +0100

    allow cache even if a closure get used for query_builder
    get it running on php5.3

--- src/Symfony/Bridge/Doctrine/Form/Type/EntityType.php
+++ src/Symfony/Bridge/Doctrine/Form/Type/EntityType.php
@@ -12,10 +12,17 @@
 namespace Symfony\Bridge\Doctrine\Form\Type;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\Form\ChoiceList\ORMQueryBuilderLoader;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
 
 class EntityType extends DoctrineType
 {
+    /**
+     * @var array
+     */
+    private $loaderCache = array();
+
     /**
      * Return the default loader object.
      *
@@ -24,14 +31,61 @@ class EntityType extends DoctrineType
      * @param string        $class
      *
      * @return ORMQueryBuilderLoader
+     *
+     * @throws UnexpectedTypeException
      */
     public function getLoader(ObjectManager $manager, $queryBuilder, $class)
     {
-        return new ORMQueryBuilderLoader(
-            $queryBuilder,
-            $manager,
-            $class
-        );
+        if (!($queryBuilder instanceof QueryBuilder || $queryBuilder instanceof \Closure)) {
+            throw new UnexpectedTypeException($queryBuilder, 'Doctrine\ORM\QueryBuilder or \Closure');
+        }
+
+        if ($queryBuilder instanceof \Closure) {
+            $reflection = new \ReflectionFunction($queryBuilder);
+            $queryBuilderHashParts = $this->replaceObjectWithHash(array(
+                'filename' => $reflection->getFileName(),
+                'startLine' => $reflection->getStartLine(),
+                'stopFile' => $reflection->getEndLine(),
+                'uses' => $reflection->getStaticVariables(),
+                'this' => is_callable(array($reflection, 'getClosureThis')) ? $reflection->getClosureThis() : null,
+            ));
+            $queryBuilderHash = hash('sha256', json_encode($queryBuilderHashParts));
+        } else {
+            $queryBuilderHash = spl_object_hash($queryBuilder);
+        }
+
+        $loaderHash = hash('sha256', json_encode(array(
+            'manager' => spl_object_hash($manager),
+            'queryBuilder' => $queryBuilderHash,
+            'class' => $class,
+        )));
+
+        if (!isset($this->loaderCache[$loaderHash])) {
+            $this->loaderCache[$loaderHash] = new ORMQueryBuilderLoader(
+                $queryBuilder,
+                $manager,
+                $class
+            );
+        }
+
+        return $this->loaderCache[$loaderHash];
+    }
+
+    /**
+     * @param  array $data
+     * @return array
+     */
+    protected function replaceObjectWithHash(array $data)
+    {
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                $data[$key] = $this->replaceObjectWithHash($value);
+            } elseif (is_object($value)) {
+                $data[$key] = spl_object_hash($value);
+            }
+        }
+
+        return $data;
     }
 
     public function getName()

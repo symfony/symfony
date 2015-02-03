8ddc888f00c1148d5d761809b45f75e3157daf4d
Kévin Dunglas <dunglas@gmail.com>
2015-02-03 23:24:43 +0100

    [Serializer] Use Serializer's LogicException when applicable

--- src/Symfony/Component/Serializer/Normalizer/GetSetMethodNormalizer.php
+++ src/Symfony/Component/Serializer/Normalizer/GetSetMethodNormalizer.php
@@ -12,6 +12,7 @@
 namespace Symfony\Component\Serializer\Normalizer;
 
 use Symfony\Component\Serializer\Exception\CircularReferenceException;
+use Symfony\Component\Serializer\Exception\LogicException;
 use Symfony\Component\Serializer\Exception\RuntimeException;
 
 /**
@@ -40,6 +41,7 @@ class GetSetMethodNormalizer extends AbstractNormalizer
     /**
      * {@inheritdoc}
      *
+     * @throws LogicException
      * @throws CircularReferenceException
      */
     public function normalize($object, $format = null, array $context = array())
@@ -71,7 +73,7 @@ class GetSetMethodNormalizer extends AbstractNormalizer
                 }
                 if (null !== $attributeValue && !is_scalar($attributeValue)) {
                     if (!$this->serializer instanceof NormalizerInterface) {
-                        throw new \LogicException(sprintf('Cannot normalize attribute "%s" because injected serializer is not a normalizer', $attributeName));
+                        throw new LogicException(sprintf('Cannot normalize attribute "%s" because injected serializer is not a normalizer', $attributeName));
                     }
 
                     $attributeValue = $this->serializer->normalize($attributeValue, $format, $context);

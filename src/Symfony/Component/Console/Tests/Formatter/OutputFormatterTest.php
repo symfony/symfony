aae2e28bc560d56154d1763cb9fdda5fe0c970e0
Stefan Hüsges <Tronsha@gmx.de>
2015-02-05 15:06:51 +0100

    Coding Standard

--- src/Symfony/Component/Console/Tests/Formatter/OutputFormatterTest.php
+++ src/Symfony/Component/Console/Tests/Formatter/OutputFormatterTest.php
@@ -101,7 +101,7 @@ class OutputFormatterTest extends \PHPUnit_Framework_TestCase
             "(\033[32mz>=2.0,<a2.3\033[39m)",
             $formatter->format('(<info>'.$formatter->escape('z>=2.0,<a2.3').'</info>)')
         );
-        
+
         $this->assertEquals(
             "\033[32m<error>some error</error>\033[39m",
             $formatter->format('<info>'.$formatter->escape('<error>some error</error>').'</info>')

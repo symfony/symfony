b0a4c384652847e7c2da391f0e5d2bea1cb2067a
Fabien Potencier <fabien.potencier@gmail.com>
2015-02-05 10:43:13 +0100

    fixed assertion

--- src/Symfony/Component/HttpFoundation/Tests/RequestTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/RequestTest.php
@@ -925,7 +925,7 @@ class RequestTest extends \PHPUnit_Framework_TestCase
     {
         $req = Request::create('test');
 
-        $this->assertEquals('', $req->getContent());
+        $this->assertSame('', $req->getContent());
     }
 
     /**

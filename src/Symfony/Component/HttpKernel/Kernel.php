0a003b7750a9b4c9682f9968e799731376ae27ef
Fabien Potencier <fabien.potencier@gmail.com>
2015-02-08 08:08:16 +0100

    Revert "bumped Symfony version to 2.5.11"
    This reverts commit 45e42a8d2127d4c7dca26a4ef7394ac8dce503ef.

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -60,12 +60,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION = '2.5.11-DEV';
-    const VERSION_ID = '20511';
+    const VERSION = '2.5.10';
+    const VERSION_ID = '20510';
     const MAJOR_VERSION = '2';
     const MINOR_VERSION = '5';
-    const RELEASE_VERSION = '11';
-    const EXTRA_VERSION = 'DEV';
+    const RELEASE_VERSION = '10';
+    const EXTRA_VERSION = '';
 
     /**
      * Constructor.

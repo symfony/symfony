5799947e36bd8edd2e23d14a024eb83967c9a7d3
Walter Dal Mut <walter.dalmut@gmail.com>
2015-02-14 14:38:59 +0100

    Annotated routes with a variadic parameter
    There are no variadic default values and that generate a fatal error.

---
+++ src/Symfony/Component/Routing/Tests/Fixtures/AnnotatedClasses/VariadicClass.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Routing\Tests\Fixtures\AnnotatedClasses;
+
+class VariadicClass
+{
+    public function routeAction(...$params)
+    {
+    }
+}

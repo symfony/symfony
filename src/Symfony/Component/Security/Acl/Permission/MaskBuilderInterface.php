3391bc74ba304cc88075b3810d5252fe73e3b37b
Alexandre Dupuy <alex.dpy@gmail.com>
2015-03-16 23:35:06 +0100

    added some phpdoc

--- src/Symfony/Component/Security/Acl/Permission/MaskBuilderInterface.php
+++ src/Symfony/Component/Security/Acl/Permission/MaskBuilderInterface.php
@@ -1,7 +1,19 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Component\Security\Acl\Permission;
 
+/**
+ * This is the interface that must be implemented by mask builders.
+ */
 interface MaskBuilderInterface
 {
     /**

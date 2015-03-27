bbdb37c2297b5c828a00324b7cf8da96a9aadef0
Kevin Bond <kevinbond@gmail.com>
2015-03-27 12:04:30 -0400

    use DIRECTORY_SEPARATOR to detect Windows

--- src/Symfony/Component/Console/Style/SymfonyStyle.php
+++ src/Symfony/Component/Console/Style/SymfonyStyle.php
@@ -269,7 +269,7 @@ class SymfonyStyle extends OutputStyle
     {
         $progressBar = parent::createProgressBar($max);
 
-        if (!defined('PHP_WINDOWS_VERSION_BUILD')) {
+        if ('\\' === DIRECTORY_SEPARATOR) {
             $progressBar->setEmptyBarCharacter('░'); // light shade character \u2591
             $progressBar->setProgressCharacter('');
             $progressBar->setBarCharacter('▓'); // dark shade character \u2593

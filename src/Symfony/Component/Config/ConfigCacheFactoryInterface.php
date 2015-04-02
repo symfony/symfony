282a75ac7dc097cb3decc717b5dc773926faf2d5
Matthias Pigulla <mp@webfactory.de>
2015-04-02 16:52:09 +0200

    CS fix

--- src/Symfony/Component/Config/ConfigCacheFactoryInterface.php
+++ src/Symfony/Component/Config/ConfigCacheFactoryInterface.php
@@ -25,6 +25,7 @@ interface ConfigCacheFactoryInterface
      *
      * @param  string               $file        The absolute cache file path
      * @param  callable             $callable    The callable to be executed when the cache needs to be filled (i. e. is not fresh). The cache will be passed as the only parameter to this callback.
+     *
      * @return ConfigCacheInterface $configCache The cache instance.
      */
     public function cache($file, $callable);

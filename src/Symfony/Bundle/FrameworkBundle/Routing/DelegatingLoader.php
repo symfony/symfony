e83266739867cbb8a1c728a59156a401b3776c06
Nicolas Grekas <nicolas.grekas@gmail.com>
2015-04-15 09:53:47 +0200

    [FrameworkBundle] Fix Routing\DelegatingLoader resiliency to fatal errors

--- src/Symfony/Bundle/FrameworkBundle/Routing/DelegatingLoader.php
+++ src/Symfony/Bundle/FrameworkBundle/Routing/DelegatingLoader.php
@@ -12,10 +12,10 @@
 namespace Symfony\Bundle\FrameworkBundle\Routing;
 
 use Symfony\Bundle\FrameworkBundle\Controller\ControllerNameParser;
+use Symfony\Component\Config\Exception\FileLoaderLoadException;
 use Symfony\Component\Config\Loader\DelegatingLoader as BaseDelegatingLoader;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Routing\RouteCollection;
 
 /**
  * DelegatingLoader delegates route loading to other loaders using a loader resolver.
@@ -29,6 +29,7 @@ class DelegatingLoader extends BaseDelegatingLoader
 {
     protected $parser;
     protected $logger;
+    private $loading = false;
 
     /**
      * Constructor.
@@ -46,16 +47,39 @@ class DelegatingLoader extends BaseDelegatingLoader
     }
 
     /**
-     * Loads a resource.
-     *
-     * @param mixed  $resource A resource
-     * @param string $type     The resource type
-     *
-     * @return RouteCollection A RouteCollection instance
+     * {@inheritdoc}
      */
     public function load($resource, $type = null)
     {
-        $collection = parent::load($resource, $type);
+        if ($this->loading) {
+            // This can happen if a fatal error occurs in parent::load().
+            // Here is the scenario:
+            // - while routes are being loaded by parent::load() below, a fatal error
+            //   occurs (e.g. parse error in a controller while loading annotations);
+            // - PHP abruptly empties the stack trace, bypassing all catch blocks;
+            //   it then calls the registered shutdown functions;
+            // - the ErrorHandler catches the fatal error and re-injects it for rendering
+            //   thanks to HttpKernel->terminateWithException() (that calls handleException());
+            // - at this stage, if we try to load the routes again, we must prevent
+            //   the fatal error from occurring a second time,
+            //   otherwise the PHP process would be killed immediately;
+            // - while rendering the exception page, the router can be required
+            //   (by e.g. the web profiler that needs to generate an URL);
+            // - this handles the case and prevents the second fatal error
+            //   by triggering an exception beforehand.
+
+            throw new FileLoaderLoadException($resource);
+        }
+        $this->loading = true;
+
+        try {
+            $collection = parent::load($resource, $type);
+        } catch (\Exception $e) {
+            $this->loading = false;
+            throw $e;
+        }
+
+        $this->loading = false;
 
         foreach ($collection->all() as $route) {
             if ($controller = $route->getDefault('_controller')) {

343d2828d6263f79bfa60629bf7eb02aa30f0e38
Nicolas Grekas <nicolas.grekas@gmail.com>
2015-04-27 18:33:29 +0200

    Fix merge

--- src/Symfony/Component/Validator/Tests/Constraints/ChoiceValidatorTest.php
+++ src/Symfony/Component/Validator/Tests/Constraints/ChoiceValidatorTest.php
@@ -167,20 +167,6 @@ class ChoiceValidatorTest extends AbstractConstraintValidatorTest
             ->assertRaised();
     }
 
-    public function testInvalidChoiceEmptyChoices()
-    {
-        $constraint = new Choice(array(
-            'choices' => array(),
-            'message' => 'myMessage',
-        ));
-
-        $this->validator->validate('baz', $constraint);
-
-        $this->buildViolation('myMessage')
-            ->setParameter('{{ value }}', '"baz"')
-            ->assertRaised();
-    }
-
     public function testInvalidChoiceMultiple()
     {
         $constraint = new Choice(array(

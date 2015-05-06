7e866b67b3c2f0843fd1746bf06a27e3a6f1fef9
ymorocutti <ymorocutti@users.noreply.github.com>
2015-05-06 19:04:24 +0200

    Update ORMQueryBuilderLoader.php

--- src/Symfony/Bridge/Doctrine/Form/ChoiceList/ORMQueryBuilderLoader.php
+++ src/Symfony/Bridge/Doctrine/Form/ChoiceList/ORMQueryBuilderLoader.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Exception\UnexpectedTypeException;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Decorator\EntityManagerDecorator;
 
 /**
  * Loads entities using a {@link QueryBuilder} instance.
@@ -59,7 +60,7 @@ class ORMQueryBuilderLoader implements EntityLoaderInterface
         if ($queryBuilder instanceof \Closure) {
             trigger_error('Passing a QueryBuilder closure to '.__CLASS__.'::__construct() is deprecated since version 2.7 and will be removed in 3.0.', E_USER_DEPRECATED);
 
-            if (!$manager instanceof EntityManager) {
+            if (!$manager instanceof EntityManager && !$manager instanceof EntityManagerDecorator) {
                 throw new UnexpectedTypeException($manager, 'Doctrine\ORM\EntityManager');
             }
 

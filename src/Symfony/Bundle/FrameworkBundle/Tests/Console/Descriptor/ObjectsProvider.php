128140eccf10eda4923c2a49d42a3e16f19c3653
Fabien Potencier <fabien.potencier@gmail.com>
2015-05-06 18:31:54 +0200

    fixed CS

--- src/Symfony/Bundle/FrameworkBundle/Tests/Console/Descriptor/ObjectsProvider.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Console/Descriptor/ObjectsProvider.php
@@ -114,6 +114,7 @@ class ObjectsProvider
 
     /**
      * @deprecated since version 2.7, to be removed in 3.0
+     *
      * @internal
      */
     public static function getLegacyContainerDefinitions()

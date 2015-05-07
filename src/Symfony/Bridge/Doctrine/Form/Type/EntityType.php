3172d73b6c7969fe2f01664c495c641256418f39
Maciej Malarz <malarzm@gmail.com>
2015-05-07 10:06:26 +0200

    [DoctrineBridge][Form] Fix BC break in DoctrineType

--- src/Symfony/Bridge/Doctrine/Form/Type/EntityType.php
+++ src/Symfony/Bridge/Doctrine/Form/Type/EntityType.php
@@ -14,9 +14,34 @@ namespace Symfony\Bridge\Doctrine\Form\Type;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\Form\ChoiceList\ORMQueryBuilderLoader;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EntityType extends DoctrineType
 {
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        // Invoke the query builder closure so that we can cache choice lists
+        // for equal query builders
+        $queryBuilderNormalizer = function (Options $options, $queryBuilder) {
+            if (is_callable($queryBuilder)) {
+                $queryBuilder = call_user_func($queryBuilder, $options['em']->getRepository($options['class']));
+
+                if (!$queryBuilder instanceof QueryBuilder) {
+                    throw new UnexpectedTypeException($queryBuilder, 'Doctrine\ORM\QueryBuilder');
+                }
+            }
+
+            return $queryBuilder;
+        };
+
+        $resolver->setNormalizer('query_builder', $queryBuilderNormalizer);
+        $resolver->setAllowedTypes('query_builder', array('null', 'callable', 'Doctrine\ORM\QueryBuilder'));
+    }
+
     /**
      * Return the default loader object.
      *
@@ -34,5 +59,24 @@ class EntityType extends DoctrineType
     public function getName()
     {
         return 'entity';
+    }
+
+    /**
+     * We consider two query builders with an equal SQL string and
+     * equal parameters to be equal.
+     * 
+     * @param QueryBuilder $queryBuilder
+     * 
+     * @return array
+     * 
+     * @internal This method is public to be usable as callback. It should not
+     *           be used in user code.
+     */
+    public function getQueryBuilderPartsForCachingHash($queryBuilder)
+    {
+        return array(
+                $queryBuilder->getQuery()->getSQL(),
+                $queryBuilder->getParameters()->toArray(),
+        );
     }
 }

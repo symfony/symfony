314901e798b11ef387f1391d40e9cceb87a980f9
Christian Flothmann <christian.flothmann@xabbuh.de>
2015-05-12 22:06:34 +0200

    [Console] fix code style
    Prevent fabbot from complaining when the `SymfonyStyle` class or the
    `StyleInterface` are modified.

--- src/Symfony/Component/Console/Style/SymfonyStyle.php
+++ src/Symfony/Component/Console/Style/SymfonyStyle.php
@@ -52,11 +52,11 @@ class SymfonyStyle extends OutputStyle
     /**
      * Formats a message as a block of text.
      *
-     * @param string|array $messages  The message to write in the block
-     * @param string|null  $type      The block type (added in [] on first line)
-     * @param string|null  $style     The style to apply to the whole block
-     * @param string       $prefix    The prefix for the block
-     * @param bool         $padding   Whether to add vertical padding
+     * @param string|array $messages The message to write in the block
+     * @param string|null  $type     The block type (added in [] on first line)
+     * @param string|null  $style    The style to apply to the whole block
+     * @param string       $prefix   The prefix for the block
+     * @param bool         $padding  Whether to add vertical padding
      */
     public function block($messages, $type = null, $style = null, $prefix = ' ', $padding = false)
     {

ca91662f125b03f21147957c869f9a096d3a4ac6
Gonzalo Vilaseca <gonzalo.vilaseca@gmail.com>
2015-05-15 15:28:42 +0100

    Update PdoSessionHandler.php

--- src/Symfony/Component/HttpFoundation/Session/Storage/Handler/PdoSessionHandler.php
+++ src/Symfony/Component/HttpFoundation/Session/Storage/Handler/PdoSessionHandler.php
@@ -323,7 +323,7 @@ class PdoSessionHandler implements \SessionHandlerInterface
     {
         $maxlifetime = (int) ini_get('session.gc_maxlifetime');
 
-        $metaStmnt = $this->pdo->prepare('SELECT `' . $this->dataCol . '` FROM ' . $this->table);
+        $metaStmnt = $this->pdo->prepare('SELECT `'.$this->dataCol.'` FROM '.$this->table);
         $metaStmnt->execute();
         if (strlen($data) > $metaStmnt->getColumnMeta(0)['len']) {
             throw new \DomainException('The data to be saved in session table exceeds the size of the field');

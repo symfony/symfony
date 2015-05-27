93c8f02c876550c5e5d3a8ab4e5fe3af67ffa79c
Kevin Bond <kevinbond@gmail.com>
2015-05-27 16:05:37 -0400

    Warmup twig templates in non-standard paths (closes #12507)

--- src/Symfony/Bundle/TwigBundle/DependencyInjection/TwigExtension.php
+++ src/Symfony/Bundle/TwigBundle/DependencyInjection/TwigExtension.php
@@ -76,6 +76,8 @@ class TwigExtension extends Extension
             }
         }
 
+        $container->getDefinition('twig.cache_warmer')->replaceArgument(2, $config['paths']);
+
         // register bundles as Twig namespaces
         foreach ($container->getParameter('kernel.bundles') as $bundle => $class) {
             if (is_dir($dir = $container->getParameter('kernel.root_dir').'/Resources/'.$bundle.'/views')) {

314b51dc637c6c6d258d630fa7efdda1a200f933
Gerrit Drost <mail@gerritdrost.com>
2015-06-01 18:24:07 +0200

    Issue #14815
    Removed @deprecated tags and moved their descriptions to the method description as a note.

--- src/Symfony/Component/Yaml/Yaml.php
+++ src/Symfony/Component/Yaml/Yaml.php
@@ -38,6 +38,8 @@ class Yaml
      * you must validate the input before calling this method. Passing a file
      * as an input is a deprecated feature and will be removed in 3.0.
      *
+     * Note: the ability to pass file names to the Yaml::parse method is deprecated since version 2.2 and will be removed in 3.0. Pass the YAML contents of the file instead.
+     *
      * @param string $input                  Path to a YAML file or a string containing YAML
      * @param bool   $exceptionOnInvalidType True if an exception must be thrown on invalid types false otherwise
      * @param bool   $objectSupport          True if object support is enabled, false otherwise
@@ -46,8 +48,6 @@ class Yaml
      * @return array The YAML converted to a PHP array
      *
      * @throws ParseException If the YAML is not valid
-     *
-     * @deprecated The ability to pass file names to the Yaml::parse method is deprecated since version 2.2 and will be removed in 3.0. Pass the YAML contents of the file instead.
      *
      * @api
      */

746cf987ac416081e960138cc8d738293e9b8faf
Zander Baldwin <hello@zanderbaldwin.com>
2015-06-02 12:05:17 +0100

    [2.7][SecurityBundle] Remove SecurityContext from Compile
    Remove the deprecated SecurityContext from the list of classes to compile into `classes.map` and `classes.php`.

--- src/Symfony/Bundle/SecurityBundle/DependencyInjection/SecurityExtension.php
+++ src/Symfony/Bundle/SecurityBundle/DependencyInjection/SecurityExtension.php
@@ -100,7 +100,6 @@ class SecurityExtension extends Extension
         // add some required classes for compilation
         $this->addClassesToCompile(array(
             'Symfony\\Component\\Security\\Http\\Firewall',
-            'Symfony\\Component\\Security\\Core\\SecurityContext',
             'Symfony\\Component\\Security\\Core\\User\\UserProviderInterface',
             'Symfony\\Component\\Security\\Core\\Authentication\\AuthenticationProviderManager',
             'Symfony\\Component\\Security\\Core\\Authentication\\Token\\Storage\\TokenStorage',

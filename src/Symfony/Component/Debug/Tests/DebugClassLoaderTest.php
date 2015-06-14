8d68502ed9cacdde9505753fda73c44aad940653
Martin Hujer <mhujer@gmail.com>
2015-06-14 19:02:39 +0200

    Fix [Debug] tests on Windows
    If the file loads properly on a case insensitive filesystem (on Windows),
    the test should be skipped, because the exception won't be thrown.
    
    @stof even commented on this change before https://github.com/symfony/symfony/pull/10776/files#r11956586

--- src/Symfony/Component/Debug/Tests/DebugClassLoaderTest.php
+++ src/Symfony/Component/Debug/Tests/DebugClassLoaderTest.php
@@ -133,7 +133,8 @@ class DebugClassLoaderTest extends \PHPUnit_Framework_TestCase
      */
     public function testFileCaseMismatch()
     {
-        if (!file_exists(__DIR__.'/Fixtures/CaseMismatch.php')) {
+        //Cannot be tested if the file loads (there is is casemismatch.php in the filesystem)
+        if (file_exists(__DIR__.'/Fixtures/CaseMismatch.php')) {
             $this->markTestSkipped('Can only be run on case insensitive filesystems');
         }
 

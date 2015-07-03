912fc4de8fd6de1e5397be4a94d39091423e5188
Tobias Schultze <webmaster@tubo-world.de>
2015-07-03 23:44:03 +0200

    [Routing] deprecate the old url generator reference type values

--- src/Symfony/Bundle/FrameworkBundle/Controller/Controller.php
+++ src/Symfony/Bundle/FrameworkBundle/Controller/Controller.php
@@ -39,9 +39,9 @@ class Controller extends ContainerAware
     /**
      * Generates a URL from the given parameters.
      *
-     * @param string      $route         The name of the route
-     * @param mixed       $parameters    An array of parameters
-     * @param bool|string $referenceType The type of reference (one of the constants in UrlGeneratorInterface)
+     * @param string $route         The name of the route
+     * @param mixed  $parameters    An array of parameters
+     * @param int    $referenceType The type of reference (one of the constants in UrlGeneratorInterface)
      *
      * @return string The generated URL
      *

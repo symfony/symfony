ca6b165b8dab8a757784e8abb8a91732f30cbd08
Xavier Leune <xleune@commentcamarche.net>
2015-07-06 15:00:07 +0200

    Deprecated : correct version number

--- src/Symfony/Component/HttpFoundation/Request.php
+++ src/Symfony/Component/HttpFoundation/Request.php
@@ -848,7 +848,7 @@ class Request
      */
     public function getClientIps()
     {
-        @trigger_error('The '.__METHOD__.' method is deprecated since version 2.7 and will be removed in 3.0. Use the ip_retriever service instead.', E_USER_DEPRECATED);
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 2.8 and will be removed in 3.0. Use the ip_retriever service instead.', E_USER_DEPRECATED);
         if (null === $this->ipRetriever) {
             $this->getDefaultIpRetriever();
         }
@@ -873,7 +873,7 @@ class Request
      */
     public function getClientIp()
     {
-        @trigger_error('The '.__METHOD__.' method is deprecated since version 2.7 and will be removed in 3.0. Use the ip_retriever service instead.', E_USER_DEPRECATED);
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 2.8 and will be removed in 3.0. Use the ip_retriever service instead.', E_USER_DEPRECATED);
         if (null === $this->ipRetriever) {
             $this->getDefaultIpRetriever();
         }
@@ -1988,7 +1988,7 @@ class Request
      */
     public function setIpRetriever(IpRetrieverInterface $ipRetriever)
     {
-        @trigger_error('The '.__METHOD__.' method is deprecated since version 2.7 and will be removed in 3.0. Use the ip_retriever service instead.', E_USER_DEPRECATED);
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 2.8 and will be removed in 3.0. Use the ip_retriever service instead.', E_USER_DEPRECATED);
         $this->ipRetriever = $ipRetriever;
     }
 
@@ -1999,7 +1999,7 @@ class Request
      */
     private function getDefaultIpRetriever()
     {
-        @trigger_error('The '.__METHOD__.' method is deprecated since version 2.7 and will be removed in 3.0. Use the ip_retriever service instead.', E_USER_DEPRECATED);
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 2.8 and will be removed in 3.0. Use the ip_retriever service instead.', E_USER_DEPRECATED);
         if (null === $this->ipRetriever) {
             $this->ipRetriever = new IpRetriever();
         }

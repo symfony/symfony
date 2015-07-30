2fb33191fe71430ac42528deeb5d5855ae7e4ac8
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-07-30 09:30:53 +0200

    Removed an unused import

--- src/Symfony/Bundle/WebProfilerBundle/Controller/ProfilerController.php
+++ src/Symfony/Bundle/WebProfilerBundle/Controller/ProfilerController.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Flash\AutoExpireFlashBag;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\HttpKernel\Profiler\Profiler;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 

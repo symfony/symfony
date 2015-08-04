b913695682f88c483748d8d390118c5528c625b3
Marcos Sánchez <marcosdsanchez@gmail.com>
2015-08-04 16:28:45 -0300

    Add support for target attributes.
    The previous implementation ignored attributes in target nodes in xliff files
    so they were lost when you load and then dump the same file. This change
    should fix that problem.

--- src/Symfony/Component/Translation/Tests/Dumper/XliffFileDumperTest.php
+++ src/Symfony/Component/Translation/Tests/Dumper/XliffFileDumperTest.php
@@ -38,4 +38,39 @@ class XliffFileDumperTest extends \PHPUnit_Framework_TestCase
 
         unlink($tempDir.'/messages.en_US.xlf');
     }
+
+    public function testTargetAttributesMetadataIsSetInFile()
+    {
+        $catalogue = new MessageCatalogue('en_US');
+        $catalogue->add(array(
+            'foo' => 'bar',
+        ));
+        $catalogue->setMetadata('foo', array('target-attributes' => array('state' => 'needs-translation')));
+
+        $tempDir = sys_get_temp_dir();
+        $dumper = new XliffFileDumper();
+        $dumper->dump($catalogue, array('path' => $tempDir, 'default_locale' => 'fr_FR'));
+
+        $content = <<<EOT
+<?xml version="1.0" encoding="utf-8"?>
+<xliff xmlns="urn:oasis:names:tc:xliff:document:1.2" version="1.2">
+  <file source-language="fr-FR" target-language="en-US" datatype="plaintext" original="file.ext">
+    <body>
+      <trans-unit id="acbd18db4cc2f85cedef654fccc4a4d8" resname="foo">
+        <source>foo</source>
+        <target state="needs-translation">bar</target>
+      </trans-unit>
+    </body>
+  </file>
+</xliff>
+
+EOT;
+
+        $this->assertEquals(
+            $content,
+            file_get_contents($tempDir.'/messages.en_US.xlf')
+        );
+
+        unlink($tempDir.'/messages.en_US.xlf');
+    }
 }

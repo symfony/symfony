7fdba25e4fc92e4b3add0d515061d681691c9872
Nicolas Grekas <nicolas.grekas@gmail.com>
2015-08-05 17:43:06 +0200

    Clean wrong whitespaces

--- src/Symfony/Bridge/Doctrine/HttpFoundation/DbalSessionHandler.php
+++ src/Symfony/Bridge/Doctrine/HttpFoundation/DbalSessionHandler.php
@@ -161,10 +161,10 @@ class DbalSessionHandler implements \SessionHandlerInterface
                 $mergeStmt->bindValue(':time', time(), \PDO::PARAM_INT);
 
                 //Oracle has a bug that will intermitently happen if you
-                //have only 1 bind on a CLOB field for 2 different statements 
+                //have only 1 bind on a CLOB field for 2 different statements
                 //(INSERT and UPDATE in this case)
                 if ('oracle' == $this->con->getDatabasePlatform()->getName()) {
-                	$mergeStmt->bindParam(':data2', $encoded, \PDO::PARAM_STR);
+                    $mergeStmt->bindParam(':data2', $encoded, \PDO::PARAM_STR);
                 }
 
                 $mergeStmt->execute();

fd9613c9630bc7e7364c3d7be71e3a3851bcd054
SaschaDens <sascha.dens@telenet.be>
2015-08-08 08:08:31 +0200

    [VarDumper] fixed HtmlDumper to target specific the head tag

--- src/Symfony/Component/VarDumper/Dumper/HtmlDumper.php
+++ src/Symfony/Component/VarDumper/Dumper/HtmlDumper.php
@@ -125,7 +125,7 @@ var refStyle = doc.createElement('style'),
     rxEsc = /([.*+?^${}()|\[\]\/\\])/g,
     idRx = /\bsf-dump-\d+-ref[012]\w+\b/;
 
-doc.documentElement.firstChild.appendChild(refStyle);
+doc.getElementsByTagName('head')[0].appendChild(refStyle);
 
 function toggle(a) {
     var s = a.nextSibling || {};

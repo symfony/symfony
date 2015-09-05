3f9424fc9b3ef9b23164597107ae25d8df2d2732
Matthias Pigulla <mp@webfactory.de>
2015-09-05 10:41:26 +0200

    Add docblock

--- src/Symfony/Component/Config/MetadataValidatorInterface.php
+++ src/Symfony/Component/Config/MetadataValidatorInterface.php
@@ -12,15 +12,36 @@
 namespace Symfony\Component\Config;
 
 /**
- * Base interface to test arbitrary metadata for freshness.
+ * Interface for MetadataValidators.
+ *
+ * When a ConfigCache instance is checked for freshness, all its associated
+ * metadata resources are passed to MetadataValidators. The MetadataValidators
+ * can then inspect the resources and decide whether the cache can be considered
+ * fresh or not.
  *
  * @author Benjamin Klotz <bk@webfactory.de>
  */
 interface MetadataValidatorInterface
 {
 
+    /**
+     * Queries the MetadataValidator whether it can validate a given
+     * resource or not.
+     *
+     * @param $metadata The resource to be checked for freshness
+     *
+     * @return bool True if the MetadataValidator can handle this resource type, false if not
+     */
     public function supports($metadata);
 
+    /**
+     * Validates the resource.
+     *
+     * @param $metadata The resource to be validated.
+     * @param $timestamp The timestamp at which the cache associated with this resource was created.
+     *
+     * @return bool True if the resource has not changed since the given timestamp, false otherwise.
+     */
     public function isFresh($metadata, $timestamp);
 
 }

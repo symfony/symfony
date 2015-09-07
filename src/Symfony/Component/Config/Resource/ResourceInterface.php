e47189097670843bf3ee50beae58edf5ed7df0ab
Matthias Pigulla <mp@webfactory.de>
2015-09-07 10:34:37 +0200

    Add a note why the ResourceInterface::__toString method is important and unlikely to disappear in the future

--- src/Symfony/Component/Config/Resource/ResourceInterface.php
+++ src/Symfony/Component/Config/Resource/ResourceInterface.php
@@ -21,7 +21,13 @@ interface ResourceInterface
     /**
      * Returns a string representation of the Resource.
      *
-     * @return string A string representation of the Resource
+     * This method is necessary to allow for resource de-duplication, for example by means
+     * of array_unique(). The string returned need not have a particular meaning, but has
+     * to be identical for different ResourceInterface instances referring to the same
+     * resource; and it should be unlikely to collide with that of other, unrelated
+     * resource instances.
+     *
+     * @return string A string representation unique to the underlying Resource
      */
     public function __toString();
 

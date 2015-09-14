a845b3cef9a1fbc20957e2a6574f8e502d711ac3
Matthias Pigulla <mp@webfactory.de>
2015-09-14 16:39:54 +0200

    CS fix / License added

--- src/Symfony/Component/Config/Tests/Resource/ResourceStub.php
+++ src/Symfony/Component/Config/Tests/Resource/ResourceStub.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Component\Config\Tests\Resource;
 
 use Symfony\Component\Config\Resource\SelfCheckingResourceInterface;
@@ -13,7 +22,8 @@ class ResourceStub implements SelfCheckingResourceInterface
         $this->fresh = $isFresh;
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         return 'stub';
     }
 

2c4da3ceff3e27463b87767d2c2516f9075f54c6
WouterJ <waldio.webdesign@gmail.com>
2015-09-25 09:39:51 +0200

    Merged LegacySecurityContext tests

--- src/Symfony/Component/Security/Core/Tests/LegacySecurityContextTest.php
+++ src/Symfony/Component/Security/Core/Tests/LegacySecurityContextTest.php
@@ -119,4 +119,14 @@ class LegacySecurityContextTest extends \PHPUnit_Framework_TestCase
             array(true, null),
         );
     }
+
+    /**
+     * Test if the BC Layer is working as intended.
+     */
+    public function testConstantSync()
+    {
+        $this->assertSame(Security::ACCESS_DENIED_ERROR, SecurityContextInterface::ACCESS_DENIED_ERROR);
+        $this->assertSame(Security::AUTHENTICATION_ERROR, SecurityContextInterface::AUTHENTICATION_ERROR);
+        $this->assertSame(Security::LAST_USERNAME, SecurityContextInterface::LAST_USERNAME);
+    }
 }

e32fcc0aa3e7b8217fb84292cf83bc72a70137b5
Michal Trojanowski <michal@flep.pl>
2015-09-27 23:02:53 +0200

    Added UserLoaderInterface for loading users through Doctrine.

---
+++ src/Symfony/Bridge/Doctrine/Security/User/UserLoaderInterface.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bridge\Doctrine\Security\User;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+
+/**
+ * Represents a class that loads UserInterface objects from Doctrine source for the authentication system.
+ *
+ * This interface is meant to facilitate the loading of a User from Doctrine source using a custom method.
+ * If you want to implement your own logic of retrieving the user from Doctrine your repository should implement this
+ * interface.
+ *
+ * @see UserInterface
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+interface UserLoaderInterface
+{
+    /**
+     * Loads the user for the given username.
+     *
+     * This method must throw UsernameNotFoundException if the user is not
+     * found.
+     *
+     * @param string $username The username
+     *
+     * @return UserInterface
+     *
+     * @see UsernameNotFoundException
+     *
+     * @throws UsernameNotFoundException if the user is not found
+     */
+    public function loadUserByUsername($username);
+}

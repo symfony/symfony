7720f5c70bc079dac3d86612da98157fbbbc7727
Gábor Egyed <1ed@mailbox.hu>
2015-09-28 22:51:14 +0200

    [Console] Fix tests for SymfonyStyle

--- src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_1.php
+++ src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_1.php
@@ -2,11 +2,11 @@
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Tests\Style\SymfonyStyleWithForcedLineLength;
 
 //Ensure has single blank line between titles and blocks
 return function (InputInterface $input, OutputInterface $output) {
-    $output = new SymfonyStyle($input, $output);
+    $output = new SymfonyStyleWithForcedLineLength($input, $output);
     $output->title('Title');
     $output->warning('Lorem ipsum dolor sit amet');
     $output->title('Title');
--- src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_2.php
+++ src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_2.php
@@ -2,11 +2,11 @@
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Tests\Style\SymfonyStyleWithForcedLineLength;
 
 //Ensure has single blank line between blocks
 return function (InputInterface $input, OutputInterface $output) {
-    $output = new SymfonyStyle($input, $output);
+    $output = new SymfonyStyleWithForcedLineLength($input, $output);
     $output->warning('Warning');
     $output->caution('Caution');
     $output->error('Error');

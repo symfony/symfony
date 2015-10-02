c92809a1fdc0e49f9f28107f2fdfa1c64d580fdd
Nicolas Grekas <nicolas.grekas@gmail.com>
2015-10-02 18:35:24 +0200

    [Form] minor CS fix

--- src/Symfony/Component/Form/Extension/DependencyInjection/DependencyInjectionExtension.php
+++ src/Symfony/Component/Form/Extension/DependencyInjection/DependencyInjectionExtension.php
@@ -25,9 +25,7 @@ class DependencyInjectionExtension implements FormExtensionInterface
     private $guesserLoaded = false;
     private $typeExtensionServiceIds;
 
-    public function __construct(ContainerInterface $container,
-        array $typeServiceIds, array $typeExtensionServiceIds,
-        array $guesserServiceIds)
+    public function __construct(ContainerInterface $container, array $typeServiceIds, array $typeExtensionServiceIds, array $guesserServiceIds)
     {
         $this->container = $container;
         $this->typeServiceIds = $typeServiceIds;

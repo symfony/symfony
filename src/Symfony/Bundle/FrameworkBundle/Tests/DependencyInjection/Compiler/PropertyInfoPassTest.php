4705b21ef90b8fde5b94ab352f5fea1fde984919
Kévin Dunglas <dunglas@gmail.com>
2015-10-14 17:26:32 +0200

    Fix @jakzal comment

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Compiler/PropertyInfoPassTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Compiler/PropertyInfoPassTest.php
@@ -32,7 +32,7 @@ class PropertyInfoPassTest extends \PHPUnit_Framework_TestCase
 
         $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerBuilder', array('findTaggedServiceIds'));
 
-        $container->expects($this->atLeastOnce())
+        $container->expects($this->any())
             ->method('findTaggedServiceIds')
             ->will($this->returnValue($services));
 
@@ -53,7 +53,7 @@ class PropertyInfoPassTest extends \PHPUnit_Framework_TestCase
     {
         $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerBuilder', array('findTaggedServiceIds'));
 
-        $container->expects($this->atLeastOnce())
+            $container->expects($this->any())
             ->method('findTaggedServiceIds')
             ->will($this->returnValue(array()));
 

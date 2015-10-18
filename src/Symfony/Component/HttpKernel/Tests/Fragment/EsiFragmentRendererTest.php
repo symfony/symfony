0b489aa6cc271e74ffa63216210f57eb473e3e1a
Jakub Zalas <jakub@zalas.pl>
2015-10-18 21:42:01 +0100

    [HttpKernel] Make the behaviour of surrogate renderers consistsent in all environments
    Surrogate renderers cannot work with objects. As renderers should be interchangable, we should not allow passing objects to the fallback renderer either.

--- src/Symfony/Component/HttpKernel/Tests/Fragment/EsiFragmentRendererTest.php
+++ src/Symfony/Component/HttpKernel/Tests/Fragment/EsiFragmentRendererTest.php
@@ -90,6 +90,20 @@ class EsiFragmentRendererTest extends \PHPUnit_Framework_TestCase
         $strategy->render('/', $request, array('alt' => new ControllerReference('alt_controller')));
     }
 
+    /**
+     * @expectedException \LogicException
+     */
+    public function testRenderFallbackWithObjectAttributesThrowsException()
+    {
+        $strategy = new EsiFragmentRenderer(new Esi(), $this->getInlineStrategy(), new UriSigner('foo'));
+
+        $request = Request::create('/');
+
+        $reference = new ControllerReference('main_controller', array('foo' => new \stdClass()), array());
+
+        $strategy->render($reference, $request);
+    }
+
     private function getInlineStrategy($called = false)
     {
         $inline = $this->getMockBuilder('Symfony\Component\HttpKernel\Fragment\InlineFragmentRenderer')->disableOriginalConstructor()->getMock();

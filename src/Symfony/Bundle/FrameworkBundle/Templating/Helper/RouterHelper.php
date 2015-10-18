97d6828b51c7a3dd316a03d9a9b6d2a83b7481fd
Tobias Schultze <webmaster@tubo-world.de>
2015-10-18 20:03:06 +0200

    [Templating] introduce path and url methods in php templates to be in line with twig templates

--- src/Symfony/Bundle/FrameworkBundle/Templating/Helper/RouterHelper.php
+++ src/Symfony/Bundle/FrameworkBundle/Templating/Helper/RouterHelper.php
@@ -50,6 +50,38 @@ class RouterHelper extends Helper
     }
 
     /**
+     * Generates a URL reference (as an absolute or relative path) to the route with the given parameters.
+     *
+     * @param string $name       The name of the route
+     * @param mixed  $parameters An array of parameters
+     * @param bool   $relative   Whether to generate a relative or absolute path
+     *
+     * @return string The generated URL reference
+     *
+     * @see UrlGeneratorInterface
+     */
+    public function path($name, $parameters = array(), $relative = false)
+    {
+        return $this->generator->generate($name, $parameters, $relative ? UrlGeneratorInterface::RELATIVE_PATH : UrlGeneratorInterface::ABSOLUTE_PATH);
+    }
+
+    /**
+     * Generates a URL reference (as an absolute URL or network path) to the route with the given parameters.
+     *
+     * @param string $name           The name of the route
+     * @param mixed  $parameters     An array of parameters
+     * @param bool   $schemeRelative Whether to omit the scheme in the generated URL reference
+     *
+     * @return string The generated URL reference
+     *
+     * @see UrlGeneratorInterface
+     */
+    public function url($name, $parameters = array(), $schemeRelative = false)
+    {
+        return $this->generator->generate($name, $parameters, $schemeRelative ? UrlGeneratorInterface::NETWORK_PATH : UrlGeneratorInterface::ABSOLUTE_URL);
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function getName()

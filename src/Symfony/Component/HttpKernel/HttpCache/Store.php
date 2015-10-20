3f944d60d88f0f84ec72eb5e55985f9718609417
Evgeniy Sokolov <ewgraf@gmail.com>
2015-10-20 10:09:17 +0200

    fix race condition on HttpCache store

--- src/Symfony/Component/HttpKernel/HttpCache/Store.php
+++ src/Symfony/Component/HttpKernel/HttpCache/Store.php
@@ -32,12 +32,16 @@ class Store implements StoreInterface
      * Constructor.
      *
      * @param string $root The path to the cache directory
+     *
+     * @throws \RuntimeException
      */
     public function __construct($root)
     {
         $this->root = $root;
         if (!is_dir($this->root)) {
-            mkdir($this->root, 0777, true);
+            if (false === @mkdir($this->root, 0777, true) && !is_dir($this->root)) {
+                throw new \RuntimeException(sprintf('Unable to create the store directory (%s).', $this->root));
+            }
         }
         $this->keyCache = new \SplObjectStorage();
         $this->locks = array();

b4dd25477423398cda1d24bc728d0ab1eaaa9b3e
Nicolas Grekas <nicolas.grekas@gmail.com>
2015-11-03 15:53:03 +0100

    [travis] session.gc_probability=0 to fix transient tests on hhvm

--- src/Symfony/Component/HttpKernel/Tests/Fragment/FragmentHandlerTest.php
+++ src/Symfony/Component/HttpKernel/Tests/Fragment/FragmentHandlerTest.php
@@ -14,12 +14,20 @@ namespace Symfony\Component\HttpKernel\Tests\Fragment;
 use Symfony\Component\HttpKernel\Fragment\FragmentHandler;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bridge\PhpUnit\ClockMock;
 
 /**
  * @group time-sensitive
  */
 class FragmentHandlerTest extends \PHPUnit_Framework_TestCase
 {
+    protected function setUp()
+    {
+        if (class_exists('Symfony\Bridge\PhpUnit\ClockMock')) {
+            ClockMock::register('Symfony\Component\HttpFoundation\Request');
+        }
+    }
+
     /**
      * @expectedException \InvalidArgumentException
      */

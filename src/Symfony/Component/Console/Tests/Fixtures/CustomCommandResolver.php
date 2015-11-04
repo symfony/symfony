d883f899ec2ea5e9c57878edcdd814efb8dd5495
Ivan Scherbak <dev@funivan.com>
2015-11-04 09:48:15 +0200

    fix code style and phpdoc

--- src/Symfony/Component/Console/Tests/Fixtures/CustomCommandResolver.php
+++ src/Symfony/Component/Console/Tests/Fixtures/CustomCommandResolver.php
@@ -19,123 +19,107 @@ use Symfony\Component\Console\CommandsResolver\CommandResolverInterface;
  */
 class CustomCommandResolver implements CommandResolverInterface
 {
-  /**
-   * Cache commands.
-   *
-   * @var array
-   */
-  private $commands = array();
+    /**
+     * Cache commands.
+     *
+     * @var array
+     */
+    private $commands = array();
 
-  /**
-   * @var array
-   */
-  private static $commandsMap = array(
-    'lazyTest' => '\Symfony\Component\Console\Tests\Fixtures\LazyTestCommand',
-  );
+    /**
+     * @var array
+     */
+    private static $commandsMap = array(
+      'lazyTest' => '\Symfony\Component\Console\Tests\Fixtures\LazyTestCommand',
+    );
 
-  /**
-   * @param string $commandClassName
-   *
-   * @return string|null
-   */
-  public static function getNameFromClass($commandClassName)
-  {
-      $commandClassName = '\\'.ltrim($commandClassName, '\\');
-      $name = array_search($commandClassName, self::$commandsMap);
-      if ($name === false) {
-          return;
-      }
+    /**
+     * @param string $commandClassName
+     *
+     * @return string|null
+     */
+    public static function getNameFromClass($commandClassName)
+    {
+        $commandClassName = '\\'.ltrim($commandClassName, '\\');
+        $name = array_search($commandClassName, self::$commandsMap);
+        if ($name === false) {
+            return;
+        }
 
-      return $name;
-  }
+        return $name;
+    }
 
-  /**
-   * @param string $name
-   *
-   * @return string|null
-   */
-  public static function getClassFromName($name)
-  {
-      return isset(self::$commandsMap[$name]) ? self::$commandsMap[$name] : null;
-  }
+    /**
+     * @param string $name
+     *
+     * @return string|null
+     */
+    public static function getClassFromName($name)
+    {
+        return isset(self::$commandsMap[$name]) ? self::$commandsMap[$name] : null;
+    }
 
-  /**
-   * @param Command $command
-   *
-   * @return $this
-   */
-  public function add(Command $command)
-  {
-      $this->commands[$command->getName()] = $command;
+    /**
+     * @inheritdoc
+     */
+    public function add(Command $command)
+    {
+        $this->commands[$command->getName()] = $command;
+    }
 
-      return $this;
-  }
+    /**
+     * @inheritdoc
+     */
+    public function has($name)
+    {
+        if (isset($this->commands[$name])) {
+            return true;
+        }
 
-  /**
-   * Check if command exist.
-   *
-   * @param string $name
-   *
-   * @return bool
-   */
-  public function has($name)
-  {
-      if (isset($this->commands[$name])) {
-          return true;
-      }
+        return isset(self::$commandsMap[$name]);
+    }
 
-      return isset(self::$commandsMap[$name]);
-  }
+    /**
+     * @inheritdoc
+     */
+    public function get($name)
+    {
+        if (isset($this->commands[$name])) {
+            return $this->commands[$name];
+        }
 
-  /**
-   * Get command by name or alias.
-   *
-   * @param string $name
-   *
-   * @return Command|null
-   */
-  public function get($name)
-  {
-      if (isset($this->commands[$name])) {
-          return $this->commands[$name];
-      }
+        $class = self::getClassFromName($name);
+        if (empty($class)) {
+            return;
+        }
 
-      $class = self::getClassFromName($name);
-      if (empty($class)) {
-          return;
-      }
+        return new $class();
+    }
 
-      return new $class();
-  }
+    /**
+     * @inheritdoc
+     */
+    public function getAll()
+    {
+        $items = $this->commands;
+        foreach (self::$commandsMap as $command) {
+            $items[] = new $command();
+        }
 
-  /**
-   * Return all available commands related to this application.
-   *
-   * @return Command[]
-   */
-  public function getAll()
-  {
-      $items = $this->commands;
-      foreach (self::$commandsMap as $command) {
-          $items[] = new $command();
-      }
+        return $items;
+    }
 
-      return $items;
-  }
+    /**
+     * @inheritdoc
+     */
+    public function getAllNames()
+    {
+        $names = array_keys(self::$commandsMap);
 
-  /**
-   * Return all command names and aliases.
-   *
-   * @return array
-   */
-  public function getAllNames()
-  {
-      $names = array_keys(self::$commandsMap);
+        if (!empty($this->commands)) {
+            $names = array_merge($names, array_keys($this->commands));
+        }
 
-      if (!empty($this->commands)) {
-          $names = array_merge($names, array_keys($this->commands));
-      }
-
-      return $names;
-  }
+        return $names;
+    }
 }

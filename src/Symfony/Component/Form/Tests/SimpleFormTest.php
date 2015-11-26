f495410d25e0ce7c2bf503be91b1776b4e2359ff
Bernhard Schussek <bschussek@gmail.com>
2015-11-26 10:11:54 +0100

    [Form] Disabled view data validation if "data_class" is set to null

--- src/Symfony/Component/Form/Tests/SimpleFormTest.php
+++ src/Symfony/Component/Form/Tests/SimpleFormTest.php
@@ -840,19 +840,19 @@ class SimpleFormTest extends AbstractFormTest
         $this->assertEquals(new PropertyPath('[name]'), $form->getPropertyPath());
     }
 
-    /**
-     * @expectedException \Symfony\Component\Form\Exception\LogicException
-     */
-    public function testViewDataMustNotBeObjectIfDataClassIsNull()
+    public function testViewDataMayBeObjectIfDataClassIsNull()
     {
+        $object = new \stdClass();
         $config = new FormConfigBuilder('name', null, $this->dispatcher);
         $config->addViewTransformer(new FixedDataTransformer(array(
             '' => '',
-            'foo' => new \stdClass(),
+            'foo' => $object,
         )));
         $form = new Form($config);
 
         $form->setData('foo');
+
+        $this->assertSame($object, $form->getViewData());
     }
 
     public function testViewDataMayBeArrayAccessIfDataClassIsNull()

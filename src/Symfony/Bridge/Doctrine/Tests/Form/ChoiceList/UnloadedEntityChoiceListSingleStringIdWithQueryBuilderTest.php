7d9237200fc6eb986f5f3e9fb6740e1ec29bfeaf
Nicolas Grekas <nicolas.grekas@gmail.com>
2015-11-29 10:45:59 +0100

    [Bridge/Doctrine] Fix legacy tests

--- src/Symfony/Bridge/Doctrine/Tests/Form/ChoiceList/UnloadedEntityChoiceListSingleStringIdWithQueryBuilderTest.php
+++ src/Symfony/Bridge/Doctrine/Tests/Form/ChoiceList/UnloadedEntityChoiceListSingleStringIdWithQueryBuilderTest.php
@@ -14,6 +14,10 @@ namespace Symfony\Bridge\Doctrine\Tests\Form\ChoiceList;
 use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityChoiceList;
 use Symfony\Bridge\Doctrine\Form\ChoiceList\ORMQueryBuilderLoader;
 
+if (!class_exists('Symfony\Component\Form\Tests\Extension\Core\ChoiceList\AbstractChoiceListTest')) {
+    return;
+}
+
 /**
  * @author Bernhard Schussek <bschussek@gmail.com>
  * @group legacy

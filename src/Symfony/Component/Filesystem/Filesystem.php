687a28c517506a5979f462ac0cfc64ddda0d0396
Dariusz Ruminski <d.ruminski@foodpanda.com>
2015-12-01 11:40:41 +0100

    CS: remove unneeded parentheses around control statements

--- src/Symfony/Component/Filesystem/Filesystem.php
+++ src/Symfony/Component/Filesystem/Filesystem.php
@@ -443,13 +443,13 @@ class Filesystem
      */
     public function isAbsolutePath($file)
     {
-        return (strspn($file, '/\\', 0, 1)
+        return strspn($file, '/\\', 0, 1)
             || (strlen($file) > 3 && ctype_alpha($file[0])
                 && substr($file, 1, 1) === ':'
                 && (strspn($file, '/\\', 2, 1))
             )
             || null !== parse_url($file, PHP_URL_SCHEME)
-        );
+        ;
     }
 
     /**

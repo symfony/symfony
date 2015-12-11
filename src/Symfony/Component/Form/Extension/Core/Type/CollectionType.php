20b787755e53a4154bd281834f3b4b4134449c7e
Jules Pietri <heah@heahprod.com>
2015-12-11 05:22:20 +0100

    fix-15544-alt

--- src/Symfony/Component/Form/Extension/Core/Type/CollectionType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/CollectionType.php
@@ -28,6 +28,7 @@ class CollectionType extends AbstractType
     {
         if ($options['allow_add'] && $options['prototype']) {
             $prototype = $builder->create($options['prototype_name'], $options['type'], array_replace(array(
+                'required' => $options['required'],
                 'label' => $options['prototype_name'].'label__',
             ), $options['options']));
             $builder->setAttribute('prototype', $prototype->getForm());
@@ -55,7 +56,6 @@ class CollectionType extends AbstractType
 
         if ($form->getConfig()->hasAttribute('prototype')) {
             $view->vars['prototype'] = $form->getConfig()->getAttribute('prototype')->createView($view);
-            $view->vars['prototype']->vars['required'] = $options['required'];
         }
     }
 

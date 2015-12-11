b4b5d63660cd1509c39a4458b83c475db0dabfb8
Jules Pietri <heah@heahprod.com>
2015-12-11 03:19:25 +0100

    [Form] fix #15544 when a collection type attribute "required" is false, "prototype" should too

--- src/Symfony/Component/Form/Extension/Core/Type/CollectionType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/CollectionType.php
@@ -28,6 +28,7 @@ class CollectionType extends AbstractType
     {
         if ($options['allow_add'] && $options['prototype']) {
             $prototype = $builder->create($options['prototype_name'], $options['type'], array_replace(array(
+                'required' => $options['required'],
                 'label' => $options['prototype_name'].'label__',
             ), $options['options']));
             $builder->setAttribute('prototype', $prototype->getForm());

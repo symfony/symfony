e74fe0c82384e7cdd313c01741274c1aa0a8bbd7
Wouter de Wild <wildewouter@gmail.com>
2016-01-14 14:35:35 +0100

    Added php 5.5 annotation

--- src/Symfony/Component/HttpFoundation/Tests/CookieTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/CookieTest.php
@@ -85,6 +85,9 @@ class CookieTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expire->format('U'), $cookie->getExpiresTime(), '->getExpiresTime() returns the expire date');
     }
 
+    /**
+     * @requires PHP 5.5
+     */
     public function testConstructorWithDateTimeImmutable()
     {
         $expire = new \DateTimeImmutable();

d41518fdaca1e4f98cd1e1722b53ea160e72940e
Joel Wurtz <jwurtz@jolicode.com>
2016-01-23 22:37:58 +0100

    Add normalizer generator

--- src/Symfony/Component/AstGenerator/Normalizer/NormalizerGenerator.php
+++ src/Symfony/Component/AstGenerator/Normalizer/NormalizerGenerator.php
@@ -11,14 +11,177 @@
 
 namespace Symfony\Component\AstGenerator\Normalizer;
 
+use PhpParser\Node\Name;
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Scalar;
 use Symfony\Component\AstGenerator\AstGeneratorInterface;
 
 /**
- * Generate a Normalizer given a
+ * Generate a Normalizer given a Class
  *
  * @author Joel Wurtz <joel.wurtz@gmail.com>
  */
 class NormalizerGenerator implements AstGeneratorInterface
 {
+    /** @var AstGeneratorInterface Generator which generate the statements for normalization of a given class */
+    protected $normalizeStatementsGenerator;
 
+    /** @var AstGeneratorInterface Generator which generate the statements for denormalization of a given class */
+    protected $denormalizeStatementsGenerator;
+
+    /**
+     * NormalizerGenerator constructor.
+     *
+     * @param AstGeneratorInterface $normalizeStatementsGenerator   Generator which generate the statements for normalization of a given class
+     * @param AstGeneratorInterface $denormalizeStatementsGenerator Generator which generate the statements for denormalization of a given class
+     */
+    public function __construct(AstGeneratorInterface $normalizeStatementsGenerator, AstGeneratorInterface $denormalizeStatementsGenerator)
+    {
+        $this->normalizeStatementsGenerator = $normalizeStatementsGenerator;
+        $this->denormalizeStatementsGenerator = $denormalizeStatementsGenerator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function generate($object, array $context = [])
+    {
+        if (!isset($context['name'])) {
+            $reflectionClass = new \ReflectionClass($object);
+            $context['name'] = $reflectionClass->getShortName().'Normalizer';
+        }
+
+        return [new Stmt\Class_(
+            new Name($context['name']),
+            [
+                'stmts' => [
+                    $this->createSupportsNormalizationMethod($object),
+                    $this->createSupportsDenormalizationMethod($object),
+                    $this->createNormalizeMethod($object, $context),
+                    $this->createDenormalizeMethod($object, $context),
+                ],
+                'implements' => [
+                    new Name('\Symfony\Component\Serializer\Normalizer\DenormalizerInterface'),
+                    new Name('\Symfony\Component\Serializer\Normalizer\NormalizerInterface')
+                ],
+                'extends' => new Name('\Symfony\Component\Serializer\Normalizer\SerializerAwareNormalizer')
+            ]
+        )];
+    }
+
+    /**
+     * Create method to check if normalization is supported
+     *
+     * @param string $class Fully Qualified name of the model class
+     *
+     * @return Stmt\ClassMethod
+     */
+    protected function createSupportsNormalizationMethod($class)
+    {
+        if (strpos($class, '\\') !== 0) {
+            $class = '\\'.$class;
+        }
+
+        return new Stmt\ClassMethod('supportsNormalization', [
+            'type' => Stmt\Class_::MODIFIER_PUBLIC,
+            'params' => [
+                new Param('data'),
+                new Param('format', new Expr\ConstFetch(new Name("null"))),
+            ],
+            'stmts' => [
+                new Stmt\If_(
+                    new Expr\Instanceof_(new Expr\Variable('data'), new Name($class)),
+                    [
+                        'stmts' => [
+                            new Stmt\Return_(new Expr\ConstFetch(new Name("true")))
+                        ]
+                    ]
+                ),
+                new Stmt\Return_(new Expr\ConstFetch(new Name("false")))
+            ]
+        ]);
+    }
+
+    /**
+     * Create method to check if denormalization is supported
+     *
+     * @param string $class Fully Qualified name of the model class
+     *
+     * @return Stmt\ClassMethod
+     */
+    protected function createSupportsDenormalizationMethod($class)
+    {
+        return new Stmt\ClassMethod('supportsDenormalization', [
+            'type' => Stmt\Class_::MODIFIER_PUBLIC,
+            'params' => [
+                new Param('data'),
+                new Param('type'),
+                new Param('format', new Expr\ConstFetch(new Name("null"))),
+            ],
+            'stmts' => [
+                new Stmt\If_(
+                    new Expr\BinaryOp\NotIdentical(new Expr\Variable('type'), new Scalar\String_($class)),
+                    [
+                        'stmts' => [
+                            new Stmt\Return_(new Expr\ConstFetch(new Name("false")))
+                        ]
+                    ]
+                ),
+                new Stmt\Return_(new Expr\ConstFetch(new Name("true")))
+            ]
+        ]);
+    }
+
+    /**
+     * Create the normalization method
+     *
+     * @param string $class   Class to create normalization from
+     * @param array  $context Context of generation
+     *
+     * @return Stmt\ClassMethod
+     */
+    protected function createNormalizeMethod($class, array $context = [])
+    {
+        return new Stmt\ClassMethod('normalize', [
+            'type' => Stmt\Class_::MODIFIER_PUBLIC,
+            'params' => [
+                new Param('object'),
+                new Param('format', new Expr\ConstFetch(new Name("null"))),
+                new Param('context', new Expr\Array_(), 'array'),
+            ],
+            'stmts' => $this->normalizeStatementsGenerator->generate($class, $context)
+        ]);
+    }
+
+    /**
+     * Create the denormalization method
+     *
+     * @param string $class   Class to create denormalization from
+     * @param array  $context Context of generation
+     *
+     * @return Stmt\ClassMethod
+     */
+    protected function createDenormalizeMethod($class, array $context = [])
+    {
+        return new Stmt\ClassMethod('denormalize', [
+            'type' => Stmt\Class_::MODIFIER_PUBLIC,
+            'params' => [
+                new Param('data'),
+                new Param('class'),
+                new Param('format', new Expr\ConstFetch(new Name("null"))),
+                new Param('context', new Expr\Array_(), 'array'),
+            ],
+            'stmts' => $this->denormalizeStatementsGenerator->generate($class, $context)
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsGeneration($object)
+    {
+        return (is_string($object) && class_exists($object));
+    }
 }
---
+++ src/Symfony/Component/AstGenerator/Tests/Normalizer/NormalizerGeneratorTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\AstGenerator\Tests\Normalizer;
+
+use PhpParser\PrettyPrinter\Standard;
+use PhpParser\Node\Name;
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Scalar;
+use Symfony\Component\AstGenerator\AstGeneratorInterface;
+use Symfony\Component\AstGenerator\Normalizer\NormalizerGenerator;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+
+class NormalizerGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Standard */
+    protected $printer;
+
+    public function setUp()
+    {
+        $this->printer = new Standard();
+    }
+
+    public function testGenerateDummyNormalizer()
+    {
+        $normalizerStatementsGenerator = $this->prophesize(AstGeneratorInterface::class);
+        $normalizerStatementsGenerator->supportsGeneration(Dummy::class)->willReturn(true);
+        $normalizerStatementsGenerator->generate(Dummy::class, ['name' => 'DummyNormalizer'])->willReturn([
+            new Stmt\Return_(new Expr\New_(new Name("\\stdClass")))
+        ]);
+
+        $denormalizerStatementsGenerator = $this->prophesize(AstGeneratorInterface::class);
+        $denormalizerStatementsGenerator->supportsGeneration(Dummy::class)->willReturn(true);
+        $denormalizerStatementsGenerator->generate(Dummy::class, ['name' => 'DummyNormalizer'])->willReturn([
+            new Stmt\Return_(new Expr\New_(new Name('\\'. Dummy::class)))
+        ]);
+
+        $normalizerGenerator = new NormalizerGenerator($normalizerStatementsGenerator->reveal(), $denormalizerStatementsGenerator->reveal());
+
+        $this->assertTrue($normalizerGenerator->supportsGeneration(Dummy::class));
+
+        eval($this->printer->prettyPrint($normalizerGenerator->generate(Dummy::class)));
+
+        $this->assertTrue(class_exists('DummyNormalizer'));
+
+        $dummyNormalizer = new \DummyNormalizer();
+
+        $this->assertInstanceOf(NormalizerInterface::class, $dummyNormalizer);
+        $this->assertInstanceOf(DenormalizerInterface::class, $dummyNormalizer);
+        $this->assertInstanceOf(SerializerAwareInterface::class, $dummyNormalizer);
+
+        $this->assertTrue($dummyNormalizer->supportsNormalization(new Dummy()));
+        $this->assertTrue($dummyNormalizer->supportsDenormalization([], Dummy::class));
+
+        $normalized = $dummyNormalizer->normalize(new Dummy());
+        $denormalized = $dummyNormalizer->denormalize(new \stdClass(), Dummy::class);
+
+        $this->assertInstanceOf(\stdClass::class, $normalized);
+        $this->assertInstanceOf(Dummy::class, $denormalized);
+    }
+}
+
+class Dummy
+{
+}

fb729eecfa82979a9779a462a34ac8e30291f5f9
Fabien Potencier <fabien.potencier@gmail.com>
2016-01-25 08:27:56 +0100

    fixed CS

--- src/Symfony/Component/HttpFoundation/Response.php
+++ src/Symfony/Component/HttpFoundation/Response.php
@@ -976,12 +976,12 @@ class Response
         return $notModified;
     }
 
-    // http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-
     /**
      * Is response invalid?
      *
      * @return bool
+     *
+     * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
      */
     public function isInvalid()
     {

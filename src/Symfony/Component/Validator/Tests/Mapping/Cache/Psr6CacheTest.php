12852c2aea09284b4b76a62dc6ef26b1e03a3fba
Kévin Dunglas <dunglas@gmail.com>
2016-01-26 20:26:38 +0100

    [Validator] PSR-6 cache: Avoid name collision

--- src/Symfony/Component/Validator/Tests/Mapping/Cache/Psr6CacheTest.php
+++ src/Symfony/Component/Validator/Tests/Mapping/Cache/Psr6CacheTest.php
@@ -4,6 +4,7 @@ namespace Symfony\Component\Validator\Tests\Mapping\Cache;
 
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Validator\Mapping\Cache\Psr6Cache;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 /**
  * @author Kévin Dunglas <dunglas@gmail.com>
@@ -13,5 +14,13 @@ class Psr6CacheTest extends AbstractCacheTest
     protected function setUp()
     {
         $this->cache = new Psr6Cache(new ArrayAdapter());
+    }
+
+    public function testNameCollision()
+    {
+        $metadata = new ClassMetadata('Foo\\Bar');
+
+        $this->cache->write($metadata);
+        $this->assertFalse($this->cache->has('Foo_Bar'));
     }
 }

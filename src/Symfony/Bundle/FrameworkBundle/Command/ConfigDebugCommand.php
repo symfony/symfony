37655be008ff1a2aa2f285df15a64b6415f03862
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-01-26 09:13:20 +0100

    Increase the inlining YAML level for config:debug

--- src/Symfony/Bundle/FrameworkBundle/Command/ConfigDebugCommand.php
+++ src/Symfony/Bundle/FrameworkBundle/Command/ConfigDebugCommand.php
@@ -90,7 +90,7 @@ EOF
             $output->writeln(sprintf('# Current configuration for "%s"', $name));
         }
 
-        $output->writeln(Yaml::dump(array($extension->getAlias() => $config), 3));
+        $output->writeln(Yaml::dump(array($extension->getAlias() => $config), 10));
     }
 
     private function compileContainer()

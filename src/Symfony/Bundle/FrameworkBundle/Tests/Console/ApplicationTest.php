86c910ee6bb0ea20967940f106c123c67cda1939
Toni Uebernickel <tuebernickel@gmail.com>
2016-01-27 13:47:44 +0100

    register commands from kernel when accessing list

--- src/Symfony/Bundle/FrameworkBundle/Tests/Console/ApplicationTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Console/ApplicationTest.php
@@ -11,8 +11,9 @@
 
 namespace Symfony\Bundle\FrameworkBundle\Tests\Console;
 
-use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Tester\ApplicationTester;
@@ -38,6 +39,69 @@ class ApplicationTest extends TestCase
 
         $application = new Application($kernel);
         $application->doRun(new ArrayInput(array('list')), new NullOutput());
+
+        // Calling twice: registration should only be done once.
+        $application->doRun(new ArrayInput(array('list')), new NullOutput());
+    }
+
+    public function testBundleCommandsAreRetrievable()
+    {
+        $bundle = $this->getMock('Symfony\Component\HttpKernel\Bundle\Bundle');
+        $bundle->expects($this->once())->method('registerCommands');
+
+        $kernel = $this->getKernel(array($bundle), false);
+
+        $application = new Application($kernel);
+        $application->all();
+
+        // Calling twice: registration should only be done once.
+        $application->all();
+    }
+
+    public function testBundleSingleCommandIsRetrievable()
+    {
+        $bundle = $this->getMock('Symfony\Component\HttpKernel\Bundle\Bundle');
+        $bundle->expects($this->once())->method('registerCommands');
+
+        $kernel = $this->getKernel(array($bundle), false);
+
+        $application = new Application($kernel);
+
+        $command = new Command('example');
+        $application->add($command);
+
+        $this->assertSame($command, $application->get('example'));
+    }
+
+    public function testBundleCommandCanBeFound()
+    {
+        $bundle = $this->getMock('Symfony\Component\HttpKernel\Bundle\Bundle');
+        $bundle->expects($this->once())->method('registerCommands');
+
+        $kernel = $this->getKernel(array($bundle), false);
+
+        $application = new Application($kernel);
+
+        $command = new Command('example');
+        $application->add($command);
+
+        $this->assertSame($command, $application->find('example'));
+    }
+
+    public function testBundleCommandCanBeFoundByAlias()
+    {
+        $bundle = $this->getMock('Symfony\Component\HttpKernel\Bundle\Bundle');
+        $bundle->expects($this->once())->method('registerCommands');
+
+        $kernel = $this->getKernel(array($bundle), false);
+
+        $application = new Application($kernel);
+
+        $command = new Command('example');
+        $command->setAliases(array('alias'));
+        $application->add($command);
+
+        $this->assertSame($command, $application->find('alias'));
     }
 
     public function testBundleCommandsHaveRightContainer()
@@ -59,21 +123,23 @@ class ApplicationTest extends TestCase
         $tester->run(array('command' => 'foo'));
     }
 
-    private function getKernel(array $bundles)
+    private function getKernel(array $bundles, $withDispatcher = true)
     {
-        $dispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
-        $dispatcher
-            ->expects($this->atLeastOnce())
-            ->method('dispatch')
-        ;
-
         $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
-        $container
-            ->expects($this->atLeastOnce())
-            ->method('get')
-            ->with($this->equalTo('event_dispatcher'))
-            ->will($this->returnValue($dispatcher))
-        ;
+
+        if ($withDispatcher) {
+            $dispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+            $dispatcher
+                ->expects($this->atLeastOnce())
+                ->method('dispatch');
+
+            $container
+                ->expects($this->atLeastOnce())
+                ->method('get')
+                ->with($this->equalTo('event_dispatcher'))
+                ->will($this->returnValue($dispatcher));
+        }
+
         $container
             ->expects($this->once())
             ->method('hasParameter')

7702189cb43972f08acbca14ae524bbf5576f0cb
Christian Flothmann <christian.flothmann@xabbuh.de>
2016-01-27 08:44:59 +0100

    resolve aliases in factory services

--- src/Symfony/Component/DependencyInjection/Compiler/ResolveReferencesToAliasesPass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/ResolveReferencesToAliasesPass.php
@@ -12,6 +12,7 @@
 namespace Symfony\Component\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Alias;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -42,6 +43,7 @@ class ResolveReferencesToAliasesPass implements CompilerPassInterface
             $definition->setArguments($this->processArguments($definition->getArguments()));
             $definition->setMethodCalls($this->processArguments($definition->getMethodCalls()));
             $definition->setProperties($this->processArguments($definition->getProperties()));
+            $definition->setFactoryService($this->processFactoryService($definition->getFactoryService()));
         }
 
         foreach ($container->getAliases() as $id => $alias) {
@@ -74,6 +76,15 @@ class ResolveReferencesToAliasesPass implements CompilerPassInterface
         }
 
         return $arguments;
+    }
+
+    private function processFactoryService($factoryService)
+    {
+        if (null === $factoryService) {
+            return;
+        }
+
+        return $this->getDefinitionId($factoryService);
     }
 
     /**

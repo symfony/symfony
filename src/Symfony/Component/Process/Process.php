9f6df0365bcd153d82d81600a51c8098a1fa2b29
loostro <loostro@gmail.com>
2016-01-28 16:30:57 +0100

    Fix php doc

--- src/Symfony/Component/Process/Process.php
+++ src/Symfony/Component/Process/Process.php
@@ -129,12 +129,12 @@ class Process
     /**
      * Constructor.
      *
-     * @param string         $commandline The command line to run
-     * @param string|null    $cwd         The working directory or null to use the working dir of the current PHP process
-     * @param array|null     $env         The environment variables or null to use the same environment as the current PHP process
-     * @param string|null    $input       The input
-     * @param int|float|null $timeout     The timeout in seconds or null to disable
-     * @param array          $options     An array of options for proc_open
+     * @param string         $commandline               The command line to run
+     * @param string|null    $cwd                       The working directory or null to use the working dir of the current PHP process
+     * @param array|null     $env                       The environment variables or null to use the same environment as the current PHP process
+     * @param string|null    $input                     The input
+     * @param int|float|null $timeout                   The timeout in seconds or null to disable
+     * @param array          $options                   An array of options for proc_open
      * @param bool           $enhanceWindowsCompability Enhance windows compability
      *
      * @throws RuntimeException When proc_open is not installed

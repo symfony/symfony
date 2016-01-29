aaad5bd3d193a94b8ac02b54357bee1042ae0a5f
Benoît Burnichon <bburnichon@alchemy.fr>
2016-01-29 16:46:56 +0100

    Add check on If-Range header
    Also verify edge case where no last-modified header is available

--- src/Symfony/Component/HttpFoundation/BinaryFileResponse.php
+++ src/Symfony/Component/HttpFoundation/BinaryFileResponse.php
@@ -220,7 +220,7 @@ class BinaryFileResponse extends Response
             $this->maxlen = 0;
         } elseif ($request->headers->has('Range')) {
             // Process the range headers.
-            if (!$request->headers->has('If-Range') || $this->getEtag() === $request->headers->get('If-Range')) {
+            if (!$request->headers->has('If-Range') || $this->hasValidIfRangeHeader($request->headers->get('If-Range'))) {
                 $range = $request->headers->get('Range');
                 $fileSize = $this->file->getSize();
 
@@ -251,6 +251,19 @@ class BinaryFileResponse extends Response
         }
 
         return $this;
+    }
+
+    private function hasValidIfRangeHeader($header)
+    {
+        if ($this->getEtag() === $header) {
+            return true;
+        }
+
+        if (null === $lastModified = $this->getLastModified()) {
+            return false;
+        }
+
+        return $lastModified->format('D, d M Y H:i:s').' GMT' === $header;
     }
 
     /**

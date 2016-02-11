6d1a30e813a88b6c606c8092e0e6a0f5092ccf5f
Trent Steel <trentsteel@Trents-MBP.lan>
2016-02-11 21:12:43 +1000

    remove unnecessary retrieval and setting of data

--- src/Symfony/Component/Form/Extension/Csrf/EventListener/CsrfValidationListener.php
+++ src/Symfony/Component/Form/Extension/Csrf/EventListener/CsrfValidationListener.php
@@ -94,9 +94,10 @@ class CsrfValidationListener implements EventSubscriberInterface
     public function preSubmit(FormEvent $event)
     {
         $form = $event->getForm();
-        $data = $event->getData();
 
         if ($form->isRoot() && $form->getConfig()->getOption('compound')) {
+            $data = $event->getData();
+
             if (!isset($data[$this->fieldName]) || !$this->tokenManager->isTokenValid(new CsrfToken($this->tokenId, $data[$this->fieldName]))) {
                 $errorMessage = $this->errorMessage;
 
@@ -109,10 +110,9 @@ class CsrfValidationListener implements EventSubscriberInterface
 
             if (is_array($data)) {
                 unset($data[$this->fieldName]);
+                $event->setData($data);
             }
         }
-
-        $event->setData($data);
     }
 
     /**

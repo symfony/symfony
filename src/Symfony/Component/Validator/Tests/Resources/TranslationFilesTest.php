720779a1c0fd93368eb06cb723dbba4e7c9fd2cb
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-02-23 12:42:51 +0100

    Validate XLIFF translation files

---
+++ src/Symfony/Component/Validator/Tests/Resources/TranslationFilesTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Validator\Tests\Resources;
+
+class ConstraintTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        libxml_use_internal_errors(true);
+        libxml_clear_errors();
+    }
+
+    public function testXlfFilesAreValid()
+    {
+        $translationFiles = glob(__DIR__.'/../../Resources/translations/*.xlf');
+        foreach ($translationFiles as $filePath) {
+            $xlfFile = simplexml_load_file($filePath);
+            $errors = libxml_get_errors();
+
+            if (!empty($errors)) {
+                $this->renderErrors($errors);
+            }
+
+            $this->assertEmpty($errors, sprintf('The "%s" file is not a valid XML file.', realpath($filePath)));
+        }
+    }
+
+    private function renderErrors(array $errors)
+    {
+        $firstError = $errors[0];
+
+        echo sprintf("\nErrors found in '%s' file\n", basename($firstError->file));
+        echo sprintf("(path: %s)\n\n", realpath($firstError->file));
+
+        foreach ($errors as $error) {
+            echo sprintf("  Line %d, Column %d\n", $error->line, $error->column);
+            echo sprintf("  %s\n", $error->message);
+        }
+    }
+}

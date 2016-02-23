b58b3a83e529f8e9fb0dc15ebe092ded11dcd3cf
Nicolas Grekas <nicolas.grekas@gmail.com>
2016-02-23 08:38:51 +0100

    Fix merge

--- src/Symfony/Component/Yaml/Tests/InlineTest.php
+++ src/Symfony/Component/Yaml/Tests/InlineTest.php
@@ -273,7 +273,7 @@ class InlineTest extends \PHPUnit_Framework_TestCase
             array("'#cfcfcf'", '#cfcfcf'),
             array('::form_base.html.twig', '::form_base.html.twig'),
 
-            array('2007-10-30', mktime(0, 0, 0, 10, 30, 2007)),
+            array('2007-10-30', gmmktime(0, 0, 0, 10, 30, 2007)),
             array('2007-10-30T02:59:43Z', gmmktime(2, 59, 43, 10, 30, 2007)),
             array('2007-10-30 02:59:43 Z', gmmktime(2, 59, 43, 10, 30, 2007)),
             array('1960-10-30 02:59:43 Z', gmmktime(2, 59, 43, 10, 30, 1960)),

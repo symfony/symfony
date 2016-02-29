d4887f97575dd56ef30ac87fb9af461e4becfb18
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-02-29 08:51:06 +0100

    Refactored the solution

--- src/Symfony/Component/Routing/Matcher/TraceableUrlMatcher.php
+++ src/Symfony/Component/Routing/Matcher/TraceableUrlMatcher.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Component\Routing\Matcher;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Exception\ExceptionInterface;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
@@ -35,16 +36,18 @@ class TraceableUrlMatcher extends UrlMatcher
         try {
             $this->match($pathinfo);
         } catch (ExceptionInterface $e) {
-        } catch (\Exception $e) {
-            $this->traces = array(array(
-                'level' => -1,
-                'log' => sprintf('[ERROR] The following exception prevented the route to be matched against application routes: "%s" (in %s line %d)', $e->getMessage(), $e->getFile(), $e->getLine()),
-                'name' => '-',
-                'path' => '-',
-            ));
         }
 
         return $this->traces;
+    }
+
+    public function getTracesFromRequest(Request $request)
+    {
+        $this->request = $request;
+        $traces = $this->getTraces($request->getPathInfo());
+        $this->request = null;
+
+        return $traces;
     }
 
     protected function matchCollection($pathinfo, RouteCollection $routes)

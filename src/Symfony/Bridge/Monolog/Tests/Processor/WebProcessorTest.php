2a811427c0295e724572ada2ae1d157506e1c62f
Fabien Potencier <fabien.potencier@gmail.com>
2016-03-01 12:08:40 +0100

    fixed issue with PHP 5.3

--- src/Symfony/Bridge/Monolog/Tests/Processor/WebProcessorTest.php
+++ src/Symfony/Bridge/Monolog/Tests/Processor/WebProcessorTest.php
@@ -36,7 +36,7 @@ class WebProcessorTest extends \PHPUnit_Framework_TestCase
 
     public function testUseRequestClientIp()
     {
-        Request::setTrustedProxies(['192.168.0.1']);
+        Request::setTrustedProxies(array('192.168.0.1'));
         list($event, $server) = $this->createRequestEvent(array('X_FORWARDED_FOR' => '192.168.0.2'));
 
         $processor = new WebProcessor();

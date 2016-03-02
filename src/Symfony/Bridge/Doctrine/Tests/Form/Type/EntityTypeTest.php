symfony/symfony

commit 2336d5c466325c41b3992d8d3eb0e26d2c4230fa

fix choice_value option in EntityType and add some tests

src/Symfony/Bridge/Doctrine/Form/ChoiceList/DoctrineChoiceLoader.php
@@ -146,7 +146,7 @@ class DoctrineChoiceLoader implements ChoiceLoaderInterface
 
         // Optimize performance in case we have an object loader and
         // a single-field identifier
-        if (!$this->choiceList && $this->objectLoader && $this->idReader->isSingleId()) {
+        if (null === $value && !$this->choiceList && $this->objectLoader && $this->idReader->isSingleId()) {
             $unorderedObjects = $this->objectLoader->getEntitiesByIds($this->idReader->getIdField(), $values);
             $objectsById = array();
             $objects = array();

src/Symfony/Bridge/Doctrine/Tests/Form/Type/EntityTypeTest.php
@@ -740,6 +740,55 @@ class EntityTypeTest extends TypeTestCase
         $this->assertSame('2', $field->getViewData());
     }
 
+    public function testOverrideChoicesValues()
+    {
+        $entity1 = new SingleIntIdEntity(1, 'Foo');
+        $entity2 = new SingleIntIdEntity(2, 'Bar');
+
+        $this->persist(array($entity1, $entity2));
+
+        $field = $this->factory->createNamed('name', 'entity', null, array(
+            'em' => 'default',
+            'class' => self::SINGLE_IDENT_CLASS,
+            'choice_label' => 'name',
+            'choice_value' => 'name',
+        ));
+
+        $field->submit('Bar');
+
+        $this->assertEquals(array('Foo' => new ChoiceView($entity1, 'Foo', 'Foo'), 'Bar' => new ChoiceView($entity2, 'Bar', 'Bar')), $field->createView()->vars['choices']);
+        $this->assertTrue($field->isSynchronized(), 'Field should be synchronized.');
+        $this->assertSame($entity2, $field->getData(), 'Entity should be loaded by custom value.');
+        $this->assertSame('Bar', $field->getViewData());
+    }
+
+    public function testOverrideChoicesValuesWithCallable()
+    {
+        $entity1 = new GroupableEntity(1, 'Foo', 'BazGroup');
+        $entity2 = new GroupableEntity(2, 'Bar', 'BooGroup');
+
+        $this->persist(array($entity1, $entity2));
+
+        $field = $this->factory->createNamed('name', 'entity', null, array(
+            'em' => 'default',
+            'class' => self::ITEM_GROUP_CLASS,
+            'choice_label' => 'name',
+            'choice_value' => function (GroupableEntity $entity) {
+                return $entity->groupName.'/'.$entity->name;
+            },
+        ));
+
+        $field->submit('BooGroup/Bar');
+
+        $this->assertEquals(array(
+            'BazGroup/Foo' => new ChoiceView($entity1, 'BazGroup/Foo', 'Foo'),
+            'BooGroup/Bar' => new ChoiceView($entity2, 'BooGroup/Bar', 'Bar'),
+            ), $field->createView()->vars['choices']);
+        $this->assertTrue($field->isSynchronized(), 'Field should be synchronized.');
+        $this->assertSame($entity2, $field->getData(), 'Entity should be loaded by custom value.');
+        $this->assertSame('BooGroup/Bar', $field->getViewData());
+    }
+
     public function testGroupByChoices()
     {
         $item1 = new GroupableEntity(1, 'Foo', 'Group1');

commit c68057976d35787eb7dda582701009c6d204c6ac

[DoctrineBridge][Form] Fix performance regression in EntityType
A performance regression was introduced in
https://github.com/symfony/symfony/commit/2336d5c466325c41b3992d8d3eb0e26d2c4230fa

Before, the default behaviour of the `DoctrineLoader` was to
only fetch the entities selected in the submitted form.

After, the optimization was only performed when the `choice_value`
option was set to `null`.
However, the `DoctrineType` sets a non-null default value to `choice_value`,
which means that the default behaviour was not using the optimization
anymore.

This commit restores the default behaviour (while keeping the previous
commit intent).

References:
- https://github.com/symfony/symfony/blob/v2.7.10/src/Symfony/Bridge/Doctrine/Form/ChoiceList/DoctrineChoiceLoader.php#L149
- https://github.com/symfony/symfony/blob/v2.7.10/src/Symfony/Bridge/Doctrine/Form/Type/DoctrineType.php#L216

src/Symfony/Bridge/Doctrine/Form/ChoiceList/DoctrineChoiceLoader.php
@@ -146,7 +146,7 @@ class DoctrineChoiceLoader implements ChoiceLoaderInterface
 
         // Optimize performance in case we have an object loader and
         // a single-field identifier
-        if (null === $value && !$this->choiceList && $this->objectLoader && $this->idReader->isSingleId()) {
+        if (!($value instanceof \Closure) && !$this->choiceList && $this->objectLoader && $this->idReader->isSingleId()) {
             $unorderedObjects = $this->objectLoader->getEntitiesByIds($this->idReader->getIdField(), $values);
             $objectsById = array();
             $objects = array();

src/Symfony/Bridge/Doctrine/Tests/Form/Type/EntityTypeTest.php
@@ -802,6 +802,30 @@ class EntityTypeTest extends TypeTestCase
         $this->assertSame('BooGroup/Bar', $field->getViewData());
     }
 
+    public function testChoicesForValuesOptimization()
+    {
+        $entity1 = new SingleIntIdEntity(1, 'Foo');
+        $entity2 = new SingleIntIdEntity(2, 'Bar');
+
+        $this->persist(array($entity1, $entity2));
+
+        $field = $this->factory->createNamed('name', 'Symfony\Bridge\Doctrine\Form\Type\EntityType', null, array(
+            'em' => 'default',
+            'class' => self::SINGLE_IDENT_CLASS,
+            'choice_label' => 'name',
+        ));
+
+        $this->em->clear();
+
+        $field->submit(1);
+
+        $unitOfWorkIdentityMap = $this->em->getUnitOfWork()->getIdentityMap();
+        $managedEntitiesNames = array_map('strval', $unitOfWorkIdentityMap['Symfony\Bridge\Doctrine\Tests\Fixtures\SingleIntIdEntity']);
+
+        $this->assertContains((string) $entity1, $managedEntitiesNames);
+        $this->assertNotContains((string) $entity2, $managedEntitiesNames);
+    }
+
     public function testGroupByChoices()
     {
         $item1 = new GroupableEntity(1, 'Foo', 'Group1');
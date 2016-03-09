fc7f153440e7e8c391c17eea95da5f72edca07a7
Catalin Dan <dancatalin18@gmail.com>
2016-03-09 16:43:58 +0200

    bug #18067 [Form] Fix INT64 cast to float in IntegerType.

--- src/Symfony/Component/Form/Extension/Core/DataTransformer/NumberToLocalizedStringTransformer.php
+++ src/Symfony/Component/Form/Extension/Core/DataTransformer/NumberToLocalizedStringTransformer.php
@@ -122,7 +122,11 @@ class NumberToLocalizedStringTransformer implements DataTransformerInterface
             $value = str_replace(',', $decSep, $value);
         }
 
-        $result = $formatter->parse($value, \NumberFormatter::TYPE_DOUBLE, $position);
+        if (!strstr($value, $decSep)) {
+            $result = $formatter->parse($value, \NumberFormatter::TYPE_INT64, $position);
+        } else {
+            $result = $formatter->parse($value, \NumberFormatter::TYPE_DOUBLE, $position);
+        }
 
         if (intl_is_failure($formatter->getErrorCode())) {
             throw new TransformationFailedException($formatter->getErrorMessage());

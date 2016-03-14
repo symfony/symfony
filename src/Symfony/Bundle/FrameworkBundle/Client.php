7462fa59b1f93428db85171769a3f8dcc98f5020
Dawid Nowak <code@dnowak.pl>
2016-03-14 22:10:13 +0100

    FrameworkBundle: Client: getContainer(): fixed phpdoc
    The kernel might be shut down and then the method will return null instead of a ContainerInterface object

--- src/Symfony/Bundle/FrameworkBundle/Client.php
+++ src/Symfony/Bundle/FrameworkBundle/Client.php
@@ -41,7 +41,7 @@ class Client extends BaseClient
     /**
      * Returns the container.
      *
-     * @return ContainerInterface
+     * @return ContainerInterface|null Returns null when the Kernel has been shutdown or not started yet
      */
     public function getContainer()
     {

f7ad2857466b29c53a665e3b7c560df7fe6742db
Ener-Getick <egetick@gmail.com>
2016-03-21 20:54:26 +0100

    [Request] Fix support of custom mime types with parameters

--- src/Symfony/Component/HttpFoundation/Request.php
+++ src/Symfony/Component/HttpFoundation/Request.php
@@ -1238,8 +1238,9 @@ class Request
      */
     public function getFormat($mimeType)
     {
+        $canonicalMimeType = null;
         if (false !== $pos = strpos($mimeType, ';')) {
-            $mimeType = substr($mimeType, 0, $pos);
+            $canonicalMimeType = substr($mimeType, 0, $pos);
         }
 
         if (null === static::$formats) {
@@ -1248,6 +1249,9 @@ class Request
 
         foreach (static::$formats as $format => $mimeTypes) {
             if (in_array($mimeType, (array) $mimeTypes)) {
+                return $format;
+            }
+            if (null !== $canonicalMimeType && in_array($canonicalMimeType, (array) $mimeTypes)) {
                 return $format;
             }
         }

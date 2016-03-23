1373a5b74758c150857005bc3eac790240833d49
Nicolas Grekas <nicolas.grekas@gmail.com>
2016-03-23 14:45:24 +0100

    Fix backport

--- src/Symfony/Bridge/PhpUnit/DeprecationErrorHandler.php
+++ src/Symfony/Bridge/PhpUnit/DeprecationErrorHandler.php
@@ -131,7 +131,7 @@ class DeprecationErrorHandler
                 restore_error_handler();
                 self::register($mode);
             }
-        } elseif (!isset($mode[0]) || '/' !== $mode[0]) {
+        } else {
             self::$isRegistered = true;
             if (self::hasColorSupport()) {
                 $colorize = function ($str, $red) {
@@ -144,6 +144,9 @@ class DeprecationErrorHandler
             }
             register_shutdown_function(function () use ($getMode, &$deprecations, $deprecationHandler, $colorize) {
                 $mode = $getMode();
+                if (isset($mode[0]) && '/' === $mode[0]) {
+                    return;
+                }
                 $currErrorHandler = set_error_handler('var_dump');
                 restore_error_handler();
 

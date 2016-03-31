750dae945a8cf2ee62813b2d4dc696846f6a5c9b
Iltar van der Berg <ivanderberg@hostnet.nl>
2016-03-31 09:12:49 +0200

    Improved the BC layer

--- src/Symfony/Component/HttpKernel/Controller/TraceableControllerResolver.php
+++ src/Symfony/Component/HttpKernel/Controller/TraceableControllerResolver.php
@@ -19,21 +19,28 @@ use Symfony\Component\HttpFoundation\Request;
  *
  * @author Fabien Potencier <fabien@symfony.com>
  */
-class TraceableControllerResolver implements ControllerResolverInterface
+class TraceableControllerResolver implements ControllerResolverInterface, ArgumentResolverInterface
 {
     private $resolver;
     private $stopwatch;
+    private $argumentResolver;
 
     /**
      * Constructor.
      *
-     * @param ControllerResolverInterface $resolver  A ControllerResolverInterface instance
-     * @param Stopwatch                   $stopwatch A Stopwatch instance
+     * @param ControllerResolverInterface $resolver         A ControllerResolverInterface instance
+     * @param Stopwatch                   $stopwatch        A Stopwatch instance
+     * @param ArgumentResolverInterface   $argumentResolver Only required for BC
      */
-    public function __construct(ControllerResolverInterface $resolver, Stopwatch $stopwatch)
+    public function __construct(ControllerResolverInterface $resolver, Stopwatch $stopwatch, ArgumentResolverInterface $argumentResolver = null)
     {
         $this->resolver = $resolver;
         $this->stopwatch = $stopwatch;
+        $this->argumentResolver = $argumentResolver;
+
+        if (null === $this->argumentResolver) {
+            $this->argumentResolver = $resolver;
+        }
     }
 
     /**
@@ -44,6 +51,28 @@ class TraceableControllerResolver implements ControllerResolverInterface
         $e = $this->stopwatch->start('controller.get_callable');
 
         $ret = $this->resolver->getController($request);
+
+        $e->stop();
+
+        return $ret;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated This method is deprecated as of 3.1 and will be removed in 4.0.
+     */
+    public function getArguments(Request $request, $controller)
+    {
+        @trigger_error(sprintf('This %s method is deprecated as of 3.1 and will be removed in 4.0. Please use the %s instead.', __METHOD__, TraceableArgumentResolver::class), E_USER_DEPRECATED);
+
+        if ($this->argumentResolver instanceof TraceableArgumentResolver) {
+            return $this->argumentResolver->getArguments($request, $controller);
+        }
+
+        $e = $this->stopwatch->start('controller.get_arguments');
+
+        $ret = $this->argumentResolver->getArguments($request, $controller);
 
         $e->stop();
 

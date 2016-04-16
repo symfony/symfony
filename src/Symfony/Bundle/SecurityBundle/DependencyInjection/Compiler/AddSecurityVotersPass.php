53c78fe93b41527d561261f6e44caa6bfe59b941
Jules Pietri <heah@heahprod.com>
2016-04-16 15:39:43 +0200

    [SecurityBundle] fixed DebugAccessDecisionManager config

--- src/Symfony/Bundle/SecurityBundle/DependencyInjection/Compiler/AddSecurityVotersPass.php
+++ src/Symfony/Bundle/SecurityBundle/DependencyInjection/Compiler/AddSecurityVotersPass.php
@@ -45,10 +45,10 @@ class AddSecurityVotersPass implements CompilerPassInterface
             throw new LogicException('No security voters found. You need to tag at least one with "security.voter"');
         }
 
-        $container->getDefinition('security.access.decision_manager')->addMethodCall('setVoters', array(array_values($voters)));
-
-        if ($container->hasDefinition('debug.security.access.decision_manager')) {
-            $container->getDefinition('debug.security.access.decision_manager')->addMethodCall('setVoters', array(array_values($voters)));
-        }
+        $adm = $container->hasDefinition('debug.security.access.decision_manager')
+            ? $container->getDefinition('debug.security.access.decision_manager')
+            : $container->getDefinition('security.access.decision_manager')
+        ;
+        $adm->addMethodCall('setVoters', array(array_values($voters)));
     }
 }

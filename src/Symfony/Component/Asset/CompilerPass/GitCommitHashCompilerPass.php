e184e47499d98ccb9c1694a5669a8318fee0f3b2
Evgeniy Sokolov <ewgraf@gmail.com>
2016-04-26 11:13:50 +0200

    set version automatically from git commit hash

---
+++ src/Symfony/Component/Asset/CompilerPass/GitCommitHashCompilerPass.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Asset\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Process\Process;
+
+/**
+ * Disable version for all assets.
+ *
+ * @author Evgenii Sokolov <ewgraf@gmail.com>
+ */
+class GitCommitHashCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $process = new Process('git log -1');
+
+        if (0 !== $process->run()) {
+            throw new \Exception("Git command return wrong exit code");
+        }
+
+        $container->setParameter('git_commit_hash_version_strategy', sha1($process->getOutput()));
+    }
+}

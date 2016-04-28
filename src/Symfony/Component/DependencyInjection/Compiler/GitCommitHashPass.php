fd61ea326fa59b1c7a366d942cb867135ae8d66e
Evgeniy Sokolov <ewgraf@gmail.com>
2016-04-28 11:55:50 +0200

    fix phpdoc

--- src/Symfony/Component/DependencyInjection/Compiler/GitCommitHashPass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/GitCommitHashPass.php
@@ -23,8 +23,6 @@ class GitCommitHashPass implements CompilerPassInterface
 {
     /**
      * @param ContainerBuilder $container
-     *
-     * @throws \Exception
      */
     public function process(ContainerBuilder $container)
     {

14bcd799c71d8e8458d7c6873a0e84e2a574b5c2
Piotr Stankowski <git@trakos.pl>
2016-05-24 15:06:45 +0200

    [Cache] Added PhpFilesAdapter

--- src/Symfony/Component/Cache/Tests/Adapter/PhpFilesAdapterTest.php
+++ src/Symfony/Component/Cache/Tests/Adapter/PhpFilesAdapterTest.php
@@ -11,13 +11,13 @@
 
 namespace Symfony\Component\Cache\Tests\Adapter;
 
-use Cache\IntegrationTests\CachePoolTest;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 
 /**
  * @group time-sensitive
  */
-class FilesystemAdapterTest extends CachePoolTest
+class PhpFilesAdapterTest extends AbstractAppendOnlyAdapterTest
 {
     public static function tearDownAfterClass()
     {
@@ -46,12 +46,25 @@ class FilesystemAdapterTest extends CachePoolTest
         rmdir($dir);
     }
 
-    public function createCachePool()
+    /**
+     * @param string $cacheVersion
+     *
+     * @return CacheItemPoolInterface that is used in the tests that need to recreate the same cache pool
+     */
+    public function createVersionedCachePool($cacheVersion)
     {
         if (defined('HHVM_VERSION')) {
             $this->skippedTests['testDeferredSaveWithoutCommit'] = 'Fails on HHVM';
         }
 
-        return new FilesystemAdapter('sf-cache');
+        return new PhpFilesAdapter('sf-cache', 0, null, $cacheVersion);
+    }
+
+    /**
+     * @return mixed cache version that will be used by this adapter
+     */
+    public function createRandomCachePoolVersion()
+    {
+        return substr(str_replace('/', '-', base64_encode(md5(uniqid(mt_rand(), true), true))), 0, -2);
     }
 }

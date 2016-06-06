b98b21423879739803510b9b8622a6aa430935ed
Fabien Potencier <fabien.potencier@gmail.com>
2016-06-06 18:48:23 +0200

    bumped Symfony version to 2.8.8

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -59,12 +59,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION = '2.8.7';
-    const VERSION_ID = 20807;
+    const VERSION = '2.8.8-DEV';
+    const VERSION_ID = 20808;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 8;
-    const RELEASE_VERSION = 7;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 8;
+    const EXTRA_VERSION = 'DEV';
 
     const END_OF_MAINTENANCE = '11/2018';
     const END_OF_LIFE = '11/2019';

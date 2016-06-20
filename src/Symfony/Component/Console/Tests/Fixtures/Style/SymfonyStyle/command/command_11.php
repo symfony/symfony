85e5060fa136e39d4de491041e49fe4aeb9207f3
Fabien Potencier <fabien.potencier@gmail.com>
2016-06-20 07:04:25 +0200

    [Console] simplified tests

--- src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_11.php
+++ src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_11.php
@@ -2,11 +2,11 @@
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Tests\Style\SymfonyStyleWithForcedLineLength;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 // ensure long words are properly wrapped in blocks
 return function (InputInterface $input, OutputInterface $output) {
     $word = 'Lopadotemachoselachogaleokranioleipsanodrimhypotrimmatosilphioparaomelitokatakechymenokichlepikossyphophattoperisteralektryonoptekephalliokigklopeleiolagoiosiraiobaphetraganopterygon';
-    $sfStyle = new SymfonyStyleWithForcedLineLength($input, $output);
+    $sfStyle = new SymfonyStyle($input, $output);
     $sfStyle->block($word, 'CUSTOM', 'fg=white;bg=blue', ' § ', false);
 };

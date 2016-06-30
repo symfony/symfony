36095a03b688d8294a64dfb1fdb329782dd7bd55
Graham Campbell <graham@alt-three.com>
2016-06-30 10:23:44 +0100

    Removed unused imports

--- src/Symfony/Component/Asset/Tests/PackagesTest.php
+++ src/Symfony/Component/Asset/Tests/PackagesTest.php
@@ -14,8 +14,6 @@ namespace Symfony\Component\Asset\Tests;
 use Symfony\Component\Asset\Package;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\Asset\VersionStrategy\StaticVersionStrategy;
-use Symfony\Component\Asset\Exception\InvalidArgumentException;
-use Symfony\Component\Asset\Exception\LogicException;
 
 class PackagesTest extends \PHPUnit_Framework_TestCase
 {

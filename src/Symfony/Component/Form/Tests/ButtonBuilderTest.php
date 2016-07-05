10a4e13b86c57eef9c5a1aefce034aadccd32032
Alexander Cheprasov <cheprasov.84@ya.ru>
2016-07-05 19:24:45 +0100

    [Form] Added test for ButtonBuilder

---
+++ src/Symfony/Component/Form/Tests/ButtonBuilderTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Form\Tests;
+
+use Symfony\Component\Form\ButtonBuilder;
+use Symfony\Component\Form\Exception\InvalidArgumentException;
+
+/**
+ * @author Alexander Cheprasov <cheprasov.84@ya.ru>
+ */
+class ButtonBuilderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @return array
+     */
+    public function getValidNames()
+    {
+        return [
+            ['reset'],
+            ['submit'],
+            ['foo'],
+            ['0'],
+            [0],
+            ['button[]'],
+        ];
+    }
+
+    /**
+     * @dataProvider getValidNames
+     *
+     * @param string $name
+     */
+    public function testValidNames($name)
+    {
+        $this->assertInstanceOf(ButtonBuilder::class, new ButtonBuilder($name));
+    }
+
+    /**
+     * @return array
+     */
+    public function getInvalidNames()
+    {
+        return [
+            [''],
+            [false],
+            [null],
+        ];
+    }
+
+    /**
+     * @dataProvider getInvalidNames
+     *
+     * @param string $name
+     */
+    public function testInvalidNames($name)
+    {
+        $this->setExpectedException(InvalidArgumentException::class, 'Buttons cannot have empty names.');
+        new ButtonBuilder($name);
+    }
+
+}

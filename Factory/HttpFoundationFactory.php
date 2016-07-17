dd1111ed1603065141a17320f3a8d4268ac15d22
Royston Tong <royston.tong@gmail.com>
2016-07-17 18:34:41 +0000

    removed 'Set-Cookie' from header when it is already converted to a Symfony header cookie

--- Factory/HttpFoundationFactory.php
+++ Factory/HttpFoundationFactory.php
@@ -140,6 +140,9 @@ class HttpFoundationFactory implements HttpFoundationFactoryInterface
      */
     public function createResponse(ResponseInterface $psrResponse)
     {
+        $cookies = $psrResponse->getHeader('Set-Cookie');
+        $psrResponse = $psrResponse->withHeader('Set-Cookie', array());
+
         $response = new Response(
             $psrResponse->getBody()->__toString(),
             $psrResponse->getStatusCode(),
@@ -147,7 +150,7 @@ class HttpFoundationFactory implements HttpFoundationFactoryInterface
         );
         $response->setProtocolVersion($psrResponse->getProtocolVersion());
 
-        foreach ($psrResponse->getHeader('Set-Cookie') as $cookie) {
+        foreach ($cookies as $cookie) {
             $response->headers->setCookie($this->createCookie($cookie));
         }
 

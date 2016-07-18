4284161d9c3683dd893116a9ec9581167ee75bf5
James Halsall <james.t.halsall@googlemail.com>
2016-07-18 19:07:57 +0100

    Add basic support for automatic console exception logging

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -66,6 +66,7 @@ class FrameworkExtension extends Extension
         $loader->load('web.xml');
         $loader->load('services.xml');
         $loader->load('fragment_renderer.xml');
+        $loader->load('console.xml');
 
         // A translator must always be registered (as support is included by
         // default in the Form component). If disabled, an identity translator
---
+++ src/Symfony/Component/Console/EventListener/ExceptionListener.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Symfony\Component\Console\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleExceptionEvent;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Console exception listener.
+ *
+ * Attempts to log exceptions or abnormal terminations of console commands.
+ *
+ * @package Symfony\Component\Console\EventListener;
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ExceptionListener implements EventSubscriberInterface
+{
+    protected $logger;
+
+    /**
+     * Constructor.
+     *
+     * @param LoggerInterface $logger A logger
+     */
+    public function __construct(LoggerInterface $logger = null)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Handles console command exception.
+     *
+     * @param ConsoleExceptionEvent $event Console event
+     */
+    public function onKernelException(ConsoleExceptionEvent $event)
+    {
+        if (null === $this->logger) {
+            return;
+        }
+
+        $exception = $event->getException();
+
+        $message = sprintf(
+            '%s: %s (uncaught exception) at %s line %s while running console command `%s`',
+            get_class($exception),
+            $exception->getMessage(),
+            $exception->getFile(),
+            $exception->getLine(),
+            $event->getCommand()->getName()
+        );
+
+        $this->logger->error($message, array('exception' => $exception));
+    }
+
+    /**
+     * Handles termination of console command.
+     *
+     * @param ConsoleTerminateEvent $event Console event
+     */
+    public function onKernelTerminate(ConsoleTerminateEvent $event)
+    {
+        if (null === $this->logger) {
+            return;
+        }
+
+        $exitCode = $event->getExitCode();
+
+        if ($exitCode === 0) {
+            return;
+        }
+
+        if ($exitCode > 255) {
+            $event->setExitCode(255);
+        }
+
+        $message = sprintf('Command `%s` exited with status code %d');
+
+        $this->logger->warning($message);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            ConsoleEvents::EXCEPTION => array('onKernelException', -128),
+            ConsoleEvents::TERMINATE => array('onKernelTerminate', -128)
+        );
+    }
+}

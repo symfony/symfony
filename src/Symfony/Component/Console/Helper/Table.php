db2010cfbb346b272ea8a4c6566457a27ae046cb
ReenExe <ipourri@gmail.com>
2016-07-22 20:09:52 +0300

    fix `notice` (Undefined index: `key`)

--- src/Symfony/Component/Console/Helper/Table.php
+++ src/Symfony/Component/Console/Helper/Table.php
@@ -115,11 +115,11 @@ class Table
             self::$styles = self::initStyles();
         }
 
-        if (!self::$styles[$name]) {
-            throw new InvalidArgumentException(sprintf('Style "%s" is not defined.', $name));
+        if (isset(self::$styles[$name])) {
+            return self::$styles[$name];
         }
 
-        return self::$styles[$name];
+        throw new InvalidArgumentException(sprintf('Style "%s" is not defined.', $name));
     }
 
     /**

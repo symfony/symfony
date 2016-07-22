c2425e9e7a4cfc98dbd6b7f9f5fa74708f2ce424
Vyacheslav Pavlov <elliotweb@gmail.com>
2016-07-22 17:44:44 +0300

    Made CS fixes suggested by fabbot.io

--- src/Symfony/Component/DependencyInjection/Tests/Fixtures/php/services9_compiled.php
+++ src/Symfony/Component/DependencyInjection/Tests/Fixtures/php/services9_compiled.php
@@ -245,8 +245,8 @@ class ProjectServiceContainer extends Container
         $this->services['method_call1'] = $instance = new \Bar\FooClass();
 
         $instance->setBar($this->get('foo'));
-        $instance->setBar(NULL);
-        $instance->setBar(($this->get("foo")->foo() . (($this->hasParameter("foo")) ? ($this->getParameter("foo")) : ("default"))));
+        $instance->setBar(null);
+        $instance->setBar(($this->get('foo')->foo().(($this->hasParameter('foo')) ? ($this->getParameter('foo')) : ('default'))));
 
         return $instance;
     }

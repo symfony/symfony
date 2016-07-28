0b0f98e6f0f1a39c03b71c57227d305356035e6e
Vyacheslav Pavlov <elliotweb@gmail.com>
2016-07-28 16:38:50 +0300

    Fixed parameters number mismatch declaration.

--- src/Symfony/Component/HttpFoundation/Tests/Session/Storage/Handler/LegacyPdoSessionHandlerTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/Session/Storage/Handler/LegacyPdoSessionHandlerTest.php
@@ -58,7 +58,7 @@ class LegacyPdoSessionHandlerTest extends \PHPUnit_Framework_TestCase
     {
         $storage = new LegacyPdoSessionHandler($this->pdo, array('db_table' => 'bad_name'));
         $this->setExpectedException('RuntimeException');
-        $storage->read('foo', 'bar');
+        $storage->read('foo');
     }
 
     public function testWriteRead()

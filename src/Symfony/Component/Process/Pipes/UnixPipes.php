ac17617ac0d98ea56c0d953259b5039cbd132075
Nicolas Grekas <nicolas.grekas@gmail.com>
2016-08-02 15:52:07 +0200

    [Process] Fix double-fread() when reading unix pipes

--- src/Symfony/Component/Process/Pipes/UnixPipes.php
+++ src/Symfony/Component/Process/Pipes/UnixPipes.php
@@ -120,7 +120,7 @@ class UnixPipes extends AbstractPipes
             do {
                 $data = fread($pipe, self::CHUNK_SIZE);
                 $read[$type] .= $data;
-            } while (isset($data[0]));
+            } while (isset($data[0]) && ($close || isset($data[self::CHUNK_SIZE - 1])));
 
             if (!isset($read[$type][0])) {
                 unset($read[$type]);

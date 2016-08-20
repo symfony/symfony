428b5cc6a838ae6482367d78d6688fe20c426bf5
Roland Franssen <franssen.roland@gmail.com>
2016-08-20 08:07:04 +0000

    include dynamic services in list of alternatives

--- src/Symfony/Component/DependencyInjection/Container.php
+++ src/Symfony/Component/DependencyInjection/Container.php
@@ -288,10 +288,10 @@ class Container implements IntrospectableContainerInterface
                     }
 
                     $alternatives = array();
-                    foreach ($this->services as $key => $associatedService) {
-                        $lev = levenshtein($id, $key);
-                        if ($lev <= strlen($id) / 3 || false !== strpos($key, $id)) {
-                            $alternatives[] = $key;
+                    foreach ($this->getServiceIds() as $knownId) {
+                        $lev = levenshtein($id, $knownId);
+                        if ($lev <= strlen($id) / 3 || false !== strpos($knownId, $id)) {
+                            $alternatives[] = $knownId;
                         }
                     }
 

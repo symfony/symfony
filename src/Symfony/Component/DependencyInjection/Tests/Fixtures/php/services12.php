9a99e4edadc98b9db8a19f3a68819f8f5c4f00fe
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-22 11:57:06 +0300

    Fixed tests comparing dumped container

--- src/Symfony/Component/DependencyInjection/Tests/Fixtures/php/services12.php
+++ src/Symfony/Component/DependencyInjection/Tests/Fixtures/php/services12.php
@@ -46,6 +46,14 @@ class ProjectServiceContainer extends Container
     }
 
     /**
+     * {@inheritdoc}
+     */
+    public function isFrozen()
+    {
+        return true;
+    }
+
+    /**
      * Gets the 'test' service.
      *
      * This service is shared.

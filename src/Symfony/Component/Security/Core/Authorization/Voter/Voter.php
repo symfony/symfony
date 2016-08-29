c49e46259c081a1d0a3fbfadbe118d5e2d07cf6b
Konstantin Myakshin <koc-dp@yandex.ru>
2016-08-29 14:19:30 +0300

    [Security] Added note inside phpdoc.

--- src/Symfony/Component/Security/Core/Authorization/Voter/Voter.php
+++ src/Symfony/Component/Security/Core/Authorization/Voter/Voter.php
@@ -74,6 +74,7 @@ abstract class Voter implements VoterInterface
 
     /**
      * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
      *
      * @param string         $attribute
      * @param mixed          $subject

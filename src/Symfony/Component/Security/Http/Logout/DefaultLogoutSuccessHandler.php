a30a2b08d1b2b2906fe6b686eaa8ef95ee8c530f
David Marín <davefx@gmail.com>
2016-08-31 16:50:11 +0200

    Typo mistakes

--- src/Symfony/Component/Security/Http/Logout/DefaultLogoutSuccessHandler.php
+++ src/Symfony/Component/Security/Http/Logout/DefaultLogoutSuccessHandler.php
@@ -44,8 +44,8 @@ class DefaultLogoutSuccessHandler implements LogoutSuccessHandlerInterface
     {
         if (!empty($this->targetUrl)) {
             return $this->httpUtils->createRedirectResponse($request, $this->targetUrl);
-        } 
-        
+        }
+
         return new Response();
     }
 }

2697dd54668d46a4335e91710640b9a68f1af6a9
Nicolas Grekas <nicolas.grekas@gmail.com>
2016-08-31 10:30:10 +0200

    [VarDumper] Make the line clickable to toggle dumps

--- src/Symfony/Component/VarDumper/Dumper/HtmlDumper.php
+++ src/Symfony/Component/VarDumper/Dumper/HtmlDumper.php
@@ -190,6 +190,8 @@ return function (root, x) {
                 f(e.target, e);
             } else if ('A' == e.target.parentNode.tagName) {
                 f(e.target.parentNode, e);
+            } else if (e.target.nextElementSibling && 'A' == e.target.nextElementSibling.tagName) {
+                f(e.target.nextElementSibling, e, true);
             }
         });
     };
@@ -209,7 +211,7 @@ return function (root, x) {
             }
         }
     });
-    a('click', function (a, e) {
+    a('click', function (a, e, c) {
         if (/\bsf-dump-toggle\b/.test(a.className)) {
             e.preventDefault();
             if (!toggle(a, isCtrlKey(e))) {
@@ -230,7 +232,8 @@ return function (root, x) {
                 }
             }
 
-            if (doc.getSelection) {
+            if (c) {
+            } else if (doc.getSelection) {
                 try {
                     doc.getSelection().removeAllRanges();
                 } catch (e) {

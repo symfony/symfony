ec8aad5f50d69b260c600811159d5773655e4170
Fabien Potencier <fabien.potencier@gmail.com>
2016-09-29 10:15:28 -0700

    [TwigBridge] decoupled Form extension definitions from its runtime

--- src/Symfony/Bridge/Twig/Extension/FormExtension.php
+++ src/Symfony/Bridge/Twig/Extension/FormExtension.php
@@ -13,7 +13,6 @@ namespace Symfony\Bridge\Twig\Extension;
 
 use Symfony\Bridge\Twig\TokenParser\FormThemeTokenParser;
 use Symfony\Bridge\Twig\Form\TwigRendererInterface;
-use Symfony\Component\Form\ChoiceList\View\ChoiceView;
 
 /**
  * FormExtension extends Twig with form capabilities.
@@ -69,7 +68,7 @@ class FormExtension extends \Twig_Extension implements \Twig_Extension_InitRunti
             new \Twig_SimpleFunction('form', null, array('node_class' => 'Symfony\Bridge\Twig\Node\RenderBlockNode', 'is_safe' => array('html'))),
             new \Twig_SimpleFunction('form_start', null, array('node_class' => 'Symfony\Bridge\Twig\Node\RenderBlockNode', 'is_safe' => array('html'))),
             new \Twig_SimpleFunction('form_end', null, array('node_class' => 'Symfony\Bridge\Twig\Node\RenderBlockNode', 'is_safe' => array('html'))),
-            new \Twig_SimpleFunction('csrf_token', array($this, 'renderCsrfToken')),
+            new \Twig_SimpleFunction('csrf_token', array('Symfony\Bridge\Twig\Form\TwigRenderer', 'renderCsrfToken')),
         );
     }
 
@@ -79,7 +78,7 @@ class FormExtension extends \Twig_Extension implements \Twig_Extension_InitRunti
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('humanize', array($this, 'humanize')),
+            new \Twig_SimpleFilter('humanize', array('Symfony\Bridge\Twig\Form\TwigRenderer', 'humanize')),
         );
     }
 
@@ -89,60 +88,8 @@ class FormExtension extends \Twig_Extension implements \Twig_Extension_InitRunti
     public function getTests()
     {
         return array(
-            new \Twig_SimpleTest('selectedchoice', array($this, 'isSelectedChoice')),
+            new \Twig_SimpleTest('selectedchoice', array('Symfony\Bridge\Twig\Form\TwigRenderer', 'isSelectedChoice')),
         );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function renderCsrfToken($tokenId)
-    {
-        return $this->renderer->renderCsrfToken($tokenId);
-    }
-
-    /**
-     * Makes a technical name human readable.
-     *
-     * @param string $text The text to humanize
-     *
-     * @return string The humanized text
-     */
-    public function humanize($text)
-    {
-        return $this->renderer->humanize($text);
-    }
-
-    /**
-     * Returns whether a choice is selected for a given form value.
-     *
-     * Unfortunately Twig does not support an efficient way to execute the
-     * "is_selected" closure passed to the template by ChoiceType. It is faster
-     * to implement the logic here (around 65ms for a specific form).
-     *
-     * Directly implementing the logic here is also faster than doing so in
-     * ChoiceView (around 30ms).
-     *
-     * The worst option tested so far is to implement the logic in ChoiceView
-     * and access the ChoiceView method directly in the template. Doing so is
-     * around 220ms slower than doing the method call here in the filter. Twig
-     * seems to be much more efficient at executing filters than at executing
-     * methods of an object.
-     *
-     * @param ChoiceView   $choice        The choice to check
-     * @param string|array $selectedValue The selected value to compare
-     *
-     * @return bool Whether the choice is selected
-     *
-     * @see ChoiceView::isSelected()
-     */
-    public function isSelectedChoice(ChoiceView $choice, $selectedValue)
-    {
-        if (is_array($selectedValue)) {
-            return in_array($choice->value, $selectedValue, true);
-        }
-
-        return $choice->value === $selectedValue;
     }
 
     /**
--- src/Symfony/Bridge/Twig/Form/TwigRenderer.php
+++ src/Symfony/Bridge/Twig/Form/TwigRenderer.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Bridge\Twig\Form;
 
+use Symfony\Component\Form\ChoiceList\View\ChoiceView;
 use Symfony\Component\Form\FormRenderer;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
@@ -37,5 +38,37 @@ class TwigRenderer extends FormRenderer implements TwigRendererInterface
     public function setEnvironment(\Twig_Environment $environment)
     {
         $this->engine->setEnvironment($environment);
+    }
+
+    /**
+     * Returns whether a choice is selected for a given form value.
+     *
+     * Unfortunately Twig does not support an efficient way to execute the
+     * "is_selected" closure passed to the template by ChoiceType. It is faster
+     * to implement the logic here (around 65ms for a specific form).
+     *
+     * Directly implementing the logic here is also faster than doing so in
+     * ChoiceView (around 30ms).
+     *
+     * The worst option tested so far is to implement the logic in ChoiceView
+     * and access the ChoiceView method directly in the template. Doing so is
+     * around 220ms slower than doing the method call here in the filter. Twig
+     * seems to be much more efficient at executing filters than at executing
+     * methods of an object.
+     *
+     * @param ChoiceView   $choice        The choice to check
+     * @param string|array $selectedValue The selected value to compare
+     *
+     * @return bool Whether the choice is selected
+     *
+     * @see ChoiceView::isSelected()
+     */
+    public function isSelectedChoice(ChoiceView $choice, $selectedValue)
+    {
+        if (is_array($selectedValue)) {
+            return in_array($choice->value, $selectedValue, true);
+        }
+
+        return $choice->value === $selectedValue;
     }
 }

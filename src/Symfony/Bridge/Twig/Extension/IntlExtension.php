d16d60cf0e2ccd7eb8a95c6d5ecdf7ec64fd2b1e
HeahDude <heah@heahprod.com>
2016-10-01 19:00:50 +0200

    added support for number formatter attributes

--- src/Symfony/Bridge/Twig/Extension/IntlExtension.php
+++ src/Symfony/Bridge/Twig/Extension/IntlExtension.php
@@ -24,22 +24,22 @@ class IntlExtension extends \Twig_Extension
 {
     private $defaultLocale;
     private $dateFormatters = array();
-    private $dateFormatValues = array(
+    private $numberFormatters = array();
+    private $dateFormats = array(
         'none' => IntlDateFormatter::NONE,
         'short' => IntlDateFormatter::SHORT,
         'medium' => IntlDateFormatter::MEDIUM,
         'long' => IntlDateFormatter::LONG,
         'full' => IntlDateFormatter::FULL,
     );
-    private $numberFormatters = array();
-    private $numberTypeValues = array(
+    private $numberTypes = array(
         'default' => NumberFormatter::TYPE_DEFAULT,
         'int32' => NumberFormatter::TYPE_INT32,
         'int64' => NumberFormatter::TYPE_INT64,
         'double' => NumberFormatter::TYPE_DOUBLE,
         'currency' => NumberFormatter::TYPE_CURRENCY,
     );
-    private $numberStyleValues = array(
+    private $numberStyles = array(
         'decimal' => NumberFormatter::DECIMAL,
         'currency' => NumberFormatter::CURRENCY,
         'percent' => NumberFormatter::PERCENT,
@@ -47,6 +47,73 @@ class IntlExtension extends \Twig_Extension
         'spellout' => NumberFormatter::SPELLOUT,
         'ordinal' => NumberFormatter::ORDINAL,
         'duration' => NumberFormatter::DURATION,
+    );
+    private $numberAttributes = array(
+        'parse_intl_only' => NumberFormatter::PARSE_INT_ONLY,
+        'grouping_used' => NumberFormatter::GROUPING_USED,
+        'decimal_always_shown' => NumberFormatter::DECIMAL_ALWAYS_SHOWN,
+        'max_integer_digit' => NumberFormatter::MAX_INTEGER_DIGITS,
+        'min_integer_digit' => NumberFormatter::MIN_INTEGER_DIGITS,
+        'integer_digit' => NumberFormatter::INTEGER_DIGITS,
+        'max_fraction_digit' => NumberFormatter::MAX_FRACTION_DIGITS,
+        'min_fraction_digit' => NumberFormatter::MIN_FRACTION_DIGITS,
+        'fraction_digit' => NumberFormatter::FRACTION_DIGITS,
+        'multiplier' => NumberFormatter::MULTIPLIER,
+        'grouping_size' => NumberFormatter::GROUPING_SIZE,
+        'rounding_mode' => NumberFormatter::ROUNDING_MODE,
+        'rounding_increment' => NumberFormatter::ROUNDING_INCREMENT,
+        'format_width' => NumberFormatter::FORMAT_WIDTH,
+        'padding_position' => NumberFormatter::PADDING_POSITION,
+        'secondary_grouping_size' => NumberFormatter::SECONDARY_GROUPING_SIZE,
+        'significant_digits_used' => NumberFormatter::SIGNIFICANT_DIGITS_USED,
+        'min_significant_digits_used' => NumberFormatter::MIN_SIGNIFICANT_DIGITS,
+        'max_significant_digits_used' => NumberFormatter::MAX_SIGNIFICANT_DIGITS,
+        'lenient_parse' => NumberFormatter::LENIENT_PARSE,
+    );
+    private $numberRoundingAttributes = array(
+        'ceiling' => NumberFormatter::ROUND_CEILING,
+        'floor' => NumberFormatter::ROUND_FLOOR,
+        'down' => NumberFormatter::ROUND_DOWN,
+        'up' => NumberFormatter::ROUND_UP,
+        'halfeven' => NumberFormatter::ROUND_HALFEVEN,
+        'halfdown' => NumberFormatter::ROUND_HALFDOWN,
+        'halfup' => NumberFormatter::ROUND_HALFUP,
+    );
+    private $numberPaddingAttributes = array(
+        'before_prefix' => NumberFormatter::PAD_BEFORE_PREFIX,
+        'after_prefix' => NumberFormatter::PAD_AFTER_PREFIX,
+        'before_suffix' => NumberFormatter::PAD_BEFORE_SUFFIX,
+        'after_suffix' => NumberFormatter::PAD_AFTER_SUFFIX,
+    );
+    private $numberTextAttributes = array(
+        'positive_prefix' => NumberFormatter::POSITIVE_PREFIX,
+        'positive_suffix' => NumberFormatter::POSITIVE_SUFFIX,
+        'negative_prefix' => NumberFormatter::NEGATIVE_PREFIX,
+        'negative_suffix' => NumberFormatter::NEGATIVE_SUFFIX,
+        'padding_character' => NumberFormatter::PADDING_CHARACTER,
+        'currency_mode' => NumberFormatter::CURRENCY_CODE,
+        'default_ruleset' => NumberFormatter::DEFAULT_RULESET,
+        'public_rulesets' => NumberFormatter::PUBLIC_RULESETS,
+    );
+    private $symbols = array(
+        'decimal_separator' => NumberFormatter::DECIMAL_SEPARATOR_SYMBOL,
+        'grouping_separator' => NumberFormatter::GROUPING_SEPARATOR_SYMBOL,
+        'pattern_separator' => NumberFormatter::PATTERN_SEPARATOR_SYMBOL,
+        'percent' => NumberFormatter::PERCENT_SYMBOL,
+        'zero_digit' => NumberFormatter::ZERO_DIGIT_SYMBOL,
+        'digit' => NumberFormatter::DIGIT_SYMBOL,
+        'minus_sign' => NumberFormatter::MINUS_SIGN_SYMBOL,
+        'plus_sign' => NumberFormatter::PLUS_SIGN_SYMBOL,
+        'currency' => NumberFormatter::CURRENCY_SYMBOL,
+        'intl_currency' => NumberFormatter::INTL_CURRENCY_SYMBOL,
+        'monetary_separator' => NumberFormatter::MONETARY_SEPARATOR_SYMBOL,
+        'exponential' => NumberFormatter::EXPONENTIAL_SYMBOL,
+        'permill' => NumberFormatter::PERMILL_SYMBOL,
+        'pad_escape' => NumberFormatter::PAD_ESCAPE_SYMBOL,
+        'infinity' => NumberFormatter::INFINITY_SYMBOL,
+        'nan' => NumberFormatter::NAN_SYMBOL,
+        'significant_digit' => NumberFormatter::SIGNIFICANT_DIGIT_SYMBOL,
+        'monetary_grouping_separator' => NumberFormatter::MONETARY_GROUPING_SEPARATOR_SYMBOL,
     );
 
     public function __construct($defaultLocale = null)
@@ -199,14 +266,17 @@ class IntlExtension extends \Twig_Extension
      * @param string $style
      * @param string $type
      * @param null   $displayLocale
+     * @param array  $attr
+     * @param array  $textAttr
+     * @param array  $symbols
      *
      * @return bool|string
      *
      * @throws \InvalidArgumentException When the type or the style is invalid
      */
-    public function getLocalizedNumber($number, $style = 'decimal', $type = 'default', $displayLocale = null)
+    public function getLocalizedNumber($number, $style = 'decimal', $type = 'default', $displayLocale = null, array $attr = array(), array $textAttr = array(), array $symbols = array())
     {
-        $typeValues = $this->numberTypeValues;
+        $typeValues = $this->numberTypes;
         if (!isset($typeValues[$type])) {
             throw new \InvalidArgumentException(sprintf('The type "%s" does not exist. Known types are: "%s"', $type, implode('", "', array_keys($typeValues))));
         }
@@ -215,6 +285,7 @@ class IntlExtension extends \Twig_Extension
             $displayLocale ?: $this->defaultLocale,
             $style
         );
+        $this->configureNumberFormatterAttributes($formatter, $attr, $textAttr, $symbols);
 
         return $formatter->format($number, $typeValues[$type]);
     }
@@ -234,7 +305,7 @@ class IntlExtension extends \Twig_Extension
      */
     private function createDateFormatter($locale, $dateFormat, $timeFormat, $timezone, $calendar, $format)
     {
-        $formatValues = $this->dateFormatValues;
+        $formatValues = $this->dateFormats;
         if (!isset($formatValues[$dateFormat])) {
             throw new \InvalidArgumentException(sprintf('The date format "%s" does not exist. Known formats are: "%s"', $dateFormat, implode(', ', array_keys($formatValues))));
         }
@@ -270,7 +341,7 @@ class IntlExtension extends \Twig_Extension
      */
     private function createNumberFormatter($locale, $style)
     {
-        $styleValues = $this->numberStyleValues;
+        $styleValues = $this->numberStyles;
         if (!isset($styleValues[$style])) {
             throw new \InvalidArgumentException(sprintf('The style "%s" does not exist. Known styles are: "%s"', $style, implode('", "', array_keys($styleValues))));
         }
@@ -281,5 +352,60 @@ class IntlExtension extends \Twig_Extension
         }
 
         return $this->numberFormatters[$hash];
+    }
+
+    private function configureNumberFormatterAttributes(NumberFormatter $formatter, array $attr, array $textAttr, array $symbols)
+    {
+        $attributesKeys = $this->numberAttributes;
+        $textAttributesKeys = $this->numberTextAttributes;
+        $symbolsKeys = $this->symbols;
+
+        foreach ($attr as $attribute => $value) {
+            if (!isset($attributesKeys[$attribute])) {
+                throw new \InvalidArgumentException(sprintf('The number formatter attribute "%s" does not exist. Known attributes are: "%s"', $attribute, implode('", "', array_keys($attributesKeys))));
+            }
+
+            $attributeKey = $attributesKeys[$attribute];
+
+            if ('rounding_mode' === $attribute) {
+                $roundingModes = $this->numberRoundingAttributes;
+                if (!isset($roundingModes[$value])) {
+                    throw new \InvalidArgumentException(sprintf('The number formatter rounding mode "%s" does not exist. Known modes are: "%s"', $value, implode('", "', array_keys($roundingModes))));
+                }
+
+                $formatter->setAttribute($attributeKey, $roundingModes[$value]);
+
+                continue;
+            }
+
+            if ('padding_position' === $attribute) {
+                $paddingPositions = $this->numberRoundingAttributes;
+                if (!isset($paddingPositions[$value])) {
+                    throw new \InvalidArgumentException(sprintf('The number formatter padding position "%s" does not exist. Known positions are: "%s"', $value, implode('", "', array_keys($paddingPositions))));
+                }
+
+                $formatter->setAttribute($attributeKey, $paddingPositions[$value]);
+
+                continue;
+            }
+
+            $formatter->setAttribute($attributeKey, $value);
+        }
+
+        foreach ($textAttr as $attribute => $value) {
+            if (!isset($textAttributesKeys[$attribute])) {
+                throw new \InvalidArgumentException(sprintf('The number formatter text attribute "%s" does not exist. Known text attributes are: "%s"', $attribute, implode('", "', array_keys($textAttributesKeys))));
+            }
+
+            $formatter->setTextAttribute($textAttributesKeys[$attribute], $value);
+        }
+
+        foreach ($symbols as $symbol => $value) {
+            if (!isset($symbolsKeys[$symbol])) {
+                throw new \InvalidArgumentException(sprintf('The number formatter symbol "%s" does not exist. Known symbols are: "%s"', $symbol, implode('", "', array_keys($symbolsKeys))));
+            }
+
+            $formatter->setSymbol($symbolsKeys[$symbol], $value);
+        }
     }
 }

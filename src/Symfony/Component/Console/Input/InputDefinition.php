36e470e73148753bfd3c4c3b4e8e8592457f88ca
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-10-05 14:49:31 +0200

    Minor rewording

--- src/Symfony/Component/Console/Input/InputDefinition.php
+++ src/Symfony/Component/Console/Input/InputDefinition.php
@@ -280,9 +280,10 @@ class InputDefinition
     }
 
     /**
-     * Returns true if an InputOption object exists by name. This method can't
-     * be used to check if the user included the option when executing the
-     * command (use getOption() instead).
+     * Returns true if an InputOption object exists by name.
+     *
+     * This method can't be used to check if the user included the option when
+     * executing the command (use getOption() instead).
      *
      * @param string $name The InputOption name
      *

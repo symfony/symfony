7def83c0b1c7f1abde900c1fe7371d0f2e8f2571
Christian Flothmann <christian.flothmann@sensiolabs.de>
2016-10-11 23:07:33 +0200

    move tags from decorated to decorating service

--- src/Symfony/Component/DependencyInjection/Tests/Compiler/DecoratorServicePassTest.php
+++ src/Symfony/Component/DependencyInjection/Tests/Compiler/DecoratorServicePassTest.php
@@ -82,6 +82,24 @@ class DecoratorServicePassTest extends \PHPUnit_Framework_TestCase
         $this->assertNull($fooExtendedDefinition->getDecoratedService());
     }
 
+    public function testProcessMovesTagsFromDecoratedDefinitionToDecoratingDefinition()
+    {
+        $container = new ContainerBuilder();
+        $container
+            ->register('foo')
+            ->setTags(array('name' => 'bar'))
+        ;
+        $container
+            ->register('baz')
+            ->setDecoratedService('foo')
+        ;
+
+        $this->process($container);
+
+        $this->assertEmpty($container->getDefinition('baz.inner')->getTags());
+        $this->assertEquals(array('name' => 'bar'), $container->getDefinition('baz')->getTags());
+    }
+
     protected function process(ContainerBuilder $container)
     {
         $repeatedPass = new DecoratorServicePass();

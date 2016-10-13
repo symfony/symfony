c43de7f21a587649bb42ca08bce9ad8d0c0e731d
David Maicher <mail@dmaicher.de>
2016-10-13 22:14:35 +0200

    [HttpCache] fix: do not cache OPTIONS request

--- src/Symfony/Component/HttpFoundation/Request.php
+++ src/Symfony/Component/HttpFoundation/Request.php
@@ -1474,6 +1474,16 @@ class Request
     }
 
     /**
+     * Checks whether the method is cachaeble or not.
+     *
+     * @return bool
+     */
+    public function isMethodCacheable()
+    {
+        return in_array($this->getMethod(), array('GET', 'HEAD'));
+    }
+
+    /**
      * Returns the request body content.
      *
      * @param bool $asResource If true, a resource will be returned

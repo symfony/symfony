98c59c3d8971a32765df3eab55aefa437e2b54f3
Xavier HAUSHERR <xkobal@users.noreply.github.com>
2016-10-27 11:00:39 +0200

    Update phpdoc

--- src/Symfony/Bundle/TwigBundle/Loader/FilesystemLoader.php
+++ src/Symfony/Bundle/TwigBundle/Loader/FilesystemLoader.php
@@ -58,7 +58,7 @@ class FilesystemLoader extends \Twig_Loader_Filesystem
      * Otherwise the template is located using the locator from the twig library.
      *
      * @param string|TemplateReferenceInterface $template The template
-     * @param bool $throw Throw \Twig_Error_Loader exception if template is not found
+     * @param bool                              $throw    When true, a \Twig_Error_Loader exception will be thrown if a template could not be found
      *
      * @return string The path to the template file
      *

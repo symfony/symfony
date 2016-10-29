af3b264114e573bc48678469b7e4e513b19ed7bb
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2016-10-30 00:28:56 +0200

    CS: apply no_trailing_whitespace_in_comment

--- src/Symfony/Component/VarDumper/Dumper/CliDumper.php
+++ src/Symfony/Component/VarDumper/Dumper/CliDumper.php
@@ -59,7 +59,7 @@ class CliDumper extends AbstractDumper
         parent::__construct($output, $charset);
 
         if ('\\' === DIRECTORY_SEPARATOR && 'ON' !== @getenv('ConEmuANSI') && 'xterm' !== @getenv('TERM')) {
-            // Use only the base 16 xterm colors when using ANSICON or standard Windows 10 CLI 
+            // Use only the base 16 xterm colors when using ANSICON or standard Windows 10 CLI
             $this->setStyles(array(
                 'default' => '31',
                 'num' => '1;34',

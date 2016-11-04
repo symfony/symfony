c0e880e277fba48b51c25e077ecef477c57e7bac
Nicolas Grekas <nicolas.grekas@gmail.com>
2016-11-04 14:53:25 +0100

    [DI] Add missing deprecation in ContainerBuilder::addCompilerPass

--- src/Symfony/Component/DependencyInjection/ContainerBuilder.php
+++ src/Symfony/Component/DependencyInjection/ContainerBuilder.php
@@ -316,6 +316,13 @@ class ContainerBuilder extends Container implements TaggedContainerInterface
         if (func_num_args() >= 3) {
             $priority = func_get_arg(2);
         } else {
+            if (__CLASS__ !== get_class($this)) {
+                $r = new \ReflectionMethod($this, __FUNCTION__);
+                if (__CLASS__ !== $r->getDeclaringClass()->getName()) {
+                    @trigger_error(sprintf('Method %s() will have a third `$priority = 0` argument in version 4.0. Not defining it is deprecated since 3.2.', get_class($this), __FUNCTION__), E_USER_DEPRECATED);
+                }
+            }
+
             $priority = 0;
         }
 

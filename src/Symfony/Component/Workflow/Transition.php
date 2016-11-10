8eed2aa7eaca31498623472e7d4ac8f7fe64b576
Grégoire Pineau <lyrixx@lyrixx.info>
2016-11-10 11:39:59 +0100

    [Workflow] Fixed PHPDocs

--- src/Symfony/Component/Workflow/Transition.php
+++ src/Symfony/Component/Workflow/Transition.php
@@ -24,8 +24,6 @@ class Transition
     private $tos;
 
     /**
-     * Transition constructor.
-     *
      * @param string          $name
      * @param string|string[] $froms
      * @param string|string[] $tos

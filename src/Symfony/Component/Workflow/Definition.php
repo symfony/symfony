c546857957c6d3e5477046cf13df97bf0516b56b
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-11-10 15:55:12 +0100

    Removed private Definition::addPlaces
    There is no need for this method

--- src/Symfony/Component/Workflow/Definition.php
+++ src/Symfony/Component/Workflow/Definition.php
@@ -34,11 +34,15 @@ final class Definition
      */
     public function __construct(array $places, array $transitions, $initialPlace = null)
     {
-        $this->addPlaces($places);
-        $this->setInitialPlace($initialPlace);
+        foreach ($places as $place) {
+            $this->addPlace($place);
+        }
+
         foreach ($transitions as $transition) {
             $this->addTransition($transition);
         }
+
+        $this->setInitialPlace($initialPlace);
     }
 
     /**
@@ -89,13 +93,6 @@ final class Definition
         }
 
         $this->places[$place] = $place;
-    }
-
-    private function addPlaces(array $places)
-    {
-        foreach ($places as $place) {
-            $this->addPlace($place);
-        }
     }
 
     private function addTransition(Transition $transition)

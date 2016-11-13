bd4d0b70e1f5c986876501c9c4af1a8ba541057f
Jules Pietri <heah@heahprod.com>
2016-11-13 19:36:40 +0100

    [Workflow] some minor fixes

--- src/Symfony/Component/Workflow/Tests/WorkflowTest.php
+++ src/Symfony/Component/Workflow/Tests/WorkflowTest.php
@@ -47,7 +47,6 @@ class WorkflowTest extends \PHPUnit_Framework_TestCase
     public function testGetMarkingWithImpossiblePlace()
     {
         $subject = new \stdClass();
-        $subject->marking = null;
         $subject->marking = array('nope' => true);
         $workflow = new Workflow(new Definition(array(), array()), new MultipleStateMarkingStore());
 

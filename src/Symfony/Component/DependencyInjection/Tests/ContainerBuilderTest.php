7a5e11eb1206a121824b09a3e645d74b0781c92e
kiler129 <grzegorz@noflash.pl>
2016-11-23 11:45:56 -0600

    [DI] Fixed custom services definition BC break introduced in ec7e70fb…

--- src/Symfony/Component/DependencyInjection/Tests/ContainerBuilderTest.php
+++ src/Symfony/Component/DependencyInjection/Tests/ContainerBuilderTest.php
@@ -29,6 +29,7 @@ use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Symfony\Component\DependencyInjection\Scope;
 use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\CustomDefinition;
 use Symfony\Component\ExpressionLanguage\Expression;
 
 class ContainerBuilderTest extends \PHPUnit_Framework_TestCase
@@ -407,7 +408,7 @@ class ContainerBuilderTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @expectedException \Symfony\Component\DependencyInjection\Exception\RuntimeException
-     * @expectedExceptionMessage Constructing service "foo" from a Symfony\Component\DependencyInjection\DefinitionDecorator is not supported at build time.
+     * @expectedExceptionMessage Constructing decorated service "foo" from a Symfony\Component\DependencyInjection\DefinitionDecorator is not supported at build time.
      */
     public function testResolveServicesWithDecoratedDefinition()
     {
@@ -417,6 +418,14 @@ class ContainerBuilderTest extends \PHPUnit_Framework_TestCase
         $builder->setDefinition('foo', new DefinitionDecorator('parent'));
 
         $builder->get('foo');
+    }
+
+    public function testResolveServicesWithCustomDefinitionClass()
+    {
+        $builder = new ContainerBuilder();
+        $builder->setDefinition('foo', new CustomDefinition('stdClass'));
+
+        $this->assertInstanceOf('stdClass', $builder->get('foo'));
     }
 
     public function testMerge()

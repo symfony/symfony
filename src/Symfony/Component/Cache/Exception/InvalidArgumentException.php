848a33ed3e4d06c50ee9f39d71a9aabf044d39bb
Nicolas Grekas <nicolas.grekas@gmail.com>
2016-11-25 17:25:06 +0100

    [Cache] Implement PSR-16 SimpleCache v1.0

--- src/Symfony/Component/Cache/Exception/InvalidArgumentException.php
+++ src/Symfony/Component/Cache/Exception/InvalidArgumentException.php
@@ -11,8 +11,9 @@
 
 namespace Symfony\Component\Cache\Exception;
 
-use Psr\Cache\InvalidArgumentException as InvalidArgumentExceptionInterface;
+use Psr\Cache\InvalidArgumentException as Psr6CacheInterface;
+use Psr\SimpleCache\InvalidArgumentException as SimpleCacheInterface;
 
-class InvalidArgumentException extends \InvalidArgumentException implements InvalidArgumentExceptionInterface
+class InvalidArgumentException extends \InvalidArgumentException implements Psr6CacheInterface, SimpleCacheInterface
 {
 }

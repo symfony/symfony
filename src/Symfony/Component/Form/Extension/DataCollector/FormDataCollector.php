b161e2039112c418f77059a47773c03835c7747a
Adam Prager <prager.adam87@gmail.com>
2016-12-05 18:24:28 +0100

    Failing test for data collector bug

--- src/Symfony/Component/Form/Extension/DataCollector/FormDataCollector.php
+++ src/Symfony/Component/Form/Extension/DataCollector/FormDataCollector.php
@@ -256,7 +256,7 @@ class FormDataCollector extends DataCollector implements FormDataCollectorInterf
                     case 'resolved_options':
                     case 'default_data':
                     case 'submitted_data':
-                        if ($v) {
+                        if (is_array($v)) {
                             $form[$k] = array_map($cloneVar, $v);
                         }
                         break;

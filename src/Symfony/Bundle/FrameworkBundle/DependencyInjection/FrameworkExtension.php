69ee1843f8668c5afb4e5e9fd9535d93887417d3
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-29 13:12:01 +0100

    code style fix

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -645,7 +645,9 @@ class FrameworkExtension extends Extension
         }
 
         if (!empty($config['loaders'])) {
-            $loaders = array_map(function ($loader) { return new Reference($loader); }, $config['loaders']);
+            $loaders = array_map(function ($loader) {
+                return new Reference($loader);
+            }, $config['loaders']);
 
             // Use a delegation unless only a single loader was registered
             if (1 === count($loaders)) {
@@ -674,7 +676,9 @@ class FrameworkExtension extends Extension
         ));
 
         $container->setParameter('templating.engines', $config['engines']);
-        $engines = array_map(function ($engine) { return new Reference('templating.engine.'.$engine); }, $config['engines']);
+        $engines = array_map(function ($engine) {
+            return new Reference('templating.engine.'.$engine);
+        }, $config['engines']);
 
         // Use a delegation unless only a single engine was registered
         if (1 === count($engines)) {

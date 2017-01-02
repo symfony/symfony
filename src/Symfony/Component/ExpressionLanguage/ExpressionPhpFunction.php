d9b8e9dcbb52289d0b3726d56773fd9c7c24ac99
Dany Maillard <danymaillard93b@gmail.com>
2017-01-02 02:49:09 +0100

    Add ExpressionPhpFunction class

---
+++ src/Symfony/Component/ExpressionLanguage/ExpressionPhpFunction.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\ExpressionLanguage;
+
+/**
+ * Represents a PHP function that can be used in an expression.
+ *
+ * @author Dany Maillard <danymaillard93b@gmail.com>
+ */
+class ExpressionPhpFunction extends ExpressionFunction
+{
+    /**
+     * Constructor.
+     *
+     * @param string $name The PHP function name
+     */
+    public function __construct($name)
+    {
+        if (!function_exists($name)) {
+            throw new \InvalidArgumentException(sprintf('PHP function "%s" does not exist.', $name));
+        }
+
+        $compiler = function () use ($name) {
+            return sprintf('%s(%s)', $name, implode(', ', func_get_args()));
+        };
+
+        $evaluator = function () use ($name) {
+            $args = func_get_args();
+
+            return call_user_func_array($name, array_splice($args, 1));
+        };
+
+        parent::__construct($name, $compiler, $evaluator);
+    }
+}

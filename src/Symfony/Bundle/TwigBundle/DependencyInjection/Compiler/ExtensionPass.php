cb1a4b778f522c7ae85bcc977cbaceaafc62d3f1
Christian Flothmann <christian.flothmann@sensiolabs.de>
2017-01-14 10:53:53 +0100

    [TwigBundle] do not lose already set method calls

--- src/Symfony/Bundle/TwigBundle/DependencyInjection/Compiler/ExtensionPass.php
+++ src/Symfony/Bundle/TwigBundle/DependencyInjection/Compiler/ExtensionPass.php
@@ -91,7 +91,7 @@ class ExtensionPass implements CompilerPassInterface
         $twigLoader = $container->getDefinition('twig.loader.native_filesystem');
         if ($container->has('templating')) {
             $loader = $container->getDefinition('twig.loader.filesystem');
-            $loader->setMethodCalls($twigLoader->getMethodCalls());
+            $loader->setMethodCalls(array_merge($twigLoader->getMethodCalls(), $loader->getMethodCalls()));
 
             $twigLoader->clearTag('twig.loader');
         } else {

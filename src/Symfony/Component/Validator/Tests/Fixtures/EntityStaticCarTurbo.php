e23e7e4f4bc6aaea03deb60ea78d92b8d47709d0
po_taka <angel.koilov@gmail.com>
2017-02-05 19:58:39 +0200

    added test for staticClassLoader in LazyLoadingMetadatafactory

--- src/Symfony/Component/Validator/Tests/Fixtures/EntityStaticCarTurbo.php
+++ src/Symfony/Component/Validator/Tests/Fixtures/EntityStaticCarTurbo.php
@@ -1,16 +1,20 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Component\Validator\Tests\Fixtures;
 
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 use Symfony\Component\Validator\Constraints\Length;
 
-/**
- * Description of EntityStaticParent
- *
- * @author po_taka <angel.koilov@gmail.com>
- */
 class EntityStaticCarTurbo extends EntityStaticCar
 {
     public static function loadValidatorMetadata(ClassMetadata $metadata)

6ccc479d72e0b082538ceec5bcfea51623eb93c7
klemens <ka7@github.com>
2017-02-11 12:12:36 +0100

    spelling fixes

--- src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_5.php
+++ src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_5.php
@@ -4,7 +4,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Tests\Style\SymfonyStyleWithForcedLineLength;
 
-//Ensure has proper line ending before outputing a text block like with SymfonyStyle::listing() or SymfonyStyle::text()
+//Ensure has proper line ending before outputting a text block like with SymfonyStyle::listing() or SymfonyStyle::text()
 return function (InputInterface $input, OutputInterface $output) {
     $output = new SymfonyStyleWithForcedLineLength($input, $output);
 

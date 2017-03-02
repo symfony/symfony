ceee5d5be305c23e624f58bdb99899af002e380d
foaly-nr1 <jonny@fourlabs.co.uk>
2017-03-02 11:01:46 +0000

    String representation to avoid rounding errors, credit to @dmaicher

--- src/Symfony/Component/Form/Extension/Core/DataTransformer/NumberToLocalizedStringTransformer.php
+++ src/Symfony/Component/Form/Extension/Core/DataTransformer/NumberToLocalizedStringTransformer.php
@@ -266,7 +266,7 @@ class NumberToLocalizedStringTransformer implements DataTransformerInterface
         if (null !== $this->precision && null !== $this->roundingMode) {
             // shift number to maintain the correct scale during rounding
             $roundingCoef = pow(10, $this->precision);
-            $number = bcmul($number, $roundingCoef, 6);
+            $number = (string) ($number * $roundingCoef);
 
             switch ($this->roundingMode) {
                 case self::ROUND_CEILING:

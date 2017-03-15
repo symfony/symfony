f74a53c8ac45b16c4d7f0f97559c0a9b15fa716e
Pierre du Plessis <pierre@pcservice.co.za>
2017-03-15 11:07:37 +0200

    Add ValidatorExtensionTrait to SubmitTypeValidatorExtensionTest

--- src/Symfony/Component/Form/Tests/Extension/Validator/Type/SubmitTypeValidatorExtensionTest.php
+++ src/Symfony/Component/Form/Tests/Extension/Validator/Type/SubmitTypeValidatorExtensionTest.php
@@ -11,8 +11,12 @@
 
 namespace Symfony\Component\Form\Tests\Extension\Validator\Type;
 
+use Symfony\Component\Form\Test\Traits\ValidatorExtensionTrait;
+
 class SubmitTypeValidatorExtensionTest extends BaseValidatorExtensionTest
 {
+    use ValidatorExtensionTrait;
+
     protected function createForm(array $options = array())
     {
         return $this->factory->create('Symfony\Component\Form\Extension\Core\Type\SubmitType', null, $options);

7a9b086d569bf1a20940af887389e582aceb7c18
Dany Maillard <danymaillard93b@gmail.com>
2017-04-03 13:58:31 +0200

    Fold Travis CI output by component

---
+++ .github/travis_fold.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#######################################
+# Fold Travis CI output
+# Arguments:
+#   $1 fold name
+#   $2 command to execute
+#######################################
+tfold () {
+    FOLD=$(echo $1 | tr / .)
+    echo "travis_fold:start:$FOLD"
+    echo -e "\\e[34m$1\\e[0m"
+    sh -c "$2" && echo "travis_fold:end:$FOLD"
+}
+
+export -f tfold

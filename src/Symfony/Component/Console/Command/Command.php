7489a146569c8b0cd49d088ffeaef1e38b0497fd
Dany Maillard <danymaillard93b@gmail.com>
2017-04-13 00:18:28 +0200

    Add @throws phpdoc

--- src/Symfony/Component/Console/Command/Command.php
+++ src/Symfony/Component/Console/Command/Command.php
@@ -202,6 +202,8 @@ class Command
      *
      * @return int The command exit code
      *
+     * @throws \Exception When binding input fails. Bypass this by calling {@link ignoreValidationErrors()}.
+     *
      * @see setCode()
      * @see execute()
      */

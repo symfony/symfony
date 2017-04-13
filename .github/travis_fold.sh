42a67d90cc43048f713122ee024d6866fbb0e7d4
Dany Maillard <danymaillard93b@gmail.com>
2017-04-13 19:28:00 +0200

    Rename to tfold to avoid collisions with fold unix command

--- .github/travis_fold.sh
+++ .github/travis_fold.sh
@@ -6,11 +6,11 @@
 #   $1 fold name
 #   $2 command to execute
 #######################################
-fold () {
+tfold () {
     FOLD=$(echo $1 | tr / .)
     echo "travis_fold:start:$FOLD"
     echo -e "\\e[32m$FOLD\\e[0m"
     sh -c "$2" && echo "travis_fold:end:$FOLD"
 }
 
-export -f fold
+export -f tfold

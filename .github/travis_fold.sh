759b5057f440a476e760d7a3fa4fefb9971eba60
Dany Maillard <danymaillard93b@gmail.com>
2017-04-13 17:54:58 +0200

    Add travis_fold.sh file

---
+++ .github/travis_fold.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#######################################
+# Fold Travis CI output
+# Arguments:
+#   $1 fold name
+#   $2 command to execute
+#######################################
+fold () {
+    FOLD=$(echo $1 | tr / .)
+    echo "travis_fold:start:$FOLD"
+    echo -e "\\e[32m$FOLD\\e[0m"
+    sh -c "$2" && echo "travis_fold:end:$FOLD"
+}
+
+export -f fold

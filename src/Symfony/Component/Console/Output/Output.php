2d229596841ee7ab204f5b1b608faf8ed942059e
alexdee2007 <alexdee2007@bk.ru>
2017-04-26 09:30:24 +0300

    Update Output.php

--- src/Symfony/Component/Console/Output/Output.php
+++ src/Symfony/Component/Console/Output/Output.php
@@ -140,7 +140,8 @@ abstract class Output implements OutputInterface
     public function write($messages, $newline = false, $options = self::OUTPUT_NORMAL)
     {
         $messages = (array) $messages;
-
+        $options = is_numeric($options) ? (int) $options : 0;
+        
         $types = self::OUTPUT_NORMAL | self::OUTPUT_RAW | self::OUTPUT_PLAIN;
         $type = $types & $options ?: self::OUTPUT_NORMAL;
 

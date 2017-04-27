0d073128dec5c789b96fccf13aba8512b13107ab
maryo <mario.dweller@seznam.cz>
2017-04-27 15:12:46 +0200

    [Process] Ecaping of CLI arguments containing slashes on Windows

--- src/Symfony/Component/Process/Process.php
+++ src/Symfony/Component/Process/Process.php
@@ -1717,7 +1717,7 @@ class Process implements \IteratorAggregate
         if (false !== strpos($argument, "\0")) {
             $argument = str_replace("\0", '?', $argument);
         }
-        if (!preg_match('/[()%!^"<>&|\s]/', $argument)) {
+        if (!preg_match('/[\/()%!^"<>&|\s]/', $argument)) {
             return $argument;
         }
         $argument = preg_replace('/(\\\\+)$/', '$1$1', $argument);

d8594de9c64d3f84fd4c27053a9f7b06d3cb47e0
Fabien Potencier <fabien.potencier@gmail.com>
2017-05-17 18:33:00 +0200

    updated version to 4.0

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -61,15 +61,15 @@ abstract class Kernel implements KernelInterface, TerminableInterface
 
     private $projectDir;
 
-    const VERSION = '3.4.0-DEV';
-    const VERSION_ID = 30400;
-    const MAJOR_VERSION = 3;
-    const MINOR_VERSION = 4;
+    const VERSION = '4.0.0-DEV';
+    const VERSION_ID = 40000;
+    const MAJOR_VERSION = 0;
+    const MINOR_VERSION = 0;
     const RELEASE_VERSION = 0;
     const EXTRA_VERSION = 'DEV';
 
-    const END_OF_MAINTENANCE = '11/2020';
-    const END_OF_LIFE = '11/2021';
+    const END_OF_MAINTENANCE = '07/2018';
+    const END_OF_LIFE = '01/2019';
 
     /**
      * Constructor.

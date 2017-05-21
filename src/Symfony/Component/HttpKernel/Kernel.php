ce519b6e444abb12174fbd57ec0936ea4961ab56
Fabien Potencier <fabien.potencier@gmail.com>
2017-05-21 08:14:41 +0200

    fixed typo

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -61,7 +61,7 @@ abstract class Kernel implements KernelInterface, TerminableInterface
 
     const VERSION = '4.0.0-DEV';
     const VERSION_ID = 40000;
-    const MAJOR_VERSION = 0;
+    const MAJOR_VERSION = 4;
     const MINOR_VERSION = 0;
     const RELEASE_VERSION = 0;
     const EXTRA_VERSION = 'DEV';

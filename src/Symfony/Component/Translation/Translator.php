7b14d4f4ec2fd089da0a07956f4d4a753bc75cc9
Robin Chalas <robin.chalas@gmail.com>
2017-05-21 11:59:35 +0200

    Add missing doc param & deprecation changelog entries

--- src/Symfony/Component/Translation/Translator.php
+++ src/Symfony/Component/Translation/Translator.php
@@ -74,10 +74,11 @@ class Translator implements TranslatorInterface, TranslatorBagInterface
     /**
      * Constructor.
      *
-     * @param string               $locale   The locale
-     * @param MessageSelector|null $selector The message selector for pluralization
-     * @param string|null          $cacheDir The directory to use for the cache
-     * @param bool                 $debug    Use cache in debug mode ?
+     * @param string               $locale        The locale
+     * @param MessageSelector|null $selector      The message selector for pluralization
+     * @param string               $defaultLocale
+     * @param string|null          $cacheDir      The directory to use for the cache
+     * @param bool                 $debug         Use cache in debug mode ?
      *
      * @throws InvalidArgumentException If a locale contains invalid characters
      */

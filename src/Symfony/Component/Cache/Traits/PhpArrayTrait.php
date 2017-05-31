36893dc405ae17744d45dac6dab267034c57252b
Roland Franssen <franssen.roland@gmail.com>
2017-05-31 09:13:11 +0200

    Update PhpArrayTrait.php

--- src/Symfony/Component/Cache/Traits/PhpArrayTrait.php
+++ src/Symfony/Component/Cache/Traits/PhpArrayTrait.php
@@ -126,6 +126,6 @@ EOF;
      */
     private function initialize()
     {
-        $this->values = false !== stream_resolve_include_path($this->file) ? @(include $this->file) : array();
+        $this->values = file_exists($this->file) ? (include $this->file ?: array()) : array();
     }
 }

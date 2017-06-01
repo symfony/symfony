3371db9f8cd332932495eaa592ccbbf0e9c66729
Romain Neutron <imprec@gmail.com>
2017-06-01 14:40:39 +0200

    Fix optional cache warmers are always instantiated whereas they should be lazy-loaded

--- src/Symfony/Bundle/TwigBundle/CacheWarmer/TemplateCacheWarmer.php
+++ src/Symfony/Bundle/TwigBundle/CacheWarmer/TemplateCacheWarmer.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Bundle\TwigBundle\CacheWarmer;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
 use Twig\Environment;
 use Twig\Error\Error;
@@ -22,12 +23,27 @@ use Twig\Error\Error;
  */
 class TemplateCacheWarmer implements CacheWarmerInterface
 {
+    private $container;
     private $twig;
     private $iterator;
 
-    public function __construct(Environment $twig, \Traversable $iterator)
+    /**
+     * TemplateCacheWarmer constructor.
+     *
+     * @param ContainerInterface|Environment $container
+     * @param \Traversable                   $iterator
+     */
+    public function __construct($container, \Traversable $iterator)
     {
-        $this->twig = $twig;
+        // As this cache warmer is optional, dependencies should be lazy-loaded, that's why a container should be injected.
+        if ($container instanceof ContainerInterface) {
+            $this->container = $container;
+        } elseif ($container instanceof Environment) {
+            $this->twig = $container;
+        } else {
+            throw new \InvalidArgumentException(sprintf('%s only accepts instance of Symfony\Component\DependencyInjection\ContainerInterface or Environment as first argument.', __CLASS__));
+        }
+
         $this->iterator = $iterator;
     }
 
@@ -36,6 +52,10 @@ class TemplateCacheWarmer implements CacheWarmerInterface
      */
     public function warmUp($cacheDir)
     {
+        if (null === $this->twig) {
+            $this->twig = $this->container->get('twig');
+        }
+
         foreach ($this->iterator as $template) {
             try {
                 $this->twig->loadTemplate($template);

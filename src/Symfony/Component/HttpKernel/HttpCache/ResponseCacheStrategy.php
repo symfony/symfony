c6e8c07e4df22bdf950954266a255dabefaee69a
Matthias Pigulla <mp@webfactory.de>
2017-06-11 00:34:20 +0200

    Fix two edge cases in ResponseCacheStrategy

--- src/Symfony/Component/HttpKernel/HttpCache/ResponseCacheStrategy.php
+++ src/Symfony/Component/HttpKernel/HttpCache/ResponseCacheStrategy.php
@@ -39,7 +39,7 @@ class ResponseCacheStrategy implements ResponseCacheStrategyInterface
      */
     public function add(Response $response)
     {
-        if ($response->isValidateable()) {
+        if ($response->isValidateable() || !$response->isCacheable()) {
             $this->cacheable = false;
         } else {
             $maxAge = $response->getMaxAge();

894ae0d0ebe1b4a1564cba8497040567d9db90f9
Matthias Pigulla <mp@webfactory.de>
2017-06-11 00:34:20 +0200

    Fix two edge cases when merging private responses or responses with no caching information

--- src/Symfony/Component/HttpKernel/Tests/HttpCache/ResponseCacheStrategyTest.php
+++ src/Symfony/Component/HttpKernel/Tests/HttpCache/ResponseCacheStrategyTest.php
@@ -137,4 +137,45 @@ class ResponseCacheStrategyTest extends TestCase
 
         $this->assertTrue($masterResponse->isFresh());
     }
+
+    public function testMasterResponseIsNotCacheableWhenEmbeddedResponseIsNotCacheable()
+    {
+        $cacheStrategy = new ResponseCacheStrategy();
+
+        $masterResponse = new Response();
+        $masterResponse->setSharedMaxAge(3600); // Public, cacheable
+
+        /* This response has no validation or expiration information.
+           That makes it uncacheable, it is always stale.
+           (It does *not* make this private, though.) */
+        $embeddedResponse = new Response();
+        $this->assertFalse($embeddedResponse->isFresh()); // not fresh, as no lifetime is provided
+
+        $cacheStrategy->add($embeddedResponse);
+        $cacheStrategy->update($masterResponse);
+
+        $this->assertTrue($masterResponse->headers->hasCacheControlDirective('no-cache'));
+        $this->assertTrue($masterResponse->headers->hasCacheControlDirective('must-revalidate'));
+        $this->assertFalse($masterResponse->isFresh());
+    }
+
+    public function testEmbeddingPrivateResponseMakesMainResponsePrivate()
+    {
+        $cacheStrategy = new ResponseCacheStrategy();
+
+        $masterResponse = new Response();
+        $masterResponse->setSharedMaxAge(3600); // public, cacheable
+
+        // The embedded response might for example contain per-user data that remains valid for 60 seconds
+        $embeddedResponse = new Response();
+        $embeddedResponse->setPrivate();
+        $embeddedResponse->setMaxAge(60); // this would implicitly set "private" as well, but let's be explicit
+
+        $cacheStrategy->add($embeddedResponse);
+        $cacheStrategy->update($masterResponse);
+
+        $this->assertTrue($masterResponse->headers->hasCacheControlDirective('private'));
+        // Not sure if we should pass "max-age: 60" in this case, as long as the response is private and
+        // that's the more conservative of both the master and embedded response...?
+    }
 }

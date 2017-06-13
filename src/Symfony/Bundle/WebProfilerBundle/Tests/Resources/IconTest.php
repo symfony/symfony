c8fc718551236ea4ea71bf660d1a8ad2fb082028
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-06-13 12:50:53 +0200

    Improved the failed test message

--- src/Symfony/Bundle/WebProfilerBundle/Tests/Resources/IconTest.php
+++ src/Symfony/Bundle/WebProfilerBundle/Tests/Resources/IconTest.php
@@ -19,7 +19,7 @@ class IconTest extends TestCase
     {
         $iconFiles = glob(__DIR__.'/../../Resources/views/Icon/*.svg');
         foreach ($iconFiles as $iconFile) {
-            $this->assertRegExp('/<svg version="1.1" xmlns="http:\/\/www.w3.org\/2000\/svg" x="0px" y="0px" width="\d+" height="\d+" viewBox="0 0 \d+ \d+" enable-background="new 0 0 \d+ \d+" xml:space="preserve">.*<\/svg>/s', file_get_contents($iconFile), $iconFile);
+            $this->assertRegExp('/<svg version="1.1" xmlns="http:\/\/www.w3.org\/2000\/svg" x="0px" y="0px" width="\d+" height="\d+" viewBox="0 0 \d+ \d+" enable-background="new 0 0 \d+ \d+" xml:space="preserve">.*<\/svg>/s', file_get_contents($iconFile), sprintf('The SVG metadata of the %s icon is different than expected (use the same as the other icons).', $iconFile));
         }
     }
 }

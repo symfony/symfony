97fe4b152782ced8decf7ccb5a0ad2a206380db4
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-06-13 14:42:23 +0200

    Improved the test

--- src/Symfony/Bundle/WebProfilerBundle/Tests/Resources/IconTest.php
+++ src/Symfony/Bundle/WebProfilerBundle/Tests/Resources/IconTest.php
@@ -11,15 +11,23 @@
 
 namespace Symfony\Bundle\WebProfilerBundle\Tests\Resources;
 
-use Symfony\Bundle\WebProfilerBundle\Tests\TestCase;
+use PHPUnit\Framework\TestCase;
 
 class IconTest extends TestCase
 {
-    public function testIconFileContents()
+    /**
+     * @dataProvider provideIconFilePaths
+     */
+    public function testIconFileContents($iconFilePath)
     {
-        $iconFiles = glob(__DIR__.'/../../Resources/views/Icon/*.svg');
-        foreach ($iconFiles as $iconFile) {
-            $this->assertRegExp('/<svg version="1.1" xmlns="http:\/\/www.w3.org\/2000\/svg" x="0px" y="0px" width="\d+" height="\d+" viewBox="0 0 \d+ \d+" enable-background="new 0 0 \d+ \d+" xml:space="preserve">.*<\/svg>/s', file_get_contents($iconFile), sprintf('The SVG metadata of the %s icon is different than expected (use the same as the other icons).', $iconFile));
-        }
+        $this->assertRegExp('/<svg version="1.1" xmlns="http:\/\/www.w3.org\/2000\/svg" x="0px" y="0px" width="\d+" height="\d+" viewBox="0 0 \d+ \d+" enable-background="new 0 0 \d+ \d+" xml:space="preserve">.*<\/svg>/s', file_get_contents($iconFilePath), sprintf('The SVG metadata of the %s icon is different than expected (use the same as the other icons).', $iconFilePath));
+    }
+
+    public function provideIconFilePaths()
+    {
+        return array_map(
+            function ($filePath) { return (array) $filePath; },
+            glob(__DIR__.'/../../Resources/views/Icon/*.svg')
+        );
     }
 }

8f9791106bb4504ba65033bf30098415d9395ac9
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-06-13 12:49:01 +0200

    Added a unit test to check the icon contents

---
+++ src/Symfony/Bundle/WebProfilerBundle/Tests/Resources/IconTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\WebProfilerBundle\Tests\Resources;
+
+use Symfony\Bundle\WebProfilerBundle\Tests\TestCase;
+
+class IconTest extends TestCase
+{
+    public function testIconFileContents()
+    {
+        $iconFiles = glob(__DIR__.'/../../Resources/views/Icon/*.svg');
+        foreach ($iconFiles as $iconFile) {
+            $this->assertRegExp('/<svg version="1.1" xmlns="http:\/\/www.w3.org\/2000\/svg" x="0px" y="0px" width="\d+" height="\d+" viewBox="0 0 \d+ \d+" enable-background="new 0 0 \d+ \d+" xml:space="preserve">.*<\/svg>/s', file_get_contents($iconFile), $iconFile);
+        }
+    }
+}

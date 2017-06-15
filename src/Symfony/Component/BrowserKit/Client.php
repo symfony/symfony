6131667a7715018b41dad71c8369a7e82b77ac98
Henne Van Och <henne.van.och@gmail.com>
2017-06-15 15:24:26 +0200

    Reset redirectCount when throwing exception that max numbers redirects was reached

--- src/Symfony/Component/BrowserKit/Client.php
+++ src/Symfony/Component/BrowserKit/Client.php
@@ -468,6 +468,7 @@ abstract class Client
 
         if (-1 !== $this->maxRedirects) {
             if ($this->redirectCount > $this->maxRedirects) {
+                $this->redirectCount = 0;
                 throw new \LogicException(sprintf('The maximum number (%d) of redirections was reached.', $this->maxRedirects));
             }
         }

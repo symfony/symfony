30728d999c4f579dff052e61da5587f34bd76b6e
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-26 09:36:22 +0200

    Make sure the events are dispatched in the correct order

--- src/Symfony/Component/Workflow/Workflow.php
+++ src/Symfony/Component/Workflow/Workflow.php
@@ -300,10 +300,13 @@ class Workflow
 
         $event = new Event($subject, $marking, $initialTransition);
 
-        $this->dispatcher->dispatch('workflow.announce', $event);
-        $this->dispatcher->dispatch(sprintf('workflow.%s.announce', $this->name), $event);
-
+        $eventsDispatched = false;
         foreach ($this->getEnabledTransitions($subject) as $transition) {
+            if (!$eventsDispatched) {
+                $this->dispatcher->dispatch('workflow.announce', $event);
+                $this->dispatcher->dispatch(sprintf('workflow.%s.announce', $this->name), $event);
+                $eventsDispatched = true;
+            }
             $this->dispatcher->dispatch(sprintf('workflow.%s.announce.%s', $this->name, $transition->getName()), $event);
         }
     }

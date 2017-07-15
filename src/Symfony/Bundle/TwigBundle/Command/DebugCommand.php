d796f957b0d54f942c57cc517aebaf11b62e30c4
Roland Franssen <franssen.roland@gmail.com>
2017-07-15 12:11:15 +0200

    [TwigBundle] Commands as a service

--- src/Symfony/Bundle/TwigBundle/Command/DebugCommand.php
+++ src/Symfony/Bundle/TwigBundle/Command/DebugCommand.php
@@ -11,6 +11,8 @@
 
 namespace Symfony\Bundle\TwigBundle\Command;
 
+@trigger_error(sprintf('The %s class is deprecated since version 3.4 and will be removed in 4.0. Use Symfony\Bridge\Twig\Command\DebugCommand instead.', DebugCommand::class), E_USER_DEPRECATED);
+
 use Symfony\Bridge\Twig\Command\DebugCommand as BaseDebugCommand;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
@@ -19,6 +21,8 @@ use Symfony\Component\DependencyInjection\ContainerAwareTrait;
  * Lists twig functions, filters, globals and tests present in the current project.
  *
  * @author Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * @deprecated since version 3.4, to be removed in 4.0.
  */
 final class DebugCommand extends BaseDebugCommand implements ContainerAwareInterface
 {

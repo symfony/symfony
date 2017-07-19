fa0b9428c5c895f14f60d7f5544a29aeba725f62
Nicolas Grekas <nicolas.grekas@gmail.com>
2017-07-19 09:37:29 +0200

    [Config] Minor fix

--- src/Symfony/Component/Config/Resource/ClassExistenceResource.php
+++ src/Symfony/Component/Config/Resource/ClassExistenceResource.php
@@ -149,7 +149,7 @@ class ClassExistenceResource implements SelfCheckingResourceInterface, \Serializ
             $props = array(
                 'file' => $trace[$i]['file'],
                 'line' => $trace[$i]['line'],
-                'trace' => array_slice($trace, 0, 1 + $i),
+                'trace' => array_slice($trace, 1 + $i),
             );
 
             foreach ($props as $p => $v) {

6c8aa4044ca7bbc1b024e617d7c29cd2d35acdc2
Tomáš Fejfar <tomas.fejfar@gmail.com>
2017-07-31 22:13:10 +0200

    Tests for "executable" option

--- src/Symfony/Bundle/WebServerBundle/Tests/WebServerConfig/WebServerConfigTest.php
+++ src/Symfony/Bundle/WebServerBundle/Tests/WebServerConfig/WebServerConfigTest.php
@@ -22,7 +22,8 @@ class WebServerConfigTest extends TestCase
             __DIR__.'/fixtures',
             'dev',
             '85.111.31.18:8080',
-            __DIR__.'/fixtures/router.php'
+            __DIR__.'/fixtures/router.php',
+            '/usr/bin/php -c /tmp/custom/php.ini'
         );
 
         $this->assertSame(
@@ -36,6 +37,7 @@ class WebServerConfigTest extends TestCase
             $this->normalizePath(__DIR__.'/fixtures/router.php'),
             $this->normalizePath($config->getRouter())
         );
+        $this->assertSame('/usr/bin/php -c /tmp/custom/php.ini', $config->getExecutable());
     }
 
     public function testWillSetCorrectAddressAndPortAutomatically()
@@ -112,6 +114,14 @@ class WebServerConfigTest extends TestCase
             $this->normalizePath(dirname(dirname(__DIR__)).'/Resources/router.php'),
             $this->normalizePath($config->getRouter())
         );
+    }
+
+    public function testWillTryToFindExectuableIfNotPresent()
+    {
+        $config = new WebServerConfig(__DIR__.'/fixtures', 'dev');
+
+        // symplified as path will vary a lot on different systems
+        $this->assertNotEmpty($config->getExecutable());
     }
 
     /**

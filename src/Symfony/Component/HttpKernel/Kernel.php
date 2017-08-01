927f95001913724cb12e6b79043193497e7062e1
Fabien Potencier <fabien.potencier@gmail.com>
2017-08-01 12:20:44 +0200

    bumped Symfony version to 3.2.14

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -58,12 +58,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION = '3.2.13';
-    const VERSION_ID = 30213;
+    const VERSION = '3.2.14-DEV';
+    const VERSION_ID = 30214;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 2;
-    const RELEASE_VERSION = 13;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 14;
+    const EXTRA_VERSION = 'DEV';
 
     const END_OF_MAINTENANCE = '07/2017';
     const END_OF_LIFE = '01/2018';

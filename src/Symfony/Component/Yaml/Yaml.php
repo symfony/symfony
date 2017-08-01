6e88335f533178663c2a9f6bc1c42489efc84d31
Guilhem Niot <guilhem.niot@gmail.com>
2017-08-01 22:29:09 +0200

    fix

--- src/Symfony/Component/Yaml/Yaml.php
+++ src/Symfony/Component/Yaml/Yaml.php
@@ -33,8 +33,7 @@ class Yaml
     const DUMP_EMPTY_ARRAY_AS_SEQUENCE = 1024;
 
     /**
-     * @deprecated since version 3.4, to be removed in 4.0. Quote your evaluable
-     * keys instead.
+     * @deprecated since version 3.4, to be removed in 4.0. Quote your evaluable keys instead.
      */
     const PARSE_KEYS_AS_STRINGS = 2048;
 

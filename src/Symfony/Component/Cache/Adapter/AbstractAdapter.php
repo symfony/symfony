94b1b12b55f400a9b38e3307375311a06db9fea6
Lars Strojny <lars@strojny.net>
2017-08-02 15:53:45 +0200

    Hash cache keys on save

--- src/Symfony/Component/Cache/Adapter/AbstractAdapter.php
+++ src/Symfony/Component/Cache/Adapter/AbstractAdapter.php
@@ -55,19 +55,20 @@ abstract class AbstractAdapter implements AdapterInterface, LoggerAwareInterface
             null,
             CacheItem::class
         );
+        $getId = function ($key) { return $this->getId((string) $key); };
         $this->mergeByLifetime = \Closure::bind(
-            function ($deferred, $namespace, &$expiredIds) {
+            function ($deferred, $namespace, &$expiredIds) use ($getId) {
                 $byLifetime = array();
                 $now = time();
                 $expiredIds = array();
 
                 foreach ($deferred as $key => $item) {
                     if (null === $item->expiry) {
-                        $byLifetime[0 < $item->defaultLifetime ? $item->defaultLifetime : 0][$namespace.$key] = $item->value;
+                        $byLifetime[0 < $item->defaultLifetime ? $item->defaultLifetime : 0][$getId($key)] = $item->value;
                     } elseif ($item->expiry > $now) {
-                        $byLifetime[$item->expiry - $now][$namespace.$key] = $item->value;
+                        $byLifetime[$item->expiry - $now][$getId($key)] = $item->value;
                     } else {
-                        $expiredIds[] = $namespace.$key;
+                        $expiredIds[] = $getId($key);
                     }
                 }
 
--- src/Symfony/Component/Cache/Tests/Adapter/PhpArrayAdapterTest.php
+++ src/Symfony/Component/Cache/Tests/Adapter/PhpArrayAdapterTest.php
@@ -22,6 +22,7 @@ class PhpArrayAdapterTest extends AdapterTestCase
 {
     protected $skippedTests = array(
         'testBasicUsage' => 'PhpArrayAdapter is read-only.',
+        'testBasicUsageWithLongKey' => 'PhpArrayAdapter is read-only.',
         'testClear' => 'PhpArrayAdapter is read-only.',
         'testClearWithDeferredItems' => 'PhpArrayAdapter is read-only.',
         'testDeleteItem' => 'PhpArrayAdapter is read-only.',

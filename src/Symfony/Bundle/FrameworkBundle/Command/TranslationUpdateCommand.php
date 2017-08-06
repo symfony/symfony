183a121045297961abc333b6f7aebbdee10e222b
Roland Franssen <franssen.roland@gmail.com>
2017-08-06 13:36:56 +0200

    tweaked xliff+yaml lint commands

--- src/Symfony/Bundle/FrameworkBundle/Command/TranslationUpdateCommand.php
+++ src/Symfony/Bundle/FrameworkBundle/Command/TranslationUpdateCommand.php
@@ -46,7 +46,6 @@ class TranslationUpdateCommand extends ContainerAwareCommand
      */
     public function __construct($writer = null, TranslationLoader $loader = null, ExtractorInterface $extractor = null, $defaultLocale = null)
     {
-
         if (!$writer instanceof TranslationWriter) {
             @trigger_error(sprintf('Passing a command name as the first argument of "%s" is deprecated since version 3.4 and will be removed in 4.0. If the command was registered by convention, make it a service instead.', __METHOD__), E_USER_DEPRECATED);
 

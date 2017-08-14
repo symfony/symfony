c36d2dd211054166443fe394bc4c85466ae70e7c
Sartech-B <sarthakbawal@gmail.com>
2017-08-14 18:20:13 +0400

    Removed unnecessary use of extended class 'Testcase'

--- src/Symfony/Bridge/PhpUnit/Tests/DnsMockTest.php
+++ src/Symfony/Bridge/PhpUnit/Tests/DnsMockTest.php
@@ -14,7 +14,7 @@ namespace Symfony\Bridge\PhpUnit\Tests;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bridge\PhpUnit\DnsMock;
 
-class DnsMockTest/* extends TestCase*/
+class DnsMockTest
 {
     protected function tearDown()
     {

cce2dd1d5e7c72125140f28ab23d9b045f3bae67
Dany Maillard <danymaillard93b@gmail.com>
2017-08-28 22:48:26 +0200

    Remove deprecated feature

--- src/Symfony/Component/Translation/Writer/TranslationWriter.php
+++ src/Symfony/Component/Translation/Writer/TranslationWriter.php
@@ -89,21 +89,4 @@ class TranslationWriter implements TranslationWriterInterface
         // save
         $dumper->dump($catalogue, $options);
     }
-
-    /**
-     * Writes translation from the catalogue according to the selected format.
-     *
-     * @param MessageCatalogue $catalogue The message catalogue to write
-     * @param string           $format    The format to use to dump the messages
-     * @param array            $options   Options that are passed to the dumper
-     *
-     * @throws InvalidArgumentException
-     *
-     * @deprecated since 3.4 will be removed in 4.0. Use write instead.
-     */
-    public function writeTranslations(MessageCatalogue $catalogue, $format, $options = array())
-    {
-        @trigger_error(sprintf('Method %s() is deprecated since version 3.4 and will be removed in 4.0. Use write() instead.', __METHOD__), E_USER_DEPRECATED);
-        $this->write($catalogue, $format, $options);
-    }
 }

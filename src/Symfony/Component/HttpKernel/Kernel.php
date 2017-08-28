9280ca429ea515c4d49a72ce236872562eb9c4a9
Fabien Potencier <fabien.potencier@gmail.com>
2017-08-28 15:49:36 -0700

    bumped Symfony version to 3.3.9

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -61,12 +61,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
 
     private $projectDir;
 
-    const VERSION = '3.3.8';
-    const VERSION_ID = 30308;
+    const VERSION = '3.3.9-DEV';
+    const VERSION_ID = 30309;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 3;
-    const RELEASE_VERSION = 8;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 9;
+    const EXTRA_VERSION = 'DEV';
 
     const END_OF_MAINTENANCE = '01/2018';
     const END_OF_LIFE = '07/2018';

1a32a731c1be11d5756ff6d09ca8ef19184c23cb
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-09-07 16:58:22 +0200

    Use the new setDeprecated() method

--- src/Symfony/Bundle/WebProfilerBundle/DependencyInjection/Configuration.php
+++ src/Symfony/Bundle/WebProfilerBundle/DependencyInjection/Configuration.php
@@ -39,12 +39,7 @@ class Configuration implements ConfigurationInterface
                 ->booleanNode('toolbar')->defaultFalse()->end()
                 ->scalarNode('position')
                     ->defaultValue('bottom')
-                    ->beforeNormalization()
-                        ->ifTrue(function ($v) { return null !== $v; })
-                        ->then(function () {
-                            @trigger_error('The "web_profiler.position" configuration key has been deprecated in Symfony 3.4 and it will be removed in 4.0.', E_USER_DEPRECATED);
-                        })
-                    ->end()
+                    ->setDeprecated('The "web_profiler.position" configuration key has been deprecated in Symfony 3.4 and it will be removed in 4.0.')
                     ->validate()
                         ->ifNotInArray(array('bottom', 'top'))
                         ->thenInvalid('The CSS position %s is not supported')

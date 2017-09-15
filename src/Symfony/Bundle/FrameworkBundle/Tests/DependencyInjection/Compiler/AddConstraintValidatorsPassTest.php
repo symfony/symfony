1ee77ff24607a5b29780cbd0da118298a9a9b977
Christian Flothmann <christian.flothmann@sensiolabs.de>
2017-09-15 17:15:08 +0200

    deal with decorated constraint validator factories

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Compiler/AddConstraintValidatorsPassTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Compiler/AddConstraintValidatorsPassTest.php
@@ -13,67 +13,70 @@ namespace Symfony\Bundle\FrameworkBundle\Tests\DependencyInjection\Compiler;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\DependencyInjection\Compiler\AddConstraintValidatorsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 class AddConstraintValidatorsPassTest extends TestCase
 {
     public function testThatConstraintValidatorServicesAreProcessed()
     {
-        $services = array(
-            'my_constraint_validator_service1' => array(0 => array('alias' => 'my_constraint_validator_alias1')),
-            'my_constraint_validator_service2' => array(),
-        );
+        $container = new ContainerBuilder();
+        $validatorFactoryDefinition = $container->register('validator.validator_factory')
+            ->setArguments(array(new Reference('service_container'), array()));
 
-        $validatorFactoryDefinition = $this->getMockBuilder('Symfony\Component\DependencyInjection\Definition')->getMock();
-        $container = $this->getMockBuilder('Symfony\Component\DependencyInjection\ContainerBuilder')->setMethods(array('findTaggedServiceIds', 'getDefinition', 'hasDefinition'))->getMock();
-
-        $validatorDefinition1 = $this->getMockBuilder('Symfony\Component\DependencyInjection\Definition')->setMethods(array('getClass'))->getMock();
-        $validatorDefinition2 = $this->getMockBuilder('Symfony\Component\DependencyInjection\Definition')->setMethods(array('getClass'))->getMock();
-
-        $validatorDefinition1->expects($this->atLeastOnce())
-            ->method('getClass')
-            ->willReturn('My\Fully\Qualified\Class\Named\Validator1');
-        $validatorDefinition2->expects($this->atLeastOnce())
-            ->method('getClass')
-            ->willReturn('My\Fully\Qualified\Class\Named\Validator2');
-
-        $container->expects($this->any())
-            ->method('getDefinition')
-            ->with($this->anything())
-            ->will($this->returnValueMap(array(
-                array('my_constraint_validator_service1', $validatorDefinition1),
-                array('my_constraint_validator_service2', $validatorDefinition2),
-                array('validator.validator_factory', $validatorFactoryDefinition),
-            )));
-
-        $container->expects($this->atLeastOnce())
-            ->method('findTaggedServiceIds')
-            ->will($this->returnValue($services));
-        $container->expects($this->atLeastOnce())
-            ->method('hasDefinition')
-            ->with('validator.validator_factory')
-            ->will($this->returnValue(true));
-
-        $validatorFactoryDefinition->expects($this->once())
-            ->method('replaceArgument')
-            ->with(1, array(
-                'My\Fully\Qualified\Class\Named\Validator1' => 'my_constraint_validator_service1',
-                'my_constraint_validator_alias1' => 'my_constraint_validator_service1',
-                'My\Fully\Qualified\Class\Named\Validator2' => 'my_constraint_validator_service2',
-            ));
+        $container->register('my_constraint_validator_service1', 'My\Fully\Qualified\Class\Named\Validator1')
+            ->addTag('validator.constraint_validator', array('alias' => 'my_constraint_validator_alias1'));
+        $container->register('my_constraint_validator_service2', 'My\Fully\Qualified\Class\Named\Validator2')
+            ->addTag('validator.constraint_validator');
 
         $addConstraintValidatorsPass = new AddConstraintValidatorsPass();
         $addConstraintValidatorsPass->process($container);
+
+        $this->assertEquals(
+            array(
+                'My\Fully\Qualified\Class\Named\Validator1' => 'my_constraint_validator_service1',
+                'my_constraint_validator_alias1' => 'my_constraint_validator_service1',
+                'My\Fully\Qualified\Class\Named\Validator2' => 'my_constraint_validator_service2',
+            ),
+            $validatorFactoryDefinition->getArgument(1)
+        );
+    }
+
+    public function testThatConstraintValidatorServicesAreProcessedIfTheValidatorFactoryIsDecorated()
+    {
+        $container = new ContainerBuilder();
+        $validatorFactoryDefinition = $container->register('validator_factory_decorator.inner')
+            ->setArguments(array(new Reference('service_container'), array()));
+        $container->register('validator_factory_decorator')->setArguments(array(new Reference('validator_factory_decorator.inner')));
+        $container->setAlias('validator.validator_factory', 'validator_factory_decorator.inner');
+
+        $container->register('my_constraint_validator_service1', 'My\Fully\Qualified\Class\Named\Validator1')
+            ->addTag('validator.constraint_validator', array('alias' => 'my_constraint_validator_alias1'));
+        $container->register('my_constraint_validator_service2', 'My\Fully\Qualified\Class\Named\Validator2')
+            ->addTag('validator.constraint_validator');
+
+        $addConstraintValidatorsPass = new AddConstraintValidatorsPass();
+        $addConstraintValidatorsPass->process($container);
+
+        $this->assertEquals(
+            array(
+                'My\Fully\Qualified\Class\Named\Validator1' => 'my_constraint_validator_service1',
+                'my_constraint_validator_alias1' => 'my_constraint_validator_service1',
+                'My\Fully\Qualified\Class\Named\Validator2' => 'my_constraint_validator_service2',
+            ),
+            $validatorFactoryDefinition->getArgument(1)
+        );
     }
 
     public function testThatCompilerPassIsIgnoredIfThereIsNoConstraintValidatorFactoryDefinition()
     {
         $definition = $this->getMockBuilder('Symfony\Component\DependencyInjection\Definition')->getMock();
-        $container = $this->getMockBuilder('Symfony\Component\DependencyInjection\ContainerBuilder')->setMethods(array('hasDefinition', 'findTaggedServiceIds', 'getDefinition'))->getMock();
+        $container = $this->getMockBuilder('Symfony\Component\DependencyInjection\ContainerBuilder')->setMethods(array('has', 'findTaggedServiceIds', 'getDefinition'))->getMock();
 
         $container->expects($this->never())->method('findTaggedServiceIds');
         $container->expects($this->never())->method('getDefinition');
         $container->expects($this->atLeastOnce())
-            ->method('hasDefinition')
+            ->method('has')
             ->with('validator.validator_factory')
             ->will($this->returnValue(false));
         $definition->expects($this->never())->method('replaceArgument');

6c3602d0919c1bb368d78ddd7cbe525165a053ea
Matthias Pigulla <mp@webfactory.de>
2017-09-17 23:59:02 +0200

    Fix LICENSE header (although I did not touch it)

--- src/Symfony/Component/HttpKernel/HttpCache/HttpCache.php
+++ src/Symfony/Component/HttpKernel/HttpCache/HttpCache.php
@@ -5,6 +5,15 @@
  *
  * (c) Fabien Potencier <fabien@symfony.com>
  *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
  * This code is partially based on the Rack-Cache library by Ryan Tomayko,
  * which is released under the MIT license.
  * (based on commit 02d2b48d75bcb63cf1c0c7149c077ad256542801)

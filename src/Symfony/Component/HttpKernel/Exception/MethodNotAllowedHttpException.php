47d4b78a270e7918155f1108a80c47ff442cc763
Peter Gribanov <wcode404@gmail.com>
2017-09-21 14:41:14 +0300

    wrap MethodNotAllowedHttpException

--- src/Symfony/Component/HttpKernel/Exception/MethodNotAllowedHttpException.php
+++ src/Symfony/Component/HttpKernel/Exception/MethodNotAllowedHttpException.php
@@ -32,4 +32,15 @@ class MethodNotAllowedHttpException extends HttpException
 
         parent::__construct(405, $message, $previous, $headers, $code);
     }
+
+    /**
+     * @param \Exception $previous The previous exception
+     * @param int        $code     The internal exception code
+     *
+     * @return MethodNotAllowedHttpException
+     */
+    public static function wrap(\Exception $previous, $code = 0)
+    {
+        return new static($previous->getMessage(), $previous, $code);
+    }
 }

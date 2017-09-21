d1733ee5cfae3dd22603fb258b28eebe3aed30ee
Peter Gribanov <wcode404@gmail.com>
2017-09-21 14:46:26 +0300

    add $retryAfter

--- src/Symfony/Component/HttpKernel/Exception/TooManyRequestsHttpException.php
+++ src/Symfony/Component/HttpKernel/Exception/TooManyRequestsHttpException.php
@@ -39,13 +39,14 @@ class TooManyRequestsHttpException extends HttpException
     }
 
     /**
-     * @param \Exception $previous The previous exception
-     * @param int        $code     The internal exception code
+     * @param int|string $retryAfter The number of seconds or HTTP-date after which the request may be retried
+     * @param \Exception $previous   The previous exception
+     * @param int        $code       The internal exception code
      *
      * @return TooManyRequestsHttpException
      */
-    public static function wrap(\Exception $previous, $code = 0)
+    public static function wrap($retryAfter = null, \Exception $previous, $code = 0)
     {
-        return new static($previous->getMessage(), $previous, $code);
+        return new static($retryAfter, $previous->getMessage(), $previous, $code);
     }
 }

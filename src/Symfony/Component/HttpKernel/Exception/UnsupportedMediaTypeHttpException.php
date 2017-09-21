8963ccad74a782f3387ce3d608242af095e10a65
Peter Gribanov <wcode404@gmail.com>
2017-09-21 14:45:20 +0300

    wrap UnsupportedMediaTypeHttpException

--- src/Symfony/Component/HttpKernel/Exception/UnsupportedMediaTypeHttpException.php
+++ src/Symfony/Component/HttpKernel/Exception/UnsupportedMediaTypeHttpException.php
@@ -29,4 +29,15 @@ class UnsupportedMediaTypeHttpException extends HttpException
     {
         parent::__construct(415, $message, $previous, array(), $code);
     }
+
+    /**
+     * @param \Exception $previous The previous exception
+     * @param int        $code     The internal exception code
+     *
+     * @return UnsupportedMediaTypeHttpException
+     */
+    public static function wrap(\Exception $previous, $code = 0)
+    {
+        return new static($previous->getMessage(), $previous, $code);
+    }
 }

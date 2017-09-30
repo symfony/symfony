c109dcd5ae0a9c2979bf5ad2957b029f79960b51
Maxime Steinhausser <maxime.steinhausser@gmail.com>
2017-09-30 14:58:47 +0200

    [FrameworkBundle] Register a NullLogger from test kernels

--- src/Symfony/Bundle/FrameworkBundle/Tests/Command/CacheClearCommand/Fixture/TestAppKernel.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Command/CacheClearCommand/Fixture/TestAppKernel.php
@@ -11,8 +11,10 @@
 
 namespace Symfony\Bundle\FrameworkBundle\Tests\Command\CacheClearCommand\Fixture;
 
+use Psr\Log\NullLogger;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 
 class TestAppKernel extends Kernel
@@ -32,5 +34,10 @@ class TestAppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.DIRECTORY_SEPARATOR.'config.yml');
+    }
+
+    protected function build(ContainerBuilder $container)
+    {
+        $container->register('logger', NullLogger::class);
     }
 }

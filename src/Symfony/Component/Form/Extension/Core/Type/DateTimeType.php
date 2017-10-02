802c67c3242d6df99b3da202fab1c017c88a20a4
Pierre du Plessis <pierre@pcservice.co.za>
2017-10-02 14:38:17 +0200

    Change datetime input to datetime-local

--- src/Symfony/Component/Form/Extension/Core/Type/DateTimeType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/DateTimeType.php
@@ -34,17 +34,12 @@ class DateTimeType extends AbstractType
 
     /**
      * This is not quite the HTML5 format yet, because ICU lacks the
-     * capability of parsing and generating RFC 3339 dates, which
-     * are like the below pattern but with a timezone suffix. The
-     * timezone suffix is.
-     *
-     *  * "Z" for UTC
-     *  * "(-|+)HH:mm" for other timezones (note the colon!)
+     * capability of parsing and generating RFC 3339 dates.
      *
      * For more information see:
      *
      * http://userguide.icu-project.org/formatparse/datetime#TOC-Date-Time-Format-Syntax
-     * http://www.w3.org/TR/html-markup/input.datetime.html
+     * https://www.w3.org/TR/html5/sec-forms.html#local-date-and-time-state-typedatetimelocal
      * http://tools.ietf.org/html/rfc3339
      *
      * An ICU ticket was created:
@@ -54,7 +49,7 @@ class DateTimeType extends AbstractType
      * yet. To temporarily circumvent this issue, DateTimeToRfc3339Transformer
      * is used when the format matches this constant.
      */
-    const HTML5_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZZZZZ";
+    const HTML5_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
     private static $acceptedFormats = array(
         \IntlDateFormatter::FULL,
@@ -192,7 +187,7 @@ class DateTimeType extends AbstractType
         //  * the format matches the one expected by HTML5
         //  * the html5 is set to true
         if ($options['html5'] && 'single_text' === $options['widget'] && self::HTML5_FORMAT === $options['format']) {
-            $view->vars['type'] = 'datetime';
+            $view->vars['type'] = 'datetime-local';
         }
     }
 

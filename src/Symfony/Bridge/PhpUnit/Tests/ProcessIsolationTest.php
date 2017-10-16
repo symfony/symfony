fb8ba79799ea851a58eab6c8820acc946c3b2a3f
Alex Pott <alex.a.pott@googlemail.com>
2017-10-16 14:30:39 +0100

    Ensure that PHPUnit's error handler is still working in isolated tests

--- src/Symfony/Bridge/PhpUnit/Tests/ProcessIsolationTest.php
+++ src/Symfony/Bridge/PhpUnit/Tests/ProcessIsolationTest.php
@@ -3,6 +3,7 @@
 namespace Symfony\Bridge\PhpUnit\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\PhpUnit\DeprecationErrorHandler;
 
 /**
  * Don't remove this test case, it tests the legacy group.
@@ -14,11 +15,32 @@ use PHPUnit\Framework\TestCase;
 class ProcessIsolationTest extends TestCase
 {
     /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        // Ensure we are using the deprecation error handler. Unfortunately the code in bootstrap.php does not appear to
+        // be working.
+        DeprecationErrorHandler::collectDeprecations(getenv('SYMFONY_DEPRECATIONS_SERIALIZE'));
+        parent::setUp();
+    }
+    /**
      * @expectedDeprecation Test abc
      */
     public function testIsolation()
     {
         @trigger_error('Test abc', E_USER_DEPRECATED);
-        $this->addToAssertionCount(1);
+    }
+
+    public function testCallingOtherErrorHandler()
+    {
+        if (method_exists($this, 'expectException')) {
+            $this->expectException('PHPUnit\Framework\Error\Warning');
+            $this->expectExceptionMessage('Test that PHPUnit\'s error handler fires.');
+        } else {
+            $this->setExpectedException('PHPUnit_Framework_Error_Warning', 'Test that PHPUnit\'s error handler fires.');
+        }
+
+        trigger_error('Test that PHPUnit\'s error handler fires.', E_USER_WARNING);
     }
 }

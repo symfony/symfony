0fb4956c7aa17807a9602836474d48aeaf330be3
Kévin Dunglas <dunglas@gmail.com>
2017-10-20 18:23:04 +0200

    Better fix

--- src/Symfony/Component/Routing/RouteCollectionBuilder.php
+++ src/Symfony/Component/Routing/RouteCollectionBuilder.php
@@ -311,14 +311,11 @@ class RouteCollectionBuilder
                 $subCollection->addPrefix($this->prefix);
 
                 $routeCollection->addCollection($subCollection);
-                foreach ($route->resources as $resource) {
-                    $routeCollection->addResource($resource);
-                }
             }
+        }
 
-            foreach ($this->resources as $resource) {
-                $routeCollection->addResource($resource);
-            }
+        foreach ($this->resources as $resource) {
+            $routeCollection->addResource($resource);
         }
 
         return $routeCollection;

5a075005d0aeb1c8223c098b0be4f9d6f07d05f2
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2017-10-20 09:16:22 +0200

    Remove useless Command deletion

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -17,7 +17,6 @@ use Symfony\Bundle\FrameworkBundle\Command\RouterDebugCommand;
 use Symfony\Bundle\FrameworkBundle\Command\RouterMatchCommand;
 use Symfony\Bundle\FrameworkBundle\Command\TranslationDebugCommand;
 use Symfony\Bundle\FrameworkBundle\Command\TranslationUpdateCommand;
-use Symfony\Bundle\FrameworkBundle\Command\WorkflowDumpCommand;
 use Symfony\Bundle\FrameworkBundle\Command\XliffLintCommand;
 use Symfony\Bundle\FrameworkBundle\Command\YamlLintCommand;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -624,10 +623,6 @@ class FrameworkExtension extends Extension
     private function registerWorkflowConfiguration(array $config, ContainerBuilder $container, XmlFileLoader $loader)
     {
         if (!$config['enabled']) {
-            if (!class_exists(Workflow\Workflow::class)) {
-                $container->removeDefinition(WorkflowDumpCommand::class);
-            }
-
             return;
         }
 

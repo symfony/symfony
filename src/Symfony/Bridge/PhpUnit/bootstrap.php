6523039193c35176388371ac361ffc8c5c627dcd
Alex Pott <alex.a.pott@googlemail.com>
2017-10-24 20:55:53 +0100

    Coding standards

--- src/Symfony/Bridge/PhpUnit/bootstrap.php
+++ src/Symfony/Bridge/PhpUnit/bootstrap.php
@@ -15,6 +15,7 @@ use Symfony\Bridge\PhpUnit\DeprecationErrorHandler;
 // Detect if we need to serialize deprecations.
 if ($ser = getenv('SYMFONY_DEPRECATIONS_SERIALIZE')) {
     DeprecationErrorHandler::collectDeprecations($ser);
+
     return;
 }
 

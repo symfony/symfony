18596507bbdda421fc9133c64ee2de3e190ae5b7
pkowalczyk <pkowalczyk90@gmail.com>
2017-11-12 23:24:17 +0100

    [DependencyInjection] Added support for variadics in named arguments

--- src/Symfony/Component/DependencyInjection/Compiler/ResolveNamedArgumentsPass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/ResolveNamedArgumentsPass.php
@@ -84,7 +84,10 @@ class ResolveNamedArgumentsPass extends AbstractRecursivePass
 
             $lastResolvedArgument = \end($resolvedArguments);
 
-            if (\is_array($lastResolvedArgument) && !empty($lastResolvedArgument)) {
+            if (\is_array($lastResolvedArgument) &&
+                !empty($lastResolvedArgument) &&
+                method_exists('ReflectionParameter', 'isVariadic')
+            ) {
                 try {
                     $reflection = $this->getReflectionMethod($value, $method);
                     $parameters = $reflection->getParameters();

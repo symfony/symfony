aa3994489e12831435d995ac629de08a7a4ef9d3
Gary PEGEOT <g.pegeot@highco-data.fr>
2017-11-15 12:11:01 +0100

    [FrameworkBundle] [DI] Automatically add "setLogger" method call.

---
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/LoggerAwarePass.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\FrameworkBundle\DependencyInjection\Compiler;
+
+use Psr\Log\LoggerAwareInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Automatically add setLogger method call to any service that implements Psr\Log\LoggerAwareInterface.
+ *
+ * @see http://www.php-fig.org/psr/psr-3/
+ *
+ * @author Gary PEGEOT <garypegeot@gmail.com>
+ */
+class LoggerAwarePass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('logger') && !$container->hasAlias('logger')) {
+            return;
+        }
+
+        $reference = new Reference('logger');
+
+        foreach ($container->getDefinitions() as $definition) {
+            if (!class_exists($definition->getClass())) {
+                continue;
+            }
+
+            $refl = new \ReflectionClass($definition->getClass());
+            if ($refl->implementsInterface(LoggerAwareInterface::class) && !$definition->hasMethodCall('setLogger')) {
+                $definition->addMethodCall('setLogger', [$reference]);
+            }
+        }
+    }
+}
---
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Compiler/LoggerAwarePassTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\FrameworkBundle\Tests\DependencyInjection\Compiler;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\DependencyInjection\Compiler\LoggerAwarePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Unit tests for App\DependencyInjection\Compiler\LoggerAwarePass
+ *
+ * @author Gary PEGEOT <g.pegeot@highco-data.fr>
+ */
+class LoggerAwarePassTest extends TestCase
+{
+    /**
+     * @covers \Symfony\Bundle\FrameworkBundle\DependencyInjection\Compiler\LoggerAwarePass::process()
+     */
+    public function testProcess()
+    {
+        $container = new ContainerBuilder();
+        $container->register('logger', LoggerInterface::class);
+
+        $definition = $container->register('foo', get_class($this->createMock(LoggerAwareInterface::class)));
+        $container->register('bar', 'stdClass');
+        $this->assertFalse(
+            $definition->hasMethodCall('setLogger'),
+            'Service should not have "setLogger" method call before Pass execution.'
+        );
+
+        (new LoggerAwarePass())->process($container);
+
+        $this->assertTrue($definition->hasMethodCall('setLogger'), 'Service should have "setLogger" method call.');
+        $this->assertFalse(
+            $container->findDefinition('bar')->hasMethodCall('setLogger'),
+            '"bar" service should not be affected'
+        );
+    }
+}

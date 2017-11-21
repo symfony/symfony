737c997d8bfee2195f3f03dbdd67aa812e8b06ce
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-11-21 21:34:39 +0100

    Let user configure the attribute bag and flash bag parameters of the session object.

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -638,6 +638,14 @@ class FrameworkExtension extends Extension
 
         $container->setParameter('session.storage.options', $options);
 
+        if (null !== $config['attribute_bag']) {
+            $container->getDefinition('session')->replaceArgument(1, new Reference($config['attribute_bag']));
+        }
+
+        if (null !== $config['flash_bag']) {
+            $container->getDefinition('session')->replaceArgument(2, new Reference($config['flash_bag']));
+        }
+
         // session handler (the internal callback registered with PHP session management)
         if (null === $config['handler_id']) {
             // Set the handler class to be null

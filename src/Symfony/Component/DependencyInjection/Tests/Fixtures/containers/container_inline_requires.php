a80aff34270ecc60d36d6c50dddc5fc94c321742
Kévin Dunglas <dunglas@gmail.com>
2017-11-26 19:54:08 +0100

    Set public

--- src/Symfony/Component/DependencyInjection/Tests/Fixtures/containers/container_inline_requires.php
+++ src/Symfony/Component/DependencyInjection/Tests/Fixtures/containers/container_inline_requires.php
@@ -14,6 +14,6 @@ $container = new ContainerBuilder();
 $container->register(HotPath\C1::class)->addTag('container.hot_path')->setPublic(true);
 $container->register(HotPath\C2::class)->addArgument(new Reference(HotPath\C3::class))->setPublic(true);
 $container->register(HotPath\C3::class);
-$container->register(ParentNotExists::class);
+$container->register(ParentNotExists::class)->setPublic(true);
 
 return $container;

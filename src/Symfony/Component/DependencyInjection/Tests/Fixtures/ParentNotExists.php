1ea72c68aa483c08889ab54568908a714c377c80
Kévin Dunglas <dunglas@gmail.com>
2017-11-26 16:05:37 +0100

    Remove license + docblock on fixtures

--- src/Symfony/Component/DependencyInjection/Tests/Fixtures/ParentNotExists.php
+++ src/Symfony/Component/DependencyInjection/Tests/Fixtures/ParentNotExists.php
@@ -1,19 +1,7 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Symfony\Component\DependencyInjection\Tests\Fixtures;
 
-/**
- * @author Kévin Dunglas <dunglas@gmail.com>
- */
 class ParentNotExists extends \NotExists
 {
 }

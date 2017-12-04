2c3344b8b72f0202d671c0abeccb73669f1539a8
Fabien Potencier <fabien.potencier@gmail.com>
2017-12-04 13:53:49 -0800

    bumped Symfony version to 2.7.40

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -58,12 +58,12 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION = '2.7.39';
-    const VERSION_ID = 20739;
+    const VERSION = '2.7.40-DEV';
+    const VERSION_ID = 20740;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 7;
-    const RELEASE_VERSION = 39;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 40;
+    const EXTRA_VERSION = 'DEV';
 
     const END_OF_MAINTENANCE = '05/2018';
     const END_OF_LIFE = '05/2019';

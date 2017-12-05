5f5d021fff0b3a66254daba695d63f0925b00c52
Matthieu <Matthieu.cneude@gmail.com>
2017-12-05 13:50:29 +0100

    [DI] Fix when adding to the container paramaters environment variables and then an array

--- src/Symfony/Component/DependencyInjection/ContainerBuilder.php
+++ src/Symfony/Component/DependencyInjection/ContainerBuilder.php
@@ -1294,6 +1294,11 @@ class ContainerBuilder extends Container implements TaggedContainerInterface
             $format = '%%env(%s)%%';
         }
 
+        $bag = $this->getParameterBag();
+        if (true === $format) {
+            $value = $bag->resolveValue($value);
+        }
+
         if (is_array($value)) {
             $result = array();
             foreach ($value as $k => $v) {
@@ -1305,11 +1310,6 @@ class ContainerBuilder extends Container implements TaggedContainerInterface
 
         if (!is_string($value)) {
             return $value;
-        }
-
-        $bag = $this->getParameterBag();
-        if (true === $format) {
-            $value = $bag->resolveValue($value);
         }
         $envPlaceholders = $bag instanceof EnvPlaceholderParameterBag ? $bag->getEnvPlaceholders() : $this->envPlaceholders;
 

8bc2fbb934fc1dff897acfc206aaf10835cbfb68
Titouan Galopin <galopintitouan@gmail.com>
2017-12-08 14:17:14 +0100

    Remove some unused variables and properties

--- src/Symfony/Component/Serializer/Encoder/JsonDecode.php
+++ src/Symfony/Component/Serializer/Encoder/JsonDecode.php
@@ -24,7 +24,6 @@ class JsonDecode implements DecoderInterface
 
     private $associative;
     private $recursionDepth;
-    private $lastError = JSON_ERROR_NONE;
 
     /**
      * Constructs a new JsonDecode instance.
@@ -75,7 +74,7 @@ class JsonDecode implements DecoderInterface
 
         $decodedData = json_decode($data, $associative, $recursionDepth, $options);
 
-        if (JSON_ERROR_NONE !== $this->lastError = json_last_error()) {
+        if (JSON_ERROR_NONE !== json_last_error()) {
             throw new UnexpectedValueException(json_last_error_msg());
         }
 

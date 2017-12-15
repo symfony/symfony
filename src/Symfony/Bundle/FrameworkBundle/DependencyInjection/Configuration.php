903f120816374ebb11edac924610c922b8d0a27d
Fabien Potencier <fabien.potencier@gmail.com>
2017-12-14 17:44:04 -0800

    Revert "bug #25151 [FrameworkBundle] Automatically enable the CSRF protection if CSRF manager exists (sroze)"
    This reverts commit d5f04284f9aacf3eed825c69eca7426b0f333487, reversing
    changes made to e52825e2533af77361ed7a424fb0952e9161ff2d.

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Configuration.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Configuration.php
@@ -21,7 +21,6 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Lock\Lock;
 use Symfony\Component\Lock\Store\SemaphoreStore;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Translation\Translator;
 use Symfony\Component\Validator\Validation;
@@ -143,7 +142,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
                 ->arrayNode('csrf_protection')
-                    ->{!class_exists(FullStack::class) && class_exists(CsrfTokenManagerInterface::class) ? 'canBeDisabled' : 'canBeEnabled'}()
+                    ->canBeEnabled()
                 ->end()
             ->end()
         ;

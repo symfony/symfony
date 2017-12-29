61fc5f767186cc486252046e9bf056c6d24b853f
Robin Chalas <robin.chalas@elao.com>
2017-12-29 16:39:13 +0100

    remove flex-specific suggestion on 3.4

--- src/Symfony/Bundle/FrameworkBundle/Controller/ControllerTrait.php
+++ src/Symfony/Bundle/FrameworkBundle/Controller/ControllerTrait.php
@@ -366,14 +366,14 @@ trait ControllerTrait
      *
      * @return AccessDeniedException
      *
-     * @throws \LogicException If SecurityBundle is not available
+     * @throws \LogicException If the Security component is not available
      *
      * @final since version 3.4
      */
     protected function createAccessDeniedException($message = 'Access Denied.', \Exception $previous = null)
     {
         if (!class_exists(AccessDeniedException::class)) {
-            throw new \LogicException('You can not use the "createAccessDeniedException" method if the SecurityBundle is not registered in your application. Try running "composer require security-core".');
+            throw new \LogicException('You can not use the "createAccessDeniedException" method if the Security component is not available.');
         }
 
         return new AccessDeniedException($message, $previous);

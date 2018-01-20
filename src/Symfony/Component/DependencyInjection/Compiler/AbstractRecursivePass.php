ad8aff4203fe060ae5b736b9481fb57f343fc90e
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-01-20 09:34:19 +0100

    [DI] compilation perf tweak

--- src/Symfony/Component/DependencyInjection/Compiler/AbstractRecursivePass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/AbstractRecursivePass.php
@@ -49,7 +49,7 @@ abstract class AbstractRecursivePass implements CompilerPassInterface
      */
     protected function processValue($value, $isRoot = false)
     {
-        if (is_array($value)) {
+        if (\is_array($value)) {
             foreach ($value as $k => $v) {
                 if ($isRoot) {
                     $this->currentId = $k;

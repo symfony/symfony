73592eed8fa46dd2b7cbbc8cd856d5ff7943ddc4
Thomas Decaux <ebuildy@gmail.com>
2018-01-22 13:30:00 +0100

    fix code style

--- src/Symfony/Component/Translation/Writer/TranslationWriterInterface.php
+++ src/Symfony/Component/Translation/Writer/TranslationWriterInterface.php
@@ -31,12 +31,12 @@ interface TranslationWriterInterface
      * @throws InvalidArgumentException
      */
     public function write(MessageCatalogue $catalogue, $format, $options = array());
-    
+
     /**
      * Disables dumper backup.
      */
     public function disableBackup();
-    
+
     /**
      * Obtains the list of supported formats.
      *

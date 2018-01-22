e0ecd175264a74f3c982a55b23548c41af31be62
Benjamin Lazarecki <benjamin.lazarecki@gmail.com>
2018-01-22 16:34:27 +0100

    [Validator] Propagate embedded groups for collection validator

--- src/Symfony/Component/Validator/Tests/LegacyValidatorTest.php
+++ src/Symfony/Component/Validator/Tests/LegacyValidatorTest.php
@@ -12,6 +12,9 @@
 namespace Symfony\Component\Validator\Tests;
 
 use Symfony\Component\Translation\IdentityTranslator;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\Constraints\Range;
 use Symfony\Component\Validator\Constraints\Valid;
 use Symfony\Component\Validator\ConstraintValidatorFactory;
 use Symfony\Component\Validator\MetadataFactoryInterface;
@@ -38,5 +41,49 @@ class LegacyValidatorTest extends AbstractLegacyApiTest
     public function testValidateValueRejectsValid()
     {
         $this->validator->validateValue(new Entity(), new Valid());
+    }
+
+    public function testValidateMultipleGroupsForCollectionConstraint()
+    {
+        $entity = new Entity();
+        $entity->firstName = array('baz' => 2);
+
+        $this->metadata->addPropertyConstraint('firstName', new Collection(
+            array(
+                'fields' => array(
+                    'baz' => array(
+                        new Range(array('min' => 3, 'minMessage' => 'Group foo', 'groups' => 'foo')),
+                        new Range(array('min' => 5, 'minMessage' => 'Group bar', 'groups' => 'bar')),
+                    ),
+                ),
+            )
+        ));
+
+        $violations = $this->validate($entity, null, array('foo', 'bar'));
+
+        $this->assertCount(2, $violations);
+        $this->assertSame('Group foo', $violations[0]->getMessage());
+        $this->assertSame('Group bar', $violations[1]->getMessage());
+    }
+
+    public function testValidateMultipleGroupsForAllConstraint()
+    {
+        $entity = new Entity();
+        $entity->firstName = array(1);
+
+        $this->metadata->addPropertyConstraint('firstName', new All(
+            array(
+                'constraints' => array(
+                    new Range(array('min' => 3, 'minMessage' => 'Group foo', 'groups' => 'foo')),
+                    new Range(array('min' => 5, 'minMessage' => 'Group bar', 'groups' => 'bar')),
+                ),
+            )
+        ));
+
+        $violations = $this->validate($entity, null, array('foo', 'bar'));
+
+        $this->assertCount(2, $violations);
+        $this->assertSame('Group foo', $violations[0]->getMessage());
+        $this->assertSame('Group bar', $violations[1]->getMessage());
     }
 }

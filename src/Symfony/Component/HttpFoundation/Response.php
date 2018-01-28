6904a25f180da6a8efade29ee6c68441b9f7d7cb
XieBiao <xiebiao@medlinker.com>
2018-01-29 00:32:57 +0800

    support set charset for content-type: application/json

--- src/Symfony/Component/HttpFoundation/Response.php
+++ src/Symfony/Component/HttpFoundation/Response.php
@@ -273,10 +273,12 @@ class Response
             // Fix Content-Type
             $charset = $this->charset ?: 'UTF-8';
             if (!$headers->has('Content-Type')) {
-                $headers->set('Content-Type', 'text/html; charset='.$charset);
-            } elseif (0 === stripos($headers->get('Content-Type'), 'text/') && false === stripos($headers->get('Content-Type'), 'charset')) {
-                // add the charset
-                $headers->set('Content-Type', $headers->get('Content-Type').'; charset='.$charset);
+                $headers->set('Content-Type', 'text/html; charset=' . $charset);
+            } elseif (false === stripos($headers->get('Content-Type'), 'charset')) {
+                if (0 === stripos($headers->get('Content-Type'), 'text/') || 0 === stripos($headers->get('Content-Type'), 'application/json')) {
+                    // add the charset
+                    $headers->set('Content-Type', $headers->get('Content-Type') . '; charset=' . $charset);
+                }
             }
 
             // Fix Content-Length

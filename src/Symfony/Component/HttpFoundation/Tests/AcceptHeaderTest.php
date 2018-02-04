634157ed0de38a2de45847a52e3e3119e5c3e87d
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-02-04 17:49:11 +0100

    Added support for image/* kind of default values

--- src/Symfony/Component/HttpFoundation/Tests/AcceptHeaderTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/AcceptHeaderTest.php
@@ -119,6 +119,10 @@ class AcceptHeaderTest extends TestCase
         yield array('text/plain;q=0.5, text/html, text/x-dvi;q=0.8, */*;q=0.3', '*', 0.3);
         yield array('text/plain;q=0.5, text/html, text/x-dvi;q=0.8, */*', '*', 1.0);
         yield array('text/plain;q=0.5, text/html, text/x-dvi;q=0.8, */*', 'text/xml', 1.0);
+        yield array('text/plain;q=0.5, text/html, text/x-dvi;q=0.8, */*', 'text/*', 1.0);
+        yield array('text/plain;q=0.5, text/html, text/*;q=0.8, */*', 'text/*', 0.8);
+        yield array('text/plain;q=0.5, text/html, text/*;q=0.8, */*', 'text/html', 1.0);
+        yield array('text/plain;q=0.5, text/html, text/*;q=0.8, */*', 'text/x-dvi', 0.8);
         yield array('*;q=0.3, ISO-8859-1;q=0.7, utf-8;q=0.7', '*', 0.3);
         yield array('*;q=0.3, ISO-8859-1;q=0.7, utf-8;q=0.7', 'utf-8', 0.7);
         yield array('*;q=0.3, ISO-8859-1;q=0.7, utf-8;q=0.7', 'SHIFT_JIS', 0.3);

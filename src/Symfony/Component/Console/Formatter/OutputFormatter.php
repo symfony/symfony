a04144250b473d8b5dccc535b732bddd2b001f3c
Michael Gauthier <mike@silverorange.com>
2018-02-08 17:09:20 -0400

    Improve compatibility of output formatter
    preg_match with offset capture returns string offsets in bytes. If
    mbstring function overloading is enabled this means subsequent
    calculations on characters will not line up.
    
    In practice this causes parts of the formatting tags to appear inline in
    the formatted string.
    
    This updates the code to use mb_string function with encoding explicitly
    set to ASCII. This works for servers where function overloading is
    enabled and servers where it is disabled.

--- src/Symfony/Component/Console/Formatter/OutputFormatter.php
+++ src/Symfony/Component/Console/Formatter/OutputFormatter.php
@@ -134,21 +134,23 @@ class OutputFormatter implements OutputFormatterInterface
         $offset = 0;
         $output = '';
         $tagRegex = '[a-z][a-z0-9,_=;-]*+';
+        // Note: Offset capture is in bytes. For compatibility, explicitly use
+        // mbstring with ASCII encoding for string manipulations.
         preg_match_all("#<(($tagRegex) | /($tagRegex)?)>#ix", $message, $matches, PREG_OFFSET_CAPTURE);
         foreach ($matches[0] as $i => $match) {
             $pos = $match[1];
             $text = $match[0];
 
-            if (0 != $pos && '\\' == $message[$pos - 1]) {
+            if (0 != $pos && '\\' == mb_substr($message, $pos - 1, 1, 'ASCII')) {
                 continue;
             }
 
             // add the text up to the next tag
-            $output .= $this->applyCurrentStyle(substr($message, $offset, $pos - $offset));
+            $output .= $this->applyCurrentStyle(mb_substr($message, $offset, $pos - $offset, 'ASCII'));
             $offset = $pos + strlen($text);
 
             // opening tag?
-            if ($open = '/' != $text[1]) {
+            if ($open = '/' != mb_substr($text, 1, 1, 'ASCII')) {
                 $tag = $matches[1][$i][0];
             } else {
                 $tag = isset($matches[3][$i][0]) ? $matches[3][$i][0] : '';
@@ -166,9 +168,9 @@ class OutputFormatter implements OutputFormatterInterface
             }
         }
 
-        $output .= $this->applyCurrentStyle(substr($message, $offset));
+        $output .= $this->applyCurrentStyle(mb_substr($message, $offset, null, 'ASCII'));
 
-        if (false !== strpos($output, "\0")) {
+        if (false !== mb_strpos($output, "\0", 0, 'ASCII')) {
             return strtr($output, array("\0" => '\\', '\\<' => '<'));
         }
 

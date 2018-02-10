79c74e5636abda4651c194c3f7cff67a54de981e
Matthieu Napoli <matthieu@mnapoli.fr>
2018-02-10 18:16:30 +0100

    Document explicitly that dotfiles and vcs files are ignored by default
    This change makes it clear whether or not each of those two options are enabled by default.
    
    Without this documentation one has to look into the code.

--- src/Symfony/Component/Finder/Finder.php
+++ src/Symfony/Component/Finder/Finder.php
@@ -313,6 +313,8 @@ class Finder implements \IteratorAggregate, \Countable
     /**
      * Excludes "hidden" directories and files (starting with a dot).
      *
+     * This option is enabled by default.
+     *
      * @param bool $ignoreDotFiles Whether to exclude "hidden" files or not
      *
      * @return $this
@@ -332,6 +334,8 @@ class Finder implements \IteratorAggregate, \Countable
 
     /**
      * Forces the finder to ignore version control directories.
+     *
+     * This option is enabled by default.
      *
      * @param bool $ignoreVCS Whether to exclude VCS files or not
      *

9ba64165454acbd0b644331b262e731efbf8fc14
Jakub Kisielewski <sejntcik@gmail.com>
2018-02-11 15:45:34 +0100

    fix coding standards

--- src/Symfony/Component/Serializer/Normalizer/AbstractNormalizer.php
+++ src/Symfony/Component/Serializer/Normalizer/AbstractNormalizer.php
@@ -351,10 +351,10 @@ abstract class AbstractNormalizer implements NormalizerInterface, DenormalizerIn
                         }
                     } catch (\ReflectionException $e) {
                         throw new RuntimeException(sprintf('Could not determine the class of the parameter "%s".', $key), 0, $e);
-                    } catch(MissingConstructorArgumentsException $e) {
-                        if(!$constructorParameter->getType()->allowsNull())
+                    } catch (MissingConstructorArgumentsException $e) {
+                        if(!$constructorParameter->getType()->allowsNull()) {
                             throw $e;
-
+                        }
                         $parameterData = null;
                     }
 

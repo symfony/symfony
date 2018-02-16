1a427b181dec0ffe920b604d5b42f23d83495c12
Andreas <iquito@gmx.net>
2018-02-16 06:32:23 +0100

    Fix false-positive deprecation notices for TranslationLoader and WriteCheckSessionHandler

--- src/Symfony/Bundle/FrameworkBundle/Translation/TranslationLoader.php
+++ src/Symfony/Bundle/FrameworkBundle/Translation/TranslationLoader.php
@@ -14,13 +14,16 @@ namespace Symfony\Bundle\FrameworkBundle\Translation;
 use Symfony\Component\Translation\Reader\TranslationReader;
 use Symfony\Component\Translation\MessageCatalogue;
 
-@trigger_error(sprintf('The class "%s" is deprecated since Symfony 3.4 and will be removed in 4.0. Use "%s" instead. ', TranslationLoader::class, TranslationReader::class), E_USER_DEPRECATED);
-
 /**
  * @deprecated since version 3.4 and will be removed in 4.0. Use Symfony\Component\Translation\Reader\TranslationReader instead
  */
 class TranslationLoader extends TranslationReader
 {
+    public function __construct()
+    {
+        @trigger_error(sprintf('The class "%s" is deprecated since Symfony 3.4 and will be removed in 4.0. Use "%s" instead. ', self::class, TranslationReader::class), E_USER_DEPRECATED);
+    }
+
     /**
      * Loads translation messages from a directory to the catalogue.
      *

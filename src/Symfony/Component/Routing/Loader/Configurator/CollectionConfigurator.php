239f2e21e554feb0a02c615b6da48272879d844a
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-02-26 14:41:42 +0100

    [Routing] Fix GC control of PHP-DSL

--- src/Symfony/Component/Routing/Loader/Configurator/CollectionConfigurator.php
+++ src/Symfony/Component/Routing/Loader/Configurator/CollectionConfigurator.php
@@ -23,13 +23,15 @@ class CollectionConfigurator
     use Traits\RouteTrait;
 
     private $parent;
+    private $parentConfigurator;
 
-    public function __construct(RouteCollection $parent, $name)
+    public function __construct(RouteCollection $parent, $name, self $parentConfigurator = null)
     {
         $this->parent = $parent;
         $this->name = $name;
         $this->collection = new RouteCollection();
         $this->route = new Route('');
+        $this->parentConfigurator = $parentConfigurator; // for GC control
     }
 
     public function __destruct()
@@ -50,7 +52,7 @@ class CollectionConfigurator
     {
         $this->collection->add($this->name.$name, $route = clone $this->route);
 
-        return new RouteConfigurator($this->collection, $route->setPath($path), $this->name);
+        return new RouteConfigurator($this->collection, $route->setPath($path), $this->name, $this);
     }
 
     /**
@@ -60,7 +62,7 @@ class CollectionConfigurator
      */
     final public function collection($name = '')
     {
-        return new self($this->collection, $this->name.$name);
+        return new self($this->collection, $this->name.$name, $this);
     }
 
     /**

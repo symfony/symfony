98065d38b587fb389db9006ca4ca48f7d002470c
Mathieu Piot <mathieu.piot@inra.fr>
2018-03-01 10:19:34 +0100

    fabpot.io fix

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_help.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_help.html.php
@@ -1,3 +1,14 @@
-<?php if (!empty($help)): ?>
-    <p id="<?php echo $view->escape($id) ?>_help" class="help-text"><?php echo $view->escape(false !== $translation_domain ? $view['translator']->trans($help, array(), $translation_domain) : $help) ?></p>
-<?php endif ?>
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!empty($help)): ?>
+    <p id="<?php echo $view->escape($id); ?>_help" class="help-text"><?php echo $view->escape(false !== $translation_domain ? $view['translator']->trans($help, array(), $translation_domain) : $help); ?></p>
+<?php endif; ?>
--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_row.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_row.html.php
@@ -1,6 +1,6 @@
 <div>
-    <?php echo $view['form']->label($form) ?>
-    <?php echo $view['form']->errors($form) ?>
-    <?php echo $view['form']->widget($form, array('helpBlockDisplayed' => true)) ?>
-    <?php echo $view['form']->help($form) ?>
+    <?php echo $view['form']->label($form); ?>
+    <?php echo $view['form']->errors($form); ?>
+    <?php echo $view['form']->widget($form, array('helpBlockDisplayed' => true)); ?>
+    <?php echo $view['form']->help($form); ?>
 </div>

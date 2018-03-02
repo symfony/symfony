c5bd0bdacb0fe1c1adbb004a79e6e9678dcc478e
Boris Vujicic <boris.vujicic@boutsourcing.com>
2018-03-02 09:14:52 +0100

    [Security] added userChecker to SimpleAuthenticationProvider
    Added UserChecker to SimpleAuthenticationProvider and updated SimpleFormFactory

--- src/Symfony/Component/Security/Core/Authentication/Provider/SimpleAuthenticationProvider.php
+++ src/Symfony/Component/Security/Core/Authentication/Provider/SimpleAuthenticationProvider.php
@@ -11,6 +11,8 @@
 
 namespace Symfony\Component\Security\Core\Authentication\Provider;
 
+use Symfony\Component\Security\Core\User\UserChecker;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\SimpleAuthenticatorInterface;
@@ -24,17 +26,22 @@ class SimpleAuthenticationProvider implements AuthenticationProviderInterface
     private $simpleAuthenticator;
     private $userProvider;
     private $providerKey;
+    private $userChecker;
 
-    public function __construct(SimpleAuthenticatorInterface $simpleAuthenticator, UserProviderInterface $userProvider, $providerKey)
+    public function __construct(SimpleAuthenticatorInterface $simpleAuthenticator, UserProviderInterface $userProvider, $providerKey, UserCheckerInterface $userChecker = null)
     {
         $this->simpleAuthenticator = $simpleAuthenticator;
         $this->userProvider = $userProvider;
         $this->providerKey = $providerKey;
+        $this->userChecker = $userChecker ?: new UserChecker();
     }
 
     public function authenticate(TokenInterface $token)
     {
+        $user = $this->userProvider->loadUserByUsername($token->getUsername());
+        $this->userChecker->checkPreAuth($user);
         $authToken = $this->simpleAuthenticator->authenticateToken($token, $this->userProvider, $this->providerKey);
+        $this->userChecker->checkPostAuth($user);
 
         if ($authToken instanceof TokenInterface) {
             return $authToken;

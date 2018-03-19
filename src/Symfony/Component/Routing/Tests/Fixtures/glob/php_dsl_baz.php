5f5f3aad8acb09e6f4b74487ec50d4bf4d647254
Pascal Luna <skalpa@zetareticuli.org>
2018-03-19 17:28:53 +0000

    Fixed CS

--- src/Symfony/Component/Routing/Tests/Fixtures/glob/php_dsl_baz.php
+++ src/Symfony/Component/Routing/Tests/Fixtures/glob/php_dsl_baz.php
@@ -6,7 +6,7 @@ return function (RoutingConfigurator $routes) {
     $collection = $routes->collection();
 
     $collection->add('baz_route', '/baz')
-        ->defaults(['_controller' => 'AppBundle:Baz:view']);
+        ->defaults(array('_controller' => 'AppBundle:Baz:view'));
 
     return $collection;
 };

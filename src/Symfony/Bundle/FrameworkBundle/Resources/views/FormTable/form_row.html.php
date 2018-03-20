075fcfd07cae26a0a6aa1e4447296ad7a515eb5c
Mathieu Piot <math.piot@gmail.com>
2018-03-20 17:57:01 +0100

    [FrameworkBundle] Add widgetAtt to formTable/form_row

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/FormTable/form_row.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/FormTable/form_row.html.php
@@ -1,10 +1,11 @@
 <tr>
+    <?php $widgetAtt = empty($help) ? array() : array('attr' => array('aria-describedby' => $id.'_help')); ?>
     <td>
         <?php echo $view['form']->label($form); ?>
     </td>
     <td>
         <?php echo $view['form']->errors($form); ?>
-        <?php echo $view['form']->widget($form); ?>
+        <?php echo $view['form']->widget($form, $widgetAtt); ?>
         <?php echo $view['form']->help($form); ?>
     </td>
 </tr>

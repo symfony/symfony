e62a0c97ecd499a879c5ad01bef49950aff2083d
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-03-23 09:36:55 +0100

    Add a middleware that wraps all handlers in one Doctrine transaction.

---
+++ src/Symfony/Component/Messenger/Middleware/WrapsMessageHandlingInTransaction.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Messenger\Middleware;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\MiddlewareInterface;
+
+/**
+ * Wraps all handlers in a single doctrine transaction.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class WrapsMessageHandlingInTransaction implements MiddlewareInterface
+{
+    /**
+     * @var ManagerRegistry
+     */
+    private $managerRegistry;
+
+    /**
+     * @var string
+     */
+    private $entityManagerName;
+
+    /**
+     * @param ManagerRegistry $managerRegistry
+     * @param string $entityManagerName
+     */
+    public function __construct(ManagerRegistry $managerRegistry, string $entityManagerName = null)
+    {
+        $this->managerRegistry = $managerRegistry;
+        $this->entityManagerName = $entityManagerName ?? 'default';
+    }
+
+    public function handle($message, callable $next)
+    {
+        /** @var $entityManager EntityManagerInterface */
+        $entityManager = $this->managerRegistry->getManager($this->entityManagerName);
+
+        try {
+            $entityManager->transactional(
+                function () use ($message, $next) {
+                    $next($message);
+                }
+            );
+        } catch (\Throwable $exception) {
+            $this->managerRegistry->resetManager($this->entityManagerName);
+
+            throw $exception;
+        }
+    }
+}

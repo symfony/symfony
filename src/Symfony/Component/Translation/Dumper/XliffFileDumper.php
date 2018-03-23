ca41fecfd98520a4d30a495d01d5e1cbc45a9352
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-03-23 19:39:12 +0100

    Make sure to trim source if it is too long

--- src/Symfony/Component/Translation/Dumper/XliffFileDumper.php
+++ src/Symfony/Component/Translation/Dumper/XliffFileDumper.php
@@ -146,7 +146,11 @@ class XliffFileDumper extends FileDumper
         foreach ($messages->all($domain) as $source => $target) {
             $translation = $dom->createElement('unit');
             $translation->setAttribute('id', strtr(substr(base64_encode(hash('sha256', $source, true)), 0, 7), '/+', '._'));
-            $translation->setAttribute('name', $source);
+            $name = $source;
+            if (strlen($source) > 80) {
+                $name = substr(md5($source), -7);
+            }
+            $translation->setAttribute('name', $name);
             $metadata = $messages->getMetadata($source, $domain);
 
             // Add notes section

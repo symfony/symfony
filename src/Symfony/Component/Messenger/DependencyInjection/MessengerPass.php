2bcf93db2d6975308e5cdd6ae9a5aaface12d439
Samuel ROZE <samuel.roze@gmail.com>
2018-03-23 09:57:11 +0000

    Remove the default transport if no serializer

--- src/Symfony/Component/Messenger/DependencyInjection/MessengerPass.php
+++ src/Symfony/Component/Messenger/DependencyInjection/MessengerPass.php
@@ -51,6 +51,12 @@ class MessengerPass implements CompilerPassInterface
             $container->removeDefinition('messenger.middleware.debug.logging');
         }
 
+        if (!$container->has('serializer')) {
+            $container->removeDefinition('messenger.transport.serialize_message_with_type_in_headers');
+            $container->removeAlias('messenger.transport.default_encoder');
+            $container->removeAlias('messenger.transport.default_decoder');
+        }
+
         $this->registerReceivers($container);
         $this->registerHandlers($container);
     }

a62962027d71f37d7f93d903e70c23781b9f2f91
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-03-23 15:50:46 +0100

    cs

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/messenger_doctrine.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/messenger_doctrine.php
@@ -4,6 +4,6 @@ $container->loadFromExtension('framework', array(
     'messenger' => array(
         'doctrine_transaction' => array(
             'entity_manager_name' => 'foobar',
-        )
+        ),
     ),
 ));

d723756331caffe12526a2edbcadca74a0e94a46
Mathieu Piot <math.piot@gmail.com>
2018-03-23 10:14:41 +0100

    Fix some mistakes

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_row.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_row.html.php
@@ -1,7 +1,7 @@
 <div>
-    <?php $widgetAtt = empty($help) ? array() : array('attr' => array('aria-describedby' => $id.'_help')); ?>
+    <?php $widgetAttr = empty($help) ? array() : array('attr' => array('aria-describedby' => $id.'_help')); ?>
     <?php echo $view['form']->label($form); ?>
     <?php echo $view['form']->errors($form); ?>
-    <?php echo $view['form']->widget($form, $widgetAtt); ?>
+    <?php echo $view['form']->widget($form, $widgetAttr); ?>
     <?php echo $view['form']->help($form); ?>
 </div>
--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/widget_attributes.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/widget_attributes.html.php
@@ -1,3 +1,3 @@
-id="<?php echo $view->escape($id); ?>" name="<?php echo $view->escape($full_name); ?>"<?php if ($disabled): ?> disabled="disabled"<?php endif; ?>
-<?php if ($required): ?> required="required"<?php endif; ?>
-<?php echo $attr ? ' '.$view['form']->block($form, 'attributes') : ''; ?>
+id="<?php echo $view->escape($id) ?>" name="<?php echo $view->escape($full_name) ?>"<?php if ($disabled): ?> disabled="disabled"<?php endif ?>
+<?php if ($required): ?> required="required"<?php endif ?>
+<?php echo $attr ? ' '.$view['form']->block($form, 'attributes') : '' ?>

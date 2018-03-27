1998523cb733c7ecb4808b1b674be61da54043e3
Tomas Votruba <tomas.vot@gmail.com>
2018-03-27 22:21:35 +0200

    typos

--- src/Symfony/Component/DependencyInjection/Loader/YamlFileLoader.php
+++ src/Symfony/Component/DependencyInjection/Loader/YamlFileLoader.php
@@ -809,7 +809,7 @@ class YamlFileLoader extends FileLoader
     /**
      * Merges configurations. Left has higher priority than right one.
      *
-     * @autor David Grudl (https://davidgrudl.com)
+     * @author David Grudl (https://davidgrudl.com)
      * @source https://github.com/nette/di/blob/8eb90721a131262f17663e50aee0032a62d0ef08/src/DI/Config/Helpers.php#L31
      *
      * @param mixed $left

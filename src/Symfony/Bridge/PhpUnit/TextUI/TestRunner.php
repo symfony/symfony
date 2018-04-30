b1fb985ebc7c6dbdf032af34a6f6d37fc4583adb
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-04-30 08:36:51 -0700

    [PhpUnitBridge] Fix

--- src/Symfony/Bridge/PhpUnit/TextUI/TestRunner.php
+++ src/Symfony/Bridge/PhpUnit/TextUI/TestRunner.php
@@ -13,7 +13,7 @@ namespace Symfony\Bridge\PhpUnit\TextUI;
 
 if (class_exists('PHPUnit_Runner_Version') && version_compare(\PHPUnit_Runner_Version::id(), '6.0.0', '<')) {
     class_alias('Symfony\Bridge\PhpUnit\Legacy\TestRunnerForV5', 'Symfony\Bridge\PhpUnit\TextUI\TestRunner');
-} elseif (version_compare(\PHPUnit_Runner_Version::id(), '7.0.0', '<')) {
+} elseif (version_compare(\PHPUnit\Runner\Version::id(), '7.0.0', '<')) {
     class_alias('Symfony\Bridge\PhpUnit\Legacy\TestRunnerForV6', 'Symfony\Bridge\PhpUnit\TextUI\TestRunner');
 } else {
     class_alias('Symfony\Bridge\PhpUnit\Legacy\TestRunnerForV7', 'Symfony\Bridge\PhpUnit\TextUI\TestRunner');

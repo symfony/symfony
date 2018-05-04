29b3e31382ee75d112eae8f8fefd09fa71e716ca
Robin Chalas <robin.chalas@elao.com>
2018-05-04 16:54:39 +0200

    [FrameworkBundle] Remove dead code

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -98,11 +98,6 @@ class FrameworkExtension extends Extension
     private $validatorConfigEnabled = false;
 
     /**
-     * @var string|null
-     */
-    private $kernelRootHash;
-
-    /**
      * Responds to the app.config configuration parameter.
      *
      * @throws LogicException
@@ -1389,20 +1384,6 @@ class FrameworkExtension extends Extension
                 $propertyAccessDefinition->setArguments(array(0, false));
             }
         }
-    }
-
-    /**
-     * Gets a hash of the kernel root directory.
-     *
-     * @return string
-     */
-    private function getKernelRootHash(ContainerBuilder $container)
-    {
-        if (!$this->kernelRootHash) {
-            $this->kernelRootHash = hash('sha256', $container->getParameter('kernel.root_dir'));
-        }
-
-        return $this->kernelRootHash;
     }
 
     /**

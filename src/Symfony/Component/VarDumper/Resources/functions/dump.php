addf4089a5f9546ddb8c78a242ed823e7d220ca9
Chi-teck <Chi-teck@users.noreply.github.com>
2018-06-12 12:53:09 +0500

    Added dumps function.

--- src/Symfony/Component/VarDumper/Resources/functions/dump.php
+++ src/Symfony/Component/VarDumper/Resources/functions/dump.php
@@ -9,6 +9,8 @@
  * file that was distributed with this source code.
  */
 
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\ServerDumper;
 use Symfony\Component\VarDumper\VarDumper;
 
 if (!function_exists('dump')) {
@@ -41,5 +43,29 @@ if (!function_exists('dd')) {
         }
 
         exit(1);
+    }
+}
+
+if (!function_exists('dumps')) {
+    function dumps($var, ...$moreVars)
+    {
+        $cloner = new VarCloner();
+        $dumper = new ServerDumper($_SERVER['VAR_DUMPER_SERVER'] ?? '127.0.0.1:9912');
+        $handler = function ($var) use ($cloner, $dumper) {
+            $dumper->dump($cloner->cloneVar($var));
+        };
+        VarDumper::setHandler($handler);
+
+        VarDumper::dump($var);
+
+        foreach ($moreVars as $var) {
+           VarDumper::dump($var);
+        }
+
+        if (1 < func_num_args()) {
+            return func_get_args();
+        }
+
+        return $var;
     }
 }

ba8b63b195a87a89a3a7dc3cdf777721824adeb9
Constantine Shtompel <constantine.shtompel@gmail.com>
2018-06-27 23:40:07 +0300

    [Cache] provider does not respect option maxIdLength with versioning enabled

--- src/Symfony/Component/Cache/Tests/Adapter/MaxIdLengthAdapterTest.php
+++ src/Symfony/Component/Cache/Tests/Adapter/MaxIdLengthAdapterTest.php
@@ -34,6 +34,34 @@ class MaxIdLengthAdapterTest extends TestCase
         $cache->hasItem(str_repeat('-', 39));
     }
 
+    public function testLongKeyVersioning()
+    {
+        $cache = $this->getMockBuilder(MaxIdLengthAdapter::class)
+            ->setConstructorArgs(array(str_repeat('-', 26)))
+            ->getMock();
+
+        $reflectionClass = new \ReflectionClass(AbstractAdapter::class);
+
+        $reflectionMethod = $reflectionClass->getMethod('getId');
+        $reflectionMethod->setAccessible(true);
+
+        // No versioning enabled
+        $this->assertEquals('--------------------------:------------', $reflectionMethod->invokeArgs($cache, array(str_repeat('-', 12))));
+        $this->assertLessThanOrEqual(50, strlen($reflectionMethod->invokeArgs($cache, array(str_repeat('-', 12)))));
+        $this->assertLessThanOrEqual(50, strlen($reflectionMethod->invokeArgs($cache, array(str_repeat('-', 23)))));
+        $this->assertLessThanOrEqual(50, strlen($reflectionMethod->invokeArgs($cache, array(str_repeat('-', 40)))));
+
+        $reflectionProperty = $reflectionClass->getProperty('versioningIsEnabled');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($cache, true);
+
+        // Versioning enabled
+        $this->assertEquals('--------------------------:1:------------', $reflectionMethod->invokeArgs($cache, array(str_repeat('-', 12))));
+        $this->assertLessThanOrEqual(50, strlen($reflectionMethod->invokeArgs($cache, array(str_repeat('-', 12)))));
+        $this->assertLessThanOrEqual(50, strlen($reflectionMethod->invokeArgs($cache, array(str_repeat('-', 23)))));
+        $this->assertLessThanOrEqual(50, strlen($reflectionMethod->invokeArgs($cache, array(str_repeat('-', 40)))));
+    }
+
     /**
      * @expectedException \Symfony\Component\Cache\Exception\InvalidArgumentException
      * @expectedExceptionMessage Namespace must be 26 chars max, 40 given ("----------------------------------------")

6c9cdf77a8d459c73455398bb2fe04c150db4066
Rudy Onfroy <ronfroy@macbook-pro-de-rudy.home>
2018-07-01 15:52:35 +0200

    add missing PHPDoc

--- src/Symfony/Component/HttpKernel/Bundle/BundleInterface.php
+++ src/Symfony/Component/HttpKernel/Bundle/BundleInterface.php
@@ -36,6 +36,8 @@ interface BundleInterface extends ContainerAwareInterface
      * Builds the bundle.
      *
      * It is only ever called once when the cache is empty.
+     *
+     * @param ContainerBuilder $container
      */
     public function build(ContainerBuilder $container);
 

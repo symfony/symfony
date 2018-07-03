ac5f5dce5668d83d3e87093e2a70936c60f18052
guillaumedebeauvoir <32831668+guillaumedebeauvoir@users.noreply.github.com>
2018-07-03 09:12:36 +0200

    Allow no auto caching by adding special header
    Add same fix as for version 4

--- src/Symfony/Component/HttpKernel/EventListener/AbstractSessionListener.php
+++ src/Symfony/Component/HttpKernel/EventListener/AbstractSessionListener.php
@@ -27,7 +27,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 abstract class AbstractSessionListener implements EventSubscriberInterface
 {
     private $sessionUsageStack = array();
-
+    const NO_AUTO_CACHE_CONTROL_HEADER = 'Symfony-Session-NoAutoCacheControl';
+    
     public function onKernelRequest(GetResponseEvent $event)
     {
         if (!$event->isMasterRequest()) {
@@ -55,10 +56,15 @@ abstract class AbstractSessionListener implements EventSubscriberInterface
         }
 
         if ($session instanceof Session ? $session->getUsageIndex() !== end($this->sessionUsageStack) : $session->isStarted()) {
-            $event->getResponse()
-                ->setPrivate()
-                ->setMaxAge(0)
-                ->headers->addCacheControlDirective('must-revalidate');
+            $response = $event->getResponse();
+            if (!$response->headers->has(self::NO_AUTO_CACHE_CONTROL_HEADER)) {
+                $event->getResponse()
+                    ->setPrivate()
+                    ->setMaxAge(0)
+                    ->headers->addCacheControlDirective('must-revalidate');
+            }else{
+                $response->headers->remove(self::NO_AUTO_CACHE_CONTROL_HEADER);
+            }
         }
     }
 

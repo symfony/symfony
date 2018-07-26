1ef1e3b8594ffd7ef05fa10b176cc4801cee9c46
Ivan Nikolaev <destillat.mailbox@gmail.com>
2018-07-26 13:42:48 +0300

    [CS] remove docblocks in guard expression class

--- src/Symfony/Component/Workflow/EventListener/GuardExpression.php
+++ src/Symfony/Component/Workflow/EventListener/GuardExpression.php
@@ -6,38 +6,20 @@ use Symfony\Component\Workflow\Transition;
 
 class GuardExpression
 {
-
-    /**
-     * @var Transition
-     */
     private $transition;
 
-    /**
-     * @var string
-     */
     private $expression;
 
-    /**
-     * @return Transition
-     */
     public function getTransition(): Transition
     {
         return $this->transition;
     }
 
-    /**
-     * @return string
-     */
     public function getExpression(): string
     {
         return $this->expression;
     }
 
-    /**
-     * GuardConfiguration constructor.
-     * @param Transition $transition
-     * @param string $expression
-     */
     public function __construct(Transition $transition, string $expression)
     {
         $this->transition = $transition;

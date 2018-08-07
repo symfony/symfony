a540aee35c8bb45037a6ad333e07b7d82f453d70
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-08-07 17:19:50 +0200

    [travis] ignore ordering when validating composer.lock files for deps=low

--- .github/rm-invalid-lowest-lock-files.php
+++ .github/rm-invalid-lowest-lock-files.php
@@ -3,7 +3,7 @@
 array_shift($_SERVER['argv']);
 $dirs = $_SERVER['argv'];
 
-function getContentHash($composerJson)
+function getRelevantContent(array $composerJson)
 {
     $relevantKeys = array(
         'name',
@@ -27,12 +27,18 @@ function getContentHash($composerJson)
         $relevantContent['config']['platform'] = $composerJson['config']['platform'];
     }
 
+    return $relevantContent;
+}
+
+function getContentHash(array $composerJson)
+{
+    $relevantContent = getRelevantContent($composerJson);
     ksort($relevantContent);
 
     return md5(json_encode($relevantContent));
 }
 
-$composerLocks = array();
+$composerJsons = array();
 
 foreach ($dirs as $dir) {
     if (!file_exists($dir.'/composer.lock') || !$composerLock = @json_decode(file_get_contents($dir.'/composer.lock'), true)) {
@@ -50,28 +56,29 @@ foreach ($dirs as $dir) {
         @unlink($dir.'/composer.lock');
         continue;
     }
-    $composerLocks[$composerJson['name']] = array($dir, $composerLock, $composerJson);
+    $composerJsons[$composerJson['name']] = array($dir, $composerLock['packages'], getRelevantContent($composerJson));
 }
 
-foreach ($composerLocks as list($dir, $composerLock)) {
-    foreach ($composerLock['packages'] as $composerJson) {
-        if (0 !== strpos($version = $composerJson['version'], 'dev-') && '-dev' !== substr($version, -4)) {
+foreach ($composerJsons as list($dir, $lockedPackages)) {
+    foreach ($lockedPackages as $lockedJson) {
+        if (0 !== strpos($version = $lockedJson['version'], 'dev-') && '-dev' !== substr($version, -4)) {
             continue;
         }
 
-        if (!isset($composerLocks[$name = $composerJson['name']])) {
+        if (!isset($composerJsons[$name = $lockedJson['name']])) {
             echo "$dir/composer.lock references missing $name.\n";
             @unlink($dir.'/composer.lock');
             continue 2;
         }
 
         foreach (array('minimum-stability', 'prefer-stable', 'repositories') as $key) {
-            if (array_key_exists($key, $composerLocks[$name][2])) {
-                $composerJson[$key] = $composerLocks[$name][2][$key];
+            if (array_key_exists($key, $composerJsons[$name][2])) {
+                $lockedJson[$key] = $composerJsons[$name][2][$key];
             }
         }
 
-        if (getContentHash($composerJson) !== $composerLocks[$name][1]['content-hash']) {
+        // use weak comparison to ignore ordering
+        if (getRelevantContent($lockedJson) != $composerJsons[$name][2]) {
             echo "$dir/composer.lock is not in sync with $name.\n";
             @unlink($dir.'/composer.lock');
             continue 2;

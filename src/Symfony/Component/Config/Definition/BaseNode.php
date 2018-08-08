6b413ab49d1984017870ac840c38435ea6cf1a8f
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-08-08 08:37:38 +0200

    [Config] minor fix that removes a PHP 7.3 deprecation

--- src/Symfony/Component/Config/Definition/BaseNode.php
+++ src/Symfony/Component/Config/Definition/BaseNode.php
@@ -499,7 +499,7 @@ abstract class BaseNode implements NodeInterface
                 return self::$placeholders[$value];
             }
 
-            if (0 === strpos($value, self::$placeholderUniquePrefix)) {
+            if (self::$placeholderUniquePrefix && 0 === strpos($value, self::$placeholderUniquePrefix)) {
                 return array();
             }
         }

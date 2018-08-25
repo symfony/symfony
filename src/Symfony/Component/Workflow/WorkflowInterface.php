b0e33110e2a331d79a876cc3446a575df00ad53b
ChangePlaces <ChangePlaces@users.noreply.github.com>
2018-08-25 23:43:54 +0100

    Update WorkflowInterface.php

--- src/Symfony/Component/Workflow/WorkflowInterface.php
+++ src/Symfony/Component/Workflow/WorkflowInterface.php
@@ -85,10 +85,11 @@ interface WorkflowInterface
     public function getMarkingStore();
 
     public function getMetadataStore(): MetadataStoreInterface;
-    
+
     /**
-     * Returns the optional Marking History Store for logging the state history on the entity
+     * Returns the optional Marking History Store for logging the state history on the entity.
+     *
      * @return null|MarkingHistoryStore
      */
-    public function getMarkingHistoryStore() : ?MarkingHistoryStore;
+    public function getMarkingHistoryStore(): ?MarkingHistoryStore;
 }

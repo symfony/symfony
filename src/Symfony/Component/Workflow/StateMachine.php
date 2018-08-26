74247a090d867d1a6aecabd964e8623ad03700a2
a <a@a>
2018-08-26 01:36:24 +0000

    adding marking history store to the workflow component

--- src/Symfony/Component/Workflow/StateMachine.php
+++ src/Symfony/Component/Workflow/StateMachine.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Component\Workflow;
 
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -11,8 +20,8 @@ use Symfony\Component\Workflow\MarkingStore\SingleStateMarkingStore;
  */
 class StateMachine extends Workflow
 {
-    public function __construct(Definition $definition, MarkingStoreInterface $markingStore = null, EventDispatcherInterface $dispatcher = null, string $name = 'unnamed')
+    public function __construct(Definition $definition, MarkingStoreInterface $markingStore = null, EventDispatcherInterface $dispatcher = null, string $name = 'unnamed', MarkingHistoryStore $markingHistoryStore = null)
     {
-        parent::__construct($definition, $markingStore ?: new SingleStateMarkingStore(), $dispatcher, $name);
+        parent::__construct($definition, $markingStore ?: new SingleStateMarkingStore(), $dispatcher, $name, $markingHistoryStore);
     }
 }

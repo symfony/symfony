c51dda0fe152adfa95249e9d801f96e4202ed53b
Robin Chalas <robin.chalas@elao.com>
2018-09-08 13:31:39 +0200

    [Console] Fix SymfonyQuestionHelper::askQuestion() with choice value as default

--- src/Symfony/Component/Console/Tests/Helper/SymfonyQuestionHelperTest.php
+++ src/Symfony/Component/Console/Tests/Helper/SymfonyQuestionHelperTest.php
@@ -75,6 +75,19 @@ class SymfonyQuestionHelperTest extends TestCase
         $this->assertOutputContains('What is your favorite superhero? [Superman, Batman]', $output);
     }
 
+    public function testAskChoiceWithChoiceValueAsDefault()
+    {
+        $questionHelper = new SymfonyQuestionHelper();
+        $helperSet = new HelperSet(array(new FormatterHelper()));
+        $questionHelper->setHelperSet($helperSet);
+        $questionHelper->setInputStream($this->getInputStream("Batman\n"));
+        $question = new ChoiceQuestion('What is your favorite superhero?', array('Superman', 'Batman', 'Spiderman'), 'Batman');
+        $question->setMaxAttempts(1);
+
+        $this->assertSame('Batman', $questionHelper->ask($this->createInputInterfaceMock(), $output = $this->createOutputInterface(), $question));
+        $this->assertOutputContains('What is your favorite superhero? [Batman]', $output);
+    }
+
     public function testAskReturnsNullIfValidatorAllowsIt()
     {
         $questionHelper = new SymfonyQuestionHelper();

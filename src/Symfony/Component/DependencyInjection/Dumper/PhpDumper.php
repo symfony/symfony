7cf1505a5b2118bd595684b547421903b320c1d2
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-09-19 09:18:46 +0200

    [DI] fix dumping lazy services

--- src/Symfony/Component/DependencyInjection/Dumper/PhpDumper.php
+++ src/Symfony/Component/DependencyInjection/Dumper/PhpDumper.php
@@ -154,7 +154,7 @@ class PhpDumper extends Dumper
             }
         }
 
-        (new AnalyzeServiceReferencesPass(false, !$this->getProxyDumper() instanceof NullDumper))->process($this->container);
+        (new AnalyzeServiceReferencesPass(false))->process($this->container);
         $this->circularReferences = array();
         $checkedNodes = array();
         foreach ($this->container->getCompiler()->getServiceReferenceGraph()->getNodes() as $id => $node) {

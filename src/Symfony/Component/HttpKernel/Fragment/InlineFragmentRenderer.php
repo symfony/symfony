41eb1914f6cd9feff78b180e1102b24d8b716ee5
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-09-23 16:32:19 +0200

    [HttpFoundation][Security] forward locale and format to subrequests

--- src/Symfony/Component/HttpKernel/Fragment/InlineFragmentRenderer.php
+++ src/Symfony/Component/HttpKernel/Fragment/InlineFragmentRenderer.php
@@ -122,6 +122,13 @@ class InlineFragmentRenderer extends RoutableFragmentRenderer
             $subRequest->setSession($session);
         }
 
+        if ($request->get('_format')) {
+            $subRequest->attributes->set('_format', $request->get('_format'));
+        }
+        if ($request->getDefaultLocale() !== $request->getLocale()) {
+            $subRequest->setLocale($request->getLocale());
+        }
+
         return $subRequest;
     }
 

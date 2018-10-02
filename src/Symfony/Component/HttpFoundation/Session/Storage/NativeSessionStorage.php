4c1f7c5877ce2a7261a9836cb659c7a6ceea031f
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-10-02 18:23:27 +0200

    [HttpFoundation] fix compat of tests with PHP 7.3

--- src/Symfony/Component/HttpFoundation/Session/Storage/NativeSessionStorage.php
+++ src/Symfony/Component/HttpFoundation/Session/Storage/NativeSessionStorage.php
@@ -153,7 +153,7 @@ class NativeSessionStorage implements SessionStorageInterface
         if (null !== $this->emulateSameSite) {
             $originalCookie = SessionUtils::popSessionCookie(session_name(), session_id());
             if (null !== $originalCookie) {
-                header(sprintf('%s; samesite=%s', $originalCookie, $this->emulateSameSite));
+                header(sprintf('%s; SameSite=%s', $originalCookie, $this->emulateSameSite));
             }
         }
 

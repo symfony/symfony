a854b0a01cedb5299c32e962ece55cf326de03e7
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-10-05 22:38:23 +0200

    [DI] fix dumping inline services again

--- src/Symfony/Component/DependencyInjection/Tests/Fixtures/php/services19.php
+++ src/Symfony/Component/DependencyInjection/Tests/Fixtures/php/services19.php
@@ -21,6 +21,8 @@ class ProjectServiceContainer extends Container
 
     public function __construct()
     {
+        $this->parameters = $this->getDefaultParameters();
+
         $this->services = array();
         $this->methodMap = array(
             'service_from_anonymous_factory' => 'getServiceFromAnonymousFactoryService',
@@ -58,11 +60,11 @@ class ProjectServiceContainer extends Container
     /**
      * Gets the public 'service_from_anonymous_factory' shared service.
      *
-     * @return \Bar\FooClass
+     * @return object A %env(FOO)% instance
      */
     protected function getServiceFromAnonymousFactoryService()
     {
-        return $this->services['service_from_anonymous_factory'] = (new \Bar\FooClass())->getInstance();
+        return $this->services['service_from_anonymous_factory'] = (new ${($_ = $this->getEnv('FOO')) && false ?: "_"}())->getInstance();
     }
 
     /**
@@ -77,5 +79,103 @@ class ProjectServiceContainer extends Container
         $instance->setBar(\Bar\FooClass::getInstance());
 
         return $instance;
+    }
+
+    public function getParameter($name)
+    {
+        $name = (string) $name;
+        if (!(isset($this->parameters[$name]) || isset($this->loadedDynamicParameters[$name]) || array_key_exists($name, $this->parameters))) {
+            $name = $this->normalizeParameterName($name);
+
+            if (!(isset($this->parameters[$name]) || isset($this->loadedDynamicParameters[$name]) || array_key_exists($name, $this->parameters))) {
+                throw new InvalidArgumentException(sprintf('The parameter "%s" must be defined.', $name));
+            }
+        }
+        if (isset($this->loadedDynamicParameters[$name])) {
+            return $this->loadedDynamicParameters[$name] ? $this->dynamicParameters[$name] : $this->getDynamicParameter($name);
+        }
+
+        return $this->parameters[$name];
+    }
+
+    public function hasParameter($name)
+    {
+        $name = (string) $name;
+        $name = $this->normalizeParameterName($name);
+
+        return isset($this->parameters[$name]) || isset($this->loadedDynamicParameters[$name]) || array_key_exists($name, $this->parameters);
+    }
+
+    public function setParameter($name, $value)
+    {
+        throw new LogicException('Impossible to call set() on a frozen ParameterBag.');
+    }
+
+    public function getParameterBag()
+    {
+        if (null === $this->parameterBag) {
+            $parameters = $this->parameters;
+            foreach ($this->loadedDynamicParameters as $name => $loaded) {
+                $parameters[$name] = $loaded ? $this->dynamicParameters[$name] : $this->getDynamicParameter($name);
+            }
+            $this->parameterBag = new FrozenParameterBag($parameters);
+        }
+
+        return $this->parameterBag;
+    }
+
+    private $loadedDynamicParameters = array(
+        'foo' => false,
+    );
+    private $dynamicParameters = array();
+
+    /**
+     * Computes a dynamic parameter.
+     *
+     * @param string The name of the dynamic parameter to load
+     *
+     * @return mixed The value of the dynamic parameter
+     *
+     * @throws InvalidArgumentException When the dynamic parameter does not exist
+     */
+    private function getDynamicParameter($name)
+    {
+        switch ($name) {
+            case 'foo': $value = $this->getEnv('FOO'); break;
+            default: throw new InvalidArgumentException(sprintf('The dynamic parameter "%s" must be defined.', $name));
+        }
+        $this->loadedDynamicParameters[$name] = true;
+
+        return $this->dynamicParameters[$name] = $value;
+    }
+
+    private $normalizedParameterNames = array(
+        'env(foo)' => 'env(FOO)',
+    );
+
+    private function normalizeParameterName($name)
+    {
+        if (isset($this->normalizedParameterNames[$normalizedName = strtolower($name)]) || isset($this->parameters[$normalizedName]) || array_key_exists($normalizedName, $this->parameters)) {
+            $normalizedName = isset($this->normalizedParameterNames[$normalizedName]) ? $this->normalizedParameterNames[$normalizedName] : $normalizedName;
+            if ((string) $name !== $normalizedName) {
+                @trigger_error(sprintf('Parameter names will be made case sensitive in Symfony 4.0. Using "%s" instead of "%s" is deprecated since Symfony 3.4.', $name, $normalizedName), E_USER_DEPRECATED);
+            }
+        } else {
+            $normalizedName = $this->normalizedParameterNames[$normalizedName] = (string) $name;
+        }
+
+        return $normalizedName;
+    }
+
+    /**
+     * Gets the default parameters.
+     *
+     * @return array An array of the default parameters
+     */
+    protected function getDefaultParameters()
+    {
+        return array(
+            'env(FOO)' => 'Bar\\FaooClass',
+        );
     }
 }
---
+++ src/Symfony/Component/DependencyInjection/Tests/Fixtures/php/services_adawson.php
@@ -0,0 +1,184 @@
+<?php
+
+use Symfony\Component\DependencyInjection\Argument\RewindableGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
+use Symfony\Component\DependencyInjection\ParameterBag\FrozenParameterBag;
+
+/**
+ * This class has been auto-generated
+ * by the Symfony Dependency Injection Component.
+ *
+ * @final since Symfony 3.3
+ */
+class ProjectServiceContainer extends Container
+{
+    private $parameters;
+    private $targetDirs = array();
+
+    public function __construct()
+    {
+        $this->services = array();
+        $this->normalizedIds = array(
+            'app\\bus' => 'App\\Bus',
+            'app\\db' => 'App\\Db',
+            'app\\handler1' => 'App\\Handler1',
+            'app\\handler2' => 'App\\Handler2',
+            'app\\processor' => 'App\\Processor',
+            'app\\registry' => 'App\\Registry',
+            'app\\schema' => 'App\\Schema',
+        );
+        $this->methodMap = array(
+            'App\\Bus' => 'getBusService',
+            'App\\Db' => 'getDbService',
+            'App\\Handler1' => 'getHandler1Service',
+            'App\\Handler2' => 'getHandler2Service',
+            'App\\Processor' => 'getProcessorService',
+            'App\\Registry' => 'getRegistryService',
+            'App\\Schema' => 'getSchemaService',
+        );
+        $this->privates = array(
+            'App\\Handler1' => true,
+            'App\\Handler2' => true,
+            'App\\Processor' => true,
+            'App\\Registry' => true,
+            'App\\Schema' => true,
+        );
+
+        $this->aliases = array();
+    }
+
+    public function getRemovedIds()
+    {
+        return array(
+            'App\\Handler1' => true,
+            'App\\Handler2' => true,
+            'App\\Processor' => true,
+            'App\\Registry' => true,
+            'App\\Schema' => true,
+            'Psr\\Container\\ContainerInterface' => true,
+            'Symfony\\Component\\DependencyInjection\\ContainerInterface' => true,
+        );
+    }
+
+    public function compile()
+    {
+        throw new LogicException('You cannot compile a dumped container that was already compiled.');
+    }
+
+    public function isCompiled()
+    {
+        return true;
+    }
+
+    public function isFrozen()
+    {
+        @trigger_error(sprintf('The %s() method is deprecated since Symfony 3.3 and will be removed in 4.0. Use the isCompiled() method instead.', __METHOD__), E_USER_DEPRECATED);
+
+        return true;
+    }
+
+    /**
+     * Gets the public 'App\Bus' shared service.
+     *
+     * @return \App\Bus
+     */
+    protected function getBusService()
+    {
+        $this->services['App\Bus'] = $instance = new \App\Bus(${($_ = isset($this->services['App\Db']) ? $this->services['App\Db'] : $this->getDbService()) && false ?: '_'});
+
+        $instance->handler1 = ${($_ = isset($this->services['App\Handler1']) ? $this->services['App\Handler1'] : $this->getHandler1Service()) && false ?: '_'};
+        $instance->handler2 = ${($_ = isset($this->services['App\Handler2']) ? $this->services['App\Handler2'] : $this->getHandler2Service()) && false ?: '_'};
+
+        return $instance;
+    }
+
+    /**
+     * Gets the public 'App\Db' shared service.
+     *
+     * @return \App\Db
+     */
+    protected function getDbService()
+    {
+        $this->services['App\Db'] = $instance = new \App\Db();
+
+        $instance->schema = ${($_ = isset($this->services['App\Schema']) ? $this->services['App\Schema'] : $this->getSchemaService()) && false ?: '_'};
+
+        return $instance;
+    }
+
+    /**
+     * Gets the private 'App\Handler1' shared service.
+     *
+     * @return \App\Handler1
+     */
+    protected function getHandler1Service()
+    {
+        $a = ${($_ = isset($this->services['App\Processor']) ? $this->services['App\Processor'] : $this->getProcessorService()) && false ?: '_'};
+
+        if (isset($this->services['App\Handler1'])) {
+            return $this->services['App\Handler1'];
+        }
+
+        return $this->services['App\Handler1'] = new \App\Handler1(${($_ = isset($this->services['App\Db']) ? $this->services['App\Db'] : $this->getDbService()) && false ?: '_'}, ${($_ = isset($this->services['App\Schema']) ? $this->services['App\Schema'] : $this->getSchemaService()) && false ?: '_'}, $a);
+    }
+
+    /**
+     * Gets the private 'App\Handler2' shared service.
+     *
+     * @return \App\Handler2
+     */
+    protected function getHandler2Service()
+    {
+        return $this->services['App\Handler2'] = new \App\Handler2(${($_ = isset($this->services['App\Db']) ? $this->services['App\Db'] : $this->getDbService()) && false ?: '_'}, ${($_ = isset($this->services['App\Schema']) ? $this->services['App\Schema'] : $this->getSchemaService()) && false ?: '_'}, ${($_ = isset($this->services['App\Processor']) ? $this->services['App\Processor'] : $this->getProcessorService()) && false ?: '_'});
+    }
+
+    /**
+     * Gets the private 'App\Processor' shared service.
+     *
+     * @return \App\Processor
+     */
+    protected function getProcessorService()
+    {
+        $a = ${($_ = isset($this->services['App\Registry']) ? $this->services['App\Registry'] : $this->getRegistryService()) && false ?: '_'};
+
+        if (isset($this->services['App\Processor'])) {
+            return $this->services['App\Processor'];
+        }
+
+        return $this->services['App\Processor'] = new \App\Processor($a, ${($_ = isset($this->services['App\Db']) ? $this->services['App\Db'] : $this->getDbService()) && false ?: '_'});
+    }
+
+    /**
+     * Gets the private 'App\Registry' shared service.
+     *
+     * @return \App\Registry
+     */
+    protected function getRegistryService()
+    {
+        $this->services['App\Registry'] = $instance = new \App\Registry();
+
+        $instance->processor = array(0 => ${($_ = isset($this->services['App\Db']) ? $this->services['App\Db'] : $this->getDbService()) && false ?: '_'}, 1 => ${($_ = isset($this->services['App\Bus']) ? $this->services['App\Bus'] : $this->getBusService()) && false ?: '_'});
+
+        return $instance;
+    }
+
+    /**
+     * Gets the private 'App\Schema' shared service.
+     *
+     * @return \App\Schema
+     */
+    protected function getSchemaService()
+    {
+        $a = ${($_ = isset($this->services['App\Db']) ? $this->services['App\Db'] : $this->getDbService()) && false ?: '_'};
+
+        if (isset($this->services['App\Schema'])) {
+            return $this->services['App\Schema'];
+        }
+
+        return $this->services['App\Schema'] = new \App\Schema($a);
+    }
+}

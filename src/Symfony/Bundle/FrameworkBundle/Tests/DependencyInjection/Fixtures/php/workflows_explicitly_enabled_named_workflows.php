017fd56bd85de2cf5c544f202743e90785ef4608
Christian Flothmann <christian.flothmann@sensiolabs.de>
2018-10-05 09:14:48 +0200

    deal with explicitly enabled workflow nodes

---
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/workflows_explicitly_enabled_named_workflows.php
@@ -0,0 +1,19 @@
+<?php
+
+$container->loadFromExtension('framework', array(
+    'workflows' => array(
+        'enabled' => true,
+        'workflows' => array(
+            'type' => 'workflow',
+            'supports' => array('Symfony\Bundle\FrameworkBundle\Tests\DependencyInjection\FrameworkExtensionTest'),
+            'initial_place' => 'bar',
+            'places' => array('bar', 'baz'),
+            'transitions' => array(
+                'bar_baz' => array(
+                    'from' => array('foo'),
+                    'to' => array('bar'),
+                ),
+            ),
+        ),
+    ),
+));

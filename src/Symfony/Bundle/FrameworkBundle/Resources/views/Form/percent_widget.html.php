2a05c4566b33d901156bc6063351c1e525bdd4f2
Ken Stanley <dohpaz@gmail.com>
2018-10-09 16:07:48 -0400

    Add license headers to percent_widget.html.php #28797

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/percent_widget.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/percent_widget.html.php
@@ -1,1 +1,12 @@
-<?php echo $view['form']->block($form, 'form_widget_simple', array('type' => isset($type) ? $type : 'text')) . (isset($symbol) && $symbol ? ' %': '') ?>
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+echo $view['form']->block($form, 'form_widget_simple', array('type' => isset($type) ? $type : 'text')) . (isset($symbol) && $symbol ? ' %': '') ?>

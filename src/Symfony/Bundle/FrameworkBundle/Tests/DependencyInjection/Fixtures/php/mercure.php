421279da1a9bd03badadc52c12f0c48cb8a36d88
Kévin Dunglas <dunglas@gmail.com>
2018-10-23 08:49:21 +0200

    Fix tests

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/mercure.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/mercure.php
@@ -5,7 +5,7 @@ $container->loadFromExtension('framework', array(
         'hubs' => array(
             array(
                 'name' => 'default',
-                'url' => 'https://demo.mercure.rocks/publish',
+                'url' => 'https://demo.mercure.rocks/hub',
                 'jwt' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.HB0k08BaV8KlLZ3EafCRlTDGbkd9qdznCzJQ_l8ELTU',
             ),
         ),

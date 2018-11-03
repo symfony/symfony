91957aa4cc8d9ebd9671bc7f9198e3800d5ffc06
Fabien Potencier <fabien@potencier.org>
2018-11-03 13:37:30 +0100

    updated VERSION for 4.2.0-BETA1

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -72,12 +72,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
     private $requestStackSize = 0;
     private $resetServices = false;
 
-    const VERSION = '4.2.0-DEV';
+    const VERSION = '4.2.0-BETA1';
     const VERSION_ID = 40200;
     const MAJOR_VERSION = 4;
     const MINOR_VERSION = 2;
     const RELEASE_VERSION = 0;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = 'BETA1';
 
     const END_OF_MAINTENANCE = '07/2019';
     const END_OF_LIFE = '01/2020';

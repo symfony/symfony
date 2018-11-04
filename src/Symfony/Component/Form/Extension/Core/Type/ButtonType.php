f8ad25da21b76f0669263fd960adea197484ee23
girayk@gmail.com <girayk@gmail.com>
2018-11-04 05:24:18 +0300

    Option resolver Exception added, Twig tweak

--- src/Symfony/Component/Form/Extension/Core/Type/ButtonType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/ButtonType.php
@@ -12,8 +12,10 @@
 namespace Symfony\Component\Form\Extension\Core\Type;
 
 use Symfony\Component\Form\ButtonTypeInterface;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -33,7 +35,26 @@ class ButtonType extends BaseType implements ButtonTypeInterface
         $view->vars = array_merge($view->vars, array(
             'icon' => $options['icon'],
         ));
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if (\is_array($options['icon'])) {
+            if (!isset($options['icon']['name'])) {
+                throw new InvalidOptionsException('The "icon" option must contain the key name.');
+            }
+
+            if (isset($options['icon']['space']) && !\is_numeric($options['icon']['space'])) {
+                throw new InvalidOptionsException('The "space" option must be numeric.');
+            }
+
+            if (isset($options['icon']['align']) && !\in_array($options['icon']['align'], array('left', 'right'), true)) {
+                throw new InvalidOptionsException('The "align" option must be one of the constants (left, right)');
+            }
+        }
     }
 
     /**
@@ -64,6 +85,5 @@ class ButtonType extends BaseType implements ButtonTypeInterface
         ));
 
         $resolver->setAllowedTypes('icon', array('array', 'string', 'null'));
-
     }
 }

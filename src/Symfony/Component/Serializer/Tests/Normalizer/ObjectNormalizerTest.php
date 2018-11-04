5fd0f3f453a8c995b6c40c2015b3fc6fd72d232f
Daniel Gorgan <danut007ro@gmail.com>
2018-11-04 02:29:33 +0200

    [Serializer] Allow null values when denormalizing with constructor missing data

--- src/Symfony/Component/Serializer/Tests/Normalizer/ObjectNormalizerTest.php
+++ src/Symfony/Component/Serializer/Tests/Normalizer/ObjectNormalizerTest.php
@@ -245,11 +245,11 @@ class ObjectNormalizerTest extends TestCase
 
         $result = $normalizer->denormalize($data, DummyValueObject::class, 'json', array(
             'default_constructor_arguments' => array(
-                DummyValueObject::class => array('foo' => '', 'bar' => ''),
+                DummyValueObject::class => array('foo' => '', 'bar' => '', 'baz' => null),
             ),
         ));
 
-        $this->assertEquals(new DummyValueObject(10, ''), $result);
+        $this->assertEquals(new DummyValueObject(10, '', null), $result);
     }
 
     public function testGroupsNormalize()
@@ -1117,11 +1117,13 @@ class DummyValueObject
 {
     private $foo;
     private $bar;
+    private $baz;
 
-    public function __construct($foo, $bar)
+    public function __construct($foo, $bar, $baz)
     {
         $this->foo = $foo;
         $this->bar = $bar;
+        $this->baz = $baz;
     }
 }
 

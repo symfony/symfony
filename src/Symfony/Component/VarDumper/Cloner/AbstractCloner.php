d6a594bf426527a61bb404fdbbf5125d9ef0c9a5
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-11-17 10:06:42 +0100

    [EventDispatcher][VarDumper] optimize perf by leveraging Closure::fromCallable()

--- src/Symfony/Component/VarDumper/Cloner/AbstractCloner.php
+++ src/Symfony/Component/VarDumper/Cloner/AbstractCloner.php
@@ -176,7 +176,10 @@ abstract class AbstractCloner implements ClonerInterface
     public function addCasters(array $casters)
     {
         foreach ($casters as $type => $callback) {
-            $this->casters[strtolower($type)][] = \is_string($callback) && false !== strpos($callback, '::') ? explode('::', $callback, 2) : $callback;
+            $closure = &$this->casters[strtolower($type)][];
+            $closure = $callback instanceof \Closure ? $callback : static function (...$args) use ($callback, &$closure) {
+                return ($closure = \Closure::fromCallable($callback))(...$args);
+            };
         }
     }
 

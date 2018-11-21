205a44ea7db84e6408c7bdef0b89b071be235f28
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-11-21 13:01:00 +0100

    [Form] Filter file uploads out of regular form types

--- src/Symfony/Component/Form/Extension/Core/Type/FormType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/FormType.php
@@ -213,6 +213,7 @@ class FormType extends BaseType
             'attr' => $defaultAttr,
             'post_max_size_message' => 'The uploaded file was too large. Please try to upload a smaller file.',
             'upload_max_size_message' => $uploadMaxSizeMessage, // internal
+            'allow_file_upload' => false,
         ));
 
         $resolver->setAllowedTypes('label_attr', 'array');

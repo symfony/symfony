63cd219a5c5339218c73485d9ebe9fc6ff55ec14
Robin Chalas <robin.chalas@elao.com>
2018-11-25 16:28:24 +0100

    [Console] Move back root exception to stack trace in verbose mode

--- src/Symfony/Component/Console/Application.php
+++ src/Symfony/Component/Console/Application.php
@@ -783,6 +783,13 @@ class Application
                 // exception related properties
                 $trace = $e->getTrace();
 
+                array_unshift($trace, array(
+                    'function' => '',
+                    'file' => $e->getFile() ?: 'n/a',
+                    'line' => $e->getLine() ?: 'n/a',
+                    'args' => array(),
+                ));
+
                 for ($i = 0, $count = \count($trace); $i < $count; ++$i) {
                     $class = isset($trace[$i]['class']) ? $trace[$i]['class'] : '';
                     $type = isset($trace[$i]['type']) ? $trace[$i]['type'] : '';

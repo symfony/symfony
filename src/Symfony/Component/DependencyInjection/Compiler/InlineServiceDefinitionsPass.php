484c49edb35d53bbe35d345f664d9c8875c7d46d
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-12-02 16:50:25 +0100

    [DI] dont inline when lazy edges are found

--- src/Symfony/Component/DependencyInjection/Compiler/InlineServiceDefinitionsPass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/InlineServiceDefinitionsPass.php
@@ -130,7 +130,7 @@ class InlineServiceDefinitionsPass extends AbstractRecursivePass implements Repe
         $isReferencedByConstructor = false;
         foreach ($graph->getNode($id)->getInEdges() as $edge) {
             $isReferencedByConstructor = $isReferencedByConstructor || $edge->isReferencedByConstructor();
-            if ($edge->isWeak()) {
+            if ($edge->isWeak() || $edge->isLazy()) {
                 return false;
             }
             $ids[] = $edge->getSourceNode()->getId();

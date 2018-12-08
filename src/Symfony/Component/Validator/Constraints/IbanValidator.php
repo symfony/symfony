5f0261429ab38a7d16b72ff666834f3949352269
Raul Fraile <raulfraile@gmail.com>
2018-12-08 16:03:46 +0100

    [Validator] Added IBAN format for Vatican City State

--- src/Symfony/Component/Validator/Constraints/IbanValidator.php
+++ src/Symfony/Component/Validator/Constraints/IbanValidator.php
@@ -129,6 +129,7 @@ class IbanValidator extends ConstraintValidator
         'TN' => 'TN59\d{2}\d{3}\d{13}\d{2}', // Tunisia
         'TR' => 'TR\d{2}\d{5}[\dA-Z]{1}[\dA-Z]{16}', // Turkey
         'UA' => 'UA\d{2}\d{6}[\dA-Z]{19}', // Ukraine
+        'VA' => 'VA\d{2}\d{3}\d{15}', // Vatican City State
         'VG' => 'VG\d{2}[A-Z]{4}\d{16}', // Virgin Islands, British
         'WF' => 'FR\d{2}\d{5}\d{5}[\dA-Z]{11}\d{2}', // Wallis and Futuna Islands
         'XK' => 'XK\d{2}\d{4}\d{10}\d{2}', // Republic of Kosovo

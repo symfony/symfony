c0a0362ed5b3f8e26e3a1deac3830f0e0c47c8ea
Roland Franssen <franssen.roland@gmail.com>
2018-12-09 19:49:25 +0100

    introduce scss

--- assets/entries/profiler.js
+++ assets/entries/profiler.js
@@ -1,6 +1,6 @@
 import '../components/_vars.css';
 import '../components/_reset.css';
-import '../components/_basics.css';
+import '../components/_basics.scss';
 import {default as createTabs} from '../components/tabs';
 import {addEventListener} from '../components/_event';
 

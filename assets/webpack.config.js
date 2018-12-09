01233a8e0ea6e8664294ca4493447eebf6f9b251
Roland Franssen <franssen.roland@gmail.com>
2018-12-09 19:30:09 +0100

    fixes

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,6 +4,7 @@ Encore
     .setOutputPath('build/')
     .setPublicPath('/')
     .addEntry('profiler', './entries/profiler.js')
+    .enableSassLoader()
     .disableSingleRuntimeChunk() // @todo ok?
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()

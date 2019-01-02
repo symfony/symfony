e5420f7c564c76dcf1f6a7fbf584616b6c799ea7
Grégoire Paris <postmaster@greg0ire.fr>
2019-01-02 23:14:16 +0100

    Add build statistics

--- src/Symfony/Component/Process/Tests/KillableProcessWithOutput.php
+++ src/Symfony/Component/Process/Tests/KillableProcessWithOutput.php
@@ -9,6 +9,23 @@
  * file that was distributed with this source code.
  */
 
+// green builds: 6/7 with 97dab5f
+// green builds: 3/4 without 97dab5f
+
+/*
+ * builds with the weird output assertion failure:
+ * - https://ci.appveyor.com/project/fabpot/symfony/builds/21344209
+ *
+ * builds with a weirder output assertion failure:
+ * - https://ci.appveyor.com/project/fabpot/symfony/builds/21344789#L1190
+ *
+ * builds where the process is not even running:
+ * - https://ci.appveyor.com/project/fabpot/symfony/builds/21344543
+ * - https://ci.appveyor.com/project/fabpot/symfony/builds/21345038
+ * - https://ci.appveyor.com/project/fabpot/symfony/builds/21364454
+ * - https://ci.appveyor.com/project/fabpot/symfony/builds/21364899
+ */
+
 $outputs = array(
     'First iteration output',
     'Second iteration output',

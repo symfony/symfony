c3df6545ceb40e158df7e7b501126add3d5008f9
Shrey Puranik <shreypuranik@users.noreply.github.com>
2019-01-08 10:19:33 +0000

    Update Request.php
    Removing comment as it's potentially confusing

--- src/Symfony/Component/HttpFoundation/Request.php
+++ src/Symfony/Component/HttpFoundation/Request.php
@@ -709,7 +709,6 @@ class Request
 
         if (null === $session) {
             @trigger_error(sprintf('Calling "%s()" when no session has been set is deprecated since Symfony 4.1 and will throw an exception in 5.0. Use "hasSession()" instead.', __METHOD__), E_USER_DEPRECATED);
-            // throw new \BadMethodCallException('Session has not been set');
         }
 
         return $session;

4b0eb1705f53330d9837b78861d72e119bcb5f64
Franck Gamess <franck.gamess@gmail.com>
2019-01-08 12:25:45 +0400

    [HttpKernel] added Response::HTTP_NOT_FOUND constant in NotFoundHttpException

--- src/Symfony/Component/HttpKernel/Exception/NotFoundHttpException.php
+++ src/Symfony/Component/HttpKernel/Exception/NotFoundHttpException.php
@@ -11,6 +11,8 @@
 
 namespace Symfony\Component\HttpKernel\Exception;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * @author Fabien Potencier <fabien@symfony.com>
  */
@@ -23,6 +25,6 @@ class NotFoundHttpException extends HttpException
      */
     public function __construct($message = null, \Exception $previous = null, $code = 0)
     {
-        parent::__construct(404, $message, $previous, array(), $code);
+        parent::__construct(Response::HTTP_NOT_FOUND, $message, $previous, array(), $code);
     }
 }

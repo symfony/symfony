02bd6893a53ccc09295162bc8d06be93bbbc727c
Alexander Schranz <alexander@sulu.io>
2019-01-12 17:21:48 +0100

    Add block prefix to csrf token field

--- src/Symfony/Component/Form/Extension/Csrf/Type/FormTypeCsrfExtension.php
+++ src/Symfony/Component/Form/Extension/Csrf/Type/FormTypeCsrfExtension.php
@@ -90,9 +90,10 @@ class FormTypeCsrfExtension extends AbstractTypeExtension
             $tokenId = $options['csrf_token_id'] ?: ($form->getName() ?: \get_class($form->getConfig()->getType()->getInnerType()));
             $data = (string) $options['csrf_token_manager']->getToken($tokenId);
 
-            $csrfForm = $factory->createNamed($options['csrf_field_name'], 'Symfony\Component\Form\Extension\Core\Type\HiddenType', $data, array(
+            $csrfForm = $factory->createNamed($options['csrf_field_name'], 'Symfony\Component\Form\Extension\Core\Type\HiddenType', $data, [
+                'block_prefix' => 'csrf_token',
                 'mapped' => false,
-            ));
+            ]);
 
             $view->children[$options['csrf_field_name']] = $csrfForm->createView($view);
         }
@@ -103,13 +104,13 @@ class FormTypeCsrfExtension extends AbstractTypeExtension
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'csrf_protection' => $this->defaultEnabled,
             'csrf_field_name' => $this->defaultFieldName,
             'csrf_message' => 'The CSRF token is invalid. Please try to resubmit the form.',
             'csrf_token_manager' => $this->defaultTokenManager,
             'csrf_token_id' => null,
-        ));
+        ]);
     }
 
     /**
@@ -117,6 +118,6 @@ class FormTypeCsrfExtension extends AbstractTypeExtension
      */
     public static function getExtendedTypes(): iterable
     {
-        return array(FormType::class);
+        return [FormType::class];
     }
 }

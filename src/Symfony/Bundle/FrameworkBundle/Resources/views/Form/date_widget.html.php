7ffd8d3e03512e849c7dac07ddf30e9d12d9fe11
Fabien Potencier <fabien@potencier.org>
2019-01-16 12:03:13 +0100

    fixed CS on Form PHP templates

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/date_widget.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/date_widget.html.php
@@ -2,10 +2,10 @@
     <?php echo $view['form']->block($form, 'form_widget_simple'); ?>
 <?php else: ?>
     <div <?php echo $view['form']->block($form, 'widget_container_attributes') ?>>
-        <?php echo str_replace(array('{{ year }}', '{{ month }}', '{{ day }}'), array(
+        <?php echo str_replace(['{{ year }}', '{{ month }}', '{{ day }}'], [
             $view['form']->widget($form['year']),
             $view['form']->widget($form['month']),
             $view['form']->widget($form['day']),
-        ), $date_pattern) ?>
+        ], $date_pattern) ?>
     </div>
 <?php endif ?>

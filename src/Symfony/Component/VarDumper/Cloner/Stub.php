46d6a4d9ec54a57e271accf955c4a7bd9945ae3e
Nicolas Grekas <nicolas.grekas@gmail.com>
2019-02-16 23:37:38 +0100

    [VarDumper] fix serializing Stub instances

--- src/Symfony/Component/VarDumper/Cloner/Stub.php
+++ src/Symfony/Component/VarDumper/Cloner/Stub.php
@@ -39,22 +39,29 @@ class Stub
     public $position = 0;
     public $attr = [];
 
+    private static $defaultProperties = [];
+
     /**
      * @internal
      */
     public function __sleep()
     {
-        $this->serialized = [$this->class, $this->position, $this->cut, $this->type, $this->value, $this->handle, $this->refCount, $this->attr];
+        $properties = [];
 
-        return ['serialized'];
-    }
+        if (!isset(self::$defaultProperties[$c = \get_class($this)])) {
+            self::$defaultProperties[$c] = get_class_vars($c);
 
-    /**
-     * @internal
-     */
-    public function __wakeup()
-    {
-        list($this->class, $this->position, $this->cut, $this->type, $this->value, $this->handle, $this->refCount, $this->attr) = $this->serialized;
-        unset($this->serialized);
+            foreach ((new \ReflectionClass($c))->getStaticProperties() as $k => $v) {
+                unset(self::$defaultProperties[$c][$k]);
+            }
+        }
+
+        foreach (self::$defaultProperties[$c] as $k => $v) {
+            if ($this->$k !== $v) {
+                $properties[] = $k;
+            }
+        }
+
+        return $properties;
     }
 }

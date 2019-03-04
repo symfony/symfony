04dc6921bddaffd2c3db160bbce678c7f99a0935
Mantis Development <mantis@users.noreply.github.com>
2019-03-04 22:04:58 +0000

    Remove whitespace (tab on blank line)

--- src/Symfony/Component/PropertyInfo/Tests/Extractor/ReflectionExtractorTest.php
+++ src/Symfony/Component/PropertyInfo/Tests/Extractor/ReflectionExtractorTest.php
@@ -293,7 +293,7 @@ class ReflectionExtractorTest extends TestCase
             [NotInstantiable::class, 'foo', false],
         ];
     }
-    
+
     /**
      * @dataProvider constructorTypesProvider
      */

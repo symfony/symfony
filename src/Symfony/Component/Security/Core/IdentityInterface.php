02ca12d19afeb5577c1c7925edfd9d77171a56e1
Ricardo de Vries <ricardozegt@gmail.com>
2019-03-17 06:49:54 +0100

    Implemented a identityinterface.

---
+++ src/Symfony/Component/Security/Core/IdentityInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Security\Core;
+
+/**
+ * @author Ricardo de Vries <ricardozegt@gmail.com>
+ * @author Wouter J <wouter@wouterj.nl>
+ */
+interface IdentityInterface
+{
+    public function getIdentifier();
+}

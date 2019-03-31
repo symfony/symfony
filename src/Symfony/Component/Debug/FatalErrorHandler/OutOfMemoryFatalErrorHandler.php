cbcf49c614d4e46b4a8f0b80f05fc61752285a4e
error56 <46536532+error56@users.noreply.github.com>
2019-03-31 14:56:08 +0200

    Add OutOfMemory handler

---
+++ src/Symfony/Component/Debug/FatalErrorHandler/OutOfMemoryFatalErrorHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Debug\FatalErrorHandler;
+
+use Symfony\Component\Debug\Exception\FatalErrorException;
+use Symfony\Component\Debug\Exception\OutOfMemoryException;
+
+/**
+ * ErrorHandler for out of memory errors.
+ *
+ * @author error56 <nalldev2@gmail.com>
+ */
+class OutOfMemoryFatalErrorHandler implements FatalErrorHandlerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function handleError(array $error, FatalErrorException $exception)
+    {
+        if (false !== strpos($error['message'], 'memory')) {
+            $message = 'I didn\t got enough memory for execution :\/';
+            return new OutOfMemoryException($message, $exception);
+        }
+    }
+}

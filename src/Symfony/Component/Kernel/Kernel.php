419688ac903f28509c17bffc336be010262114c1
Alexandre Quercia <alquerci@email.com>
2019-03-31 20:45:05 +0200

    Revert "feature #30651 Allow user to set the project dir (tdutrion)"

--- src/Symfony/Component/Kernel/Kernel.php
+++ src/Symfony/Component/Kernel/Kernel.php
@@ -81,13 +81,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface
     const END_OF_MAINTENANCE = '01/2020';
     const END_OF_LIFE = '07/2020';
 
-    public function __construct(string $environment, bool $debug, string $projectDir = null)
+    public function __construct(string $environment, bool $debug)
     {
         $this->environment = $environment;
         $this->debug = $debug;
         $this->rootDir = $this->getRootDir(false);
         $this->name = $this->getName(false);
-        $this->projectDir = $projectDir;
     }
 
     public function __clone()

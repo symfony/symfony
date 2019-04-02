a362b8b8384570519f5465cb45b8009d6afd53b3
Fabien Potencier <fabien@potencier.org>
2019-04-02 21:06:21 +0200

    bumped Symfony version to 4.2.6

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -73,12 +73,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
     private $requestStackSize = 0;
     private $resetServices = false;
 
-    const VERSION = '4.2.5';
-    const VERSION_ID = 40205;
+    const VERSION = '4.2.6-DEV';
+    const VERSION_ID = 40206;
     const MAJOR_VERSION = 4;
     const MINOR_VERSION = 2;
-    const RELEASE_VERSION = 5;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 6;
+    const EXTRA_VERSION = 'DEV';
 
     const END_OF_MAINTENANCE = '07/2019';
     const END_OF_LIFE = '01/2020';

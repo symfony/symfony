2ae2fd800d99123e1107d08afb1345f54932e8ec
Александр Ли <aweelex@gmail.com>
2019-04-08 12:19:48 +0500

    [FrameworkBundle] Fix Controller deprecated when using composer --optimized
    Update Controller.php
    Update Controller.php

--- src/Symfony/Bundle/FrameworkBundle/Controller/Controller.php
+++ src/Symfony/Bundle/FrameworkBundle/Controller/Controller.php
@@ -14,14 +14,12 @@ namespace Symfony\Bundle\FrameworkBundle\Controller;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-@trigger_error(sprintf('The "%s" class is deprecated since Symfony 4.2, use %s instead.', Controller::class, AbstractController::class), E_USER_DEPRECATED);
-
 /**
  * Controller is a simple implementation of a Controller.
  *
  * It provides methods to common features needed in controllers.
  *
- * @deprecated since Symfony 4.2, use {@see AbstractController} instead.
+ * @deprecated since Symfony 4.2, use "Symfony\Bundle\FrameworkBundle\Controller\AbstractController" instead.
  *
  * @author Fabien Potencier <fabien@symfony.com>
  */

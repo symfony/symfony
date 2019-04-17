d4843fe948d18611a07469d74eff0836ec988ce7
Fabien Potencier <fabien@potencier.org>
2019-04-17 18:42:21 +0200

    fixed version

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -60,7 +60,7 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $loadClassCache;
 
     const VERSION = '2.8.50';
-    const VERSION_ID = 2.8.50;
+    const VERSION_ID = 20850;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 8;
     const RELEASE_VERSION = 50;

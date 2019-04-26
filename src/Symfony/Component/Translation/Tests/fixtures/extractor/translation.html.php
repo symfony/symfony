ca77d9a33ed706095c83bd81e0939d4eea2d9111
Stadly <magnar@myrtveit.com>
2019-04-26 16:33:25 +0200

    [Translator] Preserve default domain when extracting strings from php files

--- src/Symfony/Component/Translation/Tests/fixtures/extractor/translation.html.php
+++ src/Symfony/Component/Translation/Tests/fixtures/extractor/translation.html.php
@@ -55,3 +55,5 @@ EOF
 <?php echo $view['translator']->trans('typecast', ['a' => (int) '123'], 'not_messages'); ?>
 <?php echo $view['translator']->transChoice('msg1', 10 + 1, [], 'not_messages'); ?>
 <?php echo $view['translator']->transChoice('msg2', ceil(4.5), [], 'not_messages'); ?>
+
+<?php echo $view['translator']->trans('default domain', [], null); ?>

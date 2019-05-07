8a49eb86601b3b47ded31aa28c2b386701809620
Ryan Weaver <ryan@thatsquality.com>
2019-05-06 20:56:51 -0400

    Simplifying SyncTransport and fixing bug with handlers transport

--- src/Symfony/Component/Messenger/Tests/Transport/Sync/SyncTransportFactoryTest.php
+++ src/Symfony/Component/Messenger/Tests/Transport/Sync/SyncTransportFactoryTest.php
@@ -12,6 +12,7 @@
 namespace Symfony\Component\Messenger\Tests\Transport\AmqpExt;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 use Symfony\Component\Messenger\Transport\Sync\SyncTransport;
 use Symfony\Component\Messenger\Transport\Sync\SyncTransportFactory;
@@ -21,7 +22,8 @@ class SyncTransportFactoryTest extends TestCase
     public function testCreateTransport()
     {
         $serializer = $this->createMock(SerializerInterface::class);
-        $factory = new SyncTransportFactory();
+        $bus = $this->createMock(MessageBusInterface::class);
+        $factory = new SyncTransportFactory($bus);
         $transport = $factory->createTransport('sync://', [], $serializer);
         $this->assertInstanceOf(SyncTransport::class, $transport);
     }
--- src/Symfony/Component/Messenger/Transport/Sync/SyncTransportFactory.php
+++ src/Symfony/Component/Messenger/Transport/Sync/SyncTransportFactory.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Component\Messenger\Transport\Sync;
 
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 use Symfony\Component\Messenger\Transport\TransportFactoryInterface;
 use Symfony\Component\Messenger\Transport\TransportInterface;
@@ -22,9 +23,16 @@ use Symfony\Component\Messenger\Transport\TransportInterface;
  */
 class SyncTransportFactory implements TransportFactoryInterface
 {
+    private $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
+    {
+        $this->messageBus = $messageBus;
+    }
+
     public function createTransport(string $dsn, array $options, SerializerInterface $serializer): TransportInterface
     {
-        return new SyncTransport();
+        return new SyncTransport($this->messageBus);
     }
 
     public function supports(string $dsn, array $options): bool

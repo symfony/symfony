e6071a15c2cf06d484a787d4064bfe3b8817a263
Fabien Potencier <fabien@potencier.org>
2019-05-08 21:41:19 +0200

    fixed test on Windows

--- src/Symfony/Component/Translation/Tests/Extractor/PhpExtractorTest.php
+++ src/Symfony/Component/Translation/Tests/Extractor/PhpExtractorTest.php
@@ -70,7 +70,7 @@ EOF;
 
         $this->assertEquals($expectedCatalogue, $actualCatalogue);
 
-        $filename = __DIR__.'/../fixtures/extractor/translation.html.php';
+        $filename = str_replace(\DIRECTORY_SEPARATOR, '/', __DIR__).'/../fixtures/extractor/translation.html.php';
         $this->assertEquals(['sources' => [$filename.':2']], $catalogue->getMetadata('single-quoted key'));
         $this->assertEquals(['sources' => [$filename.':43']], $catalogue->getMetadata('other-domain-test-no-params-short-array', 'not_messages'));
     }

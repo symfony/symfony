6a542cd738c2e8e91c8b7a56d4fbedcd179286d1
Samuel ROZE <samuel.roze@gmail.com>
2019-05-12 16:09:02 +0100

    Add transport in subscriber's phpdoc

--- src/Symfony/Component/Messenger/Handler/MessageSubscriberInterface.php
+++ src/Symfony/Component/Messenger/Handler/MessageSubscriberInterface.php
@@ -32,13 +32,14 @@ interface MessageSubscriberInterface extends MessageHandlerInterface
      *     yield FirstMessage::class => ['priority' => 0];
      *     yield SecondMessage::class => ['priority => -10];
      *
-     * It can also specify a method, a priority and/or a bus per message:
+     * It can also specify a method, a priority, a bus and/or a transport per message:
      *
      *     yield FirstMessage::class => ['method' => 'firstMessageMethod'];
      *     yield SecondMessage::class => [
      *         'method' => 'secondMessageMethod',
      *         'priority' => 20,
      *         'bus' => 'my_bus_name',
+     *         'from_transport' => 'your_transport_name',
      *     ];
      *
      * The benefit of using `yield` instead of returning an array is that you can `yield` multiple times the

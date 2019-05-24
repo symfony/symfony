bc3f598bfed62110dd87cf8152a572f1d4e6c9d8
Alexander M. Turek <me@derrabus.de>
2019-05-24 22:20:57 +0200

    Allow WrappedListener to describe uncallable listeners.

--- src/Symfony/Component/EventDispatcher/Tests/Debug/WrappedListenerTest.php
+++ src/Symfony/Component/EventDispatcher/Tests/Debug/WrappedListenerTest.php
@@ -21,7 +21,7 @@ class WrappedListenerTest extends TestCase
     /**
      * @dataProvider provideListenersToDescribe
      */
-    public function testListenerDescription(callable $listener, $expected)
+    public function testListenerDescription($listener, $expected)
     {
         $wrappedListener = new WrappedListener($listener, null, $this->getMockBuilder(Stopwatch::class)->getMock(), $this->getMockBuilder(EventDispatcherInterface::class)->getMock());
 
@@ -34,6 +34,7 @@ class WrappedListenerTest extends TestCase
             [new FooListener(), 'Symfony\Component\EventDispatcher\Tests\Debug\FooListener::__invoke'],
             [[new FooListener(), 'listen'], 'Symfony\Component\EventDispatcher\Tests\Debug\FooListener::listen'],
             [['Symfony\Component\EventDispatcher\Tests\Debug\FooListener', 'listenStatic'], 'Symfony\Component\EventDispatcher\Tests\Debug\FooListener::listenStatic'],
+            [['Symfony\Component\EventDispatcher\Tests\Debug\FooListener', 'invalidMethod'], 'Symfony\Component\EventDispatcher\Tests\Debug\FooListener::invalidMethod'],
             ['var_dump', 'var_dump'],
             [function () {}, 'closure'],
             [\Closure::fromCallable([new FooListener(), 'listen']), 'Symfony\Component\EventDispatcher\Tests\Debug\FooListener::listen'],

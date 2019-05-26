df9c949dc0e735ab6420c171131e4ff93ebda6d0
Michael Käfer <michael.kaefer1@gmx.at>
2019-05-26 22:27:04 +0200

    Optimize error message ("attribute" instead of "parameter")

--- src/Symfony/Component/HttpKernel/Controller/ControllerResolver.php
+++ src/Symfony/Component/HttpKernel/Controller/ControllerResolver.php
@@ -37,7 +37,7 @@ class ControllerResolver implements ControllerResolverInterface
     {
         if (!$controller = $request->attributes->get('_controller')) {
             if (null !== $this->logger) {
-                $this->logger->warning('Unable to look for the controller as the "_controller" parameter is missing.');
+                $this->logger->warning('Unable to look for the controller as the "_controller" attribute is missing.');
             }
 
             return false;

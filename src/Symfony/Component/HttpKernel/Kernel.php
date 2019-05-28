bb9a67df3b7577cf02967ce30e9b6f113657542d
Fabien Potencier <fabien@potencier.org>
2019-05-28 11:38:37 +0200

    bumped Symfony version to 3.4.29

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -67,12 +67,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
     private $requestStackSize = 0;
     private $resetServices = false;
 
-    const VERSION = '3.4.28';
-    const VERSION_ID = 30428;
+    const VERSION = '3.4.29-DEV';
+    const VERSION_ID = 30429;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 4;
-    const RELEASE_VERSION = 28;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 29;
+    const EXTRA_VERSION = 'DEV';
 
     const END_OF_MAINTENANCE = '11/2020';
     const END_OF_LIFE = '11/2021';

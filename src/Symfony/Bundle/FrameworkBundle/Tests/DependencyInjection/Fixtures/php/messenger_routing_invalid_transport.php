79090928916350a1da48c540ec3e275855420fe7
Konstantin Myakshin <molodchick@gmail.com>
2019-05-30 00:00:39 +0300

    [Messenger] Improve error message when routing to an invalid transport (closes #31613)

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/messenger_routing_invalid_transport.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/messenger_routing_invalid_transport.php
@@ -7,15 +7,10 @@ $container->loadFromExtension('framework', [
             'default_serializer' => 'messenger.transport.symfony_serializer',
         ],
         'routing' => [
-            'Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Messenger\DummyMessage' => ['amqp', 'messenger.transport.audit'],
-            'Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Messenger\SecondMessage' => [
-                'senders' => ['amqp', 'audit'],
-            ],
-            '*' => 'amqp',
+            'Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Messenger\DummyMessage' => 'invalid',
         ],
         'transports' => [
             'amqp' => 'amqp://localhost/%2f/messages',
-            'audit' => 'null://',
         ],
     ],
 ]);

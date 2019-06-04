91aead8fe81b90d600d3cdf77ae53dd0bcf271ff
Fabien Potencier <fabien@potencier.org>
2019-06-04 07:14:11 +0200

    fixed typo

--- src/Symfony/Bundle/FrameworkBundle/Controller/ControllerTrait.php
+++ src/Symfony/Bundle/FrameworkBundle/Controller/ControllerTrait.php
@@ -206,7 +206,7 @@ trait ControllerTrait
     protected function renderView(string $view, array $parameters = []): string
     {
         if (!$this->container->has('twig')) {
-            throw new \LogicException('You can not use the "renderView" method if the Twig Bundle are not available. Try running "composer require symfony/twig-bundle".');
+            throw new \LogicException('You can not use the "renderView" method if the Twig Bundle is not available. Try running "composer require symfony/twig-bundle".');
         }
 
         return $this->container->get('twig')->render($view, $parameters);
@@ -238,7 +238,7 @@ trait ControllerTrait
     protected function stream(string $view, array $parameters = [], StreamedResponse $response = null): StreamedResponse
     {
         if (!$this->container->has('twig')) {
-            throw new \LogicException('You can not use the "stream" method if the Twig Bundle are not available. Try running "composer require symfony/twig-bundle".');
+            throw new \LogicException('You can not use the "stream" method if the Twig Bundle is not available. Try running "composer require symfony/twig-bundle".');
         }
 
         $twig = $this->container->get('twig');

b5e6c99a3b1aa46ffeb859111e5b0fde4678bde4
Ivo <ivo@wax.be>
2019-06-05 10:24:41 +0200

    [HttpFoundation] Fixed case-sensitive handling of cache-control header in RedirectResponse constructor.

--- src/Symfony/Component/HttpFoundation/Tests/RedirectResponseTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/RedirectResponseTest.php
@@ -91,6 +91,10 @@ class RedirectResponseTest extends TestCase
         $this->assertFalse($response->headers->hasCacheControlDirective('no-cache'));
         $this->assertTrue($response->headers->hasCacheControlDirective('max-age'));
 
+        $response = new RedirectResponse('foo.bar', 301, ['Cache-Control' => 'max-age=86400']);
+        $this->assertFalse($response->headers->hasCacheControlDirective('no-cache'));
+        $this->assertTrue($response->headers->hasCacheControlDirective('max-age'));
+
         $response = new RedirectResponse('foo.bar', 302);
         $this->assertTrue($response->headers->hasCacheControlDirective('no-cache'));
     }

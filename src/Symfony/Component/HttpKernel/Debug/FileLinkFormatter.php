4a4b62bf4d012f4052a1127ef075a01809bc25be
Nicolas Grekas <nicolas.grekas@gmail.com>
2019-06-06 11:21:46 +0200

    [HttpKernel] fix link to source generation

--- src/Symfony/Component/HttpKernel/Debug/FileLinkFormatter.php
+++ src/Symfony/Component/HttpKernel/Debug/FileLinkFormatter.php
@@ -68,7 +68,7 @@ class FileLinkFormatter
      */
     public function __sleep(): array
     {
-        $this->getFileLinkFormat();
+        $this->fileLinkFormat = $this->getFileLinkFormat();
 
         return ['fileLinkFormat'];
     }
@@ -87,17 +87,19 @@ class FileLinkFormatter
 
     private function getFileLinkFormat()
     {
+        if ($this->fileLinkFormat) {
+            return $this->fileLinkFormat;
+        }
+
         if ($this->requestStack && $this->baseDir && $this->urlFormat) {
             $request = $this->requestStack->getMasterRequest();
 
             if ($request instanceof Request && (!$this->urlFormat instanceof \Closure || $this->urlFormat = ($this->urlFormat)())) {
-                $this->fileLinkFormat = [
+                return [
                     $request->getSchemeAndHttpHost().$request->getBasePath().$this->urlFormat,
                     $this->baseDir.\DIRECTORY_SEPARATOR, '',
                 ];
             }
         }
-
-        return $this->fileLinkFormat;
     }
 }

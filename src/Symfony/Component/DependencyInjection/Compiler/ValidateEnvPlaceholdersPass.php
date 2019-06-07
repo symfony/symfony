98c25dd34054fc9dac68bcc7d763bbfbc8f7c48b
Roland Franssen <franssen.roland@gmail.com>
2019-06-07 22:39:31 +0200

    [DI][5.0] Fix suspicious test

--- src/Symfony/Component/DependencyInjection/Compiler/ValidateEnvPlaceholdersPass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/ValidateEnvPlaceholdersPass.php
@@ -12,7 +12,6 @@
 namespace Symfony\Component\DependencyInjection\Compiler;
 
 use Symfony\Component\Config\Definition\BaseNode;
-use Symfony\Component\Config\Definition\Exception\TreeWithoutRootNodeException;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ConfigurationExtensionInterface;
@@ -80,10 +79,7 @@ class ValidateEnvPlaceholdersPass implements CompilerPassInterface
                     continue;
                 }
 
-                try {
-                    $this->extensionConfig[$name] = $processor->processConfiguration($configuration, $config);
-                } catch (TreeWithoutRootNodeException $e) {
-                }
+                $this->extensionConfig[$name] = $processor->processConfiguration($configuration, $config);
             }
         } finally {
             BaseNode::resetPlaceholders();

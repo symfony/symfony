8e0c8006d88429d129aebf3bbe2f776d11f76d8b
Chris Tanaskoski <chris@devristo.com>
2019-06-11 12:05:04 +0200

    [WIP][Mailer] Overwrite envelope sender and recipients from config

---
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Functional/MailerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Symfony\Bundle\FrameworkBundle\Tests\Functional;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\SentMessage;
+use Symfony\Component\Mailer\Transport\AbstractTransport;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+
+class MailerTest extends WebTestCase
+{
+    public function testEnvelopeListener()
+    {
+        self::bootKernel(['test_case' => 'Mailer']);
+
+        $onDoSend = function (SentMessage $message) {
+            $envelope = $message->getEnvelope();
+
+            $this->assertEquals(
+                [new Address('redirected@example.org')],
+                $envelope->getRecipients()
+            );
+
+            $this->assertEquals('sender@example.org', $envelope->getSender()->getAddress());
+        };
+
+        $eventDispatcher = self::$container->get(EventDispatcherInterface::class);
+        $logger = self::$container->get('logger');
+
+        $testTransport = new class($eventDispatcher, $logger, $onDoSend) extends AbstractTransport {
+            /**
+             * @var callable
+             */
+            private $onDoSend;
+
+            public function __construct(EventDispatcherInterface $eventDispatcher, LoggerInterface $logger, callable $onDoSend)
+            {
+                parent::__construct($eventDispatcher, $logger);
+                $this->onDoSend = $onDoSend;
+            }
+
+            protected function doSend(SentMessage $message): void
+            {
+                $onDoSend = $this->onDoSend;
+                $onDoSend($message);
+            }
+        };
+
+        $mailer = new Mailer($testTransport, null);
+
+        $message = (new Email())
+            ->subject('Test subject')
+            ->text('Hello world')
+            ->from('from@example.org')
+            ->to('to@example.org');
+
+        $mailer->send($message);
+    }
+}

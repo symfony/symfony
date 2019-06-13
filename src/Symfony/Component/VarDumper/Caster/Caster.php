a9d0038ec0042b39a96c0141e0d8c78a6fa3f362
Nicolas Grekas <nicolas.grekas@gmail.com>
2019-06-13 14:31:28 +0200

    [VarDumper] fix dumping objects that implement __debugInfo()

--- src/Symfony/Component/VarDumper/Caster/Caster.php
+++ src/Symfony/Component/VarDumper/Caster/Caster.php
@@ -53,13 +53,9 @@ class Caster
             $hasDebugInfo = $class->hasMethod('__debugInfo');
             $class = $class->name;
         }
-        if ($hasDebugInfo) {
-            $a = $obj->__debugInfo();
-        } elseif ($obj instanceof \Closure) {
-            $a = [];
-        } else {
-            $a = (array) $obj;
-        }
+
+        $a = $obj instanceof \Closure ? [] : (array) $obj;
+
         if ($obj instanceof \__PHP_Incomplete_Class) {
             return $a;
         }
@@ -90,6 +86,17 @@ class Caster
                     $keys[$i] = $k;
                 }
                 $a = array_combine($keys, $a);
+            }
+        }
+
+        if ($hasDebugInfo && \is_array($debugInfo = $obj->__debugInfo())) {
+            foreach ($debugInfo as $k => $v) {
+                if (!isset($k[0]) || "\0" !== $k[0]) {
+                    $k = self::PREFIX_VIRTUAL.$k;
+                }
+
+                unset($a[$k]);
+                $a[$k] = $v;
             }
         }
 

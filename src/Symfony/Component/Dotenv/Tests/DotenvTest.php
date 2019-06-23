196ee5599d9e86bcf0876674b50690b9b10ba351
Christian Flothmann <christian.flothmann@sensiolabs.de>
2019-06-23 10:10:04 +0200

    fix typos

--- src/Symfony/Component/Dotenv/Tests/DotenvTest.php
+++ src/Symfony/Component/Dotenv/Tests/DotenvTest.php
@@ -209,7 +209,7 @@ class DotenvTest extends TestCase
         $dotenv->load(__DIR__);
     }
 
-    public function testServerSuperglobalIsNotOverriden()
+    public function testServerSuperglobalIsNotOverridden()
     {
         $originalValue = $_SERVER['argc'];
 
@@ -219,7 +219,7 @@ class DotenvTest extends TestCase
         $this->assertSame($originalValue, $_SERVER['argc']);
     }
 
-    public function testEnvVarIsNotOverriden()
+    public function testEnvVarIsNotOverridden()
     {
         putenv('TEST_ENV_VAR=original_value');
         $_SERVER['TEST_ENV_VAR'] = 'original_value';
@@ -230,7 +230,7 @@ class DotenvTest extends TestCase
         $this->assertSame('original_value', getenv('TEST_ENV_VAR'));
     }
 
-    public function testHttpVarIsPartiallyOverriden()
+    public function testHttpVarIsPartiallyOverridden()
     {
         $_SERVER['HTTP_TEST_ENV_VAR'] = 'http_value';
 

eb15bffa78a74ad4c003e79f5b4bff958c3836fb
Fabien Potencier <fabien@potencier.org>
2019-06-27 11:08:50 +0200

    [Mailer] fixed error message when connecting to a stream raises an error before connect()

---
+++ src/Symfony/Component/Mailer/Tests/Transport/Smtp/Stream/SocketStreamTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Mailer\Tests\Transport\Smtp\Stream;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Mailer\Transport\Smtp\Stream\SocketStream;
+
+class SocketStreamTest extends TestCase
+{
+    /**
+     * @expectedException \Symfony\Component\Mailer\Exception\TransportException
+     * @expectedExceptionMessage Connection refused
+     */
+    public function testSocketErrorNoConnection()
+    {
+        $s = new SocketStream();
+        $s->setTimeout(0.1);
+        $s->setPort(9999);
+        $s->initialize();
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Mailer\Exception\TransportException
+     * @expectedExceptionMessage no valid certs found cafile stream
+     */
+    public function testSocketErrorBeforeConnectError()
+    {
+        $s = new SocketStream();
+        $s->setStreamOptions([
+            'ssl' => [
+                // not a CA file :)
+                'cafile' => __FILE__,
+            ],
+        ]);
+        $s->setEncryption('ssl');
+        $s->setHost('smtp.gmail.com');
+        $s->setPort(465);
+        $s->initialize();
+    }
+}

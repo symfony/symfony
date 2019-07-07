088615ddaf4f8d3817e6e12e9e818e163b8c07c3
Amrouche Hamza <laharl.moon@gmail.com>
2019-07-07 09:01:32 +0200

    [Translation] deprecate passing a null locale

--- src/Symfony/Component/Translation/Translator.php
+++ src/Symfony/Component/Translation/Translator.php
@@ -132,6 +132,10 @@ class Translator implements LegacyTranslatorInterface, TranslatorInterface, Tran
             $domain = 'messages';
         }
 
+        if (null === $locale) {
+            @trigger_error(sprintf('Passing "null" to the third argument of the "%s" method has been deprecated since Symfony 4.4 and will throw an error in 5.0.', __METHOD__), E_USER_DEPRECATED);
+        }
+
         $this->assertValidLocale($locale);
 
         $this->resources[$locale][] = [$format, $resource, $domain];

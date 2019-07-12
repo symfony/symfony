eae95c4e494558a9985b800c83b7bc1350021b4c
Valentin <udaltsov.valentin@gmail.com>
2019-07-13 00:04:51 +0300

    PHPDoc fixes

--- src/Symfony/Component/Form/Form.php
+++ src/Symfony/Component/Form/Form.php
@@ -140,9 +140,9 @@ class Form implements \IteratorAggregate, FormInterface
     private $lockSetData = false;
 
     /**
-     * @var string|int|null
+     * @var string
      */
-    private $name;
+    private $name = '';
 
     /**
      * @var bool Whether the form inherits its underlying data from its parent
@@ -211,7 +211,7 @@ class Form implements \IteratorAggregate, FormInterface
             return $this->propertyPath;
         }
 
-        if (null === $this->name || '' === $this->name) {
+        if ('' === $this->name) {
             return null;
         }
 

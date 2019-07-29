489f36daa2a0f36a37792901f4d6603c4589e8c1
Robin Chalas <robin.chalas@elao.com>
2019-07-29 12:49:22 +0200

    [Lock] Add return type to the new LockFactory

--- src/Symfony/Component/Lock/LockFactory.php
+++ src/Symfony/Component/Lock/LockFactory.php
@@ -19,4 +19,15 @@ namespace Symfony\Component\Lock;
  */
 class LockFactory extends Factory
 {
+    /**
+     * Creates a lock for the given resource.
+     *
+     * @param string     $resource    The resource to lock
+     * @param float|null $ttl         Maximum expected lock duration in seconds
+     * @param bool       $autoRelease Whether to automatically release the lock or not when the lock instance is destroyed
+     */
+    public function createLock($resource, $ttl = 300.0, $autoRelease = true): Lock
+    {
+        return parent::createLock($resource, $ttl, $autoRelease);
+    }
 }

2523451be0e5be566942b2cc72126fd1b1bcbafd
Nicolas Grekas <nicolas.grekas@gmail.com>
2019-08-01 12:27:54 +0200

    fix tests

--- src/Symfony/Component/HttpFoundation/Tests/JsonResponseTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/JsonResponseTest.php
@@ -12,10 +12,13 @@
 namespace Symfony\Component\HttpFoundation\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\PhpUnit\ForwardCompatTestTrait;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class JsonResponseTest extends TestCase
 {
+    use ForwardCompatTestTrait;
+
     public function testConstructorEmptyCreatesJsonObject()
     {
         $response = new JsonResponse();

f6fae1c361a40c846a34bd09fd2f918010dc5929
Thomas Calvet <calvet.thomas@gmail.com>
2019-08-01 14:35:59 +0200

    Sync "not implementing the method" deprecations messages

--- src/Symfony/Component/Security/Core/Authentication/Token/Storage/TokenStorage.php
+++ src/Symfony/Component/Security/Core/Authentication/Token/Storage/TokenStorage.php
@@ -40,7 +40,7 @@ class TokenStorage implements TokenStorageInterface, ResetInterface
     public function setToken(TokenInterface $token = null)
     {
         if (null !== $token && !method_exists($token, 'getRoleNames')) {
-            @trigger_error(sprintf('Not implementing the getRoleNames() method in %s which implements %s is deprecated since Symfony 4.3.', \get_class($token), TokenInterface::class), E_USER_DEPRECATED);
+            @trigger_error(sprintf('Not implementing the "%s::getRoleNames()" method in "%s" is deprecated since Symfony 4.3.', TokenInterface::class, \get_class($token)), E_USER_DEPRECATED);
         }
 
         $this->token = $token;

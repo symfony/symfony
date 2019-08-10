7fb7f59385d9bdcc0399094c0f93012af0a87f62
Nicolas Grekas <nicolas.grekas@gmail.com>
2019-08-10 22:41:30 +0200

    cleanups

--- src/Symfony/Component/HttpFoundation/Tests/ResponseTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/ResponseTest.php
@@ -532,7 +532,6 @@ class ResponseTest extends ResponseTestCase
         $response->prepare($request);
         $this->assertEquals('', $response->getContent());
         $this->assertFalse($response->headers->has('Content-Type'));
-        $this->assertFalse($response->headers->has('Content-Type'));
 
         $response->setContent('content');
         $response->setStatusCode(304);

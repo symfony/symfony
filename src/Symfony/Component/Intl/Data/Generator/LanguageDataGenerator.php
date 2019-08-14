fbd4ce4c5c74347fe7657473c3246cd6a68fe332
Roland Franssen <franssen.roland@gmail.com>
2019-08-14 08:36:48 +0200

    [Intl] Explicit check

--- src/Symfony/Component/Intl/Data/Generator/LanguageDataGenerator.php
+++ src/Symfony/Component/Intl/Data/Generator/LanguageDataGenerator.php
@@ -167,9 +167,9 @@ class LanguageDataGenerator extends AbstractDataGenerator
         $aliases = iterator_to_array($metadataBundle['alias']['language']);
         $alpha2ToAlpha3 = [];
 
-        foreach ($aliases as $alias => $language) {
-            $language = $language['replacement'];
-            if (2 === \strlen($language) && 3 === \strlen($alias)) {
+        foreach ($aliases as $alias => $data) {
+            $language = $data['replacement'];
+            if (2 === \strlen($language) && 3 === \strlen($alias) && 'overlong' === $data['reason']) {
                 if (isset(self::$preferredAlpha2ToAlpha3Mapping[$language])) {
                     // Validate to prevent typos
                     if (!isset($aliases[self::$preferredAlpha2ToAlpha3Mapping[$language]])) {

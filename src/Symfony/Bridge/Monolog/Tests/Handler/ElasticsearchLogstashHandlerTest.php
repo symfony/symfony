1f4ca61408e6f3c4a6c0be2965d075a7dbfc5b9c
Grégoire Pineau <lyrixx@lyrixx.info>
2019-08-19 15:23:29 +0200

    [Monolog] Fixed ElasticsearchLogstashHandler with monolog 2+

--- src/Symfony/Bridge/Monolog/Tests/Handler/ElasticsearchLogstashHandlerTest.php
+++ src/Symfony/Bridge/Monolog/Tests/Handler/ElasticsearchLogstashHandlerTest.php
@@ -27,10 +27,16 @@ class ElasticsearchLogstashHandlerTest extends TestCase
         $responseFactory = function ($method, $url, $options) use (&$callCount) {
             $body = <<<EOBODY
 {"index":{"_index":"log","_type":"_doc"}}
-{"@timestamp":"2020-01-01T00:00:00.000000+01:00","@version":1,"host":"my hostname","message":"My info message","type":"application","channel":"app","level":"INFO"}
+{"@timestamp":"2020-01-01T00:00:00.000000+01:00","@version":1,"host":"my hostname","message":"My info message","type":"application","channel":"app","level":"INFO","monolog_level":200}
 
 
 EOBODY;
+
+            // Monolog 1X
+            if (\defined(LogstashFormatter::class.'::V1')) {
+                $body = str_replace(',"monolog_level":200', '', $body);
+                $body = str_replace(',"monolog_level":300', '', $body);
+            }
 
             $this->assertSame('POST', $method);
             $this->assertSame('http://es:9200/_bulk', $url);
@@ -64,13 +70,19 @@ EOBODY;
         $responseFactory = function ($method, $url, $options) use (&$callCount) {
             $body = <<<EOBODY
 {"index":{"_index":"log","_type":"_doc"}}
-{"@timestamp":"2020-01-01T00:00:00.000000+01:00","@version":1,"host":"my hostname","message":"My info message","type":"application","channel":"app","level":"INFO"}
+{"@timestamp":"2020-01-01T00:00:00.000000+01:00","@version":1,"host":"my hostname","message":"My info message","type":"application","channel":"app","level":"INFO","monolog_level":200}
 
 {"index":{"_index":"log","_type":"_doc"}}
-{"@timestamp":"2020-01-01T00:00:01.000000+01:00","@version":1,"host":"my hostname","message":"My second message","type":"application","channel":"php","level":"WARNING"}
+{"@timestamp":"2020-01-01T00:00:01.000000+01:00","@version":1,"host":"my hostname","message":"My second message","type":"application","channel":"php","level":"WARNING","monolog_level":300}
 
 
 EOBODY;
+
+            // Monolog 1X
+            if (\defined(LogstashFormatter::class.'::V1')) {
+                $body = str_replace(',"monolog_level":200', '', $body);
+                $body = str_replace(',"monolog_level":300', '', $body);
+            }
 
             $this->assertSame('POST', $method);
             $this->assertSame('http://es:9200/_bulk', $url);
@@ -114,6 +126,12 @@ class ElasticsearchLogstashHandlerWithHardCodedHostname extends ElasticsearchLog
 {
     protected function getDefaultFormatter(): FormatterInterface
     {
-        return new LogstashFormatter('application', 'my hostname', null, 'ctxt_', LogstashFormatter::V1);
+        // Monolog 1.X
+        if (\defined(LogstashFormatter::class.'::V1')) {
+            return new LogstashFormatter('application', 'my hostname', null, 'ctxt_', LogstashFormatter::V1);
+        }
+
+        // Monolog 2.X
+        return new LogstashFormatter('application', 'my hostname');
     }
 }

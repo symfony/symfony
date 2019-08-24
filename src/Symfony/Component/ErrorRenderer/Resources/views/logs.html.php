96a81bd7ad8899f64aad1e537d5e3bc2c2f3048e
Yonel Ceruto <yonelceruto@gmail.com>
2019-08-24 19:32:44 -0400

    Fix closing tag in logs table

--- src/Symfony/Component/ErrorRenderer/Resources/views/logs.html.php
+++ src/Symfony/Component/ErrorRenderer/Resources/views/logs.html.php
@@ -19,7 +19,7 @@
             $severity = $log['context']['exception']['severity'] ?? false;
             $status = E_DEPRECATED === $severity || E_USER_DEPRECATED === $severity ? 'warning' : 'normal';
         } ?>
-        <tr class="status-<?= $status; ?>" data-filter-level="<?= strtolower($this->escape($log['priorityName'])); ?>"<?php if ($channelIsDefined) { ?> data-filter-channel="<?= $this->escape($log['channel']); ?>"<?php } ?>
+        <tr class="status-<?= $status; ?>" data-filter-level="<?= strtolower($this->escape($log['priorityName'])); ?>"<?php if ($channelIsDefined) { ?> data-filter-channel="<?= $this->escape($log['channel']); ?>"<?php } ?>>
             <td class="text-small" nowrap>
                 <span class="colored text-bold"><?= $this->escape($log['priorityName']); ?></span>
                 <span class="text-muted newline"><?= date('H:i:s', $log['timestamp']); ?></span>

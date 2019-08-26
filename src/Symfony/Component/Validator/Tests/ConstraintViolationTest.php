5a6558b7fa21f337eb32825cf170f3406e3a79c6
Nicolas Grekas <nicolas.grekas@gmail.com>
2019-08-26 11:20:32 +0200

    fix merge

--- src/Symfony/Component/Validator/Tests/ConstraintViolationTest.php
+++ src/Symfony/Component/Validator/Tests/ConstraintViolationTest.php
@@ -13,6 +13,8 @@ namespace Symfony\Component\Validator\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Tests\Fixtures\CustomArrayObject;
+use Symfony\Component\Validator\Tests\Fixtures\ToString;
 
 class ConstraintViolationTest extends TestCase
 {

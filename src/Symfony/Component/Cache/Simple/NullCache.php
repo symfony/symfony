92ac848c576ec55a60d5b1bf33dbea466150ebf4
Fabien Potencier <fabien@potencier.org>
2019-08-28 17:44:01 +0200

    bug #33370 Fix import statement typo in NullCache (adrienbrault)
    This PR was merged into the 4.4 branch.
    
    Discussion
    ----------
    
    Fix import statement typo in NullCache
    
    | Q             | A
    | ------------- | ---
    | Branch?       | 4.4
    | Bug fix?      | yes ?
    | New feature?  | no
    | BC breaks?    | no
    | Deprecations? | no
    | Tests pass?   | yes
    | Fixed tickets |
    | License       | MIT
    | Doc PR        | N/A
    
    Commits
    -------
    
    0a3bb6d05a Fix import statement typo in NullCache

--- src/Symfony/Component/Cache/Simple/NullCache.php
+++ src/Symfony/Component/Cache/Simple/NullCache.php
@@ -12,7 +12,7 @@
 namespace Symfony\Component\Cache\Simple;
 
 use Psr\SimpleCache\CacheInterface as Psr16CacheInterface;
-use Symfony\Components\Cache\Adapter\NullAdapter;
+use Symfony\Component\Cache\Adapter\NullAdapter;
 use Symfony\Contracts\Cache\CacheInterface;
 
 @trigger_error(sprintf('The "%s" class is deprecated since Symfony 4.3, use "%s" and type-hint for "%s" instead.', NullCache::class, NullAdapter::class, CacheInterface::class), E_USER_DEPRECATED);

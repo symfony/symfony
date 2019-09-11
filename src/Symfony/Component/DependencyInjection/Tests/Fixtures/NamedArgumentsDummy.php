6612e9a9392d0e5c6108e9fd8c81fbbaca7862a1
Thomas Calvet <calvet.thomas@gmail.com>
2019-09-11 09:35:13 +0200

    [DependencyInjection] Accept existing interfaces as valid named args

--- src/Symfony/Component/DependencyInjection/Tests/Fixtures/NamedArgumentsDummy.php
+++ src/Symfony/Component/DependencyInjection/Tests/Fixtures/NamedArgumentsDummy.php
@@ -2,12 +2,14 @@
 
 namespace Symfony\Component\DependencyInjection\Tests\Fixtures;
 
+use Psr\Container\ContainerInterface;
+
 /**
  * @author Kévin Dunglas <dunglas@gmail.com>
  */
 class NamedArgumentsDummy
 {
-    public function __construct(CaseSensitiveClass $c, $apiKey, $hostName)
+    public function __construct(CaseSensitiveClass $c, $apiKey, $hostName, ContainerInterface $interface)
     {
     }
 

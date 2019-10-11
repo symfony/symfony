e2648ef36d4613d8dda32133803eaf604429bef8
Quentin Dequippe <quentin@dequippe.tech>
2019-10-11 16:17:34 +0200

    Add AllMySMS bridge to Notifier component

---
+++ src/Symfony/Component/Notifier/Bridge/AllMySMS/AllMySMSTransport.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Symfony\Component\Notifier\Bridge\AllMySMS;
+
+use Symfony\Component\Notifier\Exception\LogicException;
+use Symfony\Component\Notifier\Exception\TransportException;
+use Symfony\Component\Notifier\Message\MessageInterface;
+use Symfony\Component\Notifier\Message\SmsMessage;
+use Symfony\Component\Notifier\Transport\AbstractTransport;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+/**
+ * @author Quentin Dequippe <quentin@dequippe.tech>
+ *
+ * @experimental in 5.0
+ */
+final class AllMySMSTransport extends AbstractTransport
+{
+    protected const HOST = 'api.allmysms.com';
+
+    private $login;
+    private $apiKey;
+    private $tpoa;
+
+    public function __construct(string $login, string $apiKey, string $tpoa = null, HttpClientInterface $client = null, EventDispatcherInterface $dispatcher = null)
+    {
+        $this->login = $login;
+        $this->apiKey = $apiKey;
+        $this->tpoa = $tpoa;
+
+        parent::__construct($client, $dispatcher);
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('allmysms://%s?from=%s', $this->getEndpoint(), $this->tpoa);
+    }
+
+    public function supports(MessageInterface $message): bool
+    {
+        return $message instanceof SmsMessage;
+    }
+
+    protected function doSend(MessageInterface $message): void
+    {
+        if (!$message instanceof SmsMessage) {
+            throw new LogicException(sprintf('The "%s" transport only supports instances of "%s" (instance of "%s" given).', __CLASS__, SmsMessage::class, \get_class($message)));
+        }
+
+        $endpoint = sprintf('https://%s/http/9.0/', $this->getEndpoint());
+        $response = $this->client->request('POST', $endpoint, [
+            'body' => [
+                'login' => $this->login,
+                'apiKey' => $this->apiKey,
+                'tpoa' => $this->tpoa,
+                'mobile' => $message->getPhone(),
+                'message' => $message->getSubject(),
+            ],
+        ]);
+
+        $result = $response->toArray();
+        if ($result['status'] != 100) {
+            throw new TransportException(sprintf('Unable to send the SMS: %s (%s).', $result['statusText'], $result['status']), $response);
+        }
+    }
+}
---
+++ src/Symfony/Component/Notifier/Bridge/AllMySMS/AllMySMSTransportFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Symfony\Component\Notifier\Bridge\AllMySMS;
+
+use Symfony\Component\Notifier\Exception\UnsupportedSchemeException;
+use Symfony\Component\Notifier\Transport\AbstractTransportFactory;
+use Symfony\Component\Notifier\Transport\Dsn;
+use Symfony\Component\Notifier\Transport\TransportInterface;
+
+/**
+ * @author Quentin Dequippe <quentin@dequippe.tech>
+ *
+ * @experimental in 5.0
+ */
+final class AllMySMSTransportFactory extends AbstractTransportFactory
+{
+    public function create(Dsn $dsn): TransportInterface
+    {
+        $scheme = $dsn->getScheme();
+        $login = $this->getUser($dsn);
+        $apiKey = $this->getPassword($dsn);
+        $host = 'default' === $dsn->getHost() ? null : $dsn->getHost();
+        $port = $dsn->getPort();
+
+        if ('allmysms' === $scheme) {
+            return (new AllMySMSTransport($login, $apiKey, $this->client, $this->dispatcher))->setHost($host)->setPort($port);
+        }
+
+        throw new UnsupportedSchemeException($dsn, 'allmysms', $this->getSupportedSchemes());
+    }
+
+    protected function getSupportedSchemes(): array
+    {
+        return ['allmysms'];
+    }
+}

3bed0247c0b250c356ed9cc5b38c5fbaf23fa9a8
Denys Voronin <hurricane.voronin@gmail.com>
2019-10-16 13:30:11 +0300

    [Config] Disable default alphabet sorting in glob function due of unstable sort

--- src/Symfony/Component/Config/Resource/GlobResource.php
+++ src/Symfony/Component/Config/Resource/GlobResource.php
@@ -100,7 +100,9 @@ class GlobResource implements \IteratorAggregate, SelfCheckingResourceInterface,
         }
 
         if (0 !== strpos($this->prefix, 'phar://') && false === strpos($this->pattern, '/**/') && (\defined('GLOB_BRACE') || false === strpos($this->pattern, '{'))) {
-            foreach (glob($this->prefix.$this->pattern, \defined('GLOB_BRACE') ? GLOB_BRACE : 0) as $path) {
+            $paths = glob($this->prefix.$this->pattern, GLOB_NOSORT | (\defined('GLOB_BRACE') ? GLOB_BRACE : 0));
+            sort($paths);
+            foreach ($paths as $path) {
                 if ($this->recursive && is_dir($path)) {
                     $files = iterator_to_array(new \RecursiveIteratorIterator(
                         new \RecursiveCallbackFilterIterator(

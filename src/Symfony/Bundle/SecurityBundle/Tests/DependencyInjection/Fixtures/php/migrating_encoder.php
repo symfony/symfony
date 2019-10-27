80955be45d905a17075ed4b8043dc3007154d91b
Robin Chalas <robin.chalas@gmail.com>
2019-10-27 11:19:19 +0100

    [Security] Add migrating encoder configuration

---
+++ src/Symfony/Bundle/SecurityBundle/Tests/DependencyInjection/Fixtures/php/migrating_encoder.php
@@ -0,0 +1,14 @@
+<?php
+
+$this->load('container1.php', $container);
+
+$container->loadFromExtension('security', [
+    'encoders' => [
+        'JMS\FooBundle\Entity\User7' => [
+            'algorithm' => 'argon2i',
+            'memory_cost' => 256,
+            'time_cost' => 1,
+            'migrate_from' => 'bcrypt',
+        ],
+    ],
+]);

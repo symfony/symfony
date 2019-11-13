3fcf543f757b0355dfa9bf2690bcd74f5c5d1e60
Robin Chalas <robin.chalas@gmail.com>
2019-11-13 06:32:44 +0100

    [Serializer] Fix ProblemNormalizer signature mismatch

--- src/Symfony/Component/Serializer/Normalizer/ProblemNormalizer.php
+++ src/Symfony/Component/Serializer/Normalizer/ProblemNormalizer.php
@@ -38,7 +38,7 @@ class ProblemNormalizer implements NormalizerInterface, CacheableSupportsMethodI
     /**
      * {@inheritdoc}
      */
-    public function normalize($exception, $format = null, array $context = [])
+    public function normalize($exception, string $format = null, array $context = [])
     {
         $context += $this->defaultContext;
 
@@ -59,7 +59,7 @@ class ProblemNormalizer implements NormalizerInterface, CacheableSupportsMethodI
     /**
      * {@inheritdoc}
      */
-    public function supportsNormalization($data, $format = null): bool
+    public function supportsNormalization($data, string $format = null): bool
     {
         return $data instanceof FlattenException;
     }

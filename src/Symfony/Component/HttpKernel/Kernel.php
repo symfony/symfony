44d79c4a574c0e2ee6d91baac1d438fb7522295e
Fabien Potencier <fabien@potencier.org>
2019-11-17 21:26:07 +0100

    Fix version in Kernel

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -68,15 +68,15 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
 
     private static $freshCache = [];
 
-    const VERSION = '5.0.0-DEV';
-    const VERSION_ID = 50000;
+    const VERSION = '5.1.0-DEV';
+    const VERSION_ID = 50100;
     const MAJOR_VERSION = 5;
-    const MINOR_VERSION = 0;
+    const MINOR_VERSION = 1;
     const RELEASE_VERSION = 0;
     const EXTRA_VERSION = 'DEV';
 
-    const END_OF_MAINTENANCE = '07/2020';
-    const END_OF_LIFE = '07/2020';
+    const END_OF_MAINTENANCE = '01/2021';
+    const END_OF_LIFE = '01/2021';
 
     public function __construct(string $environment, bool $debug)
     {

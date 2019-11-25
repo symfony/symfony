6958d77f0cfd36e7a83cca0fcd5a7968a6c03c34
Christian Flothmann <christian.flothmann@sensiolabs.de>
2019-11-25 16:00:32 +0100

    do not depend on the QueryBuilder from the ORM

--- src/Symfony/Bridge/Doctrine/Form/Type/EntityType.php
+++ src/Symfony/Bridge/Doctrine/Form/Type/EntityType.php
@@ -53,6 +53,10 @@ class EntityType extends DoctrineType
      */
     public function getLoader(ObjectManager $manager, $queryBuilder, $class)
     {
+        if (!$queryBuilder instanceof QueryBuilder) {
+            throw new \TypeError(sprintf('Expected an instance of %s, but got %s.', QueryBuilder::class, \is_object($queryBuilder) ? \get_class($queryBuilder) : \gettype($queryBuilder)));
+        }
+
         return new ORMQueryBuilderLoader($queryBuilder);
     }
 
@@ -68,11 +72,17 @@ class EntityType extends DoctrineType
      * We consider two query builders with an equal SQL string and
      * equal parameters to be equal.
      *
+     * @param QueryBuilder $queryBuilder
+     *
      * @internal This method is public to be usable as callback. It should not
      *           be used in user code.
      */
-    public function getQueryBuilderPartsForCachingHash(QueryBuilder $queryBuilder): ?array
+    public function getQueryBuilderPartsForCachingHash($queryBuilder): ?array
     {
+        if (!$queryBuilder instanceof QueryBuilder) {
+            throw new \TypeError(sprintf('Expected an instance of %s, but got %s.', QueryBuilder::class, \is_object($queryBuilder) ? \get_class($queryBuilder) : \gettype($queryBuilder)));
+        }
+
         return [
             $queryBuilder->getQuery()->getSQL(),
             array_map([$this, 'parameterToArray'], $queryBuilder->getParameters()->toArray()),

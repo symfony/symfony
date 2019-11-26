85750115adb12ec2da7ca46db318c573a24bb49a
Thomas ANDRE <thomandre@gmail.com>
2019-11-26 10:06:12 +0100

    fixing coding standards

--- src/Symfony/Component/Serializer/Normalizer/AbstractObjectNormalizer.php
+++ src/Symfony/Component/Serializer/Normalizer/AbstractObjectNormalizer.php
@@ -152,7 +152,7 @@ abstract class AbstractObjectNormalizer extends AbstractNormalizer
         if ((isset($this->context['lastCircularObjectFound']) && $this->context['lastCircularObjectFound'] == \get_class($object))
             || $this->isCircularReference($object, $context)) {
             $this->context['lastCircularObjectFound'] = \get_class($object);
-        
+
             return $this->handleCircularReference($object, $format, $context);
         }
 

38493b3e4beef88be74f2510c457302e7e48f4ae
Yonel Ceruto <yonelceruto@gmail.com>
2019-11-25 19:08:07 -0500

    Add preview mode support for Html and Serializer error renderers

--- src/Symfony/Component/ErrorHandler/ErrorRenderer/SerializerErrorRenderer.php
+++ src/Symfony/Component/ErrorHandler/ErrorRenderer/SerializerErrorRenderer.php
@@ -26,19 +26,26 @@ class SerializerErrorRenderer implements ErrorRendererInterface
     private $serializer;
     private $format;
     private $fallbackErrorRenderer;
+    private $debug;
 
     /**
      * @param string|callable(FlattenException) $format The format as a string or a callable that should return it
+     * @param bool|callable                     $debug  The debugging mode as a boolean or a callable that should return it
      */
-    public function __construct(SerializerInterface $serializer, $format, ErrorRendererInterface $fallbackErrorRenderer = null)
+    public function __construct(SerializerInterface $serializer, $format, ErrorRendererInterface $fallbackErrorRenderer = null, $debug = false)
     {
         if (!\is_string($format) && !\is_callable($format)) {
             throw new \TypeError(sprintf('Argument 2 passed to %s() must be a string or a callable, %s given.', __METHOD__, \is_object($format) ? \get_class($format) : \gettype($format)));
         }
 
+        if (!\is_bool($debug) && !\is_callable($debug)) {
+            throw new \TypeError(sprintf('Argument 4 passed to %s() must be a boolean or a callable, %s given.', __METHOD__, \is_object($debug) ? \get_class($debug) : \gettype($debug)));
+        }
+
         $this->serializer = $serializer;
         $this->format = $format;
         $this->fallbackErrorRenderer = $fallbackErrorRenderer ?? new HtmlErrorRenderer();
+        $this->debug = $debug;
     }
 
     /**
@@ -51,7 +58,10 @@ class SerializerErrorRenderer implements ErrorRendererInterface
         try {
             $format = \is_string($this->format) ? $this->format : ($this->format)($flattenException);
 
-            return $flattenException->setAsString($this->serializer->serialize($flattenException, $format, ['exception' => $exception]));
+            return $flattenException->setAsString($this->serializer->serialize($flattenException, $format, [
+                'exception' => $exception,
+                'debug' => \is_bool($this->debug) ? $this->debug : ($this->debug)($exception),
+            ]));
         } catch (NotEncodableValueException $e) {
             return $this->fallbackErrorRenderer->render($exception);
         }

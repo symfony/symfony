f3d3f540a523439d97953a30379c0cba234fc705
Tim Spratt <timspratt@hotmail.co.uk>
2019-11-30 11:17:33 +0000

    add parameter type declaration
    to conform to changes made: https://github.com/symfony/translation-contracts/commit/8feb81e6bb1a42d6a3b1429c751d291eb6d05297

--- src/Symfony/Component/Translation/TranslatorInterface.php
+++ src/Symfony/Component/Translation/TranslatorInterface.php
@@ -59,7 +59,7 @@ interface TranslatorInterface extends LocaleAwareInterface
      *
      * @throws InvalidArgumentException If the locale contains invalid characters
      */
-    public function setLocale($locale);
+    public function setLocale(string $locale);
 
     /**
      * Returns the current locale.

5009b8a838135dc8eb2cc3c3a82489312a4f0c79
Fabien Potencier <fabien@potencier.org>
2019-12-01 15:06:38 +0100

    updated VERSION for 4.4.1

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -76,12 +76,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
 
     private static $freshCache = [];
 
-    const VERSION = '4.4.1-DEV';
+    const VERSION = '4.4.1';
     const VERSION_ID = 40401;
     const MAJOR_VERSION = 4;
     const MINOR_VERSION = 4;
     const RELEASE_VERSION = 1;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     const END_OF_MAINTENANCE = '11/2022';
     const END_OF_LIFE = '11/2023';

b33b9a6ad91703f6ec6997aa16943cc9ffe19851
Nicolas Grekas <nicolas.grekas@gmail.com>
2019-12-02 15:08:02 +0100

    [DI] Fix making the container path-independent when the app is in /app

--- src/Symfony/Component/DependencyInjection/Dumper/PhpDumper.php
+++ src/Symfony/Component/DependencyInjection/Dumper/PhpDumper.php
@@ -173,14 +173,14 @@ class PhpDumper extends Dumper
         if (!empty($options['file']) && is_dir($dir = \dirname($options['file']))) {
             // Build a regexp where the first root dirs are mandatory,
             // but every other sub-dir is optional up to the full path in $dir
-            // Mandate at least 2 root dirs and not more that 5 optional dirs.
+            // Mandate at least 1 root dir and not more than 5 optional dirs.
 
             $dir = explode(\DIRECTORY_SEPARATOR, realpath($dir));
             $i = \count($dir);
 
-            if (3 <= $i) {
+            if (2 + (int) ('\\' === \DIRECTORY_SEPARATOR) <= $i) {
                 $regex = '';
-                $lastOptionalDir = $i > 8 ? $i - 5 : 3;
+                $lastOptionalDir = $i > 8 ? $i - 5 : (2 + (int) ('\\' === \DIRECTORY_SEPARATOR));
                 $this->targetDirMaxMatches = $i - $lastOptionalDir;
 
                 while (--$i >= $lastOptionalDir) {

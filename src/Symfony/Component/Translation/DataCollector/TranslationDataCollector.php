6fd266dba65690eaf4106f33940a4cc811050848
Olivier Dolbeau <git@a.bbnt.me>
2019-12-27 12:18:21 +0100

    Add missing use statement

--- src/Symfony/Component/Translation/DataCollector/TranslationDataCollector.php
+++ src/Symfony/Component/Translation/DataCollector/TranslationDataCollector.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector;
 use Symfony\Component\HttpKernel\DataCollector\LateDataCollectorInterface;
 use Symfony\Component\Translation\DataCollectorTranslator;
+use Symfony\Component\VarDumper\Cloner\Data;
 
 /**
  * @author Abdellatif Ait boudad <a.aitboudad@gmail.com>

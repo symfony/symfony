df30a176ac6e9e8e886385483a3a8ed365c23d93
azjezz <azjezz@protonmail.com>
2020-01-03 13:06:55 +0100

    [Mailer] add tests for http transports

--- src/Symfony/Component/Mailer/Bridge/Amazon/Tests/Transport/SesHttpTransportTest.php
+++ src/Symfony/Component/Mailer/Bridge/Amazon/Tests/Transport/SesHttpTransportTest.php
@@ -12,7 +12,13 @@
 namespace Symfony\Component\Mailer\Bridge\Amazon\Tests\Transport;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
 use Symfony\Component\Mailer\Bridge\Amazon\Transport\SesHttpTransport;
+use Symfony\Component\Mailer\Exception\HttpTransportException;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class SesHttpTransportTest extends TestCase
 {
@@ -44,5 +50,71 @@ class SesHttpTransportTest extends TestCase
                 'ses+https://ACCESS_KEY@example.com:99',
             ],
         ];
+    }
+
+    public function testSend()
+    {
+        $client = new MockHttpClient(function (string $method, string $url, array $options): ResponseInterface {
+            $this->assertSame('POST', $method);
+            $this->assertSame('https://email.eu-west-1.amazonaws.com:8984/', $url);
+            $this->assertStringContainsString('AWS3-HTTPS AWSAccessKeyId=ACCESS_KEY,Algorithm=HmacSHA256,Signature=', $options['headers'][0] ?? $options['request_headers'][0]);
+
+            parse_str($options['body'], $body);
+            $content = base64_decode($body['RawMessage_Data']);
+
+            $this->assertStringContainsString('Hello!', $content);
+            $this->assertStringContainsString('Saif Eddin <saif.gmati@symfony.com>', $content);
+            $this->assertStringContainsString('Fabien <fabpot@symfony.com>', $content);
+            $this->assertStringContainsString('Hello There!', $content);
+
+            $xml = '<SendEmailResponse xmlns="https://email.amazonaws.com/doc/2010-03-31/">
+  <SendRawEmailResult>
+    <MessageId>foobar</MessageId>
+  </SendRawEmailResult>
+</SendEmailResponse>';
+
+            return new MockResponse($xml, [
+                'http_code' => 200,
+            ]);
+        });
+        $transport = new SesHttpTransport('ACCESS_KEY', 'SECRET_KEY', null, $client);
+        $transport->setPort(8984);
+
+        $mail = new Email();
+        $mail->subject('Hello!')
+            ->to(new Address('saif.gmati@symfony.com', 'Saif Eddin'))
+            ->from(new Address('fabpot@symfony.com', 'Fabien'))
+            ->text('Hello There!');
+
+        $message = $transport->send($mail);
+
+        $this->assertSame('foobar', $message->getMessageId());
+    }
+
+    public function testSendThrowsForErrorResponse()
+    {
+        $client = new MockHttpClient(function (string $method, string $url, array $options): ResponseInterface {
+            $xml = "<SendEmailResponse xmlns=\"https://email.amazonaws.com/doc/2010-03-31/\">
+                <Error>
+                    <Message>i'm a teapot</Message>
+                    <Code>418</Code>
+                </Error>
+            </SendEmailResponse>";
+
+            return new MockResponse($xml, [
+                'http_code' => 418,
+            ]);
+        });
+        $transport = new SesHttpTransport('ACCESS_KEY', 'SECRET_KEY', null, $client);
+
+        $mail = new Email();
+        $mail->subject('Hello!')
+            ->to(new Address('saif.gmati@symfony.com', 'Saif Eddin'))
+            ->from(new Address('fabpot@symfony.com', 'Fabien'))
+            ->text('Hello There!');
+
+        $this->expectException(HttpTransportException::class);
+        $this->expectExceptionMessage('Unable to send an email: i\'m a teapot (code 418).');
+        $transport->send($mail);
     }
 }

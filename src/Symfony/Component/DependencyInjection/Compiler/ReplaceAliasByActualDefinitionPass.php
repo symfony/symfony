1df7b0f9e7e401fef9dd9249d6744bb0dad93ed7
Jefersson Nathan <malukenho.dev@gmail.com>
2020-01-10 14:24:25 +0100

    Enable expressions for aliases
    It may be useful to have expression enabled for aliases. It turns out
    that on the development proccess, it sometimes is too much to have
    factories that doesn't create anything, but just decide which object to
    return.
    
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- src/Symfony/Component/DependencyInjection/Compiler/ReplaceAliasByActualDefinitionPass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/ReplaceAliasByActualDefinitionPass.php
@@ -11,10 +11,20 @@
 
 namespace Symfony\Component\DependencyInjection\Compiler;
 
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
 use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+use Symfony\Component\DependencyInjection\ExpressionLanguage;
 use Symfony\Component\DependencyInjection\Reference;
+use function class_exists;
+use function sprintf;
+use function stripcslashes;
+use function substr;
+use function substr_replace;
 
 /**
  * Replaces aliases with actual service definitions, effectively removing these
@@ -25,6 +35,8 @@ use Symfony\Component\DependencyInjection\Reference;
 class ReplaceAliasByActualDefinitionPass extends AbstractRecursivePass
 {
     private $replacements;
+    private $expressionLanguageInstance;
+    private $inExpressionUsage = false;
 
     /**
      * Process the Container to replace aliases with service definitions.
@@ -53,6 +65,20 @@ class ReplaceAliasByActualDefinitionPass extends AbstractRecursivePass
             // Process new target
             $seenAliasTargets[$targetId] = true;
             try {
+                if (strpos($targetId, '=') === 0) {
+                    $service = trim($targetId, '=');
+
+                    $result = $this
+                        ->getExpressionLanguage()
+                        ->evaluate($service, ['this' => 'container', 'container' => $container]);
+
+                    $container->setAlias($targetId, \get_class($result))
+                        ->setPublic($target->isPublic())
+                        ->setPrivate($target->isPrivate());
+
+                    continue;
+                }
+
                 $definition = $container->getDefinition($targetId);
             } catch (ServiceNotFoundException $e) {
                 if ('' !== $e->getId() && '@' === $e->getId()[0]) {
@@ -90,5 +116,32 @@ class ReplaceAliasByActualDefinitionPass extends AbstractRecursivePass
         }
 
         return parent::processValue($value, $isRoot);
+    }
+
+    private function getExpressionLanguage(ContainerInterface $container): ExpressionLanguage
+    {
+        if (null === $this->expressionLanguageInstance) {
+            if (!class_exists(ExpressionLanguage::class)) {
+                throw new LogicException('Unable to use expressions as the Symfony ExpressionLanguage component is not installed.');
+            }
+
+            $providers = $container->getExpressionLanguageProviders();
+            $this->expressionLanguageInstance = new ExpressionLanguage(null, $providers, function (string $arg): string {
+                if ('""' === substr_replace($arg, '', 1, -1)) {
+                    $id = stripcslashes(substr($arg, 1, -1));
+                    $this->inExpressionUsage = true;
+                    $arg = $this->processValue(new Reference($id));
+                    $this->inExpressionUsage = false;
+                    if (!$arg instanceof Reference) {
+                        throw new RuntimeException(sprintf('"%s::processValue()" must return a Reference when processing an expression, %s returned for service("%s").', \get_class($this), \is_object($arg) ? \get_class($arg) : \gettype($arg), $id));
+                    }
+                    $arg = sprintf('"%s"', $arg);
+                }
+
+                return sprintf('$this->get(%s)', $arg);
+            });
+        }
+
+        return $this->expressionLanguageInstance;
     }
 }

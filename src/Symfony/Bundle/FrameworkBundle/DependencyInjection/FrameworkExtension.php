a5f74f02c42866ca771b0c0bce700d822526e92c
Hafsah Haynes <hafsah@surfsharkbiz.com>
2020-01-14 11:57:59 +0200

    [FrameworkBundle] Remove entities from class map as it is optional

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -325,7 +325,6 @@ class FrameworkExtension extends Extension
 
         $this->addAnnotatedClassesToCompile([
             '**\\Controller\\',
-            '**\\Entity\\',
 
             // Added explicitly so that we don't rely on the class map being dumped to make it work
             'Symfony\\Bundle\\FrameworkBundle\\Controller\\AbstractController',

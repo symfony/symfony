ce29631cd827634342ac5bd806c6809e6aed8d7e
Fabien Potencier <fabien@potencier.org>
2020-02-04 08:15:38 +0100

    [Translation] Add missing use statement

--- src/Symfony/Component/Translation/Command/XliffLintCommand.php
+++ src/Symfony/Component/Translation/Command/XliffLintCommand.php
@@ -17,6 +17,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Translation\Exception\InvalidArgumentException;
 
 /**
  * Validates XLIFF files syntax and outputs encountered errors.

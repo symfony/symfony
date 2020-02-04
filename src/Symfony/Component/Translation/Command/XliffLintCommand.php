00baa290e880a44d1c97e527024e175bc64dbe80
Fabien Potencier <fabien@potencier.org>
2020-02-04 08:15:38 +0100

    [Translation] Add missing use statement

--- src/Symfony/Component/Translation/Command/XliffLintCommand.php
+++ src/Symfony/Component/Translation/Command/XliffLintCommand.php
@@ -18,6 +18,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Translation\Exception\InvalidArgumentException;
 use Symfony\Component\Translation\Util\XliffUtils;
 
 /**

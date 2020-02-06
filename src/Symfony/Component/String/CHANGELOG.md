659cdf1871f8c7c3ea9305e172fbf9746d5c1f29
Thomas Calvet <calvet.thomas@gmail.com>
2020-02-06 18:44:01 +0100

    [String] Add the s() helper method

--- src/Symfony/Component/String/CHANGELOG.md
+++ src/Symfony/Component/String/CHANGELOG.md
@@ -8,6 +8,8 @@ CHANGELOG
  * made `AbstractString::width()` follow POSIX.1-2001
  * added `LazyString` which provides memoizing stringable objects
  * The component is not marked as `@experimental` anymore.
+ * Added the `s()` helper method to get either an `UnicodeString` or `ByteString` instance,
+   depending of the input string UTF-8 compliancy.
 
 5.0.0
 -----
---
+++ src/Symfony/Component/String/Tests/FunctionsTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\String\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\String\AbstractString;
+use Symfony\Component\String\ByteString;
+use function Symfony\Component\String\s;
+use Symfony\Component\String\UnicodeString;
+
+final class FunctionsTest extends TestCase
+{
+    /**
+     * @dataProvider provideS
+     */
+    public function testS(AbstractString $expected, string $input)
+    {
+        $this->assertEquals($expected, s($input));
+    }
+
+    public function provideS()
+    {
+        return [
+            [new UnicodeString('foo'), 'foo'],
+            [new UnicodeString('अनुच्छेद'), 'अनुच्छेद'],
+            [new ByteString("b\x80ar"), "b\x80ar"],
+            [new ByteString("\xfe\xff"), "\xfe\xff"],
+        ];
+    }
+}

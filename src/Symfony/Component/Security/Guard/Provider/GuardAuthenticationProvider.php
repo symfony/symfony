f10098e9f11a121080169218c0c4627127b2f857
Robin Chalas <robin.chalas@gmail.com>
2020-02-09 18:57:03 +0100

    [Security] Fix exception name in doc comments

--- src/Symfony/Component/Security/Guard/Provider/GuardAuthenticationProvider.php
+++ src/Symfony/Component/Security/Guard/Provider/GuardAuthenticationProvider.php
@@ -82,7 +82,7 @@ class GuardAuthenticationProvider implements AuthenticationProviderInterface
                 return $token;
             }
 
-            // this AccountStatusException causes the user to be logged out
+            // this AccountExpiredException causes the user to be logged out
             throw new AuthenticationExpiredException();
         }
 

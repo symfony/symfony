9b382590ee719a3aeaad11a91a1ff666c0da1a3a
Adam Prickett <adam.prickett@journolink.com>
2020-02-11 14:40:40 +0000

    [Console] Handle zero row count in appendRow() for Table

--- src/Symfony/Component/Console/Helper/Table.php
+++ src/Symfony/Component/Console/Helper/Table.php
@@ -601,7 +601,9 @@ class Table
             ++$numberOfRows; // Add row for header separator
         }
 
-        ++$numberOfRows; // Add row for footer separator
+        if (\count($this->rows) > 0) {
+            ++$numberOfRows; // Add row for footer separator
+        }
 
         return $numberOfRows;
     }

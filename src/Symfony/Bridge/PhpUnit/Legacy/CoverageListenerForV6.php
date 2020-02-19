034e1de6e66a964b3bd4d56bfd78b118b147bcce
Marcello Mönkemeyer <marcello@moenkemeyer.email>
2020-02-19 14:20:42 +0000

    [PhpUnitBridge] Use trait instead of extending deprecated class

--- src/Symfony/Bridge/PhpUnit/Legacy/CoverageListenerForV6.php
+++ src/Symfony/Bridge/PhpUnit/Legacy/CoverageListenerForV6.php
@@ -11,8 +11,9 @@
 
 namespace Symfony\Bridge\PhpUnit\Legacy;
 
-use PHPUnit\Framework\BaseTestListener;
 use PHPUnit\Framework\Test;
+use PHPUnit\Framework\TestListener;
+use PHPUnit\Framework\TestListenerDefaultImplementation;
 
 /**
  * CoverageListener adds `@covers <className>` on each test when possible to
@@ -22,8 +23,10 @@ use PHPUnit\Framework\Test;
  *
  * @internal
  */
-class CoverageListenerForV6 extends BaseTestListener
+class CoverageListenerForV6 implements TestListener
 {
+    use TestListenerDefaultImplementation;
+
     private $trait;
 
     public function __construct(callable $sutFqcnResolver = null, $warningOnSutNotFound = false)

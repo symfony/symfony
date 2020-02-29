9e1fab60f5601a9dc440c9dc0fa8dd618fb3939b
Fabien Potencier <fabien@potencier.org>
2020-02-29 11:41:30 +0100

    updated VERSION for 5.0.5

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -68,12 +68,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
 
     private static $freshCache = [];
 
-    const VERSION = '5.0.5-DEV';
+    const VERSION = '5.0.5';
     const VERSION_ID = 50005;
     const MAJOR_VERSION = 5;
     const MINOR_VERSION = 0;
     const RELEASE_VERSION = 5;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     const END_OF_MAINTENANCE = '07/2020';
     const END_OF_LIFE = '07/2020';

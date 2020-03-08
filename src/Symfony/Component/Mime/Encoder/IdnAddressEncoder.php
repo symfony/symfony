a85fb6c496fd3983d07fe7378f135e7898d1ecc9
David Maicher <mail@dmaicher.de>
2020-03-08 11:36:03 +0100

    [Mime] remove phpdoc mentioning Utf8AddressEncoder
    that class does not exist.

--- src/Symfony/Component/Mime/Encoder/IdnAddressEncoder.php
+++ src/Symfony/Component/Mime/Encoder/IdnAddressEncoder.php
@@ -20,9 +20,7 @@ use Symfony\Component\Mime\Exception\AddressEncoderException;
  * SMTP servers.
  *
  * This encoder does not support email addresses with non-ASCII characters in
- * local-part (the substring before @). To send to such addresses, use
- * Utf8AddressEncoder together with SmtpUtf8Handler. Your outbound SMTP server must support
- * the SMTPUTF8 extension.
+ * local-part (the substring before @).
  *
  * @author Christian Schmidt
  */

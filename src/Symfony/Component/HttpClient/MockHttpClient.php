c79fe029e75ccd5dad3766ca8c1f3a81b6308713
Fabien Potencier <fabien@potencier.org>
2020-03-16 17:13:17 +0100

    Fix more quotes in exception messages

--- src/Symfony/Component/HttpClient/MockHttpClient.php
+++ src/Symfony/Component/HttpClient/MockHttpClient.php
@@ -79,7 +79,7 @@ class MockHttpClient implements HttpClientInterface
         if ($responses instanceof ResponseInterface) {
             $responses = [$responses];
         } elseif (!is_iterable($responses)) {
-            throw new \TypeError(sprintf('%s() expects parameter 1 to be an iterable of MockResponse objects, "%s" given.', __METHOD__, \is_object($responses) ? \get_class($responses) : \gettype($responses)));
+            throw new \TypeError(sprintf('"%s()" expects parameter 1 to be an iterable of MockResponse objects, "%s" given.', __METHOD__, \is_object($responses) ? \get_class($responses) : \gettype($responses)));
         }
 
         return new ResponseStream(MockResponse::stream($responses, $timeout));

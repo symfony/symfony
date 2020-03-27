6a97ffbe72b19b27929a348b067f6a9398c3f4ef
Guillaume Pédelagrabe <gpedelagrabe@gmail.com>
2020-03-27 19:46:45 +0100

    UidToStringTransformer

---
+++ src/Symfony/Component/Form/Extension/Core/DataTransformer/UidToStringTransformer.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Symfony\Component\Form\Extension\Core\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Symfony\Component\Uid\AbstractUid;
+use Symfony\Component\Uid\Ulid;
+use Symfony\Component\Uid\Uuid;
+
+class UidToStringTransformer implements DataTransformerInterface
+{
+    /**
+     * @param AbstractUid $uid An \AbstractUid object
+     *
+     * @return string A string representation of UUID or a ULID
+     *
+     * @throws TransformationFailedException If the given value is not a \AbstractUid
+     */
+    public function transform($uid)
+    {
+        if (null === $uid) {
+            return '';
+        }
+
+        if (!$uid instanceof AbstractUid) {
+            throw new TransformationFailedException('Expected an \AbstractUid.');
+        }
+
+        return (string) $uid;
+    }
+
+    /**
+     * @param string $value A string representation of UUID or a ULID
+     *
+     * @return AbstractUid|null An instance of AbstractUid
+     *
+     * @throws TransformationFailedException If the given value is not a string,
+     *                                       or could not be transformed
+     */
+    public function reverseTransform($value)
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if (!\is_string($value)) {
+            throw new TransformationFailedException('Expected a string.');
+        }
+
+        if (Uuid::isValid($value)) {
+            return Uuid::fromString($value);
+        }
+
+        if (Ulid::isValid($value)) {
+            return Ulid::fromString($value);
+        }
+
+        throw new TransformationFailedException('This value is not a valid string representation of a UUID or ULID.');
+    }
+}

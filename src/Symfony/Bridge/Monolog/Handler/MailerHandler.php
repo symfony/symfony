c2a1781eb48f98ed4f404b6889bb3e1e6de0fab3
Jordi Boggiano <j.boggiano@seld.be>
2020-04-03 15:33:33 +0200

    Fix $level type

--- src/Symfony/Bridge/Monolog/Handler/MailerHandler.php
+++ src/Symfony/Bridge/Monolog/Handler/MailerHandler.php
@@ -30,8 +30,9 @@ class MailerHandler extends AbstractProcessingHandler
 
     /**
      * @param callable|Email $messageTemplate
+     * @param string|int     $level           The minimum logging level at which this handler will be triggered
      */
-    public function __construct(MailerInterface $mailer, $messageTemplate, int $level = Logger::DEBUG, bool $bubble = true)
+    public function __construct(MailerInterface $mailer, $messageTemplate, $level = Logger::DEBUG, bool $bubble = true)
     {
         parent::__construct($level, $bubble);
 

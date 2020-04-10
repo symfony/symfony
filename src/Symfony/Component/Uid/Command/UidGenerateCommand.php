e8bf438b045271c324068e8a21d2ad5c7eefeea0
Guillaume Pédelagrabe <gpedelagrabe@gmail.com>
2020-04-10 22:54:34 +0200

    Move command to Uid Component.

--- src/Symfony/Component/Uid/Command/UidGenerateCommand.php
+++ src/Symfony/Component/Uid/Command/UidGenerateCommand.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Bundle\FrameworkBundle\Command;
+namespace Symfony\Component\Uid\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidArgumentException;

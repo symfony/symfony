4f0375dccd390206382dbd1880e953dd64a54fbb
Nicolas Grekas <nicolas.grekas@gmail.com>
2020-04-13 12:44:30 +0200

    fix merge

--- src/Symfony/Component/DependencyInjection/Tests/Fixtures/config/services_autoconfigure_with_parent.php
+++ src/Symfony/Component/DependencyInjection/Tests/Fixtures/config/services_autoconfigure_with_parent.php
@@ -3,7 +3,7 @@
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 return function (ContainerConfigurator $c) {
-    $c->services()
+    $c->services()->defaults()->public()
         ->set('parent_service', \stdClass::class)
         ->set('child_service')->parent('parent_service')->autoconfigure(true);
 };

fc6cf3d3c6903597ad4576e0123cd1f1a8155f59
Thomas Calvet <calvet.thomas@gmail.com>
2020-04-20 12:25:20 +0200

    [DX] Show the ParseException message in YAML file loaders

--- src/Symfony/Component/Routing/Loader/YamlFileLoader.php
+++ src/Symfony/Component/Routing/Loader/YamlFileLoader.php
@@ -66,7 +66,7 @@ class YamlFileLoader extends FileLoader
         try {
             $parsedConfig = $this->yamlParser->parseFile($path);
         } catch (ParseException $e) {
-            throw new \InvalidArgumentException(sprintf('The file "%s" does not contain valid YAML.', $path), 0, $e);
+            throw new \InvalidArgumentException(sprintf('The file "%s" does not contain valid YAML', $path).': '.$e->getMessage(), 0, $e);
         } finally {
             restore_error_handler();
         }

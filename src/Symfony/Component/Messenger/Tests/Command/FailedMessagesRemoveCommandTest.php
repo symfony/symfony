e66cd97ec3d843e3e3a1cce80f21e20562ae5697
tien.xuan.vo <tien.xuan.vo@gmail.com>
2020-05-02 11:26:03 +0700

    [Messenger] Fix messenger:failed:remove can not remove single message

--- src/Symfony/Component/Messenger/Tests/Command/FailedMessagesRemoveCommandTest.php
+++ src/Symfony/Component/Messenger/Tests/Command/FailedMessagesRemoveCommandTest.php
@@ -19,6 +19,23 @@ use Symfony\Component\Messenger\Transport\Receiver\ListableReceiverInterface;
 
 class FailedMessagesRemoveCommandTest extends TestCase
 {
+    public function testRemoveSingleMessage()
+    {
+        $receiver = $this->createMock(ListableReceiverInterface::class);
+        $receiver->expects($this->once())->method('find')->with(20)->willReturn(new Envelope(new \stdClass()));
+
+        $command = new FailedMessagesRemoveCommand(
+            'failure_receiver',
+            $receiver
+        );
+
+        $tester = new CommandTester($command);
+        $tester->execute(['id' => 20, '--force' => true]);
+
+        $this->assertStringContainsString('Failed Message Details', $tester->getDisplay());
+        $this->assertStringContainsString('Message with id 20 removed.', $tester->getDisplay());
+    }
+
     public function testRemoveUniqueMessage()
     {
         $receiver = $this->createMock(ListableReceiverInterface::class);

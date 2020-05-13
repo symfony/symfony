e8c9049a5ac2f6db229a9b70a1f4e70eff998203
Alexandru Patranescu <alexandru.patranescu@payu.ro>
2020-05-13 03:38:10 +0300

    Remove some magic from TypeValidator logic and OptionsResolver type verify logic

--- src/Symfony/Component/Validator/Constraints/TypeValidator.php
+++ src/Symfony/Component/Validator/Constraints/TypeValidator.php
@@ -20,6 +20,38 @@ use Symfony\Component\Validator\Exception\UnexpectedTypeException;
  */
 class TypeValidator extends ConstraintValidator
 {
+    private const VALIDATION_FUNCTIONS = [
+        'bool' => 'is_bool',
+        'boolean' => 'is_bool',
+        'int' => 'is_int',
+        'integer' => 'is_int',
+        'long' => 'is_int',
+        'float' => 'is_float',
+        'double' => 'is_float',
+        'real' => 'is_float',
+        'numeric' => 'is_numeric',
+        'string' => 'is_string',
+        'scalar' => 'is_scalar',
+        'array' => 'is_array',
+        'iterable' => 'is_iterable',
+        'countable' => 'is_countable',
+        'callable' => 'is_callable',
+        'object' => 'is_object',
+        'resource' => 'is_resource',
+        'null' => 'is_null',
+        'alnum' => 'ctype_alnum',
+        'alpha' => 'ctype_alpha',
+        'cntrl' => 'ctype_cntrl',
+        'digit' => 'ctype_digit',
+        'graph' => 'ctype_graph',
+        'lower' => 'ctype_lower',
+        'print' => 'ctype_print',
+        'punct' => 'ctype_punct',
+        'space' => 'ctype_space',
+        'upper' => 'ctype_upper',
+        'xdigit' => 'ctype_xdigit',
+    ];
+
     /**
      * {@inheritdoc}
      */
@@ -37,14 +69,11 @@ class TypeValidator extends ConstraintValidator
 
         foreach ($types as $type) {
             $type = strtolower($type);
-            $type = 'boolean' === $type ? 'bool' : $type;
-            $isFunction = 'is_'.$type;
-            $ctypeFunction = 'ctype_'.$type;
-            if (\function_exists($isFunction) && $isFunction($value)) {
+            if (isset(self::VALIDATION_FUNCTIONS[$type]) && self::VALIDATION_FUNCTIONS[$type]($value)) {
                 return;
-            } elseif (\function_exists($ctypeFunction) && $ctypeFunction($value)) {
-                return;
-            } elseif ($value instanceof $type) {
+            }
+
+            if ($value instanceof $type) {
                 return;
             }
         }

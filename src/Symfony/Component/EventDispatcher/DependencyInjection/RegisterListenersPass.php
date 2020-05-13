646878d072c9c7893dbbbf495acf97fdc213d7b0
Antonio Pauletich <antonio.pauletich95@gmail.com>
2020-05-13 18:27:55 +0200

    Fix register event listeners compiler pass

--- src/Symfony/Component/EventDispatcher/DependencyInjection/RegisterListenersPass.php
+++ src/Symfony/Component/EventDispatcher/DependencyInjection/RegisterListenersPass.php
@@ -56,12 +56,12 @@ class RegisterListenersPass implements CompilerPassInterface
             return;
         }
 
+        $aliases = [];
+
         if ($container->hasParameter($this->eventAliasesParameter)) {
             $aliases = $container->getParameter($this->eventAliasesParameter);
-            $container->getParameterBag()->remove($this->eventAliasesParameter);
-        } else {
-            $aliases = [];
         }
+
         $definition = $container->findDefinition($this->dispatcherService);
 
         foreach ($container->findTaggedServiceIds($this->listenerTag, true) as $id => $events) {

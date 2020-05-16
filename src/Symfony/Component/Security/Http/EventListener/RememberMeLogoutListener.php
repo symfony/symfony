c49d00f9848f86c33578e02134da2b9e10bc3f41
Wouter de Jong <wouter@wouterj.nl>
2020-05-16 12:36:39 +0200

    Added deprecation for RememberMe services without logout() method

--- src/Symfony/Component/Security/Http/EventListener/RememberMeLogoutListener.php
+++ src/Symfony/Component/Security/Http/EventListener/RememberMeLogoutListener.php
@@ -14,7 +14,7 @@ namespace Symfony\Component\Security\Http\EventListener;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Exception\LogicException;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
-use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
+use Symfony\Component\Security\Http\RememberMe\RememberMeServicesInterface;
 
 /**
  * @author Wouter de Jong <wouter@wouterj.nl>
@@ -25,13 +25,21 @@ class RememberMeLogoutListener implements EventSubscriberInterface
 {
     private $rememberMeServices;
 
-    public function __construct(LogoutHandlerInterface $rememberMeServices)
+    public function __construct(RememberMeServicesInterface $rememberMeServices)
     {
+        if (!method_exists($rememberMeServices, 'logout')) {
+            trigger_deprecation('symfony/security-core', '5.1', '"%s" should implement the "logout(Request $request, Response $response, TokenInterface $token)" method, this method will be added to the "%s" in version 6.0.', \get_class($rememberMeServices), RememberMeServicesInterface::class);
+        }
+
         $this->rememberMeServices = $rememberMeServices;
     }
 
     public function onLogout(LogoutEvent $event): void
     {
+        if (!method_exists($this->rememberMeServices, 'logout')) {
+            return;
+        }
+
         if (null === $event->getResponse()) {
             throw new LogicException(sprintf('No response was set for this logout action. Make sure the DefaultLogoutListener or another listener has set the response before "%s" is called.', __CLASS__));
         }

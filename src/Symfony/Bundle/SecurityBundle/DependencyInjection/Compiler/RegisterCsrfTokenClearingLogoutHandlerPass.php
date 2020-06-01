2d738b30dec88c8ff3d9b807c318bb4703b8a532
Wouter de Jong <wouter@wouterj.nl>
2020-06-01 18:23:15 +0200

    Only register CSRF protection listener if CSRF is available

--- src/Symfony/Bundle/SecurityBundle/DependencyInjection/Compiler/RegisterCsrfTokenClearingLogoutHandlerPass.php
+++ src/Symfony/Bundle/SecurityBundle/DependencyInjection/Compiler/RegisterCsrfTokenClearingLogoutHandlerPass.php
@@ -11,32 +11,21 @@
 
 namespace Symfony\Bundle\SecurityBundle\DependencyInjection\Compiler;
 
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\Security\Http\EventListener\CsrfTokenClearingLogoutListener;
+
+trigger_deprecation('symfony/security-bundle', '5.1', 'The "%s" class is deprecated.', RegisterCsrfTokenClearingLogoutHandlerPass::class);
 
 /**
- * @author Christian Flothmann <christian.flothmann@sensiolabs.de>
+ * @deprecated since symfony/security-bundle 5.1
  */
-class RegisterCsrfTokenClearingLogoutHandlerPass implements CompilerPassInterface
+class RegisterCsrfTokenClearingLogoutHandlerPass extends RegisterCsrfFeaturesPass
 {
     public function process(ContainerBuilder $container)
     {
-        if (!$container->has('security.logout_listener') || !$container->has('security.csrf.token_storage')) {
+        if (!$container->has('security.csrf.token_storage')) {
             return;
         }
 
-        $csrfTokenStorage = $container->findDefinition('security.csrf.token_storage');
-        $csrfTokenStorageClass = $container->getParameterBag()->resolveValue($csrfTokenStorage->getClass());
-
-        if (!is_subclass_of($csrfTokenStorageClass, 'Symfony\Component\Security\Csrf\TokenStorage\ClearableTokenStorageInterface')) {
-            return;
-        }
-
-        $container->register('security.logout.listener.csrf_token_clearing', CsrfTokenClearingLogoutListener::class)
-            ->addArgument(new Reference('security.csrf.token_storage'))
-            ->addTag('kernel.event_subscriber')
-            ->setPublic(false);
+        $this->registerLogoutHandler($container);
     }
 }

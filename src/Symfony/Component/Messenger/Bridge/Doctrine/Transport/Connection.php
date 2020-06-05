d12190687b35a1a380c58f90819d07eb44602727
Marco Pfeiffer <git@marco.zone>
2020-06-05 22:29:28 +0200

    set column length for mysql 5.6 compatibility
    MySQL 5.6 does not support more than 191 characters when an index is used and when using utf8mb4 as charset.
    As a workaround, I define the length of the queue_name field.

--- src/Symfony/Component/Messenger/Bridge/Doctrine/Transport/Connection.php
+++ src/Symfony/Component/Messenger/Bridge/Doctrine/Transport/Connection.php
@@ -390,6 +390,7 @@ class Connection implements ResetInterface
         $table->addColumn('headers', self::$useDeprecatedConstants ? Type::TEXT : Types::TEXT)
             ->setNotnull(true);
         $table->addColumn('queue_name', self::$useDeprecatedConstants ? Type::STRING : Types::STRING)
+            ->setLength(190) // mysql 5.6 only supports 191 characters on an indexed column in utf8mb4 mode
             ->setNotnull(true);
         $table->addColumn('created_at', self::$useDeprecatedConstants ? Type::DATETIME : Types::DATETIME_MUTABLE)
             ->setNotnull(true);

7bac792328b8df413294444bddba94104fc6bed5
Fabien Potencier <fabien@potencier.org>
2020-06-25 10:39:34 +0200

    Fix logic

--- src/Symfony/Component/Notifier/Bridge/Slack/Block/SlackSectionBlock.php
+++ src/Symfony/Component/Notifier/Bridge/Slack/Block/SlackSectionBlock.php
@@ -39,15 +39,14 @@ final class SlackSectionBlock extends AbstractSlackBlock
      */
     public function field(string $text, bool $markdown = true): self
     {
+        if (10 === \count($this->options['fields'])) {
+            throw new \LogicException('Maximum number of fields should not exceed 10.');
+        }
+
         $this->options['fields'][] = [
             'type' => $markdown ? 'mrkdwn' : 'plain_text',
             'text' => $text,
         ];
-
-        // Maximum number of items is 10
-        if (10 <= \count($this->options['fields'])) {
-            throw new \LogicException('Maximum number of fields should not exceed 10.');
-        }
 
         return $this;
     }

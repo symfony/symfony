b3d081297653a8fc0ec67db22cf33888be68ec28
Guilliam Xavier <guilliamxavier@users.noreply.github.com>
2020-07-07 20:14:00 +0200

    Fix author for class CachePoolClearerCacheWarmer

--- src/Symfony/Bundle/FrameworkBundle/CacheWarmer/CachePoolClearerCacheWarmer.php
+++ src/Symfony/Bundle/FrameworkBundle/CacheWarmer/CachePoolClearerCacheWarmer.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
  *
  * Do not use in production!
  *
- * @author Kévin Dunglas <dunglas@gmail.com>
+ * @author Teoh Han Hui <teohhanhui@gmail.com>
  *
  * @internal
  */

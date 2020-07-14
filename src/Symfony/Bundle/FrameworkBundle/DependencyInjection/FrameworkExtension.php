46c8c3791b45ed595d6db3f0bee5de49b32b9771
Youssef BENHSSAIEN <youssef.benhssaien@gmail.com>
2020-07-14 10:49:26 +0200

    [FrameworkBundle] set default session.handler alias if handler_id is not provided

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -880,6 +880,7 @@ class FrameworkExtension extends Extension
             // Set the handler class to be null
             $container->getDefinition('session.storage.native')->replaceArgument(1, null);
             $container->getDefinition('session.storage.php_bridge')->replaceArgument(0, null);
+            $container->setAlias('session.handler', 'session.handler.native_file')->setPrivate(true);
         } else {
             $container->setAlias('session.handler', $config['handler_id'])->setPrivate(true);
         }

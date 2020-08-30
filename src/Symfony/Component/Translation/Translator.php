4affb4622fe83066ad66e7a28b9fe7fe5e542b02
Fabien Potencier <fabien@potencier.org>
2020-08-30 09:20:37 +0200

    Fix CS

--- src/Symfony/Component/Translation/Translator.php
+++ src/Symfony/Component/Translation/Translator.php
@@ -217,7 +217,8 @@ class Translator implements TranslatorInterface, TranslatorBagInterface, LocaleA
         $len = \strlen(MessageCatalogue::INTL_DOMAIN_SUFFIX);
         if ($this->hasIntlFormatter
             && ($catalogue->defines($id, $domain.MessageCatalogue::INTL_DOMAIN_SUFFIX)
-                || 0 == substr_compare($domain, MessageCatalogue::INTL_DOMAIN_SUFFIX, -$len, $len))) {
+            || 0 === substr_compare($domain, MessageCatalogue::INTL_DOMAIN_SUFFIX, -$len, $len))
+        ) {
             return $this->formatter->formatIntl($catalogue->get($id, $domain), $locale, $parameters);
         }
 

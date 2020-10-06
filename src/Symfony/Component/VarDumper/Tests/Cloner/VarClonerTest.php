a8b986e35e15d93ae1d57131077808a2e7267694
Nicolas Grekas <nicolas.grekas@gmail.com>
2020-10-06 18:40:22 +0200

    [VarDumper] fix truncating big arrays

--- src/Symfony/Component/VarDumper/Tests/Cloner/VarClonerTest.php
+++ src/Symfony/Component/VarDumper/Tests/Cloner/VarClonerTest.php
@@ -162,7 +162,7 @@ EOTXT;
                     'Level 3 Item 3',
                 ],
                 [
-                    'Level 3 Item 4',
+                    999 => 'Level 3 Item 4',
                     'Level 3 Item 5',
                     'Level 3 Item 6',
                 ],
@@ -242,7 +242,7 @@ Symfony\Component\VarDumper\Cloner\Data Object
                     [1] => Array
                         (
                             [0] => 2
-                            [2] => 7
+                            [1] => 7
                         )
 
                     [2] => Array
@@ -299,7 +299,7 @@ Symfony\Component\VarDumper\Cloner\Data Object
 
             [7] => Array
                 (
-                    [0] => Level 3 Item 4
+                    [999] => Level 3 Item 4
                 )
 
         )

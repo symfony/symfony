77cd3d62fac6eec9624f8f7f1fc73013ce9b54e1
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-10-06 14:06:01 +0200

    Add test for serialization of RawMessage

--- src/Symfony/Component/Mime/Tests/RawMessageTest.php
+++ src/Symfony/Component/Mime/Tests/RawMessageTest.php
@@ -32,4 +32,26 @@ class RawMessageTest extends TestCase
         $this->assertEquals('some string', $message->toString());
         $this->assertEquals('some string', implode('', iterator_to_array($message->toIterable())));
     }
+
+    public function testSerialization()
+    {
+        $message = new RawMessage('string');
+        $serialized = serialize($message);
+        $this->assertEquals('C:33:"Symfony\Component\Mime\RawMessage":23:{a:1:{i:0;s:6:"string";}}', $serialized);
+        $this->assertEquals('string', unserialize($serialized)->toString());
+        // calling methods more than once work
+        $serialized = serialize($message);
+        $this->assertEquals('C:33:"Symfony\Component\Mime\RawMessage":23:{a:1:{i:0;s:6:"string";}}', $serialized);
+        $this->assertEquals('string', unserialize($serialized)->toString());
+
+        $message = new RawMessage(new \ArrayObject(['some', ' ', 'string']));
+        $message = new RawMessage($message->toIterable());
+        $serialized = serialize($message);
+        $this->assertEquals('C:33:"Symfony\Component\Mime\RawMessage":29:{a:1:{i:0;s:11:"some string";}}', $serialized);
+        $this->assertEquals('some string', unserialize($serialized)->toString());
+        // calling methods more than once work
+        $serialized = serialize($message);
+        $this->assertEquals('C:33:"Symfony\Component\Mime\RawMessage":29:{a:1:{i:0;s:11:"some string";}}', $serialized);
+        $this->assertEquals('some string', unserialize($serialized)->toString());
+    }
 }

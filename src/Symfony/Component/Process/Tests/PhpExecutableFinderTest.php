b8f5d87a2f30d3dfa4deddb77bb8136764a256cb
Christian Neff <christian.neff@gmail.com>
2020-10-23 17:08:27 +0200

    [Process] PhpExecutableFinder: Add tests

--- src/Symfony/Component/Process/Tests/PhpExecutableFinderTest.php
+++ src/Symfony/Component/Process/Tests/PhpExecutableFinderTest.php
@@ -34,6 +34,73 @@ class PhpExecutableFinderTest extends TestCase
     }
 
     /**
+     * tests findByName() with the constant PHP_BINARY.
+     */
+    public function testFindByName()
+    {
+        $f = new PhpExecutableFinder();
+
+        $current = \PHP_BINARY;
+        $name = \basename($current);
+
+        $this->assertEquals($current, $f->findByName($name), '::findByName() returns the executable PHP');
+    }
+
+    /**
+     * tests tryNames() with the constant PHP_BINARY.
+     */
+    public function testTryNames()
+    {
+        $f = new PhpExecutableFinder();
+
+        $current = \PHP_BINARY;
+        $real = \basename($current);
+
+        $this->assertEquals($current, $f->tryNames(['php-foo', $real]), '::tryNames() returns the executable PHP');
+    }
+
+    /**
+     * tests findByVersion() with the constant PHP_BINARY.
+     */
+    public function testFindByVersion()
+    {
+        $f = new PhpExecutableFinder();
+
+        $ver = PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;
+        $dir = sys_get_temp_dir().DIRECTORY_SEPARATOR.uniqid('php', true);
+        $file = $dir.DIRECTORY_SEPARATOR.'php'.$ver;
+
+        mkdir($dir);
+        copy(\PHP_BINARY, $file);
+
+        $this->assertEquals($file, $f->findByVersion($ver, [$dir]), '::findByVersion() returns the executable PHP');
+
+        unlink($file);
+        rmdir($dir);
+    }
+
+    /**
+     * tests tryVersions() with the constant PHP_BINARY.
+     */
+    public function testTryVersions()
+    {
+        $f = new PhpExecutableFinder();
+
+        $ver1 = PHP_MAJOR_VERSION.'.'.(PHP_MINOR_VERSION + 1);
+        $ver2 = PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;
+        $dir = sys_get_temp_dir().DIRECTORY_SEPARATOR.uniqid('php', true);
+        $file = $dir.DIRECTORY_SEPARATOR.'php'.$ver2;
+
+        mkdir($dir);
+        copy(\PHP_BINARY, $file);
+
+        $this->assertEquals($file, $f->tryVersions([$ver1, $ver2]), '::tryVersions() returns the executable PHP');
+
+        unlink($file);
+        rmdir($dir);
+    }
+
+    /**
      * tests find() with the env var PHP_PATH.
      */
     public function testFindArguments()

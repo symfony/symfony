762e23c7ebbb0215d16182d63e83d260567c909b
RSalnikov <roman.salnikov.s@gmail.com>
2020-10-26 19:28:35 +0300

    Update PHPUnit 8.3 -> 8.5

--- src/Symfony/Bridge/PhpUnit/bin/simple-phpunit.php
+++ src/Symfony/Bridge/PhpUnit/bin/simple-phpunit.php
@@ -98,7 +98,7 @@ if (\PHP_VERSION_ID >= 80000) {
     $PHPUNIT_VERSION = $getEnvVar('SYMFONY_PHPUNIT_VERSION', '9.4');
 } elseif (\PHP_VERSION_ID >= 70200) {
     // PHPUnit 8 requires PHP 7.2+
-    $PHPUNIT_VERSION = $getEnvVar('SYMFONY_PHPUNIT_VERSION', '8.3');
+    $PHPUNIT_VERSION = $getEnvVar('SYMFONY_PHPUNIT_VERSION', '8.5');
 } elseif (\PHP_VERSION_ID >= 70100) {
     // PHPUnit 7 requires PHP 7.1+
     $PHPUNIT_VERSION = $getEnvVar('SYMFONY_PHPUNIT_VERSION', '7.5');

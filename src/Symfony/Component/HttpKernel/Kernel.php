1864a9c5285f7dbc42414cab71965d0e1da7ac94
Fabien Potencier <fabien@potencier.org>
2020-10-28 06:50:56 +0100

    Update VERSION for 4.4.16

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -76,12 +76,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
 
     private static $freshCache = [];
 
-    const VERSION = '4.4.16-DEV';
+    const VERSION = '4.4.16';
     const VERSION_ID = 40416;
     const MAJOR_VERSION = 4;
     const MINOR_VERSION = 4;
     const RELEASE_VERSION = 16;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     const END_OF_MAINTENANCE = '11/2022';
     const END_OF_LIFE = '11/2023';

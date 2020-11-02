76c19393c8b21ebe37a7c19ff05d282069f9f57a
Nicolas Grekas <nicolas.grekas@gmail.com>
2020-11-02 17:05:56 +0100

    [HttpKernel] fix merge

--- src/Symfony/Component/HttpKernel/Tests/EventListener/SessionListenerTest.php
+++ src/Symfony/Component/HttpKernel/Tests/EventListener/SessionListenerTest.php
@@ -20,7 +20,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 use Symfony\Component\HttpKernel\Event\FinishRequestEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
@@ -199,7 +198,7 @@ class SessionListenerTest extends TestCase
         $container->set('session', $session);
         $container->set('request_stack', $requestStack);
 
-        $event = new GetResponseEvent($kernel, $masterRequest, HttpKernelInterface::MASTER_REQUEST);
+        $event = new RequestEvent($kernel, $masterRequest, HttpKernelInterface::MASTER_REQUEST);
 
         $listener = new SessionListener($container);
         $listener->onKernelRequest($event);

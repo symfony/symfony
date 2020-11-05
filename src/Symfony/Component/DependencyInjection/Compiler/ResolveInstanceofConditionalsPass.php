73a3b838b742f103033218a2758548b829d27061
Christian Flothmann <christian.flothmann@sensiolabs.de>
2020-11-05 11:17:07 +0100

    autoconfigure behavior describing tags on decorators

--- src/Symfony/Component/DependencyInjection/Compiler/ResolveInstanceofConditionalsPass.php
+++ src/Symfony/Component/DependencyInjection/Compiler/ResolveInstanceofConditionalsPass.php
@@ -35,16 +35,26 @@ class ResolveInstanceofConditionalsPass implements CompilerPassInterface
             }
         }
 
+        $tagsToKeep = [];
+
+        if ($container->hasParameter('container.behavior_describing_tags')) {
+            $tagsToKeep = $container->getParameter('container.behavior_describing_tags');
+        }
+
         foreach ($container->getDefinitions() as $id => $definition) {
             if ($definition instanceof ChildDefinition) {
                 // don't apply "instanceof" to children: it will be applied to their parent
                 continue;
             }
-            $container->setDefinition($id, $this->processDefinition($container, $id, $definition));
+            $container->setDefinition($id, $this->processDefinition($container, $id, $definition, $tagsToKeep));
+        }
+
+        if ($container->hasParameter('container.behavior_describing_tags')) {
+            $container->getParameterBag()->remove('container.behavior_describing_tags');
         }
     }
 
-    private function processDefinition(ContainerBuilder $container, string $id, Definition $definition): Definition
+    private function processDefinition(ContainerBuilder $container, string $id, Definition $definition, array $tagsToKeep): Definition
     {
         $instanceofConditionals = $definition->getInstanceofConditionals();
         $autoconfiguredInstanceof = $definition->isAutoconfigured() ? $container->getAutoconfiguredInstanceof() : [];
@@ -115,10 +125,10 @@ class ResolveInstanceofConditionalsPass implements CompilerPassInterface
             }
 
             // Don't add tags to service decorators
-            if (null === $definition->getDecoratedService()) {
-                $i = \count($instanceofTags);
-                while (0 <= --$i) {
-                    foreach ($instanceofTags[$i] as $k => $v) {
+            $i = \count($instanceofTags);
+            while (0 <= --$i) {
+                foreach ($instanceofTags[$i] as $k => $v) {
+                    if (null === $definition->getDecoratedService() || \in_array($k, $tagsToKeep, true)) {
                         foreach ($v as $v) {
                             if ($definition->hasTag($k) && \in_array($v, $definition->getTag($k))) {
                                 continue;

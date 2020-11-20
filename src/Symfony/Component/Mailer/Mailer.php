0e7b60be2246b55eb5752228d9c48ce5e56d292f
Pablo Schläpfer <pablo@webcraft.ch>
2020-11-20 13:16:23 +0100

    [Mailer] send cloned Message to the bus
    This more or less reverts 829566cdea90a0be0231a, but it restores the
    behaviour in 4.4: The **cloned** Mime Message is dispatched to the
    messenger instead of the original one.
    
    This allows us to change the message using an event listener and then
    get that *changed* instance in the message handler.

--- src/Symfony/Component/Mailer/Mailer.php
+++ src/Symfony/Component/Mailer/Mailer.php
@@ -45,9 +45,9 @@ final class Mailer implements MailerInterface
         }
 
         if (null !== $this->dispatcher) {
-            $clonedMessage = clone $message;
-            $clonedEnvelope = null !== $envelope ? clone $envelope : Envelope::create($clonedMessage);
-            $event = new MessageEvent($clonedMessage, $clonedEnvelope, (string) $this->transport, true);
+            $message = clone $message;
+            $clonedEnvelope = null !== $envelope ? clone $envelope : Envelope::create($message);
+            $event = new MessageEvent($message, $clonedEnvelope, (string) $this->transport, true);
             $this->dispatcher->dispatch($event);
         }
 

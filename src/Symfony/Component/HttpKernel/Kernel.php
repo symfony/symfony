d4ccae9b31f0c3c505507804990d835d74fce7d6
Fabien Potencier <fabien@potencier.org>
2020-11-29 10:31:15 +0100

    Bump Symfony version to 5.1.10

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -73,12 +73,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
 
     private static $freshCache = [];
 
-    const VERSION = '5.1.9';
-    const VERSION_ID = 50109;
+    const VERSION = '5.1.10-DEV';
+    const VERSION_ID = 50110;
     const MAJOR_VERSION = 5;
     const MINOR_VERSION = 1;
-    const RELEASE_VERSION = 9;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 10;
+    const EXTRA_VERSION = 'DEV';
 
     const END_OF_MAINTENANCE = '01/2021';
     const END_OF_LIFE = '01/2021';

668732305a518fee80a30bad4d6b1eb2d091f716
Christian Flothmann <christian.flothmann@sensiolabs.de>
2020-12-01 15:29:09 +0100

    fix lexing backslashes in single quoted strings

--- src/Symfony/Component/Yaml/Tests/ParserTest.php
+++ src/Symfony/Component/Yaml/Tests/ParserTest.php
@@ -1618,6 +1618,11 @@ YAML
         ];
     }
 
+    public function testBackslashInSingleQuotedString()
+    {
+        $this->assertSame(['foo' => 'bar\\'], $this->parser->parse("foo: 'bar\'"));
+    }
+
     public function testParseMultiLineString()
     {
         $this->assertEquals("foo bar\nbaz", $this->parser->parse("foo\nbar\n\nbaz"));

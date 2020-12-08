a7fcb0b84eb3bc89227ee5d05953ffc0548831c6
Nicolas Grekas <nicolas.grekas@gmail.com>
2020-12-08 15:04:31 +0100

    minor cs fix

--- src/Symfony/Component/DependencyInjection/Exception/AutowiringFailedException.php
+++ src/Symfony/Component/DependencyInjection/Exception/AutowiringFailedException.php
@@ -23,8 +23,7 @@ class AutowiringFailedException extends RuntimeException
     {
         $this->serviceId = $serviceId;
 
-        if (
-            $message instanceof \Closure
+        if ($message instanceof \Closure
             && (\function_exists('xdebug_is_enabled') ? xdebug_is_enabled() : \function_exists('xdebug_info'))
         ) {
             $message = $message();

4a1976b4efe13df964537079b378578622007083
Oskar Stark <oskarstark@googlemail.com>
2020-12-16 20:36:06 +0100

    [Notifier] Use assertSame()

--- src/Symfony/Component/Notifier/Bridge/Telegram/Tests/TelegramTransportTest.php
+++ src/Symfony/Component/Notifier/Bridge/Telegram/Tests/TelegramTransportTest.php
@@ -91,7 +91,7 @@ final class TelegramTransportTest extends TestCase
         ];
 
         $client = new MockHttpClient(function (string $method, string $url, array $options = []) use ($response, $expectedBody): ResponseInterface {
-            $this->assertEquals($expectedBody, json_decode($options['body'], true));
+            $this->assertSame($expectedBody, json_decode($options['body'], true));
 
             return $response;
         });
@@ -120,7 +120,7 @@ final class TelegramTransportTest extends TestCase
         ];
 
         $client = new MockHttpClient(function (string $method, string $url, array $options = []) use ($response, $expectedBody): ResponseInterface {
-            $this->assertEquals($expectedBody, json_decode($options['body'], true));
+            $this->assertSame($expectedBody, json_decode($options['body'], true));
 
             return $response;
         });

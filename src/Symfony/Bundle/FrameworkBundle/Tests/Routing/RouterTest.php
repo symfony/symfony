af6db0cfafe133383fa360c47a58c2ed207fac43
HypeMC <hypemc@gmail.com>
2021-01-27 22:59:52 +0100

    [FrameworkBundle] Fix freshness checks with boolean parameters on routes

--- src/Symfony/Bundle/FrameworkBundle/Tests/Routing/RouterTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Routing/RouterTest.php
@@ -14,11 +14,16 @@ namespace Symfony\Bundle\FrameworkBundle\Tests\Routing;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\ResourceCheckerConfigCache;
+use Symfony\Component\Config\ResourceCheckerConfigCacheFactory;
 use Symfony\Component\DependencyInjection\Config\ContainerParametersResource;
+use Symfony\Component\DependencyInjection\Config\ContainerParametersResourceChecker;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\Exception\ParameterNotFoundException;
 use Symfony\Component\DependencyInjection\Exception\RuntimeException;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -501,6 +506,51 @@ class RouterTest extends TestCase
     public function getNonStringValues()
     {
         return [[null], [false], [true], [new \stdClass()], [['foo', 'bar']], [[[]]]];
+    }
+
+    /**
+     * @dataProvider getContainerParameterForRoute
+     */
+    public function testCacheValiditiyWithContainerParameters($parameter)
+    {
+        $cacheDir = sys_get_temp_dir().\DIRECTORY_SEPARATOR.uniqid('router_', true);
+
+        try {
+            $container = new Container();
+            $container->set('routing.loader', new YamlFileLoader(new FileLocator(__DIR__.'/Fixtures')));
+
+            $container->setParameter('parameter.condition', $parameter);
+
+            $router = new Router($container, 'with_condition.yaml', [
+                'debug' => true,
+                'cache_dir' => $cacheDir,
+            ]);
+
+            $resourceCheckers = [
+                new ContainerParametersResourceChecker($container),
+            ];
+
+            $router->setConfigCacheFactory(new ResourceCheckerConfigCacheFactory($resourceCheckers));
+
+            $router->getMatcher(); // trigger cache build
+
+            $cache = new ResourceCheckerConfigCache($cacheDir.\DIRECTORY_SEPARATOR.'UrlMatcher.php', $resourceCheckers);
+
+            $this->assertTrue($cache->isFresh());
+        } finally {
+            if (is_dir($cacheDir)) {
+                array_map('unlink', glob($cacheDir.\DIRECTORY_SEPARATOR.'*'));
+                rmdir($cacheDir);
+            }
+        }
+    }
+
+    public function getContainerParameterForRoute()
+    {
+        yield 'String' => ['"foo"'];
+        yield 'Integer' => [0];
+        yield 'Boolean true' => [true];
+        yield 'Boolean false' => [false];
     }
 
     private function getServiceContainer(RouteCollection $routes): Container

91269134e2230f4c7754bbd0c8af7ae9b600f14d
Jarek Jakubowski <egger1991@gmail.com>
2021-02-25 10:31:31 +0100

    [4.4] Allow Contract Event since Component Event is deprecated
    Class `Symfony\Component\EventDispatcher\Event` is deprecated since 4.3, yet it is required by the EventDispatcher. This fix will allow using the new `Symfony\Contracts\EventDispatcher\Event` class.

--- src/Symfony/Component/EventDispatcher/Debug/TraceableEventDispatcher.php
+++ src/Symfony/Component/EventDispatcher/Debug/TraceableEventDispatcher.php
@@ -150,8 +150,8 @@ class TraceableEventDispatcher implements TraceableEventDispatcherInterface
             $event = $eventName ?? new Event();
             $eventName = $swap;
 
-            if (!$event instanceof Event) {
-                throw new \TypeError(sprintf('Argument 1 passed to "%s::dispatch()" must be an instance of "%s", "%s" given.', EventDispatcherInterface::class, Event::class, \is_object($event) ? \get_class($event) : \gettype($event)));
+            if (!$event instanceof Event && !$event instanceof ContractsEvent) {
+                throw new \TypeError(sprintf('Argument 1 passed to "%s::dispatch()" must be an instance of "%s" or "%s", "%s" given.', EventDispatcherInterface::class, Event::class, ContractsEvent::class, \is_object($event) ? \get_class($event) : \gettype($event)));
             }
         }
 

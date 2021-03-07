48419ca6f6907b460bb214b3ff5bc309ad9fffc0
MrPropre <36234981+MrPropre@users.noreply.github.com>
2021-03-08 00:57:45 +0100

    Change JsonResponse default encoding options

--- src/Symfony/Component/HttpFoundation/JsonResponse.php
+++ src/Symfony/Component/HttpFoundation/JsonResponse.php
@@ -27,11 +27,9 @@ class JsonResponse extends Response
     protected $data;
     protected $callback;
 
-    // Encode <, >, ', &, and " characters in the JSON, making it also safe to be embedded into HTML.
-    // 15 === JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT
-    public const DEFAULT_ENCODING_OPTIONS = 15;
+    public const DEFAULT_ENCODING_OPTIONS = 0;
 
-    protected $encodingOptions = self::DEFAULT_ENCODING_OPTIONS;
+    protected $encodingOptions = 0;
 
     /**
      * @param mixed $data    The response data

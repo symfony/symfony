3a2d1b4a0a38ae838ed262deb6aaa9b96ba8ec96
Oliver Klee <github@oliverklee.de>
2021-03-09 17:59:23 +0100

    bug #40427 [Console] Stop accepting ints as InputOption defaults
    The types accepted and provided by `InputInterface::getOption`
    and `setOption` do not include `int` (and passing something like
    `--option=123` will set the option to the string `"123"`, not
    to the integer `123`).
    
    The `InputOption` default types should match this.

--- src/Symfony/Component/Console/Command/Command.php
+++ src/Symfony/Component/Console/Command/Command.php
@@ -394,11 +394,11 @@ class Command
     /**
      * Adds an option.
      *
-     * @param string                        $name        The option name
-     * @param string|array|null             $shortcut    The shortcuts, can be null, a string of shortcuts delimited by | or an array of shortcuts
-     * @param int|null                      $mode        The option mode: One of the InputOption::VALUE_* constants
-     * @param string                        $description A description text
-     * @param string|string[]|int|bool|null $default     The default value (must be null for InputOption::VALUE_NONE)
+     * @param string                    $name        The option name
+     * @param string|array|null         $shortcut    The shortcuts, can be null, a string of shortcuts delimited by | or an array of shortcuts
+     * @param int|null                  $mode        The option mode: One of the InputOption::VALUE_* constants
+     * @param string                    $description A description text
+     * @param string|string[]|bool|null $default     The default value (must be null for InputOption::VALUE_NONE)
      *
      * @throws InvalidArgumentException If option mode is invalid or incompatible
      *

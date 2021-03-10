26068fa40d91305529cb168997c0b528bb6eef07
Alexander M. Turek <me@derrabus.de>
2021-03-10 16:28:02 +0100

    Minor cleanups

--- Tests/Functional/ControllerTest.php
+++ Tests/Functional/ControllerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Bridge\PsrHttpMessage\Tests\Functional;
 
 use Symfony\Bridge\PsrHttpMessage\Tests\Fixtures\App\Kernel;

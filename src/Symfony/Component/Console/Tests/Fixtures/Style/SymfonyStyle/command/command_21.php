36b36dcecc8223b8f3d02c6c463a9a2dbf48ff90
Marion Hurteau <mhurteau@jolicode.com>
2021-03-19 20:16:44 +0100

    [Command] fix emojis messing up the line width
    add tests + removed irrelevant method

---
+++ src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_21.php
@@ -0,0 +1,13 @@
+<?php
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+//Ensure texts with emojis don't make longer lines than expected
+return function (InputInterface $input, OutputInterface $output) {
+    $output = new SymfonyStyle($input, $output);
+    $output->success('Lorem ipsum dolor sit amet');
+    $output->success('Lorem ipsum dolor sit amet with one emoji 🎉');
+    $output->success('Lorem ipsum dolor sit amet with so many of them 👩‍🌾👩‍🌾👩‍🌾👩‍🌾👩‍🌾');
+};

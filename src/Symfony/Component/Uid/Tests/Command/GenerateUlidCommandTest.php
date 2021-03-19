1b937403255c9f841d6597b2bcefc0e93fe51545
Nicolas Grekas <nicolas.grekas@gmail.com>
2021-03-19 19:00:13 +0100

    CS fix

--- src/Symfony/Component/Uid/Tests/Command/GenerateUlidCommandTest.php
+++ src/Symfony/Component/Uid/Tests/Command/GenerateUlidCommandTest.php
@@ -90,7 +90,7 @@ final class GenerateUlidCommandTest extends TestCase
         Ulid::fromRfc4122(trim($commandTester->getDisplay()));
     }
 
-    public function testTimestampIncrementWhenGeneratingSeveralTimeBasedUlids()
+    public function testTimestampIncrementWhenGeneratingSeveralUlids()
     {
         $commandTester = new CommandTester(new GenerateUlidCommand());
 

692fea7848a95efe52382932006c511aa1227f68
acmedev <matt.grasmick+acmedev@gmail.com>
2021-03-31 18:48:27 -0400

    Update ProgressBar.php

--- src/Symfony/Component/Console/Helper/ProgressBar.php
+++ src/Symfony/Component/Console/Helper/ProgressBar.php
@@ -7,6 +7,8 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * Test
  */
 
 namespace Symfony\Component\Console\Helper;

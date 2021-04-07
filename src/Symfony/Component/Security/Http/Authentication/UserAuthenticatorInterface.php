55d5f647dc18c7c73816dff309072708d5ff4756
Alexandre Daubois <alex.daubois@gmail.com>
2021-04-07 11:17:44 +0200

    [Security] Fixed missing badges argument for method authenticateUser

--- src/Symfony/Component/Security/Http/Authentication/UserAuthenticatorInterface.php
+++ src/Symfony/Component/Security/Http/Authentication/UserAuthenticatorInterface.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\BadgeInterface;
 
 /**
  * @author Wouter de Jong <wouter@wouterj.nl>
@@ -26,6 +27,8 @@ interface UserAuthenticatorInterface
     /**
      * Convenience method to programmatically login a user and return a
      * Response *if any* for success.
+     *
+     * @param BadgeInterface[] $badges Optionally, pass some Passport badges to use for the manual login
      */
-    public function authenticateUser(UserInterface $user, AuthenticatorInterface $authenticator, Request $request): ?Response;
+    public function authenticateUser(UserInterface $user, AuthenticatorInterface $authenticator, Request $request, array $badges = []): ?Response;
 }

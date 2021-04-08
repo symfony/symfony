d039ce7e63014e0a082b0546c94f00e77d0f5c22
Oskar Stark <oskarstark@googlemail.com>
2021-04-08 13:04:26 +0200

    [Notifier] Add options to Microsoft Teams notifier

---
+++ src/Symfony/Component/Notifier/Bridge/MicrosoftTeams/Section/SectionInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Notifier\Bridge\MicrosoftTeams\Section;
+
+/**
+ * @author Edouard Lescot <edouard.lescot@gmail.com>
+ * @author Oskar Stark <oskarstark@googlemail.com>
+ */
+interface SectionInterface
+{
+    public function toArray(): array;
+}

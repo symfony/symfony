c7e7e4be08a01d608a88e7df0b3a547f818c2183
croncr <croncr@protonmail.com>
2021-04-16 19:11:05 +0200

    [Routing] Remove unused parameter

--- src/Symfony/Component/Routing/Loader/AnnotationClassLoader.php
+++ src/Symfony/Component/Routing/Loader/AnnotationClassLoader.php
@@ -108,7 +108,7 @@ abstract class AnnotationClassLoader implements LoaderInterface
      *
      * @throws \InvalidArgumentException When route can't be parsed
      */
-    public function load($class, string $type = null)
+    public function load($class)
     {
         if (!class_exists($class)) {
             throw new \InvalidArgumentException(sprintf('Class "%s" does not exist.', $class));

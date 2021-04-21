b761384b8c73562f263cf031130278544598a1c9
Andreas Linden <andreas.linden@coxautoinc.com>
2021-04-21 08:17:32 +0200

    use \ArrayObject instead of Doctrine's ArrayCollection

--- src/Symfony/Component/Validator/Tests/Constraints/CountValidatorArrayObjectTest.php
+++ src/Symfony/Component/Validator/Tests/Constraints/CountValidatorArrayObjectTest.php
@@ -11,19 +11,13 @@
 
 namespace Symfony\Component\Validator\Tests\Constraints;
 
-use Doctrine\Common\Collections\ArrayCollection;
-
 /**
  * @author Andreas Linden <linden.andreas@gmx.de>
  */
-class CountValidatorCollectionTest extends CountValidatorIterableTest
+class CountValidatorArrayObjectTest extends CountValidatorIterableTest
 {
     protected function createCollection(array $content)
     {
-        // travis uses deps=low with PHP 8.0 that does not include doctrine/collections.
-        // for now just return the array until it was decided how to proceed.
-        // return new ArrayCollection($content);
-
-        return $content;
+        return new \ArrayObject($content);
     }
 }

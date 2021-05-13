5b0e62ad65551e161b1e294d19e72dd6d60d425a
Marcel Jonquières <michelet.antoine.eddy>
2021-05-14 01:34:39 +0200

    fix coding standard

--- src/Symfony/Component/Routing/Tests/Matcher/UrlMatcherTest.php
+++ src/Symfony/Component/Routing/Tests/Matcher/UrlMatcherTest.php
@@ -38,7 +38,7 @@ class UrlMatcherTest extends TestCase
         $request = Request::create('http://example.com/foo');
 
         $matcher = $this->getUrlMatcher($coll);
-        $this->assertEquals(["_route" => "bar"], $matcher->matchRequest($request));
+        $this->assertEquals(['_route' => 'bar'], $matcher->matchRequest($request));
     }
 
     public function testNoMethodSoAllowed()
@@ -928,7 +928,7 @@ class UrlMatcherTest extends TestCase
         $this->assertEquals(['_route' => 'b', 'b' => 'BBB'], $matcher->match('/en-en/BBB'));
     }
 
-    public function testTrailingRequirementWithDefault_A()
+    public function testTrailingRequirementWithDefaultA()
     {
         $coll = new RouteCollection();
         $coll->add('a', new Route('/fr-fr/{a}', ['a' => 'aaa'], ['a' => '.+']));
@@ -939,7 +939,7 @@ class UrlMatcherTest extends TestCase
         $matcher->match('/fr-fr/');
     }
 
-    public function testTrailingRequirementWithDefault_B()
+    public function testTrailingRequirementWithDefaultB()
     {
         $coll = new RouteCollection();
         $coll->add('b', new Route('/en-en/{b}', ['b' => 'bbb'], ['b' => '.*']));

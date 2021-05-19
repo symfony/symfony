8f595ec78ecec6e0d4a744917c748ac089b6ed40
Fabien Potencier <fabien@potencier.org>
2021-05-19 14:50:01 +0200

    Update version to 5.4

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -75,15 +75,15 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
 
     private static $freshCache = [];
 
-    public const VERSION = '5.3.0-DEV';
-    public const VERSION_ID = 50300;
+    public const VERSION = '5.4.0-DEV';
+    public const VERSION_ID = 50400;
     public const MAJOR_VERSION = 5;
-    public const MINOR_VERSION = 3;
+    public const MINOR_VERSION = 4;
     public const RELEASE_VERSION = 0;
     public const EXTRA_VERSION = 'DEV';
 
-    public const END_OF_MAINTENANCE = '05/2021';
-    public const END_OF_LIFE = '01/2022';
+    public const END_OF_MAINTENANCE = '11/2024';
+    public const END_OF_LIFE = '11/2025';
 
     public function __construct(string $environment, bool $debug)
     {

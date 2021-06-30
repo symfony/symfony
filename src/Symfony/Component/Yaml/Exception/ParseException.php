31e1a978f5d2b7899eb2edff8a6f9b2bc4863b0f
Nicolas Grekas <nicolas.grekas@gmail.com>
2021-06-30 15:02:00 +0200

    CS fixes

--- src/Symfony/Component/Yaml/Exception/ParseException.php
+++ src/Symfony/Component/Yaml/Exception/ParseException.php
@@ -24,10 +24,10 @@ class ParseException extends RuntimeException
     private $rawMessage;
 
     /**
-     * @param string          $message    The error message
-     * @param int             $parsedLine The line where the error occurred
-     * @param string|null     $snippet    The snippet of code near the problem
-     * @param string|null     $parsedFile The file name where the error occurred
+     * @param string      $message    The error message
+     * @param int         $parsedLine The line where the error occurred
+     * @param string|null $snippet    The snippet of code near the problem
+     * @param string|null $parsedFile The file name where the error occurred
      */
     public function __construct(string $message, int $parsedLine = -1, string $snippet = null, string $parsedFile = null, \Throwable $previous = null)
     {

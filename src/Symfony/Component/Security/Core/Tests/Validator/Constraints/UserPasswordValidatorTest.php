74f5df8aaca223d9e82083efa618cdc7874d160a
Nicolas Grekas <nicolas.grekas@gmail.com>
2021-07-04 11:47:22 +0200

    Fix bad rebase

--- src/Symfony/Component/Security/Core/Tests/Validator/Constraints/UserPasswordValidatorTest.php
+++ src/Symfony/Component/Security/Core/Tests/Validator/Constraints/UserPasswordValidatorTest.php
@@ -94,7 +94,9 @@ abstract class UserPasswordValidatorTest extends ConstraintValidatorTestCase
     {
         yield 'Doctrine style' => [new UserPassword(['message' => 'myMessage'])];
 
-        yield 'named arguments' => [new UserPassword(message: "myMessage")];
+        if (\PHP_VERSION_ID >= 80000) {
+            yield 'named arguments' => [eval('return new \Symfony\Component\Security\Core\Validator\Constraints\UserPassword(message: "myMessage");')];
+        }
     }
 
     /**

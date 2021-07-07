3d46b81eb99fe1fc0f2e9804d773009a84815901
Jeroeny <spee.jeroen@gmail.com>
2021-07-07 14:24:36 +0200

    support decorated drivers by fetching true driver from the DatabasePlatform

--- src/Symfony/Component/Cache/Tests/Adapter/PdoDbalAdapterTest.php
+++ src/Symfony/Component/Cache/Tests/Adapter/PdoDbalAdapterTest.php
@@ -11,6 +11,9 @@
 
 namespace Symfony\Component\Cache\Tests\Adapter;
 
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver;
 use Doctrine\DBAL\DriverManager;
 use PHPUnit\Framework\SkippedTestSuiteError;
 use Psr\Cache\CacheItemPoolInterface;
@@ -42,5 +45,69 @@ class PdoDbalAdapterTest extends AdapterTestCase
     public function createCachePool(int $defaultLifetime = 0): CacheItemPoolInterface
     {
         return new PdoAdapter(DriverManager::getConnection(['driver' => 'pdo_sqlite', 'path' => self::$dbFile]), '', $defaultLifetime);
+    }
+
+    public function testConfigureSchemaDecoratedDbalDriver()
+    {
+        $connection = DriverManager::getConnection(['driver' => 'pdo_sqlite', 'path' => self::$dbFile]);
+        if (interface_exists(\Doctrine\DBAL\Driver\Middleware::class, false)) {
+            $middleware = $this->createMock(\Doctrine\DBAL\Driver\Middleware::class);
+            $middleware
+                ->method('wrap')
+                ->willReturn(new DriverWrapper($connection->getDriver()));
+
+            $config = new Configuration();
+            $config->setMiddlewares([$middleware]);
+
+            $connection = DriverManager::getConnection(['driver' => 'pdo_sqlite', 'path' => self::$dbFile], $config);
+        } else {
+            $reflectionProperty = new \ReflectionProperty($connection, '_driver');
+            $reflectionProperty->setAccessible(true);
+            $reflectionProperty->setValue($connection, new DriverWrapper($reflectionProperty->getValue($connection)));
+            $reflectionProperty->setAccessible(false);
+        }
+
+        $adapter = new PdoAdapter($connection);
+        $adapter->createTable();
+
+        $item = $adapter->getItem('key');
+        $item->set('value');
+        $this->assertTrue($adapter->save($item));
+    }
+}
+
+class DriverWrapper implements Driver
+{
+    /** @var Driver $driver */
+    private $driver;
+
+    public function __construct(Driver $driver)
+    {
+        $this->driver = $driver;
+    }
+
+    public function connect(array $params, $username = null, $password = null, array $driverOptions = [])
+    {
+        return $this->driver->connect($params, $username, $password, $driverOptions);
+    }
+
+    public function getDatabasePlatform()
+    {
+        return $this->driver->getDatabasePlatform();
+    }
+
+    public function getSchemaManager(Connection $conn)
+    {
+        return $this->driver->getSchemaManager($conn);
+    }
+
+    public function getName()
+    {
+        return $this->driver->getName();
+    }
+
+    public function getDatabase(Connection $conn)
+    {
+        return $this->driver->getDatabase($conn);
     }
 }

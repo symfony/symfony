acf909a5757de9a8232d27aa3e9c5aff232dabd8
dlorek <lorekd07d@gmail.com>
2021-07-10 21:08:15 +0200

    fixed phpcs

--- src/Symfony/Component/Notifier/Bridge/Smsapi/SmsapiTransport.php
+++ src/Symfony/Component/Notifier/Bridge/Smsapi/SmsapiTransport.php
@@ -67,7 +67,7 @@ final class SmsapiTransport extends AbstractTransport
 
         $responseContent = $response->toArray(false);
 
-        if ((isset($responseContent['error']) && null !== $responseContent['error']) || (200 !== $response->getStatusCode()) ) {
+        if ((isset($responseContent['error']) && null !== $responseContent['error']) || (200 !== $response->getStatusCode())) {
             throw new TransportException(sprintf('Unable to send the SMS: "%s".', $responseContent['message']), $response);
         }
 

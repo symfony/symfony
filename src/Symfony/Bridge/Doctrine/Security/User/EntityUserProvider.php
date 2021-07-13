cbbb94b383840006eb2bb131fdb1f4410dad0cf5
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-07-13 09:50:21 +0200

    [Doctrine Bridge] Fix an exception message

--- src/Symfony/Bridge/Doctrine/Security/User/EntityUserProvider.php
+++ src/Symfony/Bridge/Doctrine/Security/User/EntityUserProvider.php
@@ -139,7 +139,7 @@ class EntityUserProvider implements UserProviderInterface, PasswordUpgraderInter
             trigger_deprecation('symfony/doctrine-bridge', '5.3', 'The "%s::upgradePassword()" method expects an instance of "%s" as first argument, the "%s" class should implement it.', PasswordUpgraderInterface::class, PasswordAuthenticatedUserInterface::class, get_debug_type($user));
 
             if (!$user instanceof UserInterface) {
-                throw new \TypeError(sprintf('The "%s::upgradePassword()" method expects an instance of "%s" as first argument, "%s" given.', PasswordAuthenticatedUserInterface::class, get_debug_type($user)));
+                throw new \TypeError(sprintf('The "%s()" method expects an instance of "%s" as first argument, "%s" given.', __METHOD__, PasswordAuthenticatedUserInterface::class, get_debug_type($user)));
             }
         }
 

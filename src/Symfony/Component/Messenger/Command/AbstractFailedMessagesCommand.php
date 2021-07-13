a3ba4a9e6be25e4caf4e5ff4f32bad0c3404674a
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-07-13 10:54:50 +0200

    [Messenger] Fixed an exception message

--- src/Symfony/Component/Messenger/Command/AbstractFailedMessagesCommand.php
+++ src/Symfony/Component/Messenger/Command/AbstractFailedMessagesCommand.php
@@ -188,7 +188,7 @@ abstract class AbstractFailedMessagesCommand extends Command
         $name = \func_num_args() > 0 ? func_get_arg(0) : null;
 
         if (null === $name = $name ?? $this->globalFailureReceiverName) {
-            throw new InvalidArgumentException(sprintf('No default failure transport is defined. Available transports are: "%s".', $name, implode('", "', array_keys($this->failureTransports->getProvidedServices()))));
+            throw new InvalidArgumentException(sprintf('No default failure transport is defined. Available transports are: "%s".', implode('", "', array_keys($this->failureTransports->getProvidedServices()))));
         }
 
         if (!$this->failureTransports->has($name)) {

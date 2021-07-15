0a23fed4092cbb1448053cf7feb078eddbf53dbe
Alexander Schranz <alexander@sulu.io>
2021-07-15 21:47:44 +0200

    Update src/Symfony/Component/Messenger/HandleTrait.php
    Co-authored-by: Oskar Stark <oskarstark@googlemail.com>

--- src/Symfony/Component/Messenger/HandleTrait.php
+++ src/Symfony/Component/Messenger/HandleTrait.php
@@ -31,7 +31,7 @@ trait HandleTrait
      * the last one usually returning the handler result.
      *
      * @param object|Envelope $message The message or the message pre-wrapped in an envelope
-     * @param StampInterface[] $stamps Stamps to be set on the Envelope which are used to control middlewares behaviours
+     * @param StampInterface[] $stamps Stamps to be set on the Envelope which are used to control middlewares behaviors
      */
     private function handle(object $message, array $stamps = []): mixed
     {

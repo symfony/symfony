1c935ce9cbbd783ad06de3e4b081ba6abf363168
Valentin Nazarov <i.kozlice@protonmail.com>
2021-07-19 12:05:27 +0300

    [Notifier] Add bridge for smsc.ru

--- src/Symfony/Component/Notifier/Exception/UnsupportedSchemeException.php
+++ src/Symfony/Component/Notifier/Exception/UnsupportedSchemeException.php
@@ -144,6 +144,10 @@ class UnsupportedSchemeException extends LogicException
             'class' => Bridge\SmsBiuras\SmsBiurasTransportFactory::class,
             'package' => 'symfony/sms-biuras-notifier',
         ],
+        'smsc' => [
+            'class' => Bridge\Smsc\SmscTransportFactory::class,
+            'package' => 'symfony/smsc-notifier',
+        ],
         'spothit' => [
             'class' => Bridge\SpotHit\SpotHitTransportFactory::class,
             'package' => 'symfony/spot-hit-notifier',

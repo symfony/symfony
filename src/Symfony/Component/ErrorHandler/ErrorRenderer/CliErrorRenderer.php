2ca42c3ca368d63d3d765f0617b3eebcca6d4336
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-07-26 15:43:25 +0200

    [ErrorHandler] Improve error messages in CLI

--- src/Symfony/Component/ErrorHandler/ErrorRenderer/CliErrorRenderer.php
+++ src/Symfony/Component/ErrorHandler/ErrorRenderer/CliErrorRenderer.php
@@ -40,7 +40,44 @@ class CliErrorRenderer implements ErrorRendererInterface
             }
         };
 
-        return FlattenException::createFromThrowable($exception)
-            ->setAsString($dumper->dump($cloner->cloneVar($exception), true));
+        $flattenException = FlattenException::createFromThrowable($exception);
+        $exceptionTraceDump = $dumper->dump($cloner->cloneVar($exception)['trace'], true);
+
+        return $flattenException->setAsString($this->doRender($exception, $exceptionTraceDump));
+    }
+
+    private function doRender(\Throwable $exception, string $traceAsString): string
+    {
+        $resetStyle = "\033[0m";
+        $textBold = "\033[1m";
+        $textBrightWhite = "\033[37;1m";
+        $textBrightRed = "\033[31;1m";
+        $textGray = "\033[38;5;245m";
+
+        $result = '';
+
+        $result .= $textBold.$textBrightWhite."Exception Trace".$resetStyle."\n";
+        $result .= $traceAsString."\n";
+
+        $result .= $textBold.$textBrightRed.\get_class($exception).$resetStyle."\n";
+        $result .= $exception->getMessage()."\n\n";
+
+        $sourceCode =file($exception->getFile());
+        $sourceCodeExtract = [];
+        for ($i = $exception->getLine() - 5; $i <= $exception->getLine() + 4; ++$i) {
+            $sourceCodeExtract[$i] = ($sourceCode[$i] ?? '')."\n";
+        }
+
+        $result .= $textGray.sprintf('at %s:%d', $exception->getFile(), $exception->getLine()).$resetStyle."\n";
+        foreach ($sourceCodeExtract as $lineNumber => $code) {
+            if ($lineNumber === $exception->getLine()) {
+                $result .= sprintf("%s~~~~>%s %s\n", $textBrightRed, $resetStyle, rtrim($code));
+            } else {
+                $result .= sprintf("%s%3d |%s %s\n", $textGray, $lineNumber, $resetStyle, rtrim($code));
+            }
+        }
+        $result .= "\n\n";
+
+        return $result;
     }
 }

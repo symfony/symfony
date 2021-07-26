b43da3a714da28d79dabb61a8a1e9a3f747e26aa
Fabien Salathe <fabacrans@gmail.com>
2021-07-26 17:08:08 +0200

    [FrameworkBundle] Update cache:clear help

--- src/Symfony/Bundle/FrameworkBundle/Command/CacheClearCommand.php
+++ src/Symfony/Bundle/FrameworkBundle/Command/CacheClearCommand.php
@@ -59,7 +59,7 @@ class CacheClearCommand extends Command
             ])
             ->setDescription('Clear the cache')
             ->setHelp(<<<'EOF'
-The <info>%command.name%</info> command clears the application cache for a given environment
+The <info>%command.name%</info> command clears and warms up the application cache for a given environment
 and debug mode:
 
   <info>php %command.full_name% --env=dev</info>

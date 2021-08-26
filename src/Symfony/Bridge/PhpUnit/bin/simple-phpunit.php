d1eda26a92f2fca20cedd3415b5c6efef3e38d8b
Nicolas Grekas <nicolas.grekas@gmail.com>
2021-08-26 15:38:20 +0200

    Add no-interaction for SYMFONY_PHPUNIT_REMOVE

--- src/Symfony/Bridge/PhpUnit/bin/simple-phpunit.php
+++ src/Symfony/Bridge/PhpUnit/bin/simple-phpunit.php
@@ -226,7 +226,7 @@ if (!file_exists("$PHPUNIT_DIR/$PHPUNIT_VERSION_DIR/phpunit") || $configurationH
     @copy("$PHPUNIT_VERSION_DIR/phpunit.xsd", 'phpunit.xsd');
     chdir("$PHPUNIT_VERSION_DIR");
     if ($SYMFONY_PHPUNIT_REMOVE) {
-        $passthruOrFail("$COMPOSER remove --no-update ".$SYMFONY_PHPUNIT_REMOVE);
+        $passthruOrFail("$COMPOSER remove --no-update --no-interaction ".$SYMFONY_PHPUNIT_REMOVE);
     }
     if (5.1 <= $PHPUNIT_VERSION && $PHPUNIT_VERSION < 5.4) {
         $passthruOrFail("$COMPOSER require --no-update phpunit/phpunit-mock-objects \"~3.1.0\"");

8e6763a3e869506c08d3e3ac6f7980b36b0a6290
Simon Berger <simon_-berger@web.de>
2021-09-15 14:21:05 +0200

    [Form] Don't trim unassigned unicode characters anymore

--- src/Symfony/Component/Form/Util/StringUtil.php
+++ src/Symfony/Component/Form/Util/StringUtil.php
@@ -33,7 +33,7 @@ class StringUtil
      */
     public static function trim($string)
     {
-        if (null !== $result = @preg_replace('/^[\pZ\pC]+|[\pZ\pC]+$/u', '', $string)) {
+        if (null !== $result = @preg_replace('/^[\pZ\p{Cc}\p{Cf}]+|[\pZ\p{Cc}\p{Cf}]+$/u', '', $string)) {
             return $result;
         }
 

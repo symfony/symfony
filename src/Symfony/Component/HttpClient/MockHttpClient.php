2e028cccef4e917b6c4fbbdc5a5b9edf2db91df2
Volodymyr Kupriienko <vldmr.kuprienko@gmail.com>
2021-10-01 20:55:23 +0300

    [HttpClient] Add method to set response factory in mock client

--- src/Symfony/Component/HttpClient/MockHttpClient.php
+++ src/Symfony/Component/HttpClient/MockHttpClient.php
@@ -36,6 +36,15 @@ class MockHttpClient implements HttpClientInterface
      */
     public function __construct($responseFactory = null, ?string $baseUri = 'https://example.com')
     {
+        $this->setResponseFactory($responseFactory);
+        $this->defaultOptions['base_uri'] = $baseUri;
+    }
+
+    /**
+     * @param callable|callable[]|ResponseInterface|ResponseInterface[]|iterable|null $responseFactory
+     */
+    public function setResponseFactory($responseFactory): void
+    {
         if ($responseFactory instanceof ResponseInterface) {
             $responseFactory = [$responseFactory];
         }
@@ -47,7 +56,6 @@ class MockHttpClient implements HttpClientInterface
         }
 
         $this->responseFactory = $responseFactory;
-        $this->defaultOptions['base_uri'] = $baseUri;
     }
 
     /**

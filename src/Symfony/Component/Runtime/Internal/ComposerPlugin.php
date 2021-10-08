13f69f2e28f28804813e3babd6f86a1bb06fb7a6
Piotr Kugla <piku235@gmail.com>
2021-10-08 19:16:19 +0200

    [Runtime] Drop class validation of composer "extra.runtime.class"

--- src/Symfony/Component/Runtime/Internal/ComposerPlugin.php
+++ src/Symfony/Component/Runtime/Internal/ComposerPlugin.php
@@ -18,7 +18,6 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\ScriptEvents;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Runtime\RuntimeInterface;
 use Symfony\Component\Runtime\SymfonyRuntime;
 
 /**
@@ -97,10 +96,6 @@ class ComposerPlugin implements PluginInterface, EventSubscriberInterface
         }
 
         $runtimeClass = $extra['class'] ?? SymfonyRuntime::class;
-
-        if (SymfonyRuntime::class !== $runtimeClass && !is_subclass_of($runtimeClass, RuntimeInterface::class)) {
-            throw new \InvalidArgumentException(sprintf('Class "%s" listed under "extra.runtime.class" in your composer.json file '.(class_exists($runtimeClass) ? 'should implement "%s".' : 'not found.'), $runtimeClass, RuntimeInterface::class));
-        }
 
         unset($extra['class'], $extra['autoload_template']);
 

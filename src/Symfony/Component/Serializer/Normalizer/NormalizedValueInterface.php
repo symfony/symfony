8586a317e7501d82af58b0e2e4d13ad12218807e
bjorn <bjorn@swis.nl>
2021-10-14 12:25:10 +0200

    [Serializer] Fix codestyle as provided by fabbot

--- src/Symfony/Component/Serializer/Normalizer/NormalizedValueInterface.php
+++ src/Symfony/Component/Serializer/Normalizer/NormalizedValueInterface.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Component\Serializer\Normalizer;
 
-interface NormalizedValueInterface {
-
+interface NormalizedValueInterface
+{
     public function getNormalization(): array|string|int|float|bool|\ArrayObject|null;
-    
 }

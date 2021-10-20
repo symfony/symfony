5caaf23b7d6427ffb0c7859ea88b13aa2ded05aa
Jakub Caban <kuba.iluvatar@gmail.com>
2021-10-20 11:29:01 +0200

    [Translation] Add implode-based translatable message

---
+++ src/Symfony/Component/Translation/Message/ImplodedTranslatableMessage.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Translation\Message;
+
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @author Jakub Caban <kuba.iluvatar@gmail.com>
+ */
+class ImplodedTranslatableMessage implements TranslatableInterface
+{
+    use TranslatableParametersTrait;
+
+    private $glue;
+
+    public function __construct(
+        string $glue = '',
+        array $parameters = []
+    ) {
+        $this->glue = $glue;
+        $this->parameters = $parameters;
+    }
+
+    public function getGlue(): string
+    {
+        return $this->glue;
+    }
+
+    public function trans(TranslatorInterface $translator, string $locale = null): string
+    {
+        return implode(
+            $this->getGlue(),
+            $this->getTranslatedParameters($translator, $locale)
+        );
+    }
+}

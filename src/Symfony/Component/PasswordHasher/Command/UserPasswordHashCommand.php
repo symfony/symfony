49f45a97798f5df69418739d9bc87cd8829ba359
noniagriconomie <antoine.makdessi@agriconomie.com>
2021-10-22 12:59:09 +0200

    [Console] Add autocompletion for security commands

--- src/Symfony/Component/PasswordHasher/Command/UserPasswordHashCommand.php
+++ src/Symfony/Component/PasswordHasher/Command/UserPasswordHashCommand.php
@@ -12,6 +12,8 @@
 namespace Symfony\Component\PasswordHasher\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Completion\CompletionInput;
+use Symfony\Component\Console\Completion\CompletionSuggestions;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
@@ -166,6 +168,15 @@ EOF
         $errorIo->success('Password hashing succeeded');
 
         return 0;
+    }
+
+    public function complete(CompletionInput $input, CompletionSuggestions $suggestions): void
+    {
+        if ($input->mustSuggestArgumentValuesFor('user-class')) {
+            $suggestions->suggestValues($this->userClasses);
+
+            return;
+        }
     }
 
     /**

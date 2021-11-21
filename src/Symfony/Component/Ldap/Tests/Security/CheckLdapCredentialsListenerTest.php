30234490724ada7997dacc0f108fd34c7641f02d
Alexander M. Turek <me@derrabus.de>
2021-11-21 10:01:59 +0100

    Fix test

--- src/Symfony/Component/Ldap/Tests/Security/CheckLdapCredentialsListenerTest.php
+++ src/Symfony/Component/Ldap/Tests/Security/CheckLdapCredentialsListenerTest.php
@@ -194,27 +194,25 @@ class CheckLdapCredentialsListenerTest extends TestCase
     }
 }
 
-if (interface_exists(AuthenticatorInterface::class)) {
-    class TestAuthenticator implements AuthenticatorInterface
+class TestAuthenticator implements AuthenticatorInterface
+{
+    public function supports(Request $request): ?bool
     {
-        public function supports(Request $request): ?bool
-        {
-        }
+    }
 
-        public function authenticate(Request $request): PassportInterface
-        {
-        }
+    public function authenticate(Request $request): PassportInterface
+    {
+    }
 
-        public function createAuthenticatedToken(PassportInterface $passport, string $firewallName): TokenInterface
-        {
-        }
+    public function createAuthenticatedToken(PassportInterface $passport, string $firewallName): TokenInterface
+    {
+    }
 
-        public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-        {
-        }
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+    }
 
-        public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-        {
-        }
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
     }
 }

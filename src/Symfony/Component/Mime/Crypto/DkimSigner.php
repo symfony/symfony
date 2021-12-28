52ceda78e48d4018a4a29099b0006f3584e2882c
Antoine Lamirault <lamiraultantoine@gmail.com>
2021-12-28 14:57:09 +0100

    [Mime] Fix missing sprintf in DkimSigner

--- src/Symfony/Component/Mime/Crypto/DkimSigner.php
+++ src/Symfony/Component/Mime/Crypto/DkimSigner.php
@@ -65,7 +65,7 @@ final class DkimSigner
     {
         $options += $this->defaultOptions;
         if (!\in_array($options['algorithm'], [self::ALGO_SHA256, self::ALGO_ED25519], true)) {
-            throw new InvalidArgumentException('Invalid DKIM signing algorithm "%s".', $options['algorithm']);
+            throw new InvalidArgumentException(sprintf('Invalid DKIM signing algorithm "%s".', $options['algorithm']));
         }
         $headersToIgnore['return-path'] = true;
         $headersToIgnore['x-transport'] = true;
@@ -205,7 +205,7 @@ final class DkimSigner
         }
 
         // Add trailing Line return if last line is non empty
-        if (\strlen($currentLine) > 0) {
+        if ('' !== $currentLine) {
             hash_update($hash, "\r\n");
             $length += \strlen("\r\n");
         }

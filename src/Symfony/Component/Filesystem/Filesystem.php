6d3f3d74ef752ebc35f8d792b530da16e5e1328a
Florian Wolfsjäger <florian@wolfsjaeger.at>
2021-12-28 14:49:47 +0100

    [Filesystem] Renamed reused variable `$files` in `Filesystem::doRemove()`

--- src/Symfony/Component/Filesystem/Filesystem.php
+++ src/Symfony/Component/Filesystem/Filesystem.php
@@ -187,8 +187,8 @@ class Filesystem
                     }
                 }
 
-                $files = new \FilesystemIterator($file, \FilesystemIterator::CURRENT_AS_PATHNAME | \FilesystemIterator::SKIP_DOTS);
-                self::doRemove(iterator_to_array($files, true), true);
+                $filesystemIterator = new \FilesystemIterator($file, \FilesystemIterator::CURRENT_AS_PATHNAME | \FilesystemIterator::SKIP_DOTS);
+                self::doRemove(iterator_to_array($filesystemIterator, true), true);
 
                 if (!self::box('rmdir', $file) && file_exists($file) && !$isRecursive) {
                     $lastError = self::$lastError;

986319948037242e86abc26073a37a21a0282815
Smaine Milianni <smaine.milianni@gmail.com>
2022-01-10 09:44:53 +0100

    replace two options by a format option

--- src/Symfony/Component/Form/Extension/Core/Type/CountryType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/CountryType.php
@@ -22,6 +22,10 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CountryType extends AbstractType
 {
+    public const FORMAT_WITH_NAME = 'format_with_name';
+    public const FORMAT_WITH_FLAG = 'format_with_flag';
+    public const FORMAT_WITH_FLAG_AND_NAME = 'format_with_flag_and_name';
+
     /**
      * {@inheritdoc}
      */
@@ -35,24 +39,21 @@ class CountryType extends AbstractType
 
                 $choiceTranslationLocale = $options['choice_translation_locale'];
                 $alpha3 = $options['alpha3'];
-                $withEmoji = $options['with_emoji'];
-                $onlyEmoji = $options['only_emoji'];
+                $format = $options['format'];
 
-                if ($onlyEmoji && $withEmoji) {
-                    throw new LogicException(sprintf('Options "%s" and "%s" cannot be true together', 'with_emoji', 'only_emoji'));
-                }
+                return ChoiceList::loader($this, new IntlCallbackChoiceLoader(function () use ($choiceTranslationLocale, $alpha3, $format) {
+                    $countriesCode = $alpha3 ? Countries::getAlpha3Names($choiceTranslationLocale) : Countries::getNames($choiceTranslationLocale);
+                    $choices = $countriesCode;
+                    if (self::FORMAT_WITH_FLAG === $format) {
+                        foreach ($countriesCode as $countryCode => $displayed) {
+                            $choices[$countryCode] = self::getEmojiFlag($countryCode);
+                        }
+                    } elseif (self::FORMAT_WITH_FLAG_AND_NAME === $format) {
+                        $choices = [];
+                        foreach ($countriesCode as $countryCode => $displayed) {
+                            $choices[$countryCode] = sprintf('%s %s', self::getEmojiFlag($countryCode), $displayed);
+                        }
 
-                return ChoiceList::loader($this, new IntlCallbackChoiceLoader(function () use ($choiceTranslationLocale, $alpha3, $withEmoji, $onlyEmoji) {
-                    $names = $alpha3 ? Countries::getAlpha3Names($choiceTranslationLocale) : Countries::getNames($choiceTranslationLocale);
-                    $choices = $names;
-                    if (true === $onlyEmoji) {
-                        foreach ($names as $name => $displayed) {
-                            $choices[$name] = self::getEmojiFlag($name);
-                        }
-                    } elseif (true === $withEmoji) {
-                        foreach ($names as $name => $displayed) {
-                            $choices[$name] = sprintf('%s - %s', self::getEmojiFlag($name), $displayed);
-                        }
                     }
 
                     return array_flip($choices);
@@ -62,12 +63,13 @@ class CountryType extends AbstractType
             'choice_translation_locale' => null,
             'alpha3' => false,
             'invalid_message' => 'Please select a valid country.',
-            'with_emoji' => false,
-            'only_emoji' => false,
+            'format' => self::FORMAT_WITH_NAME,
         ]);
 
         $resolver->setAllowedTypes('choice_translation_locale', ['null', 'string']);
         $resolver->setAllowedTypes('alpha3', 'bool');
+        $resolver->setAllowedTypes('format', 'string');
+        $resolver->setAllowedValues('format', [self::FORMAT_WITH_NAME, self::FORMAT_WITH_FLAG, self::FORMAT_WITH_FLAG_AND_NAME]);
     }
 
     /**
@@ -86,15 +88,11 @@ class CountryType extends AbstractType
         return 'country';
     }
 
-    private static function getEmojiFlag(string $name): string
-    {
-        return self::toRegionalSymbol($name, 0) . self::toRegionalSymbol($name, 1);
-    }
-
-    private static function toRegionalSymbol(string $name, int $position): string
+    private static function getEmojiFlag(string $countryCode): string
     {
         $regionalOffset = 0x1F1A5;
 
-        return mb_chr($regionalOffset + mb_ord(substr($name, $position, 1), 'UTF-8'), 'UTF-8');
+        return mb_chr($regionalOffset + mb_ord($countryCode[0], 'UTF-8'), 'UTF-8')
+            . mb_chr($regionalOffset + mb_ord($countryCode[1], 'UTF-8'), 'UTF-8');
     }
 }

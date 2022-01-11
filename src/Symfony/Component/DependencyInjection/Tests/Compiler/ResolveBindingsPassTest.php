f9f78c7c2a35afff559366f10ca0b46130691580
vladimir.panivko <vladimir.panivko@together.com>
2022-01-11 08:44:24 +0200

    ResolveBindingsPass remove loading of class iterable

--- src/Symfony/Component/DependencyInjection/Tests/Compiler/ResolveBindingsPassTest.php
+++ src/Symfony/Component/DependencyInjection/Tests/Compiler/ResolveBindingsPassTest.php
@@ -27,6 +27,7 @@ use Symfony\Component\DependencyInjection\Tests\Fixtures\CaseSensitiveClass;
 use Symfony\Component\DependencyInjection\Tests\Fixtures\FooUnitEnum;
 use Symfony\Component\DependencyInjection\Tests\Fixtures\NamedArgumentsDummy;
 use Symfony\Component\DependencyInjection\Tests\Fixtures\NamedEnumArgumentDummy;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\NamedIterableArgumentDummy;
 use Symfony\Component\DependencyInjection\Tests\Fixtures\ParentNotExists;
 use Symfony\Component\DependencyInjection\TypedReference;
 
@@ -208,5 +209,18 @@ class ResolveBindingsPassTest extends TestCase
         $definition->setBindings($bindings);
         $pass = new ResolveBindingsPass();
         $pass->process($container);
+    }
+
+    public function testIterableBindingTypehint()
+    {
+        $container = new ContainerBuilder();
+        $definition = $container->register('bar', NamedIterableArgumentDummy::class);
+        $definition->setBindings([
+            'iterable $items' => new TaggedIteratorArgument('foo'),
+        ]);
+        $pass = new ResolveBindingsPass();
+        $pass->process($container);
+
+        $this->assertInstanceOf(TaggedIteratorArgument::class, $container->getDefinition('bar')->getArgument(0));
     }
 }

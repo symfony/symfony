3d2ed700eb1a78ee137966c380ba61f8316f7ee6
plozmun <lozanomunarriz@gmail.com>
2022-01-12 21:49:45 +0100

    [HttpClient] Remove deprecated usage of GuzzleHttp\Promise\promise_for

--- src/Symfony/Component/HttpClient/Response/HttplugPromise.php
+++ src/Symfony/Component/HttpClient/Response/HttplugPromise.php
@@ -11,7 +11,7 @@
 
 namespace Symfony\Component\HttpClient\Response;
 
-use function GuzzleHttp\Promise\promise_for;
+use GuzzleHttp\Promise\Create;
 use GuzzleHttp\Promise\PromiseInterface as GuzzlePromiseInterface;
 use Http\Promise\Promise as HttplugPromiseInterface;
 use Psr\Http\Message\ResponseInterface as Psr7ResponseInterface;
@@ -74,7 +74,7 @@ final class HttplugPromise implements HttplugPromiseInterface
         }
 
         return static function ($value) use ($callback) {
-            return promise_for($callback($value));
+            return Create::promiseFor($callback($value));
         };
     }
 }

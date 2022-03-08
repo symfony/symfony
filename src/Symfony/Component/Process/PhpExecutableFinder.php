947152b2ff849df9d7510654ae5b99beb3f9685e
Thomas Calvet <calvet.thomas@gmail.com>
2022-03-08 18:16:11 +0100

    [Process] Don't return executable directories in PhpExecutableFinder

--- src/Symfony/Component/Process/PhpExecutableFinder.php
+++ src/Symfony/Component/Process/PhpExecutableFinder.php
@@ -47,6 +47,10 @@ class PhpExecutableFinder
                 }
             }
 
+            if (@is_dir($php)) {
+                return false;
+            }
+
             return $php;
         }
 
@@ -59,7 +63,7 @@ class PhpExecutableFinder
         }
 
         if ($php = getenv('PHP_PATH')) {
-            if (!@is_executable($php)) {
+            if (!@is_executable($php) || @is_dir($php)) {
                 return false;
             }
 
@@ -67,12 +71,12 @@ class PhpExecutableFinder
         }
 
         if ($php = getenv('PHP_PEAR_PHP_BIN')) {
-            if (@is_executable($php)) {
+            if (@is_executable($php) && !@is_dir($php)) {
                 return $php;
             }
         }
 
-        if (@is_executable($php = \PHP_BINDIR.('\\' === \DIRECTORY_SEPARATOR ? '\\php.exe' : '/php'))) {
+        if (@is_executable($php = \PHP_BINDIR.('\\' === \DIRECTORY_SEPARATOR ? '\\php.exe' : '/php')) && !@is_dir($php)) {
             return $php;
         }
 

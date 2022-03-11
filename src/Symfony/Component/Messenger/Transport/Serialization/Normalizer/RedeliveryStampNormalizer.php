07723c93a64f1aa4cb20d6b6ed7f7994ad2fff87
Alessandro Lai <alessandro.lai85@gmail.com>
2022-03-11 12:50:51 +0100

    Add normalization step too

--- src/Symfony/Component/Messenger/Transport/Serialization/Normalizer/RedeliveryStampNormalizer.php
+++ src/Symfony/Component/Messenger/Transport/Serialization/Normalizer/RedeliveryStampNormalizer.php
@@ -12,15 +12,20 @@
 namespace Symfony\Component\Messenger\Transport\Serialization\Normalizer;
 
 use Symfony\Component\Messenger\Stamp\RedeliveryStamp;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class RedeliveryStampNormalizer implements DenormalizerInterface
 {
     public function denormalize($data, string $type, string $format = null, array $context = [])
     {
+        $redeliveredAt = $data['redeliveredAt'] ?? null;
+
         return new RedeliveryStamp(
             $data['retryCount'] ?? 0,
-            $data['redeliveredAt'] ?? null
+            $redeliveredAt ? new \DateTimeImmutable($redeliveredAt) : null
         );
     }
 
@@ -29,6 +34,27 @@ class RedeliveryStampNormalizer implements DenormalizerInterface
         return RedeliveryStamp::class === $type
             && null === ($data['exceptionMessage'] ?? null)
             && null === ($data['flattenException'] ?? null)
+        ;
+    }
+
+    public function normalize($object, string $format = null, array $context = [])
+    {
+        if (! $object instanceof RedeliveryStamp) {
+            throw new InvalidArgumentException();
+        }
+
+        $dateTimeFormat = $context[DateTimeNormalizer::FORMAT_KEY] ?? \DateTimeInterface::RFC3339;
+
+        return [
+            'retryCount' => $object->getRetryCount(),
+            'redeliveredAt' => $object->getRedeliveredAt()->format($dateTimeFormat),
+        ];
+    }
+
+    public function supportsNormalization($data, string $format = null): bool
+    {
+        return Kernel::VERSION_ID >= 50200
+            && $data instanceof RedeliveryStamp
         ;
     }
 }

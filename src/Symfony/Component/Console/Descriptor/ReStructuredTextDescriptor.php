ca7843a42d9a1bf5e2a6924e2b7bf34e82b7116b
Matthew Grasmick <matt.grasmick@gmail.com>
2022-03-11 10:48:39 -0500

    Fix PHPCS issues.

--- src/Symfony/Component/Console/Descriptor/ReStructuredTextDescriptor.php
+++ src/Symfony/Component/Console/Descriptor/ReStructuredTextDescriptor.php
@@ -24,11 +24,10 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class ReStructuredTextDescriptor extends Descriptor
 {
-
-  private $applicationTitleUnderlineChar = '#';
-  private $commandTitleUnderlineChar = '*';
-  private $argumentsTitleUnderlineChar = '=';
-  private $argumentTitleUnderlineChar = '-';
+    private $applicationTitleUnderlineChar = '#';
+    private $commandTitleUnderlineChar = '*';
+    private $argumentsTitleUnderlineChar = '=';
+    private $argumentTitleUnderlineChar = '-';
 
     /**
      * {@inheritdoc}
@@ -52,155 +51,160 @@ class ReStructuredTextDescriptor extends Descriptor
     }
 
     /**
-   * {@inheritdoc}
-   */
-  protected function describeInputArgument(InputArgument $argument, array $options = []) {
-    $this->write(
-      '``' . ($argument->getName() ?: '<none>') . "``\n" . str_repeat($this->argumentTitleUnderlineChar, Helper::width($argument->getName()) + 4) . "\n\n"
-      . ($argument->getDescription() ? preg_replace('/\s*[\r\n]\s*/', "\n", $argument->getDescription()) . "\n\n" : '')
-      . '- Is required: ' . ($argument->isRequired() ? 'yes' : 'no') . "\n"
-      . '- Is array: ' . ($argument->isArray() ? 'yes' : 'no') . "\n"
-      . '- Default: ``' . str_replace("\n", '', var_export($argument->getDefault(), TRUE)) . '``'
+     * {@inheritdoc}
+     */
+    protected function describeInputArgument(InputArgument $argument, array $options = [])
+    {
+        $this->write(
+      '``'.($argument->getName() ?: '<none>')."``\n".str_repeat($this->argumentTitleUnderlineChar, Helper::width($argument->getName()) + 4)."\n\n"
+      .($argument->getDescription() ? preg_replace('/\s*[\r\n]\s*/', "\n", $argument->getDescription())."\n\n" : '')
+      .'- Is required: '.($argument->isRequired() ? 'yes' : 'no')."\n"
+      .'- Is array: '.($argument->isArray() ? 'yes' : 'no')."\n"
+      .'- Default: ``'.str_replace("\n", '', var_export($argument->getDefault(), true)).'``'
     );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function describeInputOption(InputOption $option, array $options = []) {
-    $name = '--' . $option->getName();
-    if ($option->isNegatable()) {
-      $name .= '|--no-' . $option->getName();
-    }
-    if ($option->getShortcut()) {
-      $name .= '|-' . str_replace('|', '|-', $option->getShortcut()) . '';
     }
 
-    $this->write(
-      '``' . $name . '``' . "\n" . str_repeat($this->argumentTitleUnderlineChar, Helper::width($name) + 4) . "\n\n"
-      . ($option->getDescription() ? preg_replace('/\s*[\r\n]\s*/', "\n", $option->getDescription()) . "\n\n" : '')
-      . '- Accept value: ' . ($option->acceptValue() ? 'yes' : 'no') . "\n"
-      . '- Is value required: ' . ($option->isValueRequired() ? 'yes' : 'no') . "\n"
-      . '- Is multiple: ' . ($option->isArray() ? 'yes' : 'no') . "\n"
-      . '- Is negatable: ' . ($option->isNegatable() ? 'yes' : 'no') . "\n"
-      . '- Default: ``' . str_replace("\n", '', var_export($option->getDefault(), TRUE)) . '``'
+    /**
+     * {@inheritdoc}
+     */
+    protected function describeInputOption(InputOption $option, array $options = [])
+    {
+        $name = '--'.$option->getName();
+        if ($option->isNegatable()) {
+            $name .= '|--no-'.$option->getName();
+        }
+        if ($option->getShortcut()) {
+            $name .= '|-'.str_replace('|', '|-', $option->getShortcut()).'';
+        }
+
+        $this->write(
+      '``'.$name.'``'."\n".str_repeat($this->argumentTitleUnderlineChar, Helper::width($name) + 4)."\n\n"
+      .($option->getDescription() ? preg_replace('/\s*[\r\n]\s*/', "\n", $option->getDescription())."\n\n" : '')
+      .'- Accept value: '.($option->acceptValue() ? 'yes' : 'no')."\n"
+      .'- Is value required: '.($option->isValueRequired() ? 'yes' : 'no')."\n"
+      .'- Is multiple: '.($option->isArray() ? 'yes' : 'no')."\n"
+      .'- Is negatable: '.($option->isNegatable() ? 'yes' : 'no')."\n"
+      .'- Default: ``'.str_replace("\n", '', var_export($option->getDefault(), true)).'``'
     );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function describeInputDefinition(InputDefinition $definition, array $options = []) {
-    if ($showArguments = \count($definition->getArguments()) > 0) {
-      $this->write("Arguments\n" . str_repeat($this->argumentsTitleUnderlineChar, 9)) . "\n\n";
-      foreach ($definition->getArguments() as $argument) {
-        $this->write("\n\n");
-        if (NULL !== $describeInputArgument = $this->describeInputArgument($argument)) {
-          $this->write($describeInputArgument);
-        }
-      }
     }
 
-    if (\count($definition->getOptions()) > 0) {
-      if ($showArguments) {
-        $this->write("\n\n");
-      }
-
-      $this->write("Options\n" . str_repeat($this->argumentsTitleUnderlineChar, 7)) . "\n\n";
-      foreach ($definition->getOptions() as $option) {
-        $this->write("\n\n");
-        if (NULL !== $describeInputOption = $this->describeInputOption($option)) {
-          $this->write($describeInputOption);
+    /**
+     * {@inheritdoc}
+     */
+    protected function describeInputDefinition(InputDefinition $definition, array $options = [])
+    {
+        if ($showArguments = \count($definition->getArguments()) > 0) {
+            $this->write("Arguments\n".str_repeat($this->argumentsTitleUnderlineChar, 9))."\n\n";
+            foreach ($definition->getArguments() as $argument) {
+                $this->write("\n\n");
+                if (null !== $describeInputArgument = $this->describeInputArgument($argument)) {
+                    $this->write($describeInputArgument);
+                }
+            }
         }
-      }
-    }
-  }
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function describeCommand(Command $command, array $options = []) {
-    if ($options['short'] ?? FALSE) {
-      $this->write(
-        '``' . $command->getName() . "``\n"
-        . str_repeat($this->commandTitleUnderlineChar, Helper::width($command->getName()) + 4) . "\n\n"
-        . ($command->getDescription() ? $command->getDescription() . "\n\n" : '')
-        . "Usage\n" . str_repeat($this->argumentsTitleUnderlineChar, 5) . "\n\n"
-        . array_reduce($command->getAliases(), function ($carry, $usage) {
-          return $carry . '- ``' . $usage . '``' . "\n";
+        if (\count($definition->getOptions()) > 0) {
+            if ($showArguments) {
+                $this->write("\n\n");
+            }
+
+            $this->write("Options\n".str_repeat($this->argumentsTitleUnderlineChar, 7))."\n\n";
+            foreach ($definition->getOptions() as $option) {
+                $this->write("\n\n");
+                if (null !== $describeInputOption = $this->describeInputOption($option)) {
+                    $this->write($describeInputOption);
+                }
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function describeCommand(Command $command, array $options = [])
+    {
+        if ($options['short'] ?? false) {
+            $this->write(
+        '``'.$command->getName()."``\n"
+        .str_repeat($this->commandTitleUnderlineChar, Helper::width($command->getName()) + 4)."\n\n"
+        .($command->getDescription() ? $command->getDescription()."\n\n" : '')
+        ."Usage\n".str_repeat($this->argumentsTitleUnderlineChar, 5)."\n\n"
+        .array_reduce($command->getAliases(), function ($carry, $usage) {
+            return $carry.'- ``'.$usage.'``'."\n";
         })
       );
 
-      return;
-    }
+            return;
+        }
 
-    $command->mergeApplicationDefinition(FALSE);
+        $command->mergeApplicationDefinition(false);
 
-    foreach ($command->getAliases() as $alias) {
-      $this->write('.. _' . $alias . ":\n\n");
-    }
-    $this->write(
-      '``' . $command->getName() . "``\n"
-      . str_repeat($this->commandTitleUnderlineChar, Helper::width($command->getName()) + 4) . "\n\n"
-      . ($command->getDescription() ? $command->getDescription() . "\n\n" : '')
-      . "Usage\n" . str_repeat($this->argumentsTitleUnderlineChar, 5) . "\n\n"
-      . array_reduce(array_merge([$command->getSynopsis()], $command->getAliases(), $command->getUsages()), function ($carry, $usage) {
-        return $carry . '- ``' . $usage . '``' . "\n";
+        foreach ($command->getAliases() as $alias) {
+            $this->write('.. _'.$alias.":\n\n");
+        }
+        $this->write(
+      '``'.$command->getName()."``\n"
+      .str_repeat($this->commandTitleUnderlineChar, Helper::width($command->getName()) + 4)."\n\n"
+      .($command->getDescription() ? $command->getDescription()."\n\n" : '')
+      ."Usage\n".str_repeat($this->argumentsTitleUnderlineChar, 5)."\n\n"
+      .array_reduce(array_merge([$command->getSynopsis()], $command->getAliases(), $command->getUsages()), function ($carry, $usage) {
+          return $carry.'- ``'.$usage.'``'."\n";
       })
     );
 
-    if ($help = $command->getProcessedHelp()) {
-      $this->write("\n");
-      $this->write($help);
+        if ($help = $command->getProcessedHelp()) {
+            $this->write("\n");
+            $this->write($help);
+        }
+
+        $definition = $command->getDefinition();
+        if ($definition->getOptions() || $definition->getArguments()) {
+            $this->write("\n\n");
+            $this->describeInputDefinition($definition);
+        }
     }
 
-    $definition = $command->getDefinition();
-    if ($definition->getOptions() || $definition->getArguments()) {
-      $this->write("\n\n");
-      $this->describeInputDefinition($definition);
-    }
-  }
+    /**
+     * {@inheritdoc}
+     */
+    protected function describeApplication(Application $application, array $options = [])
+    {
+        $describedNamespace = $options['namespace'] ?? null;
+        $description = new ApplicationDescription($application, $describedNamespace);
+        $title = $this->getApplicationTitle($application);
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function describeApplication(Application $application, array $options = []) {
-    $describedNamespace = $options['namespace'] ?? NULL;
-    $description = new ApplicationDescription($application, $describedNamespace);
-    $title = $this->getApplicationTitle($application);
+        $this->write($title."\n".str_repeat($this->applicationTitleUnderlineChar, Helper::width($title)));
 
-    $this->write($title . "\n" . str_repeat($this->applicationTitleUnderlineChar, Helper::width($title)));
+        foreach ($description->getNamespaces() as $namespace) {
+            if (ApplicationDescription::GLOBAL_NAMESPACE !== $namespace['id']) {
+                $this->write("\n\n");
+                $this->write('**'.$namespace['id'].':**');
+            }
 
-    foreach ($description->getNamespaces() as $namespace) {
-      if (ApplicationDescription::GLOBAL_NAMESPACE !== $namespace['id']) {
-        $this->write("\n\n");
-        $this->write('**' . $namespace['id'] . ':**');
-      }
+            $this->write("\n\n");
+            $this->write(implode("\n", array_map(function ($commandName) {
+                return sprintf('- `%s`_', $commandName);
+            }, $namespace['commands'])));
+        }
 
-      $this->write("\n\n");
-      $this->write(implode("\n", array_map(function ($commandName) {
-        return sprintf('- `%s`_', $commandName);
-      }, $namespace['commands'])));
-    }
-
-    foreach ($description->getCommands() as $command) {
-      $this->write("\n\n");
-      if (NULL !== $describeCommand = $this->describeCommand($command, $options)) {
-        $this->write($describeCommand);
-      }
-    }
-  }
-
-  private function getApplicationTitle(Application $application): string {
-    if ('UNKNOWN' !== $application->getName()) {
-      if ('UNKNOWN' !== $application->getVersion()) {
-        return sprintf('%s %s', $application->getName(), $application->getVersion());
-      }
-
-      return $application->getName();
+        foreach ($description->getCommands() as $command) {
+            $this->write("\n\n");
+            if (null !== $describeCommand = $this->describeCommand($command, $options)) {
+                $this->write($describeCommand);
+            }
+        }
     }
 
-    return 'Console Tool';
-  }
+    private function getApplicationTitle(Application $application): string
+    {
+        if ('UNKNOWN' !== $application->getName()) {
+            if ('UNKNOWN' !== $application->getVersion()) {
+                return sprintf('%s %s', $application->getName(), $application->getVersion());
+            }
 
+            return $application->getName();
+        }
+
+        return 'Console Tool';
+    }
 }

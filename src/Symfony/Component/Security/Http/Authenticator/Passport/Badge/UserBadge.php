3ed91c4cce1754d5ae1700a6bbd5ab32d5bcafb9
Fabien Potencier <fabien@potencier.org>
2022-03-27 19:26:32 +0200

    bug #45452 [Security] Fix UserNotFoundException is not thrown (damienfa)
    This PR was merged into the 5.4 branch.
    
    Discussion
    ----------
    
    [Security] Fix UserNotFoundException is not thrown
    
    | Q             | A
    | ------------- | ---
    | Branch?       |  5.4
    | Bug fix?      | yes
    | New feature?  | no
    | Deprecations? | no
    | Tickets       | Fix #45070
    | License       | MIT
    | Doc PR        |  N/A
    
    Commits
    -------
    
    7e0ed85d9a Fix issue 45070 :  UserNotFoundException is not thrown

--- src/Symfony/Component/Security/Http/Authenticator/Passport/Badge/UserBadge.php
+++ src/Symfony/Component/Security/Http/Authenticator/Passport/Badge/UserBadge.php
@@ -13,6 +13,7 @@ namespace Symfony\Component\Security\Http\Authenticator\Passport\Badge;
 
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\AuthenticationServiceException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\EventListener\UserProviderListener;
 
@@ -65,6 +66,15 @@ class UserBadge implements BadgeInterface
             }
 
             $this->user = ($this->userLoader)($this->userIdentifier);
+
+            // No user has been found via the $this->userLoader callback
+            if (null === $this->user) {
+                $exception = new UserNotFoundException();
+                $exception->setUserIdentifier($this->userIdentifier);
+
+                throw $exception;
+            }
+
             if (!$this->user instanceof UserInterface) {
                 throw new AuthenticationServiceException(sprintf('The user provider must return a UserInterface object, "%s" given.', get_debug_type($this->user)));
             }

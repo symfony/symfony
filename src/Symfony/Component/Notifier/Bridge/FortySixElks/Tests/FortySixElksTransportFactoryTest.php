61c13ec6159106793445dda0f7487e42f1c1abe8
Grégoire Pineau <lyrixx@lyrixx.info>
2022-04-12 18:34:08 +0200

    Add missing license header

--- src/Symfony/Component/Notifier/Bridge/FortySixElks/Tests/FortySixElksTransportFactoryTest.php
+++ src/Symfony/Component/Notifier/Bridge/FortySixElks/Tests/FortySixElksTransportFactoryTest.php
@@ -6,6 +6,7 @@
  * (c) Fabien Potencier <fabien@symfony.com>
  *
  * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Symfony\Component\Notifier\Bridge\FortySixElks\Tests;

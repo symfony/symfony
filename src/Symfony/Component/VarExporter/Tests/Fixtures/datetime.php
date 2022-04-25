a09e2b4ac57fb0dd4226f7439e45d04236ee25ce
Nicolas Grekas <nicolas.grekas@gmail.com>
2022-04-25 09:39:59 -0700

    [VarExporter] Fix exporting DateTime objects on PHP 8.2

--- src/Symfony/Component/VarExporter/Tests/Fixtures/datetime.php
+++ src/Symfony/Component/VarExporter/Tests/Fixtures/datetime.php
@@ -1,13 +1,15 @@
 <?php
 
 return \Symfony\Component\VarExporter\Internal\Hydrator::hydrate(
-    $o = \Symfony\Component\VarExporter\Internal\Registry::unserialize([], [
-        'O:8:"DateTime":3:{s:4:"date";s:26:"1970-01-01 00:00:00.000000";s:13:"timezone_type";i:1;s:8:"timezone";s:6:"+00:00";}',
-        'O:17:"DateTimeImmutable":3:{s:4:"date";s:26:"1970-01-01 00:00:00.000000";s:13:"timezone_type";i:1;s:8:"timezone";s:6:"+00:00";}',
-        'O:12:"DateTimeZone":2:{s:13:"timezone_type";i:3;s:8:"timezone";s:12:"Europe/Paris";}',
-        'O:12:"DateInterval":16:{s:1:"y";i:0;s:1:"m";i:0;s:1:"d";i:7;s:1:"h";i:0;s:1:"i";i:0;s:1:"s";i:0;s:1:"f";d:0;s:7:"weekday";i:0;s:16:"weekday_behavior";i:0;s:17:"first_last_day_of";i:0;s:6:"invert";i:0;s:4:"days";i:7;s:12:"special_type";i:0;s:14:"special_amount";i:0;s:21:"have_weekday_relative";i:0;s:21:"have_special_relative";i:0;}',
-        'O:10:"DatePeriod":6:{s:5:"start";O:8:"DateTime":3:{s:4:"date";s:26:"2009-10-11 00:00:00.000000";s:13:"timezone_type";i:3;s:8:"timezone";s:12:"Europe/Paris";}s:7:"current";N;s:3:"end";N;s:8:"interval";O:12:"DateInterval":16:{s:1:"y";i:0;s:1:"m";i:0;s:1:"d";i:7;s:1:"h";i:0;s:1:"i";i:0;s:1:"s";i:0;s:1:"f";d:0;s:7:"weekday";i:0;s:16:"weekday_behavior";i:0;s:17:"first_last_day_of";i:0;s:6:"invert";i:0;s:4:"days";i:7;s:12:"special_type";i:0;s:14:"special_amount";i:0;s:21:"have_weekday_relative";i:0;s:21:"have_special_relative";i:0;}s:11:"recurrences";i:5;s:18:"include_start_date";b:1;}',
-    ]),
+    $o = [
+        clone (($p = &\Symfony\Component\VarExporter\Internal\Registry::$prototypes)['DateTime'] ?? \Symfony\Component\VarExporter\Internal\Registry::p('DateTime')),
+        clone ($p['DateTimeImmutable'] ?? \Symfony\Component\VarExporter\Internal\Registry::p('DateTimeImmutable')),
+        clone ($p['DateTimeZone'] ?? \Symfony\Component\VarExporter\Internal\Registry::p('DateTimeZone')),
+        clone ($p['DateInterval'] ?? \Symfony\Component\VarExporter\Internal\Registry::p('DateInterval')),
+        clone ($p['DatePeriod'] ?? \Symfony\Component\VarExporter\Internal\Registry::p('DatePeriod')),
+        clone $p['DateTime'],
+        clone $p['DateInterval'],
+    ],
     null,
     [],
     [
@@ -17,5 +19,57 @@ return \Symfony\Component\VarExporter\Internal\Hydrator::hydrate(
         $o[3],
         $o[4],
     ],
-    []
+    [
+        [
+            'date' => '1970-01-01 00:00:00.000000',
+            'timezone_type' => 1,
+            'timezone' => '+00:00',
+        ],
+        -1 => [
+            'date' => '1970-01-01 00:00:00.000000',
+            'timezone_type' => 1,
+            'timezone' => '+00:00',
+        ],
+        -2 => [
+            'timezone_type' => 3,
+            'timezone' => 'Europe/Paris',
+        ],
+        -3 => [
+            'y' => 0,
+            'm' => 0,
+            'd' => 7,
+            'h' => 0,
+            'i' => 0,
+            's' => 0,
+            'f' => 0.0,
+            'invert' => 0,
+            'days' => 7,
+            'from_string' => false,
+        ],
+        -5 => [
+            'date' => '2009-10-11 00:00:00.000000',
+            'timezone_type' => 3,
+            'timezone' => 'Europe/Paris',
+        ],
+        -6 => [
+            'y' => 0,
+            'm' => 0,
+            'd' => 7,
+            'h' => 0,
+            'i' => 0,
+            's' => 0,
+            'f' => 0.0,
+            'invert' => 0,
+            'days' => 7,
+            'from_string' => false,
+        ],
+        -4 => [
+            'start' => $o[5],
+            'current' => null,
+            'end' => null,
+            'interval' => $o[6],
+            'recurrences' => 5,
+            'include_start_date' => true,
+        ],
+    ]
 );

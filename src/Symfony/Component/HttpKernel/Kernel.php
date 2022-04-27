aca1d8f5bc4ddc167279aad7d2e0606244c837a6
Fabien Potencier <fabien@potencier.org>
2022-04-27 19:19:01 +0200

    Bump Symfony version to 4.4.42

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -76,12 +76,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
 
     private static $freshCache = [];
 
-    public const VERSION = '4.4.41';
-    public const VERSION_ID = 40441;
+    public const VERSION = '4.4.42-DEV';
+    public const VERSION_ID = 40442;
     public const MAJOR_VERSION = 4;
     public const MINOR_VERSION = 4;
-    public const RELEASE_VERSION = 41;
-    public const EXTRA_VERSION = '';
+    public const RELEASE_VERSION = 42;
+    public const EXTRA_VERSION = 'DEV';
 
     public const END_OF_MAINTENANCE = '11/2022';
     public const END_OF_LIFE = '11/2023';

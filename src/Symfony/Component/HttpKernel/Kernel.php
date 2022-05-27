eb7aca969fc9391b67081f8df83732aa6d756aa4
Fabien Potencier <fabien@potencier.org>
2022-05-27 09:09:08 +0200

    Update VERSION for 5.4.9

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -78,12 +78,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static $freshCache = [];
 
-    public const VERSION = '5.4.9-DEV';
+    public const VERSION = '5.4.9';
     public const VERSION_ID = 50409;
     public const MAJOR_VERSION = 5;
     public const MINOR_VERSION = 4;
     public const RELEASE_VERSION = 9;
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     public const END_OF_MAINTENANCE = '11/2024';
     public const END_OF_LIFE = '11/2025';

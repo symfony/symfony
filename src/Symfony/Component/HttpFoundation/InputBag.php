1be6c2fe916c5a11fef8de19f92bf5455202fe51
Troy Crawford <tacrawford91@gmail.com>
2022-06-30 15:28:50 -0500

    Fix typographical error in exception message in InputBag class

--- src/Symfony/Component/HttpFoundation/InputBag.php
+++ src/Symfony/Component/HttpFoundation/InputBag.php
@@ -28,7 +28,7 @@ final class InputBag extends ParameterBag
     public function get(string $key, mixed $default = null): string|int|float|bool|null
     {
         if (null !== $default && !\is_scalar($default) && !$default instanceof \Stringable) {
-            throw new \InvalidArgumentException(sprintf('Excepted a scalar value as a 2nd argument to "%s()", "%s" given.', __METHOD__, get_debug_type($default)));
+            throw new \InvalidArgumentException(sprintf('Expected a scalar value as a 2nd argument to "%s()", "%s" given.', __METHOD__, get_debug_type($default)));
         }
 
         $value = parent::get($key, $this);
@@ -67,7 +67,7 @@ final class InputBag extends ParameterBag
     public function set(string $key, mixed $value)
     {
         if (null !== $value && !\is_scalar($value) && !\is_array($value) && !$value instanceof \Stringable) {
-            throw new \InvalidArgumentException(sprintf('Excepted a scalar, or an array as a 2nd argument to "%s()", "%s" given.', __METHOD__, get_debug_type($value)));
+            throw new \InvalidArgumentException(sprintf('Expected a scalar, or an array as a 2nd argument to "%s()", "%s" given.', __METHOD__, get_debug_type($value)));
         }
 
         $this->parameters[$key] = $value;

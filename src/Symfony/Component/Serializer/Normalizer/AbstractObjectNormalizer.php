fdb42bb6af16f89001c717f138c1168944a18cd7
Nicolas Grekas <nicolas.grekas@gmail.com>
2022-07-04 17:15:44 +0200

    [Serializer] fix AbstractObjectNormalizer

--- src/Symfony/Component/Serializer/Normalizer/AbstractObjectNormalizer.php
+++ src/Symfony/Component/Serializer/Normalizer/AbstractObjectNormalizer.php
@@ -227,41 +227,6 @@ abstract class AbstractObjectNormalizer extends AbstractNormalizer
     }
 
     /**
-     * Computes the normalization context merged with current one. Metadata always wins over global context, as more specific.
-     */
-    private function getAttributeNormalizationContext(object $object, string $attribute, array $context): array
-    {
-        if (null === $metadata = $this->getAttributeMetadata($object, $attribute)) {
-            return $context;
-        }
-
-        return array_merge($context, $metadata->getNormalizationContextForGroups($this->getGroups($context)));
-    }
-
-    /**
-     * Computes the denormalization context merged with current one. Metadata always wins over global context, as more specific.
-     */
-    private function getAttributeDenormalizationContext(string $class, string $attribute, array $context): array
-    {
-        $context['deserialization_path'] = ($context['deserialization_path'] ?? false) ? $context['deserialization_path'].'.'.$attribute : $attribute;
-
-        if (null === $metadata = $this->getAttributeMetadata($class, $attribute)) {
-            return $context;
-        }
-
-        return array_merge($context, $metadata->getDenormalizationContextForGroups($this->getGroups($context)));
-    }
-
-    private function getAttributeMetadata(object|string $objectOrClass, string $attribute): ?AttributeMetadataInterface
-    {
-        if (!$this->classMetadataFactory) {
-            return null;
-        }
-
-        return $this->classMetadataFactory->getMetadataFor($objectOrClass)->getAttributesMetadata()[$attribute] ?? null;
-    }
-
-    /**
      * {@inheritdoc}
      */
     protected function instantiateObject(array &$data, string $class, array &$context, \ReflectionClass $reflectionClass, array|bool $allowedAttributes, string $format = null)

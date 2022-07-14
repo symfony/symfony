7e7e2bd006e802337ab56e47f4f4a6dda366c8fb
Kevin van Sonsbeek <kevin_787@msn.com>
2022-07-14 22:04:00 +0200

    [Validator] : Fix "PHP Warning: Undefined array key 1" in NotCompromisedPasswordValidator

--- src/Symfony/Component/Validator/Constraints/NotCompromisedPasswordValidator.php
+++ src/Symfony/Component/Validator/Constraints/NotCompromisedPasswordValidator.php
@@ -91,6 +91,10 @@ class NotCompromisedPasswordValidator extends ConstraintValidator
         }
 
         foreach (explode("\r\n", $result) as $line) {
+            if (!str_contains($line, ':')) {
+                continue;
+            }
+
             [$hashSuffix, $count] = explode(':', $line);
 
             if ($hashPrefix.$hashSuffix === $hash && $constraint->threshold <= (int) $count) {

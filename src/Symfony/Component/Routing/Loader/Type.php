72fe3e1cd1c7d009f22094cc8dc1a89b273257ed
Thomas Landauer <thomas@landauer.at>
2022-08-11 00:00:17 +0200

    Adding class with `type` constants

---
+++ src/Symfony/Component/Routing/Loader/Type.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Symfony\Component\Routing\Loader;
+
+abstract class Type
+{
+    public const ANNOTATION = 'annotation';
+    public const XML = 'xml';
+    public const YAML = 'yaml';
+}

d2370e525c6a34c4a5760d76d1dbec439cb44b22
Martin Komischke <martin.komischke@gmail.com>
2022-08-30 17:05:50 +0200

    fix typo

--- src/Symfony/Component/DependencyInjection/Attribute/When.php
+++ src/Symfony/Component/DependencyInjection/Attribute/When.php
@@ -12,7 +12,7 @@
 namespace Symfony\Component\DependencyInjection\Attribute;
 
 /**
- * An attribute to tell under which environement this class should be registered as a service.
+ * An attribute to tell under which environment this class should be registered as a service.
  *
  * @author Nicolas Grekas <p@tchwork.com>
  */

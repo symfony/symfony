3d0fb221bb12d70db7d634d80458ad79bddf0c1d
Nicolas Grekas <nicolas.grekas@gmail.com>
2022-09-28 17:49:31 +0200

    [FrameworkBundle] fix merge

--- src/Symfony/Bundle/FrameworkBundle/Resources/config/serializer.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/config/serializer.php
@@ -176,6 +176,7 @@ return static function (ContainerConfigurator $container) {
             ->tag('serializer.encoder')
 
         ->set('serializer.encoder.json', JsonEncoder::class)
+            ->args([null, null])
             ->tag('serializer.encoder')
 
         ->set('serializer.encoder.yaml', YamlEncoder::class)

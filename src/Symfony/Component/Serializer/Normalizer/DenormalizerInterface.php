a821999d5417a913ec07a95defab0dfa9293d3db
DrLuke <Lukas@Jackowski.de>
2022-10-20 13:26:59 +0200

    [Serializer] Add generic annotation to DenormalizerInterface

--- src/Symfony/Component/Serializer/Normalizer/DenormalizerInterface.php
+++ src/Symfony/Component/Serializer/Normalizer/DenormalizerInterface.php
@@ -21,6 +21,8 @@ use Symfony\Component\Serializer\Exception\UnexpectedValueException;
 
 /**
  * @author Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * @template TObject
  */
 interface DenormalizerInterface
 {
@@ -30,11 +32,11 @@ interface DenormalizerInterface
      * Denormalizes data back into an object of the given class.
      *
      * @param mixed  $data    Data to restore
-     * @param string $type    The expected class to instantiate
+     * @param class-string<TObject> $type    The expected class to instantiate
      * @param string|null $format  Format the given data was extracted from
      * @param array  $context Options available to the denormalizer
      *
-     * @return mixed
+     * @return TObject
      *
      * @throws BadMethodCallException   Occurs when the normalizer is not called in an expected context
      * @throws InvalidArgumentException Occurs when the arguments are not coherent or not supported
@@ -50,7 +52,7 @@ interface DenormalizerInterface
      * Checks whether the given class is supported for denormalization by this normalizer.
      *
      * @param mixed  $data    Data to denormalize from
-     * @param string $type    The class to which the data should be denormalized
+     * @param class-string $type    The class to which the data should be denormalized
      * @param string|null $format  The format being deserialized from
      * @param array  $context Options available to the denormalizer
      *

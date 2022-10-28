97f927a23d300afaec457f9109684404fae68466
Fabien Potencier <fabien@potencier.org>
2022-10-28 20:19:50 +0200

    Bump Symfony version to 6.1.8

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -78,12 +78,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static array $freshCache = [];
 
-    public const VERSION = '6.1.7';
-    public const VERSION_ID = 60107;
+    public const VERSION = '6.1.8-DEV';
+    public const VERSION_ID = 60108;
     public const MAJOR_VERSION = 6;
     public const MINOR_VERSION = 1;
-    public const RELEASE_VERSION = 7;
-    public const EXTRA_VERSION = '';
+    public const RELEASE_VERSION = 8;
+    public const EXTRA_VERSION = 'DEV';
 
     public const END_OF_MAINTENANCE = '01/2023';
     public const END_OF_LIFE = '01/2023';

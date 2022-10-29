926f3deba682469ca4f4e67b7cd784dad230f4c1
MatTheCat <math.lechat@gmail.com>
2022-10-29 11:24:19 +0200

    [FrameworkBundle] Split loggers debug compiler pass

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/AddDebugLogProcessorPass.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/AddDebugLogProcessorPass.php
@@ -14,7 +14,6 @@ namespace Symfony\Bundle\FrameworkBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\Log\Logger;
 
 class AddDebugLogProcessorPass implements CompilerPassInterface
 {
@@ -23,38 +22,22 @@ class AddDebugLogProcessorPass implements CompilerPassInterface
         if (!$container->hasDefinition('profiler')) {
             return;
         }
-
-        if ($container->hasDefinition('monolog.logger_prototype') && $container->hasDefinition('debug.log_processor')) {
-            $container->getDefinition('monolog.logger_prototype')
-                ->setConfigurator([__CLASS__, 'configureMonologLogger'])
-                ->addMethodCall('pushProcessor', [new Reference('debug.log_processor')])
-            ;
-
+        if (!$container->hasDefinition('monolog.logger_prototype')) {
+            return;
+        }
+        if (!$container->hasDefinition('debug.log_processor')) {
             return;
         }
 
-        if (!$container->hasDefinition('logger')) {
-            return;
-        }
-
-        $loggerDefinition = $container->getDefinition('logger');
-
-        if (Logger::class === $loggerDefinition->getClass()) {
-            $loggerDefinition->setConfigurator([__CLASS__, 'configureHttpKernelLogger']);
-        }
+        $definition = $container->getDefinition('monolog.logger_prototype');
+        $definition->setConfigurator([__CLASS__, 'configureLogger']);
+        $definition->addMethodCall('pushProcessor', [new Reference('debug.log_processor')]);
     }
 
-    public static function configureMonologLogger(mixed $logger)
+    public static function configureLogger(mixed $logger)
     {
-        if (\in_array(\PHP_SAPI, ['cli', 'phpdbg'], true) && \is_object($logger) && method_exists($logger, 'removeDebugLogger')) {
+        if (\is_object($logger) && method_exists($logger, 'removeDebugLogger') && \in_array(\PHP_SAPI, ['cli', 'phpdbg'], true)) {
             $logger->removeDebugLogger();
-        }
-    }
-
-    public static function configureHttpKernelLogger(Logger $logger)
-    {
-        if (!\in_array(\PHP_SAPI, ['cli', 'phpdbg'], true) && method_exists($logger, 'enableDebug')) {
-            $logger->enableDebug();
         }
     }
 }

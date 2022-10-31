f3df35b58d1a5f871196150d17f00bc68db06728
Rafael Neris <rafael.cruz@picpay.com>
2022-10-31 02:23:07 -0300

    Making compatible with LoggerInterface

--- src/Symfony/Bridge/Monolog/Monolog.php
+++ src/Symfony/Bridge/Monolog/Monolog.php
@@ -5,7 +5,6 @@ namespace Symfony\Bridge\Monolog;
 use DateTimeZone;
 use Psr\Log\LoggerInterface;
 use Monolog\Logger;
-use Stringable;
 
 abstract class Monolog implements LoggerInterface
 {
@@ -27,47 +26,47 @@ abstract class Monolog implements LoggerInterface
         return $this->monologLogger->pushProcessor($callback);
     }
 
-    public function emergency(Stringable|string $message, array $context = []): void
+    public function emergency(string|\Stringable $message, array $context = []): void
     {
         $this->monologLogger->emergency($message, $context);
     }
 
-    public function alert(Stringable|string $message, array $context = []): void
+    public function alert(string|\Stringable $message, array $context = []): void
     {
         $this->monologLogger->alert($message, $context);
     }
 
-    public function critical(Stringable|string $message, array $context = []): void
+    public function critical(string|\Stringable $message, array $context = []): void
     {
         $this->monologLogger->critical($message, $context);
     }
 
-    public function error(Stringable|string $message, array $context = []): void
+    public function error(string|\Stringable $message, array $context = []): void
     {
         $this->monologLogger->error($message, $context);
     }
 
-    public function warning(Stringable|string $message, array $context = []): void
+    public function warning(string|\Stringable $message, array $context = []): void
     {
         $this->monologLogger->warning($message, $context);
     }
 
-    public function notice(Stringable|string $message, array $context = []): void
+    public function notice(string|\Stringable $message, array $context = []): void
     {
         $this->monologLogger->notice($message, $context);
     }
 
-    public function info(Stringable|string $message, array $context = []): void
+    public function info(string|\Stringable $message, array $context = []): void
     {
         $this->monologLogger->info($message, $context);
     }
 
-    public function debug(Stringable|string $message, array $context = []): void
+    public function debug(string|\Stringable $message, array $context = []): void
     {
         $this->monologLogger->debug($message, $context);
     }
 
-    public function log($level, Stringable|string $message, array $context = []): void
+    public function log($level, string|\Stringable $message, array $context = []): void
     {
         $this->monologLogger->log($level, $message, $context);
     }

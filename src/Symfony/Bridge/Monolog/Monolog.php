6a7bdf66d62deb377b781ac4586e798a89c384fd
Rafael Neris <rafael.cruz@picpay.com>
2022-10-31 03:07:24 -0300

    Adapting Coding Standard

--- src/Symfony/Bridge/Monolog/Monolog.php
+++ src/Symfony/Bridge/Monolog/Monolog.php
@@ -8,6 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Symfony\Bridge\Monolog;
 
 use DateTimeZone;

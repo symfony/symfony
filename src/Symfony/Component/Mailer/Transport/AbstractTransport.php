3424989eeab019bc1864a58813d41f5b61fc7979
Thomas Hanke <thomas.hanke@pro-volution.de>
2022-11-29 17:14:13 +0100

    #48221: set manipulated message from event to original message

--- src/Symfony/Component/Mailer/Transport/AbstractTransport.php
+++ src/Symfony/Component/Mailer/Transport/AbstractTransport.php
@@ -61,6 +61,7 @@ abstract class AbstractTransport implements TransportInterface
             $event = new MessageEvent($message, $envelope, (string) $this);
             $this->dispatcher->dispatch($event);
             $envelope = $event->getEnvelope();
+            $message = $event->getMessage();
         }
 
         $message = new SentMessage($message, $envelope);

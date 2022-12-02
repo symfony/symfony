dee00b680ead8868ec2c5513051ee0c7e8879e78
Yasmany Cubela Medina <yasmanycm@gmail.com>
2022-12-02 12:50:51 -0500

    chore: set RecipientInterface as typehint of asPushMessage method

--- src/Symfony/Component/Notifier/Bridge/Pusher/PusherNotification.php
+++ src/Symfony/Component/Notifier/Bridge/Pusher/PusherNotification.php
@@ -14,14 +14,15 @@ namespace Symfony\Component\Notifier\Bridge\Pusher;
 use Symfony\Component\Notifier\Message\PushMessage;
 use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\Notification\PushNotificationInterface;
+use Symfony\Component\Notifier\Recipient\RecipientInterface;
 
 /**
  * @author Yasmany Cubela Medina <yasmanycm@gmail.com>
  */
 class PusherNotification extends Notification implements PushNotificationInterface
 {
-    public function asPushMessage(PusherRecipientInterface $recipient, string $transport = null): ?PushMessage
+    public function asPushMessage(RecipientInterface $recipient, string $transport = null): ?PushMessage
     {
-        return new PushMessage($this->getSubject(), $this->getContent(), new PusherOptions($recipient->getChannels()));
+        return new PushMessage($this->getSubject(), $this->getContent(), new PusherOptions($recipient instanceof PusherRecipientInterface ? $recipient->getChannels() : []));
     }
 }

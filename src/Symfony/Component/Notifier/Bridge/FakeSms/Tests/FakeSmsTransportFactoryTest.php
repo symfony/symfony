88e6c3e922606a8f5023de7b0d919d0f89993fdd
Benjamin Schoch <benjamin.schoch@mail.schwarz>
2022-12-08 12:55:49 +0100

    [Notifier] [FakeSms] Allow missing optional dependency

--- src/Symfony/Component/Notifier/Bridge/FakeSms/Tests/FakeSmsTransportFactoryTest.php
+++ src/Symfony/Component/Notifier/Bridge/FakeSms/Tests/FakeSmsTransportFactoryTest.php
@@ -14,10 +14,35 @@ namespace Symfony\Component\Notifier\Bridge\FakeSms\Tests;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Notifier\Bridge\FakeSms\FakeSmsTransportFactory;
+use Symfony\Component\Notifier\Exception\LogicException;
 use Symfony\Component\Notifier\Test\TransportFactoryTestCase;
+use Symfony\Component\Notifier\Transport\Dsn;
 
 final class FakeSmsTransportFactoryTest extends TransportFactoryTestCase
 {
+    /**
+     * @dataProvider missingRequiredDependencyProvider
+     */
+    public function testMissingRequiredDependency(?MailerInterface $mailer, ?LoggerInterface $logger, string $dsn, string $message)
+    {
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage($message);
+
+        $factory = new FakeSmsTransportFactory($mailer, $logger);
+        $factory->create(new Dsn($dsn));
+    }
+
+    /**
+     * @dataProvider missingOptionalDependencyProvider
+     */
+    public function testMissingOptionalDependency(?MailerInterface $mailer, ?LoggerInterface $logger, string $dsn)
+    {
+        $factory = new FakeSmsTransportFactory($mailer, $logger);
+        $transport = $factory->create(new Dsn($dsn));
+
+        $this->assertSame($dsn, (string) $transport);
+    }
+
     public function createFactory(): FakeSmsTransportFactory
     {
         return new FakeSmsTransportFactory($this->createMock(MailerInterface::class), $this->createMock(LoggerInterface::class));
@@ -62,5 +87,36 @@ final class FakeSmsTransportFactoryTest extends TransportFactoryTestCase
     public function unsupportedSchemeProvider(): iterable
     {
         yield ['somethingElse://default?to=recipient@email.net&from=sender@email.net'];
+    }
+
+    public function missingRequiredDependencyProvider(): iterable
+    {
+        $exceptionMessage = 'Cannot create a transport for scheme "%s" without providing an implementation of "%s".';
+        yield 'missing mailer' => [
+            null,
+            $this->createMock(LoggerInterface::class),
+            'fakesms+email://default?to=recipient@email.net&from=sender@email.net',
+            sprintf($exceptionMessage, 'fakesms+email', MailerInterface::class),
+        ];
+        yield 'missing logger' => [
+            $this->createMock(MailerInterface::class),
+            null,
+            'fakesms+logger://default',
+            sprintf($exceptionMessage, 'fakesms+logger', LoggerInterface::class),
+        ];
+    }
+
+    public function missingOptionalDependencyProvider(): iterable
+    {
+        yield 'missing logger' => [
+            $this->createMock(MailerInterface::class),
+            null,
+            'fakesms+email://default?to=recipient@email.net&from=sender@email.net',
+        ];
+        yield 'missing mailer' => [
+            null,
+            $this->createMock(LoggerInterface::class),
+            'fakesms+logger://default',
+        ];
     }
 }

9a248c48629e9dea12d3805b4b185216416134a2
Grégoire Pineau <lyrixx@lyrixx.info>
2022-12-12 15:17:55 +0100

    [Console] Fixed typo

--- src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_23.php
+++ src/Symfony/Component/Console/Tests/Fixtures/Style/SymfonyStyle/command/command_23.php
@@ -4,7 +4,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-// ensure that nested tags have no effect on the color of the '//' prefix
 return function (InputInterface $input, OutputInterface $output) {
     $output = new SymfonyStyle($input, $output);
     $output->text('Hello');

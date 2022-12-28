3502d7e90541139468077e27fb6273a24f9d77f4
Fabien Potencier <fabien@potencier.org>
2022-12-28 15:55:17 +0100

    Bump Symfony version to 5.4.18

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -78,12 +78,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static $freshCache = [];
 
-    public const VERSION = '5.4.17';
-    public const VERSION_ID = 50417;
+    public const VERSION = '5.4.18-DEV';
+    public const VERSION_ID = 50418;
     public const MAJOR_VERSION = 5;
     public const MINOR_VERSION = 4;
-    public const RELEASE_VERSION = 17;
-    public const EXTRA_VERSION = '';
+    public const RELEASE_VERSION = 18;
+    public const EXTRA_VERSION = 'DEV';
 
     public const END_OF_MAINTENANCE = '11/2024';
     public const END_OF_LIFE = '11/2025';

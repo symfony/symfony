71e9ca3cc0b9837e778dfcab7cb7427e1c444e4c
plfort <fort.pierrelouis@gmail.com>
2023-01-12 08:34:08 +0100

    CS fix

--- src/Symfony/Component/Validator/Constraints/UniqueValidator.php
+++ src/Symfony/Component/Validator/Constraints/UniqueValidator.php
@@ -26,11 +26,9 @@ use Symfony\Component\Validator\Exception\UnexpectedValueException;
  */
 class UniqueValidator extends ConstraintValidator
 {
-
     public function __construct(
         private ?PropertyAccessorInterface $propertyAccessor = null
-    )
-    {
+    ) {
     }
 
     public function validate(mixed $value, Constraint $constraint)

b4b633bea3b6f8c8534598536207f82831f2bf32
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-01-25 14:29:05 +0100

    Move the phpdoc

--- src/Symfony/Contracts/EventDispatcher/Event.php
+++ src/Symfony/Contracts/EventDispatcher/Event.php
@@ -35,13 +35,6 @@ class Event implements StoppableEventInterface
         return $this->propagationStopped;
     }
 
-    /**
-     * Stops the propagation of the event to further event listeners.
-     *
-     * If multiple event listeners are connected to the same event, no
-     * further event listener will be triggered once any trigger calls
-     * stopPropagation().
-     */
     public function stopPropagation(): void
     {
         $this->propagationStopped = true;
--- src/Symfony/Contracts/EventDispatcher/StoppableEventInterface.php
+++ src/Symfony/Contracts/EventDispatcher/StoppableEventInterface.php
@@ -15,5 +15,12 @@ use Psr\EventDispatcher\StoppableEventInterface as PsrStoppableEventInterface;
 
 interface StoppableEventInterface extends PsrStoppableEventInterface
 {
+    /**
+     * Stops the propagation of the event to further event listeners.
+     *
+     * If multiple event listeners are connected to the same event, no
+     * further event listener will be triggered once any trigger calls
+     * stopPropagation().
+     */
     public function stopPropagation(): void;
 }

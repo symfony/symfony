702920c486b67e4d9e4ee28e93e534dcf9890592
Mael Ortie <mael.ortie@eliberty.fr>
2023-02-06 14:36:43 +0100

    [HttpKernel] Remove private headers before storing responses

--- src/Symfony/Component/HttpKernel/HttpCache/Store.php
+++ src/Symfony/Component/HttpKernel/HttpCache/Store.php
@@ -26,21 +26,30 @@ class Store implements StoreInterface
 {
     protected $root;
     private $keyCache;
-    private $locks;
+    private $locks = [];
+    private $options;
 
     /**
      * @param string $root The path to the cache directory
+     * @param array $options List of options
+     *
+     * The available options are:
+     *
+     *   * private_headers  Set of response headers that should not be stored
+     *                      when a response is cached. (default: Set-Cookie)
      *
      * @throws \RuntimeException
      */
-    public function __construct($root)
+    public function __construct($root, array $options = [])
     {
         $this->root = $root;
         if (!file_exists($this->root) && !@mkdir($this->root, 0777, true) && !is_dir($this->root)) {
             throw new \RuntimeException(sprintf('Unable to create the store directory (%s).', $this->root));
         }
         $this->keyCache = new \SplObjectStorage();
-        $this->locks = [];
+        $this->options = array_merge([
+            'private_headers' => ['Set-Cookie'],
+        ], $options);
     }
 
     /**
@@ -216,6 +225,10 @@ class Store implements StoreInterface
 
         $headers = $this->persistResponse($response);
         unset($headers['age']);
+
+        foreach ($this->options['private_headers'] as $h) {
+            unset($headers[strtolower($h)]);
+        }
 
         array_unshift($entries, [$storedEnv, $headers]);
 

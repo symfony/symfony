75af86632bea5ed26596fa329bdeca7db9126910
Antoine Lamirault <lamiraultantoine@gmail.com>
2023-02-14 21:51:46 +0100

    [DependencyInjection] Fix autowire attribute with nullable parameters

--- src/Symfony/Component/DependencyInjection/Tests/Fixtures/includes/autowiring_classes_80.php
+++ src/Symfony/Component/DependencyInjection/Tests/Fixtures/includes/autowiring_classes_80.php
@@ -40,6 +40,8 @@ class AutowireAttribute
         public string $expression,
         #[Autowire(value: '%some.parameter%/bar')]
         public string $value,
+        #[Autowire(value: '%null.parameter%')]
+        public ?string $nullableValue,
         #[Autowire('@some.id')]
         public \stdClass $serviceAsValue,
         #[Autowire("@=parameter('some.parameter')")]

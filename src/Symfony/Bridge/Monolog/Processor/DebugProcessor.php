d0f32d4a7422c35e55c7bff86eb2ff526c494a3d
Antoine Lamirault <lamiraultantoine@gmail.com>
2023-02-21 19:48:11 +0100

    Remove unused local variable

--- src/Symfony/Bridge/Monolog/Processor/DebugProcessor.php
+++ src/Symfony/Bridge/Monolog/Processor/DebugProcessor.php
@@ -35,7 +35,7 @@ class DebugProcessor implements DebugLoggerInterface, ResetInterface
     {
         $key = $this->requestStack && ($request = $this->requestStack->getCurrentRequest()) ? spl_object_id($request) : '';
 
-        $timestamp = $timestampRfc3339 = false;
+        $timestampRfc3339 = false;
         if ($record['datetime'] instanceof \DateTimeInterface) {
             $timestamp = $record['datetime']->getTimestamp();
             $timestampRfc3339 = $record['datetime']->format(\DateTimeInterface::RFC3339_EXTENDED);

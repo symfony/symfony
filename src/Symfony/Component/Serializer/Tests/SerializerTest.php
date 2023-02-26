af34c52f80baed0192b5b319d6605fc927d137cd
Thomas Trautner <thomas@trautner.net>
2023-02-26 15:49:33 +0100

    [Serializer] Unexpected value should throw UnexpectedValueException

--- src/Symfony/Component/Serializer/Tests/SerializerTest.php
+++ src/Symfony/Component/Serializer/Tests/SerializerTest.php
@@ -940,7 +940,7 @@ class SerializerTest extends TestCase
                 ],
                 'path' => 'dateTime',
                 'useMessageForUser' => true,
-                'message' => 'The data is either an empty string or null, you should pass a string that can be parsed with the passed format or a valid DateTime string.',
+                'message' => 'The data is either not an string, an empty string, or null; you should pass a string that can be parsed with the passed format or a valid DateTime string.',
             ],
             [
                 'currentType' => 'null',
@@ -949,7 +949,7 @@ class SerializerTest extends TestCase
                 ],
                 'path' => 'dateTimeImmutable',
                 'useMessageForUser' => true,
-                'message' => 'The data is either an empty string or null, you should pass a string that can be parsed with the passed format or a valid DateTime string.',
+                'message' => 'The data is either not an string, an empty string, or null; you should pass a string that can be parsed with the passed format or a valid DateTime string.',
             ],
             [
                 'currentType' => 'null',

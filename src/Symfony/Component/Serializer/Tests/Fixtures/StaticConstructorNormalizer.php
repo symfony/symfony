c534f2f79b27e1666dd3684dce5977177a2e7848
Christian Flothmann <christian.flothmann@qossmic.com>
2023-04-11 11:40:12 +0200

    fix deprecations caused by ObjectNormalizer marked as final

--- src/Symfony/Component/Serializer/Tests/Fixtures/StaticConstructorNormalizer.php
+++ src/Symfony/Component/Serializer/Tests/Fixtures/StaticConstructorNormalizer.php
@@ -11,13 +11,33 @@
 
 namespace Symfony\Component\Serializer\Tests\Fixtures;
 
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
 
 /**
  * @author Guilhem N. <egetick@gmail.com>
  */
-class StaticConstructorNormalizer extends ObjectNormalizer
+class StaticConstructorNormalizer extends AbstractObjectNormalizer
 {
+    public function getSupportedTypes(?string $format): array
+    {
+        return [StaticConstructorDummy::class];
+    }
+
+    protected function extractAttributes(object $object, string $format = null, array $context = [])
+    {
+        return get_object_vars($object);
+    }
+
+    protected function getAttributeValue(object $object, string $attribute, string $format = null, array $context = [])
+    {
+        return $object->$attribute;
+    }
+
+    protected function setAttributeValue(object $object, string $attribute, mixed $value, string $format = null, array $context = [])
+    {
+        $object->$attribute = $value;
+    }
+
     protected function getConstructor(array &$data, string $class, array &$context, \ReflectionClass $reflectionClass, array|bool $allowedAttributes): ?\ReflectionMethod
     {
         if (is_a($class, StaticConstructorDummy::class, true)) {

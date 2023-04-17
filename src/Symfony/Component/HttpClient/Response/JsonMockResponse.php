e4a6365a453eef09f8d5d822601393c980b58b8f
Thomas Calvet <calvet.thomas@gmail.com>
2023-04-17 18:33:34 +0200

    [HttpClient] Add JsonMockResponse

---
+++ src/Symfony/Component/HttpClient/Response/JsonMockResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\HttpClient\Response;
+
+use Symfony\Component\HttpClient\Exception\InvalidArgumentException;
+
+class JsonMockResponse extends MockResponse
+{
+    public function __construct(mixed $body = [], array $info = [])
+    {
+        try {
+            $json = json_encode($body, \JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+            throw new InvalidArgumentException('JSON encoding failed: '.$e->getMessage(), $e->getCode());
+        }
+
+        $info['response_headers']['content-type'] ??= 'application/json';
+
+        parent::__construct($json, $info);
+    }
+}

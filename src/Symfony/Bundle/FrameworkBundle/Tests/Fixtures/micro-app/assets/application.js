039e64013067986bbd075dfc874dc96c4463b052
Kévin Dunglas <kevin@dunglas.fr>
2023-04-18 10:34:34 +0200

    remove yo

--- src/Symfony/Bundle/FrameworkBundle/Tests/Fixtures/micro-app/assets/application.js
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Fixtures/micro-app/assets/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
 import HelloController from "controllers/hello_controller"
 
-console.log('yo')
-
 const application = Application.start()
 
 application.debug = false

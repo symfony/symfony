380e709c5e7975393feef650ce2264bd5ca79726
Nicolas Grekas <nicolas.grekas@gmail.com>
2023-05-30 11:11:03 +0200

    CS fix

--- src/Symfony/Bundle/FrameworkBundle/Tests/Fixtures/ContainerExcluded.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Fixtures/ContainerExcluded.php
@@ -1,12 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Symfony\Bundle\FrameworkBundle\Tests\Fixtures;
 
-/**
- * @author Maksim Vorozhtsov <myks1992@mail.ru>
- */
 class ContainerExcluded
 {
 }

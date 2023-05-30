9241f844e74b545bbb0cbed9b8eeb16773b87026
Fabien Potencier <fabien@potencier.org>
2023-05-30 21:03:32 +0200

    Update VERSION for 6.3.0

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -76,12 +76,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static array $freshCache = [];
 
-    public const VERSION = '6.3.0-DEV';
+    public const VERSION = '6.3.0';
     public const VERSION_ID = 60300;
     public const MAJOR_VERSION = 6;
     public const MINOR_VERSION = 3;
     public const RELEASE_VERSION = 0;
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     public const END_OF_MAINTENANCE = '01/2024';
     public const END_OF_LIFE = '01/2024';

2032b5bd8b7a0447d93161602255efa0f7a5925b
Jules Pietri <heahdude@yahoo.fr>
2023-07-20 12:59:04 +0200

    [Form] Fix form events phpdocs

--- src/Symfony/Component/Form/Event/PostSetDataEvent.php
+++ src/Symfony/Component/Form/Event/PostSetDataEvent.php
@@ -16,7 +16,8 @@ use Symfony\Component\Form\FormEvent;
 /**
  * This event is dispatched at the end of the Form::setData() method.
  *
- * This event is mostly here for reading data after having pre-populated the form.
+ * It can be used to modify a form depending on the populated data (adding or
+ * removing fields dynamically).
  */
 final class PostSetDataEvent extends FormEvent
 {

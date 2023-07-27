2a6e93e93ce6a7b92c677b4d141ebb36bdcf5a71
Alexander M. Turek <me@derrabus.de>
2023-07-27 09:50:19 +0200

    [Serializer] Fix tests for extended Context attributes

--- src/Symfony/Component/Serializer/Tests/Normalizer/Features/DummyContextChild.php
+++ src/Symfony/Component/Serializer/Tests/Normalizer/Features/DummyContextChild.php
@@ -13,14 +13,7 @@ namespace Symfony\Component\Serializer\Tests\Normalizer\Features;
 
 use Symfony\Component\Serializer\Annotation\Context;
 
-/**
- * Annotation class for @DummyContextChild().
- *
- * @Annotation
- * @NamedArgumentConstructor
- * @Target({"PROPERTY", "METHOD"})
- */
-#[\Attribute(\Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY)]
+#[\Attribute(\Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::IS_REPEATABLE)]
 class DummyContextChild extends Context
 {
 }

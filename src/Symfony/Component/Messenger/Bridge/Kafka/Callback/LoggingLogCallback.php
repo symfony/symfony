0123d2fa06f992cad1522892bc471b0b35299fbe
Andy Thorne <contrabandvr@gmail.com>
2023-08-11 17:23:58 +0100

    Fixed syslog to PsrLog mapping

--- src/Symfony/Component/Messenger/Bridge/Kafka/Callback/LoggingLogCallback.php
+++ src/Symfony/Component/Messenger/Bridge/Kafka/Callback/LoggingLogCallback.php
@@ -12,6 +12,7 @@
 namespace Symfony\Component\Messenger\Bridge\Kafka\Callback;
 
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 
 final class LoggingLogCallback
 {
@@ -23,7 +24,17 @@ final class LoggingLogCallback
     public function __invoke(object $kafka, int $level, string $facility, string $message): void
     {
         $this->logger->log(
-            $level,
+            match ($level) {
+                0 => LogLevel::EMERGENCY,
+                1 => LogLevel::ALERT,
+                2 => LogLevel::CRITICAL,
+                3 => LogLevel::ERROR,
+                4 => LogLevel::WARNING,
+                5 => LogLevel::NOTICE,
+                6 => LogLevel::INFO,
+                7 => LogLevel::DEBUG,
+                default => LogLevel::DEBUG,
+            },
             $message,
             [
                 'facility' => $facility,
--- src/Symfony/Component/Messenger/Bridge/Kafka/Tests/Callback/LoggingLogCallbackTest.php
+++ src/Symfony/Component/Messenger/Bridge/Kafka/Tests/Callback/LoggingLogCallbackTest.php
@@ -13,6 +13,7 @@ namespace Symfony\Component\Messenger\Bridge\Kafka\Tests\Callback;
 
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use RdKafka\KafkaConsumer;
 use Symfony\Component\Messenger\Bridge\Kafka\Callback\LoggingLogCallback;
 
@@ -21,16 +22,32 @@ use Symfony\Component\Messenger\Bridge\Kafka\Callback\LoggingLogCallback;
  */
 final class LoggingLogCallbackTest extends TestCase
 {
-    public function testInvoke()
+    public function getLogLevels(): iterable
+    {
+        yield [0, LogLevel::EMERGENCY];
+        yield [1, LogLevel::ALERT];
+        yield [2, LogLevel::CRITICAL];
+        yield [3, LogLevel::ERROR];
+        yield [4, LogLevel::WARNING];
+        yield [5, LogLevel::NOTICE];
+        yield [6, LogLevel::INFO];
+        yield [7, LogLevel::DEBUG];
+        yield [8, LogLevel::DEBUG];
+    }
+
+    /**
+     * @dataProvider getLogLevels
+     */
+    public function testInvoke(int $level, $expectedLevel)
     {
         $logger = $this->createMock(LoggerInterface::class);
         $logger->expects(self::once())
             ->method('log')
-            ->with(1, 'test error message', ['facility' => 'facility-value']);
+            ->with($expectedLevel, 'test error message', ['facility' => 'facility-value']);
 
         $consumer = $this->createMock(KafkaConsumer::class);
 
         $callback = new LoggingLogCallback($logger);
-        $callback($consumer, 1, 'facility-value', 'test error message');
+        $callback($consumer, $level, 'facility-value', 'test error message');
     }
 }

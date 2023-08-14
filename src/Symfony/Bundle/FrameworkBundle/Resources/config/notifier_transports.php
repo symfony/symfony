0942d6e390a1fa688fc2123c6432e51b83862f2b
Christian Flothmann <christian.flothmann@qossmic.com>
2023-08-14 10:11:14 +0200

    fix directory casing

--- src/Symfony/Bundle/FrameworkBundle/Resources/config/notifier_transports.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/config/notifier_transports.php
@@ -303,7 +303,7 @@ return static function (ContainerConfigurator $container) {
         ->set('notifier.transport_factory.redlink', Bridge\Redlink\RedlinkTransportFactory::class)
             ->parent('notifier.transport_factory.abstract')
             ->tag('texter.transport_factory')
-        ->set('notifier.transport_factory.goip', Bridge\GoIp\GoIpTransportFactory::class)
+        ->set('notifier.transport_factory.go-ip', Bridge\GoIp\GoIpTransportFactory::class)
             ->parent('notifier.transport_factory.abstract')
             ->tag('texter.transport_factory')
     ;

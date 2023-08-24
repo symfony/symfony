11378ef65f78806a760851a4dc7959117d92e447
Mathias Arlaud <mathias.arlaud@gmail.com>
2023-08-24 16:29:20 +0200

    [Serializer] Fix union of enum denormalization

--- src/Symfony/Component/Serializer/Normalizer/AbstractObjectNormalizer.php
+++ src/Symfony/Component/Serializer/Normalizer/AbstractObjectNormalizer.php
@@ -11,7 +11,7 @@
 
 namespace Symfony\Component\Serializer\Normalizer;
 
-use Symfony\Component\PropertyAccess\Exception\InvalidArgumentException;
+use Symfony\Component\PropertyAccess\Exception\InvalidArgumentException as PropertyAccessInvalidArgumentException;
 use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
 use Symfony\Component\PropertyAccess\Exception\UninitializedPropertyException;
 use Symfony\Component\PropertyAccess\PropertyAccess;
@@ -21,6 +21,7 @@ use Symfony\Component\Serializer\Encoder\CsvEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Exception\ExtraAttributesException;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
 use Symfony\Component\Serializer\Exception\LogicException;
 use Symfony\Component\Serializer\Exception\MissingConstructorArgumentsException;
 use Symfony\Component\Serializer\Exception\NotNormalizableValueException;
@@ -387,7 +388,7 @@ abstract class AbstractObjectNormalizer extends AbstractNormalizer
 
             try {
                 $this->setAttributeValue($object, $attribute, $value, $format, $attributeContext);
-            } catch (InvalidArgumentException $e) {
+            } catch (PropertyAccessInvalidArgumentException $e) {
                 $exception = NotNormalizableValueException::createForUnexpectedDataType(
                     sprintf('Failed to denormalize attribute "%s" value for class "%s": '.$e->getMessage(), $attribute, $type),
                     $data,
@@ -562,7 +563,7 @@ abstract class AbstractObjectNormalizer extends AbstractNormalizer
                 if (('is_'.$builtinType)($data)) {
                     return $data;
                 }
-            } catch (NotNormalizableValueException $e) {
+            } catch (NotNormalizableValueException|InvalidArgumentException $e) {
                 if (!$isUnionType) {
                     throw $e;
                 }

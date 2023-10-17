965430df9cdd412848f1ae7cdda1f618b1f0a501
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-10-17 18:29:12 +0200

    DX: PHP CS Fixer - allow for no_superfluous_phpdoc_tags.allow_unused_params

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -30,6 +30,10 @@ return (new PhpCsFixer\Config())
         '@Symfony:risky' => true,
         'protected_to_private' => false,
         'native_constant_invocation' => ['strict' => false],
+        'no_superfluous_phpdoc_tags' => [
+            'remove_inheritdoc' => true,
+            'allow_unused_params' => true, // for future-ready params, to be replaced with https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/issues/7377
+        ],
         'header_comment' => ['header' => $fileHeaderComment],
         'modernize_strpos' => true,
         'get_class_to_class_keyword' => true,

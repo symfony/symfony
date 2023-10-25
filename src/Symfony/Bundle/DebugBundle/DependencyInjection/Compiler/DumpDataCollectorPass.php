99a0dba420137e42ad2ad7ce7cd8b18d4f8edf79
Christian Flothmann <christian.flothmann@qossmic.com>
2023-10-25 11:55:01 +0200

    replace a not-existing virtual request stack with the real one

--- src/Symfony/Bundle/DebugBundle/DependencyInjection/Compiler/DumpDataCollectorPass.php
+++ src/Symfony/Bundle/DebugBundle/DependencyInjection/Compiler/DumpDataCollectorPass.php
@@ -14,6 +14,7 @@ namespace Symfony\Bundle\DebugBundle\DependencyInjection\Compiler;
 use Symfony\Bundle\WebProfilerBundle\EventListener\WebDebugToolbarListener;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Registers the file link format for the {@link \Symfony\Component\HttpKernel\DataCollector\DumpDataCollector}.
@@ -32,6 +33,10 @@ class DumpDataCollectorPass implements CompilerPassInterface
         }
 
         $definition = $container->getDefinition('data_collector.dump');
+
+        if (!$container->has('.virtual_request_stack')) {
+            $definition->replaceArgument(3, new Reference('request_stack'));
+        }
 
         if (!$container->hasParameter('web_profiler.debug_toolbar.mode') || WebDebugToolbarListener::DISABLED === $container->getParameter('web_profiler.debug_toolbar.mode')) {
             $definition->replaceArgument(3, null);

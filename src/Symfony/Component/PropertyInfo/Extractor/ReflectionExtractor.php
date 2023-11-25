88f557ebbef24bf6efc73aa7df374be1fb3c6c03
Alexander M. Turek <me@derrabus.de>
2023-11-25 17:57:46 +0100

    Remove obsolete PHP version checks

--- src/Symfony/Component/PropertyInfo/Extractor/ReflectionExtractor.php
+++ src/Symfony/Component/PropertyInfo/Extractor/ReflectionExtractor.php
@@ -364,7 +364,7 @@ class ReflectionExtractor implements PropertyListExtractorInterface, PropertyTyp
 
         if ($reflClass->hasProperty($property) && ($reflClass->getProperty($property)->getModifiers() & $this->propertyReflectionFlags)) {
             $reflProperty = $reflClass->getProperty($property);
-            if (\PHP_VERSION_ID < 80100 || !$reflProperty->isReadOnly()) {
+            if (!$reflProperty->isReadOnly()) {
                 return new PropertyWriteInfo(PropertyWriteInfo::TYPE_PROPERTY, $property, $this->getWriteVisiblityForProperty($reflProperty), $reflProperty->isStatic());
             }
 
@@ -578,7 +578,7 @@ class ReflectionExtractor implements PropertyListExtractorInterface, PropertyTyp
         try {
             $reflectionProperty = new \ReflectionProperty($class, $property);
 
-            if (\PHP_VERSION_ID >= 80100 && $writeAccessRequired && $reflectionProperty->isReadOnly()) {
+            if ($writeAccessRequired && $reflectionProperty->isReadOnly()) {
                 return false;
             }
 

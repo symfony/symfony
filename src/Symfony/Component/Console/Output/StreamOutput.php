285518d91939200e83c477131c722c9fe7929531
Théo FIDRY <theo.fidry@gmail.com>
2023-12-08 14:32:57 +0100

    detect colors on not windows

--- src/Symfony/Component/Console/Output/StreamOutput.php
+++ src/Symfony/Component/Console/Output/StreamOutput.php
@@ -95,15 +95,17 @@ class StreamOutput extends Output
             return false;
         }
 
-        if (\DIRECTORY_SEPARATOR === '\\') {
-            return (\function_exists('sapi_windows_vt100_support')
-                && @sapi_windows_vt100_support($this->stream))
-                || false !== getenv('ANSICON')
-                || 'ON' === getenv('ConEmuANSI')
-                || str_starts_with((string) getenv('TERM'), 'xterm');
+        if (\DIRECTORY_SEPARATOR === '\\'
+            && \function_exists('sapi_windows_vt100_support')
+            && @sapi_windows_vt100_support($this->stream)
+        ) {
+            return true;
         }
 
         return 'Hyper' === getenv('TERM_PROGRAM')
+            || false !== getenv('ANSICON')
+            || 'ON' === getenv('ConEmuANSI')
+            || str_starts_with((string) getenv('TERM'), 'xterm')
             || stream_isatty($this->stream);
     }
 }

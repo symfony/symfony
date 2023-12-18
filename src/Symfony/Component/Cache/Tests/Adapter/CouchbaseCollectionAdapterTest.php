578eb296ce98edb2896a70c8e32382f77b5937c1
Alexandre Daubois <alex.daubois@gmail.com>
2023-12-18 11:11:27 +0100

    [Cache] Fix failing test

--- src/Symfony/Component/Cache/Tests/Adapter/CouchbaseCollectionAdapterTest.php
+++ src/Symfony/Component/Cache/Tests/Adapter/CouchbaseCollectionAdapterTest.php
@@ -11,8 +11,8 @@
 
 namespace Symfony\Component\Cache\Tests\Adapter;
 
-use Couchbase\Collection;
 use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
 use Symfony\Component\Cache\Adapter\AbstractAdapter;
 use Symfony\Component\Cache\Adapter\CouchbaseCollectionAdapter;
 
@@ -21,41 +21,32 @@ use Symfony\Component\Cache\Adapter\CouchbaseCollectionAdapter;
  * @requires extension couchbase >=3.0.0
  *
  * @group integration
+ * @group legacy
  *
  * @author Antonio Jose Cerezo Aranda <aj.cerezo@gmail.com>
  */
 class CouchbaseCollectionAdapterTest extends AdapterTestCase
 {
+    use ExpectDeprecationTrait;
+
     protected $skippedTests = [
         'testClearPrefix' => 'Couchbase cannot clear by prefix',
     ];
 
-    protected static Collection $client;
-
-    public static function setupBeforeClass(): void
+    public static function setUpBeforeClass(): void
     {
         if (!CouchbaseCollectionAdapter::isSupported()) {
             self::markTestSkipped('Couchbase >= 3.0.0 < 4.0.0 is required.');
         }
-
-        self::$client = AbstractAdapter::createConnection('couchbase://'.getenv('COUCHBASE_HOST').'/cache',
-            ['username' => getenv('COUCHBASE_USER'), 'password' => getenv('COUCHBASE_PASS')]
-        );
     }
 
     public function createCachePool($defaultLifetime = 0): CacheItemPoolInterface
     {
-        if (!CouchbaseCollectionAdapter::isSupported()) {
-            self::markTestSkipped('Couchbase >= 3.0.0 < 4.0.0 is required.');
-        }
+        $this->expectDeprecation('Since symfony/cache 7.1: The "Symfony\Component\Cache\Adapter\CouchbaseBucketAdapter" class is deprecated, use "Symfony\Component\Cache\Adapter\CouchbaseCollectionAdapter" instead.');
 
-        $client = $defaultLifetime
-            ? AbstractAdapter::createConnection('couchbase://'
-                .getenv('COUCHBASE_USER')
-                .':'.getenv('COUCHBASE_PASS')
-                .'@'.getenv('COUCHBASE_HOST')
-                .'/cache')
-            : self::$client;
+        $client = AbstractAdapter::createConnection('couchbase://'.getenv('COUCHBASE_HOST').'/cache',
+            ['username' => getenv('COUCHBASE_USER'), 'password' => getenv('COUCHBASE_PASS')]
+        );
 
         return new CouchbaseCollectionAdapter($client, str_replace('\\', '.', __CLASS__), $defaultLifetime);
     }

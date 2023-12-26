98f4fa1bd9f96772fef2740d69a5d11293e3819c
Nicolas Grekas <nicolas.grekas@gmail.com>
2023-12-26 11:22:59 +0100

    [HttpKernel] Add `HttpException::fromStatusCode()`

--- src/Symfony/Component/HttpKernel/CHANGELOG.md
+++ src/Symfony/Component/HttpKernel/CHANGELOG.md
@@ -5,6 +5,7 @@ CHANGELOG
 ---
 
  * Add method `isKernelTerminating()` to `ExceptionEvent` that allows to check if an exception was thrown while the kernel is being terminated
+ * Add `HttpException::fromStatusCode()`
 
 7.0
 ---
--- src/Symfony/Component/HttpKernel/Tests/Exception/HttpExceptionTest.php
+++ src/Symfony/Component/HttpKernel/Tests/Exception/HttpExceptionTest.php
@@ -63,6 +63,38 @@ class HttpExceptionTest extends TestCase
         $this->assertSame($previous, $exception->getPrevious());
     }
 
+    /**
+     * @dataProvider provideStatusCode
+     */
+    public function testFromStatusCode(int $statusCode)
+    {
+        $exception = HttpException::fromStatusCode($statusCode);
+        $this->assertInstanceOf(HttpException::class, $exception);
+        $this->assertSame($statusCode, $exception->getStatusCode());
+    }
+
+    public static function provideStatusCode()
+    {
+        return [
+            [400],
+            [401],
+            [403],
+            [404],
+            [406],
+            [409],
+            [410],
+            [411],
+            [412],
+            [418],
+            [423],
+            [415],
+            [422],
+            [428],
+            [429],
+            [503],
+        ];
+    }
+
     protected function createException(string $message = '', \Throwable $previous = null, int $code = 0, array $headers = []): HttpException
     {
         return new HttpException(200, $message, $previous, $headers, $code);

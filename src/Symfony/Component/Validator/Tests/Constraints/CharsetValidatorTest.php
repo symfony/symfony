9c96ea94989113698dffc1e566bbe6b1ba07487f
Christian Flothmann <christian.flothmann@qossmic.com>
2023-12-28 19:10:09 +0100

    fix merge

--- src/Symfony/Component/Validator/Tests/Constraints/CharsetValidatorTest.php
+++ src/Symfony/Component/Validator/Tests/Constraints/CharsetValidatorTest.php
@@ -40,7 +40,7 @@ class CharsetValidatorTest extends ConstraintValidatorTestCase
     {
         $this->validator->validate($value, new Charset(encodings: $encodings));
 
-        $this->buildViolation('The detected character encoding "{{ detected }}" is invalid. Allowed encodings are "{{ encodings }}".')
+        $this->buildViolation('The detected character encoding is invalid ({{ detected }}). Allowed encodings are {{ encodings }}.')
             ->setParameter('{{ detected }}', mb_detect_encoding($value, $encodings, true))
             ->setParameter('{{ encodings }}', implode(', ', $encodings))
             ->setCode(Charset::BAD_ENCODING_ERROR)

62cb6a3c36f507d78f31345c7841094e13ad2f7e
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2023-12-29 10:09:04 +0100

    [Translation] Allow global parameters

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/ConfigurationTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/ConfigurationTest.php
@@ -591,6 +591,7 @@ class ConfigurationTest extends TestCase
                     'localizable_html_attributes' => [],
                 ],
                 'providers' => [],
+                'globals' => [],
             ],
             'validation' => [
                 'enabled' => !class_exists(FullStack::class),

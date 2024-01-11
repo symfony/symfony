2b6fe56e1a16c7a1f6a8290a5785202b0b4bfb5c
Joe <108891513+lawsonjl-ornl@users.noreply.github.com>
2024-01-11 11:48:58 -0500

    [Console] Allow '0' as a $shortcut in InputOption.php

--- src/Symfony/Component/Console/Input/InputOption.php
+++ src/Symfony/Component/Console/Input/InputOption.php
@@ -69,7 +69,7 @@ class InputOption
             throw new InvalidArgumentException('An option name cannot be empty.');
         }
 
-        if (empty($shortcut)) {
+        if ('' === $shortcut || [] === $shortcut) {
             $shortcut = null;
         }
 
@@ -78,10 +78,10 @@ class InputOption
                 $shortcut = implode('|', $shortcut);
             }
             $shortcuts = preg_split('{(\|)-?}', ltrim($shortcut, '-'));
-            $shortcuts = array_filter($shortcuts);
+            $shortcuts = array_filter($shortcuts, 'strlen');
             $shortcut = implode('|', $shortcuts);
 
-            if (empty($shortcut)) {
+            if ('' === $shortcut) {
                 throw new InvalidArgumentException('An option shortcut cannot be empty.');
             }
         }

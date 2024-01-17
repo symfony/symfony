36b341e4256d87cee6cbc654ea22c36e86e892be
Vladislav Iurciuc <viurciuc@pentalog.com>
2024-01-17 18:31:51 +0200

    [String] Correct inflection of axis

--- src/Symfony/Component/String/Inflector/EnglishInflector.php
+++ src/Symfony/Component/String/Inflector/EnglishInflector.php
@@ -166,6 +166,9 @@ final class EnglishInflector implements InflectorInterface
         // Fourth entry: Whether the suffix may succeed a consonant
         // Fifth entry: plural suffix, normal
 
+        // axes (axis)
+        ['sixa', 4, false, false, 'axes'],
+
         // criterion (criteria)
         ['airetirc', 8, false, false, 'criterion'],
 

213f01534a621b8a992a4fc470927ba26a8062e3
Conrad Barthelmes <con@conbookm2.fritz.box>
2024-01-19 20:18:11 +0100

    #48167 revert formatting

--- src/Symfony/Component/Form/Extension/Core/DataAccessor/PropertyPathAccessor.php
+++ src/Symfony/Component/Form/Extension/Core/DataAccessor/PropertyPathAccessor.php
@@ -109,8 +109,7 @@ class PropertyPathAccessor implements DataAccessorInterface
 
             if (!$e instanceof UninitializedPropertyException
                 // For versions without UninitializedPropertyException check the exception message
-                && (class_exists(UninitializedPropertyException::class) || false === strpos($e->getMessage(),
-                        'You should initialize it'))
+                && (class_exists(UninitializedPropertyException::class) || false === strpos($e->getMessage(), 'You should initialize it'))
             ) {
                 throw $e;
             }

2a9eb43b9da4b840b7005db9de3fc44950451343
Nicolas Grekas <nicolas.grekas@gmail.com>
2024-01-23 14:08:26 +0100

    CS fix

--- src/Symfony/Component/Routing/Tests/Fixtures/AttributeFixtures/ExtendedRoute.php
+++ src/Symfony/Component/Routing/Tests/Fixtures/AttributeFixtures/ExtendedRoute.php
@@ -7,7 +7,8 @@ use Symfony\Component\Routing\Attribute\Route;
 #[\Attribute(\Attribute::IS_REPEATABLE | \Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD)]
 class ExtendedRoute extends Route
 {
-    public function __construct(array|string $path = null, ?string $name = null, array $defaults = []) {
+    public function __construct(array|string $path = null, ?string $name = null, array $defaults = [])
+    {
         parent::__construct("/{section<(foo|bar|baz)>}" . $path, $name, [], [], array_merge(['section' => 'foo'], $defaults));
     }
 }

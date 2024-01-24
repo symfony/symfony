1d6f7959c3ee8743f957356b9c8e00e6dd7db625
Nicolas Grekas <nicolas.grekas@gmail.com>
2024-01-24 19:53:26 +0100

    [DependencyInjection] Fix loading all env vars from secrets when only a subset is needed

--- src/Symfony/Bundle/FrameworkBundle/Secrets/SodiumVault.php
+++ src/Symfony/Bundle/FrameworkBundle/Secrets/SodiumVault.php
@@ -12,6 +12,7 @@
 namespace Symfony\Bundle\FrameworkBundle\Secrets;
 
 use Symfony\Component\DependencyInjection\EnvVarLoaderInterface;
+use Symfony\Component\String\LazyString;
 use Symfony\Component\VarExporter\VarExporter;
 
 /**
@@ -169,7 +170,14 @@ class SodiumVault extends AbstractVault implements EnvVarLoaderInterface
 
     public function loadEnvVars(): array
     {
-        return $this->list(true);
+        $envs = [];
+        $reveal = $this->reveal(...);
+
+        foreach ($this->list() as $name => $value) {
+            $envs[$name] = LazyString::fromCallable($reveal, $name);
+        }
+
+        return $envs;
     }
 
     private function loadKeys(): void

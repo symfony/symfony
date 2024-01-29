b2653a8797ad99885c8485497b343bd0703e8845
HypeMC <hypemc@gmail.com>
2024-01-29 10:19:33 +0100

    [DependencyInjection] Fix replacing arguments

--- src/Symfony/Component/DependencyInjection/Definition.php
+++ src/Symfony/Component/DependencyInjection/Definition.php
@@ -257,10 +257,6 @@ class Definition
             throw new OutOfBoundsException(sprintf('Cannot replace arguments for class "%s" if none have been configured yet.', $this->class));
         }
 
-        if (\is_int($index) && ($index < 0 || $index > \count($this->arguments) - 1)) {
-            throw new OutOfBoundsException(sprintf('The index "%d" is not in the range [0, %d] of the arguments of class "%s".', $index, \count($this->arguments) - 1, $this->class));
-        }
-
         if (!\array_key_exists($index, $this->arguments)) {
             throw new OutOfBoundsException(sprintf('The argument "%s" doesn\'t exist in class "%s".', $index, $this->class));
         }

38b2e6a73f681c07481abc5b87a44995bb95ba0a
Christian Flothmann <christian.flothmann@qossmic.com>
2024-01-30 10:34:56 +0100

    fix tests

--- src/Symfony/Component/DependencyInjection/Dumper/YamlDumper.php
+++ src/Symfony/Component/DependencyInjection/Dumper/YamlDumper.php
@@ -312,7 +312,7 @@ class YamlDumper extends Dumper
         } elseif ($value instanceof Definition) {
             return new TaggedValue('service', (new Parser())->parse("_:\n".$this->addService('_', $value), Yaml::PARSE_CUSTOM_TAGS)['_']['_']);
         } elseif ($value instanceof \UnitEnum) {
-            return new TaggedValue('php/const', sprintf('%s::%s', $value::class, $value->name));
+            return new TaggedValue('php/enum', sprintf('%s::%s', $value::class, $value->name));
         } elseif ($value instanceof AbstractArgument) {
             return new TaggedValue('abstract', $value->getText());
         } elseif (\is_object($value) || \is_resource($value)) {

49625a10951c8123d2a97d78fe57ffc8c5b013e3
Christian Flothmann <christian.flothmann@qossmic.com>
2024-02-03 17:11:47 +0100

    re-add accidentally removed property

--- src/Symfony/Component/Console/Input/InputArgument.php
+++ src/Symfony/Component/Console/Input/InputArgument.php
@@ -41,6 +41,7 @@ class InputArgument
     public const IS_ARRAY = 4;
 
     private int $mode;
+    private string|int|bool|array|float|null $default;
 
     /**
      * @param string                                                                        $name            The argument name

eb6183e6663f761e8399d8977b19f1f800ff72b1
symfonyaml <>
2024-02-03 20:07:16 +0100

    [Validator] Add Yaml constraint for validating Yaml content

---
+++ src/Symfony/Component/Validator/Constraints/Yaml.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\InvalidArgumentException;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ *
+ * @author symfonyaml
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+class Yaml extends Constraint
+{
+    public const INVALID_YAML_ERROR = '63313a31-837c-42bb-99eb-542c76aacc48';
+
+    protected const ERROR_NAMES = [
+        self::INVALID_YAML_ERROR => 'INVALID_YAML_ERROR',
+    ];
+
+    /**
+     * @deprecated since Symfony 6.1, use const ERROR_NAMES instead
+     */
+    protected static $errorNames = self::ERROR_NAMES;
+
+    public $message = 'This value should be valid YAML.';
+    public $flags = 0;
+
+    public function __construct(
+        ?array $options = null,
+        ?string $message = null,
+        ?int $flags = null,
+        ?array $groups = null,
+        mixed $payload = null,
+    ) {
+        parent::__construct($options, $groups, $payload);
+
+        $this->message = $message ?? $this->message;
+        $this->flags = $flags ?? $this->flags;
+    }
+}

63e618c102b8ab5f4159539a4e4c3b0e487cfae0
Simon André <smn.andre@gmail.com>
2024-02-07 02:02:43 +0100

    CS

--- src/Symfony/Component/Emoji/Emojis.php
+++ src/Symfony/Component/Emoji/Emojis.php
@@ -1,7 +1,20 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Component\Emoji;
 
+/**
+ * @author Simon André <smn.andre@gmail.com>
+ * @author Grégoire Pineau <lyrixx@lyrixx.info>
+ */
 final class Emojis
 {
     /**

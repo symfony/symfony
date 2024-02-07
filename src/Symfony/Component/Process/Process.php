8b376d367598d0e96c303d27207b93ddd79931ea
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-02-07 00:34:56 -0300

    Code style

--- src/Symfony/Component/Process/Process.php
+++ src/Symfony/Component/Process/Process.php
@@ -1292,19 +1292,19 @@ class Process implements \IteratorAggregate
 
         $running = $this->processInformation['running'];
 
-	    /*
+        /*
          * In PHP < 8.3, "proc_get_status" only returns the correct exit status on the first call. 
          * Subsequent calls return -1 as the process is discarded. This workaround caches the first 
          * retrieved exit status for consistent results in later calls, mimicking PHP 8.3 behavior.
          */
-        if (version_compare(PHP_VERSION, '8.3.0', '<')) {
+        if (version_compare(\PHP_VERSION, '8.3.0', '<')) {
             static $cachedExitCode = null;
         
-            if (is_null($cachedExitCode) && !$running && $this->processInformation['exitcode'] !== -1) {
+            if (null === $cachedExitCode && !$running && $this->processInformation['exitcode'] !== -1) {
                 $cachedExitCode = $this->processInformation['exitcode'];
             }
         
-            if (!is_null($cachedExitCode) && !$running && $this->processInformation['exitcode'] === -1) {
+            if (null !== $cachedExitCode && !$running && $this->processInformation['exitcode'] === -1) {
                 $this->processInformation['exitcode'] = $cachedExitCode;
             }
         }

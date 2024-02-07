b60834599ad3ab8f5aeec46da0bcee422610e683
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-02-07 00:31:48 -0300

    Typo

--- src/Symfony/Component/Process/Process.php
+++ src/Symfony/Component/Process/Process.php
@@ -1290,6 +1290,8 @@ class Process implements \IteratorAggregate
 
         $this->processInformation = proc_get_status($this->process);
 
+        $running = $this->processInformation['running'];
+
 	    /*
          * In PHP < 8.3, "proc_get_status" only returns the correct exit status on the first call. 
          * Subsequent calls return -1 as the process is discarded. This workaround caches the first 
@@ -1306,8 +1308,6 @@ class Process implements \IteratorAggregate
                 $this->processInformation['exitcode'] = $cachedExitCode;
             }
         }
-        
-        $running = $this->processInformation['running'];
 
         $this->readPipes($running && $blocking, '\\' !== \DIRECTORY_SEPARATOR || !$running);
 

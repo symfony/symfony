e8fbd7d9883ed0fbb97b0db7a323ae47388290fd
Christian Flothmann <christian.flothmann@qossmic.com>
2024-03-19 10:40:14 +0100

    fix merge

--- src/Symfony/Component/Serializer/Tests/Normalizer/ObjectNormalizerTest.php
+++ src/Symfony/Component/Serializer/Tests/Normalizer/ObjectNormalizerTest.php
@@ -854,7 +854,7 @@ class ObjectNormalizerTest extends TestCase
 
     public function testSamePropertyAsMethodWithPropertySerializedName()
     {
-        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+        $classMetadataFactory = new ClassMetadataFactory(new AttributeLoader());
         $this->normalizer = new ObjectNormalizer($classMetadataFactory, new MetadataAwareNameConverter($classMetadataFactory));
         $this->normalizer->setSerializer($this->serializer);
 
@@ -871,7 +871,7 @@ class ObjectNormalizerTest extends TestCase
 
     public function testSamePropertyAsMethodWithMethodSerializedName()
     {
-        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+        $classMetadataFactory = new ClassMetadataFactory(new AttributeLoader());
         $this->normalizer = new ObjectNormalizer($classMetadataFactory, new MetadataAwareNameConverter($classMetadataFactory));
         $this->normalizer->setSerializer($this->serializer);
 

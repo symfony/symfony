e0d97f8629f0e2608fc876ca37d71d855526cea2
Christian Flothmann <christian.flothmann@qossmic.com>
2024-04-24 19:39:38 +0200

    read form values using the chain data accessor

--- src/Symfony/Component/Form/Extension/Core/DataAccessor/PropertyPathAccessor.php
+++ src/Symfony/Component/Form/Extension/Core/DataAccessor/PropertyPathAccessor.php
@@ -12,7 +12,9 @@
 namespace Symfony\Component\Form\Extension\Core\DataAccessor;
 
 use Symfony\Component\Form\DataAccessorInterface;
+use Symfony\Component\Form\DataMapperInterface;
 use Symfony\Component\Form\Exception\AccessException;
+use Symfony\Component\Form\Extension\Core\DataMapper\DataMapper;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\PropertyAccess\Exception\AccessException as PropertyAccessException;
 use Symfony\Component\PropertyAccess\Exception\NoSuchIndexException;
@@ -57,15 +59,25 @@ class PropertyPathAccessor implements DataAccessorInterface
             throw new AccessException('Unable to write the given value as no property path is defined.');
         }
 
+        $getValue = function () use ($data, $form, $propertyPath) {
+            $dataMapper = $this->getDataMapper($form);
+
+            if ($dataMapper instanceof DataMapper && null !== $dataAccessor = $dataMapper->getDataAccessor()) {
+                return $dataAccessor->getValue($data, $form);
+            }
+
+            return $this->getPropertyValue($data, $propertyPath);
+        };
+
         // If the field is of type DateTimeInterface and the data is the same skip the update to
         // keep the original object hash
-        if ($propertyValue instanceof \DateTimeInterface && $propertyValue == $this->getPropertyValue($data, $propertyPath)) {
+        if ($propertyValue instanceof \DateTimeInterface && $propertyValue == $getValue()) {
             return;
         }
 
         // If the data is identical to the value in $data, we are
         // dealing with a reference
-        if (!\is_object($data) || !$form->getConfig()->getByReference() || $propertyValue !== $this->getPropertyValue($data, $propertyPath)) {
+        if (!\is_object($data) || !$form->getConfig()->getByReference() || $propertyValue !== $getValue()) {
             $this->propertyAccessor->setValue($data, $propertyPath, $propertyValue);
         }
     }
@@ -104,5 +116,14 @@ class PropertyPathAccessor implements DataAccessorInterface
 
             return null;
         }
+    }
+
+    private function getDataMapper(FormInterface $form): ?DataMapperInterface
+    {
+        do {
+            $dataMapper = $form->getConfig()->getDataMapper();
+        } while (null === $dataMapper && null !== $form = $form->getParent());
+
+        return $dataMapper;
     }
 }

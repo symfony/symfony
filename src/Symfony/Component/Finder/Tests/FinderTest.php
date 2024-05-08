5b4734971ccad69e3ca96d126f9331669fb749a8
Josh Richards <josh.t.richards@gmail.com>
2024-05-08 17:19:05 -0400

    add test for sub-root subdir
    Signed-off-by: Josh Richards <josh.t.richards@gmail.com>

--- src/Symfony/Component/Finder/Tests/FinderTest.php
+++ src/Symfony/Component/Finder/Tests/FinderTest.php
@@ -365,6 +365,26 @@ class FinderTest extends Iterator\RealIteratorTestCase
             'qux_12_0.php',
             'qux_2_0.php',
         ]), $finder->in(self::$tmpDir)->getIterator());
+
+        $finder = $this->buildFinder();
+        $this->assertSame($finder, $finder->exclude('/toto/foo'));
+        $this->assertIterator($this->toAbsolute([
+            'foo',
+            'foo/bar.tmp',
+            'test.php',
+            'test.py',
+            'toto',
+            'foo bar',
+            'qux',
+            'qux/baz_100_1.py',
+            'qux/baz_1_2.py',
+            'qux_0_1.php',
+            'qux_1000_1.php',
+            'qux_1002_0.php',
+            'qux_10_2.php',
+            'qux_12_0.php',
+            'qux_2_0.php',
+        ]), $finder->in(self::$tmpDir)->getIterator());
     }
 
     public function testIgnoreVCS()

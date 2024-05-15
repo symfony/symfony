ac2461025c3b483fc68919b47e5f663f9585411f
Christian Flothmann <christian.flothmann@qossmic.com>
2024-05-15 17:14:34 +0200

    inline ParamFilesMerger methods

--- src/Symfony/Component/Form/Util/FormUtil.php
+++ src/Symfony/Component/Form/Util/FormUtil.php
@@ -11,6 +11,8 @@
 
 namespace Symfony\Component\Form\Util;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 /**
  * @author Bernhard Schussek <bschussek@gmail.com>
  */
@@ -47,6 +49,100 @@ class FormUtil
      */
     public static function mergeParamsAndFiles(array $params, array $files): array
     {
-        return (new ParamFilesMerger([], $params, $files))->getResult();
+        return self::mergeAtPath($params, $files);
+    }
+
+    private static function mergeAtPath(array $params, array $files, array $path = [])
+    {
+        $paramsValue = self::getValueAtPath($params, $path);
+        $filesValue = self::getValueAtPath($files, $path);
+
+        if (null === $paramsValue) {
+            return $filesValue;
+        }
+
+        if (\is_array($paramsValue) && self::isFileUpload($filesValue)) {
+            return $filesValue; // if the array is a file upload field, it has the precedence
+        }
+
+        if (\is_array($paramsValue) && \is_array($filesValue)) {
+            // if both are lists and both does not contain array, then merge them and return
+            if (array_is_list($paramsValue) && FormUtil::doesNotContainNonFileUploadArray($paramsValue) && array_is_list($filesValue) && FormUtil::doesNotContainNonFileUploadArray($filesValue)) {
+                return array_merge($paramsValue, $filesValue);
+            }
+
+            // heuristics to preserve order, the bigger array wins
+            if (\count($filesValue) > \count($paramsValue)) {
+                $keys = array_unique(array_merge(array_keys($filesValue), array_keys($paramsValue)));
+            } else {
+                $keys = array_unique(array_merge(array_keys($paramsValue), array_keys($filesValue)));
+            }
+
+            $result = [];
+
+            foreach ($keys as $key) {
+                $subPath = $path;
+                $subPath[] = $key;
+
+                $result[$key] = self::mergeAtPath($params, $files, $subPath);
+            }
+
+            return $result;
+        }
+
+        if (\is_array($paramsValue)) {
+            return $paramsValue; // params has the precedence
+        }
+
+        if (self::isFileUpload($filesValue)) {
+            return $filesValue; // if the array is a file upload field, it has the precedence
+        }
+
+        return $paramsValue;
+    }
+
+    private static function getValueAtPath(array $params, array $path)
+    {
+        foreach ($path as $key) {
+            if (null === $params = $params[$key] ?? null) {
+                return null;
+            }
+        }
+
+        return $params;
+    }
+
+    /**
+     * @param UploadedFile|array $value
+     */
+    private static function isFileUpload($value): bool
+    {
+        if ($value instanceof UploadedFile) {
+            return true;
+        }
+
+        if (!\is_array($value) || !\in_array(\count($value), [5, 6], true)) {
+            return false;
+        }
+
+        if (\array_key_exists('full_path', $value)) {
+            unset($value['full_path']);
+        }
+
+        $keys = array_keys($value);
+        sort($keys);
+
+        return ['error', 'name', 'size', 'tmp_name', 'type'] === $keys;
+    }
+
+    private static function doesNotContainNonFileUploadArray(array $array): bool
+    {
+        foreach ($array as $value) {
+            if (\is_array($value) && !self::isFileUpload($value)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

e9de5a0b95723ffdfb5db7457d3588ecf0095cd5
Alexandre Daubois <alex.daubois@gmail.com>
2024-06-03 13:09:03 +0200

    [DependencyInjection] Add tests for repeating `#[Autoconfigure]` attributes

--- src/Symfony/Component/DependencyInjection/Tests/Compiler/RegisterAutoconfigureAttributesPassTest.php
+++ src/Symfony/Component/DependencyInjection/Tests/Compiler/RegisterAutoconfigureAttributesPassTest.php
@@ -19,6 +19,12 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Tests\Fixtures\AutoconfigureAttributed;
 use Symfony\Component\DependencyInjection\Tests\Fixtures\AutoconfiguredInterface;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\AutoconfigureRepeated;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\AutoconfigureRepeatedBindings;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\AutoconfigureRepeatedCalls;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\AutoconfigureRepeatedOverwrite;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\AutoconfigureRepeatedProperties;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\AutoconfigureRepeatedTag;
 use Symfony\Component\DependencyInjection\Tests\Fixtures\ParentNotExists;
 
 /**
@@ -75,6 +81,99 @@ class RegisterAutoconfigureAttributesPassTest extends TestCase
             ->addTag(AutoconfiguredInterface::class, ['foo' => 123])
         ;
         $this->assertEquals([AutoconfiguredInterface::class => $expected], $container->getAutoconfiguredInstanceof());
+    }
+
+    public function testAutoconfiguredRepeated()
+    {
+        $container = new ContainerBuilder();
+        $container->register('foo', AutoconfigureRepeated::class)
+            ->setAutoconfigured(true);
+
+        (new RegisterAutoconfigureAttributesPass())->process($container);
+
+        $expected = (new ChildDefinition(''))
+            ->setLazy(true)
+            ->setPublic(true)
+            ->setShared(false);
+
+        $this->assertEquals([AutoconfigureRepeated::class => $expected], $container->getAutoconfiguredInstanceof());
+    }
+
+    public function testAutoconfiguredRepeatedOverwrite()
+    {
+        $container = new ContainerBuilder();
+        $container->register('foo', AutoconfigureRepeatedOverwrite::class)
+            ->setAutoconfigured(true);
+
+        (new RegisterAutoconfigureAttributesPass())->process($container);
+
+        $expected = (new ChildDefinition(''))
+            ->setLazy(true)
+            ->setPublic(false)
+            ->setShared(true);
+
+        $this->assertEquals([AutoconfigureRepeatedOverwrite::class => $expected], $container->getAutoconfiguredInstanceof());
+    }
+
+    public function testAutoconfiguredRepeatedTag()
+    {
+        $container = new ContainerBuilder();
+        $container->register('foo', AutoconfigureRepeatedTag::class)
+            ->setAutoconfigured(true);
+
+        (new RegisterAutoconfigureAttributesPass())->process($container);
+
+        $expected = (new ChildDefinition(''))
+            ->addTag('foo', ['priority' => 2])
+            ->addTag('bar');
+
+        $this->assertEquals([AutoconfigureRepeatedTag::class => $expected], $container->getAutoconfiguredInstanceof());
+    }
+
+    public function testAutoconfiguredRepeatedCalls()
+    {
+        $container = new ContainerBuilder();
+        $container->register('foo', AutoconfigureRepeatedCalls::class)
+            ->setAutoconfigured(true);
+
+        (new RegisterAutoconfigureAttributesPass())->process($container);
+
+        $expected = (new ChildDefinition(''))
+            ->addMethodCall('setBar', ['arg2'])
+            ->addMethodCall('setFoo', ['arg1']);
+
+        $this->assertEquals([AutoconfigureRepeatedCalls::class => $expected], $container->getAutoconfiguredInstanceof());
+    }
+
+    public function testAutoconfiguredRepeatedBindingsOverwrite()
+    {
+        $container = new ContainerBuilder();
+        $container->register('foo', AutoconfigureRepeatedBindings::class)
+            ->setAutoconfigured(true);
+
+        (new RegisterAutoconfigureAttributesPass())->process($container);
+
+        $expected = (new ChildDefinition(''))
+            ->setBindings(['$arg' => new BoundArgument('bar', false, BoundArgument::INSTANCEOF_BINDING, realpath(__DIR__.'/../Fixtures/AutoconfigureRepeatedBindings.php'))]);
+
+        $this->assertEquals([AutoconfigureRepeatedBindings::class => $expected], $container->getAutoconfiguredInstanceof());
+    }
+
+    public function testAutoconfiguredRepeatedPropertiesOverwrite()
+    {
+        $container = new ContainerBuilder();
+        $container->register('foo', AutoconfigureRepeatedProperties::class)
+            ->setAutoconfigured(true);
+
+        (new RegisterAutoconfigureAttributesPass())->process($container);
+
+        $expected = (new ChildDefinition(''))
+            ->setProperties([
+                '$foo' => 'bar',
+                '$bar' => 'baz',
+            ]);
+
+        $this->assertEquals([AutoconfigureRepeatedProperties::class => $expected], $container->getAutoconfiguredInstanceof());
     }
 
     public function testMissingParent()

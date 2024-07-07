59cfab9da34159aacb8e8056aaefd3a95fa4e2bd
Thomas Unterreitmeier <t.unterreitmeier@icloud.com>
2024-07-07 05:47:45 +0200

    [Messenger][Amqp] Do not use redelivery routing key when sending to failure transport
    The failure transport uses a delay - the retry routing key from the previous stamp would interfere with publishing to the failure exchange/queue

--- src/Symfony/Component/Messenger/Bridge/Amqp/Tests/Transport/AmqpSenderTest.php
+++ src/Symfony/Component/Messenger/Bridge/Amqp/Tests/Transport/AmqpSenderTest.php
@@ -13,11 +13,14 @@ namespace Symfony\Component\Messenger\Bridge\Amqp\Tests\Transport;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Messenger\Bridge\Amqp\Tests\Fixtures\DummyMessage;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpReceivedStamp;
 use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpSender;
 use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
 use Symfony\Component\Messenger\Bridge\Amqp\Transport\Connection;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Exception\TransportException;
+use Symfony\Component\Messenger\Stamp\RedeliveryStamp;
+use Symfony\Component\Messenger\Stamp\SentToFailureTransportStamp;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 
 /**
@@ -50,6 +53,29 @@ class AmqpSenderTest extends TestCase
 
         $connection = $this->createMock(Connection::class);
         $connection->expects($this->once())->method('publish')->with($encoded['body'], $encoded['headers'], 0, $stamp);
+
+        $sender = new AmqpSender($connection, $serializer);
+        $sender->send($envelope);
+    }
+
+    public function testItDoesNotUseRetryRoutingKeyWhenSendingToFailureTransport()
+    {
+        $envelope = (new Envelope(new DummyMessage('Oy')))->with()
+            ->with(new AmqpReceivedStamp(
+                $this->createStub(\AMQPEnvelope::class),
+                'original_receiver'
+            ))
+            ->with(new RedeliveryStamp(1))
+            ->with(new SentToFailureTransportStamp('original_receiver'));
+        $encoded = ['body' => '...', 'headers' => ['type' => DummyMessage::class]];
+
+        $serializer = $this->createStub(SerializerInterface::class);
+        $serializer->method('encode')->with($envelope)->willReturn($encoded);
+
+        $connection = $this->createMock(Connection::class);
+        $connection->expects($this->once())->method('publish')->with($encoded['body'], $encoded['headers'], 0, $this->callback(
+            static fn (AmqpStamp $stamp) => '' === $stamp->getRoutingKey()
+        ));
 
         $sender = new AmqpSender($connection, $serializer);
         $sender->send($envelope);

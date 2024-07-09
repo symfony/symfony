253b5d98a43495362608922b88f9eeefed01c168
Tomas <norkunas.tom@gmail.com>
2024-07-09 14:18:27 +0300

    [Security] Cleanup useless mocks in UserValueResolverTest

--- src/Symfony/Component/Security/Http/Tests/Controller/UserValueResolverTest.php
+++ src/Symfony/Component/Security/Http/Tests/Controller/UserValueResolverTest.php
@@ -91,7 +91,6 @@ class UserValueResolverTest extends TestCase
         $tokenStorage->setToken($token);
 
         $resolver = new UserValueResolver($tokenStorage);
-        $metadata = $this->createMock(ArgumentMetadata::class);
         $metadata = new ArgumentMetadata('foo', null, false, false, null, false, [new CurrentUser()]);
 
         $this->assertSame([$user], $resolver->resolve(Request::create('/'), $metadata));
@@ -105,7 +104,6 @@ class UserValueResolverTest extends TestCase
         $tokenStorage->setToken($token);
 
         $resolver = new UserValueResolver($tokenStorage);
-        $metadata = $this->createMock(ArgumentMetadata::class);
         $metadata = new ArgumentMetadata('foo', InMemoryUser::class, false, false, null, false, [new CurrentUser()]);
 
         $this->assertSame([$user], $resolver->resolve(Request::create('/'), $metadata));

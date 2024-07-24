ed6ba61de248cdf285bc9ccbbfb57499189c855b
Alexandre Daubois <alex.daubois@gmail.com>
2024-07-24 10:06:13 +0200

    [Console][PhpUnitBridge][VarDumper] Fix `NO_COLOR` empty value handling

--- src/Symfony/Component/VarDumper/Dumper/CliDumper.php
+++ src/Symfony/Component/VarDumper/Dumper/CliDumper.php
@@ -606,7 +606,7 @@ class CliDumper extends AbstractDumper
         }
 
         // Follow https://no-color.org/
-        if (isset($_SERVER['NO_COLOR']) || false !== getenv('NO_COLOR')) {
+        if ('' !== ($_SERVER['NO_COLOR'] ?? getenv('NO_COLOR') ?: '')) {
             return false;
         }
 

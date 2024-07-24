6a96ff91161e05300f3c08295106cdec1d848760
Alexandre Daubois <alex.daubois@gmail.com>
2024-07-24 10:06:13 +0200

    [Console][PhpUnitBridge][VarDumper] Fix `NO_COLOR` empty value handling

--- src/Symfony/Component/Console/Output/StreamOutput.php
+++ src/Symfony/Component/Console/Output/StreamOutput.php
@@ -91,7 +91,7 @@ class StreamOutput extends Output
     protected function hasColorSupport()
     {
         // Follow https://no-color.org/
-        if (isset($_SERVER['NO_COLOR']) || false !== getenv('NO_COLOR')) {
+        if ('' !== ($_SERVER['NO_COLOR'] ?? getenv('NO_COLOR') ?: '')) {
             return false;
         }
 

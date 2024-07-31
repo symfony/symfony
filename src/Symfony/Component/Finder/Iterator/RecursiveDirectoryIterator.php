a449a8587b9f9b9430f483a1330f8f21de34dbab
Christian Flothmann <christian.flothmann@qossmic.com>
2024-07-31 14:33:50 +0200

    do not duplicate directory separators

--- src/Symfony/Component/Finder/Iterator/RecursiveDirectoryIterator.php
+++ src/Symfony/Component/Finder/Iterator/RecursiveDirectoryIterator.php
@@ -70,8 +70,9 @@ class RecursiveDirectoryIterator extends \RecursiveDirectoryIterator
             $subPathname .= $this->directorySeparator;
         }
         $subPathname .= $this->getFilename();
+        $basePath = $this->rootPath;
 
-        if ('/' !== $basePath = $this->rootPath) {
+        if ('/' !== $basePath && !str_ends_with($basePath, $this->directorySeparator)) {
             $basePath .= $this->directorySeparator;
         }
 

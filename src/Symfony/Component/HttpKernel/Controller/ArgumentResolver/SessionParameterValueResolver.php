dd43ddf2ade320094054bec04a3112c49907f5b3
Julien Tattevin <julien@tattevin.fr>
2024-08-23 10:14:51 +0200

    Add support for union type

--- src/Symfony/Component/HttpKernel/Controller/ArgumentResolver/SessionParameterValueResolver.php
+++ src/Symfony/Component/HttpKernel/Controller/ArgumentResolver/SessionParameterValueResolver.php
@@ -29,7 +29,13 @@ final class SessionParameterValueResolver implements ValueResolverInterface
         }
 
         if ((!$type = $argument->getType()) || (!class_exists($type) && !interface_exists($type, false))) {
-            throw new \LogicException(\sprintf('#[MapSessionParameter] cannot be used on controller argument "$%s": "%s" is not a class or interface name.', $argument->getName(), $type));
+            if ($type && (str_contains($type, '|') || str_contains($type, '&'))) {
+                if (!$argument->hasDefaultValue() && !$argument->isNullable()) {
+                    throw new \LogicException(\sprintf('#[MapSessionParameter] cannot be used on controller argument "$%s": "%s" is an union or intersection type, you need to make the parameter nullable or provide a default value..', $argument->getName(), $type));
+                }
+            } else {
+                throw new \LogicException(\sprintf('#[MapSessionParameter] cannot be used on controller argument "$%s": "%s" is not a class or interface name.', $argument->getName(), $type));
+            }
         }
 
         if (interface_exists($type, false) && !$argument->hasDefaultValue() && !$argument->isNullable()) {
@@ -39,14 +45,21 @@ final class SessionParameterValueResolver implements ValueResolverInterface
         $name = $attribute->name ?? $argument->getName();
         if ($request->getSession()->has($name)) {
             $value = $request->getSession()->get($name);
-            if (!$value instanceof $type) {
+            if (!$value instanceof $type && !str_contains($type, '|') && !str_contains($type, '&')) {
                 throw new \LogicException(\sprintf('#[MapSessionParameter] cannot be used to map controller argument "$%s": the session contains a value of type "%s" which is not an instance of "%s".', $argument->getName(), get_debug_type($value), $type));
             }
 
             return [$value];
         }
 
-        if (\is_object($value = $argument->hasDefaultValue() ? $argument->getDefaultValue() : new $type())) {
+        if ($argument->hasDefaultValue()) {
+            $value = $argument->getDefaultValue();
+        } else {
+            // handle the type SessionInterface|null $param, which doesn't have a default value and can't be instantiated.
+            $value = class_exists($type, false) ? new $type() : null;
+        }
+
+        if (\is_object($value)) {
             $request->getSession()->set($name, $value);
         }
 

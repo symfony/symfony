9741274d165149f5d95e539b9da11e5d4c092553
mondrake <mondrake.org@gmail.com>
2024-08-30 21:54:20 +0200

    add method to fixture

--- src/Symfony/Component/ErrorHandler/Tests/Fixtures/ReturnTypeInterface.php
+++ src/Symfony/Component/ErrorHandler/Tests/Fixtures/ReturnTypeInterface.php
@@ -8,4 +8,16 @@ interface ReturnTypeInterface
      * @return string
      */
     public function returnTypeInterface();
+
+    /**
+     * @return-typehint-will-change
+     * @return string
+     */
+    public function returnTypeInterfaceWithWillChange();
+
+    /**
+     * @return-typehint-will-change 42.0.0
+     * @return string
+     */
+    public function returnTypeInterfaceWithWillChangeAndVersion();
 }

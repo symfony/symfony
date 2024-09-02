f1bccdcd92d0741eaaa3d9ab5187ff164913f797
Christian Flothmann <christian.flothmann@qossmic.com>
2024-09-02 14:20:02 +0200

    fix package name in deprecation

--- src/Symfony/Bridge/Twig/Node/FormThemeNode.php
+++ src/Symfony/Bridge/Twig/Node/FormThemeNode.php
@@ -28,7 +28,7 @@ final class FormThemeNode extends Node
     public function __construct(Node $form, Node $resources, int $lineno, $only = false)
     {
         if (null === $only || \is_string($only)) {
-            trigger_deprecation('twig/twig', '3.12', 'Passing a tag to %s() is deprecated.', __METHOD__);
+            trigger_deprecation('symfony/twig-bridge', '3.12', 'Passing a tag to %s() is deprecated.', __METHOD__);
             $only = \func_num_args() > 4 ? func_get_arg(4) : true;
         } elseif (!\is_bool($only)) {
             throw new \TypeError(\sprintf('Argument 4 passed to "%s()" must be a boolean, "%s" given.', __METHOD__, get_debug_type($only)));

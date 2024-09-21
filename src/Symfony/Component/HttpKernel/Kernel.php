b6a5496580c64f5ec559ef1d2cbfbe106bb0feec
Fabien Potencier <fabien@potencier.org>
2024-09-21 08:02:06 +0200

    Bump Symfony version to 5.4.45

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -78,12 +78,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static $freshCache = [];
 
-    public const VERSION = '5.4.44';
-    public const VERSION_ID = 50444;
+    public const VERSION = '5.4.45-DEV';
+    public const VERSION_ID = 50445;
     public const MAJOR_VERSION = 5;
     public const MINOR_VERSION = 4;
-    public const RELEASE_VERSION = 44;
-    public const EXTRA_VERSION = '';
+    public const RELEASE_VERSION = 45;
+    public const EXTRA_VERSION = 'DEV';
 
     public const END_OF_MAINTENANCE = '11/2024';
     public const END_OF_LIFE = '11/2025';

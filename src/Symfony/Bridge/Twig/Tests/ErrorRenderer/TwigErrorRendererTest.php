fc8cbe15795fc2374976c28279b54455287c4959
Javier Eguiluz <javier.eguiluz@gmail.com>
2024-10-09 09:25:21 +0200

    Update tests

--- src/Symfony/Bridge/Twig/Tests/ErrorRenderer/TwigErrorRendererTest.php
+++ src/Symfony/Bridge/Twig/Tests/ErrorRenderer/TwigErrorRendererTest.php
@@ -13,6 +13,7 @@ namespace Symfony\Bridge\Twig\Tests\ErrorRenderer;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Bridge\Twig\ErrorRenderer\TwigErrorRenderer;
+use Symfony\Component\ErrorHandler\ErrorRenderer\CliErrorRenderer;
 use Symfony\Component\ErrorHandler\ErrorRenderer\HtmlErrorRenderer;
 use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -21,14 +22,14 @@ use Twig\Loader\ArrayLoader;
 
 class TwigErrorRendererTest extends TestCase
 {
-    public function testFallbackToNativeRendererIfDebugOn()
+    public function testDontUseNativeRenderInCliContext()
     {
         $exception = new \Exception();
 
         $twig = $this->createMock(Environment::class);
-        $nativeRenderer = $this->createMock(HtmlErrorRenderer::class);
+        $nativeRenderer = $this->createMock(CliErrorRenderer::class);
         $nativeRenderer
-            ->expects($this->once())
+            ->expects($this->never())
             ->method('render')
             ->with($exception)
         ;
@@ -36,21 +37,15 @@ class TwigErrorRendererTest extends TestCase
         (new TwigErrorRenderer($twig, $nativeRenderer, true))->render(new \Exception());
     }
 
-    public function testFallbackToNativeRendererIfCustomTemplateNotFound()
+    public function testCliRenderer()
     {
         $exception = new NotFoundHttpException();
-
         $twig = new Environment(new ArrayLoader([]));
 
-        $nativeRenderer = $this->createMock(HtmlErrorRenderer::class);
-        $nativeRenderer
-            ->expects($this->once())
-            ->method('render')
-            ->with($exception)
-            ->willReturn(FlattenException::createFromThrowable($exception))
-        ;
+        $exception = (new TwigErrorRenderer($twig, null, false))->render($exception);
 
-        (new TwigErrorRenderer($twig, $nativeRenderer, false))->render($exception);
+        $this->assertSame('text/plain', $exception->getHeaders()['Content-Type'], 'The exception does not return HTML contents (to prevent potential XSS vulnerabilities)');
+        $this->assertStringContainsString('0;38;5;208m', $exception->getAsString(), 'The exception includes the escape sequence for CLI colorized output');
     }
 
     public function testRenderCustomErrorTemplate()

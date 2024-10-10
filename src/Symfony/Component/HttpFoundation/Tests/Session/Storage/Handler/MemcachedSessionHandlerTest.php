6f097a03c7b88cf8eb856948c260c0c9a0f5296c
Christian Flothmann <christian.flothmann@qossmic.com>
2024-10-10 09:55:54 +0200

    session names must not be empty
    The changes done in #58453 were not enough. Since the session name is used
    as the cookie name it must not be the empty string.

--- src/Symfony/Component/HttpFoundation/Tests/Session/Storage/Handler/MemcachedSessionHandlerTest.php
+++ src/Symfony/Component/HttpFoundation/Tests/Session/Storage/Handler/MemcachedSessionHandlerTest.php
@@ -119,7 +119,7 @@ class MemcachedSessionHandlerTest extends TestCase
 
     public function testDestroySession()
     {
-        $this->storage->open('', '');
+        $this->storage->open('', 'sid');
         $this->memcached
             ->expects($this->once())
             ->method('delete')

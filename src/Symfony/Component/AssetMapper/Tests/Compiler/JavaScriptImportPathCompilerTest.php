ca4a7794bf59dcad0112c797cf5f5d419b658ff7
Tac Tacelosky <tacman@gmail.com>
2024-11-25 05:28:53 -0500

    reduce size ImportPath with comments test

--- src/Symfony/Component/AssetMapper/Tests/Compiler/JavaScriptImportPathCompilerTest.php
+++ src/Symfony/Component/AssetMapper/Tests/Compiler/JavaScriptImportPathCompilerTest.php
@@ -95,41 +95,9 @@ class JavaScriptImportPathCompilerTest extends TestCase
     {
         yield 'import_in_multiline_comment' => [
             'input' => <<<EOF
-// Asynchronous loading
 /**
- * Used in conjunction with the `m` component to reduce bundle size.
- *
- * `m` is a version of the `motion` component that only loads functionality
- * critical for the initial render.
- *
- * `LazyMotion` can then be used to either synchronously or asynchronously
- * load animation and gesture support.
- *
- * ```jsx
- * // Synchronous loading
- * import { LazyMotion, m, domAnimations } from "framer-motion"
- *
- * function App() {
- *   return (
- *     <LazyMotion features={domAnimations}>
- *       <m.div animate={{ scale: 2 }} />
- *     </LazyMotion>
- *   )
- * }
- *
- * // Asynchronous loading
- * import { LazyMotion, m } from "framer-motion"
- *
- * function App() {
- *   return (
- *     <LazyMotion features={() => import('./path/to/domAnimations')}>
- *       <m.div animate={{ scale: 2 }} />
- *     </LazyMotion>
- *   )
- * }
+ *     <LazyMotion features={() => import('./path/to/domAnimations')} />
  * ```
- *
- * @public
  */
 EOF,
             'expectedJavaScriptImports' => [],

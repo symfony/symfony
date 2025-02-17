11a2f8053550b0a343fafd70ff7bb193d85499fc
tinect <s.koenig@tinect.de>
2025-02-17 16:22:28 +0100

    [MIME] use address for body at PathHeader

--- src/Symfony/Component/Mime/Tests/Header/HeadersTest.php
+++ src/Symfony/Component/Mime/Tests/Header/HeadersTest.php
@@ -346,4 +346,11 @@ class HeadersTest extends TestCase
         $this->expectException(\LogicException::class);
         $headers->setHeaderParameter('Content-Disposition', 'name', 'foo');
     }
+
+    public function testPathHeaderHasNoName()
+    {
+        $headers = new Headers();
+        $headers->addPathHeader('Return-Path', 'some@path');
+        $this->assertSame('<some@path>', $headers->get('Return-Path')->getBodyAsString());
+    }
 }

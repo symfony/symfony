ac3ac747be8a57876ad01e9c1006248f64e79f25
Steven Renaux <steven.renaux@sensiolabs.com>
2025-04-24 23:21:16 +0200

    Fix ServiceSubscriberTrait and ServiceMethodsSubscriberTrait for nullable service

--- src/Symfony/Contracts/Service/ServiceMethodsSubscriberTrait.php
+++ src/Symfony/Contracts/Service/ServiceMethodsSubscriberTrait.php
@@ -53,7 +53,7 @@ trait ServiceMethodsSubscriberTrait
             $attribute = $attribute->newInstance();
             $attribute->key ??= self::class.'::'.$method->name;
             $attribute->type ??= $returnType instanceof \ReflectionNamedType ? $returnType->getName() : (string) $returnType;
-            $attribute->nullable = $returnType->allowsNull();
+            $attribute->nullable = $attribute->nullable ?: $returnType->allowsNull();
 
             if ($attribute->attributes) {
                 $services[] = $attribute;

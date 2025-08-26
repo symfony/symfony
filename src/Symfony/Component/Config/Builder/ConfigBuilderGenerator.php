33a072941a74aca943af903b6e60aba58318deb3
Kevin van Sonsbeek <kevin_787@msn.com>
2025-08-26 20:32:14 +0200

    bugfix(61136): Base the iterable type on the normalizeKeys config option

--- src/Symfony/Component/Config/Builder/ConfigBuilderGenerator.php
+++ src/Symfony/Component/Config/Builder/ConfigBuilderGenerator.php
@@ -229,7 +229,7 @@ public function NAME(mixed $valueDEFAULT): static
                 $nodeTypesWithoutArray = array_filter($nodeParameterTypes, static fn ($type) => 'array' !== $type);
                 $body = '
 /**
- * @param ParamConfigurator|list<ParamConfigurator|PROTOTYPE_TYPE>EXTRA_TYPE $value
+ * @param ParamConfigurator|ITERABLE_PARAM_TYPE<ParamConfigurator|PROTOTYPE_TYPE>EXTRA_TYPE $value
  *
  * @return $this
  */
@@ -246,6 +246,7 @@ public function NAME(PARAM_TYPE $value): static
                     'PROTOTYPE_TYPE' => implode('|', $prototypeParameterTypes),
                     'EXTRA_TYPE' => $nodeTypesWithoutArray ? '|'.implode('|', $nodeTypesWithoutArray) : '',
                     'PARAM_TYPE' => $this->getParamType($nodeParameterTypes, true),
+                    'ITERABLE_PARAM_TYPE' => $node->getNormalizeKeys() ? 'list' : 'array',
                 ]);
             } else {
                 $body = '

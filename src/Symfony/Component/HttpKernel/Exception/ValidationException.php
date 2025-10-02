10248c65f286e51ddabed84bf61ef328029f02c7
msenoussi <msenoussi@wanadev.fr>
2025-10-02 15:22:57 +0200

    [HttpKernel] Introduce ValidationException for better validation error handling

---
+++ src/Symfony/Component/HttpKernel/Exception/ValidationException.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\HttpKernel\Exception;
+
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+class ValidationException extends HttpException
+{
+    public function __construct(
+        private mixed $value,
+        private ConstraintViolationListInterface $violations,
+        int $statusCode = 422,
+        string $message = 'Validation failed',
+        ?\Throwable $previous = null,
+        array $headers = [],
+        int $code = 0,
+    ) {
+        parent::__construct($statusCode, $message, $previous, $headers, $code);
+    }
+
+    public function getValue(): mixed
+    {
+        return $this->value;
+    }
+
+    public function getViolations(): ConstraintViolationListInterface
+    {
+        return $this->violations;
+    }
+}

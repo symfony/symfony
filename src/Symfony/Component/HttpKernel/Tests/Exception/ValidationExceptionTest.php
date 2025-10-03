6d507a7fa91a4a917c3f5f4f67b4f5a225865513
msenoussi <msenoussi@wanadev.fr>
2025-10-03 18:33:04 +0200

    [HttpKernel] Enhance ValidationExceptionTest with additional test cases for value and violations

--- src/Symfony/Component/HttpKernel/Tests/Exception/ValidationExceptionTest.php
+++ src/Symfony/Component/HttpKernel/Tests/Exception/ValidationExceptionTest.php
@@ -13,12 +13,41 @@ namespace Symfony\Component\HttpKernel\Tests\Exception;
 
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\ValidationException;
-use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
 
 class ValidationExceptionTest extends HttpExceptionTest
 {
     protected function createException(string $message = '', ?\Throwable $previous = null, int $code = 0, array $headers = []): HttpException
     {
-        return new ValidationException('invalid value', $this->createMock(ConstraintViolationListInterface::class), 422, $message, $previous, $headers, $code);
+        return new ValidationException('invalid value', new ConstraintViolationList(), 422, $message, $previous, $headers, $code);
+    }
+
+    public function testGetValue()
+    {
+        $value = ['test' => 'data'];
+        $exception = new ValidationException($value, new ConstraintViolationList());
+
+        $this->assertSame($value, $exception->getValue());
+    }
+
+    public function testGetViolations()
+    {
+        $value = 'test';
+        $violations = new ConstraintViolationList();
+        $exception = new ValidationException($value, $violations);
+
+        $this->assertSame($violations, $exception->getViolations());
+    }
+
+    public function testDefaultPreviousIsValidationFailedException()
+    {
+        $value = 'test';
+        $violations = new ConstraintViolationList();
+        $exception = new ValidationException($value, $violations);
+
+        $this->assertInstanceOf(ValidationFailedException::class, $exception->getPrevious());
+        $this->assertSame($value, $exception->getPrevious()->getValue());
+        $this->assertSame($violations, $exception->getPrevious()->getViolations());
     }
 }

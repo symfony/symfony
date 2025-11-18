fa3095d4b652d3c10ca042171701eacb712900e2
Evgen Yurchenko <evgenijvy@gmail.com>
2025-11-18 15:32:21 +0200

    version as enum

--- src/Symfony/Contracts/Deprecation/function.php
+++ src/Symfony/Contracts/Deprecation/function.php
@@ -14,14 +14,20 @@ if (!function_exists('trigger_deprecation')) {
      * Triggers a silenced deprecation notice.
      *
      * @param string $package The name of the Composer package that is triggering the deprecation
-     * @param string $version The version of the package that introduced the deprecation
+     * @param PackageVersionInterface|string $version The version of the package that introduced the deprecation
      * @param string $message The message of the deprecation
      * @param mixed  ...$args Values to insert in the message using printf() formatting
      *
      * @author Nicolas Grekas <p@tchwork.com>
      */
-    function trigger_deprecation(string $package, string $version, string $message, mixed ...$args): void
+    function trigger_deprecation(string $package, PackageVersionInterface|string $version, string $message, mixed ...$args): void
     {
+        $version = $version instanceof PackageVersionInterface ? $version->value : $version;
+
         @trigger_error(($package || $version ? "Since $package $version: " : '').($args ? vsprintf($message, $args) : $message), \E_USER_DEPRECATED);
     }
+}
+
+interface PackageVersionInterface extends \StringBackedEnum
+{
 }

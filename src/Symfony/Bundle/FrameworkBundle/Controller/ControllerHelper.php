4987ff793b28b91613cad30efd8104de156a2c8a
Silas Joisten <silasjoisten@proton.me>
2025-11-29 12:53:54 +0100

    [FrameworkBundle] Fix typehint for `ControllerHelper::createForm`

--- src/Symfony/Bundle/FrameworkBundle/Controller/ControllerHelper.php
+++ src/Symfony/Bundle/FrameworkBundle/Controller/ControllerHelper.php
@@ -17,6 +17,8 @@ use Psr\Link\LinkInterface;
 use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Flow\FormFlowInterface;
+use Symfony\Component\Form\Flow\FormFlowTypeInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
@@ -342,6 +344,8 @@ class ControllerHelper implements ServiceSubscriberInterface
 
     /**
      * Creates and returns a Form instance from the type of the form.
+     *
+     * @return ($type is class-string<FormFlowTypeInterface> ? FormFlowInterface : FormInterface)
      */
     public function createForm(string $type, mixed $data = null, array $options = []): FormInterface
     {

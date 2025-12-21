065bca9480c0744dae5ee69fbc09f7656b9b8da0
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-12-21 01:39:42 +0100

    Dumper/HtmlDumper.php

--- src/Symfony/Component/VarDumper/Dumper/HtmlDumper.php
+++ src/Symfony/Component/VarDumper/Dumper/HtmlDumper.php
@@ -883,7 +883,7 @@ class HtmlDumper extends CliDumper
             1 === \count($dumpClasses) ? '' : '"',
             implode(' ', $dumpClasses),
             $dumpTitle ? ' title="'.$dumpTitle.'"' : '',
-            preg_replace_callback(static::$controlCharsRx, function ($c) use ($map) {
+            preg_replace_callback(static::$controlCharsRx, static function ($c) use ($map) {
                 $s = $b = '<span class="sf-dump-default';
                 $c = $c[$i = 0];
                 if ($ns = "\r" === $c[$i] || "\n" === $c[$i]) {
@@ -907,7 +907,7 @@ class HtmlDumper extends CliDumper
         );
 
         if (!($attr['binary'] ?? false)) {
-            $v = preg_replace_callback(static::$unicodeCharsRx, function ($c) {
+            $v = preg_replace_callback(static::$unicodeCharsRx, static function ($c) {
                 return '<span class=sf-dump-default>\u{'.strtoupper(dechex(mb_ord($c[0]))).'}</span>';
             }, $v);
         }

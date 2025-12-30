d5782417f2a416b934492f342fb5044528d200f5
Christian Flothmann <christian.flothmann@open.de>
2025-12-30 12:21:01 +0100

    do not use PHPUnit mock objects without configured expectations

--- src/Symfony/Bridge/Monolog/Tests/Handler/ConsoleHandlerTest.php
+++ src/Symfony/Bridge/Monolog/Tests/Handler/ConsoleHandlerTest.php
@@ -23,6 +23,7 @@ use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;

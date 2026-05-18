2a47df98530d93b932852b0e608afa6fc33b7253
Valentin RAGOT <46323272+ValentinRgt@users.noreply.github.com>
2026-05-18 12:05:18 +0200

    [Form] Add MonthType

---
+++ src/Symfony/Component/Form/Extension/Core/Type/MonthType.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Form\Extension\Core\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MonthType extends AbstractType
+{
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $view->vars['type'] = 'month';
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'invalid_message' => 'Please enter a month (YYYY-MM).',
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return TextType::class;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'month';
+    }
+}

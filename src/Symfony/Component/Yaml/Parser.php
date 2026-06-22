f5f4c7736b94373498a0e29d2a0e0e56375f88ff
Eyüp Can Akman <eyupcanakman@gmail.com>
2026-06-22 17:12:31 +0300

    [Yaml] Fix line folding after trailing backslashes in quoted strings

--- src/Symfony/Component/Yaml/Parser.php
+++ src/Symfony/Component/Yaml/Parser.php
@@ -1184,7 +1184,7 @@ class Parser
             if ($this->isCurrentLineBlank()) {
                 $previousLineWasNewline = true;
                 $previousLineWasTerminatedWithBackslash = false;
-            } elseif ('\\' === $this->currentLine[-1]) {
+            } elseif ('"' === $quotation && 1 === (\strlen($this->currentLine) - \strlen(rtrim($this->currentLine, '\\'))) % 2) {
                 $previousLineWasNewline = false;
                 $previousLineWasTerminatedWithBackslash = true;
             } else {

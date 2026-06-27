f6a70e81750e06b557496e09851fff58d5a3ff59
Fabien Potencier <fabien@potencier.org>
2026-06-27 11:15:34 +0200

    Bump Symfony version to 7.4.15

--- src/Symfony/Component/HttpKernel/Kernel.php
+++ src/Symfony/Component/HttpKernel/Kernel.php
@@ -74,12 +74,12 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static array $freshCache = [];
 
-    public const VERSION = '7.4.14';
-    public const VERSION_ID = 70414;
+    public const VERSION = '7.4.15-DEV';
+    public const VERSION_ID = 70415;
     public const MAJOR_VERSION = 7;
     public const MINOR_VERSION = 4;
-    public const RELEASE_VERSION = 14;
-    public const EXTRA_VERSION = '';
+    public const RELEASE_VERSION = 15;
+    public const EXTRA_VERSION = 'DEV';
 
     public const END_OF_MAINTENANCE = '11/2028';
     public const END_OF_LIFE = '11/2029';

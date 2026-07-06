5beb97f381c2a1627749fa5d7ed98e65eace77ef
Christian Flothmann <christian.flothmann@open.de>
2026-07-06 09:32:06 +0200

    allow Twig 4

--- src/Symfony/Bridge/Twig/Tests/Node/TransNodeTest.php
+++ src/Symfony/Bridge/Twig/Tests/Node/TransNodeTest.php
@@ -53,6 +53,10 @@ class TransNodeTest extends TestCase
 
     protected function getVariableGetterWithStrictCheck($name)
     {
+        if (Environment::MAJOR_VERSION >= 4) {
+            return \sprintf('(array_key_exists("%1$s", $context) ? $context["%1$s"] : throw new RuntimeError(\'Variable "%1$s" does not exist.\', 0, $this->source))', $name);
+        }
+
         if (Environment::MAJOR_VERSION >= 2) {
             return \sprintf('(isset($context["%1$s"]) || array_key_exists("%1$s", $context) ? $context["%1$s"] : (function () { throw new %2$s(\'Variable "%1$s" does not exist.\', 0, $this->source); })())', $name, Environment::VERSION_ID >= 20700 ? 'RuntimeError' : 'Twig_Error_Runtime');
         }

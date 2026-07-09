2c240bdf92b1d44d334bada7e926420f35e8a5ad
Javier Eguiluz <javier.eguiluz@gmail.com>
2026-07-09 17:06:31 +0200

    [Console] Fix SymfonyStyle questions when using output sections

--- src/Symfony/Component/Console/Tests/Style/SymfonyStyleTest.php
+++ src/Symfony/Component/Console/Tests/Style/SymfonyStyleTest.php
@@ -228,11 +228,41 @@ class SymfonyStyleTest extends TestCase
             'start'.\PHP_EOL. // write start
             'foo'.\PHP_EOL. // write foo
             "\x1b[1A\x1b[0Jfoo and bar".\PHP_EOL. // complete line
-            \PHP_EOL." \033[32mDummy question?\033[39m:".\PHP_EOL.' > '.\PHP_EOL.\PHP_EOL. // question
+            \PHP_EOL." \033[32mDummy question?\033[39m:".\PHP_EOL.' > '.\PHP_EOL. // question
             'foo2'.\PHP_EOL. // write foo2
             'bar2'.\PHP_EOL. // write bar
-            "\033[9A\033[0J"), // clear 9 lines (8 output lines and one from the answer input return)
+            "\033[8A\033[0J"), // clear 8 lines (7 output lines and one from the answer input return)
             escapeshellcmd(stream_get_contents($output->getStream()))
         );
+    }
+
+    public function testAskQuestionInSectionMatchesNonSectionOutput()
+    {
+        // asking a question inside a ConsoleSectionOutput must not leave an extra
+        // blank line below the answer: the output must match a regular StreamOutput.
+        $render = function (StreamOutput $output): string {
+            $inputStream = fopen('php://memory', 'r+');
+            fwrite($inputStream, 'Answer'.\PHP_EOL);
+            rewind($inputStream);
+            $input = $this->createStub(Input::class);
+            $input->method('isInteractive')->willReturn(true);
+            $input->method('getStream')->willReturn($inputStream);
+
+            $style = new SymfonyStyle($input, $output);
+            $style->writeln('before');
+            $style->ask('Dummy question?');
+            $style->writeln('after');
+
+            rewind($output->getStream());
+
+            return stream_get_contents($output->getStream());
+        };
+
+        $sections = [];
+        $sectionOutput = $render(new ConsoleSectionOutput(fopen('php://memory', 'r+', false), $sections, StreamOutput::VERBOSITY_NORMAL, true, new OutputFormatter()));
+        $streamOutput = $render(new StreamOutput(fopen('php://memory', 'r+', false), StreamOutput::VERBOSITY_NORMAL, true, new OutputFormatter()));
+
+        $this->assertSame($streamOutput, $sectionOutput);
+        $this->assertStringNotContainsString(' > '.\PHP_EOL.\PHP_EOL, $sectionOutput);
     }
 }

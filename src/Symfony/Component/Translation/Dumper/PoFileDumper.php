cc66b5cbbf113f1a3f6bf6c7e937640eeeb8eb3f
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-07-29 23:05:52 +0200

    [Translation] Do not split ICU messages on pipes in PoFileDumper

--- src/Symfony/Component/Translation/Dumper/PoFileDumper.php
+++ src/Symfony/Component/Translation/Dumper/PoFileDumper.php
@@ -30,6 +30,8 @@ class PoFileDumper extends FileDumper
         $output .= "\n";
 
         $newLine = false;
+        $isIntlDomain = str_ends_with($domain, MessageCatalogue::INTL_DOMAIN_SUFFIX);
+
         foreach ($messages->all($domain) as $source => $target) {
             if ($newLine) {
                 $output .= "\n";
@@ -48,8 +50,9 @@ class PoFileDumper extends FileDumper
                 $output .= $this->formatComments(implode(' ', (array) $metadata['sources']), ':');
             }
 
-            $sourceRules = $this->getStandardRules($source);
-            $targetRules = $this->getStandardRules($target);
+            // in an ICU domain the pipe is an ordinary character, pluralization is expressed by the message itself
+            $sourceRules = $isIntlDomain ? [] : $this->getStandardRules($source);
+            $targetRules = $isIntlDomain ? [] : $this->getStandardRules($target);
             if (2 == \count($sourceRules) && [] !== $targetRules) {
                 $output .= \sprintf('msgid "%s"'."\n", $this->escape($sourceRules[0]));
                 $output .= \sprintf('msgid_plural "%s"'."\n", $this->escape($sourceRules[1]));

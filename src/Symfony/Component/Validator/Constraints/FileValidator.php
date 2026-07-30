6765d4636c9fc86d3d71a5cc28482a22b7bacc69
Ousama Ben Younes <benyounes.ousama@gmail.com>
2026-07-30 14:37:05 +0000

    [Validator] Check File extensions and mimeTypes independently

--- src/Symfony/Component/Validator/Constraints/FileValidator.php
+++ src/Symfony/Component/Validator/Constraints/FileValidator.php
@@ -233,9 +233,7 @@ class FileValidator extends ConstraintValidator
                     $v = $mimeTypesHelper->getMimeTypes($k);
                 }
 
-                // when the configured mime types share none with the ones of the matched
-                // extension, keep the configured ones so that the file is still checked
-                $mimeTypes = $mimeTypes ? (array_intersect($v, $mimeTypes) ?: $mimeTypes) : (array) $v;
+                $mimeTypes = $mimeTypes ?: (array) $v;
                 break;
             }
 

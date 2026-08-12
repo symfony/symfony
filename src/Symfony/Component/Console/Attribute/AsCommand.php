a6a5fa7f73c29bbd32c0983662743e53504af46e
Dawid Nowak <code@dnowak.pl>
2026-08-12 15:32:35 +0200

    [Console] Allow callable for the `description` and `help` options of `#[AsCommand]`, `#[Argument]` and `#[Option]`

--- src/Symfony/Component/Console/Attribute/AsCommand.php
+++ src/Symfony/Component/Console/Attribute/AsCommand.php
@@ -19,24 +19,30 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD)]
 final class AsCommand
 {
+    public ?string $description;
+    public ?string $help;
+
     /**
      * @param string                            $name        The name of the command, used when calling it (i.e. "cache:clear")
-     * @param string|null                       $description The description of the command, displayed with the help page
+     * @param string|(callable():string)|null   $description The description of the command, displayed with the help page
      * @param string[]                          $aliases     The list of aliases of the command. The command will be executed when using one of them (i.e. "cache:clean")
      * @param bool                              $hidden      If true, the command won't be shown when listing all the available commands, but it can still be run as any other command
-     * @param string|null                       $help        The help content of the command, displayed with the help page
+     * @param string|(callable():string)|null   $help        The help content of the command, displayed with the help page
      * @param string[]                          $usages      The list of usage examples, displayed with the help page
      * @param OutputInterface::VERBOSITY_*|null $listedAt    The verbosity from which the command is listed, null to always list it. It can still be run as any other command
      */
     public function __construct(
         public string $name,
-        public ?string $description = null,
+        string|callable|null $description = null,
         array $aliases = [],
         bool $hidden = false,
-        public ?string $help = null,
+        string|callable|null $help = null,
         public array $usages = [],
         public ?int $listedAt = null,
     ) {
+        $this->description = null === $description || \is_string($description) ? $description : $description();
+        $this->help = null === $help || \is_string($help) ? $help : $help();
+
         if (!$hidden && !$aliases) {
             return;
         }

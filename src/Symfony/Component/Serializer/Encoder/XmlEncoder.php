5ee61d4192867b5be13d7ac4969dd094f08a5ae3
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-08-14 15:48:04 +0200

    [Serializer] Restore the quotes around the resource type in the XmlEncoder exception message

--- src/Symfony/Component/Serializer/Encoder/XmlEncoder.php
+++ src/Symfony/Component/Serializer/Encoder/XmlEncoder.php
@@ -429,7 +429,7 @@ class XmlEncoder implements EncoderInterface, DecoderInterface, NormalizationAwa
             return $this->appendNode($parentNode, $data, $format, $context, 'data');
         }
 
-        throw new NotEncodableValueException('An unexpected value could not be serialized: '.(!\is_resource($data) ? var_export($data, true) : get_resource_type($data).' resource'));
+        throw new NotEncodableValueException('An unexpected value could not be serialized: '.(!\is_resource($data) ? var_export($data, true) : '"'.get_resource_type($data).'" resource'));
     }
 
     /**

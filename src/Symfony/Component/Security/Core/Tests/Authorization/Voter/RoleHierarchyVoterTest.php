6a16edd4e64fc07090ffbd4d493dee67da4a41d4
Nicolas Rigaud <nicolas.rigaud@sensiolabs.com>
2026-08-14 17:27:20 +0200

    [Security] Allow wildcards as placeholders in the role hierarchy
    A key of the hierarchy can now carry a "*" wildcard, so that a whole family of
    roles inherits from the same parents without listing them one by one. A wildcard
    is only recognised between underscores or at the end of the role, so existing
    role names are unaffected.

--- src/Symfony/Component/Security/Core/Tests/Authorization/Voter/RoleHierarchyVoterTest.php
+++ src/Symfony/Component/Security/Core/Tests/Authorization/Voter/RoleHierarchyVoterTest.php
@@ -21,7 +21,11 @@ class RoleHierarchyVoterTest extends RoleVoterTest
     #[DataProvider('getVoteTests')]
     public function testVoteUsingTokenThatReturnsRoleNames($roles, $attributes, $expected)
     {
-        $voter = new RoleHierarchyVoter(new RoleHierarchy(['ROLE_FOO' => ['ROLE_FOOBAR']]));
+        $voter = new RoleHierarchyVoter(new RoleHierarchy([
+            'ROLE_FOO' => ['ROLE_FOOBAR'],
+            'ROLE_FOO_*' => ['ROLE_BAR_A', 'ROLE_FOO'],
+            'ROLE_BAR_*' => ['ROLE_BAZ'],
+        ]));
 
         $this->assertSame($expected, $voter->vote($this->getTokenWithRoleNames($roles), null, $attributes));
     }
@@ -30,6 +34,12 @@ class RoleHierarchyVoterTest extends RoleVoterTest
     {
         return array_merge(parent::getVoteTests(), [
             [['ROLE_FOO'], ['ROLE_FOOBAR'], VoterInterface::ACCESS_GRANTED],
+            // ROLE_FOO_A => ROLE_FOO_* => ROLE_BAR_A
+            [['ROLE_FOO_A'], ['ROLE_BAR_A'], VoterInterface::ACCESS_GRANTED],
+            // ROLE_FOO_A => ROLE_FOO_* => ROLE_FOO => ROLE_FOOBAR
+            [['ROLE_FOO_A'], ['ROLE_FOOBAR'], VoterInterface::ACCESS_GRANTED],
+            // ROLE_FOO_A => ROLE_FOO_* => ROLE_BAR_A => ROLE_BAR_* => ROLE_BAZ
+            [['ROLE_FOO_A'], ['ROLE_BAZ'], VoterInterface::ACCESS_GRANTED],
         ]);
     }
 

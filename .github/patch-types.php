ee2cedb5c68ca18bf93502f9be1b567d9ba4cd5d
Javier Eguiluz <javier.eguiluz@gmail.com>
2026-08-14 09:46:07 +0200

    [Validator] Add the `message` option to the `Callback` constraint

--- .github/patch-types.php
+++ .github/patch-types.php
@@ -54,6 +54,7 @@ foreach ($loader->getClassMap() as $class => $file) {
         case false !== strpos($file, '/src/Symfony/Component/Serializer/Tests/Fixtures/'):
         case false !== strpos($file, '/src/Symfony/Component/Serializer/Tests/Normalizer/Features/ObjectOuter.php'):
         case false !== strpos($file, '/src/Symfony/Component/TypeInfo/Tests/Fixtures/'):
+        case false !== strpos($file, '/src/Symfony/Component/Validator/Tests/Constraints/Fixtures/CallbackTestWithClosure.php'):
         case false !== strpos($file, '/src/Symfony/Component/Validator/Tests/Constraints/Fixtures/WhenTestWithClosure.php'):
         case false !== strpos($file, '/src/Symfony/Component/Validator/Tests/Fixtures/EntityWithHook.php'):
         case false !== strpos($file, '/src/Symfony/Component/Validator/Tests/Fixtures/NestedAttribute/Entity.php'):
--- src/Symfony/Component/Validator/Constraints/CallbackValidator.php
+++ src/Symfony/Component/Validator/Constraints/CallbackValidator.php
@@ -34,7 +34,7 @@ class CallbackValidator extends ConstraintValidator
 
         $method = $constraint->callback;
         if ($method instanceof \Closure) {
-            $method($object, $this->context, $constraint->payload);
+            $return = $method($object, $this->context, $constraint->payload);
         } elseif (\is_array($method)) {
             if (!\is_callable($method)) {
                 if (isset($method[0]) && \is_object($method[0])) {
@@ -43,7 +43,7 @@ class CallbackValidator extends ConstraintValidator
                 throw new ConstraintDefinitionException(json_encode($method).' targeted by Callback constraint is not a valid callable.');
             }
 
-            $method($object, $this->context, $constraint->payload);
+            $return = $method($object, $this->context, $constraint->payload);
         } elseif (null !== $object) {
             if (!method_exists($object, $method)) {
                 throw new ConstraintDefinitionException(\sprintf('Method "%s" targeted by Callback constraint does not exist in class "%s".', $method, get_debug_type($object)));
@@ -52,10 +52,35 @@ class CallbackValidator extends ConstraintValidator
             $reflMethod = new \ReflectionMethod($object, $method);
 
             if ($reflMethod->isStatic()) {
-                $reflMethod->invoke(null, $object, $this->context, $constraint->payload);
+                $return = $reflMethod->invoke(null, $object, $this->context, $constraint->payload);
             } else {
-                $reflMethod->invoke($object, $this->context, $constraint->payload);
+                $return = $reflMethod->invoke($object, $this->context, $constraint->payload);
             }
+        } else {
+            return;
+        }
+
+        if (null === $constraint->message) {
+            return;
+        }
+
+        if (!\is_bool($return)) {
+            $target = $this->context->getClassName();
+
+            if (null !== $target && null !== $propertyName = $this->context->getPropertyName()) {
+                $target .= '::$'.$propertyName;
+            }
+
+            $target ??= $this->context->getPropertyPath();
+
+            throw new ConstraintDefinitionException(\sprintf('The callback targeted by the Callback constraint%s must return a boolean when the "message" option is set, "%s" returned.', $target ? \sprintf(' on "%s"', $target) : '', get_debug_type($return)));
+        }
+
+        if (!$return) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ value }}', $this->formatValue($object))
+                ->setCode(Callback::NOT_SATISFIED_ERROR)
+                ->addViolation();
         }
     }
 }

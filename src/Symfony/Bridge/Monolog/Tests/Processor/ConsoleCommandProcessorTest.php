461a431c6e8aac08166465d6891403d28df2346f
Tom Sommer <mail@tomsommer.dk>
2026-08-16 09:33:45 +0200

    [MonologBridge] Release command data in ConsoleCommandProcessor when the command terminates

--- src/Symfony/Bridge/Monolog/Tests/Processor/ConsoleCommandProcessorTest.php
+++ src/Symfony/Bridge/Monolog/Tests/Processor/ConsoleCommandProcessorTest.php
@@ -14,10 +14,15 @@ namespace Symfony\Bridge\Monolog\Tests\Processor;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bridge\Monolog\Processor\ConsoleCommandProcessor;
 use Symfony\Bridge\Monolog\Tests\RecordFactory;
+use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Event\ConsoleEvent;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class ConsoleCommandProcessorTest extends TestCase
 {
@@ -74,12 +79,104 @@ class ConsoleCommandProcessorTest extends TestCase
         $this->assertSame(self::TEST_NAME, $record['extra']['command']['name']);
     }
 
-    private function getConsoleEvent(): ConsoleEvent
+    public function testCommandDataIsRemovedWhenCommandTerminates()
+    {
+        $processor = new ConsoleCommandProcessor();
+        $processor->addCommandData($this->getConsoleEvent());
+
+        $processor->removeCommandData();
+
+        $record = $processor(RecordFactory::create());
+        $this->assertEquals([], $record['extra']);
+
+        $processor->removeCommandData();
+
+        $record = $processor(RecordFactory::create());
+        $this->assertEquals([], $record['extra']);
+    }
+
+    public function testCommandDataIsRestoredWhenNestedCommandTerminates()
+    {
+        $processor = new ConsoleCommandProcessor();
+        $processor->addCommandData($this->getConsoleEvent());
+        $processor->addCommandData($this->getConsoleEvent('some:nested'));
+
+        $record = $processor(RecordFactory::create());
+        $this->assertSame('some:nested', $record['extra']['command']['name']);
+
+        $processor->removeCommandData();
+
+        $record = $processor(RecordFactory::create());
+        $this->assertSame(self::TEST_NAME, $record['extra']['command']['name']);
+    }
+
+    public function testCommandDataOfNestedCommandSurvivesReset()
+    {
+        $processor = new ConsoleCommandProcessor();
+        $processor->addCommandData($this->getConsoleEvent());
+        $processor->addCommandData($this->getConsoleEvent('some:nested'));
+
+        $processor->reset();
+
+        $record = $processor(RecordFactory::create());
+        $this->assertSame('some:nested', $record['extra']['command']['name']);
+    }
+
+    public function testCommandDataFollowsANestedCommandRun()
+    {
+        $processor = new ConsoleCommandProcessor();
+
+        $dispatcher = new EventDispatcher();
+        $dispatcher->addSubscriber($processor);
+
+        $application = new Application();
+        $application->setAutoExit(false);
+        $application->setCatchExceptions(false);
+        $application->setDispatcher($dispatcher);
+
+        $records = [];
+        $capture = static function (string $key) use ($processor, &$records) {
+            $records[$key] = $processor(RecordFactory::create());
+        };
+
+        $dispatcher->addListener(ConsoleEvents::TERMINATE, static function (ConsoleTerminateEvent $event) use ($capture) {
+            $capture('terminate of '.$event->getCommand()->getName());
+        }, -255);
+
+        $nested = new Command('some:nested');
+        $nested->setCode(static function () use ($capture) {
+            $capture('inside the nested command');
+
+            return 0;
+        });
+
+        $outer = new Command(self::TEST_NAME);
+        $outer->setCode(static function () use ($application, $capture) {
+            $capture('before the nested command');
+            $application->run(new ArrayInput(['command' => 'some:nested']), new NullOutput());
+            $capture('after the nested command');
+
+            return 0;
+        });
+
+        $application->addCommands([$nested, $outer]);
+        $application->run(new ArrayInput(['command' => self::TEST_NAME]), new NullOutput());
+        $capture('after the outer command');
+
+        $this->assertSame(self::TEST_NAME, $records['before the nested command']['extra']['command']['name']);
+        $this->assertSame('some:nested', $records['inside the nested command']['extra']['command']['name']);
+        $this->assertSame('some:nested', $records['terminate of some:nested']['extra']['command']['name']);
+        $this->assertSame(self::TEST_NAME, $records['after the nested command']['extra']['command']['name']);
+        $this->assertSame(self::TEST_NAME, $records['terminate of '.self::TEST_NAME]['extra']['command']['name']);
+        $this->assertSame([], $records['after the outer command']['extra']);
+    }
+
+    private function getConsoleEvent(string $name = self::TEST_NAME): ConsoleEvent
     {
         $input = $this->createStub(InputInterface::class);
         $input->method('getArguments')->willReturn(self::TEST_ARGUMENTS);
         $input->method('getOptions')->willReturn(self::TEST_OPTIONS);
-        $command = new Command(self::TEST_NAME);
+        $command = new Command($name);
 
         return new ConsoleEvent($command, $input, new NullOutput());
     }

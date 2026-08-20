210a3f950d9d20e40e0a24d46ffd802404c1ea1b
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-08-20 19:02:16 +0200

    [Form] Apply the guessed "pattern" and "maxlength" attributes to text inputs only

--- src/Symfony/Component/Form/Tests/FormFactoryTest.php
+++ src/Symfony/Component/Form/Tests/FormFactoryTest.php
@@ -12,6 +12,7 @@
 namespace Symfony\Component\Form\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormFactory;
@@ -144,6 +145,40 @@ class FormFactoryTest extends TestCase
         $this->assertSame('firstName', $builder->getName());
         $this->assertSame(['maxlength' => 20, 'pattern' => '.{5,}', 'class' => 'tinymce'], $builder->getOption('attr'));
         $this->assertInstanceOf(TextType::class, $builder->getType()->getInnerType());
+    }
+
+    public function testCreateBuilderUsesMaxLengthAndPatternForChildrenOfTextType()
+    {
+        $this->guesser1->configureTypeGuess(PasswordType::class, [], Guess::HIGH_CONFIDENCE);
+        $this->guesser1->configureMaxLengthGuess(20, Guess::HIGH_CONFIDENCE);
+        $this->guesser2->configurePatternGuess('.{5,}', Guess::HIGH_CONFIDENCE);
+
+        $builder = $this->factory->createBuilderForProperty('Application\Author', 'firstName');
+
+        $this->assertSame(['maxlength' => 20, 'pattern' => '.{5,}'], $builder->getOption('attr'));
+        $this->assertInstanceOf(PasswordType::class, $builder->getType()->getInnerType());
+    }
+
+    public function testCreateBuilderIgnoresMaxLengthAndPatternForTypesOtherThanTextInputs()
+    {
+        $this->guesser1->configureTypeGuess(ChoiceType::class, [], Guess::HIGH_CONFIDENCE);
+        $this->guesser1->configureMaxLengthGuess(20, Guess::HIGH_CONFIDENCE);
+        $this->guesser2->configurePatternGuess('.{5,}', Guess::HIGH_CONFIDENCE);
+
+        $builder = $this->factory->createBuilderForProperty('Application\Author', 'firstName');
+
+        $this->assertSame([], $builder->getOption('attr'));
+        $this->assertInstanceOf(ChoiceType::class, $builder->getType()->getInnerType());
+    }
+
+    public function testCreateBuilderKeepsExplicitAttributesForTypesOtherThanTextInputs()
+    {
+        $this->guesser1->configureTypeGuess(ChoiceType::class, [], Guess::HIGH_CONFIDENCE);
+        $this->guesser1->configureMaxLengthGuess(20, Guess::HIGH_CONFIDENCE);
+
+        $builder = $this->factory->createBuilderForProperty('Application\Author', 'firstName', null, ['attr' => ['maxlength' => 11]]);
+
+        $this->assertSame(['maxlength' => 11], $builder->getOption('attr'));
     }
 
     public function testCreateBuilderUsesRequiredSettingWithHighestConfidence()

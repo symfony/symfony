4433c314134983a6d6baaa717b951bbf85ff135d
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-08-20 19:20:15 +0200

    [Mailer] Remove metadata headers from the emails sent by the SES HTTP transports

--- src/Symfony/Component/Mailer/Bridge/Amazon/Transport/SesHttpAsyncAwsTransport.php
+++ src/Symfony/Component/Mailer/Bridge/Amazon/Transport/SesHttpAsyncAwsTransport.php
@@ -78,7 +78,7 @@ class SesHttpAsyncAwsTransport extends AbstractTransport
             ]),
             'Content' => [
                 'Raw' => [
-                    'Data' => $message->toString(),
+                    'Data' => $this->getRawData($message),
                 ],
             ],
         ];
@@ -100,5 +100,37 @@ class SesHttpAsyncAwsTransport extends AbstractTransport
         }
 
         return new SendEmailRequest($request);
+    }
+
+    private function getRawData(SentMessage $message): string
+    {
+        $originalMessage = $message->getOriginalMessage();
+
+        if (!$originalMessage instanceof Message) {
+            return $message->toString();
+        }
+
+        $metadataNames = [];
+        foreach ($originalMessage->getHeaders()->all() as $name => $header) {
+            if ($header instanceof MetadataHeader) {
+                $metadataNames[] = $name;
+            }
+        }
+
+        if (!$metadataNames) {
+            return $message->toString();
+        }
+
+        // the metadata is sent as email tags, it must not leak into the delivered email
+        $originalMessage = clone $originalMessage;
+        $headers = $originalMessage->getHeaders();
+        foreach ($metadataNames as $name) {
+            $headers->remove($name);
+        }
+        if (!$headers->has('Message-ID')) {
+            $headers->addIdHeader('Message-ID', $message->getMessageId());
+        }
+
+        return $originalMessage->toString();
     }
 }

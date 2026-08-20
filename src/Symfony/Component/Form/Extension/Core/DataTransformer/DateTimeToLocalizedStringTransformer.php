479a18c3e6586a4ba3dc659508e79ece85d27012
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-08-20 22:31:03 +0200

    [Form] Fix rendering and parsing dates before the Gregorian cutover

--- src/Symfony/Component/Form/Extension/Core/DataTransformer/DateTimeToLocalizedStringTransformer.php
+++ src/Symfony/Component/Form/Extension/Core/DataTransformer/DateTimeToLocalizedStringTransformer.php
@@ -172,6 +172,12 @@ class DateTimeToLocalizedStringTransformer extends BaseDateTimeTransformer
         $calendar = $this->calendar;
         $pattern = $this->pattern;
 
+        if (\IntlDateFormatter::GREGORIAN === $calendar && class_exists(\IntlGregorianCalendar::class, false)) {
+            // ICU uses the Julian calendar before October 1582, PHP always uses the Gregorian one
+            $calendar = new \IntlGregorianCalendar($timezone, \Locale::getDefault());
+            $calendar->setGregorianChange(-\PHP_FLOAT_MAX);
+        }
+
         $intlDateFormatter = new \IntlDateFormatter(\Locale::getDefault(), $dateFormat, $timeFormat, $timezone, $calendar, $pattern ?? '');
 
         // new \intlDateFormatter may return null instead of false in case of failure, see https://bugs.php.net/66323

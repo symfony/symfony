7a52a262a83f9db574455aa1597c8bdeab53a87c
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-08-31 16:04:16 +0200

    [Webhook] Reject a malformed request body with a 406 instead of a 500

--- src/Symfony/Component/Webhook/Tests/Client/RequestParserTest.php
+++ src/Symfony/Component/Webhook/Tests/Client/RequestParserTest.php
@@ -13,6 +13,10 @@ namespace Symfony\Component\Webhook\Tests\Client;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestMatcher\IsJsonRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcherInterface;
+use Symfony\Component\RemoteEvent\RemoteEvent;
+use Symfony\Component\Webhook\Client\AbstractRequestParser;
 use Symfony\Component\Webhook\Client\RequestParser;
 use Symfony\Component\Webhook\Exception\RejectWebhookException;
 
@@ -24,6 +28,28 @@ class RequestParserTest extends TestCase
 
         $request = new Request();
         $parser = new RequestParser();
+        $parser->parse($request, '$ecret');
+    }
+
+    public function testParseRejectsAJsonBodyThatIsNotAnArray()
+    {
+        $this->expectException(RejectWebhookException::class);
+        $this->expectExceptionMessage('Request body is malformed.');
+
+        $request = Request::create('/', 'POST', [], [], [], ['CONTENT_TYPE' => 'application/json'], '1');
+        $parser = new class extends AbstractRequestParser {
+            protected function getRequestMatcher(): RequestMatcherInterface
+            {
+                return new IsJsonRequestMatcher();
+            }
+
+            protected function doParse(Request $request, #[\SensitiveParameter] string $secret): ?RemoteEvent
+            {
+                $request->toArray();
+
+                return null;
+            }
+        };
         $parser->parse($request, '$ecret');
     }
 }

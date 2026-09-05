61ea0445b7453b4e31662438c1ffe0e46d784c23
Lazizbek Ergashev <lazerg2@gmail.com>
2026-09-06 02:28:37 +0500

    [Security] Reset the login rate limiter on successful login with a peekable limiter

--- src/Symfony/Component/Security/Http/Tests/EventListener/LoginThrottlingListenerTest.php
+++ src/Symfony/Component/Security/Http/Tests/EventListener/LoginThrottlingListenerTest.php
@@ -12,16 +12,20 @@
 namespace Symfony\Component\Security\Http\Tests\EventListener;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\RateLimiter\PeekableRequestRateLimiterInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\RateLimiter\RateLimit;
 use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\RateLimiter\Storage\InMemoryStorage;
+use Symfony\Component\Security\Core\Authentication\Token\NullToken;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\Event\CheckPassportEvent;
 use Symfony\Component\Security\Http\Event\LoginFailureEvent;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 use Symfony\Component\Security\Http\EventListener\LoginThrottlingListener;
 use Symfony\Component\Security\Http\RateLimiter\DefaultLoginRateLimiter;
 use Symfony\Component\Security\Http\Tests\Fixtures\DummyAuthenticator;
@@ -100,6 +104,46 @@ class LoginThrottlingListenerTest extends TestCase
         $this->listener->checkPassport($this->createCheckPassportEvent($passports[0]));
     }
 
+    public function testSuccessfulLoginResetsThrottling()
+    {
+        $request = $this->createRequest();
+        $passport = $this->createPassport('wouter');
+
+        $this->requestStack->push($request);
+
+        for ($i = 0; $i < 2; ++$i) {
+            $this->listener->checkPassport($this->createCheckPassportEvent($passport));
+            $this->listener->onFailedLogin($this->createLoginFailedEvent($passport));
+        }
+
+        $this->listener->checkPassport($this->createCheckPassportEvent($passport));
+        $this->listener->onSuccessfulLogin($this->createLoginSuccessEvent($passport));
+
+        for ($i = 0; $i < 3; ++$i) {
+            $this->listener->checkPassport($this->createCheckPassportEvent($passport));
+            $this->listener->onFailedLogin($this->createLoginFailedEvent($passport));
+        }
+
+        $this->expectException(TooManyLoginAttemptsAuthenticationException::class);
+        $this->listener->checkPassport($this->createCheckPassportEvent($passport));
+    }
+
+    public function testSuccessfulLoginDoesNotResetUntouchedPeekableLimiter()
+    {
+        $request = $this->createRequest();
+        $passport = $this->createPassport('wouter');
+
+        $this->requestStack->push($request);
+
+        $limiter = $this->createMock(PeekableRequestRateLimiterInterface::class);
+        $limiter->expects($this->once())->method('peek')->willReturn(new RateLimit(3, new \DateTimeImmutable(), true, 3));
+        $limiter->expects($this->never())->method('reset');
+
+        $listener = new LoginThrottlingListener($this->requestStack, $limiter);
+        $listener->checkPassport($this->createCheckPassportEvent($passport));
+        $listener->onSuccessfulLogin($this->createLoginSuccessEvent($passport));
+    }
+
     private function createPassport($username)
     {
         return new SelfValidatingPassport(new UserBadge($username));
@@ -108,6 +152,11 @@ class LoginThrottlingListenerTest extends TestCase
     private function createLoginFailedEvent($passport)
     {
         return new LoginFailureEvent(new AuthenticationException(), new DummyAuthenticator(), $this->requestStack->getCurrentRequest(), null, 'main', $passport);
+    }
+
+    private function createLoginSuccessEvent($passport)
+    {
+        return new LoginSuccessEvent(new DummyAuthenticator(), $passport, new NullToken(), $this->requestStack->getCurrentRequest(), null, 'main');
     }
 
     private function createCheckPassportEvent($passport)

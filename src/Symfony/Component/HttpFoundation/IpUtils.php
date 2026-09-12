f90da9fcd9aa01b7b032790a847cda20ce7e7af6
tanguy chenier <tanguychenier@gmail.com>
2026-09-12 23:57:44 +0200

    [HttpFoundation] Compare packed bytes to detect IPv4-mapped addresses in anonymize()
    The mapped and compatible checks compared the address as it was written with the
    canonical form returned by inet_ntop(). Equivalent spellings such as ::FFFF:1.2.3.4
    in upper case, ::ffff:102:304 with the IPv4 part in hexadecimal, or the fully
    expanded form failed that comparison, fell through to the generic IPv6 mask and
    came back as ::, while ::ffff:1.2.3.4 was anonymized as expected.
    
    Comparing the packed bytes against themselves masked leaves the detection
    independent of the notation.

--- src/Symfony/Component/HttpFoundation/IpUtils.php
+++ src/Symfony/Component/HttpFoundation/IpUtils.php
@@ -213,9 +213,9 @@ class IpUtils
         $packedAddress = inet_pton($ip);
         if (4 === \strlen($packedAddress)) {
             $mask = '255.255.255.0';
-        } elseif ($ip === inet_ntop($packedAddress & inet_pton('::ffff:ffff:ffff'))) {
+        } elseif ($packedAddress === ($packedAddress & inet_pton('::ffff:ffff:ffff'))) {
             $mask = '::ffff:ffff:ff00';
-        } elseif ($ip === inet_ntop($packedAddress & inet_pton('::ffff:ffff'))) {
+        } elseif ($packedAddress === ($packedAddress & inet_pton('::ffff:ffff'))) {
             $mask = '::ffff:ff00';
         } else {
             $mask = 'ffff:ffff:ffff:ffff:0000:0000:0000:0000';
